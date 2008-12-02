e1a9c7bb5f05a6630d72371df1bdb8a83aa70348
Clément Gallet <clement.gallet@ens-lyon.org>
2008-12-02 19:34:42 +0000

    Precompute ranks

--- admin/compute_auxiliary_data.php
+++ admin/compute_auxiliary_data.php
@@ -9,6 +9,7 @@ require( '../_header.php' );
 require( '_helpers.php' );
 showDescription();
 computeConciseRecords();
+computeRanks();
 computeCachedDatabase('../cachedDatabase.php');
 require( '../_footer.php' );
 
@@ -56,6 +57,171 @@ function computeConciseRecords () {
       ORDER BY
         $valueSource DESC, personName
     ");
+    echo "... done<br /><br />\n";
+
+    stopTimer( "blah" );
+  }
+}
+
+#----------------------------------------------------------------------
+function computeRanks () {
+#----------------------------------------------------------------------
+
+  foreach( array( array( 'best', 'Single' ), array( 'average', 'Average' )) as $foo ){
+    $valueSource = $foo[0];
+    $valueName = $foo[1];
+
+    startTimer();
+    echo "Building table Ranks$valueName...<br />\n";
+    dbCommand( "DROP TABLE IF EXISTS Ranks$valueName" );
+    dbCommand( "CREATE TABLE Ranks$valueName (
+      `id` INTEGER NOT NULL AUTO_INCREMENT,
+      `personId` VARCHAR(10) NOT NULL DEFAULT '',
+      `eventId` VARCHAR(6) NOT NULL DEFAULT '',
+      `best` INTEGER NOT NULL DEFAULT '0',
+      `worldRank` INTEGER NOT NULL DEFAULT '0',
+      `continentRank` INTEGER NOT NULL DEFAULT '0',
+      `countryRank` INTEGER NOT NULL DEFAULT '0',
+    PRIMARY KEY  (`id`),
+    KEY `fk_persons` (`personId`),
+    KEY `fk_events` (`eventId`))
+    " );
+
+    $world = dbQuery("
+      SELECT
+        min($valueSource) min,
+        personId,
+        eventId
+      FROM 
+        Concise${valueName}Results
+      GROUP BY
+        eventId,
+        personId
+      ORDER BY
+        eventId, min
+    ");
+
+    $rank = 0;
+    $event = $world[0]['eventId'];
+    $value = -42;
+    $count = 1;
+
+
+    foreach( $world as $w ){
+      extract( $w );
+      if( $event != $eventId ){
+        $rank = 0;
+        $count = 1;
+        $value = -42;
+      }
+      if( $value == $min )
+        $count++;
+      else { 
+        $rank += $count;
+        $count = 1;
+      }
+      $ranks[$personId][$eventId][0] = $rank;
+      $event = $eventId;
+      $value = $min;
+    }
+
+    $continent = dbQuery("
+      SELECT
+        min($valueSource) min,
+        personId,
+        eventId,
+        continentId
+      FROM 
+        Concise${valueName}Results
+      GROUP BY
+        eventId,
+        personId
+      ORDER BY
+        eventId, continentId, min
+    ");
+
+    $rank = 0;
+    $event = $continent[0]['eventId'];
+    $ct = $continent[0]['continentId'];
+    $value = -42;
+    $count = 1;
+
+    foreach( $continent as $c ){
+      extract( $c );
+      if(( $event != $eventId ) || ( $ct != $continentId )){
+        $rank = 0;
+        $count = 1;
+        $value = -42;
+      }
+      if( $value == $min )
+        $count++;
+      else { 
+        $rank += $count;
+        $count = 1;
+      }
+      $ranks[$personId][$eventId][1] = $rank;
+      $ranks[$personId][$eventId][3] = $min;
+      $event = $eventId;
+      $value = $min;
+      $ct = $continentId;
+    }
+
+    $country = dbQuery("
+      SELECT
+        min($valueSource) min,
+        personId,
+        eventId,
+        countryId
+      FROM 
+        Concise${valueName}Results
+      GROUP BY
+        eventId,
+        personId
+      ORDER BY
+        eventId, countryId, min
+    ");
+
+    $rank = 0;
+    $event = $country[0]['eventId'];
+    $cy = $country[0]['countryId'];
+    $value = -42;
+    $count = 1;
+
+    foreach( $country as $c ){
+      extract( $c );
+      if(( $event != $eventId ) || ( $cy != $countryId )){ 
+        $rank = 0;
+        $count = 1;
+        $value = -42;
+      }
+      if( $value == $min )
+        $count++;
+      else { 
+        $rank += $count;
+        $count = 1;
+      }
+      $ranks[$personId][$eventId][2] = $rank;
+      $event = $eventId;
+      $cy = $countryId;
+      $value = $min;
+    }
+
+    $first = true;
+    $query = "INSERT INTO Ranks$valueName (personId, eventId, best, worldRank, continentRank, countryRank) VALUES ";
+    foreach( $ranks as $personId => $rankse )
+      foreach( $rankse as $eventId => $rankspe ){
+        if( ! $first ) $query .= ",";
+        $query .= "('$personId', '$eventId', '" . $rankspe[3] . "','";
+        $query .= $rankspe[0] . "','";
+        $query .= $rankspe[1] . "','";
+        $query .= $rankspe[2] . "')";
+        $first = false;
+      }
+
+    unset( $ranks );
+
+    dbCommand("$query");
+
     echo "... done<br /><br />\n";
 
     stopTimer( "blah" );
--- person_personal_records_current.php
+++ person_personal_records_current.php
@@ -8,169 +8,70 @@ function showCurrentPersonalRecords () {
   global $chosenPersonId;
 
   $bests = dbQuery("
-    SELECT * FROM
 
-      (SELECT
-         record.eventId           eventId,
-         event.cellName           eventCellName,
-         record.single            single,
-         count(distinct personId) singleRank,
-         event.format             valueFormat
-       FROM
-         (SELECT eventId, min(valueAndId)/1000000000 single
-            FROM ConciseSingleResults
-            WHERE personId = '$chosenPersonId'
-            GROUP BY eventId) record,
-         ConciseSingleResults result,
-         Events event
-       WHERE 1
-         AND result.eventId = record.eventId
-         AND (result.best < record.single OR personId = '$chosenPersonId')
-         AND event.id = record.eventId
-       GROUP BY
-         eventId
-       ORDER BY
-         event.rank) singles
+  SELECT * FROM
 
+   (SELECT 
+      eventId,
+      best single,
+      worldRank singleRank,
+      continentRank singleRankContinent,
+      countryRank singleRankCountry
+    FROM RanksSingle
+    WHERE personId='$chosenPersonId') singles
+    
     LEFT JOIN
+   
+   (SELECT 
+      eventId eC,
+      best average,
+      worldRank averageRank,
+      continentRank averageRankContinent,
+      countryRank averageRankCountry
+    FROM RanksAverage
+    WHERE personId='$chosenPersonId') average
+    
+    ON eventId = eC,
+    Events event
+    WHERE
+      eventId = event.id
+    ORDER BY
+      event.rank
 
-      (SELECT
-         record.eventId           eventIdContinent,
-         count(distinct personId) singleRankContinent
-       FROM
-         (SELECT eventId, continentId, min(valueAndId)/1000000000 single
-            FROM ConciseSingleResults
-            WHERE personId = '$chosenPersonId'
-            GROUP BY eventId) record,
-         ConciseSingleResults result,
-         Events event
-       WHERE 1
-         AND result.eventId = record.eventId
-         AND (result.best < record.single OR personId = '$chosenPersonId')
-         AND event.id = record.eventId
-         AND result.continentId = record.continentId
-       GROUP BY
-         eventIdContinent
-       ORDER BY
-         event.rank) singlesContinent
 
-    ON eventIdContinent = eventId
-    LEFT JOIN
-
-      (SELECT
-         record.eventId           eventIdCountry,
-         count(distinct personId) singleRankCountry
-       FROM
-         (SELECT eventId, countryId, min(valueAndId)/1000000000 single
-            FROM ConciseSingleResults
-            WHERE personId = '$chosenPersonId'
-            GROUP BY eventId) record,
-         ConciseSingleResults result,
-         Events event
-       WHERE 1
-         AND result.eventId = record.eventId
-         AND (result.best < record.single OR personId = '$chosenPersonId')
-         AND event.id = record.eventId
-         AND result.countryId = record.countryId
-       GROUP BY
-         eventIdCountry
-       ORDER BY
-         event.rank) singlesCountry
-
-    ON eventIdCountry = eventId
-    LEFT JOIN
-
-      (SELECT
-         record.eventId           averageEventId,
-         record.average           average,
-         count(distinct personId) averageRank
-       FROM
-         (SELECT eventId, min(valueAndId)/1000000000 average
-            FROM ConciseAverageResults
-            WHERE personId = '$chosenPersonId'
-            GROUP BY eventId) record,
-         ConciseAverageResults result,
-         Events event
-       WHERE ".randomDebug()."
-         AND result.eventId = record.eventId
-         AND (result.average < record.average OR personId = '$chosenPersonId')
-         AND event.id = record.eventId
-       GROUP BY
-         averageEventId
-       ORDER BY
-         event.rank) averages
-
-    ON eventId = averageEventId
-    LEFT JOIN
-
-      (SELECT
-         record.eventId           averageEventIdContinent,
-         count(distinct personId) averageRankContinent
-       FROM
-         (SELECT eventId, continentId, min(valueAndId)/1000000000 average
-            FROM ConciseAverageResults
-            WHERE personId = '$chosenPersonId'
-            GROUP BY eventId) record,
-         ConciseAverageResults result,
-         Events event
-       WHERE ".randomDebug()."
-         AND result.eventId = record.eventId
-         AND (result.average < record.average OR personId = '$chosenPersonId')
-         AND event.id = record.eventId
-         AND result.continentId = record.continentId
-       GROUP BY
-         averageEventIdContinent
-       ORDER BY
-         event.rank) averagesContinent
-
-    ON averageEventId = averageEventIdContinent
-    LEFT JOIN
-
-      (SELECT
-         record.eventId           averageEventIdCountry,
-         count(distinct personId) averageRankCountry
-       FROM
-         (SELECT eventId, countryId, min(valueAndId)/1000000000 average
-            FROM ConciseAverageResults
-            WHERE personId = '$chosenPersonId'
-            GROUP BY eventId) record,
-         ConciseAverageResults result,
-         Events event
-       WHERE ".randomDebug()."
-         AND result.eventId = record.eventId
-         AND (result.average < record.average OR personId = '$chosenPersonId')
-         AND event.id = record.eventId
-         AND result.countryId = record.countryId
-       GROUP BY
-         averageEventIdCountry
-       ORDER BY
-         event.rank) averagesCountry
-
-    ON averageEventIdCountry = averageEventIdContinent
   ");
 
   tableBegin( 'results', 10 );
   tableCaption( false, "Current Personal Records" );
   tableHeader( split( " ", "Event  Ranks  Single Average  Ranks  " ),
-               array( "", "class='r'", "class='c'", "class='L'", "class='R2'", "class='R2'", "class='R'", "class='c'", "class='l'", "class='f'" ));
+               array( "", "class='r'", "class='c'", "class='R'", "class='R2'", "class='R2'", "class='R'", "class='c'", "class='r'", "class='f'" ));
 
   foreach( $bests as $best ){
     extract( $best );
     tableRow( array(
-      internalEventLink( "#$eventId", $eventCellName ),
-      "<span style='color:#999'>$singleRankCountry</span>",
-      $singleRankContinent,
-      $singleRank,
-      eventLink( $eventId, formatValue( $single, $valueFormat )),
-      eventAverageLink( $eventId, formatValue( $average, $valueFormat )),
-      $averageRank,
-      $averageRankContinent,
-      "<span style='color:#999'>$averageRankCountry</span>",
+      internalEventLink( "#$eventId", eventCellName( $eventId )),
+      "<span style='color:#999'>" . colorMe( $singleRankCountry ) . "</span>",
+      colorMe( $singleRankContinent ),
+      colorMe( $singleRank ),
+      eventLink( $eventId, formatValue( $single, valueFormat( $eventId ) )),
+      eventAverageLink( $eventId, formatValue( $average, valueFormat( $eventId ))),
+      colorMe( $averageRank ),
+      colorMe( $averageRankContinent ),
+      "<span style='color:#999'>" . colorMe( $averageRankCountry ) . "</span>",
       ''
     ));
   }
 
   tableEnd();
+}
+
+#----------------------------------------------------------------------
+function colorMe ( $rank ) {
+#----------------------------------------------------------------------
+  if( $rank == 1 )
+    return "<span style='color:#F60;font-weight:bold'>$rank</span";
+  else
+    return $rank;
 }
 
 ?>
