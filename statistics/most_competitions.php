877b7b2c5d8c037d13da91d91d1953e49e386780
Stefan Pochmann <stefan.pochmann@gmail.com>
2009-11-28 20:22:09 +0000

    Statistics: most_competitions now faster and easier to read.

--- statistics/most_competitions.php
+++ statistics/most_competitions.php
@@ -1,25 +1,35 @@
 <?
 
-$rows = 0;
-foreach( split( ' ', 'personId eventId competition.countryId' ) as $source ){
-  $source2 = ( $source != 'personId' ) ? $source : "personId";
-  $source3 = ( $source != 'personId' ) ? $source : "personName";
-  $r = dbQuery("
-    SELECT $source2, count(DISTINCT competitionId) numberOfCompetitions
-    FROM Results, Competitions competition
-    $WHERE competition.id = competitionId
-    GROUP BY $source
-    ORDER BY numberOfCompetitions DESC, $source3
-    LIMIT 10
-  ");
-  $rows = $rows ? my_merge( $rows, $r ) : $r;
-}
+$person = dbQuery( "
+  SELECT    personId, count(DISTINCT competitionId) numberOfCompetitions
+  FROM      Results
+  GROUP BY  personId
+  ORDER BY  numberOfCompetitions DESC
+  LIMIT     10
+" );
+
+$event = dbQuery( "
+  SELECT    eventId, count(DISTINCT competitionId) numberOfCompetitions
+  FROM      Results
+  GROUP BY  eventId
+  ORDER BY  numberOfCompetitions DESC
+  LIMIT     10
+" );
+
+$country = dbQuery( "
+  SELECT    competition.countryId, count(DISTINCT competitionId) numberOfCompetitions
+  FROM      Results, Competitions competition
+  WHERE     competition.id = competitionId
+  GROUP BY  competition.countryId
+  ORDER BY  numberOfCompetitions DESC
+  LIMIT     10
+" );
 
 $lists[] = array(
   "Most Competitions",
   "",
   "[P] Person [N] Competitions [T] | [E] Event [N] Competitions [T] | [T] Country [N] Competitions",
-  $rows,
+  my_merge( my_merge( $person, $event ), $country ),
   "[Person] In how many competitions the person participated. [Event] In how many competitions the event was included. [Country] How many competitions took place in the country."
 );
 
