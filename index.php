0b79373ec814ff52fda947b924aef495cc2117ca
Stefan Pochmann <stefan.pochmann@gmail.com>
2009-12-02 19:34:56 +0000

    Front index now mentions Clement.

--- index.php
+++ index.php
@@ -43,8 +43,8 @@ function showWelcomeMessage () {
 <div>
 <br />
 <br />
-The WCA result pages are designed and built by <?= externalLink( 'http://www.stefan-pochmann.info/', 'Stefan Pochmann' ) ?>, based on earlier work by <?= externalLink( 'http://www.rubikscube.info/', 'Josef Jelinek' ) ?>.<br />
-We would like to thank Stefan for this great work for the cubing community.<br>
+The WCA result pages are designed and built by <?= externalLink( 'http://www.rubikscube.info/', 'Josef Jelinek' ) ?>, <?= externalLink( 'http://www.stefan-pochmann.info/', 'Stefan Pochmann' ) ?> and <?= externalLink( 'http://www.worldcubeassociation.org/results/p.php?i=2004GALL02', 'Clément Gallet' ) ?>.<br />
+We would like to thank them for their great work for the cubing community.<br>
 <br>
 If you have questions about the rankings or if you want to correct something, then please contact <a href="mailto:rbruchem@worldcubeassociation.org">Ron van Bruchem</a>.
 </div>
