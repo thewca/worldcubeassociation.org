4df109af0481eb90242c51160ad5b7283489a96e
Stefan Pochmann <stefan.pochmann@gmail.com>
2009-12-15 22:29:40 +0000

    Misc: Cleaned up a bit.

--- misc.php
+++ misc.php
@@ -11,10 +11,11 @@ require( '_header.php' );
 <p>This is a place for miscellaneous things having to do with the WCA results.</p>
 
 <dl>
-  <dt><a href='misc/export.html'>WCA database export</a></dt>
-  <dd>Export of the WCA database in several formats, allowing to analyze the data at large.</dd>
   <dt><a href='misc/age_vs_speed.html'>Age vs Speed</a></dt>
   <dd>A little analysis showing the fastest 3x3x3 results at all ages.</dd>
+
+  <dt><a href='misc/export.html'>WCA database export</a></dt>
+  <dd>Export of the WCA database in several formats, allowing to analyze the data at large.</dd>
 </dl>
 
 <?php
