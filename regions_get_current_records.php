84ab25b2ede0d7b0a0f14f466fc0a70d844cd3b8
Stefan Pochmann <stefan.pochmann@gmail.com>
2011-06-17 20:58:42 +0000

    regions_get_current_records.php: Much faster now, thanks to keeping single and average records separated longer.

--- regions_get_current_records.php
+++ regions_get_current_records.php
@@ -1,41 +1,46 @@
 <?php
 
 $results = dbQuery("
-  SELECT
-                     type,
-                     result.*,
-                     value,
-    event.name       eventName,
-    event.cellName   eventCellName,
-                     format,
-    country.name     countryName,
-    competition.cellName competitionName
+  SELECT *
   FROM
-    (SELECT eventId recordEventId, MIN(valueAndId) DIV 1000000000 value, 'Single' type
-     FROM ConciseSingleResults
-     WHERE 1 " . regionCondition('') . eventCondition() . yearCondition() . "
-     GROUP BY eventId
-       UNION
-     SELECT eventId recordEventId, MIN(valueAndId) DIV 1000000000 value, 'Average' type
-     FROM ConciseAverageResults
-     WHERE 1 " . regionCondition('') . eventCondition() . yearCondition() . "
-     GROUP BY eventId) record,
-    Results result,
-    Events event,
-    Countries country,
-    Competitions competition
-  WHERE " . randomDebug() . "
-
-    AND ((type = 'Single'  AND  result.best = value)  OR  (type = 'Average'  AND  result.average = value))
-    " . regionCondition('result') . eventCondition() . yearCondition() . "
-
-    AND result.eventId = recordEventId
-    AND event.id       = result.eventId
-    AND country.id     = result.countryId
-    AND competition.id = result.competitionId
-    AND event.rank < 999
+    (" . regionsGetCurrentRecordsQuery( 'best', 'Single' ) . "
+    UNION
+    " . regionsGetCurrentRecordsQuery( 'average', 'Average' ) . ") helper
   ORDER BY
     rank, type DESC, year, month, day, roundId, personName
 ");
+
+function regionsGetCurrentRecordsQuery ( $valueId, $valueName ) {
+  return
+   "SELECT
+      '$valueName'     type,
+                       result.*,
+                       value,
+      event.name       eventName,
+      event.cellName   eventCellName,
+                       format,
+      country.name     countryName,
+      competition.cellName competitionName,
+                       rank, year, month, day
+    FROM
+      (SELECT eventId recordEventId, MIN(valueAndId) DIV 1000000000 value
+       FROM Concise{$valueName}Results
+       WHERE 1 " . regionCondition('') . eventCondition() . yearCondition() . "
+       GROUP BY eventId) record,
+      Results result,
+      Events event,
+      Countries country,
+      Competitions competition
+    WHERE " . randomDebug() . "
+
+      AND result.$valueId = value
+      " . regionCondition('result') . eventCondition() . yearCondition() . "
+
+      AND result.eventId = recordEventId
+      AND event.id       = result.eventId
+      AND country.id     = result.countryId
+      AND competition.id = result.competitionId
+      AND event.rank < 999";
+}
 
 ?>
