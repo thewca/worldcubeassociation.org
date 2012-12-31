a53f39dbeadc34727ec84c02fa78064f7ca82f3e
Lucas Garron <lucasg@gmx.de>
2012-12-31 13:53:04 -0800

    Page titles.

--- html/create_html.sh
+++ html/create_html.sh
@@ -2,18 +2,21 @@
 
 function htmlify {
   FILE="${1}"
-  SOURCE="${2}"
+  TITLE="${2}"
+  SOURCE="${3}"
 
   rm "${FILE}"
-  cat html_header.html >> "${FILE}"
+  cat html_header_1.html >> "${FILE}"
+  echo "${TITLE}" >> "${FILE}"
+  cat html_header_2.html >> "${FILE}"
   rdiscount "${SOURCE}" >> "${FILE}" # Markdown doesn't handle the nested lists properly.
   cat html_footer.html >> "${FILE}"
 }
 
-htmlify "index.html"        "../wca-documents/wca-regulations-2013.md"
-htmlify "guidelines.html"   "../wca-documents/wca-guidelines-2013.md"
-htmlify "history.html"      "history.md"
-htmlify "translations.html" "translations.md"
-htmlify "scrambles.html"    "scrambles.md"
+htmlify "index.html"        "WCA Regulations 2013"    "../wca-documents/wca-regulations-2013.md"
+htmlify "guidelines.html"   "WCA Guidelines 2013"     "../wca-documents/wca-guidelines-2013.md"
+htmlify "history.html"      "WCa Regulations History" "history.md"
+htmlify "translations.html" "WCA Translations"        "translations.md"
+htmlify "scrambles.html"    "WCA Scrambles"           "scrambles.md"
 
 ./create_html.py
