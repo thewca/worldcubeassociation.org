38f6188fbef1066d7e092a4d41c9597aa1a13414
Lucas Garron <lucasg@gmx.de>
2012-12-31 04:48:16 -0800

    Update for 2013 release.

--- pdf/create_pdf.sh
+++ pdf/create_pdf.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-TEX_FILE="wca-regs.tex"
+TEX_FILE="wca-regulations.tex"
 
 rm "${TEX_FILE}"
-cat tex_header.tex > "${TEX_FILE}"
-pandoc -t latex ../wca-documents/wca-regulations-2012.md >> "${TEX_FILE}"
-pandoc -t latex ../wca-documents/wca-guidelines-2012.md >> "${TEX_FILE}"
+cat tex_header.tex >> "${TEX_FILE}"
+pandoc -t latex ../wca-documents/wca-regulations-2013.md >> "${TEX_FILE}"
+cat tex_middle.tex >> "${TEX_FILE}"
+pandoc -t latex ../wca-documents/wca-guidelines-2013.md >> "${TEX_FILE}"
 cat tex_footer.tex >> "${TEX_FILE}"
 
 pdflatex "${TEX_FILE}"
