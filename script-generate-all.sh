d111ad317e05bb36f7e3312b7f3de761ab2e5fd0
Lucas Garron <lucasg@gmx.de>
2013-02-05 21:20:39 -0800

    Current build scripts.

---
+++ script-generate-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+HTML_FRAGMENT="0"
+if [ "$#" -lt "2" ]
+then
+  HTML_FRAGMENT="${1}"
+fi
+
+rm -rf ./upload
+
+./script-generate-language.sh "${HTML_FRAGMENT}" english
+
+./script-generate-language.sh "${HTML_FRAGMENT}" german
+./script-generate-language.sh "${HTML_FRAGMENT}" indonesian
+./script-generate-language.sh "${HTML_FRAGMENT}" russian
+./script-generate-language.sh "${HTML_FRAGMENT}" chinese
+./script-generate-language.sh "${HTML_FRAGMENT}" hungarian
+
+cd wca-documents
+git checkout master
