16ba8524ca74251129c9c7b48e5860af490362e6
Jim Mertens <jbcm627@gmail.com>
2013-08-19 11:11:56 -0400

    Fix autoloader (no error messages)

--- includes/WCAClasses/autoload.php
+++ includes/WCAClasses/autoload.php
@@ -1,15 +1,11 @@
 <?php
 namespace WCAClasses;
 
-function Load($class) {
+function wcaLoad($class) {
   $file = dirname(__DIR__) . '/' . str_replace("\\", DIRECTORY_SEPARATOR, $class) . ".class.php";
   if(is_file($file) && file_exists($file)) {
     require_once($file);
-  } else {
-    print '<p>Class <pre>' . $class . '</pre> not loaded - could not include ' . $file . '.</p>';
-    print '<p>Working in directory: ' . dirname(__DIR__) .'</p>';
   }
 }
 // autoload WCA classes - assume namespace structure is same as directory structure
-spl_autoload_register("WCAClasses\Load");
-
+spl_autoload_register("WCAClasses\wcaLoad");
