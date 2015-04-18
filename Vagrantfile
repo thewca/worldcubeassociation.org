d0f55a1aca9800b4bb7ae49d449b1a92c07e70b2
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-04-18 01:32:08 -0700

    LAMP and port forwarding.

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.provision :shell, path: "bootstrap.sh"
-  config.vm.network :forwarded_port, host: 4567, guest: 80
+  config.vm.network :forwarded_port, host: 8080, guest: 80
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,15 @@
 
 apt-get update
 
-apt-get install -y apache2
+# Installing mysql-server prompts for a password. Workaround from:
+#  http://stackoverflow.com/a/7740571
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y lamp-server^
 if ! [ -L /var/www/html ]; then
   rm -rf /var/www/html
   ln -fs /vagrant/webroot /var/www/html
 fi
+
+# Enable .htaccess (disabled by default)
+sudo sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
