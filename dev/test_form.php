754a671de5a06248109a287fbfefe6794e4517b9
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-04-18 17:50:02 -0700

    Misc cleanup for cubing/worldcubeassociation.org. Getting rid of .template files wherever possible, and added a generated/cache/ directory to make the results scripts happy. Also removed the unused dev/ directory.

--- dev/test_form.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$currentSection = 'persons';
-require( '../includes/_header.php' );
-
-$form = new WCAClasses\FormBuilder("submission");
-$form->render();
-
-require( '../includes/_footer.php' );
