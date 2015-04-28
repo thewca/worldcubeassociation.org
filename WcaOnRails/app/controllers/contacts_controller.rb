b989e59cefaf5c8ec1ab5220e155b34a5ea45c19
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-04-27 21:18:05 -0700

    Added recaptcha for contact forms.

--- WcaOnRails/app/controllers/contacts_controller.rb
+++ WcaOnRails/app/controllers/contacts_controller.rb
@@ -8,13 +8,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     @contact.to_email = "wrc@worldcubeassociation.org"
     @contact.subject = "WRC Contact Form"
-    if @contact.valid? && @contact.deliver
-      flash[:success] = 'Thank you for your message. We will contact you soon!'
-      redirect_to contact_wrc_url
-    else
-      flash.now[:danger] = 'Could not send message.'
-      render :wrc
-    end
+    maybe_send_email success_url: contact_wrc_url, fail_view: :wrc
   end
 
   def website
@@ -26,12 +20,23 @@ class ContactsController < ApplicationController
     @contact.request = request
     @contact.to_email = "contact@worldcubeassociation.org"
     @contact.subject = "WCA Website Comments"
-    if @contact.valid? && @contact.deliver
+    maybe_send_email success_url: contact_website_url, fail_view: :website
+  end
+
+  private def maybe_send_email(success_url: nil, fail_view: nil)
+    if not @contact.valid?
+      flash.now[:danger] = "Invalid fields, please correct errors below."
+      render fail_view
+    elsif not verify_recaptcha
+      # Convert flash to a flash.now, since we're about to render, not redirect.
+      flash.now[:recaptcha_error] = flash[:recaptcha_error]
+      render fail_view
+    elsif @contact.deliver
       flash[:success] = 'Thank you for your message. We will contact you soon!'
-      redirect_to contact_website_url
+      redirect_to success_url
     else
-      flash.now[:danger] = 'Could not send message.'
-      render :wca
+      flash.now[:danger] = 'Error sending message.'
+      render fail_view
     end
   end
 end
--- WcaOnRails/app/helpers/application_helper.rb
+++ WcaOnRails/app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  def bootstrap_class_for(flash_type)
+    {
+      success: "alert-success",
+      danger: "alert-danger",
+      warning: "alert-warning",
+      info: "alert-info",
+
+      recaptcha_error: "alert-danger"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
 end
