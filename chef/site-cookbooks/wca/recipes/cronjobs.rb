b91fa888eb633ea3f6eeab220e1917c7e301f49c
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-05-15 16:06:02 -0700

    Only run cron when not in development mode.

--- chef/site-cookbooks/wca/recipes/cronjobs.rb
+++ chef/site-cookbooks/wca/recipes/cronjobs.rb
@@ -4,47 +4,55 @@ admin_email = "admin@worldcubeassociation.org"
 
 init_php_commands = []
 init_php_commands << "#{repo_root}/scripts/db.sh dump /secrets/worldcubeassociation.org_alldbs.tar.gz"
-cron "db backup" do
-  minute '0'
-  hour '0'
-  weekday '1'
+if node.chef_environment != "dev"
+  cron "db backup" do
+    minute '0'
+    hour '0'
+    weekday '1'
 
-  mailto admin_email
-  user username
-  command init_php_commands.last
+    mailto admin_email
+    user username
+    command init_php_commands.last
+  end
 end
 
 init_php_commands << "#{repo_root}/scripts/cronned_results_scripts.sh"
-cron "cronned results scripts" do
-  minute '0'
-  hour '4'
-  weekday '1,3,5'
+if node.chef_environment != "dev"
+  cron "cronned results scripts" do
+    minute '0'
+    hour '4'
+    weekday '1,3,5'
 
-  mailto admin_email
-  user username
-  command init_php_commands.last
+    mailto admin_email
+    user username
+    command init_php_commands.last
+  end
 end
 
 init_php_commands << "(cd #{repo_root}/webroot/results/misc/missing_averages; time php update7205.php)"
-cron "Update Missing Averages" do
-  minute '0'
-  hour '0'
-  weekday '*'
+if node.chef_environment != "dev"
+  cron "Update Missing Averages" do
+    minute '0'
+    hour '0'
+    weekday '*'
 
-  mailto admin_email
-  user username
-  command init_php_commands.last
+    mailto admin_email
+    user username
+    command init_php_commands.last
+  end
 end
 
 init_php_commands << "(cd #{repo_root}/webroot/results/misc/evolution; time php update7205.php)"
-cron "Update Evolution of Records" do
-  minute '0'
-  hour '0'
-  weekday '*'
+if node.chef_environment != "dev"
+  cron "Update Evolution of Records" do
+    minute '0'
+    hour '0'
+    weekday '*'
 
-  mailto admin_email
-  user username
-  command init_php_commands.last
+    mailto admin_email
+    user username
+    command init_php_commands.last
+  end
 end
 
 init_php_commands << "(cd #{repo_root}/webroot/results/admin/; time SERVER_NAME=wca REQUEST_URI='doit=live' php compute_auxiliary_data.php;)"
