f8db75842d95acda9004a21f92d2ea2da6dc9608
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-05-21 09:18:29 -0700

    Wow, drupal, not django.

--- WcaOnRails/db/migrate/20150521005227_no_more_drupal.rb
+++ WcaOnRails/db/migrate/20150521005227_no_more_drupal.rb
@@ -6,9 +6,9 @@ class NoMoreDrupal < ActiveRecord::Migration
   end
   class Node < ActiveRecord::Base
     self.table_name = "node"
-    # Django has a "type" column that we don't want ActiveRecord to get excited about.
+    # Drupal has a "type" column that we don't want ActiveRecord to get excited about.
     self.inheritance_column = :_type_disabled
-    # Django also has "changed" column that conflicts with ActiveRecord.
+    # Drupal also has "changed" column that conflicts with ActiveRecord.
     ignore_columns :changed
 
     has_one :field_data_body, -> { where(entity_type: "node") }, primary_key: "nid", foreign_key: "entity_id"
