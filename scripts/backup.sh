51186fbba44f8c3a3281fbf71b10f97451383883
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-05-27 18:21:57 +0000

    Use relative path for secrets folder when backing up.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"/..
-SECRETS_FOLDER=`readlink -f secrets/`
+SECRETS_FOLDER=secrets/
 print_usage_and_exit() {
   echo "Usage: $0"
-  echo "Back up secrets folder ($SECRETS_FOLDER) via tarsnap"
+  echo "Back up secrets folder (`readlink -f $SECRETS_FOLDER`) snap"
   exit
 }
 if [ $# -gt 0 ]; then
