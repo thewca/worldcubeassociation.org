7c05160dfb3261ab4cd685feb5ecd5d5c1a217c5
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-07-24 01:47:02 -0700

    Monkeypatching to remove selectize's darned clear-on-blur behavior.

--- WcaOnRails/app/assets/javascripts/users-autocomplete.js
+++ WcaOnRails/app/assets/javascripts/users-autocomplete.js
@@ -9,7 +9,6 @@ $(function() {
       }
     });
     $(this).selectize({
-      // TODO - don't clear on blur
       plugins: ['restore_on_backspace', 'remove_button'],
       options: users,
       preload: true,
@@ -54,5 +53,44 @@ $(function() {
         });
       }
     });
+
+    // Monkeypatching selectize to not clear the textbox on blur.
+    this.selectize.onBlur = function(e, dest) {
+      var self = this;
+      if (!self.isFocused) return;
+      self.isFocused = false;
+
+      if (self.ignoreFocus) {
+        return;
+      } else if (!self.ignoreBlur && document.activeElement === self.$dropdown_content[0]) {
+        // necessary to prevent IE closing the dropdown when the scrollbar is clicked
+        self.ignoreBlur = true;
+        self.onFocus(e);
+        return;
+      }
+
+      var deactivate = function() {
+        self.close();
+        //JFLY self.setTextboxValue('');
+        self.setActiveItem(null);
+        self.setActiveOption(null);
+        self.setCaret(self.items.length);
+        self.refreshState();
+
+        // IE11 bug: element still marked as active
+        (dest || document.body).focus();
+
+        self.ignoreFocus = false;
+        self.trigger('blur');
+      };
+
+      self.ignoreFocus = true;
+      if (self.settings.create && self.settings.createOnBlur) {
+        self.createItem(null, false, deactivate);
+      } else {
+        deactivate();
+      }
+    };
+
   });
 });
