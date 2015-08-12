786bf2af7e4cf3344c008ea0f6bc49e7e17ce84b
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-08-11 18:33:04 -0700

    Restrict competition ids to 32 characters.

--- WcaOnRails/app/models/competition.rb
+++ WcaOnRails/app/models/competition.rb
@@ -11,7 +11,7 @@ class Competition < ActiveRecord::Base
   ENDS_WITH_YEAR_RE = /\A.* \d{4}\z/
   PATTERN_LINK_RE = /\[\{([^}]+)}\{((https?:|mailto:)[^}]+)}\]/
   PATTERN_TEXT_WITH_LINKS_RE = /\A[^{}]*(#{PATTERN_LINK_RE.source}[^{}]*)*\z/
-  validates :id, presence: true, uniqueness: true
+  validates :id, presence: true, uniqueness: true, length: { maximum: 32 }
   validates :name, length: { maximum: 50 },
                    format: { with: ENDS_WITH_YEAR_RE }
   validates :cellName, length: { maximum: 45 },
