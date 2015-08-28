fa82670cb8846c80864d97e1950838fe06378356
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-08-28 08:30:54 -0700

    Fix approve and reject, which were broken because some css class names had changed.

--- WcaOnRails/app/assets/javascripts/registrations.js
+++ WcaOnRails/app/assets/javascripts/registrations.js
@@ -8,10 +8,10 @@ $(function() {
     var $selectedRows = $registrationsTable.find("tr.selected-row");
     $('.selected-registrations-actions').toggle($selectedRows.length > 0);
 
-    var $selectedApprovedRows = $selectedRows.filter(".registration-a");
+    var $selectedApprovedRows = $selectedRows.filter(".registration-accepted");
     $('.selected-approved-registrations-actions').toggle($selectedApprovedRows.length > 0);
 
-    var $selectedPendingRows = $selectedRows.filter(".registration-p");
+    var $selectedPendingRows = $selectedRows.filter(".registration-pending");
     $('.selected-pending-registrations-actions').toggle($selectedPendingRows.length > 0);
 
     var emails = $selectedRows.find("a[href^=mailto]").map(function() { return this.href.match(/^mailto:(.*)/)[1]; }).toArray();
