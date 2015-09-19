d7aef1043b266ba3f9de4d2fba164ed87f9a54b2
Tim Habermaas <tim.habermaas@gmail.com>
2015-09-20 00:11:24 +0200

    Switch schema format to SQL
    This gets rid of the schema monkey patching.

--- WcaOnRails/config/application.rb
+++ WcaOnRails/config/application.rb
@@ -23,6 +23,7 @@ module WcaOnRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.schema_format = :sql
 
     config.generators do |g|
       g.test_framework :rspec,
