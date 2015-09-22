99d02e1385c47bb79ff81ea3669f1abdcebf34a6
Thomas Bukowski <me@neodude.net>
2015-09-21 19:10:23 -0700

    Nuke the competitions layout.

--- WcaOnRails/app/helpers/layouts_helper.rb
+++
@@ -1,8 +0,0 @@
-# From https://mattbrictson.com/easier-nested-layouts-in-rails
-module LayoutsHelper
-  def parent_layout(layout)
-    @view_flow.set(:layout, output_buffer)
-    output = render(file: "layouts/#{layout}")
-    self.output_buffer = ActionView::OutputBuffer.new(output)
-  end
-end
