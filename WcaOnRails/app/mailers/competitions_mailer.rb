ea4187bb50be01d855daad26f429d0fae31fd0a6
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-09-23 02:12:04 -0700

    Remove vestigial comment.

--- WcaOnRails/app/mailers/competitions_mailer.rb
+++ WcaOnRails/app/mailers/competitions_mailer.rb
@@ -1,9 +1,4 @@
 class CompetitionsMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.competitions_mailer.notify_board_of_confirmed_competition.subject
-  #
   def notify_board_of_confirmed_competition(confirmer, competition)
     @competition = competition
     mail(
