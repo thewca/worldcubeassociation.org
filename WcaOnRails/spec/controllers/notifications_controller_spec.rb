c93bf80b2cc7668018c42307757c0f5dee248318
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-09-23 13:13:52 -0700

    Added a /notifications panel. It currently shows delegates competitions they need to confirm.

---
+++ WcaOnRails/spec/controllers/notifications_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe NotificationsController, type: :controller do
+  describe "GET #index" do
+    context "when not signed in" do
+      it "redirects to the sign in page" do
+        get :index
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context "when signed in as delegate" do
+      let(:delegate) { FactoryGirl.create :delegate }
+      let!(:unconfirmed_competition) { FactoryGirl.create :competition, delegates: [delegate], isConfirmed: false, showAtAll: false }
+      let!(:confirmed_competition) { FactoryGirl.create :competition, delegates: [delegate], isConfirmed: true, showAtAll: false }
+      let!(:visible_competition) { FactoryGirl.create :competition, delegates: [delegate], isConfirmed: true, showAtAll: true }
+      before :each do
+        sign_in delegate
+      end
+
+      it "shows unconfirmed competitions" do
+        get :index
+        notifications = assigns(:notifications)
+        expect(notifications).to eq [
+          {
+            text: "#{unconfirmed_competition.name} is not confirmed",
+            url: edit_competition_path(unconfirmed_competition),
+          }
+        ]
+      end
+    end
+  end
+end
