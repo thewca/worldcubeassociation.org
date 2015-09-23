61a82be81b5ba565a99385d1c10774d8c84155c6
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-09-23 13:27:19 -0700

    Add a link to the notifications panel in the emails we send the to board when competitions are confirmed.

--- WcaOnRails/spec/mailers/previews/competitions_mailer_preview.rb
+++ WcaOnRails/spec/mailers/previews/competitions_mailer_preview.rb
@@ -3,7 +3,7 @@ class CompetitionsMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/competitions_mailer/notify_board_of_confirmed_competition
   def notify_board_of_confirmed_competition
-    c = Competition.first
+    c = CompetitionDelegate.first.competition
     CompetitionsMailer.notify_board_of_confirmed_competition(c.delegates[0], c)
   end
 
