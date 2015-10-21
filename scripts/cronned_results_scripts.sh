77360a1e89b6247ebf334b5fb8d4076d8a1d8cad
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-10-21 01:40:15 +0000

    Include SERVER_NAME to avoid the "This is only a copy of the WCA results system used for testing stuff" message.

--- scripts/cronned_results_scripts.sh
+++ scripts/cronned_results_scripts.sh
@@ -10,7 +10,9 @@ time (cd webroot/results/admin/; REQUEST_URI="export=" php export_public.php)
 time (cd webroot/results/; rm -f generated/statistics.cache; php statistics.php)
 
 # Update Evolution of Records
-time (cd webroot/results/misc/evolution; php update7205.php)
+# We need to set SERVER_NAME to avoid generating the "This is only a copy of
+# the WCA results system used for testing stuff" message.
+time (cd webroot/results/misc/evolution; SERVER_NAME=www.worldcubeassociation.org php update7205.php)
 
 # Update Missing Averages
-time (cd webroot/results/misc/missing_averages; php update7205.php)
+time (cd webroot/results/misc/missing_averages; SERVER_NAME=www.worldcubeassociation.org php update7205.php)
