1d994d8bd893c4e5f965ca41bcdcbb364e0abb14
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-11-04 23:00:18 -0800

    Ensure thumbnails are always 100x100, even if padding is required.

--- WcaOnRails/app/uploaders/avatar_uploader.rb
+++ WcaOnRails/app/uploaders/avatar_uploader.rb
@@ -2,6 +2,6 @@ class AvatarUploader < AvatarUploaderBase
   # Create different versions of your uploaded files:
   version :thumb do
     process crop: :avatar
-    process resize_to_fit: [100, 100]
+    process resize_and_pad: [100, 100]
   end
 end
