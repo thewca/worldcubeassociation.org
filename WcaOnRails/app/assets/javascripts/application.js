040ac66af17593073b14c520006f4ad44f32c776
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-11-12 16:39:52 -0800

    Only send the id, dates, and location when ajaxing. Previously, the url was getting so long that webrick would crash with a WEBrick: RequestURITooLarge.

--- WcaOnRails/app/assets/javascripts/application.js
+++ WcaOnRails/app/assets/javascripts/application.js
@@ -66,3 +66,14 @@ $(function() {
   });
   $(".best_in_place").best_in_place();
 });
+
+// http://stackoverflow.com/a/5603156
+(function($) {
+  $.fn.serializeJSON = function() {
+    var json = {};
+    $.map($(this).serializeArray(), function(n, i) {
+      json[n.name] = n.value;
+    });
+    return json;
+  };
+})(jQuery);
