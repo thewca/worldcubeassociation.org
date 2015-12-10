947b5d96cd32613eb715c25310120e8c27aea206
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-12-10 12:08:24 -0800

    Custom bootstrap_devise_error_messages! to not show silly message about "N errors prohibited this user from being saved". This fixes #235.

---
+++ WcaOnRails/app/helpers/devise_bootstrap_views_helper.rb
@@ -0,0 +1,16 @@
+module DeviseBootstrapViewsHelper
+  def bootstrap_devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+    html = <<-HTML
+    <div class="alert alert-danger alert-block">
+      <button type="button" class="close" data-dismiss="alert">x</button>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+end
