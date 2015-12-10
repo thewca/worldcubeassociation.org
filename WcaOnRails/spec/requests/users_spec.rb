f693001fe2a056755ed22484bc79921d4ff581cf
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-12-10 02:46:50 -0800

    Better messages when someone without an account is trying to log in with a WCA id that has competed. This fixes #235.

--- WcaOnRails/spec/requests/users_spec.rb
+++ WcaOnRails/spec/requests/users_spec.rb
@@ -20,7 +20,7 @@ describe "users" do
   end
 
   it 'can change password' do
-    user = FactoryGirl.create(:user)
+    user = FactoryGirl.create :user
 
     # sign in
     post_via_redirect user_session_path, 'user[login]' => user.email, 'user[password]' => user.password
@@ -41,5 +41,23 @@ describe "users" do
     post_via_redirect user_session_path, 'user[login]' => user.email, 'user[password]' => new_password
     get profile_edit_path
     expect(response).to be_success
+  end
+
+  it 'sign in shows conversion message for competitors missing accounts' do
+    person = FactoryGirl.create :person
+
+    # attempt to sign in
+    post_via_redirect user_session_path, 'user[login]' => person.id, 'user[password]' => "a password"
+    expect(response).to be_success
+    expect(response.body).to include "It looks like you have not created a WCA website account yet"
+  end
+
+  it 'reset password shows conversion message for competitors missing accounts' do
+    person = FactoryGirl.create :person
+
+    # attempt to reset password
+    post_via_redirect user_password_path, 'user[login]' => person.id, 'user[password]' => "a password"
+    expect(response).to be_success
+    expect(response.body).to include "It looks like you have not created a WCA website account yet"
   end
 end
