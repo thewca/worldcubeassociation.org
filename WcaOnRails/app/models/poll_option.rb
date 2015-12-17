2a39110b78dcb6af38c8c1a2d4d2456d7992862f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-12-17 09:48:34 -0800

    Added a percentage method to PollOption and a check for closed poll on edit. This fixes #271.

--- WcaOnRails/app/models/poll_option.rb
+++ WcaOnRails/app/models/poll_option.rb
@@ -6,4 +6,12 @@ class PollOption < ActiveRecord::Base
   has_many :users, through: :vote_options
 
   validates :description, presence: true
+
+  def percentage
+    if self.poll.votes.count > 0
+      (self.vote_options.count.to_f / self.poll.votes.count * 100).round(2)
+    else
+      return 0
+    end
+  end
 end
