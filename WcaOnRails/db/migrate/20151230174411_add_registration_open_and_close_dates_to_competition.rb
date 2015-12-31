37d41ac915ca03ac65f11b9f4fa479223dc3f845
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-12-30 18:54:43 -0800

    Fancier migration to handle existing competitions.

--- WcaOnRails/db/migrate/20151230174411_add_registration_open_and_close_dates_to_competition.rb
+++ WcaOnRails/db/migrate/20151230174411_add_registration_open_and_close_dates_to_competition.rb
@@ -1,25 +1,42 @@
 class AddRegistrationOpenAndCloseDatesToCompetition < ActiveRecord::Migration
   def change
-    add_column :Competitions, :registration_open, :datetime
-    add_column :Competitions, :registration_close, :datetime
-    add_column :Competitions, :use_wca_registration, :boolean, null: false, default: false
-
     reversible do |dir|
       dir.up do
+        add_column :Competitions, :registration_open, :datetime
+        add_column :Competitions, :registration_close, :datetime
+        add_column :Competitions, :use_wca_registration, :boolean, null: false, default: false
         execute <<-SQL
           UPDATE Competitions
             SET use_wca_registration=1
             WHERE showPreregForm=1 OR showPreregList=1;
         SQL
-
+        execute <<-SQL
+          UPDATE Competitions
+            SET registration_open=NOW()
+            SET registration_close=CONCAT(year,'-',LPAD(endMonth,2,'00'),'-',LPAD(endDay,2,'00'))
+            WHERE showPreregForm=1;
+        SQL
+        remove_column :Competitions, :showPreregForm
+        remove_column :Competitions, :showPreregList
       end
 
       dir.down do
-        raise ActiveRecord::IrreversibleMigration
+        add_column :Competitions, :showPreregForm, :boolean, null: false, default: false
+        add_column :Competitions, :showPreregList, :boolean, null: false, default: false
+        execute <<-SQL
+          UPDATE Competitions
+            SET showPreregList=1
+            WHERE use_wca_registration=1;
+        SQL
+        execute <<-SQL
+          UPDATE Competitions
+            SET showPreregForm=1
+            WHERE registration_open < NOW() AND NOW() < registration_close;
+        SQL
+        add_column :Competitions, :registration_open
+        add_column :Competitions, :registration_close
+        add_column :Competitions, :use_wca_registration
       end
     end
-
-    remove_column :Competitions, :showPreregForm, :boolean
-    remove_column :Competitions, :showPreregList, :boolean
   end
 end
