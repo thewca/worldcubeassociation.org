e48f2b8e59dd7fb960dedbadd6da5080ae54603c
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-01-05 17:57:52 -0800

    Changed status to radio buttons, and styled the delete button a little differently. Also fixed a bug where clicking "Update Registration" would actually delete the registration, because nested forms aren't a thing.

--- WcaOnRails/spec/features/register_for_competition_spec.rb
+++ WcaOnRails/spec/features/register_for_competition_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.feature "Registering for a competition" do
-  let(:competition) { FactoryGirl.create :competition, :registration_open }
   let(:user) { FactoryGirl.create :user }
+  let(:delegate) { FactoryGirl.create :user }
+  let(:competition) { FactoryGirl.create :competition, :registration_open, delegates: [delegate] }
 
-  context "when signed in as user" do
-
+  context "signed in as user" do
     before :each do
       visit "/users/sign_in"
       fill_in "Email or WCA ID", with: user.email
@@ -23,7 +23,7 @@ RSpec.feature "Registering for a competition" do
     end
   end
 
-  context "when not signed in" do
+  context "not signed in" do
     scenario "following sign in link on register page should redirect back to register page" do
       visit competition_register_path(competition)
 
@@ -35,6 +35,29 @@ RSpec.feature "Registering for a competition" do
       click_button "Sign in"
 
       expect(current_url).to eq competition_register_url(competition)
+    end
+  end
+
+  context "signed in as delegate" do
+    let(:registration) { FactoryGirl.create(:registration, user: user, competition: competition) }
+    before :each do
+      visit "/users/sign_in"
+      fill_in "Email or WCA ID", with: delegate.email
+      fill_in "Password", with: delegate.password
+      click_button "Sign in"
+    end
+
+    scenario "updating registration" do
+      visit edit_registration_path(registration)
+      fill_in "Guests", with: "test guests!"
+      click_button "Update Registration"
+      expect(registration.reload.guests).to eq "test guests!"
+    end
+
+    scenario "deleting registration" do
+      visit edit_registration_path(registration)
+      click_link "Delete"
+      expect(Registration.find_by_id(registration.id)).to eq nil
     end
   end
 end
