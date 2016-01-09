e114684ad3ef57084b4ec9cbac6c261a57be494f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-01-08 23:33:40 -0800

    Added a migration to convert all processLinks style columns into markdown style links.

---
+++ WcaOnRails/db/migrate/20160109070723_convert_process_links_format_to_markdown.rb
@@ -0,0 +1,23 @@
+class ConvertProcessLinksFormatToMarkdown < ActiveRecord::Migration
+  def processLinks_to_markdown(s)
+    s ? s.gsub(/\[ *{([^}]+)} *{([^}]+)} *\]/, '[\1](\2)') : nil
+  end
+
+  def change
+    reversible do |dir|
+      dir.up do
+        Competition.all.each do |competition|
+          competition.update_columns(
+            venue: processLinks_to_markdown(competition.venue),
+            venueDetails: processLinks_to_markdown(competition.venueDetails),
+            contact: processLinks_to_markdown(competition.contact),
+            information: processLinks_to_markdown(competition.information),
+          )
+        end
+      end
+
+      dir.down do
+      end
+    end
+  end
+end
