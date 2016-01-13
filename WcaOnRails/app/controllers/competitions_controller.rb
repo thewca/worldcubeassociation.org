12c157eadcf308605e63b13b8d7aa9f364d218d5
Pedro Santos <pedrosino@gmail.com>
2016-01-13 08:47:22 -0200

    Forgot to update the competitions_controller to allow non-authenticated users to access /competitions.

--- WcaOnRails/app/controllers/competitions_controller.rb
+++ WcaOnRails/app/controllers/competitions_controller.rb
@@ -1,6 +1,6 @@
 class CompetitionsController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
-  before_action :can_admin_results_only, only: [:index, :post_announcement, :post_results, :admin_edit]
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :can_admin_results_only, only: [:post_announcement, :post_results, :admin_edit]
 
   private def competition_from_params
     competition = Competition.find(params[:id])
