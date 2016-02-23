2e72fa0067b000c6f12e557608942aadd09e8c96
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-02-23 11:12:44 -0800

    Removed OauthApplication in favor of Doorkeeper::Application which has the correct associations for the now required owner (owner_id + owner_type) field.

--- WcaOnRails/app/models/oauth_application.rb
+++
@@ -1,2 +0,0 @@
-class OauthApplication < ActiveRecord::Base
-end
--- WcaOnRails/db/seeds/development/oauth.seeds.rb
+++ WcaOnRails/db/seeds/development/oauth.seeds.rb
@@ -1,6 +1,10 @@
-OauthApplication.create!(
-  name: "test app",
-  uid: "9ad911ea379bd6f49c4f923644dbea3f44aeab5625a25f468210026a862b0c3d",
-  secret: "3b787d2f6c9e51d1f8c4f758e569517b37d281978812ffea304b965c9bd59720",
-  redirect_uri: "urn:ietf:wg:oauth:2.0:oob"
-)
+after "development:users" do
+  software_team_user = User.find_by_email!("software_team")
+  Doorkeeper::Application.create!(
+    name: "test app",
+    uid: "9ad911ea379bd6f49c4f923644dbea3f44aeab5625a25f468210026a862b0c3d",
+    secret: "3b787d2f6c9e51d1f8c4f758e569517b37d281978812ffea304b965c9bd59720",
+    redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+    owner: software_team_user,
+  )
+end
