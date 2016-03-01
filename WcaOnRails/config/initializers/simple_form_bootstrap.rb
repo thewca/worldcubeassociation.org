b7e56901a0edc5d141002478fca55dfb94983091
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-03-01 00:49:38 -0800

    Better styling of simple_form checkboxes to simplify our competitions edit page. Also changed guests_enabled checkbox to a painfully explicit set of 2 radio buttons inspired by CubingUSA: https://github.com/cubing/worldcubeassociation.org/issues/286#issuecomment-168251174 (fixes #404).

--- WcaOnRails/config/initializers/simple_form_bootstrap.rb
+++ WcaOnRails/config/initializers/simple_form_bootstrap.rb
@@ -84,10 +84,9 @@ SimpleForm.setup do |config|
   config.wrappers :horizontal_boolean, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :readonly
-    b.use :label, class: 'col-sm-2 control-label'
 
-    b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
-      ba.use :input
+    b.wrapper tag: 'div', class: 'col-sm-10 col-sm-offset-2' do |ba|
+      ba.use :label_input, wrap_with: { tag: 'div', class: 'checkbox' }
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
