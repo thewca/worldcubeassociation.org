066097485d13057795d739517b6b5c5a998c9806
Pedro Santos <pedrosino@gmail.com>
2016-03-02 16:22:58 -0300

    Fixed some things from comments.

--- WcaOnRails/app/models/team.rb
+++ WcaOnRails/app/models/team.rb
@@ -1,6 +1,5 @@
 class Team < ActiveRecord::Base
   has_many :team_members, dependent: :destroy
-  belongs_to :leader, class_name: "User", foreign_key: "leader_id"
 
   accepts_nested_attributes_for :team_members, reject_if: :all_blank, allow_destroy: true
 end
