850d27c12b0df78fb8c03943d3ba8b9b8b63805a
Pedro Santos <pedrosino@gmail.com>
2016-03-05 14:13:33 -0300

    Removing old teams columns.

---
+++ WcaOnRails/db/migrate/20160305170821_remove_teams_columns.rb
@@ -0,0 +1,12 @@
+class RemoveTeamsColumns < ActiveRecord::Migration
+  def change
+    remove_column :users, :wrc_team
+    remove_column :users, :wrc_team_leader
+    remove_column :users, :wdc_team
+    remove_column :users, :wdc_team_leader
+    remove_column :users, :results_team
+    remove_column :users, :results_team_leader
+    remove_column :users, :software_team
+    remove_column :users, :software_team_leader
+  end
+end
