22ed9273b7a01c384557f445b8962c96b49530aa
Pedro Santos <pedrosino@gmail.com>
2016-03-10 21:12:57 -0300

    Finished the new design.

--- WcaOnRails/app/assets/javascripts/application.js
+++ WcaOnRails/app/assets/javascripts/application.js
@@ -105,13 +105,7 @@ $.fn.competitionsMap = function(competitions) {
 };
 
 function detect_mobile() {
- if( navigator.userAgent.match(/Android/i)
-  || navigator.userAgent.match(/webOS/i)
-  || navigator.userAgent.match(/iPhone/i)
-  || navigator.userAgent.match(/iPad/i)
-  || navigator.userAgent.match(/iPod/i)
-  || navigator.userAgent.match(/BlackBerry/i)
-  || navigator.userAgent.match(/Windows Phone/i) ) {
+ if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) ) {
     return true;
   } else {
     return false;
