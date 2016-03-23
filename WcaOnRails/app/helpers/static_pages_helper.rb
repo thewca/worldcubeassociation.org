faf7207b1bf835dc063495558032c4afdfc8a64d
Pedro Santos <pedrosino@gmail.com>
2016-03-23 17:02:07 -0300

    Changing the format_team_member method.

--- WcaOnRails/app/helpers/static_pages_helper.rb
+++ WcaOnRails/app/helpers/static_pages_helper.rb
@@ -1,9 +1,8 @@
 module StaticPagesHelper
-  def format_team_members(team)
-    leader_team = :"#{team}_leader"
-    User.where(team => true).order(leader_team => :desc, name: :asc).map do |u|
-      s = u.name
-      if u.send(leader_team)
+  def format_team_members(team_friendly_id)
+    TeamMember.where(team_id: Team.find_by_friendly_id!(team_friendly_id).id).order(:team_leader => :desc).map do |u|
+      s = u.user.name
+      if u.team_leader
         s += " (leader)"
       end
       s
