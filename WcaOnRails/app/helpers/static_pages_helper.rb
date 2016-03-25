d7ad3c181e51546b6a665b733b421386a776a620
Pedro Santos <pedrosino@gmail.com>
2016-03-25 17:06:47 -0300

    Ordering by team_leader and user.name.

--- WcaOnRails/app/helpers/static_pages_helper.rb
+++ WcaOnRails/app/helpers/static_pages_helper.rb
@@ -1,6 +1,6 @@
 module StaticPagesHelper
   def format_team_members(team_friendly_id)
-    TeamMember.where(team_id: Team.find_by_friendly_id!(team_friendly_id).id).order(:team_leader => :desc).map do |u|
+    TeamMember.where(team_id: Team.find_by_friendly_id!(team_friendly_id).id).includes(:user).order(team_leader: :desc).order("users.name asc").map do |u|
       s = u.user.name
       if u.team_leader
         s += " (leader)"
