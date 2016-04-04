9c004fc0b2824ca12f586017872ef8a2d914ebc2
Philippe Virouleau <philippe.virouleau@imag.fr>
2016-04-05 00:03:37 +0200

    Raise error on missing translation in test too.

--- WcaOnRails/config/environments/test.rb
+++ WcaOnRails/config/environments/test.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000, locale: nil }
 end
