b460a63146b65047bda604e0826cb0465ce020ea
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-05-05 19:26:32 +0200

    Get rid of unnecessary migration.

--- WcaOnRails/db/migrate/20160502113530_add_preferred_events_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPreferredEventsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :preferred_event_ids, :text
-  end
-end
--- WcaOnRails/db/migrate/20160504170758_create_user_preferred_events.rb
+++ WcaOnRails/db/migrate/20160504170758_create_user_preferred_events.rb
@@ -1,9 +1,5 @@
 class CreateUserPreferredEvents < ActiveRecord::Migration
   def change
-    # Remove the column from users.
-    remove_column :users, :preferred_event_ids
-
-    # Instead, add a separate table for preferred events.
     create_table :user_preferred_events do |t|
       t.references :user
       t.string :event_id
