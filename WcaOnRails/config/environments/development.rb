a4d1147fb21479faec2f898b6f83555f3c482395
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-05-07 16:33:50 -0700

    Add bullet. That was easy =) This fixes #358.

--- WcaOnRails/config/environments/development.rb
+++ WcaOnRails/config/environments/development.rb
@@ -54,4 +54,12 @@ Rails.application.configure do
   # config.i18n.fallbacks = true
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
