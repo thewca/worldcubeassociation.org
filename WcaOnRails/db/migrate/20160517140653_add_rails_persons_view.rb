ca6824bdb7550b0842ff71b1e63213d06c888bf5
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-05-17 16:50:10 +0200

    Add rails_persons view. Change Persons primary key.

---
+++ WcaOnRails/db/migrate/20160517140653_add_rails_persons_view.rb
@@ -0,0 +1,23 @@
+class AddRailsPersonsView < ActiveRecord::Migration
+  def change
+    execute "ALTER TABLE Persons DROP PRIMARY KEY;"
+    add_index :Persons, [:id, :subId], unique: true
+    add_column :Persons, :rails_id, :primary_key
+
+    execute <<-SQL
+      CREATE VIEW rails_persons
+      AS SELECT
+        rails_id AS id,
+        id AS wca_id,
+        subId,
+        name,
+        countryId,
+        gender,
+        year,
+        month,
+        day,
+        comments
+      FROM Persons;
+    SQL
+  end
+end
