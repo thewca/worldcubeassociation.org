389185b3833c75cf1e7ffc8aeb9bfe4238c63a58
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-05-17 15:10:09 +0200

    Fix.

--- WcaOnRails/db/migrate/20160514141051_fill_accepted_at_registrations_column.rb
+++ WcaOnRails/db/migrate/20160514141051_fill_accepted_at_registrations_column.rb
@@ -2,7 +2,7 @@ class FillAcceptedAtRegistrationsColumn < ActiveRecord::Migration
   def change
     # Move the data
     Registration.where(status: "a").includes(:competition).each do |registration|
-      registration.update_column(:accepted_at, registration.competition.registration_open)
+      registration.update_column(:accepted_at, registration.competition.registration_open) if competition
     end
 
     # Remove the column
