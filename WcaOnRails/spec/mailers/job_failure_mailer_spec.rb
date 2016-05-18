4352c678ea5b96c7e59baf3983c7c01a9d792bb0
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-05-18 09:04:57 -0700

    Add handler to the error notification email.

--- WcaOnRails/spec/mailers/job_failure_mailer_spec.rb
+++ WcaOnRails/spec/mailers/job_failure_mailer_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe JobFailureMailer, type: :mailer do
   describe "notify_admin_of_job_failure" do
-    let(:job) { Delayed::Job.new(id: 42) }
+    let(:job) { Delayed::Job.new(id: 42, handler: "I tried to take care of this") }
     let(:exception) do
       begin
         raise "error!"
@@ -21,6 +21,9 @@ RSpec.describe JobFailureMailer, type: :mailer do
 
     it "renders the body" do
       expect(mail.body.encoded).to match("Job 42 failed")
+      expect(mail.body.encoded).to match("Handler")
+      expect(mail.body.encoded).to match("I tried to take care of this")
+      expect(mail.body.encoded).to match("Backtrace")
     end
   end
 end
