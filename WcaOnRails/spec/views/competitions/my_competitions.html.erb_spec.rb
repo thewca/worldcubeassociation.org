519f9c1008f2bf7ebbb6842f13f4b1ddf31e0d65
Jeff Plumb <jeff@pingskills.com>
2016-05-25 22:55:37 +1000

    Show registration status on My Competitions page - fixes #586
    The list and registered icons from Font Awesome Icons have been used
    to show the registration status along with some extra text in the tooltip.
    Two tests were created to test the my_competitions view.

---
+++ WcaOnRails/spec/views/competitions/my_competitions.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "competitions/my_competitions" do
+  let(:competition) { FactoryGirl.create(:competition, :registration_open, name: "Melbourne Open 2016") }
+  let(:registration) { FactoryGirl.create(:registration, competition: competition) }
+
+  before do
+    allow(view).to receive(:current_user) { registration.user }
+    assign(:not_past_competitions, [competition])
+    assign(:past_competitions, [])
+  end
+
+  it "shows upcoming competitions" do
+    render
+    expect(rendered).to match '<td><a href="/competitions/MelbourneOpen2016">Melbourne Open 2016</a></td>'
+  end
+
+  it "shows you are on the waiting list" do
+    render
+    expect(rendered).to match 'You are currently on the waiting list'
+    expect(rendered).to match '<i class="fa fa-hourglass-half"></i>'
+  end
+end
