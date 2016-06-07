13b7603771d8d8613c19334ab21272f6f7c9bb27
Pedro Santos <pedrosino@gmail.com>
2016-06-07 11:27:48 -0300

    Remove "Delegate report for" from email notification.

--- WcaOnRails/app/mailers/competitions_mailer.rb
+++ WcaOnRails/app/mailers/competitions_mailer.rb
@@ -26,7 +26,7 @@ class CompetitionsMailer < ApplicationMailer
     mail(
       to: "delegates@worldcubeassociation.org",
       cc: competition.delegates.pluck(:email),
-      subject: "[wca-report] [#{competition.continent.name}] Delegate report for #{competition.name}",
+      subject: "[wca-report] [#{competition.continent.name}] #{competition.name}",
     )
   end
 
