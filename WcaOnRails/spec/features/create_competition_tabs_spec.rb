24b61278416306492244f2a59b7b4c5294b70ec9
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-06-12 20:31:35 +0200

    Add a feature test.

---
+++ WcaOnRails/spec/features/create_competition_tabs_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.feature "create competition tabs" do
+  let(:organizer) { FactoryGirl.create :user }
+  let(:competition) { FactoryGirl.create :competition, organizers: [organizer] }
+
+  it "creating a new tab" do
+    sign_in organizer
+    visit competition_path(competition)
+    click_on "Manage tabs"
+    click_on "New tab"
+
+    fill_in "Name", with: "Accomodation"
+    fill_in "Content", with: "On your own."
+    click_on "Create"
+
+    visit competition_path(competition)
+    expect(page).to have_content "Accomodation"
+    expect(page).to have_content "On your own."
+  end
+
+  it "editing an existing tab" do
+    FactoryGirl.create(:competition_tab, competition: competition, name: "Accomodation", content: "On your own.")
+
+    sign_in organizer
+    visit competition_path(competition)
+    click_on "Manage tabs"
+    within("#competition-tabs tbody tr:first") { click_on "Edit" }
+
+    fill_in "Name", with: "Travel!"
+    fill_in "Content", with: "Travel informations."
+    click_on "Update"
+
+    visit competition_path(competition)
+    expect(page).to have_content "Travel!"
+    expect(page).to have_content "Travel informations."
+    expect(page).to_not have_content "Accomodation"
+    expect(page).to_not have_content "On your own."
+  end
+end
