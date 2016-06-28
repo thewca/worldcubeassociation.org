1fc18888174e984f6253c63e4b52e525e8a38136
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-06-28 11:56:22 +0200

    Perform first sort server-side on registrations#index page.

--- WcaOnRails/app/controllers/registrations_controller.rb
+++ WcaOnRails/app/controllers/registrations_controller.rb
@@ -54,7 +54,7 @@ class RegistrationsController < ApplicationController
 
   def index
     @competition = competition_from_params
-    @registrations = @competition.registrations.accepted.includes(:user, :registration_events, :events)
+    @registrations = @competition.registrations.accepted.includes(:user, :registration_events, :events).order("users.name")
   end
 
   def edit
