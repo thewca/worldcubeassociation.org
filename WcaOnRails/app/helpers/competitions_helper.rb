52a32bddaf43e5f5d6e35826170d146530cc676c
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-08-02 11:58:35 +0200

    Move message logic to competitions helper.

--- WcaOnRails/app/helpers/competitions_helper.rb
+++ WcaOnRails/app/helpers/competitions_helper.rb
@@ -1,2 +1,16 @@
 module CompetitionsHelper
+  def competition_message_for_user(competition, user)
+    messages = []
+    registration = competition.registrations.find_by_user_id(user.id)
+    if registration
+      messages << "You are " + (registration.accepted? ? "registered." : "currently on the waiting list.")
+    end
+    visible = competition.showAtAll?
+    messages << if competition.isConfirmed?
+                  "This competition is confirmed #{visible ? 'and' : 'but not'} visible."
+                else
+                  "This competition is not confirmed #{visible ? 'but' : 'and not'} visible."
+                end
+    messages.join(' ')
+  end
 end
