8e7427e56e08a4f8cb65f422ad8c172b7aae1bc2
Pedro Santos <pedrosino@gmail.com>
2016-08-20 13:58:14 -0300

    Change 'comps' to 'competitions'

--- WcaOnRails/app/controllers/admin/competitions_controller.rb
+++ WcaOnRails/app/controllers/admin/competitions_controller.rb
@@ -1,6 +1,7 @@
 module Admin
-  class CompsController < AdminController
-    def admin_index
+  class CompetitionsController < AdminController
+    before_action -> { redirect_unless_user(:board_member?) }, only: [:index]
+    def index
       # Default params
       params[:region] ||= "all"
       params[:display] ||= "list"
