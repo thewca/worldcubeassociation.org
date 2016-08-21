6df7959e906c70db532eeb58dd8c0b97ab06456a
Pedro Santos <pedrosino@gmail.com>
2016-08-21 11:45:45 -0300

    Move classes to competition_helper.

--- WcaOnRails/app/helpers/competitions_helper.rb
+++ WcaOnRails/app/helpers/competitions_helper.rb
@@ -14,4 +14,38 @@ module CompetitionsHelper
                 end
     messages.join(' ')
   end
+
+  def announced_class(distance_announced)
+    announced_class = if distance_announced <= 21
+                        "alert-danger"
+                      elsif distance_announced <= 28
+                        "alert-orange"
+                      else
+                        "alert-green"
+                      end
+  end
+
+  def report_class(distance_report)
+    report_class = if distance_report > 21
+                     "alert-danger"
+                   elsif distance_report > 14
+                     "alert-orange"
+                   elsif distance_report > 7
+                     "alert-success"
+                   else
+                     "alert-green"
+                   end
+  end
+
+  def results_class(distance_results)
+    results_class = if distance_results > 21
+                      "alert-danger"
+                    elsif distance_results > 14
+                      "alert-orange"
+                    elsif distance_results > 7
+                      "alert-success"
+                    else
+                      "alert-green"
+                    end
+  end
 end
