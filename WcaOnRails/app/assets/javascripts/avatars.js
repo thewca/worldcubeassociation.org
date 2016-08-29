c858d959e0b13126a6f04ed66abebcf5010ef9a4
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-08-29 19:15:27 +0200

    Fix bug with rejection reason textarea toggling.

--- WcaOnRails/app/assets/javascripts/avatars.js
+++ WcaOnRails/app/assets/javascripts/avatars.js
@@ -1,6 +1,6 @@
 onPage('avatars#index', function() {
   $('.actions').on('change', function() {
-    var $rejectButton = $('input[value="reject"]');
+    var $rejectButton = $(this).find('input[value="reject"]');
     $rejectButton.closest('.row').find('.rejection-reason').toggle($rejectButton.prop('checked'));
   }).trigger('change');
 });
