5b8fa4d4f95c8f8c6c8a87b150d3968b3223de77
Caleb Hoover <choover11@gmail.com>
2016-09-02 14:50:44 -0700

    fixed various things

--- WcaOnRails/app/controllers/api/v0/competitions_controller.rb
+++ WcaOnRails/app/controllers/api/v0/competitions_controller.rb
@@ -3,19 +3,19 @@ class Api::V0::CompetitionsController < Api::V0::ApiController
   def show
     competition = Competition.visible.find_by(id: params[:id])
     if competition
-	    render json: competition
+      render json: competition, status: ok
     else
       render json: { error: "Competition with id #{params[:id]} not found" }, status: 404
     end
   end
 
   def results
-  	competition = Competition.visible.find_by(id: params[:id])
-  	if competition
-  		results = Result.search_by_competition(competition.id, params: params)
-  		render json: Result.search_by_competition(competition.id, params: params)
-  	else
-  		render json: { error: "Competition with id #{params[:id]} not found" }, status: 404
-  	end
+    competition = Competition.visible.find_by(id: params[:id])
+    if competition
+      results = Result.search_by_competition(competition.id, params: params)
+      render json: results, status: ok
+    else
+      render json: { error: "Competition with id #{params[:id]} not found" }, status: 404
+    end
   end
 end
