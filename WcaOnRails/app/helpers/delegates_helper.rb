cc76687dc070ccded3a7ce3a21ea1c3f166f09a5
Pedro Santos <pedrosino@gmail.com>
2016-09-06 17:18:10 -0300

    Frozen string literal.

--- WcaOnRails/app/helpers/delegates_helper.rb
+++ WcaOnRails/app/helpers/delegates_helper.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 module DelegatesHelper
   def delegate_class(code)
     {
       'board_member' => 'alert-success',
       'candidate_delegate' => 'alert-warning',
-      'senior_delegate' => 'alert-info'
+      'senior_delegate' => 'alert-info',
     }[code]
   end
 end
