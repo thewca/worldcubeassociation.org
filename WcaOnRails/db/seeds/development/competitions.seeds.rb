d27178e331cf7dd3c19d8f24bb8f6aa9318fccc9
Jeff Plumb <jeff@pingskills.com>
2016-09-13 23:04:54 +1000

    Update competition.seeds.rb as suggested by @jonatanklosko

--- WcaOnRails/db/seeds/development/competitions.seeds.rb
+++ WcaOnRails/db/seeds/development/competitions.seeds.rb
@@ -42,12 +42,11 @@ after "development:users" do
       latitude_degrees: rand(-90.0..90.0),
       longitude_degrees: rand(-180.0..180.0),
     )
-    events = random_events
-    competition.events = events
+    competition.events = random_events
 
     competition.save!
 
-    events.each do |event|
+    competition.events.each do |event|
       %w(1 2 f).each do |roundId|
         users.each_with_index do |competitor, i|
           person = competitor.person
@@ -103,8 +102,7 @@ after "development:users" do
       latitude_degrees: rand(-90.0..90.0),
       longitude_degrees: rand(-180.0..180.0),
     )
-    events = random_events
-    competition.events = events
+    competition.events = random_events
 
     competition.save!
   end
@@ -156,8 +154,7 @@ after "development:users" do
       latitude_degrees: rand(-90.0..90.0),
       longitude_degrees: rand(-180.0..180.0),
     )
-    events = random_events
-    competition.events = events
+    competition.events = random_events
 
     competition.save!
 
@@ -165,7 +162,7 @@ after "development:users" do
     next if i < 480
     users.each_with_index do |user, i|
       accepted_at = i % 4 == 0 ? Time.now : nil
-      registration_events = events.sample(rand(1..events.count))
+      registration_events = competition.events.sample(rand(1..competition.events.count))
       FactoryGirl.create(:registration, user: user, competition: competition, accepted_at: accepted_at, events: registration_events)
     end
   end
