699faccb42c1228cab980d79af9a428968aff3a3
gagou9 <gagou9@gmail.com>
2016-09-18 16:04:55 +0200

    delete unattended spaces in logrotate's config

--- chef/site-cookbooks/wca/recipes/default.rb
+++ chef/site-cookbooks/wca/recipes/default.rb
@@ -120,11 +120,11 @@ chef_env_to_rails_env = {
 rails_env = chef_env_to_rails_env[node.chef_environment]
 
 logrotate_app 'rails-wca' do
-  path		"#{repo_root}/WcaOnRails/log/production.log"
-  size		"512M"
-  maxage	90
-  postrotate	"[ ! -f /home/cubing/worldcubeassociation.org/WcaOnRails/pids/unicorn.pid ] || kill -USR1 `cat /home/cubing/worldcubeassociation.org/WcaOnRails/pids/unicorn.pid`"
-  options	['nodelaycompress']
+  path "#{repo_root}/WcaOnRails/log/production.log"
+  size "512M"
+  maxage 90
+  postrotate "[ ! -f /home/cubing/worldcubeassociation.org/WcaOnRails/pids/unicorn.pid ] || kill -USR1 `cat /home/cubing/worldcubeassociation.org/WcaOnRails/pids/unicorn.pid`"
+  options ['nodelaycompress']
 end
 
 # Run mailcatcher in every environment except production.
@@ -198,11 +198,11 @@ directory "/etc/nginx/conf.d" do
   group 'root'
 end
 logrotate_app 'nginx-wca' do
-  path		"/var/log/nginx/*.log"
-  size		"512M"
-  maxage	30
-  postrotate	"[ ! -f /var/run/nginx.pid ] || kill -USR1 `cat /var/run/nginx.pid`"
-  options	['nodelaycompress']
+  path "/var/log/nginx/*.log"
+  size "512M"
+  maxage 30
+  postrotate "[ ! -f /var/run/nginx.pid ] || kill -USR1 `cat /var/run/nginx.pid`"
+  options ['nodelaycompress']
 end
 
 server_name = { "production" => "www.worldcubeassociation.org", "staging" => "staging.worldcubeassociation.org", "development" => "" }[node.chef_environment]
