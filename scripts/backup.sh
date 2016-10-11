8b7e8e363606b576735aa73a90cf8815c8d1b22f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-10-11 12:54:06 -0700

    Unconditionally run tarnsap --fsk before doing a backup. This takes longer, but should work.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Exit on error
+set -e
+
 cd "$(dirname "$0")"/..
 SECRETS_FOLDER=secrets/
 print_usage_and_exit() {
@@ -10,5 +13,10 @@ print_usage_and_exit() {
 if [ $# -gt 0 ]; then
   print_usage_and_exit
 fi
+
+# We've been running into issues with tarsnap needed to run fsk. Rather
+# than fixing these manually, we just run fsk unconditionally.
+# See: https://groups.google.com/d/msg/wca-admin/u_jIaAgP6us/NkDFiHo-CgAJ.
+sudo tarsnap --fsck
 
 sudo tarsnap -c -f wca-backup-`date +"%Y%m%d_%H%M%S"` $SECRETS_FOLDER
