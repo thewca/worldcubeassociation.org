ee0fb381b6226d1687096af284558090be730092
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-10-19 19:58:15 +0200

    Add a unique index.

--- WcaOnRails/db/migrate/20161018220122_clean_duplicated_registration_competition_events.rb
+++ WcaOnRails/db/migrate/20161018220122_clean_duplicated_registration_competition_events.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class CleanDuplicatedRegistrationCompetitionEvents < ActiveRecord::Migration
-  def change
+  def up
     execute <<-SQL
       DELETE FROM registration_competition_events
       WHERE registration_competition_events.id NOT IN (
@@ -12,5 +12,12 @@ class CleanDuplicatedRegistrationCompetitionEvents < ActiveRecord::Migration
         ) as keep
       )
     SQL
+
+    add_index :registration_competition_events, [:registration_id, :competition_event_id],
+              unique: true, name: "idx_registration_competition_events_on_reg_id_and_comp_event_id"
+  end
+
+  def down
+    remove_index :registration_competition_events, name: "idx_registration_competition_events_on_reg_id_and_comp_event_id"
   end
 end
