879c650de6b6d8b2e9fb48032047f397cd141406
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-10-18 21:08:18 -0700

    Some minor tweaks to the server status page.

--- WcaOnRails/app/controllers/server_status_controller.rb
+++ WcaOnRails/app/controllers/server_status_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 class ServerStatusController < ApplicationController
+  MINUTES_IN_WHICH_A_JOB_SHOULD_HAVE_STARTED_RUNNING = 5
+
   def index
-    @oldest_job_that_should_have_run_by_now = Delayed::Job.
-        where(attempts: 0).where('created_at < ?', 5.minutes.ago).
-        order(:created_at).
-        first
+    @jobs_that_should_have_run_by_now = Delayed::Job.where(attempts: 0).where('created_at < ?', MINUTES_IN_WHICH_A_JOB_SHOULD_HAVE_STARTED_RUNNING.minutes.ago)
+    @oldest_job_that_should_have_run_by_now = @jobs_that_should_have_run_by_now.order(:created_at).first
 
     @everything_good = @oldest_job_that_should_have_run_by_now.nil?
 
