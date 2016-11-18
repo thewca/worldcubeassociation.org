bbfb25f0f2d9babc3b3aae3cad01f2af50aa3abb
Pedro Santos <pedrosino@gmail.com>
2016-11-18 14:42:50 -0200

    Fix migration.

--- WcaOnRails/db/migrate/20161118141833_change_latitude_and_longitude_default.rb
+++ WcaOnRails/db/migrate/20161118141833_change_latitude_and_longitude_default.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 class ChangeLatitudeAndLongitudeDefault < ActiveRecord::Migration
   def up
-    change_column_default(:Competitions, :latitude, nil)
-    change_column_default(:Competitions, :longitude, nil)
+    change_column_null(:Competitions, :latitude, true)
+    change_column_null(:Competitions, :longitude, true)
+    change_column_default(:Competitions, :latitude, default: nil)
+    change_column_default(:Competitions, :longitude, default: nil)
   end
 
   def down
     change_column_default(:Competitions, :latitude, 0)
     change_column_default(:Competitions, :longitude, 0)
+    change_column_null(:Competitions, :latitude, false)
+    change_column_null(:Competitions, :longitude, false)
   end
 end
