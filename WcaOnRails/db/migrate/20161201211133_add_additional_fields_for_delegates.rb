96a36a9485af22eef69dbbc06097008a313b6870
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-12-01 22:35:29 +0100

    Add a migration.

---
+++ WcaOnRails/db/migrate/20161201211133_add_additional_fields_for_delegates.rb
@@ -0,0 +1,7 @@
+class AddAdditionalFieldsForDelegates < ActiveRecord::Migration
+  def change
+    add_column :users, :location_description, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :notes, :string
+  end
+end
