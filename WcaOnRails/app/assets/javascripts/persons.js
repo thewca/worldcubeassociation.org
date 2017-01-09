7103056a2c1f6499841303a6287bd982564aba32
Lars Petrus <github@lar5.com>
2017-01-09 12:23:56 -0800

    Avoid double load of person search results on page load. (#1077)
    * Avoid double load of person search results on page load.

--- WcaOnRails/app/assets/javascripts/persons.js
+++ WcaOnRails/app/assets/javascripts/persons.js
@@ -1,6 +1,10 @@
 // Add params from the search fields to the bootstrap-table for on Ajax request.
 var personsTableAjax = {
   queryParams: function(params) {
+    if (personsTableAjax.queriesOK !== true) {
+      return false; // Stop Bootstrap Table's uncontrollable initial load
+    }
+
     return $.extend(params || {}, {
       region: $('#region').val(),
       search: $('#search').val(),
@@ -15,16 +19,17 @@ onPage('persons#index', function() {
   var $table = $('.persons-table');
   var options = $table.bootstrapTable('getOptions');
 
-  // Set the table options from the url params.
-  options.pageNumber = parseInt($.getUrlParam('page')) || options.pageNumber;
-  // Load the data using the options set above.
-  $table.bootstrapTable('refresh');
-
   function reloadPersons() {
     $('#search-box i').removeClass('fa-search').addClass('fa-spinner fa-spin');
     options.pageNumber = 1;
     $table.bootstrapTable('refresh');
   }
+
+  // Set the table options from the url params.
+  options.pageNumber = parseInt($.getUrlParam('page')) || options.pageNumber;
+  // Load the data using the options set above.
+  personsTableAjax.queriesOK = true;
+  $table.bootstrapTable('refresh');
 
   $('#region').on('change', reloadPersons);
   $('#search').on('input', _.debounce(reloadPersons, TEXT_INPUT_DEBOUNCE_MS));
