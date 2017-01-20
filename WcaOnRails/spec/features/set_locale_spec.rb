ae9f793a926f240a63cd3b1eb96622ba9666e880
Philippe Virouleau <philippe.44@gmail.com>
2017-01-20 15:05:57 +0100

    Implement a locale selector.
    Added:
     - The feature spec.
     - Restored the accept-language header logic.

---
+++ WcaOnRails/spec/features/set_locale_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.feature "Set the locale" do
+  context "As a visitor" do
+    let(:user) { FactoryGirl.create :user }
+
+    scenario "visiting the home page and changing the locale" do
+      visit "/"
+      expect(I18n.locale).to eq I18n.default_locale
+      click_on "Français"
+      visit "/"
+      expect(I18n.locale).to eq :fr
+    end
+
+    scenario "signing in updates to the preferred_locale" do
+      user.update!(preferred_locale: "fr")
+      sign_in user
+      visit "/"
+      expect(I18n.locale).to eq :fr
+    end
+  end
+end
