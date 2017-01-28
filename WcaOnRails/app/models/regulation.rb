b115de799c58f89b8092651a5570e1da2e40dc7e
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-01-28 12:12:38 -0800

    Don't crash on startup if wca-regulations.json is missing. Instead, we have a warning on /server-status if anything looks wrong. This fixes #1063.

--- WcaOnRails/app/models/regulation.rb
+++ WcaOnRails/app/models/regulation.rb
@@ -1,14 +1,28 @@
 # frozen_string_literal: true
 
 class Regulation < SimpleDelegator
-  REGULATIONS = JSON.parse(File.read(Rails.root.to_s + "/app/views/regulations/wca-regulations.json")).freeze
+  REGULATIONS_JSON_PATH = Rails.root.to_s + "/app/views/regulations/wca-regulations.json"
+
+  def self.reload_regulations
+    @regulations = JSON.parse(File.read(REGULATIONS_JSON_PATH)).freeze
+    @regulations_load_error = nil
+  rescue StandardError => error
+    @regulations = []
+    @regulations_load_error = error
+  end
+
+  reload_regulations
+
+  class << self
+    attr_accessor :regulations_load_error
+  end
 
   def limit(number)
     first(number)
   end
 
   def self.search(query, *)
-    matched_regulations = REGULATIONS.dup
+    matched_regulations = @regulations.dup
     query.downcase.split.each do |part|
       matched_regulations.select! do |reg|
         %w(content_html id).any? { |field| reg[field].downcase.include?(part) }
