33e298d00bc7f896447810c58857a02c7155b70d
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-01-30 00:25:54 +0100

    Update Envfile to use an empty token in development by default.

--- WcaOnRails/config/initializers/octokit.rb
+++ WcaOnRails/config/initializers/octokit.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-if ENVied.WCA_LIVE_SITE
-  Octokit.configure do |c|
-    c.access_token = ENVied.GITHUB_BOT_ACCESS_TOKEN
-  end
+Octokit.configure do |c|
+  c.access_token = ENVied.GITHUB_BOT_ACCESS_TOKEN
 end
