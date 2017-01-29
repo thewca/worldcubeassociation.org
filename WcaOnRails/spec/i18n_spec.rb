aef8b6985b7e0a081228c903bb6b70493e6fbb92
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-01-29 12:14:56 -0800

    Actually print out the missing/unused keys when a test fails. That way people can see what's wrong by looking at travis, rather than having to set up a local environment.

--- WcaOnRails/spec/i18n_spec.rb
+++ WcaOnRails/spec/i18n_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe 'I18n' do
   let(:unused_keys) { i18n.unused_keys }
 
   it 'does not have missing keys' do
-    expect(missing_keys).to be_empty, "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+    expect(missing_keys).to be_empty, "Missing #{missing_keys.leaves.count} i18n keys\n#{missing_keys.inspect}\nYou can also run `i18n-tasks missing' to show them"
   end
 
   it 'does not have unused keys' do
-    expect(unused_keys).to be_empty, "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+    expect(unused_keys).to be_empty, "#{unused_keys.leaves.count} unused i18n keys\n#{unused_keys.inspect}\nYou can also run `i18n-tasks unused' to show them"
   end
 end
