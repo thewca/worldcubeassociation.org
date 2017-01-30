04fb6ac8f3ed715f3154294d0a242f94bc291766
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-01-30 01:22:39 -0800

    Fixed bug with bad_keys_count calculation.

--- WcaOnRails/app/controllers/server_status_controller.rb
+++ WcaOnRails/app/controllers/server_status_controller.rb
@@ -11,11 +11,12 @@ class ServerStatusController < ApplicationController
     @ref_english = Locale.new('en')
     @bad_keys_by_type_by_locale = {}
     bad_keys_count = 0
-    (I18n.available_locales - [:en]).each do |l|
-      ref_locale = Locale.new(l, true)
+    (I18n.available_locales - [:en]).each do |locale|
+      ref_locale = Locale.new(locale, true)
       missing, unused, outdated = ref_locale.compare_to(@ref_english)
-      @bad_keys_by_type_by_locale[l] = { missing: missing, unused: unused, outdated: outdated }
-      bad_keys_count += @bad_keys_by_type_by_locale.values.flatten.size
+      bad_keys_by_type = { missing: missing, unused: unused, outdated: outdated }
+      bad_keys_count += bad_keys_by_type.values.flatten.size
+      @bad_keys_by_type_by_locale[locale] = bad_keys_by_type
     end
     @all_translations_perfect = bad_keys_count == 0
 
