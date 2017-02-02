275d3465c83ccb37a3a83d57d94d5b86a0502484
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-02-01 23:35:08 -0800

    Remember the status of our translations. This is safe to do because they'll only change when we restart the server. This fixes #1270.

--- WcaOnRails/app/controllers/server_status_controller.rb
+++ WcaOnRails/app/controllers/server_status_controller.rb
@@ -2,6 +2,14 @@
 class ServerStatusController < ApplicationController
   MINUTES_IN_WHICH_A_JOB_SHOULD_HAVE_STARTED_RUNNING = 5
 
+  def self.bad_i18n_keys
+    @bad_keys ||= (I18n.available_locales - [:en]).each_with_object({}) do |locale, hash|
+      ref_english = Locale.new('en')
+      missing, unused, outdated = Locale.new(locale, true).compare_to(ref_english)
+      hash[locale] = { missing: missing, unused: unused, outdated: outdated }
+    end
+  end
+
   def index
     @everything_good = true
 
@@ -12,17 +20,9 @@ class ServerStatusController < ApplicationController
     @regulations_load_error = Regulation.regulations_load_error
     @everything_good &&= @regulations_load_error.blank?
 
-    @ref_english = Locale.new('en')
-    @bad_keys_by_type_by_locale = {}
-    bad_keys_count = 0
-    (I18n.available_locales - [:en]).each do |locale|
-      ref_locale = Locale.new(locale, true)
-      missing, unused, outdated = ref_locale.compare_to(@ref_english)
-      bad_keys_by_type = { missing: missing, unused: unused, outdated: outdated }
-      bad_keys_count += bad_keys_by_type.values.flatten.size
-      @bad_keys_by_type_by_locale[locale] = bad_keys_by_type
-    end
-    @all_translations_perfect = bad_keys_count == 0
+    @bad_i18n_keys = self.class.bad_i18n_keys
+    bad_keys = @bad_i18n_keys.values.map(&:values).flatten
+    @all_translations_perfect = bad_keys.empty?
 
     if !@everything_good
       render status: 503
