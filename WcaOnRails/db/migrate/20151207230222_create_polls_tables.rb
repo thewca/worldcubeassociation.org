6a259b30320190737c39e19493f838b0567c7787
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-02-03 13:54:00 -0800

    Fix Style/SpaceAfterComma.

--- WcaOnRails/db/migrate/20151207230222_create_polls_tables.rb
+++ WcaOnRails/db/migrate/20151207230222_create_polls_tables.rb
@@ -2,27 +2,27 @@
 class CreatePollsTables < ActiveRecord::Migration
   def change
     create_table "polls", force: :cascade do |t|
-      t.string  "question", limit: 300, null: false
+      t.string "question", limit: 300, null: false
       t.boolean "multiple", limit: 1, null: false
-      t.date    "deadline", null: false
+      t.date "deadline", null: false
       t.boolean "confirmed", limit: 1, null: false
       t.timestamps null: false
     end
 
     create_table "poll_options", force: :cascade do |t|
-      t.string  "description", limit: 200, null: false
-      t.integer "poll_id",     null: false
+      t.string "description", limit: 200, null: false
+      t.integer "poll_id", null: false
     end
 
     add_index "poll_options", ["poll_id"], name: "poll_id", using: :btree
 
     create_table "votes", force: :cascade do |t|
-      t.integer "user_id",        null: false
+      t.integer "user_id", null: false
       t.integer "poll_option_id", null: false
-      t.string  "comment",        limit: 200
+      t.string "comment", limit: 200
     end
 
-    add_index :votes,:poll_option_id
+    add_index :votes, :poll_option_id
     add_index :votes, :user_id
 
     add_foreign_key "poll_options", "polls", name: "poll_options_ibfk_1"
