6f580e6fcc52af32e86057037182bc1bec0b6964
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-03-06 18:08:17 +0100

    Add a custom template for the dob form email.

--- WcaOnRails/app/models/dob_contact.rb
+++ WcaOnRails/app/models/dob_contact.rb
@@ -6,4 +6,8 @@ class DobContact < ContactForm
   def validate_wca_id
     errors.add(:wca_id, I18n.t('users.errors.not_found')) unless Person.find_by_wca_id(wca_id)
   end
+
+  def headers
+    super.merge(template_name: "dob_contact")
+  end
 end
