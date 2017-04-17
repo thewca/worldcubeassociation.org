00554007355ccee23d4da221008ebd54cad1a586
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-04-17 02:02:27 +0200

    Fix the bahaviour for people with multiple subIDs and a double render exception.

--- WcaOnRails/app/controllers/relations_controller.rb
+++ WcaOnRails/app/controllers/relations_controller.rb
@@ -7,13 +7,13 @@ class RelationsController < ApplicationController
   def relation
     @wca_id1 = params[:wca_id1]
     @wca_id2 = params[:wca_id2]
-    @selected_people = Person.where(wca_id: [@wca_id1, @wca_id2]).includes(:user)
+    @selected_people = Person.current.where(wca_id: [@wca_id1, @wca_id2]).includes(:user)
     if @selected_people.count != 2
       flash[:danger] = I18n.t('relations.messages.invalid_wca_ids')
-      redirect_to relations_url
+      redirect_to relations_url and return
     end
     wca_ids_chain = Relations.get_chain(@wca_id1, @wca_id2)
-    @people_chain = Person.where(wca_id: wca_ids_chain).includes(:user).sort_by { |person| wca_ids_chain.find_index person.wca_id }
+    @people_chain = Person.current.where(wca_id: wca_ids_chain).includes(:user).sort_by { |person| wca_ids_chain.find_index person.wca_id }
     render :relation
   end
 end
