1bbeb76863eea2551550f4508133f6110f4ddaf6
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-05-02 13:56:43 +0200

    Clean the way of showing solves.

--- WcaOnRails/app/helpers/results_helper.rb
+++ WcaOnRails/app/helpers/results_helper.rb
@@ -2,14 +2,11 @@
 
 module ResultsHelper
   def solve_tds_for_result(result)
-    trimmed_indices = result.trimmed_indices
-    best_index = result.best_index
-    worst_index = result.worst_index
     result.solve_times.each_with_index.map do |solve_time, i|
-      classes = ["solve#{i + 1}"]
-      classes << "trimmed" if trimmed_indices.include?(i)
-      classes << "best" if i == best_index
-      classes << "worst" if i == worst_index
+      classes = ["solve", i.to_s]
+      classes << "trimmed" if result.trimmed_indices.include?(i)
+      classes << "best" if i == result.best_index
+      classes << "worst" if i == result.worst_index
       content_tag :td, solve_time.clock_format, class: classes.join(' ')
     end.reduce(:+)
   end
