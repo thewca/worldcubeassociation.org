9f71aee9301a8176a4c18d2ff241dfa2eb9bd442
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-05-03 01:17:49 +0200

    Add support for In Memoriam.

--- WcaOnRails/app/helpers/persons_helper.rb
+++ WcaOnRails/app/helpers/persons_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module PersonsHelper
+  IN_MEMORIAM = {
+    "2008COUR01" => "/forum/viewtopic.php?t=2028",
+    "2003LARS01" => "/forum/viewtopic.php?t=1982",
+    "2012GALA02" => "/forum/viewtopic.php?t=1044",
+    "2008LIMR01" => "/forum/viewtopic.php?t=945",
+    "2008KIRC01" => "/forum/viewtopic.php?t=470",
+  }.freeze
+
   def rank_td(rank_object, type)
     rank = rank_object&.public_send("#{type}_rank")
     rank = "-" if rank == 0
