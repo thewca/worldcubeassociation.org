dc51ae0a96861228dc21ef05eb7fe4cc8621e1d5
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-05-06 18:16:15 +0200

    Test concise results tables computation.

--- WcaOnRails/spec/lib/auxiliary_data_computation_spec.rb
+++ WcaOnRails/spec/lib/auxiliary_data_computation_spec.rb
@@ -43,4 +43,52 @@ RSpec.describe "AuxiliaryDataComputation" do
       expect(with_completed_solves.reload.average).to eq (10.minutes + 10.seconds) * 100
     end
   end
+
+  describe ".compute_concise_results", clean_db_with_truncation: true do
+    let(:person) { FactoryGirl.create :person, countryId: "China" }
+    let(:competition_2016) { FactoryGirl.create :competition, starts: Date.parse("2016-04-04") }
+    let(:next_competition_2016) { FactoryGirl.create :competition, starts: Date.parse("2016-07-07") }
+    let(:competition_2017) { FactoryGirl.create :competition, starts: Date.parse("2017-08-08") }
+
+    it "creates tables containing best results data for each person per even per year" do
+      FactoryGirl.create :result, eventId: "333", best: 700, average: 800, competition: competition_2016, person: person
+      FactoryGirl.create :result, eventId: "333", best: 750, average: 850, competition: competition_2016, person: person
+      FactoryGirl.create :result, eventId: "333", best: 800, average: 900, competition: competition_2017, person: person
+      FactoryGirl.create :result, eventId: "222", best: 100, average: 150, competition: competition_2017, person: person
+      AuxiliaryDataComputation.compute_concise_results
+      # Concise single results
+      concise_single_results = ActiveRecord::Base.connection.execute "SELECT eventId, personId, year, best FROM ConciseSingleResults"
+      expect(concise_single_results).to match_array [
+        ["333", person.wca_id, 2016, 700],
+        ["333", person.wca_id, 2017, 800],
+        ["222", person.wca_id, 2017, 100],
+      ]
+      # Concise average results
+      concise_average_results = ActiveRecord::Base.connection.execute "SELECT eventId, personId, year, average FROM ConciseAverageResults"
+      expect(concise_average_results).to match_array [
+        ["333", person.wca_id, 2016, 800],
+        ["333", person.wca_id, 2017, 900],
+        ["222", person.wca_id, 2017, 150],
+      ]
+    end
+
+    it "creates multiple entries for people that have switched country in the middle of a year" do
+      FactoryGirl.create :result, eventId: "333", best: 700, average: 800, competition: competition_2016, person: person
+      person.update_using_sub_id! countryId: "Chile"
+      FactoryGirl.create :result, eventId: "333", best: 750, average: 850, competition: next_competition_2016, person: person
+      AuxiliaryDataComputation.compute_concise_results
+      # Concise single results
+      concise_single_results = ActiveRecord::Base.connection.execute "SELECT eventId, personId, countryId, year, best FROM ConciseSingleResults"
+      expect(concise_single_results).to match_array [
+        ["333", person.wca_id, "China", 2016, 700],
+        ["333", person.wca_id, "Chile", 2016, 750],
+      ]
+      # Concise average results
+      concise_average_results = ActiveRecord::Base.connection.execute "SELECT eventId, personId, countryId, year, average FROM ConciseAverageResults"
+      expect(concise_average_results).to match_array [
+        ["333", person.wca_id, "China", 2016, 800],
+        ["333", person.wca_id, "Chile", 2016, 850],
+      ]
+    end
+  end
 end
