fb452de5e574aa9f8da59c379351c71fc00ead79
Philippe Virouleau <philippe.44@gmail.com>
2017-05-18 21:21:33 +0200

    Update Countries using WCA List of States
    - Create a view to display the WCA List of States
    - Update Country table in the db, as well as seeds
    - Handle migration for User/Person/Result without a valid country anymore

--- WcaOnRails/app/models/country.rb
+++ WcaOnRails/app/models/country.rb
@@ -2,9 +2,29 @@
 
 class Country < ApplicationRecord
   include Cachable
+  WCA_STATES_JSON_PATH = Rails.root.to_s + "/config/wca-states.json"
   self.table_name = "Countries"
 
-  MULTIPLE_COUNTRIES_IDS = %w(XA XE XS).freeze
+  MULTIPLE_COUNTRIES = [
+    { id: 'XA', name: 'Multiple Countries (Asia)', continentId: '_Asia', iso2: 'XA' },
+    { id: 'XE', name: 'Multiple Countries (Europe)', continentId: '_Europe', iso2: 'XE' },
+    { id: 'XS', name: 'Multiple Countries (South America)', continentId: '_South America', iso2: 'XS' },
+  ].freeze
+
+  MULTIPLE_COUNTRIES_IDS = MULTIPLE_COUNTRIES.map { |c| c[:id] }.freeze
+
+  WCA_STATES = JSON.parse(File.read(WCA_STATES_JSON_PATH)).freeze
+
+  ALL_STATES = [
+    WCA_STATES["states_lists"].map do |list|
+      list["states"].map do |state|
+        state_id = state["id"] || I18n.transliterate(state["name"]).tr("'", "_")
+        { id: state_id, continentId: state["continent_id"],
+          iso2: state["iso2"], name: state["name"] }
+      end
+    end,
+    MULTIPLE_COUNTRIES,
+  ].flatten.freeze
 
   belongs_to :continent, foreign_key: :continentId
   has_many :competitions, foreign_key: :countryId
