a9052d6538aab4f8ee807bcff723c4fd46130c54
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-06-14 19:53:19 +0200

    Extend notifications_helper_spec.

--- WcaOnRails/spec/helpers/notifications_helper_spec.rb
+++ WcaOnRails/spec/helpers/notifications_helper_spec.rb
@@ -6,32 +6,35 @@ RSpec.describe NotificationsHelper do
   describe "#notifications_for_user" do
     context "when delegate" do
       let(:delegate) { FactoryGirl.create :delegate }
-      let!(:unconfirmed_competition) { FactoryGirl.create :competition, delegates: [delegate] }
-      let!(:confirmed_competition) { FactoryGirl.create :competition, delegates: [delegate], isConfirmed: true }
 
-      it "shows unconfirmed competitions" do
-        notifications = helper.notifications_for_user(delegate)
-        expect(notifications).to eq [
-          {
-            text: "#{unconfirmed_competition.name} is not confirmed",
-            url: edit_competition_path(unconfirmed_competition),
-          },
-        ]
-      end
+      context "with some unconfirmed competitions" do
+        let!(:unconfirmed_competition) { FactoryGirl.create :competition, delegates: [delegate] }
+        let!(:confirmed_competition) { FactoryGirl.create :competition, delegates: [delegate], isConfirmed: true }
 
-      it "doesn't duplicate competitions which we are both delegating and organizing" do
-        # Add ourselves as an organizer in addition to being a delegate
-        # for this competition.
-        unconfirmed_competition.organizers << delegate
-        unconfirmed_competition.save
+        it "shows unconfirmed competitions" do
+          notifications = helper.notifications_for_user(delegate)
+          expect(notifications).to eq [
+            {
+              text: "#{unconfirmed_competition.name} is not confirmed",
+              url: edit_competition_path(unconfirmed_competition),
+            },
+          ]
+        end
 
-        notifications = helper.notifications_for_user(delegate)
-        expect(notifications).to eq [
-          {
-            text: "#{unconfirmed_competition.name} is not confirmed",
-            url: edit_competition_path(unconfirmed_competition),
-          },
-        ]
+        it "doesn't duplicate competitions which we are both delegating and organizing" do
+          # Add ourselves as an organizer in addition to being a delegate
+          # for this competition.
+          unconfirmed_competition.organizers << delegate
+          unconfirmed_competition.save
+
+          notifications = helper.notifications_for_user(delegate)
+          expect(notifications).to eq [
+            {
+              text: "#{unconfirmed_competition.name} is not confirmed",
+              url: edit_competition_path(unconfirmed_competition),
+            },
+          ]
+        end
       end
 
       it "shows WCA ID claims for confirmed accounts, but not for unconfirmed accounts" do
@@ -45,14 +48,26 @@ RSpec.describe NotificationsHelper do
         notifications = helper.notifications_for_user(delegate)
         expect(notifications).to eq [
           {
-            text: "#{unconfirmed_competition.name} is not confirmed",
-            url: edit_competition_path(unconfirmed_competition),
-          },
-          {
             text: "#{user.email} has claimed WCA ID #{person.wca_id}",
             url: edit_user_path(user.id, anchor: "wca_id"),
           },
         ]
+      end
+
+      context "have delegated competitions that are missing reports" do
+        let!(:past_competition_missing_report) { FactoryGirl.create :competition, :past, :visible, :confirmed, delegates: [delegate] }
+        let!(:past_competition_having_report) { FactoryGirl.create :competition, :past, :visible, :confirmed, :with_delegate_report, delegates: [delegate] }
+        let!(:future_competition) { FactoryGirl.create :competition, :future, :visible, :confirmed, delegates: [delegate] }
+
+        it "asks me to submit the reports" do
+          notifications = helper.notifications_for_user(delegate)
+          expect(notifications).to eq [
+            {
+              text: "The delegate report for #{past_competition_missing_report.name} has not been submitted.",
+              url: delegate_report_path(past_competition_missing_report),
+            },
+          ]
+        end
       end
     end
 
