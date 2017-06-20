94ae0a20a153f2a179767b3ebb97154b9b9d50f5
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-06-20 15:17:46 +0200

    Remove remainings of teams creation. Add a test.

---
+++ WcaOnRails/spec/views/teams/index.html.erb_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "teams/index.html.erb" do
+  describe "when signed in as an admin" do
+    let!(:user) { FactoryGirl.create :admin }
+    let!(:teams) { Team.all }
+    let!(:team_member) { FactoryGirl.create :team_member, user_id: user.id, team_id: teams.first.id }
+
+    before do
+      allow(view).to receive(:current_user) { user }
+      assign(:teams, teams)
+      render
+    end
+
+    it "lists teams" do
+      teams.each do |team|
+        expect(rendered).to match team.name
+      end
+    end
+
+    it "shows members of a team" do
+      expect(rendered).to match user.name
+    end
+  end
+end
