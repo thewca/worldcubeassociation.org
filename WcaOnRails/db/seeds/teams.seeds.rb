142ce6892ac26c91e37a93b97b596d68dcde69ca
Pedro Santos <pedrosino@gmail.com>
2017-06-24 16:46:22 -0300

    Create WCA Financial Committee.

--- WcaOnRails/db/seeds/teams.seeds.rb
+++ WcaOnRails/db/seeds/teams.seeds.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Team.create(friendly_id: 'wct', rank: 1, email: "communication@worldcubeassociation.org")
-Team.create(friendly_id: 'wdc', rank: 2, email: "wdc@worldcubeassociation.org")
-Team.create(friendly_id: 'wrc', rank: 3, email: "wrc@worldcubeassociation.org")
-Team.create(friendly_id: 'wrt', rank: 4, email: "results@worldcubeassociation.org")
-Team.create(friendly_id: 'wst', rank: 5, email: "software@worldcubeassociation.org")
+Team.create(friendly_id: 'wdc', rank: 3, email: "wdc@worldcubeassociation.org")
+Team.create(friendly_id: 'wfc', rank: 5, email: "finance@worldcubeassociation.org")
+Team.create(friendly_id: 'wrc', rank: 7, email: "wrc@worldcubeassociation.org")
+Team.create(friendly_id: 'wrt', rank: 9, email: "results@worldcubeassociation.org")
+Team.create(friendly_id: 'wst', rank: 11, email: "software@worldcubeassociation.org")
