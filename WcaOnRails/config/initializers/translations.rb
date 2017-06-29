b3a411aad5f0ca050e9288c4f2dc3cf0df475aee
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-06-29 12:27:57 +0200

    Remove pointless condition that doesn't work.

--- WcaOnRails/config/initializers/translations.rb
+++ WcaOnRails/config/initializers/translations.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if defined?(Rails::Server) # See: https://stackoverflow.com/a/44441168
+if Rails.env.production?
   Rails.configuration.after_initialize do
     modification_timestamp = Timestamp.find_or_create_by!(name: 'en_translation_modification')
     latest_modification_date = DateTime.parse(`git log -1 --format='%ai' #{Rails.root.to_s}/config/locales/en.yml`)
