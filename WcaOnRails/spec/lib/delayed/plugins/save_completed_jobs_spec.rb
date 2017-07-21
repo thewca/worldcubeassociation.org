ad96e41bdd12c0f7955fef3910aaea229538636d
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-07-20 17:59:38 -0700

    Woohoo upgrading rails!

--- WcaOnRails/spec/lib/delayed/plugins/save_completed_jobs_spec.rb
+++ WcaOnRails/spec/lib/delayed/plugins/save_completed_jobs_spec.rb
@@ -19,11 +19,14 @@ RSpec.describe Delayed::Plugins::SaveCompletedJobs, type: :feature do
     around(:each) do |example|
       old_delay_jobs = Delayed::Worker.delay_jobs
       old_queue_adapter = ActiveJob::Base.queue_adapter
+      old_test_adapter = ActiveJob::Base._test_adapter
       Delayed::Worker.delay_jobs = true
       ActiveJob::Base.queue_adapter = :delayed_job
+      ActiveJob::Base.disable_test_adapter
       example.run
       Delayed::Worker.delay_jobs = old_delay_jobs
       ActiveJob::Base.queue_adapter = old_queue_adapter
+      ActiveJob::Base.enable_test_adapter(old_test_adapter)
     end
 
     it "stores completed jobs in completed_jobs table" do
