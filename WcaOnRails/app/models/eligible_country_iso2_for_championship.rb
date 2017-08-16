3e8b651c6aed1b52d3e1ef5d3a3a4f3a1d4ef3e4
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-08-16 16:33:35 +0200

    Add the corresponding model and seeds.

---
+++ WcaOnRails/app/models/eligible_country_iso2_for_championship.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class EligibleCountryIso2ForChampionship < ApplicationRecord
+  self.table_name = "eligible_country_iso2s_for_championship"
+
+  validates :eligible_country_iso2, uniqueness: { scope: :championship_type },
+                                    inclusion: { in: Country.all.map(&:iso2) }
+end
