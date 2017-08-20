5d551a6f74687a99861ad0f7c6ed001cf927f415
Philippe Virouleau <philippe.44@gmail.com>
2017-08-20 18:32:03 +0200

    Add a section for SSL certificate in server status
    Server status will go red if we enter the last 10 days of our SSL certificate (either on staging or production).
    
    In test or development, this part of the server status will always been green, and will indicate whether or not it has found a certificate.

--- WcaOnRails/app/controllers/server_status_controller.rb
+++ WcaOnRails/app/controllers/server_status_controller.rb
@@ -2,6 +2,9 @@
 
 class ServerStatusController < ApplicationController
   MINUTES_IN_WHICH_A_JOB_SHOULD_HAVE_STARTED_RUNNING = 5
+  CERTIFICATE_PATH = "#{Rails.root}/../secrets/#{URI.parse(ENVied.ROOT_URL).host}"
+  # We want to be warned 10 days before certificate's renewal
+  CERTIFICATE_RENEW_DELAY = 10
 
   def index
     @everything_good = true
@@ -12,6 +15,21 @@ class ServerStatusController < ApplicationController
 
     @regulations_load_error = Regulation.regulations_load_error
     @everything_good &&= @regulations_load_error.blank?
+
+    # We set @expires_in the following way:
+    #  - if nil then no certificate was found
+    #  - a positive integer indicates we have this number of days before expiration
+    #  - a negative integer indicates it's expired by this number of days
+    begin
+      raw = File.read(CERTIFICATE_PATH)
+      certificate = OpenSSL::X509::Certificate.new(raw)
+      @expires_in = (certificate.not_after.to_date - Time.now.to_date).to_i
+    rescue
+      @expires_in = nil
+    end
+    # If we're in test or development, we don't want to go red on the SSL certificate.
+    @certificate_good = Rails.env.test? || Rails.env.development? || (@expires_in || 0) > ServerStatusController::CERTIFICATE_RENEW_DELAY
+    @everything_good &&= @certificate_good
 
     @locale_stats = ApplicationController.locale_counts.sort_by { |locale, count| count }.reverse
 
