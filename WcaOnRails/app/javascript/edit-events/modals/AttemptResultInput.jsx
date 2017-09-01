cec04e43a180f134e7e90c9ae13d9cfaedf9b3de
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-08-31 18:10:23 -0700

    Add support for mbld points =)

--- WcaOnRails/app/javascript/edit-events/modals/AttemptResultInput.jsx
+++ WcaOnRails/app/javascript/edit-events/modals/AttemptResultInput.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import events from 'wca/events.js.erb'
+import { mbPointsToAttemptResult, attemptResultToMbPoints } from './utils'
 
 export default class extends React.Component {
   constructor(props) {
@@ -12,7 +13,17 @@ export default class extends React.Component {
   }
 
   get value() {
-    return parseInt(this.centisInput.value);
+    let event = events.byId[this.props.eventId];
+
+    if(event.timed_event) {
+      return parseInt(this.centisInput.value);
+    } else if(event.fewest_moves) {
+      return parseInt(this.movesInput.value);
+    } else if(event.multiple_blindfolded) {
+      return mbPointsToAttemptResult(parseInt(this.mbldPointsInput.value));
+    } else {
+      throw new Error(`Unrecognized event type: ${event.id}`);
+    }
   }
 
   render() {
@@ -35,13 +46,27 @@ export default class extends React.Component {
     } else if(event.fewest_moves) {
       return (
         <div>
-          fewest moves? urg
+          <input type="number"
+                 id={id}
+                 className="form-control"
+                 autoFocus={autoFocus}
+                 value={this.props.value}
+                 ref={c => this.movesInput = c}
+                 onChange={this.onChange} />
+          (moves)
         </div>
       );
     } else if(event.multiple_blindfolded) {
       return (
         <div>
-          multiblind? urg
+          <input type="number"
+                 id={id}
+                 className="form-control"
+                 autoFocus={autoFocus}
+                 value={attemptResultToMbPoints(this.props.value)}
+                 ref={c => this.mbldPointsInput = c}
+                 onChange={this.onChange} />
+          (mbld points)
         </div>
       );
     } else {
--- WcaOnRails/app/javascript/edit-events/modals/utils.js
+++ WcaOnRails/app/javascript/edit-events/modals/utils.js
@@ -1,5 +1,53 @@
 import events from 'wca/events.js.erb'
 
+function parseMbValue(mbValue) {
+  let old = Math.floor(mbValue / 1000000000) !== 0;
+  let timeSeconds, attempted, solved;
+  if(old) {
+    timeSeconds = mbValue % 100000;
+    mbValue = Math.floor(mbValue / 100000);
+    attempted = mbValue % 100;
+    mbValue = Math.floor(mbValue / 100);
+    solved = 99 - mbValue % 100;
+  } else {
+    let missed = mbValue % 100;
+    mbValue = Math.floor(mbValue / 100);
+    timeSeconds = mbValue % 100000;
+    mbValue = Math.floor(mbValue / 100000);
+    let difference = 99 - (mbValue % 100);
+
+    solved = difference + missed;
+    attempted = solved + missed;
+  }
+
+  let timeCentiseconds = timeSeconds == 99999 ? null : timeSeconds * 100;
+  return { solved, attempted, timeCentiseconds };
+}
+
+function parsedMbToAttemptResult(parsedMb) {
+  let { solved, attempted, timeCentiseconds } = parsedMb;
+  let missed = attempted - solved;
+
+  let mm = missed;
+  let dd = 99 - (solved - missed);
+  let ttttt = Math.floor(timeCentiseconds / 100);
+  return (dd * 1e7 + ttttt * 1e2 + mm);
+}
+
+// See https://www.worldcubeassociation.org/regulations/#9f12c
+export function attemptResultToMbPoints(mbValue) {
+  let { solved, attempted } = parseMbValue(mbValue);
+  let missed = attempted - solved;
+  return solved - missed;
+}
+
+export function mbPointsToAttemptResult(mbPoints) {
+  let solved = mbPoints;
+  let attempted = mbPoints;
+  let timeCentiseconds = 99999*100;
+  return parsedMbToAttemptResult({ solved, attempted, timeCentiseconds });
+}
+
 export function attemptResultToString(attemptResult, eventId) {
   let event = events.byId[eventId];
   if(event.timed_event) {
@@ -7,7 +55,7 @@ export function attemptResultToString(attemptResult, eventId) {
   } else if(event.fewest_moves) {
     return `${attemptResult} moves`;
   } else if(event.multiple_blindfolded) {
-    return `${attemptResult} points`; // TODO <<<>>>
+    return `${attemptResultToMbPoints(attemptResult)} points`;
   } else {
     throw new Error(`Unrecognized event type: ${eventId}`);
   }
@@ -18,7 +66,6 @@ export function centisecondsToString(centiseconds) {
   const minutes = seconds / 60;
   const hours = minutes / 60;
 
-  // TODO <<< >>>
   if(hours >= 1) {
     return `${hours.toFixed(2)} hours`;
   } else if(minutes >= 1) {
