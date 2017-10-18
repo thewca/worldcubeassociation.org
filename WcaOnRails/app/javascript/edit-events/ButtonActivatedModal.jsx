9dbe957274c9df98a679371cbc8a00c2a84f0679
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-10-18 10:21:49 -0700

    Get rid of the KeydownDismissModal now that we've upgraded react-overlays.

--- WcaOnRails/app/javascript/edit-events/ButtonActivatedModal.jsx
+++ WcaOnRails/app/javascript/edit-events/ButtonActivatedModal.jsx
@@ -26,7 +26,7 @@ export default class ButtonActivatedModal extends React.Component {
     return (
       <button type="button" name={this.props.name} className={cn("btn", this.props.buttonClass)} onClick={this.open}>
         {this.props.buttonValue}
-        <KeydownDismissModal show={this.state.showModal} onHide={this.close}>
+        <Modal show={this.state.showModal} onHide={this.close}>
           <form className={this.props.formClass}
                 onSubmit={e => { e.preventDefault(); this.props.onOk(); }}
                 onClick={e => {
@@ -43,44 +43,8 @@ export default class ButtonActivatedModal extends React.Component {
               <Button type="submit" bsStyle="success">Ok</Button>
             </Modal.Footer>
           </form>
-        </KeydownDismissModal>
+        </Modal>
       </button>
     );
-  }
-}
-
-// More or less copied from https://github.com/react-bootstrap/react-overlays/pull/195
-// This can go away once a new version of react-overlays is released and react-bootstrap
-// is updated to depend on it.
-class KeydownDismissModal extends React.Component {
-  static defaultProps = Modal.defaultProps;
-
-  handleDocumentKeyDown = (e) => {
-    if (this.props.keyboard && e.key === 'Escape' && this._modal._modal.isTopModal()) {
-      if (this.props.onEscapeKeyDown) {
-        this.props.onEscapeKeyDown(e);
-      }
-
-      this.props.onHide();
-    }
-  }
-
-  onShow = () => {
-    let doc = ownerDocument(this);
-    this._onDocumentKeydownListener =
-      addEventListener(doc, 'keydown', this.handleDocumentKeyDown);
-  }
-
-  onHide = () => {
-    this._onDocumentKeydownListener.remove();
-  }
-
-  render() {
-    let subprops = {
-      ...this.props,
-      keyboard: false,
-      onShow: this.onShow,
-    };
-    return <Modal {...subprops} ref={m => this._modal = m} />;
   }
 }
