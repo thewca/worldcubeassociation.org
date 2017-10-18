cc06f6142cba0c8c86542e2d71c7d5708b846663
AlbertoPdRF <albertopdrf.wca@gmail.com>
2017-10-18 19:48:41 +0200

    Putting some color to the e-mail to make the warning more clear.

--- WcaOnRails/app/models/championship.rb
+++ WcaOnRails/app/models/championship.rb
@@ -6,6 +6,7 @@ class Championship < ApplicationRecord
   validates_presence_of :competition
   validates :championship_type, uniqueness: { scope: :competition_id },
                                 inclusion: { in: ["world", *Continent.all.map(&:id), *Country.all.map(&:iso2), *EligibleCountryIso2ForChampionship.championship_types] }
+
   def name
     return "World Championship" if championship_type == "world"
 
