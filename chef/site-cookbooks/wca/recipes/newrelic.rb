5e42ef1eb181928351470d4032d38a1f85e0240d
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-10-19 09:55:05 -0700

    New Relic Servers is going away. See https://discuss.newrelic.com/t/important-upcoming-changes-for-new-relic-servers-and-legacy-alerting-features/49474..

--- chef/site-cookbooks/wca/recipes/newrelic.rb
+++
@@ -1,6 +0,0 @@
-secrets = WcaHelper.get_secrets(self)
-
-if node.chef_environment == "production" || node.chef_environment == "staging"
-  node.default['newrelic-sysmond']['license_key'] = secrets['NEW_RELIC_LICENSE_KEY']
-  include_recipe "newrelic-sysmond"
-end
