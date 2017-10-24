4cc65df1b024af5604a66e180ad81663504da76e
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-10-24 09:38:14 -0700

    Added :long_utc date format as per @pedrosino's recommendation.

---
+++ WcaOnRails/config/initializers/datetime_formats.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# See http://gavinmorrice.com/posts/3-keeping-your-dates-and-times-dry-with-to_formatted_s
+[Time, Date].map do |klass|
+  klass::DATE_FORMATS[:long_utc] = lambda { |t| t.strftime("#{t.to_s(:long)} %Z") }
+end
