d1d5ebef201a077e642d8794be25d74150479533
Emric <w.e.w@live.se>
2017-11-07 00:09:18 +0100

    Add informative message for unique wca id validation error, closes #774
    When editing a user you would not get a sufficient error
    message explaining what is actually wrong. This commit adds an
    error message explaining that the WCA ID is already taken.

--- WcaOnRails/app/models/user.rb
+++ WcaOnRails/app/models/user.rb
@@ -78,7 +78,9 @@ class User < ApplicationRecord
       user = User.find_by_wca_id(wca_id)
       # If there is a non dummy user with this WCA ID, fail validation.
       if user && !user.dummy_account?
-        errors.add(:wca_id, I18n.t('users.errors.unique'))
+        errors.add(:wca_id, I18n.t('users.errors.unique',
+                   used_name: user.name,
+                   used_email: user.email))
       end
     end
   end
