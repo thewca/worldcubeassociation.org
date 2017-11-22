dd6f60fd742f961b5edcba4835be68b6fac5e7d8
Caleb Hoover <choover11@gmail.com>
2017-11-21 20:55:21 -0800

    added migration to remove rubik's from event names

---
+++ WcaOnRails/db/migrate/20171122010954_remove_rubiks.rb
@@ -0,0 +1,23 @@
+class RemoveRubiks < ActiveRecord::Migration[5.1]
+  def change
+    execute <<-SQL
+      update Events set name = '3x3x3 Cube' where id = '333';
+    SQL
+    execute <<-SQL
+      update Events set name = 'Clock', rank = 110 where id = 'clock';
+    SQL
+    execute <<-SQL
+      update Events set  rank = 120 where id = 'minx';
+    SQL
+    execute <<-SQL
+      update Events set rank = 130 where id = 'pyram';
+    SQL
+    execute <<-SQL
+      update Events set name = '3x3x3 Multi-Blind Old Style' where id = '333mbo';
+    SQL
+
+    execute <<-SQL
+      update Events set cellName = name;
+    SQL
+  end
+end
