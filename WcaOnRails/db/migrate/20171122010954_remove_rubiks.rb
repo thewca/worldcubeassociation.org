e2627a07a7e23322321d87cb1b7ce60778e7fb30
Caleb Hoover <choover11@gmail.com>
2017-11-22 21:21:02 -0800

    redid remove_rubiks migration to just empty events and load from events.seeds.rb

--- WcaOnRails/db/migrate/20171122010954_remove_rubiks.rb
+++ WcaOnRails/db/migrate/20171122010954_remove_rubiks.rb
@@ -1,25 +1,13 @@
 # frozen_string_literal: true
 
 class RemoveRubiks < ActiveRecord::Migration[5.1]
-  def change
-    execute <<-SQL
-      update Events set name = '3x3x3 Cube' where id = '333';
-    SQL
-    execute <<-SQL
-      update Events set name = 'Clock', rank = 110 where id = 'clock';
-    SQL
-    execute <<-SQL
-      update Events set  rank = 120 where id = 'minx';
-    SQL
-    execute <<-SQL
-      update Events set rank = 130 where id = 'pyram';
-    SQL
-    execute <<-SQL
-      update Events set name = '3x3x3 Multi-Blind Old Style' where id = '333mbo';
-    SQL
+  def up
+    execute "DELETE FROM Events"
+    load "#{Rails.root}/db/seeds/events.seeds.rb"
+  end
 
-    execute <<-SQL
-      update Events set cellName = name;
-    SQL
+  def down
+    execute "DELETE FROM Events"
+    load "#{Rails.root}/db/seeds/events.seeds.rb"
   end
 end
