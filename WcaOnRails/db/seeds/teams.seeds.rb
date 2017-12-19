4fb69891956266725e6e5a528926751b8e0fbdf0
Pedro Santos <pedrosino@gmail.com>
2017-12-19 18:54:53 -0200

    WEC and WQAC descriptions.

--- WcaOnRails/db/seeds/teams.seeds.rb
+++ WcaOnRails/db/seeds/teams.seeds.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Team.create(friendly_id: 'wct', rank: 10, email: "communication@worldcubeassociation.org")
-Team.create(friendly_id: 'wdc', rank: 20, email: "wdc@worldcubeassociation.org")
+Team.create(friendly_id: 'wdc', rank: 20, email: "disciplinary@worldcubeassociation.org")
 Team.create(friendly_id: 'wec', rank: 30, email: "ethics@worldcubeassociation.org")
 Team.create(friendly_id: 'wfc', rank: 40, email: "finance@worldcubeassociation.org")
 Team.create(friendly_id: 'wqac', rank: 50, email: "quality@worldcubeassociation.org")
-Team.create(friendly_id: 'wrc', rank: 60, email: "wrc@worldcubeassociation.org")
+Team.create(friendly_id: 'wrc', rank: 60, email: "regulations@worldcubeassociation.org")
 Team.create(friendly_id: 'wrt', rank: 70, email: "results@worldcubeassociation.org")
 Team.create(friendly_id: 'wst', rank: 80, email: "software@worldcubeassociation.org")
