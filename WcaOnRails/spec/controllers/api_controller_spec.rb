9c54e4bbdccff56c16ef25d5ee916b550c3bf722
Philippe Virouleau <philippe.44@gmail.com>
2018-01-29 17:42:15 +0100

    Fix typos

--- WcaOnRails/spec/controllers/api_controller_spec.rb
+++ WcaOnRails/spec/controllers/api_controller_spec.rb
@@ -245,7 +245,7 @@ RSpec.describe Api::V0::ApiController do
       get :scramble_program
       expect(response.status).to eq 200
       json = JSON.parse(response.body)
-      expect(json["current"]["name"]).to eq "TNoodle-WCA-0.13.0"
+      expect(json["current"]["name"]).to eq "TNoodle-WCA-0.13.1"
     end
   end
 
