0a763e530e91c2cb951614e0ec0af5da5d25f8cb
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-03-05 08:46:11 -0800

    Making rubocop happy.

--- WcaOnRails/spec/rails_helper.rb
+++ WcaOnRails/spec/rails_helper.rb
@@ -3,7 +3,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
