af2d4f5f8b046147cd59f561af01804a5a770e40
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-03-06 23:14:28 +0100

    Add an information to translation PR update indicating whether the translator is verified or not.

--- WcaOnRails/app/controllers/translations_controller.rb
+++ WcaOnRails/app/controllers/translations_controller.rb
@@ -46,12 +46,41 @@ class TranslationsController < ApplicationController
     Octokit.create_ref(origin_repo, "heads/#{branch_name}", upstream_sha)
     current_content_sha = Octokit.content(origin_repo, path: file_path, ref: branch_name)[:sha]
     Octokit.update_content(origin_repo, file_path, message, current_content_sha, content, branch: branch_name)
-    @pr_url = Octokit.create_pull_request(upstream_repo, "master", "#{user_login}:#{branch_name}", message, pr_description_for(current_user))[:html_url]
+    @pr_url = Octokit.create_pull_request(upstream_repo, "master", "#{user_login}:#{branch_name}", message, pr_description_for(current_user, locale))[:html_url]
   end
 
-  private def pr_description_for(user)
+  VERIFIED_TRANSLATORS_BY_LOCALE = {
+    "cs" => %w(2015VAST01),
+    "de" => %w(2009OHRN01),
+    "es" => %w(2013MORA02 2010GARC02),
+    "fi" => %w(2017NORR01),
+    "fr" => %w(2008VIRO01),
+    "hu" => %w(2008PLAC01),
+    "it" => %w(2009COLO03),
+    "ja" => %w(2010TERA01),
+    "pl" => %w(2013KOSK01),
+    "pt" => %w(2014GOME07),
+    "pt-BR" => %w(2007GUIM01),
+    "zh-CN" => %w(2008DONG06),
+    "zh-TW" => %w(2011LIUR02),
+    "nl" => %w(2003BRUC01),
+    "ro" => %w(2015TOMA04),
+    "ru" => %w(2011GRIT01),
+    "da" => %w(2013EGDA01),
+    "hr" => %w(2013VIDA03),
+    "sk" => %w(2015MOZO01),
+    "sl" => %w(2010OMUL01),
+    "id" => %w(2015HUDO01),
+  }.freeze
+
+  private def pr_description_for(user, locale)
     info = ["WCA Account ID: *#{user.id}*"]
-    info.unshift("WCA ID: *#{user.wca_id}*") if user.wca_id
-    "Submitted by #{user.name} (#{info.join(', ')})."
+    info.unshift("WCA ID: *[#{user.wca_id}](#{person_url(user.wca_id)})*") if user.wca_id
+    verification_info = if VERIFIED_TRANSLATORS_BY_LOCALE[locale]&.include?(user.wca_id)
+                          ":heavy_check_mark: This translation comes from a verified translator for this language."
+                        else
+                          ":warning: This translation doesn't come from a verified translator for this language."
+                        end
+    "Submitted by #{user.name} (#{info.join(', ')}).\n\n#{verification_info}"
   end
 end
