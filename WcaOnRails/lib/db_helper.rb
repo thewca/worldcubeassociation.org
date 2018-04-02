82369b43d50520ed110a23c14e5048f39e99c398
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-04-03 00:29:59 +0200

    Utilize the multiline feature of mysql2 for multiple SQL queries.

--- WcaOnRails/lib/db_helper.rb
+++ WcaOnRails/lib/db_helper.rb
@@ -3,8 +3,11 @@
 module DbHelper
   # Executes a multiline SQL.
   def self.execute_sql(sql)
-    sql.split(/;\s*$/).each do |statement|
-      ActiveRecord::Base.connection.execute statement if statement.present?
-    end
+    connection = ActiveRecord::Base.connection
+    raw_connection = connection.raw_connection
+    raw_connection.set_server_option(Mysql2::Client::OPTION_MULTI_STATEMENTS_ON)
+    connection.execute sql
+    raw_connection.set_server_option(Mysql2::Client::OPTION_MULTI_STATEMENTS_OFF)
+    raw_connection.abandon_results!
   end
 end
