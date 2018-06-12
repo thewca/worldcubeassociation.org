ae649c285a2e8938514c881023086b097bebeced
AlbertoPdRF <albertopdrf.wca@gmail.com>
2018-06-12 18:38:15 +0200

    Fix flaky tests with names that require html entity encoding (fixes #2954).

--- WcaOnRails/spec/mailers/delegate_status_change_mailer_spec.rb
+++ WcaOnRails/spec/mailers/delegate_status_change_mailer_spec.rb
@@ -4,16 +4,16 @@ require "rails_helper"
 
 RSpec.describe DelegateStatusChangeMailer, type: :mailer do
   describe "notify_board_and_wqac_of_delegate_status_change" do
-    let(:senior_delegate1) { FactoryBot.create :senior_delegate }
-    let(:senior_delegate2) { FactoryBot.create :senior_delegate }
-    let(:delegate) { FactoryBot.create :delegate, delegate_status: "candidate_delegate", senior_delegate: senior_delegate1 }
-    let(:user) { FactoryBot.create :user }
+    let(:senior_delegate1) { FactoryBot.create :senior_delegate, name: "Eddard Stark" }
+    let(:senior_delegate2) { FactoryBot.create :senior_delegate, name: "Catelyn Stark" }
+    let(:delegate) { FactoryBot.create :delegate, name: "Daenerys Targaryen", delegate_status: "candidate_delegate", senior_delegate: senior_delegate1 }
+    let(:user) { FactoryBot.create :user, name: "Jon Snow" }
 
     it "email headers are correct" do
       user.update!(delegate_status: "candidate_delegate", senior_delegate: senior_delegate1)
       mail = DelegateStatusChangeMailer.notify_board_and_wqac_of_delegate_status_change(user, senior_delegate1)
 
-      expect(mail.subject).to eq("#{senior_delegate1.name} just changed the Delegate status of #{user.name}")
+      expect(mail.subject).to eq("Eddard Stark just changed the Delegate status of Jon Snow")
       expect(mail.to).to eq(["board@worldcubeassociation.org"])
       expect(mail.cc).to eq(["quality@worldcubeassociation.org", senior_delegate1.email, senior_delegate1.email])
       expect(mail.from).to eq(["notifications@worldcubeassociation.org"])
@@ -24,7 +24,7 @@ RSpec.describe DelegateStatusChangeMailer, type: :mailer do
       user.update!(delegate_status: "candidate_delegate", senior_delegate: senior_delegate1)
       mail = DelegateStatusChangeMailer.notify_board_and_wqac_of_delegate_status_change(user, senior_delegate1)
 
-      expect(mail.body.encoded).to match("#{senior_delegate1.name} has changed the Delegate status of #{user.name} from Registered Speedcuber to Candidate Delegate.")
+      expect(mail.body.encoded).to match("Eddard Stark has changed the Delegate status of Jon Snow from Registered Speedcuber to Candidate Delegate.")
       expect(mail.body.encoded).not_to match("Warning")
       expect(mail.body.encoded).to match(edit_user_url(user))
     end
@@ -35,7 +35,7 @@ RSpec.describe DelegateStatusChangeMailer, type: :mailer do
       mail = DelegateStatusChangeMailer.notify_board_and_wqac_of_delegate_status_change(user, senior_delegate1)
 
       expect(mail.cc).to eq(["quality@worldcubeassociation.org", senior_delegate1.email])
-      expect(mail.body.encoded).to match("Warning: #{senior_delegate1.name} forgot to assign a Senior Delegate to #{user.name}")
+      expect(mail.body.encoded).to match("Warning: Eddard Stark forgot to assign a Senior Delegate to Jon Snow")
       expect(mail.body.encoded).to match(edit_user_url(user))
     end
 
@@ -43,7 +43,7 @@ RSpec.describe DelegateStatusChangeMailer, type: :mailer do
       delegate.update!(delegate_status: "delegate")
       mail = DelegateStatusChangeMailer.notify_board_and_wqac_of_delegate_status_change(delegate, senior_delegate1)
 
-      expect(mail.body.encoded).to match("#{senior_delegate1.name} has changed the Delegate status of #{delegate.name} from Candidate Delegate to Delegate.")
+      expect(mail.body.encoded).to match("Eddard Stark has changed the Delegate status of Daenerys Targaryen from Candidate Delegate to Delegate.")
       expect(mail.body.encoded).not_to match("Warning")
       expect(mail.body.encoded).to match(edit_user_url(delegate))
     end
@@ -52,7 +52,7 @@ RSpec.describe DelegateStatusChangeMailer, type: :mailer do
       delegate.update!(delegate_status: nil, senior_delegate: nil)
       mail = DelegateStatusChangeMailer.notify_board_and_wqac_of_delegate_status_change(delegate, senior_delegate1)
 
-      expect(mail.body.encoded).to match("#{senior_delegate1.name} has changed the Delegate status of #{delegate.name} from Candidate Delegate to Registered Speedcuber.")
+      expect(mail.body.encoded).to match("Eddard Stark has changed the Delegate status of Daenerys Targaryen from Candidate Delegate to Registered Speedcuber.")
       expect(mail.body.encoded).not_to match("Warning")
       expect(mail.body.encoded).to match(edit_user_url(delegate))
     end
@@ -61,7 +61,7 @@ RSpec.describe DelegateStatusChangeMailer, type: :mailer do
       delegate.update!(delegate_status: "delegate")
       mail = DelegateStatusChangeMailer.notify_board_and_wqac_of_delegate_status_change(delegate, senior_delegate2)
 
-      expect(mail.body.encoded).to match("Warning: #{senior_delegate2.name} is not #{delegate.name}'s Senior Delegate.")
+      expect(mail.body.encoded).to match("Warning: Catelyn Stark is not Daenerys Targaryen's Senior Delegate.")
     end
   end
 end
