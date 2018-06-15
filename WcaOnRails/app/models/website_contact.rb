27dab137439d33032cd617b69152f09f14136326
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-06-15 13:21:57 +0200

    Fix radio button values to be unique.

--- WcaOnRails/app/models/website_contact.rb
+++ WcaOnRails/app/models/website_contact.rb
@@ -3,14 +3,14 @@
 class WebsiteContact < ContactForm
   attribute :message, validate: true
 
-  attr_accessor :inquiry_target
+  attr_accessor :inquiry
   attr_accessor :competition_id
 
-  # Override the `to_mail` validation, to show errors for `inquiry_target` instead.
+  # Override the `to_mail` validation, to show errors for `inquiry` instead.
   def validate_to_email
     super
     if errors[:to_email].any?
-      errors.add(:inquiry_target, I18n.t('common.errors.invalid'))
+      errors.add(:inquiry, I18n.t('common.errors.invalid'))
       errors.delete(:to_email)
     end
   end
