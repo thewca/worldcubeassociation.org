ca9e61a0dbf0a99739e6b59f3d961b605d065844
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-06-21 21:02:53 +0200

    Move determining to_email logic to the corresponding model.

--- WcaOnRails/app/controllers/contacts_controller.rb
+++ WcaOnRails/app/controllers/contacts_controller.rb
@@ -8,18 +8,6 @@ class ContactsController < ApplicationController
   def website_create
     @contact = WebsiteContact.new(params[:website_contact])
     @contact.request = request
-    case @contact.inquiry
-    when "competitions_in_general", "different"
-      @contact.to_email = Team.wct.email
-    when "wca_id_or_profile", "media"
-      @contact.to_email = Team.wrt.email
-    when "software"
-      @contact.to_email = Team.wst.email
-    when "competition"
-      @contact.to_email = Competition.find_by_id(@contact.competition_id)&.managers&.map(&:email)
-    else
-      raise "Invalid inquiry type: `#{@contact.inquiry}`" if @contact.inquiry.present?
-    end
     @contact.subject = Time.now.strftime("WCA Website Comments by #{@contact.name} on %d %b %Y at %R")
     maybe_send_email success_url: contact_website_url, fail_view: :website
   end
