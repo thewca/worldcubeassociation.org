3f98885609db868318bb2ef187c3751f4601a420
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-07-03 19:32:20 +0200

    Add database migration.

---
+++ WcaOnRails/db/migrate/20180703172949_add_hidden_to_teams.rb
@@ -0,0 +1,6 @@
+class AddHiddenToTeams < ActiveRecord::Migration[5.2]
+  def change
+    add_column :teams, :hidden, :boolean, null: false, default: false
+    Team.update_all hidden: false
+  end
+end
