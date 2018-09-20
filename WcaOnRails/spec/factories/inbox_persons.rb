349e9fcb7d432fbdef3e91b2c7295e189aca2c94
Philippe Virouleau <philippe.44@gmail.com>
2018-09-20 14:38:44 +0200

    fixes after rebase

--- WcaOnRails/spec/factories/inbox_persons.rb
+++ WcaOnRails/spec/factories/inbox_persons.rb
@@ -2,11 +2,11 @@
 
 FactoryBot.define do
   factory :inbox_person do
-    id 1
-    wcaId ""
+    id { 1 }
+    wcaId { "" }
     name { Faker::Name.name }
     countryId { Country.real.sample.iso2 }
-    gender "m"
+    gender { "m" }
     dob { Date.new(1966, 4, 4) }
   end
 end
