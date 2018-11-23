02700e6b2e58bffde4e5192bd9d9f367d2706598
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-11-23 01:24:27 +0100

    Add validations and WCIF interface to WcifExtension.

--- WcaOnRails/app/models/wcif_extension.rb
+++ WcaOnRails/app/models/wcif_extension.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
 class WcifExtension < ApplicationRecord
+  serialize :data, JSON
+
+  validates :extension_id, format: { with: /\A\w+(\.\w+)*\z/ }
+  validates :spec_url, url: true
+  validates :data, presence: true
+
+  def to_wcif
+    { "id" => self.extension_id, "specUrl" => self.spec_url, "data" => self.data }
+  end
+
+  def self.wcif_json_schema
+    {
+      "type" => ["object"],
+      "properties" => {
+        "id" => { "type" => "string" },
+        "specUrl" => { "type" => "string" },
+        "data" => { "type" => "object" },
+      },
+    }
+  end
 end
