12f88e44ff4d8f3f65d084e03362095f884b9f2a
Jeremy Fleischman <jeremyfleischman@gmail.com>
2019-01-16 21:22:57 -0800

    Revert "Stricter date parsing."
    This reverts commit b4607ccf041ac792572190980f479d9e59051c63.
    
    Jonatan points out that lots of clients (such as TNoodle
    https://github.com/thewca/tnoodle/blob/8152b1dd2a07647aa5287506c3551abff09973de/tnoodle-ui/src/WcaApi.js#L60)
    actually do send us datetime strings, and we don't want to break that.
    
    I'll merge this up immediately to avoid breaking anything, and then
    we can follow up with a better fix, because I would like us to be string
    about 2 character months and dates.

--- WcaOnRails/spec/models/user_spec.rb
+++ WcaOnRails/spec/models/user_spec.rb
@@ -369,7 +369,7 @@ RSpec.describe User, type: :model do
       FactoryBot.create(:user, unconfirmed_wca_id: person.wca_id,
                                delegate_id_to_handle_wca_id_claim: delegate.id,
                                claiming_wca_id: true,
-                               dob_verification: "1990-01-02")
+                               dob_verification: "1990-01-2")
     end
 
     let!(:person_without_dob) { FactoryBot.create :person, year: 0, month: 0, day: 0 }
@@ -502,7 +502,7 @@ RSpec.describe User, type: :model do
                                              unconfirmed_wca_id: person.wca_id,
                                              delegate_id_to_handle_wca_id_claim: delegate.id,
                                              claiming_wca_id: true,
-                                             dob_verification: "1990-01-02")
+                                             dob_verification: "1990-01-2")
         expect(WcaIdClaimMailer).to receive(:notify_user_of_delegate_demotion).with(user, delegate, senior_delegate).and_call_original
         expect(WcaIdClaimMailer).not_to receive(:notify_user_of_delegate_demotion).with(unconfirmed_user, delegate, senior_delegate).and_call_original
         delegate.update!(delegate_status: nil, senior_delegate_id: nil)
