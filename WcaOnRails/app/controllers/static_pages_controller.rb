47b19425d0a93fc21de390916985598833aadfa6
Matthew McMillan <mmcmillan@wpi.edu>
2019-03-24 17:52:24 -0400

    fix translation key issues and officer roles

--- WcaOnRails/app/controllers/static_pages_controller.rb
+++ WcaOnRails/app/controllers/static_pages_controller.rb
@@ -160,10 +160,14 @@ class StaticPagesController < ApplicationController
   end
 
   def teams_committees
-    @officers = [["2003BURT01", "Executive Director"],
-                 ["2007PERG01", "Chair"],
-                 ["2011FIOL01", "Secretary"]]
-    @officers += Team.wfc.current_members.select(&:team_leader).map { |leader| [leader.user.wca_id, "Treasurer"] }
+    # get all users who hold one or more officer positions
+    officer_users = Team.all_officers.map(&:current_members).inject(&:+).map(&:user)
+    @officers = []
+    officer_users.uniq.each do |user|
+      # for each officer, find all officer teams they belong to
+      positions = user.current_teams.filter{|team| Team.all_officers.include? team}
+      @officers.push([user, positions.map(&:name).join(", ")])
+    end
   end
 
   def wca_workbook_assistant
