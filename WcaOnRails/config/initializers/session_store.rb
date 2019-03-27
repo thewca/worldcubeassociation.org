dee9959c0aa04ca5f24232150b52f9d43c014544
Matthew McMillan <mmcmillan@wpi.edu>
2019-03-27 13:18:19 -0400

    use secure cookies for all cookies

--- WcaOnRails/config/initializers/session_store.rb
+++ WcaOnRails/config/initializers/session_store.rb
@@ -2,5 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_WcaOnRails_session',
-                                                      secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_WcaOnRails_session'
