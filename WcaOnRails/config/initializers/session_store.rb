27d30fd22083ecb4fa9aba3a8c76859f7a8e1dfb
Matthew McMillan <mmcmillan@wpi.edu>
2019-03-27 00:00:07 -0400

    add missing comma

--- WcaOnRails/config/initializers/session_store.rb
+++ WcaOnRails/config/initializers/session_store.rb
@@ -2,5 +2,5 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_WcaOnRails_session'
+Rails.application.config.session_store :cookie_store, key: '_WcaOnRails_session',
                                                       secure: Rails.env.production?
