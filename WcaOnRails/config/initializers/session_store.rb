48813662d00971cf937de83af0fad28823944682
Matthew McMillan <mmcmillan@wpi.edu>
2019-03-26 23:05:42 -0400

    add secure flag to cookies

--- WcaOnRails/config/initializers/session_store.rb
+++ WcaOnRails/config/initializers/session_store.rb
@@ -3,3 +3,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_WcaOnRails_session'
+                                                      secure: Rails.env.production?
