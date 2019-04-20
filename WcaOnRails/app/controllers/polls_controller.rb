2caf3b65b426146178d32c294c1bfd630ff1c74b
Marcelo <marjox7@hotmail.com>
2019-04-20 00:48:43 -0500

    Separating voting and viewing polls This fixes #3658

--- WcaOnRails/app/controllers/polls_controller.rb
+++ WcaOnRails/app/controllers/polls_controller.rb
@@ -3,7 +3,7 @@
 class PollsController < ApplicationController
   before_action :authenticate_user!
   before_action -> { redirect_to_root_unless_user(:can_create_poll?) }, only: [:new, :create, :update, :destroy]
-  before_action -> { redirect_to_root_unless_user(:can_vote_in_poll?) }, only: [:index, :vote, :results]
+  before_action -> { redirect_to_root_unless_user(:can_view_poll?) }, only: [:index, :results]
 
   def index
     @polls = current_user.can_create_poll? ? Poll.all : Poll.confirmed
