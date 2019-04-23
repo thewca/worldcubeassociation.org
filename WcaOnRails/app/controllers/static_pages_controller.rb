6281f482d09db4f08888e0f4ff163075d85c605c
Matthew McMillan <mmcmillan@wpi.edu>
2019-04-23 11:44:21 -0400

    replace two more filters with selects

--- WcaOnRails/app/controllers/static_pages_controller.rb
+++ WcaOnRails/app/controllers/static_pages_controller.rb
@@ -166,8 +166,8 @@ class StaticPagesController < ApplicationController
     @officers = []
     (officer_users + treasurers).uniq.each do |user|
       # for each officer, find all officer teams they belong to
-      positions = user.current_teams.filter { |team| Team.all_officers.include? team }.map(&:name)
-      if Team.wfc.current_members.filter(&:team_leader).map(&:user).include?(user)
+      positions = user.current_teams.select { |team| Team.all_officers.include? team }.map(&:name)
+      if Team.wfc.current_members.select(&:team_leader).map(&:user).include?(user)
         positions.push(t('about.structure.treasurer.name'))
       end
       @officers.push([user, positions.join("<br />").html_safe])
