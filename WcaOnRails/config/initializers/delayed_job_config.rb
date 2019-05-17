464804c4debee28a5f482a64bb72b1e3f5d5a68d
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-05-17 21:08:20 +0200

    Increase Delayed Job Worker timeout.

--- WcaOnRails/config/initializers/delayed_job_config.rb
+++ WcaOnRails/config/initializers/delayed_job_config.rb
@@ -3,7 +3,7 @@
 require 'delayed/plugins/save_completed_jobs'
 
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.max_run_time = 10.minutes
+Delayed::Worker.max_run_time = 15.minutes
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.plugins << Delayed::Plugins::SaveCompletedJobs
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
