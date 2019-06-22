53b8eb3fd353925a9124e6176be87cf15b1a38d9
AlbertoPdRF <albertopdrf.wca@gmail.com>
2019-06-22 10:07:54 +0200

    Add more columns to Regional Organizations.

--- WcaOnRails/app/models/regional_organization.rb
+++ WcaOnRails/app/models/regional_organization.rb
@@ -2,13 +2,22 @@
 
 class RegionalOrganization < ApplicationRecord
   has_one_attached :logo
+  has_one_attached :bylaws
+  has_one_attached :extra_file
 
   scope :currently_acknowledged, -> { where("end_date IS NULL OR end_date > ?", Date.today) }
   scope :not_currently_acknowledged, -> { where("end_date < ?", Date.today) }
 
-  validates_presence_of :name, :country
+  validates_presence_of :name, :country, :email, :address, :directors_and_officers, :area_description, :past_and_current_activities, :future_plans
   validates :website, presence: true, format: { with: %r{\Ahttps?://.*\z} }
-  validates :logo, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/svg'], size_range: 0..100.kilobytes }
+  validates :logo, presence: true, blob: { content_type: 'image/png', size_range: 0..200.kilobytes }
+  validates :bylaws, presence: true, blob: { content_type: 'application/pdf', size_range: 0..200.kilobytes }
+  validates :extra_file, blob: { content_type: 'application/pdf', size_range: 0..200.kilobytes }
+
+  validate :validate_email
+  def validate_email
+    errors.add(:email, I18n.t('common.errors.invalid')) unless ValidateEmail.valid?(email)
+  end
 
   validates :start_date, presence: true
   validate :start_date_must_be_earlier_than_end_date
