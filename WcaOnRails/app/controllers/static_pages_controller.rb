8646d1aacaaebb36e69fa39c833718973c34a2c1
AlbertoPdRF <albertopdrf.wca@gmail.com>
2019-07-01 22:46:41 +0200

    Add teams/committees page and redesign about page.
    The work on this commit has mostly been done by @UnsolvedCypher, I 
    (@AlbertoPdRF) just rebased everything, added support for Committee/Team 
    Senior Members, and commited.

--- WcaOnRails/app/controllers/static_pages_controller.rb
+++ WcaOnRails/app/controllers/static_pages_controller.rb
@@ -17,6 +17,21 @@ class StaticPagesController < ApplicationController
   def logo
   end
 
+  def teams_committees
+    # get all users who hold one or more officer positions
+    officer_users = Team.all_officers.map(&:current_members).inject(&:+).map(&:user)
+    treasurers = Team.wfc.current_members.select(&:team_leader).map(&:user)
+    @officers = []
+    (officer_users + treasurers).uniq.each do |user|
+      # for each officer, find all officer teams they belong to
+      positions = user.current_teams.select { |team| Team.all_officers.include? team }.map(&:name)
+      if Team.wfc.current_members.select(&:team_leader).map(&:user).include?(user)
+        positions.push(t('about.structure.treasurer.name'))
+      end
+      @officers.push([user, positions.join("<br />").html_safe])
+    end
+  end
+
   def wca_workbook_assistant
   end
 
