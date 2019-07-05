e7c5029b89eeb8608db83aa0021efca5a5e0421f
Philippe Virouleau <philippe.44@gmail.com>
2019-07-05 13:18:15 +0200

    Fix pdf CSS assets in production

--- WcaOnRails/config/initializers/assets.rb
+++ WcaOnRails/config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(oms.js)
 Rails.application.config.assets.precompile += %w(email.css)
+Rails.application.config.assets.precompile += %w(pdf.css)
