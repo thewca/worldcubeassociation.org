7cc2d3768b2d786453515fa6b3207a451b3f78be
Philippe Virouleau <philippe.44@gmail.com>
2019-07-28 10:51:10 +0200

    Fix InboxResult's personName

--- WcaOnRails/app/models/inbox_result.rb
+++ WcaOnRails/app/models/inbox_result.rb
@@ -8,6 +8,6 @@ class InboxResult < ApplicationRecord
   # NOTE: don't use this too often, as it triggers one person load per call!
   # If you need names for a batch of InboxResult, consider joining the InboxPerson table.
   def personName # rubocop:disable Naming/MethodName
-    InboxPerson.find_by(id: personId)&.name || "<personId=#{personId}>"
+    InboxPerson.find_by(id: personId, competitionId: competitionId)&.name || "<personId=#{personId}>"
   end
 end
