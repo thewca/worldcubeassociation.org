03252fd50fd3c462d8c1c08ff022552cda4b99d0
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-08-15 01:13:24 +0200

    Optimise and clean up results queries.

--- WcaOnRails/app/controllers/results_controller.rb
+++ WcaOnRails/app/controllers/results_controller.rb
@@ -24,7 +24,6 @@ class ResultsController < ApplicationController
 
     event = Event.c_find!(params[:event_id])
     event_condition = "AND eventId = '#{event.id}'"
-    event_condition_without_and = "eventId = '#{event.id}'"
 
     @is_by_region = params[:show] == "by region"
     splitted_show_param = params[:show].split
@@ -33,13 +32,12 @@ class ResultsController < ApplicationController
     @is_results = splitted_show_param[1] == "results"
     limit_condition = "LIMIT #{@show}"
 
-    id_string = @is_results ? ".id" : "Id"
     @continent = Continent.c_find(params[:region])
     @country = Country.c_find(params[:region])
     if @continent.present?
-      region_condition = "AND continentId = '#{@continent.id}'"
+      region_condition = "AND result.countryId IN (#{@continent.country_ids.map { |id| "'#{id}'" }.join(',')})"
     elsif @country.present?
-      region_condition = "AND country#{id_string} = '#{@country.id}'"
+      region_condition = "AND result.countryId = '#{@country.id}'"
     else
       region_condition = ""
     end
@@ -59,128 +57,92 @@ class ResultsController < ApplicationController
 
     if @is_persons
       query = <<-SQL
-        SELECT result.*,
-          result.#{value}       value,
-          competition.cellName  competitionName,
-          country.name          countryName
-        FROM
-          (SELECT MIN(valueAndId) valueAndId
-            FROM Concise#{capitalized_type_param}Results result
-            WHERE #{value} > 0
+        SELECT
+          result.*,
+          result.#{value} value
+        FROM (
+          SELECT MIN(valueAndId) valueAndId
+          FROM Concise#{capitalized_type_param}Results result
+          WHERE 1
             #{event_condition}
+            AND #{value} > 0
             #{years_condition}
             #{region_condition}
-            GROUP BY personId
-            ORDER BY valueAndId
-            #{limit_condition}) top,
-          Results      result,
-          Competitions competition,
-          Countries    country
-        WHERE
-          result.id          = valueAndId % 1000000000
-          AND competition.id = competitionId
-          AND country.id     = result.countryId
-        ORDER BY
-          value, personName
+          GROUP BY personId
+          ORDER BY valueAndId
+          #{limit_condition}
+        ) top
+        JOIN Results result ON result.id = valueAndId % 1000000000
+        ORDER BY value, personName
       SQL
     elsif @is_results
       if @is_average
-        query = <<-SQL
-          SELECT result.*,
-            result.#{value}      value,
-            competition.cellName competitionName,
-            country.name         countryName
-          FROM
-            Results      result,
-            Competitions competition,
-            Countries    country
-          WHERE #{value} > 0
+        subquery = <<-SQL
+          SELECT
+            result.*,
+            average value
+          FROM Results result
+          #{years_condition.present? ? "JOIN Competitions competition on competition.id = competitionId" : ""}
+          WHERE 1
             #{event_condition}
+            AND average > 0
             #{years_condition}
             #{region_condition}
-            AND competition.id = competitionId
-            AND country.id     = result.countryId
           ORDER BY
-            value, best, personName, competitionId, roundTypeId
+            average
           #{limit_condition}
         SQL
+        query = <<-SQL
+          SELECT *
+          FROM (#{subquery}) result
+          ORDER BY average, personName, competitionId, roundTypeId
+        SQL
       else
-        subqueries = []
-        (1..5).each do |i|
-          subqueries[i-1] = <<-SQL
+        subqueries = (1..5).map do |i|
+          <<-SQL
             SELECT
-              value#{i}            value,
-              personId,            personName,
-              country.id           countryId,
-              competitionId,
-              competition.cellName competitionName,
-              roundTypeId
-            FROM
-              Results      result,
-              Competitions competition,
-              Countries    country
-            WHERE value#{i} > 0
+              result.*,
+              value#{i} value
+            FROM Results result
+            #{years_condition.present? ? "JOIN Competitions competition on competition.id = competitionId" : ""}
+            WHERE 1
               #{event_condition}
+              AND value#{i} > 0
               #{years_condition}
               #{region_condition}
-              AND competition.id = competitionId
-              AND country.id     = result.countryId
-            ORDER BY
-              value, personName
+            ORDER BY value
             #{limit_condition}
           SQL
         end
-        subquery = ("(" + subqueries.join(") UNION ALL (") + ")")
-
+        subquery = "(" + subqueries.join(") UNION ALL (") + ")"
         query = <<-SQL
           SELECT *
-          FROM
-            (#{subquery}) result
-          ORDER BY
-            value, personName, competitionId, roundTypeId
+          FROM (#{subquery}) result
+          ORDER BY value, personName, competitionId, roundTypeId
           #{limit_condition}
         SQL
       end
     elsif @is_by_region
       query = <<-SQL
         SELECT
-          result.personId      personId,
-          result.personName    personName,
-          result.eventId       eventId,
-          result.formatId      formatId,
-          result.roundTypeId   roundTypeId,
-          country.id           countryId,
-          country.name         countryName,
-          continent.id         continentId,
-          continent.name       continentName,
-          competition.id       competitionId,
-          competition.cellName competitionName,
-          #{value}             value,
-          event.format         valueFormat,
-          value1, value2, value3, value4, value5
-        FROM
-          (SELECT countryId recordCountryId, MIN(#{value}) recordValue
-            FROM Concise#{capitalized_type_param}Results result
-            WHERE 1
+          result.*,
+          result.#{value} value
+        FROM (
+          SELECT
+            countryId recordCountryId,
+            MIN(#{value}) recordValue
+          FROM Concise#{capitalized_type_param}Results result
+          WHERE 1
             #{event_condition}
             #{years_condition}
-            GROUP BY countryId) record,
-          Results      result,
-          Countries    country,
-          Continents   continent,
-          Competitions competition,
-          Events       event
-        WHERE
-          #{event_condition_without_and}
+          GROUP BY countryId
+        ) record
+        JOIN Results result ON result.#{value} = recordValue AND result.countryId = recordCountryId
+        JOIN Competitions competition on competition.id = competitionId
+        WHERE 1
+          #{event_condition}
           #{years_condition}
-          AND result.#{value}  = recordValue
-          AND result.countryId = recordCountryId
-          AND country.id       = result.countryId
-          AND continent.id     = continentId
-          AND competition.id   = competitionId
-          AND event.id         = eventId
-        ORDER BY
-          value, countryId, year, month, day, personName
+        ORDER BY value, countryId, start_date, personName
       SQL
     else
       flash[:danger] = t(".unknown_show")
