c1043a253b626339fea545aaa2ada2abd48c8580
Philippe Virouleau <philippe.44@gmail.com>
2019-08-23 13:08:26 +0200

    Update WCA states (#4398)
    Update WCA states.

---
+++ WcaOnRails/db/migrate/20190803202212_update_wca_states.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class UpdateWcaStates < ActiveRecord::Migration[5.2]
+  def up
+    Country.delete_all
+    Country::ALL_STATES.each(&:save!)
+    # Extra changes due to some changes in country names
+    models=[Person, Result, Competition]
+    models.each do |m|
+      m.where(countryId: "Macedonia").update_all(countryId: "North Macedonia")
+    end
+    ActiveRecord::Base.connection.execute("update `archive_registrations` set `countryId` = 'North Macedonia' WHERE `archive_registrations`.`countryId` = 'Macedonia'")
+  end
+
+  def down
+    Country.delete_all
+    Country::ALL_STATES.each(&:save!)
+    models=[Person, Result, Competition]
+    models.each do |m|
+      m.where(countryId: "North Macedonia").update_all(countryId: "Macedonia")
+    end
+    ActiveRecord::Base.connection.execute("update `archive_registrations` set `countryId` = 'Macedonia' WHERE `archive_registrations`.`countryId` = 'North Macedonia'")
+  end
+end
