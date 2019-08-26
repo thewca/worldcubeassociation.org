f40ea9de73a4149d84b603c9f5a192a831639733
Jeremy Fleischman <jeremyfleischman@gmail.com>
2019-08-25 23:31:38 -0700

    Refactoring server status page to be more OOPy.

--- WcaOnRails/spec/controllers/server_status_controller_spec.rb
+++ WcaOnRails/spec/controllers/server_status_controller_spec.rb
@@ -3,87 +3,122 @@
 require 'rails_helper'
 
 RSpec.describe ServerStatusController, type: :controller do
-  before :each do
+  it "is happy" do
+    allow_any_instance_of(ServerStatusController).to receive(:checks).and_return([HappyCheck.new])
+
+    get :index
+
+    expect(response).to have_http_status 200
+  end
+
+  it "can fail" do
+    allow_any_instance_of(ServerStatusController).to receive(:checks).and_return([HappyCheck.new, UnhappyCheck.new])
+
+    get :index
+
+    expect(response).to have_http_status 503
+  end
+end
+
+class HappyCheck < StatusCheck
+  def _status_description
+    [:success, nil]
+  end
+end
+
+class UnhappyCheck < StatusCheck
+  def _status_description
+    [:danger, "uh oh"]
+  end
+end
+
+RSpec.describe "JobsCheck" do
+  let(:check) { JobsCheck.new }
+
+  it "passes if there are young jobs" do
+    _young_job = Delayed::Job.create(created_at: 1.minutes.ago, handler: "")
+
+    status, description = check.status_description
+
+    expect(status).to eq :success
+    expect(description).to be_nil
+  end
+
+  it "finds the oldest job that has been waiting to run" do
+    _old_job = Delayed::Job.create(created_at: 10.minutes.ago, handler: "")
+    oldest_job = Delayed::Job.create(created_at: 15.minutes.ago, handler: "")
+
+    status, description = check.status_description
+
+    expect(status).to eq :danger
+    expect(description).to match(/Job #{oldest_job.id} was/)
+  end
+
+  it "ignores jobs in progress" do
+    old_job = Delayed::Job.create(created_at: 10.minutes.ago, handler: "")
+    _old_but_running_job = Delayed::Job.create(created_at: 15.minutes.ago, handler: "", locked_at: Time.now)
+
+    status, description = check.status_description
+
+    expect(status).to eq :danger
+    expect(description).to match(/Job #{old_job.id} was/)
+  end
+end
+
+RSpec.describe "StripeChargesCheck" do
+  let(:check) { StripeChargesCheck.new }
+
+  it "passes" do
+    status, description = check.status_description
+    expect(status).to eq :success
+    expect(description).to be_nil
+  end
+
+  it "warns about stripe charges with status unknown" do
+    StripeCharge.create!(
+      metadata: "",
+      status: "unknown",
+    )
+
+    status, description = check.status_description
+
+    expect(status).to eq :danger
+    expect(description).to match("1 Stripe charge with status 'unknown'")
+  end
+end
+
+RSpec.describe "RegulationsCheck" do
+  let(:check) { RegulationsCheck.new }
+
+  it "passes" do
     allow(File).to receive(:read).with(any_args).and_call_original
     allow(File).to receive(:read).with(Regulation::REGULATIONS_JSON_PATH).and_return("{}")
     Regulation.reload_regulations
+
+    status, description = check.status_description
+    expect(status).to eq :success
+    expect(description).to be_nil
   end
 
-  it "is happy" do
-    get :index
+  it "warns about missing regulations" do
+    allow(File).to receive(:read).with(any_args).and_call_original
+    allow(File).to receive(:read).with(Regulation::REGULATIONS_JSON_PATH).and_raise(Errno::ENOENT.new)
+    Regulation.reload_regulations
 
-    expect(assigns(:everything_good)).to eq true
+    status, description = check.status_description
+
+    expect(status).to eq :danger
+    expect(description).to eq "Error while loading regulations: No such file or directory"
   end
 
-  context "jobs" do
-    it "finds the oldest job that has been waiting to run" do
-      _old_job = Delayed::Job.create(created_at: 10.minutes.ago, handler: "")
-      oldest_job = Delayed::Job.create(created_at: 15.minutes.ago, handler: "")
+  it "warns about malformed regulations" do
+    allow(File).to receive(:read).with(any_args).and_call_original
+    allow(File).to receive(:read).with(Regulation::REGULATIONS_JSON_PATH).and_return("i am definitely not json")
+    Regulation.reload_regulations
 
-      get :index
+    status, description = check.status_description
 
-      oldest_job_that_should_have_run_by_now = assigns(:oldest_job_that_should_have_run_by_now)
-      expect(oldest_job_that_should_have_run_by_now).to eq oldest_job
-
-      expect(assigns(:everything_good)).to eq false
-    end
-
-    it "ignores young jobs" do
-      _young_job = Delayed::Job.create(created_at: 1.minutes.ago, handler: "")
-
-      get :index
-
-      oldest_job_that_should_have_run_by_now = assigns(:oldest_job_that_should_have_run_by_now)
-      expect(oldest_job_that_should_have_run_by_now).to eq nil
-
-      expect(assigns(:everything_good)).to eq true
-    end
-
-    it "ignores jobs in progress" do
-      old_job = Delayed::Job.create(created_at: 10.minutes.ago, handler: "")
-      _oldest_but_running_job = Delayed::Job.create(created_at: 15.minutes.ago, handler: "", locked_at: Time.now)
-
-      get :index
-
-      oldest_job_that_should_have_run_by_now = assigns(:oldest_job_that_should_have_run_by_now)
-      expect(oldest_job_that_should_have_run_by_now).to eq old_job
-
-      expect(assigns(:everything_good)).to eq false
-    end
-  end
-
-  context "regulations" do
-    it "warns about missing regulations" do
-      allow(File).to receive(:read).with(any_args).and_call_original
-      allow(File).to receive(:read).with(Regulation::REGULATIONS_JSON_PATH).and_raise(Errno::ENOENT.new)
-      Regulation.reload_regulations
-
-      get :index
-
-      expect(assigns(:everything_good)).to eq false
-    end
-
-    it "warns about malformed regulations" do
-      allow(File).to receive(:read).with(any_args).and_call_original
-      allow(File).to receive(:read).with(Regulation::REGULATIONS_JSON_PATH).and_return("i am definitely not json")
-      Regulation.reload_regulations
-
-      get :index
-
-      expect(assigns(:everything_good)).to eq false
-    end
-  end
-
-  context "stripe charges" do
-    it "warns about stripe charges with status unknown" do
-      StripeCharge.create!(
-        metadata: "",
-        status: "unknown",
-      )
-
-      get :index
-
-      expect(assigns(:everything_good)).to eq false
-    end
+    expect(status).to eq :danger
+    expect(description).to eq "Error while loading regulations: 785: unexpected token at 'i am definitely not json'"
   end
 end
