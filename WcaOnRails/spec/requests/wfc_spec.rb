1c43b3ab296ac7dfd81abb97a96284987c783475
Philippe Virouleau <philippe.44@gmail.com>
2019-10-13 22:34:02 +0200

    Spec for csv export

--- WcaOnRails/spec/requests/wfc_spec.rb
+++ WcaOnRails/spec/requests/wfc_spec.rb
@@ -28,4 +28,22 @@ RSpec.describe "WFC controller" do
       end
     end
   end
+  describe "GET /competitions_export" do
+    context "when signed in as a regular user" do
+      sign_in { FactoryBot.create :user }
+      it "redirect to root" do
+        get wfc_competitions_export_path(from_date: Time.now, to_date: Time.now)
+        expect(response).to redirect_to root_url
+      end
+    end
+
+    context "when signed in as a WFC member" do
+      sign_in { FactoryBot.create :user, :wfc_member }
+      it "shows the page" do
+        get wfc_competitions_export_path(from_date: Time.now, to_date: Time.now)
+        expect(response).to be_successful
+        # Don't bother checking the actual data here, the WFC will complain if it's broken and may want to change it overtime.
+      end
+    end
+  end
 end
