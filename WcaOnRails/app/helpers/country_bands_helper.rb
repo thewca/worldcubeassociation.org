bc5cf0def2d6dee920ca07eb7cde8532940106bb
Philippe Virouleau <philippe.44@gmail.com>
2019-10-19 11:00:58 +0200

    Add filter on continent

---
+++ WcaOnRails/app/helpers/country_bands_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module CountryBandsHelper
+  def continent_id_as_class(id)
+    id.tr(" ", "")
+  end
+
+  def continent_options
+    [["None", ""]].concat(Continent.real.map { |c| [c.name, continent_id_as_class(c.id)] })
+  end
+end
