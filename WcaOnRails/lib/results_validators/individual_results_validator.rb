504abfb7dcb2d50dceeb5ee0728af2ebe13114e3
Sébastien Auroux <sebastien.auroux.sa@gmail.com>
2019-11-08 18:06:28 +0100

    Fixed line length.

--- WcaOnRails/lib/results_validators/individual_results_validator.rb
+++ WcaOnRails/lib/results_validators/individual_results_validator.rb
@@ -13,7 +13,8 @@ module ResultsValidators
     RESULT_OVER_TIME_LIMIT_ERROR = "[%{round_id}] At least one result for %{person_name} is over the time limit which is %{time_limit} for one solve. All solves over the time limit must be changed to DNF."
     RESULTS_OVER_CUMULATIVE_TIME_LIMIT_ERROR = "[%{round_ids}] The sum of results for %{person_name} is over the cumulative time limit which is %{time_limit}."
     NO_ROUND_INFORMATION_WARNING = "[%{round_id}] Could not find information about cutoff and timelimit for this round, these validations have been skipped."
-    SUSPICIOUS_DNF_WARNING = "[%{round_ids}] The round has a cumulative time limit and %{person_name} has at least one suspicious DNF result. Please let us know if the participant really started the DNF attempts irrespective of little time left. If the participant did not start one of the attempts because of too little time left, the result should be DNS instead."
+    SUSPICIOUS_DNF_WARNING = "[%{round_ids}] The round has a cumulative time limit and %{person_name} has at least one suspicious DNF result. Please let us know if the participant really started the DNF"\
+    " attempts irrespective of little time left. If the participant did not start one of the attempts because of too little time left, the result should be DNS instead."
 
     # Miscelaneous errors
     MISSING_CUMULATIVE_ROUND_ID_ERROR = "[%{original_round_id}] Unable to find the round \"%{wcif_id}\" for the cumulative time limit specified in the WCIF."\
