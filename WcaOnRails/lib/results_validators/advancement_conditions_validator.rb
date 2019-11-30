00fa7d435300590b43df2fb9fed9d364abd339d7
tussosedan <32999689+tussosedan@users.noreply.github.com>
2019-11-30 10:10:16 -0800

    Extract AdvancementConditionsValidator and add tests

---
+++ WcaOnRails/lib/results_validators/advancement_conditions_validator.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+module ResultsValidators
+  class AdvancementConditionsValidator < GenericValidator
+    REGULATION_9M_ERROR = "Event %{event_id} has more than four rounds, which must not happen per Regulation 9m."
+    REGULATION_9M1_ERROR = "Round %{round_id} has 99 competitors or less but has at least three subsequents rounds, which must not happen per Regulation 9m1."
+    REGULATION_9M2_ERROR = "Round %{round_id} has 15 competitors or less but has at least two subsequents rounds, which must not happen per Regulation 9m2."
+    REGULATION_9M3_ERROR = "Round %{round_id} has 7 competitors or less but has at least one subsequent round, which must not happen per Regulation 9m3."
+    REGULATION_9P1_ERROR = "Round %{round_id}: there was not 25%% of competitors eliminated, which is needed per Regulation 9p1."
+    OLD_REGULATION_9P_ERROR = "Round %{round_id}: there must be at least one competitor eliminated, which is required per Regulation 9p (competitions before April 2010)."
+
+    @@desc = "This validator checks that advancement between rounds is correct according to the regulations."
+
+    def validate(competition_ids: [], model: Result, results: nil)
+      reset_state
+      # Get all results if not provided
+      results ||= model.sorted_for_competitions(competition_ids)
+
+      results_by_competition_id = results.group_by(&:competitionId)
+
+      competitions_start_dates = Hash[
+        Competition.where(id: results_by_competition_id.keys).select(:id, :start_date).map do |c|
+          [c.id, c.start_date]
+        end
+      ]
+
+      results_by_competition_id.each do |competition_id, results_for_comp|
+        comp_start_date = competitions_start_dates[competition_id]
+        results_by_event_id = results_for_comp.group_by(&:eventId)
+        results_by_event_id.each do |event_id, results_for_event|
+          results_by_event_id[event_id] = results_for_event.group_by(&:roundTypeId)
+        end
+        ordered_round_type_ids = RoundType.order(:rank).all.map(&:id)
+        results_by_event_id.each do |event_id, results_by_round_type_id|
+          remaining_number_of_rounds = results_by_round_type_id.keys.size
+          if remaining_number_of_rounds > 4
+            # https://www.worldcubeassociation.org/regulations/#9m: Events must have at most four rounds.
+            # Should not happen as we already have a validation to create rounds, but who knows...
+            @errors << ValidationError.new(:rounds, competition_id,
+                                           REGULATION_9M_ERROR,
+                                           event_id: event_id)
+          end
+          number_of_people_in_previous_round = nil
+          (ordered_round_type_ids & results_by_round_type_id.keys).each do |round_type_id|
+            remaining_number_of_rounds -= 1
+            number_of_people_in_round = results_by_round_type_id[round_type_id].size
+            round_id = "#{event_id}-#{round_type_id}"
+            if number_of_people_in_round <= 7 && remaining_number_of_rounds > 0
+              # https://www.worldcubeassociation.org/regulations/#9m3: Rounds with 7 or fewer competitors must not have subsequent rounds.
+              @errors << ValidationError.new(:rounds, competition_id,
+                                             REGULATION_9M3_ERROR,
+                                             round_id: round_id)
+            end
+            if number_of_people_in_round <= 15 && remaining_number_of_rounds > 1
+              # https://www.worldcubeassociation.org/regulations/#9m2: Rounds with 15 or fewer competitors must have at most one subsequent round.
+              @errors << ValidationError.new(:rounds, competition_id,
+                                             REGULATION_9M2_ERROR,
+                                             round_id: round_id)
+            end
+            if number_of_people_in_round <= 99 && remaining_number_of_rounds > 2
+              # https://www.worldcubeassociation.org/regulations/#9m1: Rounds with 99 or fewer competitors must have at most one subsequent round.
+              @errors << ValidationError.new(:rounds, competition_id,
+                                             REGULATION_9M1_ERROR,
+                                             round_id: round_id)
+            end
+
+            # Check for the number of qualified competitors (only if we are not
+            # in a first round).
+            if number_of_people_in_previous_round
+              # Article 9p, since July 20, 2006 until April 13, 2010
+              if Date.new(2006, 7, 20) <= comp_start_date &&
+                 comp_start_date <= Date.new(2010, 4, 13)
+                if number_of_people_in_round >= number_of_people_in_previous_round
+                  @errors << ValidationError.new(:rounds, competition_id,
+                                                 OLD_REGULATION_9P_ERROR,
+                                                 round_id: round_id)
+                end
+              else
+                # Article 9p1, since April 14, 2010
+                # https://www.worldcubeassociation.org/regulations/#9p1: At least 25% of competitors must be eliminated between consecutive rounds of the same event.
+                if number_of_people_in_round > 3 * number_of_people_in_previous_round / 4
+                  @errors << ValidationError.new(:rounds, competition_id,
+                                                 REGULATION_9P1_ERROR,
+                                                 round_id: round_id)
+                end
+              end
+            end
+            number_of_people_in_previous_round = number_of_people_in_round
+          end
+        end
+      end
+      self
+    end
+  end
+end
---
+++ WcaOnRails/spec/lib/results_validators/advancement_conditions_validator_spec.rb
@@ -0,0 +1,119 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RV = ResultsValidators
+ACV = RV::AdvancementConditionsValidator
+
+RSpec.describe ACV do
+  context "on InboxResult and Result" do
+    let!(:competition1) { FactoryBot.create(:competition, starts: Date.new(2010, 3, 1), event_ids: ["333oh"]) }
+    let!(:competition2) { FactoryBot.create(:competition, :past, event_ids: ["222"]) }
+
+    # The idea behind this variable is the following: the validator can be applied
+    # on either a particular model for given competition ids, or on a set of results.
+    # We simply want to check it has the expected behavior on all the possible cases.
+    let(:validator_args) {
+      [InboxResult, Result].flat_map { |model|
+        [
+          { competition_ids: [competition1.id, competition2.id], model: model },
+          { results: model.sorted_for_competitions([competition1.id, competition2.id]), model: model },
+        ]
+      }
+    }
+
+    it "doesn't complain when it's fine" do
+      [Result, InboxResult].each do |model|
+        result_kind = model.model_name.singular.to_sym
+        # Using a single fake person for all the results for better performance.
+        fake_person = build_person(result_kind, competition1)
+        # Collecting all the results and using bulk import for better performance.
+        results = []
+        results += FactoryBot.build_list(result_kind, 100, competition: competition1, eventId: "333oh", roundTypeId: "1", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 16, competition: competition1, eventId: "333oh", roundTypeId: "2", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 8, competition: competition1, eventId: "333oh", roundTypeId: "3", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 7, competition: competition1, eventId: "333oh", roundTypeId: "f", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 8, competition: competition2, eventId: "222", roundTypeId: "1", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 5, competition: competition2, eventId: "222", roundTypeId: "2", person: fake_person)
+        model.import(results)
+      end
+
+      validator_args.each do |arg|
+        acv = ACV.new.validate(arg)
+        expect(acv.warnings).to be_empty
+        expect(acv.errors).to be_empty
+      end
+    end
+
+    # Triggers:
+    # REGULATION_9M_ERROR
+    # REGULATION_9M1_ERROR
+    # REGULATION_9M2_ERROR
+    # REGULATION_9M3_ERROR
+    # REGULATION_9P1_ERROR
+    # OLD_REGULATION_9P_ERROR
+    it "complains when it should" do
+      [Result, InboxResult].each do |model|
+        result_kind = model.model_name.singular.to_sym
+        # Using a single fake person for all the results for better performance.
+        fake_person = build_person(result_kind, competition1)
+        # Collecting all the results and using bulk import for better performance.
+        results = []
+        results += FactoryBot.build_list(result_kind, 99, competition: competition1, eventId: "333oh", roundTypeId: "1", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 15, competition: competition1, eventId: "333oh", roundTypeId: "2", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 7, competition: competition1, eventId: "333oh", roundTypeId: "3", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 4, competition: competition1, eventId: "333oh", roundTypeId: "c", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 4, competition: competition1, eventId: "333oh", roundTypeId: "f", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 8, competition: competition2, eventId: "222", roundTypeId: "1", person: fake_person)
+        results += FactoryBot.build_list(result_kind, 7, competition: competition2, eventId: "222", roundTypeId: "2", person: fake_person)
+        model.import(results)
+      end
+
+      expected_errors = [
+        RV::ValidationError.new(:rounds, competition1.id,
+                                ACV::REGULATION_9M1_ERROR,
+                                round_id: "333oh-1"),
+        RV::ValidationError.new(:rounds, competition1.id,
+                                ACV::REGULATION_9M1_ERROR,
+                                round_id: "333oh-2"),
+        RV::ValidationError.new(:rounds, competition1.id,
+                                ACV::REGULATION_9M2_ERROR,
+                                round_id: "333oh-2"),
+        RV::ValidationError.new(:rounds, competition1.id,
+                                ACV::REGULATION_9M2_ERROR,
+                                round_id: "333oh-3"),
+        RV::ValidationError.new(:rounds, competition1.id,
+                                ACV::REGULATION_9M3_ERROR,
+                                round_id: "333oh-3"),
+        RV::ValidationError.new(:rounds, competition1.id,
+                                ACV::REGULATION_9M3_ERROR,
+                                round_id: "333oh-c"),
+        RV::ValidationError.new(:rounds, competition1.id,
+                                ACV::REGULATION_9M_ERROR,
+                                event_id: "333oh"),
+        RV::ValidationError.new(:rounds, competition1.id,
+                                ACV::OLD_REGULATION_9P_ERROR,
+                                round_id: "333oh-f"),
+        RV::ValidationError.new(:rounds, competition2.id,
+                                ACV::REGULATION_9P1_ERROR,
+                                round_id: "222-2"),
+      ]
+
+      validator_args.each do |arg|
+        acv = ACV.new.validate(arg)
+        expect(acv.warnings).to be_empty
+        expect(acv.errors).to match_array(expected_errors)
+      end
+    end
+  end
+
+  private
+
+  def build_person(result_kind, competition)
+    if result_kind == :result
+      FactoryBot.build(:person)
+    else
+      FactoryBot.build(:inbox_person, competitionId: competition.id)
+    end
+  end
+end
