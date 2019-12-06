e28387a21364419668b7714232b6bc5211917dd1
Philippe Virouleau <philippe.44@gmail.com>
2019-12-06 20:02:02 +0100

    Handle reviews

--- WcaOnRails/spec/lib/results_validators/events_rounds_validator_spec.rb
+++ WcaOnRails/spec/lib/results_validators/events_rounds_validator_spec.rb
@@ -7,7 +7,7 @@ ERV = RV::EventsRoundsValidator
 
 RSpec.describe ERV do
   context "on InboxResult and Result" do
-    let!(:competition1) { FactoryBot.create(:competition, :past, event_ids: ["333oh"]) }
+    let!(:competition1) { FactoryBot.create(:competition, :past, event_ids: ["333", "333oh"]) }
     let!(:competition2) { FactoryBot.create(:competition, :past, event_ids: ["222", "555"]) }
 
     # The idea behind this variable is the following: the validator can be applied
@@ -27,6 +27,7 @@ RSpec.describe ERV do
       # CHOOSE_MAIN_EVENT_WARNING
       # UNEXPECTED_RESULTS_ERROR
       # MISSING_RESULTS_WARNING
+      # UNEXPECTED_COMBINED_ROUND_ERROR
       [Result, InboxResult].each do |model|
         result_kind = model.model_name.singular.to_sym
         FactoryBot.create(result_kind, competition: competition1, eventId: "333oh")
@@ -42,6 +43,9 @@ RSpec.describe ERV do
         RV::ValidationWarning.new(:events, competition2.id,
                                   ERV::MISSING_RESULTS_WARNING,
                                   event_id: "555"),
+        RV::ValidationWarning.new(:events, competition1.id,
+                                  ERV::MISSING_RESULTS_WARNING,
+                                  event_id: "333"),
       ]
 
       expected_errors = [
@@ -63,22 +67,27 @@ RSpec.describe ERV do
       # UNEXPECTED_ROUND_RESULTS_ERROR
       # MISSING_ROUND_RESULTS_ERROR
       cutoff = Cutoff.new(number_of_attempts: 2, attempt_result: 50*100)
-      # Add some rounds to trigger the rounds validation
+      # Add some rounds to trigger the rounds validation.
       FactoryBot.create(:round,
                         competition: competition1, event_id: "333oh",
                         total_number_of_rounds: 2)
-      # This round is added to trigger the missing round error
+      # This round is added to trigger the missing round error.
       FactoryBot.create(:round,
                         competition: competition1, event_id: "333oh",
                         number: 2, total_number_of_rounds: 2)
-      FactoryBot.create(:round, competition: competition2, event_id: "555")
+      FactoryBot.create(:round, competition: competition1, event_id: "333")
+      FactoryBot.create(:round, competition: competition2, event_id: "555",
+                                cutoff: cutoff)
 
       [Result, InboxResult].each do |model|
         result_kind = model.model_name.singular.to_sym
-        # Create a result over a cutoff which does not exist in rounds data
+        # Create a result over a cutoff which does not exist in rounds data.
         FactoryBot.create(result_kind, :over_cutoff,
                           competition: competition1, eventId: "333oh",
                           cutoff: cutoff)
+        FactoryBot.create(result_kind, competition: competition1, eventId: "333")
+        # This creates results below the cutoff for 5x5, which effectively turns
+        # it into a "regular" round instead of a combined round.
         FactoryBot.create(result_kind, competition: competition2, eventId: "555")
         FactoryBot.create(result_kind,
                           competition: competition2, eventId: "222", roundTypeId: "c")
@@ -96,8 +105,6 @@ RSpec.describe ERV do
       ]
 
       expected_warnings = [
-        RV::ValidationWarning.new(:events, competition1.id,
-                                  ERV::CHOOSE_MAIN_EVENT_WARNING),
         RV::ValidationWarning.new(:events, competition2.id,
                                   ERV::CHOOSE_MAIN_EVENT_WARNING),
       ]
