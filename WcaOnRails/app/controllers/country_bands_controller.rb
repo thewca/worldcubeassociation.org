f3978bad2097a8f1ac066e9a15ad0bf31bd8bd0a
Philippe Virouleau <philippe.44@gmail.com>
2019-12-16 20:44:37 +0100

    Reviews

--- WcaOnRails/app/controllers/country_bands_controller.rb
+++ WcaOnRails/app/controllers/country_bands_controller.rb
@@ -21,25 +21,27 @@ class CountryBandsController < ApplicationController
     @number = id_from_params
     iso2s = params.require(:countries).require(:iso2s)
     has_anything_changed = false
-    previously_in_band = CountryBand.where(number: @number).map(&:iso2)
+    previously_in_band = CountryBand.where(number: @number).pluck(:iso2)
     begin
-      iso2s.split(",").each do |iso2|
-        cb = CountryBand.find_or_initialize_by(iso2: iso2)
-        cb.number = @number
-        has_anything_changed ||= cb.changed?
-        cb.save!
-        previously_in_band.delete(iso2)
+      ActiveRecord::Base.transaction do
+        iso2s.split(",").each do |iso2|
+          cb = CountryBand.find_or_initialize_by(iso2: iso2)
+          cb.number = @number
+          has_anything_changed ||= cb.changed?
+          cb.save!
+          previously_in_band.delete(iso2)
+        end
+        if previously_in_band.any?
+          # Clean up removed countries
+          CountryBand.where(iso2: previously_in_band).delete_all
+          has_anything_changed = true
+        end
+        flash[:success] = if has_anything_changed
+                            "Successfully updated band data."
+                          else
+                            "No change to band data."
+                          end
       end
-      if previously_in_band.any?
-        # Clean up removed countries
-        CountryBand.where(iso2: previously_in_band).delete_all
-        has_anything_changed = true
-      end
-      flash[:success] = if has_anything_changed
-                          "Successfully updated band data."
-                        else
-                          "No change to band data."
-                        end
     rescue ActiveRecord::RecordInvalid => e
       flash[:danger] = "Couldn't save all countries, here is the error: #{e.message}."
     end
@@ -52,13 +54,14 @@ class CountryBandsController < ApplicationController
   def id_from_params
     Integer(params.require(:id))
   rescue ArgumentError
-    false
+    nil
   end
 
   def set_instance_variables
     @in_band = CountryBand.where(number: @number).map(&:country).sort_by(&:name).map(&:iso2)
-    # We do want to include countries for our band here, as they are filtered
-    # by selectize.
+    # The list of unused countries should contain both unused countries and countries for the selected band.
+    # Therefore we can get the list of relevant countries by substracting all the
+    # other bands countries to the overall list of countries.
     used_countries = CountryBand.where.not(number: @number).map(&:country)
     @unused = (Country.real - used_countries).map do |c|
       { name: c.name, iso2: c.iso2 }
