a4ff184b3317af0cbdb2228ce6aebc855e664550
alexmaasswca <48773372+alexmaasswca@users.noreply.github.com>
2020-01-25 12:51:19 -0500

    Update 20200123042042_markfeetasunofficial.rb
    added up and down

--- WcaOnRails/db/migrate/20200123042042_markfeetasunofficial.rb
+++ WcaOnRails/db/migrate/20200123042042_markfeetasunofficial.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class MarkFeetAsUnofficial < ActiveRecord::Migration[5.2]
-  def change
+  def up
     Event.find("333ft").update!(rank: 996)
+    end
+  end
+  def down
+    Event.find("333ft").update!(rank: 100)
   end
 end
