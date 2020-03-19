adf847afe31ec5f54ea9940d268041806a55c2ef
Iván Brigidano Pérez <ivan.brigidano.perez@gmail.com>
2020-03-19 23:09:56 +0100

    Delete unnecesary 's' and run the migration

--- WcaOnRails/db/migrate/20200319193625_rename_combined_rounds_to_cutoff_rounds.rb
+++ WcaOnRails/db/migrate/20200319193625_rename_combined_rounds_to_cutoff_rounds.rb
@@ -3,7 +3,7 @@
 class RenameCombinedRoundsToCutoffRounds < ActiveRecord::Migration[5.2]
   def change
     # This change goes along renaming Combined Rounds to Cutoff Rounds in the seeds file to comply with 2020 WCA Regulations.
-    RoundTypes.delete_all
+    RoundType.delete_all
     load "#{Rails.root}/db/seeds/round_types.seeds.rb"
   end
 end
