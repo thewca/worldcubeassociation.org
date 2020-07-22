740ed8404d5fbb66caecd6f245448e8ad930c68c
Saransh Grover <ycubiksrube@gmail.com>
2020-07-22 03:37:44 -0400

    Use parameters if specified

--- WcaOnRails/app/webpacker/javascript/competition_results/index.js
+++ WcaOnRails/app/webpacker/javascript/competition_results/index.js
@@ -1,76 +1,72 @@
-import React from "react";
-import useLoadedData from "../requests/loadable";
-import { registerComponent } from "../wca/react-utils";
-import Loading from "../requests/Loading";
-import Errored from "../requests/Errored";
+import React, { useState, useEffect } from 'react';
+import classnames from 'classnames';
+import { Table, Popup } from 'semantic-ui-react';
+import useLoadedData from '../requests/loadable';
+import { registerComponent } from '../wca/react-utils';
+import Loading from '../requests/Loading';
+import Errored from '../requests/Errored';
 import { formatAttemptResult } from '../wca-live/attempts';
-import { useState } from "react";
-import classnames from "classnames";
-import "./index.scss";
-import { Table, Popup } from "semantic-ui-react";
+
 import EventIcon from '../wca/EventIcon';
 import CountryFlag from '../wca/CountryFlag';
+import events from '../wca/events.js.erb';
+import './index.scss';
 
-const CompetitionResultsNavigation = ({ events, selected, onSelect }) => {
-  return (
-    <div className={"events-list"}>
-      {events.map((event, index) => (
-        <Popup
-          key={event}
-          content={`${event}`}
-          trigger={
-            <EventIcon
-              key={event}
-              id={event}
-              onClick={() => onSelect(index)}
-              className={classnames(selected === index && "selected")}
-            />
-          }
-          inverted={true}
-        />
-      ))}
-    </div>
-  );
-};
+const CompetitionResultsNavigation = ({ eventIds, selected, onSelect }) => (
+  <div className="events-list">
+    {eventIds.map((event, index) => (
+      <Popup
+        key={event}
+        content={events.byId[event].name}
+        trigger={(
+          <EventIcon
+            key={event}
+            id={event}
+            onClick={() => onSelect(index)}
+            className={classnames(selected === index && 'selected')}
+          />
+          )}
+        inverted
+      />
+    ))}
+  </div>
+);
 
-const RoundResultsTable = ({ round, eventName, eventId }) => {
-  return (
-    <>
-      <h2>{`${eventName} ${round.name}`}</h2>
-
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>#</Table.HeaderCell>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Best</Table.HeaderCell>
-            <Table.HeaderCell> Average</Table.HeaderCell>
-            <Table.HeaderCell>Citizen Of</Table.HeaderCell>
-            <Table.HeaderCell>Solves</Table.HeaderCell>
+const RoundResultsTable = ({ round, eventName, eventId }) => (
+  <>
+    <h2>{`${eventName} ${round.name}`}</h2>
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>#</Table.HeaderCell>
+          <Table.HeaderCell>Name</Table.HeaderCell>
+          <Table.HeaderCell>Best</Table.HeaderCell>
+          <Table.HeaderCell>Average</Table.HeaderCell>
+          <Table.HeaderCell>Citizen Of</Table.HeaderCell>
+          <Table.HeaderCell>Solves</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {round.results.map((result) => (
+          <Table.Row key={result.id}>
+            <Table.Cell>{result.pos}</Table.Cell>
+            <Table.Cell>
+              <a href={`/persons/${result.wca_id}`}>{`${result.name}`}</a>
+            </Table.Cell>
+            <Table.Cell>{formatAttemptResult(result.best, eventId)}</Table.Cell>
+            <Table.Cell>{formatAttemptResult(result.average, eventId, true)}</Table.Cell>
+            <Table.Cell><CountryFlag iso2={result.country_iso2} /></Table.Cell>
+            <Table.Cell className="table-cell-solves">{result.attempts.map((a) => formatAttemptResult(a, eventId)).join(' ')}</Table.Cell>
           </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {round.results.map((result) => (
-            <Table.Row key={result.id}>
-              <Table.Cell>{result.pos}</Table.Cell>
-              <Table.Cell>
-                <a href={`/persons/${result.wca_id}`}>{`${result.name}`}</a>
-              </Table.Cell>
-              <Table.Cell>{formatAttemptResult(result.best, eventId)}</Table.Cell>
-              <Table.Cell>{formatAttemptResult(result.average, eventId, true)}</Table.Cell>
-              <Table.Cell><CountryFlag iso2={result.country_iso2} /></Table.Cell>
-              <Table.Cell>{result.attempts.map(a => formatAttemptResult(a, eventId)).join("\t\t")}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    </>
-  );
-};
+        ))}
+      </Table.Body>
+    </Table>
+  </>
+);
 
 const EventResults = ({ competitionId, eventId }) => {
   const { loading, error, data } = useLoadedData(
-    `/api/v0/competitions/${competitionId}/results/${eventId}`
+    `/api/v0/competitions/${competitionId}/results/${eventId}`,
   );
 
   if (loading) return <Loading />;
@@ -86,15 +82,23 @@ const EventResults = ({ competitionId, eventId }) => {
 
 const CompetitionResults = ({ competitionId }) => {
   const { loading, error, data } = useLoadedData(
-    `/api/v0/competitions/${competitionId}/`
+    `/api/v0/competitions/${competitionId}/`,
   );
-  const [selectedEvent, setSelectedEvent] = useState(0);
-  if (loading) return <Loading />;
+  const [selectedEvent, setSelectedEvent] = useState();
+  const params = new URLSearchParams(window.location.search);
+  useEffect(() => {
+    if (data) {
+      const eventParam = params.get('event');
+      const index = eventParam ? data.event_ids.indexOf(eventParam) : 0;
+      setSelectedEvent(index);
+    }
+  }, [data]);
+  if (loading || selectedEvent === undefined) return <Loading />;
   if (error) return <Errored />;
   return (
     <div className="competition-results">
       <CompetitionResultsNavigation
-        events={data.event_ids}
+        eventIds={data.event_ids}
         selected={selectedEvent}
         onSelect={(eventIndex) => setSelectedEvent(eventIndex)}
       />
@@ -105,4 +109,4 @@ const CompetitionResults = ({ competitionId }) => {
     </div>
   );
 };
-registerComponent(CompetitionResults, "CompetitionResults");
+registerComponent(CompetitionResults, 'CompetitionResults');
