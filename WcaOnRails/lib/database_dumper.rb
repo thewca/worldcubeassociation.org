9c30eb323ed0a77f02aabd4b0baa3bff3838c5b9
Jambrose777 <Jaa74458@uga.edu>
2021-01-04 16:52:38 -0500

    added new tables to database dump definition

--- WcaOnRails/lib/database_dumper.rb
+++ WcaOnRails/lib/database_dumper.rb
@@ -579,6 +579,7 @@ module DatabaseDumper
       ),
     }.freeze,
     "sanity_checks" => :skip_all_rows,
+    "sanity_check_categories" => :skip_all_rows,
     "sanity_check_exclusions" => :skip_all_rows,
     "schema_migrations" => :skip_all_rows, # This is populated when loading our schema dump
     "team_members" => {
