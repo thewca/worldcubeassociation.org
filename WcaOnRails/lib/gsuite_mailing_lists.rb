878fd50d74c9a4b5db9e0101a41554fb17c1f426
Gregor Billing <gbilling@worldcubeassociation.org>
2021-06-04 13:35:03 +0200

    Filter out Board aliases when they are on double-duty as Senior

--- WcaOnRails/lib/gsuite_mailing_lists.rb
+++ WcaOnRails/lib/gsuite_mailing_lists.rb
@@ -7,6 +7,11 @@ require 'googleauth/stores/file_token_store'
 require 'google/apis/admin_directory_v1'
 
 module GsuiteMailingLists
+  # The Board may be required to act as Senior for multiple regions. Unfortunately, this clashes with assumptions
+  # about one Delegate only leading one region in the `users` SQL table. So we realise this feature by hijacking aliases
+  # but Google complains when trying to add multiple aliases to seniors@ or delegates@.
+  BOARD_PRIMARY_EMAIL = "board@worldcubeassociation.org"
+
   def self.sync_group(group, desired_emails)
     service = get_service
 
@@ -17,6 +22,25 @@ module GsuiteMailingLists
         puts "Warning: '#{old_email}' contains a plus sign, and google groups seems to not support + signs in email addresses, so we're going to add '#{email}' instead."
       end
       email
+    end
+
+    # Aliases should only be removed if the primary address is in use as well. This works because the "list_members"
+    # invocation from Google only returns non-alias mail addresses. In cases where the "hijacked" alias Senior
+    # should be part of their own regional sub-list, we don't need to remove him if the root address is not included.
+    if desired_emails.include?(BOARD_PRIMARY_EMAIL)
+      board_aliases = service.list_group_aliases(BOARD_PRIMARY_EMAIL)
+
+      # This is using a Hash key access because Google is not deserializing their own responses properly
+      # and doing it for them only for the sake of retrieving one key seems overkill.
+      #
+      # see https://github.com/googleapis/google-api-ruby-client/blob/google-api-client/v0.53.0/generated/google-apis-admin_directory_v1/lib/google/apis/admin_directory_v1/representations.rb#L555
+      board_aliases = board_aliases.aliases.map { |a| a['alias'] }
+
+      contained_aliases = desired_emails & board_aliases
+      unless contained_aliases.empty?
+        desired_emails -= contained_aliases
+        puts "Warning: Board aliases are contained in the sync group. #{contained_aliases} have been removed."
+      end
     end
 
     members = service.fetch_all(items: :members) do |token|
