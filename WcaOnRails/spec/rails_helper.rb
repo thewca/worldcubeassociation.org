f6892a4032c914ba61ff778bbdb262d8253e309b
notrichardpeng <notrichardpeng@gmail.com>
2021-07-01 17:07:07 +0800

    Attempting to click cookie banner before all tests

--- WcaOnRails/spec/rails_helper.rb
+++ WcaOnRails/spec/rails_helper.rb
@@ -92,7 +92,7 @@ end
 RSpec::Matchers.define_negated_matcher :not_change, :change
 
 RSpec.feature "Hide Cookie Banner" do
-  before :all do
+  before :each do
     find(:css, 'body > div.cookies-eu.js-cookies-eu > span.cookies-eu-button-holder > button').click
   end
 end
