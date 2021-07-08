dc8aba5e73ce46f6e2e806c6411eb01c3a9b0fb8
Daniel V. Egdal <28397408+DanielEgdal@users.noreply.github.com>
2021-07-08 17:45:48 +0200

    Fix error

--- WcaOnRails/app/models/competition.rb
+++ WcaOnRails/app/models/competition.rb
@@ -378,8 +378,11 @@ class Competition < ApplicationRecord
       # NOTE: this will show up on the edit schedule page, and stay even if the
       # schedule matches when saved. Should we add some logic to not show this
       # message on the edit schedule page?
-      unless has_any_round_per_event? && schedule_includes_rounds?
-        warnings[:schedule] = I18n.t('competitions.messages.schedule_must_match_rounds')
+      # Competitions after 2018-7-24 will have this check, date set to avoid errors at old competitions
+      if start_date.present? && start_date > Date.new(2018, 7, 24)
+        unless has_any_round_per_event? && schedule_includes_rounds?
+          warnings[:schedule] = I18n.t('competitions.messages.schedule_must_match_rounds')
+        end
       end
 
       unless rounds.all?(&:advancement_condition_is_valid?)
