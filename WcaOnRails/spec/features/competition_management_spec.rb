9752ac55bfc426968ebc6fa4999452958fc80869
notrichardpeng <notrichardpeng@gmail.com>
2021-09-04 14:08:26 -0400

    Add display tests

--- WcaOnRails/spec/features/competition_management_spec.rb
+++ WcaOnRails/spec/features/competition_management_spec.rb
@@ -115,6 +115,22 @@ RSpec.feature "Competition management" do
 
       expect(page).not_to have_text("Display message for free guest entry")
     end
+
+    scenario "select anyone free guest entry status" do
+      competition = FactoryBot.create(:competition, :with_delegate, id: "OldId2016", free_guest_entry_status: 1)
+      visit competition_path(competition)
+      find('div', id: 'show_registration_requirements').click_link('here')
+
+      expect(page).to have_text("Any spectator can attend for free.")
+    end
+
+    scenario "select restricted free guest entry status" do
+      competition = FactoryBot.create(:competition, :with_delegate, id: "OldId2016", free_guest_entry_status: 2)
+      visit competition_path(competition)
+      find('div', id: 'show_registration_requirements').click_link('here')
+
+      expect(page).to have_text("Spectators are only permitted as companions of competitors.")
+    end
   end
 
   context "when signed in as delegate" do
