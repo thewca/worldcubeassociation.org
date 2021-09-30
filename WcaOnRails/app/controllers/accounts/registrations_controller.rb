e6d3dc7028b717679978f870e610401753da7f40
Meenakshi <crazymeens@gmail.com>
2021-09-30 18:23:16 +0530

    Added recaptcha for signup page.

--- WcaOnRails/app/controllers/accounts/registrations_controller.rb
+++ WcaOnRails/app/controllers/accounts/registrations_controller.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 class Accounts::RegistrationsController < Devise::RegistrationsController
+  prepend_before_action :check_captcha, only: [:create]
   protected def after_update_path_for(resource)
     edit_user_registration_path
+  end
+
+  private
+
+  def check_captcha
+    return if verify_recaptcha
+    self.resource = resource_class.new sign_up_params
+    set_minimum_password_length
+    respond_with_navigational(resource) do
+      flash.now[:recaptcha_error] = flash[:recaptcha_error]
+      render :new
+    end
   end
 end
