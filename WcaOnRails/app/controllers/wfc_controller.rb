9cc0988f9ad25f4a8f108bbe400dd3d93ae1caab
Daphne Soegijono <d.soegijono@gmail.com>
2021-10-10 15:49:08 +1100

    Add columns to WFC competition export. Closes #6414

--- WcaOnRails/app/controllers/wfc_controller.rb
+++ WcaOnRails/app/controllers/wfc_controller.rb
@@ -11,16 +11,17 @@ class WfcController < ApplicationController
     select_attributes = [
       :id, :name, :start_date, :end_date,
       :countryId, :announced_at, :results_posted_at,
+      :currency_code, :base_entry_fee_lowest_denomination,
       "count(distinct rails_persons.id) as num_competitors"
     ]
     from = params.require(:from_date)
     to = params.require(:to_date)
-    @competitions=Competition
-                  .select(select_attributes)
-                  .includes(:delegates)
-                  .left_joins(:competitors)
-                  .group("Competitions.id")
-                  .where("results_posted_at >= ? and results_posted_at <= ?", from, to)
-                  .order(:results_posted_at, :name)
+    @competitions = Competition
+                      .select(select_attributes)
+                      .includes(:delegates)
+                      .left_joins(:competitors)
+                      .group("Competitions.id")
+                      .where("results_posted_at >= ? and results_posted_at <= ?", from, to)
+                      .order(:results_posted_at, :name)
   end
 end
