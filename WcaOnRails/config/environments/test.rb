e010ee23d97f272125369122bfc3dd820dc75da4
Gregor Billing <gbilling@worldcubeassociation.org>
2022-02-23 17:35:55 +0100

    Run Rails app:update migration tool

--- WcaOnRails/config/environments/test.rb
+++ WcaOnRails/config/environments/test.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   # config.active_support.disallowed_deprecation_warnings = []
 
+  # Raises error for missing translations.
+  # config.i18n.raise_on_missing_translations = true
+
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 end
