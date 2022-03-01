7ef15cecf66ff554ab627f32e61503c35ad07d23
Gregor Billing <gbilling@worldcubeassociation.org>
2022-03-01 08:56:53 +0100

    Fix RuboCop complaints

--- WcaOnRails/db/migrate/20220223163446_add_service_name_to_active_storage_blobs.active_storage.rb
+++ WcaOnRails/db/migrate/20220223163446_add_service_name_to_active_storage_blobs.active_storage.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 # This migration comes from active_storage (originally 20190112182829)
 class AddServiceNameToActiveStorageBlobs < ActiveRecord::Migration[6.0]
   def up
     unless column_exists?(:active_storage_blobs, :service_name)
       add_column :active_storage_blobs, :service_name, :string
 
-      if configured_service = ActiveStorage::Blob.service.name
+      if (configured_service = ActiveStorage::Blob.service.name)
         ActiveStorage::Blob.unscoped.update_all(service_name: configured_service)
       end
 
--- WcaOnRails/db/migrate/20220223163447_create_active_storage_variant_records.active_storage.rb
+++ WcaOnRails/db/migrate/20220223163447_create_active_storage_variant_records.active_storage.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This migration comes from active_storage (originally 20191206030411)
 class CreateActiveStorageVariantRecords < ActiveRecord::Migration[6.0]
   def change
@@ -5,7 +7,7 @@ class CreateActiveStorageVariantRecords < ActiveRecord::Migration[6.0]
       t.belongs_to :blob, null: false, index: false
       t.string :variation_digest, null: false
 
-      t.index %i[ blob_id variation_digest ], name: "index_active_storage_variant_records_uniqueness", unique: true
+      t.index %i[blob_id variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
   end
