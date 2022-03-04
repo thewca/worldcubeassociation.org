c7bf18adad5714f9301b64e5b958b786352ac2a2
Meenakshi <crazymeens@gmail.com>
2022-03-04 15:33:44 +0530

    Changes according to PR

--- WcaOnRails/app/controllers/accounts/registrations_controller.rb
+++ WcaOnRails/app/controllers/accounts/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Accounts::RegistrationsController < Devise::RegistrationsController
-  prepend_before_action :check_captcha, only: [:create]
+  before_action :check_captcha, only: [:create]
   protected def after_update_path_for(resource)
     edit_user_registration_path
   end
@@ -11,6 +11,7 @@ class Accounts::RegistrationsController < Devise::RegistrationsController
   def check_captcha
     return if verify_recaptcha
     self.resource = resource_class.new sign_up_params
+    resource.validate
     set_minimum_password_length
     respond_with_navigational(resource) do
       flash.now[:recaptcha_error] = flash[:recaptcha_error]
