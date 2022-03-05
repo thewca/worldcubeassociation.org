d3cde675930698dee642c88fb68c305b95dc4723
root <root@notrichard.localdomain>
2022-03-05 18:24:58 -0500

    Fix code formatting

--- WcaOnRails/app/jobs/update_currency_rates.rb
+++ WcaOnRails/app/jobs/update_currency_rates.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-class UpdateCurrencyRates < SingletonApplicationJob        
-    queue_as :default
-  
-    def perform
-        if !Money.default_bank.rates_updated_at || Money.default_bank.rates_updated_at < 1.days.ago
-            Money.default_bank.update_rates
-        end
+class UpdateCurrencyRates < SingletonApplicationJob
+  queue_as :default
+
+  def perform
+    if !Money.default_bank.rates_updated_at || Money.default_bank.rates_updated_at < 1.days.ago
+      Money.default_bank.update_rates
     end
   end
-  
+end
