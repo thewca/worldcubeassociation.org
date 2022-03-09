d3ca5ecef9d6ea17d69bcea08ed05af3608f1de9
Jiankun (Richard) Peng <58278719+notrichardpeng@users.noreply.github.com>
2022-03-09 11:46:37 -0500

    Variable name grammar

--- WcaOnRails/app/jobs/update_currency_rates.rb
+++ WcaOnRails/app/jobs/update_currency_rates.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class UpdateCurrencyRates < SingletonApplicationJob
-  CURRENCY_RATE_UPDATE_INTERVAL = 1.days
+  CURRENCY_RATES_UPDATE_INTERVAL = 1.days
   queue_as :default
 
   def perform
-    if !Money.default_bank.rates_updated_at || Money.default_bank.rates_updated_at < CURRENCY_RATE_UPDATE_INTERVAL.ago
+    if !Money.default_bank.rates_updated_at || Money.default_bank.rates_updated_at < CURRENCY_RATES_UPDATE_INTERVAL.ago
       Money.default_bank.update_rates
     end
   end
