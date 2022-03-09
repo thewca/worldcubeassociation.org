01d4e89d2872ef652cd46af8dbbd99e9547a2192
Jiankun (Richard) Peng <58278719+notrichardpeng@users.noreply.github.com>
2022-03-09 11:45:09 -0500

    Add currency update interval constant

--- WcaOnRails/app/jobs/update_currency_rates.rb
+++ WcaOnRails/app/jobs/update_currency_rates.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class UpdateCurrencyRates < SingletonApplicationJob
+  CURRENCY_RATE_UPDATE_INTERVAL = 1.days
   queue_as :default
 
   def perform
-    if !Money.default_bank.rates_updated_at || Money.default_bank.rates_updated_at < 1.days.ago
+    if !Money.default_bank.rates_updated_at || Money.default_bank.rates_updated_at < CURRENCY_RATE_UPDATE_INTERVAL.ago
       Money.default_bank.update_rates
     end
   end
