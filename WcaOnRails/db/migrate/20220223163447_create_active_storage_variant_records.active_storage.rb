336cf8173170b96fb40bf757782d97331691f166
Philippe Virouleau <philippe.44@gmail.com>
2022-05-01 20:30:34 +0200

    Run migration on 6.1

--- WcaOnRails/db/migrate/20220223163447_create_active_storage_variant_records.active_storage.rb
+++ WcaOnRails/db/migrate/20220223163447_create_active_storage_variant_records.active_storage.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This migration comes from active_storage (originally 20191206030411)
-class CreateActiveStorageVariantRecords < ActiveRecord::Migration[6.0]
+class CreateActiveStorageVariantRecords < ActiveRecord::Migration[6.1]
   def change
     create_table :active_storage_variant_records do |t|
       t.belongs_to :blob, null: false, index: false
