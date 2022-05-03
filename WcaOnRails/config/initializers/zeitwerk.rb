0eeaf40496edd8e29ba89268a7919f78796e9ad4
Gregor Billing <gbilling@worldcubeassociation.org>
2022-05-03 21:15:05 +0900

    Fix Zeitwerk loading error in production mode

--- WcaOnRails/config/initializers/zeitwerk.rb
+++ WcaOnRails/config/initializers/zeitwerk.rb
@@ -3,4 +3,8 @@
 Rails.autoloaders.main.ignore(Rails.root.join('app/webpacker'))
 Rails.autoloaders.main.ignore(Rails.root.join('lib/tasks'))
 
+if Rails.env.production?
+  Rails.autoloaders.main.ignore(Rails.root.join('lib/custom_wca_i18n_scanner.rb'))
+end
+
 Rails.autoloaders.main.inflector.inflect "json_schemas" => "JSONSchemas"
