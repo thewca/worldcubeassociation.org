067c72615d05c85d9e9bdb926a991bfc0e46e3bc
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-05-29 07:26:20 -0300

    Remove evolution of records and missing averages

--- scripts/cronned_results_scripts.sh
+++ scripts/cronned_results_scripts.sh
@@ -5,11 +5,3 @@ cd "$(dirname "$0")"/..
 # The export_public script only will do an actual export if it sees the
 # export url paramter.
 time (cd webroot/results/admin/; REQUEST_URI="export=" php export_public.php)
-
-# Update Evolution of Records
-# We need to set SERVER_NAME to avoid generating the "This is only a copy of
-# the WCA results system used for testing stuff" message.
-time (cd webroot/results/misc/evolution; SERVER_NAME=www.worldcubeassociation.org php update7205.php)
-
-# Update Missing Averages
-time (cd webroot/results/misc/missing_averages; SERVER_NAME=www.worldcubeassociation.org php update7205.php)
