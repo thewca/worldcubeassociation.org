4fd7def5ad7f68c97493aba3e3bf4163a4a17e46
notrichardpeng <notrichardpeng@gmail.com>
2022-06-12 11:38:15 -0400

    Use rails_representation_path

--- WcaOnRails/app/models/regional_organization.rb
+++ WcaOnRails/app/models/regional_organization.rb
@@ -36,7 +36,7 @@ class RegionalOrganization < ApplicationRecord
       name: name,
       website: website,
       country: country,
-      logo: logo.attached? ? Rails.application.routes.url_helpers.rails_representation_url(logo.variant(resize: "500x300").processed, only_path: true) : '',
+      logo: logo.attached? ? Rails.application.routes.url_helpers.rails_representation_path(logo.variant(resize: "500x300").processed) : '',
     }
   end
 end
