ea15baba2227d3f918118737f8484e929f6608be
Philippe Virouleau <philippe.44@gmail.com>
2022-08-04 23:19:28 +0200

    Fix a spring loader error
    When everything is in the same file, it's very easy to trigger the
    following error:
    "superclass mismatch for class RankingCondition"
    
    This commits splits each class to its own file.

---
+++ WcaOnRails/lib/advancement_conditions/ranking_condition.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module AdvancementConditions
+  class RankingCondition < AdvancementCondition
+    alias_method :ranking, :level
+
+    def self.wcif_type
+      "ranking"
+    end
+
+    def to_s(round, short: false)
+      I18n.t("advancement_condition#{".short" if short}.ranking", ranking: ranking)
+    end
+  end
+end
