11eaaef35f04b24dc35fffcca8793007d618d4e2
tnorr <timoartturi@gmail.com>
2022-08-05 01:41:55 +0300

    fix unit test

--- WcaOnRails/spec/lib/results_validators/persons_validator_spec.rb
+++ WcaOnRails/spec/lib/results_validators/persons_validator_spec.rb
@@ -164,7 +164,7 @@ RSpec.describe PV do
         res_lowercase2 = FactoryBot.create(:inbox_result,
                                            competition: competition1,
                                            eventId: "333oh")
-        res_lowercase2.person.update(name: "İlis хocavənd V")
+        res_lowercase2.person.update(name: "ilis Xocavənd")
         res_missing_period = FactoryBot.create(:inbox_result,
                                                competition: competition1,
                                                eventId: "333oh")
