a2a8cafb3a86445b4c68eca8725717666b5be9c6
Philippe Virouleau <philippe.44@gmail.com>
2022-08-05 08:50:54 +0200

    Explicitly set Money's rounding mode
    This silences a warning that the default value will change.

--- WcaOnRails/config/initializers/ruby_money.rb
+++ WcaOnRails/config/initializers/ruby_money.rb
@@ -5,3 +5,4 @@ Money.locale_backend = :i18n
 eu_bank = EuCentralBank.new
 Money.default_bank = eu_bank
 Money.default_currency = Money::Currency.new("USD")
+Money.rounding_mode = BigDecimal::ROUND_HALF_UP
