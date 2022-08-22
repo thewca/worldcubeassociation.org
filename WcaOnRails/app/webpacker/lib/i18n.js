c0b4a38b5d8023b7fbcc53c86e9e044c797de982
Gregor Billing <gregor.billing.dev@gmail.com>
2022-08-22 18:10:18 +0900

    Bump i18n-js library to v4 (#7131)
    * Bump i18n-js versions
    
    * Migrate to i18n-js V4
    
    Loosely follows https://github.com/fnando/i18n-js/blob/main/MIGRATING_FROM_V3_TO_V4.md
    
    * Check for dev mode before running I18N JS listener
    
    Co-authored-by: Philippe Virouleau <philippe.44@gmail.com>
    
    Co-authored-by: Philippe Virouleau <philippe.44@gmail.com>

--- WcaOnRails/app/webpacker/lib/i18n.js
+++ WcaOnRails/app/webpacker/lib/i18n.js
@@ -1,7 +1,21 @@
-import I18n from 'i18n-js';
+import { I18n } from 'i18n-js';
 
-window.I18n = window.I18n || I18n;
-window.I18n.locale = window.wca.currentLocale;
+const i18nFileContext = require.context('rails_translations');
+
+function loadTranslations(i18n, locale) {
+  const translations = i18nFileContext(`./${locale}.json`);
+  i18n.store(translations);
+}
+
+window.I18n = window.I18n || new I18n();
+
 // We always load English + the user locale so that we can fallback.
-window.I18n.fallbacks = true;
+window.I18n.defaultLocale = 'en';
+window.I18n.enableFallback = true;
+// load the actual locale as determined by app/views/layouts/application.html.erb
+window.I18n.locale = window.wca.currentLocale;
 export default window.I18n;
+
+// store the actual translations.
+loadTranslations(window.I18n, 'en');
+loadTranslations(window.I18n, window.wca.currentLocale);
