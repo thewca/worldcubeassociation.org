7f1b491c8f4d0c7c2efc13499fe7f5f948c6738b
revers3ntropy <josephcoppin@gmail.com>
2022-08-22 11:52:13 +0100

    Use List from SemanticUI over raw ui and li elements

--- WcaOnRails/app/webpacker/components/StaticPages/Disclaimer/index.js
+++ WcaOnRails/app/webpacker/components/StaticPages/Disclaimer/index.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
+import { List } from 'semantic-ui-react'
+
 function Disclaimer() {
   return (
-    <ul>
-      <li>
+    <List as="ul">
+      <List.Item as="li">
         The World Cube Association (WCA) works to verify that appropriate WCA Regulations are
         followed for the competition portion of WCA Competitions.
-      </li>
-      <li>
+      </List.Item>
+      <List.Item as="li">
         However, all other aspects of competitions, including (but not limited to) financial
         matters and competition websites, are solely the responsibility of the competition
         organizers and not the WCA.
-      </li>
-      <li>
+      </List.Item>
+      <List.Item as="li">
         Unless otherwise noted, sponsorship and partnership agreements are arranged with
         individual competition organizers and not with the WCA.
-      </li>
-      <li>
+      </List.Item>
+      <List.Item as="li">
         Since the WCA itself does not organize competitions, the WCA is not responsible for
         damage or injury incurred at WCA Competitions.
-      </li>
-      <li>
+      </List.Item>
+      <List.Item as="li">
         It is not permitted to use the WCA&apos;s non-profit status determination letter
         without explicit permission from the WCA Board of Directors.
-      </li>
-    </ul>
+      </List.Item>
+    </List>
   );
 }
 
