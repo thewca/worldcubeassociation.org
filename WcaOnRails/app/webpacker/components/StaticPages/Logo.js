3f41092fdc005ef74eb37fa165dee06a6d5bf30f
revers3ntropy <josephcoppin@gmail.com>
2022-08-30 13:42:58 +0100

    Use I18nHTMLTranslate component

--- WcaOnRails/app/webpacker/components/StaticPages/Logo.js
+++ WcaOnRails/app/webpacker/components/StaticPages/Logo.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { sanitize } from 'dompurify';
 import I18n from '../../lib/i18n';
+import I18nHTMLTranslate from '../I18nHTMLTranslate';
+import '../../stylesheets/static_pages/logo.scss';
 
 /**
  * @returns {JSX.Element}
@@ -8,17 +9,16 @@ import I18n from '../../lib/i18n';
  */
 function Logo({ title }) {
   return (
-    <>
+    <div className="wca-logo-information">
       <h1>{title}</h1>
-      {I18n.t('logo.paragraphs').map((paragraph, index) => paragraph && (
-        <p
+      {I18n.t('logo.paragraphs').map((_, index) => (
+        <I18nHTMLTranslate
           // eslint-disable-next-line react/no-array-index-key
           key={`logo-paragraphs-${index}`}
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: sanitize(paragraph) }}
+          i18nKey={`logo.paragraphs.${index}`}
         />
       ))}
-    </>
+    </div>
   );
 }
 
