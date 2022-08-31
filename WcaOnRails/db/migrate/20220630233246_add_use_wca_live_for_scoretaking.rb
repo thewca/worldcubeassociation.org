8094bba6837d7b0e335714f8824694053795aafd
Daniel James <djdany444@gmail.com>
2022-08-31 12:41:21 +0530

    Changed use_wca_live_for_scoretaking to NOT NULL

--- WcaOnRails/db/migrate/20220630233246_add_use_wca_live_for_scoretaking.rb
+++ WcaOnRails/db/migrate/20220630233246_add_use_wca_live_for_scoretaking.rb
@@ -2,6 +2,7 @@
 
 class AddUseWcaLiveForScoretaking < ActiveRecord::Migration[6.0]
   def change
-    add_column :competitions, :use_wca_live_for_scoretaking, :boolean, null: true, default: false
+    add_column :competitions, :use_wca_live_for_scoretaking, :boolean, null: false, default: true
+    Competition.update_all(use_wca_live_for_scoretaking: false)
   end
 end
