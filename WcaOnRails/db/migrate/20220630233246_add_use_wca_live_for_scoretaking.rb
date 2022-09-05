7b2cf0641df0f374a09af720a07831bacd7daff0
Gregor Billing <gbilling@worldcubeassociation.org>
2022-09-05 23:56:42 +0900

    Fix case-sensitive naming in scoretaking migration
    Is this a misconfiguration on our part or is Rails really that sassy?

--- WcaOnRails/db/migrate/20220630233246_add_use_wca_live_for_scoretaking.rb
+++ WcaOnRails/db/migrate/20220630233246_add_use_wca_live_for_scoretaking.rb
@@ -2,7 +2,7 @@
 
 class AddUseWcaLiveForScoretaking < ActiveRecord::Migration[6.0]
   def change
-    add_column :competitions, :use_wca_live_for_scoretaking, :boolean, null: false, default: true
+    add_column :Competitions, :use_wca_live_for_scoretaking, :boolean, null: false, default: true
     Competition.update_all(use_wca_live_for_scoretaking: false)
   end
 end
