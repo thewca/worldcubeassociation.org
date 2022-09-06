0499be12b5752d47f731fd23cc48a53de03f5db4
Joseph Coppin <49319713+revers3ntropy@users.noreply.github.com>
2022-09-06 17:32:33 +0100

    [Migrate React] Static Pages: Logo (#7167)
    * Migrated logo page to React.
    
    - Created React component
    - Added required translations to config
    - Replaced /logo page content with react_component call
    
    * Use I18nHTMLTranslate component
    
    * Wrapped logo info lines in <p>
    
    * Use Object.keys to filter out empty elements in the array.
    
    Cannot use .filter(Boolean) as it makes the array start from 0, and there is no 0th element in the yml.

---
+++ WcaOnRails/app/webpacker/components/StaticPages/Logo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import I18n from '../../lib/i18n';
+import I18nHTMLTranslate from '../I18nHTMLTranslate';
+
+/**
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Logo({ title }) {
+  return (
+    <div className="wca-logo-information">
+      <h1>{title}</h1>
+      {Object.keys(I18n.t('logo.paragraphs')).map((key) => (
+        <p key={`logo-paragraphs-${key}`}>
+          <I18nHTMLTranslate
+            i18nKey={`logo.paragraphs.${key}`}
+          />
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default Logo;
