8c2347ddbe88e628dcd5a2a7821777af053e99e6
revers3ntropy <josephcoppin@gmail.com>
2022-09-11 17:13:09 +0100

    Remove unused method from person.rb

--- WcaOnRails/app/models/person.rb
+++ WcaOnRails/app/models/person.rb
@@ -165,10 +165,6 @@ class Person < ApplicationRecord
     country&.iso2
   end
 
-  def country_name
-    country&.name
-  end
-
   private def rank_for_event_type(event, type)
     case type
     when :single
