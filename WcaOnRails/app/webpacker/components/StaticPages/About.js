6cc22e0ca35c5ee49671afff1b297f0046837133
revers3ntropy <josephcoppin@gmail.com>
2022-11-04 13:43:01 +0000

    Added correct images and removed faulty images

--- WcaOnRails/app/webpacker/components/StaticPages/About.js
+++ WcaOnRails/app/webpacker/components/StaticPages/About.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { List, Button, Icon } from 'semantic-ui-react';
 import I18n from '../../lib/i18n';
 import I18nHTMLTranslate from '../I18nHTMLTranslate';
+import Worlds2015 from './worlds-2015.png';
+import BulgarianOpen2018 from './bulgarian-open-2018.png';
 
 const DONATE_PAYPAL_URL = 'https://www.paypal.com/donate/?hosted_button_id=W5JL8J4S8UTJU';
 const DONATE_GUIDESTAR_URL = 'https://www.nfggive.com/guidestar/82-3825954';
@@ -52,8 +54,7 @@ function About({ currentBoardMembers }) {
         </div>
         <div className="col-md-4">
           <img
-            /* eslint-disable-next-line global-require */
-            src={require('./bulgarian-open-2018.jpg').default}
+            src={BulgarianOpen2018}
             className="about-page-image"
             alt="Bulgarian Open 2018"
           />
@@ -86,8 +87,7 @@ function About({ currentBoardMembers }) {
         <div className="col-md-4">
           <img
             className="about-page-image"
-            /* eslint-disable-next-line global-require */
-            src={require('./worlds-2015.jpg')}
+            src={Worlds2015}
             alt="World Championship 2015"
           />
         </div>
