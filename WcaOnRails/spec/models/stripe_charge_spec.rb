63676d34bf0703a937cf5fc2d535ee8821c3f56a
Gregor Billing <gbilling@worldcubeassociation.org>
2022-11-05 18:55:55 +0900

    Add cents checks to the Stripe API amount tests

--- WcaOnRails/spec/models/stripe_charge_spec.rb
+++ WcaOnRails/spec/models/stripe_charge_spec.rb
@@ -7,6 +7,7 @@ require 'rails_helper'
 RSpec.describe StripeCharge do
   it "handles HUF as a special currency" do
     six_thousand_huf = Money.from_amount(6_000, 'HUF')
+    expect(six_thousand_huf.cents).to eq(6_000)
 
     stripe_amount = StripeCharge.amount_to_stripe(six_thousand_huf.cents, six_thousand_huf.currency.iso_code)
     expect(stripe_amount).to eq(600_000)
@@ -14,6 +15,7 @@ RSpec.describe StripeCharge do
 
   it "handles UGX as a special currency" do
     sixty_thousand_ugx = Money.from_amount(60_000, 'UGX')
+    expect(sixty_thousand_ugx.cents).to eq(60_000)
 
     stripe_amount = StripeCharge.amount_to_stripe(sixty_thousand_ugx.cents, sixty_thousand_ugx.currency.iso_code)
     expect(stripe_amount).to eq(6_000_000)
@@ -29,6 +31,7 @@ RSpec.describe StripeCharge do
 
   it "handles USD as a normal currency" do
     fifteen_usd = Money.from_amount(15, 'USD')
+    expect(fifteen_usd.cents).to eq(1_500)
 
     stripe_amount = StripeCharge.amount_to_stripe(fifteen_usd.cents, fifteen_usd.currency.iso_code)
     expect(stripe_amount).to eq(1_500)
@@ -36,6 +39,7 @@ RSpec.describe StripeCharge do
 
   it "handles EUR as a normal currency" do
     fifteen_eur = Money.from_amount(15, 'EUR')
+    expect(fifteen_eur.cents).to eq(1_500)
 
     stripe_amount = StripeCharge.amount_to_stripe(fifteen_eur.cents, fifteen_eur.currency.iso_code)
     expect(stripe_amount).to eq(1_500)
@@ -43,6 +47,7 @@ RSpec.describe StripeCharge do
 
   it "handles JPY as a normal currency" do
     two_thousand_yen = Money.from_amount(2_000, 'JPY')
+    expect(two_thousand_yen.cents).to eq(2_000)
 
     stripe_amount = StripeCharge.amount_to_stripe(two_thousand_yen.cents, two_thousand_yen.currency.iso_code)
     expect(stripe_amount).to eq(2_000)
@@ -52,6 +57,7 @@ RSpec.describe StripeCharge do
     # TWD is one of the fancy-snowflake sub-hundred currecies in the Stripe API
     # but it is also the only one that actually has subunits in the RubyMoney gem so it needs no special treatment.
     five_hundred_twd = Money.from_amount(500, 'TWD')
+    expect(five_hundred_twd.cents).to eq(50_000)
 
     stripe_amount = StripeCharge.amount_to_stripe(five_hundred_twd.cents, five_hundred_twd.currency.iso_code)
     expect(stripe_amount).to eq(50_000)
