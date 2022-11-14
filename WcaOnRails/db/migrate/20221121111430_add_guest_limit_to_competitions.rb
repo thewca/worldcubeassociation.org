df798aea9dd4b70c93d7be948cd1f905e3f3e2d6
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2022-11-14 00:49:37 -0500

    Fix indentation width, add missing newline

--- WcaOnRails/db/migrate/20221121111430_add_guest_limit_to_competitions.rb
+++ WcaOnRails/db/migrate/20221121111430_add_guest_limit_to_competitions.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class AddGuestLimitToCompetitions < ActiveRecord::Migration[6.1]
-    def change
-        add_column :Competitions, :guests_per_registration_limit, :integer
-        rename_column :Competitions, :free_guest_entry_status, :guest_entry_status
-    end
+  def change
+    add_column :Competitions, :guests_per_registration_limit, :integer
+    rename_column :Competitions, :free_guest_entry_status, :guest_entry_status
+  end
 end
