3fae8bbfe78e8ecaa450cf2fdf9549b2f6494e11
Daniel V. Egdal <28397408+DanielEgdal@users.noreply.github.com>
2023-01-13 18:56:31 +0100

    Update competitions_controller_spec.rb

--- WcaOnRails/spec/controllers/competitions_controller_spec.rb
+++ WcaOnRails/spec/controllers/competitions_controller_spec.rb
@@ -905,7 +905,7 @@ RSpec.describe CompetitionsController do
       it "can close registration with full limit" do
         comp_with_full_reg = FactoryBot.create(:competition, :registration_open, competitor_limit_enabled: true, competitor_limit: 1, competitor_limit_reason: "we have a tiny venue")
         comp_with_full_reg.organizers << orga
-        FactoryBot.create(:registration, :accepted, :newcomer, competition: comp_with_full_reg) 
+        FactoryBot.create(:registration, :accepted, :newcomer, competition: comp_with_full_reg)
         patch :orga_close_reg_when_full_limit, params: { id: comp_with_full_reg }
         expect(response).to redirect_to edit_competition_path(comp_with_full_reg)
         expect(comp_with_full_reg.reload.registration_close).to be < Time.now
