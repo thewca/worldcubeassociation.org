a4e304d9e2c436e96955d940ebd43bdbcfb4dfd2
Tim Reynolds <tim@cubingusa.org>
2023-02-04 09:22:58 -0500

    Add missing frozen string literal comment

--- WcaOnRails/db/migrate/20221224215047_add_non_competing_staff_to_registration.rb
+++ WcaOnRails/db/migrate/20221224215047_add_non_competing_staff_to_registration.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddNonCompetingStaffToRegistration < ActiveRecord::Migration[7.0]
   def change
     add_column :registrations, :non_competing_staff, :boolean, default: false
