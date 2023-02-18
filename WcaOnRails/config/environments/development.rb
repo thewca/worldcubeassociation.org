501f9c93a0c40d4534aae8c2fb156aa43f5181fa
Finn Ickler <finnickler@gmail.com>
2023-02-18 17:01:11 +0100

    Only use redis in development when running through docker

--- WcaOnRails/config/environments/development.rb
+++ WcaOnRails/config/environments/development.rb
@@ -22,7 +22,13 @@ Rails.application.configure do
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-    config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+    # If the Developer is not running through Docker, Redis caching is disabled
+    redis_url = ENV.fetch("REDIS_URL", nil)
+    if redis_url.present?
+      config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+    else
+      config.cache_store = :memory_store
+    end
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}",
     }
