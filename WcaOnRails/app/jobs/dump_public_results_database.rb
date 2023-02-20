00250c1addbe6617e24195e1d2e708928c4c5a8e
Gregor Billing <gbilling@worldcubeassociation.org>
2023-02-20 10:40:29 +0100

    Change results export cron schedule

--- WcaOnRails/app/jobs/dump_public_results_database.rb
+++ WcaOnRails/app/jobs/dump_public_results_database.rb
@@ -4,9 +4,9 @@ class DumpPublicResultsDatabase < SingletonApplicationJob
   queue_as :default
 
   def perform
-    # Create developer database dump every 3 days.
+    # Create results database dump every day.
     last_public_results_dump = Timestamp.find_or_create_by(name: 'public_results_dump')
-    if last_public_results_dump.not_after?(3.days.ago)
+    if last_public_results_dump.not_after?(24.hours.ago)
       Rake::Task["db:dump:public_results"].invoke
       last_public_results_dump.touch :date
     end
