2699427724e97acd8e92f0b208a97949df4eae20
Finn Ickler <finnickler@gmail.com>
2023-02-20 15:38:24 +0100

    Revert .catch for the import

--- WcaOnRails/app/webpacker/lib/i18n.js
+++ WcaOnRails/app/webpacker/lib/i18n.js
@@ -46,11 +46,7 @@ export default window.I18n;
 function loadTranslations(i18n, locale) {
   import(`../rails_translations/${locale}.json`).then((translations) => {
     i18n.store(translations);
-  }).catch((err) => {
-    if (err) {
-      console.error(`Could not load translations because of ${err}, if this is test this is intended`);
-    }
-  });
+  })
 }
 
 // store the actual translations.
