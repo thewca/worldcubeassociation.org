09f748669574c313fd204f22c6fcae9eeaee779a
Gregor Billing <gbilling@worldcubeassociation.org>
2023-03-11 15:03:05 +0100

    Fix events picker layout

--- WcaOnRails/app/inputs/events_picker_input.rb
+++ WcaOnRails/app/inputs/events_picker_input.rb
@@ -13,59 +13,47 @@ class EventsPickerInput < SimpleForm::Inputs::Base
     only_one = @options[:only_one].present? && @options[:only_one]
     include_all = @options[:include_all].present? && @options[:include_all]
 
-    template.content_tag(:span) do
-      element_class = only_one ? "event-radio" : "event-checkbox"
+    template.content_tag(:p) do
+      if include_all
+        icon = template.unofficial_cubing_icon('miniguild', data: { toggle: "tooltip", placement: "top" }, title: 'All events')
+        selected = selected_events.empty?
 
-      if only_one && include_all
-        checked_options = merged_input_options.merge(checked: selected_events.empty?)
-        check_box = ActionView::Helpers::Tags::RadioButton.new(@builder.object_name, attribute_name, template, "all", checked_options)
-
-        label_id = "#{check_box.send(:tag_id)}_all"
-
-        template.concat(
-          template.content_tag(
-            :span,
-            template.content_tag(
-              :label,
-              (
-                check_box.render +
-                  template.content_tag(:i, "", data: { toggle: "tooltip", placement: "top" }, title: 'ALL', class: "cubing-icon icon unofficial-miniguild")
-              ),
-              for: label_id,
-            ),
-            class: element_class + (merged_input_options[:disabled] ? " disabled" : ""),
-          ),
-        )
+        add_input_field(only_one, 'all', icon, merged_input_options, selected: selected)
       end
 
       allowed_events.each do |event|
-        checked_options = merged_input_options.merge(checked: selected_events.include?(event))
+        icon = template.cubing_icon(event.id, data: { toggle: "tooltip", placement: "top" }, title: event.name)
+        selected = selected_events.include?(event)
 
-        check_box = if only_one
-                      ActionView::Helpers::Tags::RadioButton.new(@builder.object_name, attribute_name, template, event.id, checked_options)
-                    else
-                      ActionView::Helpers::Tags::CheckBox.new("#{@builder.object_name}[#{attribute_name}]", event.id, template, CHECKED_VALUE, UNCHECKED_VALUE, checked_options)
-                    end
-
-        label_id = check_box.send(:tag_id)
-        # quirk in the ActionView template lib that I'm not bothering to investigate because we're moving to React soon anyways.
-        label_id += "_#{event.id}" if only_one
-
-        template.concat(
-          template.content_tag(
-            :span,
-            template.content_tag(
-              :label,
-              (
-                check_box.render +
-                  template.cubing_icon(event.id, data: { toggle: "tooltip", placement: "top" }, title: event.name)
-              ),
-              for: label_id,
-            ),
-            class: element_class + (merged_input_options[:disabled] ? " disabled" : ""),
-          ),
-        )
+        add_input_field(only_one, event.id, icon, merged_input_options, selected: selected)
       end
     end
+  end
+
+  def add_input_field(only_one, id, icon, input_options, selected: false)
+    element_class = only_one ? "event-radio" : "event-checkbox"
+    checked_options = input_options.merge(checked: selected)
+
+    check_box = if only_one
+                  ActionView::Helpers::Tags::RadioButton.new(@builder.object_name, attribute_name, template, id, checked_options)
+                else
+                  ActionView::Helpers::Tags::CheckBox.new("#{@builder.object_name}[#{attribute_name}]", id, template, CHECKED_VALUE, UNCHECKED_VALUE, checked_options)
+                end
+
+    label_id = check_box.send(:tag_id)
+    # quirk in the ActionView template lib that I'm not bothering to investigate because we're moving to React soon anyways.
+    label_id += "_#{id}" if only_one
+
+    template.concat(
+      template.content_tag(
+        :span,
+        template.content_tag(
+          :label,
+          check_box.render + icon,
+          for: label_id,
+        ),
+        class: element_class + (input_options[:disabled] ? " disabled" : ""),
+      ),
+    )
   end
 end
