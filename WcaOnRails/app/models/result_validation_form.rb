f40f1b6a52f9b81aea28f17b5d74f9bb2db114a2
Gregor Billing <gbilling@worldcubeassociation.org>
2023-03-23 19:24:40 +0100

    Don't abuse i18n strings for form values

--- WcaOnRails/app/models/result_validation_form.rb
+++ WcaOnRails/app/models/result_validation_form.rb
@@ -4,10 +4,10 @@ class ResultValidationForm
   ALL_VALIDATOR_NAMES = ResultsValidators::Utils::ALL_VALIDATORS.map(&:class_name)
   VALIDATOR_WITH_FIX_NAMES = ResultsValidators::Utils::VALIDATORS_WITH_FIX.map(&:class_name)
 
-  COMP_VALIDATION_ALL = "Execute for ALL competitions"
-  COMP_VALIDATION_MANUAL = "Pick competition(s) manually"
+  COMP_VALIDATION_ALL = :all
+  COMP_VALIDATION_MANUAL = :manual
 
-  COMP_VALIDATION_MODES = [COMP_VALIDATION_MANUAL, COMP_VALIDATION_ALL].freeze
+  COMP_VALIDATION_MODES = [["Pick competition(s) manually", COMP_VALIDATION_MANUAL], ["Execute for ALL competitions", COMP_VALIDATION_ALL]].freeze
 
   include ActiveModel::Model
 
@@ -15,7 +15,7 @@ class ResultValidationForm
   attr_writer :apply_fixes, :competition_selection
 
   def competitions
-    if @competition_selection == COMP_VALIDATION_ALL
+    if @competition_selection == COMP_VALIDATION_ALL.to_s
       Competition.over.not_cancelled.ids
     else
       @competition_ids.split(",").uniq.compact
