7df510444ecd66243bfc4485fdc3ffec61225576
Gregor Billing <gbilling@worldcubeassociation.org>
2023-03-25 23:17:05 +0100

    Fix calculation of pending newcomers

--- WcaOnRails/app/controllers/admin_controller.rb
+++ WcaOnRails/app/controllers/admin_controller.rb
@@ -132,7 +132,7 @@ class AdminController < ApplicationController
       inbox_result: InboxResult,
       inbox_person: InboxPerson,
       newcomer_person: InboxPerson.where(wcaId: ''),
-      newcomer_result: Result.where("personId REGEXP '^[0-9]+$'"),
+      newcomer_result: Result.select(:personId).distinct.where("personId REGEXP '^[0-9]+$'"),
     }
 
     @existing_data = data_tables.transform_values { |table| table.where(competitionId: @competition.id).count }
