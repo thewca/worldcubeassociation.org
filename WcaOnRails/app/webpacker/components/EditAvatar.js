65749cba955a63d93d56db0a473e889baa640f2d
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2023-04-07 16:24:30 -0300

    Display user's image

--- WcaOnRails/app/webpacker/components/EditAvatar.js
+++ WcaOnRails/app/webpacker/components/EditAvatar.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-const EditAvatar = ({ cdn_explanation, cdn_warning }) => {
+const EditAvatar = ({ cdn_explanation, cdn_warning, user }) => {
   return (
     <div>
       <header className="alert alert-warning">
         <p>{cdn_explanation}</p>
         <p>{cdn_warning}</p>
       </header>
+      <section className="text-center">
+        <img src={user.avatar.url} style={{ width: "50%", height: "auto" }} />
+      </section>
     </div>
   );
 };
