3ed348e7bc16d4709139236aee73bd7704710045
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2023-04-09 15:14:27 -0300

    Add save and remove avatar buttons

--- WcaOnRails/app/webpacker/components/UploadAvatar.js
+++ WcaOnRails/app/webpacker/components/UploadAvatar.js
@@ -1,27 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
-const UploadAvatar = ({ user, translations }) => {
-  console.log(user);
+const UploadAvatar = ({
+  user,
+  staff,
+  translations,
+  uploadDisabled,
+  canRemoveAvatar,
+}) => {
+  const [confirmation, setConfirmation] = useState(false);
+  const [isRemovingAvatar, setIsRemovingAvatar] = useState(false);
   return (
     <section className="container">
       <div className="row">
-        <div className="col-sm-6 well">
-          <h3>{translations.guidelines}</h3>
-          <ul>
-            {translations.avatarGuidelines.map((guideline, idx) => (
-              <li key={idx}>{guideline}</li>
-            ))}
-          </ul>
-          {!!user.teams.length > 0 && (
-            <>
-              <h3>{translations.staffAvatarGuidelinesTitle}</h3>
-              <ul>
-                {translations.staffAvatarGuidelines.map((guideline, idx) => (
-                  <li key={idx}>{guideline}</li>
-                ))}
-              </ul>
-            </>
-          )}
+        <div className="col-sm-6 ">
+          <div className="well">
+            <h3>{translations.guidelines}</h3>
+            <ul>
+              {translations.avatarGuidelines.map((guideline, idx) => (
+                <li key={idx}>{guideline}</li>
+              ))}
+            </ul>
+            {staff && (
+              <>
+                <h3>{translations.staffAvatarGuidelinesTitle}</h3>
+                <ul>
+                  {translations.staffAvatarGuidelines.map((guideline, idx) => (
+                    <li key={idx}>{guideline}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </div>
+          <div>
+            <label htmlFor="send-new-avatar" className="form-label">
+              {translations.sendNewAvatar}
+            </label>
+            <input className="form-control" type="file" id="send-new-avatar" />
+          </div>
+          <p className="text-muted">{translations.afterAvatarUpload}</p>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              value={confirmation}
+              type="checkbox"
+              id="guidelines-confirmation"
+              onChange={() => setConfirmation((old) => !old)}
+              disabled={uploadDisabled}
+            />
+            &nbsp;
+            <label
+              className="form-check-label"
+              htmlFor="guidelines-confirmation"
+            >
+              {translations.guidelinesConfirmation}
+            </label>
+          </div>
+          <div>
+            <button className="btn btn-primary">{translations.save}</button>
+            {!!canRemoveAvatar && (
+              <button className="btn btn-danger">
+                {translations.removeAvatar}
+              </button>
+            )}
+          </div>
         </div>
         <div className="col-sm-6 text-center">
           <img src={user.avatar.url} style={{ width: "60%", height: "auto" }} />
