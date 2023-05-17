98f8c03989673f63dc7e74150694c04a55cc17b3
Simon Kelly <simonkellly@gmail.com>
2023-05-17 23:12:03 +0100

    Hook up api

--- WcaOnRails/app/webpacker/components/CompetitionForm/DuesEstimate.js
+++ WcaOnRails/app/webpacker/components/CompetitionForm/DuesEstimate.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchJsonOrError } from '../../lib/requests/fetchWithAuthenticityToken';
+import { calculateDuesUrl } from '../../lib/requests/routes.js.erb';
 
 export default function DuesEstimate({
   country,
@@ -8,6 +10,21 @@ export default function DuesEstimate({
   compLimitEnabled,
   compLimit,
 }) {
+  useEffect(() => {
+    const params = new URLSearchParams();
+    params.append('competitor_limit_enabled', compLimitEnabled);
+    params.append('competitor_limit', compLimit);
+    params.append('currency_code', currency);
+    params.append('country_id', country);
+    params.append('entry_fee_cents', feeCents);
+
+    fetchJsonOrError(`${calculateDuesUrl}?${params.toString()}`)
+      .then((e) => {
+        // eslint-disable-next-line no-console
+        console.log(e);
+      });
+  });
+
   return (
     <p className="help-block">
       <b>
