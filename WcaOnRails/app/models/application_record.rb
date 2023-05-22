4902a86708f3501715de0824b565d2dd241993ab
FinnIckler <finnickler@gmail.com>
2023-05-22 13:28:24 +0200

    Add Read Replica for Active Record Models (#7641)
    Add the necessary config changes to be able to use the read replica in rails active record models.

--- WcaOnRails/app/models/application_record.rb
+++ WcaOnRails/app/models/application_record.rb
@@ -2,4 +2,6 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  # We are still reading from the primary in most cases
+  connects_to database: { writing: :primary, reading: :primary, read_replica: :primary_replica }
 end
