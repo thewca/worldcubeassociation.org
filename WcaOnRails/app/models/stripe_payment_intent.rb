a0e17249c85a79c8a76f9dbe6b923b0d41d646e0
Gregor Billing <gbilling@worldcubeassociation.org>
2023-06-04 23:22:39 +0200

    Wrap reading Stripe API updates (webhook and return_url) into transaction

--- WcaOnRails/app/models/stripe_payment_intent.rb
+++ WcaOnRails/app/models/stripe_payment_intent.rb
@@ -35,42 +35,44 @@ class StripePaymentIntent < ApplicationRecord
   end
 
   def update_status_and_charges(api_intent, action_source)
-    self.stripe_transaction.update_status(api_intent)
-    self.update!(error_details: api_intent.last_payment_error)
+    ActiveRecord::Base.transaction do
+      self.stripe_transaction.update_status(api_intent)
+      self.update!(error_details: api_intent.last_payment_error)
 
-    # Payment Intent lifecycle as per https://stripe.com/docs/payments/intents#intent-statuses
-    case api_intent.status
-    when 'succeeded'
-      # The payment didn't need any additional actions and is completed!
+      # Payment Intent lifecycle as per https://stripe.com/docs/payments/intents#intent-statuses
+      case api_intent.status
+      when 'succeeded'
+        # The payment didn't need any additional actions and is completed!
 
-      # Record the success timestamp if not already done
-      self.update!(confirmed_at: DateTime.current, confirmed_by: action_source) if self.pending?
+        # Record the success timestamp if not already done
+        self.update!(confirmed_at: DateTime.current, confirmed_by: action_source) if self.pending?
 
-      api_intent.charges.data.each do |charge|
-        recorded_transaction = StripeTransaction.find_by(stripe_id: charge.id)
+        api_intent.charges.data.each do |charge|
+          recorded_transaction = StripeTransaction.find_by(stripe_id: charge.id)
 
-        if recorded_transaction.present?
-          recorded_transaction.update_status(charge)
-        else
-          fresh_transaction = StripeTransaction.create_from_api(charge, {})
-          fresh_transaction.update!(parent_transaction: self.stripe_transaction)
+          if recorded_transaction.present?
+            recorded_transaction.update_status(charge)
+          else
+            fresh_transaction = StripeTransaction.create_from_api(charge, {})
+            fresh_transaction.update!(parent_transaction: self.stripe_transaction)
 
-          yield fresh_transaction if block_given?
+            yield fresh_transaction if block_given?
+          end
         end
+      when 'canceled'
+        # Canceled by Stripe
+
+        self.update!(canceled_at: DateTime.current, canceled_by: action_source)
+      when 'requires_payment_method'
+        # Reset by Stripe
+
+        self.update!(
+          confirmed_at: nil,
+          confirmed_by: nil,
+          canceled_at: nil,
+          canceled_by: nil,
+        )
       end
-    when 'canceled'
-      # Canceled by Stripe
-
-      self.update!(canceled_at: DateTime.current, canceled_by: action_source)
-    when 'requires_payment_method'
-      # Reset by Stripe
-
-      self.update!(
-        confirmed_at: nil,
-        confirmed_by: nil,
-        canceled_at: nil,
-        canceled_by: nil,
-      )
     end
   end
 end
