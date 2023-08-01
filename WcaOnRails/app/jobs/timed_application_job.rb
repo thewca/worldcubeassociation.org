72e3a0cc86ebd1a1b8a2df8582a586c85addf61a
Daniel James <djdany444@gmail.com>
2023-08-01 08:11:10 +0530

    Added queue status for delayed jobs

--- WcaOnRails/app/jobs/timed_application_job.rb
+++ WcaOnRails/app/jobs/timed_application_job.rb
@@ -3,6 +3,10 @@
 module TimedApplicationJob
   extend ActiveSupport::Concern
 
+  def queued_timestamp
+    Timestamp.find_or_create_by!(name: "#{self.name.underscore}_queued")
+  end
+
   def start_timestamp
     Timestamp.find_or_create_by!(name: "#{self.name.underscore}_start")
   end
@@ -11,12 +15,20 @@ module TimedApplicationJob
     Timestamp.find_or_create_by!(name: "#{self.name.underscore}_end")
   end
 
+  def queued_date
+    queued_timestamp.date
+  end
+
   def start_date
     start_timestamp.date
   end
 
   def end_date
     end_timestamp.date
+  end
+
+  def in_queue?
+    queued_date.present? && start_date.nil?
   end
 
   def in_progress?
@@ -29,11 +41,14 @@ module TimedApplicationJob
 
   included do
     after_enqueue do |job|
-      # Reset the end timestamp so the job is no longer considered finished.
+      # When queued, clears the start and end timestamps, and updates the queued timestamp.
+      job.class.queued_timestamp.touch :date
+      job.class.start_timestamp.update! date: nil
       job.class.end_timestamp.update! date: nil
     end
 
     around_perform do |job, block|
+      # When performing, updates the start timestamp.
       job.class.start_timestamp.touch :date
       job.class.end_timestamp.update! date: nil
       block.call
