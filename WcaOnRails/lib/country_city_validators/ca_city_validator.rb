c1f3c37842a074be441bf90d544d3e1fcdc784ee
Nicholas McKee <nicholas.mckee@mail.utoronto.ca>
2023-08-07 14:17:11 -0300

    Added recognition of Canadian Territories

--- WcaOnRails/lib/country_city_validators/ca_city_validator.rb
+++ WcaOnRails/lib/country_city_validators/ca_city_validator.rb
@@ -14,9 +14,15 @@ module CountryCityValidators
     Saskatchewan
   ).to_set
 
+  CA_TERRITORIES = %w(
+    Northwest\ Territories
+    Nunavut
+    Yukon
+  )
+
   class CaCityValidator < CityCommaRegionValidator
     def initialize
-      super(type_of_region: "province", valid_regions: CA_PROVINCES)
+      super(type_of_region: "province", valid_regions: (CA_PROVINCES | CA_TERRITORIES))
     end
 
     def self.country_iso_2
