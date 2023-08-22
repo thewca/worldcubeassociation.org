fa92e708b5a560739ac101073fcd5e8909a8aeb8
Gregor Billing <gbilling@worldcubeassociation.org>
2023-08-22 18:15:04 +0200

    Recompute skipped indexes after Persons column renames

---
+++ WcaOnRails/db/migrate/20230822160657_recompute_persons_table_indices.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class RecomputePersonsTableIndices < ActiveRecord::Migration[7.0]
+  def change
+    remove_index :Persons, name: :Persons_id
+    remove_index :Persons, name: :index_Persons_on_id_and_subId
+
+    add_index :Persons, :wca_id
+    add_index :Persons, [:wca_id, :subId], unique: true
+  end
+end
