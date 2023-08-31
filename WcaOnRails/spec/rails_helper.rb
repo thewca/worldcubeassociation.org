684b9f6418cf60a9b28eb68cf5cc460c923754db
Gregor Billing <gbilling@worldcubeassociation.org>
2023-08-31 20:55:50 +0200

    Reinstate ActiveJob Test helper

--- WcaOnRails/spec/rails_helper.rb
+++ WcaOnRails/spec/rails_helper.rb
@@ -85,6 +85,8 @@ RSpec.configure do |config|
   config.include ApiSignInHelper, type: :request
 
   config.include ApplicationHelper
+
+  config.include ActiveJob::TestHelper
 end
 
 # See: https://github.com/rspec/rspec-expectations/issues/664#issuecomment-58134735
