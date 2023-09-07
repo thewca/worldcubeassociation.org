551d14b6fcf0d801d7875417620063d65d2c4832
Gregor Billing <gbilling@worldcubeassociation.org>
2023-09-07 11:17:33 +0200

    Remove rspec-sidekiq and affiliated tests

--- WcaOnRails/spec/support/sidekiq.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'middlewares/job_reporting_middleware'
-
-Sidekiq::Testing.server_middleware do |chain|
-  chain.add Middlewares::JobReportingMiddleware
-end
