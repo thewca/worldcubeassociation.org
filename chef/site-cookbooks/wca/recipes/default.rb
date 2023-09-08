be9747c99bd4bb6ea75837efa5327a83ff9b765f
FinnIckler <finnickler@gmail.com>
2023-09-08 13:07:11 +0200

    remove port

--- chef/site-cookbooks/wca/recipes/default.rb
+++ chef/site-cookbooks/wca/recipes/default.rb
@@ -274,7 +274,7 @@ redis = {
 
 if node.chef_environment == "production"
   # In production mode, we use Amazon ElasticCache.
-  redis[:host] = "wca-main-cache-001.iebvzt.0001.usw2.cache.amazonaws.com:6379"
+  redis[:host] = "wca-main-cache-001.iebvzt.0001.usw2.cache.amazonaws.com"
 elsif node.chef_environment == "staging"
   # In staging mode, we use Amazon ElasticCache.
   redis[:host] = "redis-main-staging-001.iebvzt.0001.usw2.cache.amazonaws.com"
