5e2f55a1cee57fd06fd129467ec7559f6142d696
Gregor Billing <gbilling@worldcubeassociation.org>
2023-09-19 11:23:04 +0200

    Specify non-cronjob queues in Sidekiq default config

--- WcaOnRails/config/initializers/sidekiq.rb
+++ WcaOnRails/config/initializers/sidekiq.rb
@@ -3,6 +3,9 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: EnvConfig.SIDEKIQ_REDIS_URL }
 
+  # Run all queues except cronjobs in the default runner
+  config.queues = %w[critical default mailers]
+
   # Our cronjobs generally react allergic to concurrency because we never designed them with idempotency in mind.
   # Instead of redesigning our whole zoo of cronjobs, pipe them through a linear execution worker.
   config.capsule("cronjobs") do |cap|
