98f91be23029689d95600ae986c1e88b0289c284
Gregor Billing <gbilling@worldcubeassociation.org>
2023-09-19 11:40:39 +0200

    Push cronjobs to non-concurrent queue via Sidekiq capsule (#8322)
    * Push cronjobs to non-concurrent queue via Sidekiq capsule
    
    * Specify non-cronjob queues in Sidekiq default config

--- WcaOnRails/config/initializers/sidekiq.rb
+++ WcaOnRails/config/initializers/sidekiq.rb
@@ -2,6 +2,16 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: EnvConfig.SIDEKIQ_REDIS_URL }
+
+  # Run all queues except cronjobs in the default runner
+  config.queues = %w[critical default mailers]
+
+  # Our cronjobs generally react allergic to concurrency because we never designed them with idempotency in mind.
+  # Instead of redesigning our whole zoo of cronjobs, pipe them through a linear execution worker.
+  config.capsule("cronjobs") do |cap|
+    cap.concurrency = 1
+    cap.queues = [WcaCronjob::QUEUE_NAME]
+  end
 end
 
 Sidekiq.configure_client do |config|
