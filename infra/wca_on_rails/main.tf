45d7e6c28426cae02cccb0028fe198052c58dc48
FinnIckler <finnickler@gmail.com>
2023-09-20 13:58:34 +0200

    Ready to deploy shared resources

--- infra/wca_on_rails/main.tf
+++ infra/wca_on_rails/main.tf
@@ -1,17 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  # Supports multiple workspaces
+  backend "s3" {
+    bucket = "wca-main-terraform-state"
+    key    = "wca-main"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Project = var.name_prefix
+    }
+  }
+}
+
 #module "production" {
 #  source = "./production"
 #  name_prefix = "${var.name_prefix}-prod"
 #  region = var.region
 #}
 
-module "staging" {
-  source = "./staging"
-  name_prefix = "${var.name_prefix}-staging"
-  region = var.region
-  VAULT_ADDR = var.VAULT_ADDR
-  DATABASE_WRT_USER = var.DATABASE_WRT_USER
-  shared = module.shared
-}
+#module "staging" {
+#  source = "./staging"
+#  name_prefix = "${var.name_prefix}-staging"
+#  region = var.region
+#  VAULT_ADDR = var.VAULT_ADDR
+#  DATABASE_WRT_USER = var.DATABASE_WRT_USER
+#  shared = module.shared
+#}
 
 module "shared" {
   source = "./shared"
