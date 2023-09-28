6caf2d25e9acf5aff0afc3b21f7f2c7d734104e3
Gregor Billing <gbilling@worldcubeassociation.org>
2023-09-28 21:56:27 +0200

    Remove more unused JS code

--- WcaOnRails/app/webpacker/lib/utils/edit-schedule.js
+++ WcaOnRails/app/webpacker/lib/utils/edit-schedule.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
-import { parseActivityCode } from './wcif';
 import { DateTime, Duration } from 'luxon';
+import { parseActivityCode } from './wcif';
 
 export function toMicrodegrees(coord) {
   return Math.trunc(parseFloat(coord) * 1e6);
@@ -10,12 +10,6 @@ export function toDegrees(coord) {
   return coord / 1e6;
 }
 
-const currentElementsIds = {
-  venue: 0,
-  room: 0,
-  activity: 0,
-};
-
 function withNestedActivities(activities) {
   if (activities.length === 0) return [];
   return [
@@ -24,59 +18,12 @@ function withNestedActivities(activities) {
   ];
 }
 
-export function initElementsIds(venues) {
-  // Explore the WCIF to get the highest ids.
-  const maxId = (objects) => _.max(_.map(objects, 'id')) || 0;
-  const rooms = _.flatMap(venues, 'rooms');
-  const activities = _.flatMap(rooms, (room) => withNestedActivities(room.activities));
-  currentElementsIds.venue = maxId(venues);
-  currentElementsIds.room = maxId(rooms);
-  currentElementsIds.activity = maxId(activities);
-}
-
-export function newVenueId() {
-  currentElementsIds.venue += 1;
-  return currentElementsIds.venue;
-}
-
-export function newRoomId() {
-  currentElementsIds.room += 1;
-  return currentElementsIds.room;
-}
-
-export function newActivityId() {
-  currentElementsIds.activity += 1;
-  return currentElementsIds.activity;
-}
-
 export function nextActivityId(wcifSchedule) {
   // Explore the WCIF to get the highest ids.
   const maxId = (objects) => _.max(_.map(objects, 'id')) || 0;
   const rooms = wcifSchedule.venues.flatMap((venue) => venue.rooms);
   const activities = rooms.flatMap((room) => withNestedActivities(room.activities));
   return maxId(activities) + 1;
-}
-
-export function convertVenueActivitiesToVenueTimezone(oldTZ, venueWcif) {
-  // Called when a venue's timezone has been updated, to update all the activities times.
-  // The WCA website expose times in UTC, so we need to do two steps:
-  //   - first, express each activity times in the old venue's timezone
-  //   - second, change the timezone without changing the actual time figure
-  //   (eg: 4pm stays 4pm, but in a different timezone).
-  const newTZ = venueWcif.timezone;
-  venueWcif.rooms.forEach((room) => {
-    withNestedActivities(room.activities).forEach((activity) => {
-      // Undocumented "keepTime" parameter, see here:
-      // https://stackoverflow.com/questions/28593304/same-date-in-different-time-zone/28615654#28615654
-      // This enables us to change the UTC offset without changing
-      // the *actual* time of the activity!
-      // NOTE: we intentionally modify the object referenced by activity.
-      /* eslint-disable-next-line */
-      activity.startTime = window.moment(activity.startTime).tz(oldTZ).tz(newTZ, true).format();
-      /* eslint-disable-next-line */
-      activity.endTime = window.moment(activity.endTime).tz(oldTZ).tz(newTZ, true).format();
-    });
-  });
 }
 
 export function defaultDurationFromActivityCode(activityCode) {
