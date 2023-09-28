35e9f026c3e725c34a53a2a61f8b4d57747f906b
Daniel James <djdany444@gmail.com>
2023-09-28 17:06:09 +0530

    Modified post to call fetchWithAuthenticityToken

--- WcaOnRails/app/webpacker/lib/requests/post.js
+++ WcaOnRails/app/webpacker/lib/requests/post.js
@@ -1,18 +1,11 @@
-export default function post(url, data = {}, config = {
-  headers: { 'Content-Type': 'application/json' },
-  auth: { required: true },
-}) {
+import { fetchWithAuthenticityToken } from './fetchWithAuthenticityToken';
+
+export default function post(url, data = {}, config = { headers: {} }) {
   const headers = {
     'Content-Type': 'application/json',
     ...(config.headers || {}),
   };
-  if (config.auth && config.auth.required) {
-    const csrfTokenElement = document.querySelector('meta[name=csrf-token]');
-    if (csrfTokenElement) {
-      headers['X-CSRF-Token'] = csrfTokenElement.content;
-    }
-  }
-  return fetch(url, {
+  return fetchWithAuthenticityToken(url, {
     method: 'POST',
     body: JSON.stringify(data),
     headers,
