06dbbdaf82b89fd3dacd04f1f5f2ff4d0b0afb60
FinnIckler <finnickler@gmail.com>
2023-10-04 12:26:22 +0200

    Added payment routes

---
+++ WcaOnRails/app/controllers/api/internal/v1/payment_controller.rb
@@ -0,0 +1,57 @@
+class Api::Internal::V1::PaymentController < Api::Internal::V1::ApiController
+  def init
+    competition_id, user_id = params["attendee_id"].split("-")
+
+    amount = params["amount"].to_i
+
+    competition = Competition.find(competition_id)
+    user = User.find(user_id)
+    account_id = competition.connected_stripe_account_id
+
+    registration_metadata = {
+      competition: competition.name,
+      registration_url: "https://www.worldcubeassociation.org/competitions/#{competition_id}/#{user_id}/edit",
+    }
+
+    currency_iso = params["currency_code"]
+    stripe_amount = StripeTransaction.amount_to_stripe(amount, currency_iso)
+
+    payment_intent_args = {
+      amount: stripe_amount,
+      currency: currency_iso,
+      receipt_email: user.email,
+      description: "Registration payment for #{competition.name}",
+      metadata: registration_metadata,
+    }
+
+    # The Stripe API forces the user to provide a return_url when using automated payment methods.
+    # In our test suite however, we want to be able to confirm specific payment methods without a return URL
+    # because our CI containers are not exposed to the public. So we need this little hack :/
+    enable_automatic_pm = !Rails.env.test?
+
+    # we cannot recycle an existing intent, so we create a new one which needs all possible PaymentMethods enabled.
+    # Required as per https://stripe.com/docs/payments/accept-a-payment-deferred?type=payment&client=html#create-intent
+    payment_intent_args[:automatic_payment_methods] = { enabled: enable_automatic_pm }
+
+    # Create the PaymentIntent, overriding the stripe_account for the request
+    # by the connected stripe account for the competition.
+    intent = Stripe::PaymentIntent.create(
+      payment_intent_args,
+      stripe_account: account_id,
+      )
+
+    # Log the payment attempt. We register the payment intent ID to find it later after checkout completed.
+    stripe_transaction = StripeTransaction.create_from_api(intent, payment_intent_args, account_id)
+
+    # memoize the payment intent in our DB because payments are handled asynchronously
+    # so we need to be able to retrieve this later at any time, even when our server crashes in the meantime…
+    StripePaymentIntent.create!(
+      holder: params["attendee_id"],
+      stripe_transaction: stripe_transaction,
+      client_secret: intent.client_secret,
+      user: user,
+      )
+
+    render json: { client_secret: intent.client_secret, connected_account_id: account_id }
+  end
+end
---
+++ WcaOnRails/app/controllers/payment_controller.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+class PaymentController < ApplicationController
+  def payment_config
+    competition = Competition.find(params[:competition_id])
+    render json: { stripe_publishable_key: EnvVars.STRIPE_PUBLISHABLE_KEY, connected_account_id: competition.connected_stripe_account_id }
+  end
+
+  def payment_finish
+    competition = Competition.find(params[:competition_id])
+
+    # Provided by Stripe upon redirect when the "PaymentElement" workflow is completed
+    intent_id = params[:payment_intent]
+    intent_secret = params[:payment_intent_client_secret]
+
+    stored_transaction = StripeTransaction.find_by(stripe_id: intent_id)
+    stored_intent = stored_transaction.stripe_payment_intent
+
+    unless stored_intent.client_secret == intent_secret
+      flash[:error] = t("registrations.payment_form.errors.stripe_secret_invalid")
+      return redirect_to competition_register_path(competition)
+    end
+
+    # No need to create a new intent here. We can just query the stored intent from Stripe directly.
+    stripe_intent = stored_intent.retrieve_intent
+
+    unless stripe_intent.present?
+      flash[:error] = t("registrations.payment_form.errors.stripe_not_found")
+      return redirect_to competition_register_path(competition)
+    end
+
+    stored_intent.update_status_and_charges(stripe_intent, current_user) do |charge_transaction|
+      ruby_money = charge_transaction.money_amount
+      registration_payments.create!(
+        amount_lowest_denomination: ruby_money.cents,
+        currency_code: ruby_money.currency.iso_code,
+        receipt: charge_transaction,
+        user_id: current_user.id,
+        )
+    end
+
+    # Payment Intent lifecycle as per https://stripe.com/docs/payments/intents#intent-statuses
+    case stored_transaction.status
+    when 'succeeded'
+      flash[:success] = t("registrations.payment_form.payment_successful")
+    when 'requires_action'
+      # Customer did not complete the payment
+      # For example, 3DSecure could still be pending.
+      flash[:warning] = t("registrations.payment_form.errors.payment_pending")
+    when 'requires_payment_method'
+      # Payment failed. If a payment fails, it is "reset" by Stripe,
+      # so from our end it looks like it never even started (i.e. the customer didn't choose a payment method yet)
+      flash[:error] = t("registrations.payment_form.errors.payment_reset")
+    when 'processing'
+      # The payment can be pending, for example bank transfers can take multiple days to be fulfilled.
+      flash[:warning] = t("registrations.payment_form.payment_processing")
+    else
+      # Invalid status
+      flash[:error] = "Invalid PaymentIntent status"
+    end
+
+    redirect_to competition_register_path(competition)
+  end
+
+  def payment_refund
+    competition_id, user_id = params["attendee_id"].split("-")
+    competition = Competition.find(competition_id)
+
+    unless competition.using_stripe_payments?
+      flash[:danger] = "You cannot emit refund for this competition anymore. Please use your Stripe dashboard to do so."
+      return redirect_to "https://www.worldcubeassociation.org/competitions/#{competition_id}/#{user_id}/edit"
+    end
+
+    payment = RegistrationPayment.find(params[:payment_id])
+
+    refund_amount_param = params.require(:payment).require(:refund_amount)
+    refund_amount = refund_amount_param.to_i
+
+    if refund_amount > payment.amount_available_for_refund
+      flash[:danger] = "You are not allowed to refund more than the competitor has paid."
+      return redirect_to "https://www.worldcubeassociation.org/competitions/#{competition_id}/#{user_id}/edit"
+    end
+
+    if refund_amount < 0
+      flash[:danger] = "The refund amount must be greater than zero."
+      return redirect_to "https://www.worldcubeassociation.org/competitions/#{competition_id}/#{user_id}/edit"
+    end
+
+    currency_iso = competition.currency_code
+    stripe_amount = StripeTransaction.amount_to_stripe(refund_amount, currency_iso)
+
+    # Backwards compatibility: We may at some point try to record a refund for a payment that was
+    #   - created before the introduction of receipts
+    #   - but refunded after the new receipts feature was introduced. Fall back to the old stripe_charge_id if that happens.
+    charge_id = payment.receipt&.stripe_id || payment.stripe_charge_id
+
+    refund_args = {
+      charge: charge_id,
+      amount: stripe_amount,
+    }
+
+    account_id = competition.connected_stripe_account_id
+
+    refund = Stripe::Refund.create(
+      refund_args,
+      stripe_account: account_id,
+      )
+
+    refund_receipt = StripeTransaction.create_from_api(refund, refund_args, account_id)
+    refund_receipt.update!(parent_transaction: payment.receipt) if payment.receipt.present?
+
+    # Should be the same as `refund_amount`, but by double-converting from the Stripe object
+    # we can also double-check that they're on the same page as we are (to be _really_ sure!)
+    ruby_money = refund_receipt.money_amount
+
+    registration_payments.create!(
+      amount_lowest_denomination: ruby_money.cents * -1,
+      currency_code: ruby_money.currency.iso_code,
+      receipt: refund_receipt,
+      refunded_registration_payment_id: payment.id,
+      user_id: current_user.id,
+      )
+
+    flash[:success] = 'Payment was refunded'
+    redirect_to "https://www.worldcubeassociation.org/competitions/#{competition_id}/#{user_id}/edit"
+  end
+end
