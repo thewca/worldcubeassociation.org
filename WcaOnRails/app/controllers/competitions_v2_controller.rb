adc9b54603c401a9cf3980806cf507fe7bd423f8
FinnIckler <finnickler@gmail.com>
2023-10-18 17:41:00 +0200

    make competition available in the view to set the title

--- WcaOnRails/app/controllers/competitions_v2_controller.rb
+++ WcaOnRails/app/controllers/competitions_v2_controller.rb
@@ -4,8 +4,8 @@ class CompetitionsV2Controller < ApplicationController
   def show
     competition_id = params[:id]
     redirect_to competition_list_path unless competition_id.present?
-    competition = Competition.find(competition_id)
-    raise ActionController::RoutingError.new('Not Found') unless competition.present? && competition.user_can_view?(current_user)
-    redirect_to competition_path(competition) unless competition.uses_new_registration_service?
+    @competition = Competition.find(competition_id)
+    raise ActionController::RoutingError.new('Not Found') unless @competition.present? && @competition.user_can_view?(current_user)
+    redirect_to competition_path(@competition) unless @competition.uses_new_registration_service?
   end
 end
