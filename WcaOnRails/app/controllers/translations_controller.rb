4d1ea0224eb03d4e74eba09b2fd8b31f9cc1a2f0
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-10-30 00:31:49 +0700

    Sync fork repo before opening a translation PR

--- WcaOnRails/app/controllers/translations_controller.rb
+++ WcaOnRails/app/controllers/translations_controller.rb
@@ -42,6 +42,14 @@ class TranslationsController < ApplicationController
     message = "Update #{locale} translation."
     content_digest = Digest::SHA1.hexdigest(content)
     branch_name = "translation-#{locale}-#{content_digest}"
+    # We create a branch pointing to upstream master SHA. This stopped working
+    # unless the commit exists in the fork repository, so we always sync first.
+    # Octokit doesn't expost a method for this, so we make a regular request.
+    RestClient.post("https://api.github.com/repos/#{origin_repo}/merge-upstream", '{"branch":"master"}', {
+      accept: "application/vnd.github+json",
+      authorization: "Bearer #{AppSecrets.GITHUB_CREATE_PR_ACCESS_TOKEN}",
+      x_github_api_version: "2022-11-28"
+    })
     upstream_sha = Octokit.ref(upstream_repo, "heads/master")[:object][:sha]
     Octokit.create_ref(origin_repo, "heads/#{branch_name}", upstream_sha)
     current_content_sha = Octokit.content(origin_repo, path: file_path, ref: branch_name)[:sha]
