0f4950dd4acf1a899c00fe62d2a01964a1e4064e
Daniel James <djdany444@gmail.com>
2023-10-29 08:17:27 +0530

    Changed link to probation and removed probation from teams (#8412)
    * Changed link to probation and removed probation from teams
    
    * Removed migration
    
    * Added authorization for probation edit
    
    * Added test cases for probation roles

--- WcaOnRails/app/controllers/delegates_controller.rb
+++ WcaOnRails/app/controllers/delegates_controller.rb
@@ -2,10 +2,21 @@
 
 class DelegatesController < ApplicationController
   before_action :authenticate_user!
-  before_action -> { redirect_to_root_unless_user(:can_view_delegate_matters?) }
+  before_action -> { redirect_to root unless user.can_view_delegate_matters? }, only: [:stats]
+  before_action -> { redirect_to root_url unless current_user_is_authorized? }, only: [:delegate_probation_data, :probations]
+  before_action :current_user_is_authorized_for_action!, only: [:delegate_probation_data, :start_delegate_probation, :end_delegate_probation]
+  private def current_user_is_authorized_for_action!
+    unless current_user_is_authorized?
+      render json: {}, status: 401
+    end
+  end
 
   def stats
     @delegates = User.delegates.includes(:actually_delegated_competitions)
+  end
+
+  private def current_user_is_authorized?
+    current_user.senior_delegate? || current_user.team_leader?(Team.wfc) || current_user.team_senior_member?(Team.wfc)
   end
 
   def delegate_probation_data
