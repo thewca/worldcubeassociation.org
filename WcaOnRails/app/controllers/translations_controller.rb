54da67574ac4dd9c5a48127ec4919e09175110ef
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-11-01 18:44:17 +0100

    Update WcaOnRails/app/controllers/translations_controller.rb

--- WcaOnRails/app/controllers/translations_controller.rb
+++ WcaOnRails/app/controllers/translations_controller.rb
@@ -44,8 +44,8 @@ class TranslationsController < ApplicationController
     branch_name = "translation-#{locale}-#{content_digest}"
     # We create a branch pointing to upstream master SHA. This stopped working
     # unless the commit exists in the fork repository, so we always sync first.
-    # Octokit doesn't expost a direct method for this, so we monkey-patch our request.
-    Octokit.client.post "#{Octokit::Repository.path origin_repo}/merge-upstream", { branch: 'master' }
+    # Octokit doesn't expose a direct method for this, so we make a custom request.
+    Octokit.client.post("#{Octokit::Repository.path(origin_repo)}/merge-upstream", { branch: "master" })
     upstream_sha = Octokit.ref(upstream_repo, "heads/master")[:object][:sha]
     Octokit.create_ref(origin_repo, "heads/#{branch_name}", upstream_sha)
     current_content_sha = Octokit.content(origin_repo, path: file_path, ref: branch_name)[:sha]
