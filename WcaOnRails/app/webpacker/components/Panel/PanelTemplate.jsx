d5ae66c737b3e9a139cc62dbce7bd204a7b829b3
Daniel James <djdany444@gmail.com>
2023-11-05 08:38:53 +0530

    Added external icon and fixed click issue in mobile view

--- WcaOnRails/app/webpacker/components/Panel/PanelTemplate.jsx
+++ WcaOnRails/app/webpacker/components/Panel/PanelTemplate.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {
   Dropdown,
-  Grid, Header, Menu, Segment,
+  Grid, Header, Icon, Menu, Segment,
 } from 'semantic-ui-react';
 
 export default function PanelTemplate({ heading, sections }) {
-  const [selectedMenu, setSelectedMenu] = React.useState(sections[0].id);
+  const [selectedMenu, setSelectedMenu] = React.useState(0);
   const SelectedComponent = React.useMemo(() => sections.find(
-    (section) => section.id === selectedMenu,
+    (_, index) => index === selectedMenu,
   ).component, [sections, selectedMenu]);
 
-  function menuClickHandler(section) {
+  function menuClickHandler(index) {
+    const section = sections[index];
     if (section.component) {
-      setSelectedMenu(section.id);
+      setSelectedMenu(index);
     } else {
       window.open(section.link);
     }
@@ -24,13 +25,14 @@ export default function PanelTemplate({ heading, sections }) {
       <Grid container>
         <Grid.Column only="computer" computer={4}>
           <Menu vertical>
-            {sections.map((section) => (
+            {sections.map((section, index) => (
               <Menu.Item
                 key={section.id}
                 name={section.name}
-                active={selectedMenu === section.id}
-                onClick={() => menuClickHandler(section)}
+                active={selectedMenu === index}
+                onClick={() => menuClickHandler(index)}
               >
+                {!section.component && <Icon name="external alternate" />}
                 {section.name}
               </Menu.Item>
             ))}
@@ -43,10 +45,11 @@ export default function PanelTemplate({ heading, sections }) {
               <Grid.Row only="tablet mobile">
                 <Dropdown
                   inline
-                  options={sections.map((section) => ({
+                  options={sections.map((section, index) => ({
                     key: section.id,
                     text: section.name,
-                    value: section.id,
+                    value: index,
+                    icon: !section.component && 'external alternate',
                   }))}
                   value={selectedMenu}
                   onChange={(_, { value }) => menuClickHandler(value)}
