45be37f955660bca129dee8fe35427326c8d1423
Gregor Billing <gbilling@worldcubeassociation.org>
2023-12-02 14:41:55 +0100

    Update database schemas for Rails 7.1

--- WcaOnRails/db/results_dump_schema.rb
+++ WcaOnRails/db/results_dump_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   create_table "Competitions", id: false, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "id", limit: 32, default: "", null: false
     t.string "name", limit: 50, default: "", null: false
--- WcaOnRails/db/schema.rb
+++ WcaOnRails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_25_061943) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_25_061943) do
   create_table "Competitions", id: { type: :string, limit: 32, default: "" }, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 50, default: "", null: false
     t.string "cityName", limit: 50, default: "", null: false
