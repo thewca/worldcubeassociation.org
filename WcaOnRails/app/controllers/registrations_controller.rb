7c457f0b395e6a2d1c64b1de3db7d90ddede478c
Duncan <duncanonthejob@gmail.com>
2023-12-06 09:44:58 +0200

    removed old competition redirect logic

--- WcaOnRails/app/controllers/registrations_controller.rb
+++ WcaOnRails/app/controllers/registrations_controller.rb
@@ -20,7 +20,7 @@ class RegistrationsController < ApplicationController
   end
 
   before_action -> { redirect_to_root_unless_user(:can_manage_competition?, competition_from_params) },
-                except: [:create, :index, :psych_sheet, :psych_sheet_event, :register, :register_require_sign_in, :payment_completion, :load_payment_intent, :stripe_webhook, :stripe_denomination, :destroy, :update]
+                except: [:create, :index, :psych_sheet, :psych_sheet_event, :register, :payment_completion, :load_payment_intent, :stripe_webhook, :stripe_denomination, :destroy, :update]
 
   before_action :competition_must_be_using_wca_registration!, except: [:import, :do_import, :add, :do_add, :index, :psych_sheet, :psych_sheet_event, :stripe_webhook, :stripe_denomination]
   private def competition_must_be_using_wca_registration!
@@ -426,11 +426,6 @@ class RegistrationsController < ApplicationController
         render :register
       end
     end
-  end
-
-  def register_require_sign_in
-    @competition = competition_from_params
-    redirect_to competition_register_path(@competition)
   end
 
   def register
