2792bd85c2b8e1204c8443c78e5432f604035395
Gregor Billing <gbilling@worldcubeassociation.org>
2023-12-07 18:38:59 +0100

    Dispatch 'confirmed' flag when confirming

--- WcaOnRails/app/webpacker/components/CompetitionForm/ConfirmationActions.js
+++ WcaOnRails/app/webpacker/components/CompetitionForm/ConfirmationActions.js
@@ -1,7 +1,7 @@
 import { Button } from 'semantic-ui-react';
 import React, { useMemo } from 'react';
 import I18n from '../../lib/i18n';
-import { useStore } from '../../lib/providers/StoreProvider';
+import { useDispatch, useStore } from '../../lib/providers/StoreProvider';
 import useLoadedData from '../../lib/hooks/useLoadedData';
 import {
   competitionConfirmationDataUrl,
@@ -11,6 +11,7 @@ import {
 import Loading from '../Requests/Loading';
 import ConfirmProvider, { useConfirm } from '../../lib/providers/ConfirmProvider';
 import useSaveAction from '../../lib/hooks/useSaveAction';
+import { changesSaved, updateFormValue } from './store/actions';
 
 export function CreateOrUpdateButton({
   createComp,
@@ -49,11 +50,20 @@ function ConfirmButton({
   const { save } = useSaveAction();
   const confirm = useConfirm();
 
+  const dispatch = useDispatch();
+
   const confirmCompetition = () => {
     confirm({
       content: I18n.t('competitions.competition_form.submit_confirm'),
     }).then(() => {
-      save(confirmCompetitionUrl(competitionId), null, sync, {
+      save(confirmCompetitionUrl(competitionId), null, () => {
+        sync();
+
+        // mark the competition as announced and commit immediately.
+        // (we do not want the announce button to trigger the "there are unsaved changes" alert)
+        dispatch(updateFormValue('isConfirmed', true, ['admin']));
+        dispatch(changesSaved());
+      }, {
         body: null,
         method: 'PUT',
       });
