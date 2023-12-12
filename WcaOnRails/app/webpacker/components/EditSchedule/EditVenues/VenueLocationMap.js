e961a3d889bcce1fddb4a6d5d43663c1af64c462
Gregor Billing <gbilling@worldcubeassociation.org>
2023-12-12 17:15:17 +0100

    Migrate React-Leaflet to v4 and React to v18 (#8612)

--- WcaOnRails/app/webpacker/components/EditSchedule/EditVenues/VenueLocationMap.js
+++ WcaOnRails/app/webpacker/components/EditSchedule/EditVenues/VenueLocationMap.js
@@ -3,83 +3,139 @@ import React, {
   useEffect,
   useMemo,
   useRef,
+  useState,
 } from 'react';
-import { Map, Marker, TileLayer } from 'react-leaflet';
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+} from 'react-leaflet';
 import { toDegrees, toMicrodegrees } from '../../../lib/utils/edit-schedule';
-import InvisibleIFrame from './InvisibleIFrame';
 import { userTileProvider } from '../../../lib/leaflet-wca/providers';
 import { useDispatch } from '../../../lib/providers/StoreProvider';
 import { editVenue } from '../store/actions';
 
+function GeoSearchControl({
+  onGeoSearchResult,
+}) {
+  const map = useMap();
+
+  useEffect(() => {
+    const searchControl = window.wca.createSearchInput(map);
+
+    map.on('geosearch/showlocation', onGeoSearchResult);
+    map.zoomControl.setPosition('bottomright');
+
+    return () => {
+      map.removeControl(searchControl);
+    };
+  }, [map, onGeoSearchResult]);
+
+  return null;
+}
+
+export function DraggableMarker({
+  position,
+  setPosition,
+  disabled = false,
+  markerRef = null,
+  children,
+}) {
+  const updatePosition = useCallback((e) => setPosition(e, e.target.getLatLng()), [setPosition]);
+
+  return (
+    <Marker
+      ref={markerRef}
+      position={position}
+      draggable={!disabled}
+      eventHandlers={{
+        dragend: updatePosition,
+      }}
+    >
+      {children}
+    </Marker>
+  );
+}
+
+function ResizeMapIFrame() {
+  const map = useMap();
+  const iframeRef = useRef();
+
+  const onResizeAction = useCallback(() => map.invalidateSize(false), [map]);
+
+  useEffect(() => {
+    const iframeNode = iframeRef.current;
+
+    iframeNode.contentWindow.addEventListener('resize', onResizeAction);
+
+    return () => {
+      iframeNode.contentWindow.removeEventListener('resize', onResizeAction);
+    };
+  }, [iframeRef, onResizeAction]);
+
+  return (
+    <iframe
+      title="invisibleIFrame"
+      src="about:blank"
+      className="invisible-iframe-map"
+      ref={iframeRef}
+    />
+  );
+}
+
 function VenueLocationMap({
   venue,
 }) {
-  const mapElem = useRef();
-  const markerElem = useRef();
-
   const dispatch = useDispatch();
+  const markerRef = useRef();
+
+  const [searchResultPopup, setSearchResultPopup] = useState();
+
+  const markerPopup = useMemo(() => {
+    if (searchResultPopup) {
+      return <Popup>{searchResultPopup}</Popup>;
+    }
+
+    return null;
+  }, [searchResultPopup]);
 
   const mapPosition = useMemo(() => ({
     lat: toDegrees(venue.latitudeMicrodegrees),
     lng: toDegrees(venue.longitudeMicrodegrees),
   }), [venue.latitudeMicrodegrees, venue.longitudeMicrodegrees]);
 
+  const setMapPosition = useCallback((evt, { lat, lng }) => {
+    dispatch(editVenue(venue.id, 'latitudeMicrodegrees', toMicrodegrees(lat)));
+    dispatch(editVenue(venue.id, 'longitudeMicrodegrees', toMicrodegrees(lng)));
+  }, [dispatch, venue.id]);
+
   const provider = userTileProvider;
 
-  const invalidateSize = () => {
-    mapElem.current.leafletElement.invalidateSize(false);
-  };
-
-  const onGeoSearchResult = useCallback((result) => {
-    const marker = markerElem.current.leafletElement;
-
-    marker.setLatLng({
-      lat: result.location.y,
-      lng: result.location.x,
+  const onGeoSearchResult = useCallback((evt) => {
+    setMapPosition(evt, {
+      lat: evt.location.y,
+      lng: evt.location.x,
     });
 
-    marker
-      .bindPopup(result.location.label)
-      .openPopup();
-  }, [markerElem]);
-
-  useEffect(() => {
-    const map = mapElem.current.leafletElement;
-
-    window.wca.createSearchInput(map);
-
-    map.on('geosearch/showlocation', onGeoSearchResult);
-    map.zoomControl.setPosition('bottomright');
-  }, [mapElem, onGeoSearchResult]);
-
-  const onPositionChange = (evt) => {
-    /* eslint-disable-next-line */
-    const pos = evt.target._latlng;
-
-    const newLat = toMicrodegrees(pos.lat);
-    const newLng = toMicrodegrees(pos.lng);
-
-    dispatch(editVenue(venue.id, 'latitudeMicrodegrees', newLat));
-    dispatch(editVenue(venue.id, 'longitudeMicrodegrees', newLng));
-  };
+    setSearchResultPopup(evt.location.label);
+  }, [setMapPosition, setSearchResultPopup]);
 
   return (
-    <Map
+    <MapContainer
       center={mapPosition}
       zoom={16}
       scrollWheelZoom={false}
-      ref={mapElem}
       style={{ height: '100%' }}
     >
-      <InvisibleIFrame onResizeAction={invalidateSize} />
+      <ResizeMapIFrame />
       <TileLayer url={provider.url} attribution={provider.attribution} />
-      <Marker
-        position={mapPosition}
-        draggable
-        onMove={onPositionChange}
-        ref={markerElem}
-      />
-    </Map>
+      <GeoSearchControl onGeoSearchResult={onGeoSearchResult} />
+      <DraggableMarker markerRef={markerRef} position={mapPosition} setPosition={setMapPosition}>
+        {markerPopup}
+      </DraggableMarker>
+    </MapContainer>
   );
 }
 
