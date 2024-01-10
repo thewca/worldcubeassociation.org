da417540a2742631dd0fd1d41761fceaad18aea9
notrichardpeng <rpeng@worldcubeassociation.org>
2024-01-10 01:11:43 -0800

    Simplify createSearchParams

--- WcaOnRails/app/webpacker/components/CompetitionOverview/queryUtils.js
+++ WcaOnRails/app/webpacker/components/CompetitionOverview/queryUtils.js
@@ -18,25 +18,29 @@ export function calculateQueryKey(filterState) {
 }
 
 export function createSearchParams(filterState, pageParam) {
+  const {
+    region, delegate, search, timeOrder, selectedYear, customStartDate, customEndDate,
+  } = filterState;
+
   const dateNow = new Date();
   const searchParams = new URLSearchParams({});
 
-  if (filterState.region && filterState.region !== 'all_regions') {
-    const regionParam = isContinent(filterState.region[0]) ? 'continent' : 'country_iso2';
-    searchParams.append(regionParam, filterState.region);
+  if (region && region !== 'all_regions') {
+    const regionParam = isContinent(region) ? 'continent' : 'country_iso2';
+    searchParams.append(regionParam, region);
   }
-  if (filterState.delegate) {
-    searchParams.append('delegate', filterState.delegate);
+  if (delegate) {
+    searchParams.append('delegate', delegate);
   }
-  if (filterState.search) {
-    searchParams.append('q', filterState.search);
+  if (search) {
+    searchParams.append('q', search);
   }
 
-  if (filterState.timeOrder === 'present') {
+  if (timeOrder === 'present') {
     searchParams.append('sort', 'start_date,end_date,name');
     searchParams.append('ongoing_and_future', dateNow.toISOString().split('T')[0]);
     searchParams.append('page', pageParam);
-  } else if (filterState.timeOrder === 'recent') {
+  } else if (timeOrder === 'recent') {
     const thirtyDaysAgo = new Date();
     thirtyDaysAgo.setDate(dateNow.getDate() - 30);
 
@@ -44,24 +48,24 @@ export function createSearchParams(filterState, pageParam) {
     searchParams.append('start', thirtyDaysAgo.toISOString().split('T')[0]);
     searchParams.append('end', dateNow.toISOString().split('T')[0]);
     searchParams.append('page', pageParam);
-  } else if (filterState.timeOrder === 'past') {
-    if (filterState.selectedYear === 'all_years') {
+  } else if (timeOrder === 'past') {
+    if (selectedYear === 'all_years') {
       searchParams.append('sort', '-end_date,-start_date,name');
       searchParams.append('end', dateNow.toISOString().split('T')[0]);
       searchParams.append('page', pageParam);
     } else {
       searchParams.append('sort', '-end_date,-start_date,name');
-      searchParams.append('start', `${filterState.selectedYear}-1-1`);
-      searchParams.append('end', dateNow.getFullYear() === filterState.selectedYear ? dateNow.toISOString().split('T')[0] : `${filterState.selectedYear}-12-31`);
+      searchParams.append('start', `${selectedYear}-1-1`);
+      searchParams.append('end', dateNow.getFullYear() === selectedYear ? dateNow.toISOString().split('T')[0] : `${selectedYear}-12-31`);
       searchParams.append('page', pageParam);
     }
-  } else if (filterState.timeOrder === 'by_announcement') {
+  } else if (timeOrder === 'by_announcement') {
     searchParams.append('sort', '-announced_at,name');
     searchParams.append('page', pageParam);
-  } else if (filterState.timeOrder === 'custom') {
+  } else if (timeOrder === 'custom') {
     searchParams.append('sort', 'start_date,end_date,name');
-    searchParams.append('start', filterState.customStartDate?.toISOString().split('T')[0] || '');
-    searchParams.append('end', filterState.customEndDate?.toISOString().split('T')[0] || '');
+    searchParams.append('start', customStartDate?.toISOString().split('T')[0] || '');
+    searchParams.append('end', customEndDate?.toISOString().split('T')[0] || '');
     searchParams.append('page', pageParam);
   }
 
