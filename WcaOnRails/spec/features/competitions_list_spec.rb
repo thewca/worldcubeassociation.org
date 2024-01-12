fd9d6f0c756724653256abeec1ae237fc78699f0
notrichardpeng <rpeng@worldcubeassociation.org>
2024-01-12 00:40:17 -0800

    Attempt to fix test

--- WcaOnRails/spec/features/competitions_list_spec.rb
+++ WcaOnRails/spec/features/competitions_list_spec.rb
@@ -8,7 +8,13 @@ RSpec.feature "Competitions list" do
       let(:competition) { FactoryBot.create :competition, :visible, :future }
       let(:delegate) { competition.delegates.first }
 
-      before { visit "/competitions?delegate=#{delegate.id}" }
+      before do
+        visit "/competitions"
+        within(:css, "#delegate") do
+          find(".search").set(delegate.name)
+          find(".search").send_keys(:enter)
+        end
+      end
 
       it "the delegate is selected within the form", js: true do
         expect(page.find("#competition-query-form #delegate")).to have_text(delegate.name)
