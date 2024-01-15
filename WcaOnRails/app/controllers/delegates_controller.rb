f31c2c0e3dd1ac959038065bab7fe03da8b672ff
Daniel James <djdany444@gmail.com>
2024-01-15 07:47:15 +0530

    Rebase changes

--- WcaOnRails/app/controllers/delegates_controller.rb
+++ WcaOnRails/app/controllers/delegates_controller.rb
@@ -2,28 +2,10 @@
 
 class DelegatesController < ApplicationController
   before_action :authenticate_user!
-  before_action -> { redirect_to_root_unless_user(:can_manage_delegate_probation?) }, only: [:probations]
   before_action :current_user_can_manage_delegate_probation!, only: [:end_delegate_probation]
   private def current_user_can_manage_delegate_probation!
     unless current_user.can_manage_delegate_probation?
       render json: {}, status: 401
-    end
-  end
-
-  def start_delegate_probation
-    respond_to do |format|
-      format.json do
-        user_id = params[:userId]
-        role = UserRole.create!(
-          user_id: user_id,
-          group_id: UserGroup.find_by!(name: "Delegate Probation").id,
-          start_date: Date.today,
-        )
-        RoleChangeMailer.notify_role_start(role, current_user).deliver_later
-        render json: {
-          success: true,
-        }
-      end
     end
   end
 
