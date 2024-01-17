d8014c287da0cdcfdf057b493b09cefae0ab30fd
FinnIckler <finnickler@gmail.com>
2024-01-17 14:30:00 +0100

    generate migrations

--- WcaOnRails/config/initializers/doorkeeper_openid_connect.rb
+++ WcaOnRails/config/initializers/doorkeeper_openid_connect.rb
@@ -4,8 +4,8 @@ Doorkeeper::OpenidConnect.configure do
     resource_owner.id
   end
   signing_key AppSecrets.OIDC_SECRET_KEY
-  # This is an asymmetric enccryption, meaning the clients validating the JWT Token use the Public key instead
-  signing_algorithm = :rs256
+  # This is an asymmetric encryption, meaning the clients validating the JWT Token use the Public key instead
+  signing_algorithm :rs256
 
   resource_owner_from_access_token do |access_token|
     User.find(access_token.resource_owner_id)
