eff32e5121404c78da22c225fd800ba6cb325441
MEENAKSHI MURALIDHARAN <55012076+MibettyMeanie@users.noreply.github.com>
2024-01-21 16:54:31 +0000

    added year and region select

--- WcaOnRails/app/webpacker/components/Media/MediaTable.jsx
+++ WcaOnRails/app/webpacker/components/Media/MediaTable.jsx
@@ -1,15 +1,38 @@
 import React, { useEffect, useState } from 'react';
 
 import _ from 'lodash';
-import { Table, Confirm,Modal,Button } from 'semantic-ui-react';
+import { Table, Confirm, Modal, Button, GridRow } from 'semantic-ui-react';
 
 import { competitionUrl } from '../../lib/requests/routes.js.erb';
-import { countries } from '../../lib/wca-data.js.erb';
+import { countries, continents, years } from '../../lib/wca-data.js.erb';
 import { dateRangeBetween } from '../../lib/helpers/media-table';
 import useLoadedData from '../../lib/hooks/useLoadedData';
 import useSaveAction from '../../lib/hooks/useSaveAction';
-
+import {
+  DropdownMenu,
+  DropdownItem,
+  DropdownHeader,
+  DropdownDivider,
+  Dropdown,
+} from 'semantic-ui-react'
 export default function MediaTable({ isValidate }) {
+  const countryOptions = _.map(countries.byIso2, (country) => ({
+    key: country.iso2,
+    text: country.name,
+    value: country.iso2,
+  }));
+  const continentOptions = continents.map((continent) => ({
+    key: continent.id,
+    text: continent.name,
+    value: continent.name,
+  }));
+  const yearOptions = years.map((year) => ({
+    key: year,
+    text: year,
+    value: year,
+  }));
+
+
   const handleUpdateSuccess = () => {
     setModalOpen(true);
   };
@@ -22,7 +45,7 @@ export default function MediaTable({ isValidate }) {
   const [mediaCombined, setMediaCombined] = useState([])
   const type = isValidate ? 'pending' : 'accepted'
   const loadUrl = `/api/v0/media?status=${type}`
-  const { loading, error, data,sync } = useLoadedData(
+  const { loading, error, data, sync } = useLoadedData(
     loadUrl,
   );
   const [modalOpen, setModalOpen] = useState(false);
@@ -59,8 +82,9 @@ export default function MediaTable({ isValidate }) {
   });
   const confimMedia = () => {
     const url = "/api/v0/media/" + modalParams.mediaId
-    save(url, { status: modalParams.status }, ()=>{sync();handleUpdateSuccess();setModalParams({ ...modalParams, open: false })
-  }, { method: 'PATCH' });
+    save(url, { status: modalParams.status }, () => {
+      sync(); handleUpdateSuccess(); setModalParams({ ...modalParams, open: false })
+    }, { method: 'PATCH' });
   };
   const [sortColumn, setSortColumn] = useState(null);
   const [sortDirection, setSortDirection] = useState(null);
@@ -75,97 +99,118 @@ export default function MediaTable({ isValidate }) {
     }
   };
   return (
-    <Table sortable celled fixed>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell
-            sorted={sortColumn === 'timestampSubmitted' ? sortDirection : null}
-            onClick={handleSort('timestampSubmitted')}
-          >
-            Submission Date
-          </Table.HeaderCell>
-          <Table.HeaderCell
-            sorted={sortColumn === 'timestampDecided' ? sortDirection : null}
-            onClick={handleSort('timestampDecided')}
-          >
-            Competition Date
-          </Table.HeaderCell>
-          <Table.HeaderCell
-            sorted={sortColumn === 'competetion' ? sortDirection : null}
-            onClick={handleSort('competetion')}
-          >{I18n.t('activerecord.attributes.competition_medium.competitionId')}</Table.HeaderCell>
-          <Table.HeaderCell
-            sorted={sortColumn === 'location' ? sortDirection : null}
-            onClick={handleSort('location')}
-          >{I18n.t('media.media_table.location')}</Table.HeaderCell>
-          <Table.HeaderCell
-            sorted={sortColumn === 'type' ? sortDirection : null}
-            onClick={handleSort('type')}
-          >{I18n.t('activerecord.attributes.competition_medium.type')}</Table.HeaderCell>
-          <Table.HeaderCell
-            sorted={sortColumn === 'link' ? sortDirection : null}
-            onClick={handleSort('link')}>{I18n.t('activerecord.attributes.competition_medium.uri')}</Table.HeaderCell>
-          {isValidate && <Table.HeaderCell></Table.HeaderCell>}
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
+    <div>
+      <Dropdown placeholder='Select Year'
+        search selection options={yearOptions} />
+      <Dropdown
+        placeholder='Select Country'
+        search
+        selection
+      >
+        <DropdownMenu>
+          <DropdownHeader>Continent</DropdownHeader>
+          {continentOptions && continentOptions.map((continent) => (
+            <DropdownItem key={continent.id}>{continent.text}</DropdownItem>
+          ))}
+          <DropdownDivider />
+          <DropdownHeader>Region</DropdownHeader>
+          {countryOptions && countryOptions.map((country) => (
+            <DropdownItem key={country.key}>{country.text}</DropdownItem>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
 
-        {mediaCombined?.map((media_row) => (
+      <Table sortable celled fixed>
+        <Table.Header>
           <Table.Row>
-            <Table.Cell>
-              {moment.utc(media_row.timestampSubmitted).format('MMMM DD, YYYY HH:mm UTC')}
-            </Table.Cell>
-            <Table.Cell>
-              {media_row.competetionDate}
-            </Table.Cell>
-            <Table.Cell>
-              <a href={media_row.competetionUrl}>{media_row.name}</a>
-            </Table.Cell>
-            <Table.Cell>
-              {media_row.country_name}
-              ,
-              {media_row.city}
-            </Table.Cell>
-            <Table.Cell>
-              {media_row.type}
-            </Table.Cell>
-            <Table.Cell>
-              <a href={media_row.uri}>{media_row.text}</a>
-            </Table.Cell>
-            {isValidate &&
-              <Table.Cell>
-                <a href="#" onClick={() => setModalParams({ open: true, mediaId: media_row.mediaId, status: "accepted" })}>
-                  <i className="check icon"></i>
-                </a>
-                <a href={`/media/${media_row.mediaId}/edit`}>
-                  <i class="edit icon"></i>
-                </a>
-                <a href="#" onClick={() => setModalParams({ open: true, mediaId: media_row.mediaId, status: "rejected" })}>
-                  <i className="trash icon"></i>
-                </a>
-              </Table.Cell>
-            }
+            <Table.HeaderCell
+              sorted={sortColumn === 'timestampSubmitted' ? sortDirection : null}
+              onClick={handleSort('timestampSubmitted')}
+            >
+              Submission Date
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={sortColumn === 'timestampDecided' ? sortDirection : null}
+              onClick={handleSort('timestampDecided')}
+            >
+              Competition Date
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={sortColumn === 'competetion' ? sortDirection : null}
+              onClick={handleSort('competetion')}
+            >{I18n.t('activerecord.attributes.competition_medium.competitionId')}</Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={sortColumn === 'location' ? sortDirection : null}
+              onClick={handleSort('location')}
+            >{I18n.t('media.media_table.location')}</Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={sortColumn === 'type' ? sortDirection : null}
+              onClick={handleSort('type')}
+            >{I18n.t('activerecord.attributes.competition_medium.type')}</Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={sortColumn === 'link' ? sortDirection : null}
+              onClick={handleSort('link')}>{I18n.t('activerecord.attributes.competition_medium.uri')}</Table.HeaderCell>
+            {isValidate && <Table.HeaderCell></Table.HeaderCell>}
           </Table.Row>
-        ))}
-        <Confirm
-          open={modalParams.open}
-          onCancel={() => setConfirmOpen(false)}
-          onConfirm={() => confimMedia()}
-          content={`Are you sure you want to ${modalParams.status === 'accepted' ? 'approve' : 'reject'} this media?`}
-        />
-        <Modal open={modalOpen} onClose={handleCloseModal}>
-        <Modal.Header>Update Successful</Modal.Header>
-        <Modal.Content>
-          <p>Media {modalParams.status} successfully!</p>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button positive onClick={handleCloseModal}>
-            OK
-          </Button>
-        </Modal.Actions>
-      </Modal>
-      </Table.Body>
-    </Table>
+        </Table.Header>
+        <Table.Body>
 
+          {mediaCombined?.map((media_row) => (
+            <Table.Row>
+              <Table.Cell>
+                {moment.utc(media_row.timestampSubmitted).format('MMMM DD, YYYY HH:mm UTC')}
+              </Table.Cell>
+              <Table.Cell>
+                {media_row.competetionDate}
+              </Table.Cell>
+              <Table.Cell>
+                <a href={media_row.competetionUrl}>{media_row.name}</a>
+              </Table.Cell>
+              <Table.Cell>
+                {media_row.country_name}
+                ,
+                {media_row.city}
+              </Table.Cell>
+              <Table.Cell>
+                {media_row.type}
+              </Table.Cell>
+              <Table.Cell>
+                <a href={media_row.uri}>{media_row.text}</a>
+              </Table.Cell>
+              {isValidate &&
+                <Table.Cell>
+                  <a href="#" onClick={() => setModalParams({ open: true, mediaId: media_row.mediaId, status: "accepted" })}>
+                    <i className="check icon"></i>
+                  </a>
+                  <a href={`/media/${media_row.mediaId}/edit`}>
+                    <i class="edit icon"></i>
+                  </a>
+                  <a href="#" onClick={() => setModalParams({ open: true, mediaId: media_row.mediaId, status: "rejected" })}>
+                    <i className="trash icon"></i>
+                  </a>
+                </Table.Cell>
+              }
+            </Table.Row>
+          ))}
+          <Confirm
+            open={modalParams.open}
+            onCancel={() => setConfirmOpen(false)}
+            onConfirm={() => confimMedia()}
+            content={`Are you sure you want to ${modalParams.status === 'accepted' ? 'approve' : 'reject'} this media?`}
+          />
+          <Modal open={modalOpen} onClose={handleCloseModal}>
+            <Modal.Header>Update Successful</Modal.Header>
+            <Modal.Content>
+              <p>Media {modalParams.status} successfully!</p>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button positive onClick={handleCloseModal}>
+                OK
+              </Button>
+            </Modal.Actions>
+          </Modal>
+        </Table.Body>
+      </Table>
+    </div>
   );
 }
