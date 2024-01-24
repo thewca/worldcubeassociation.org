24efdbba178de7daa53706db38a801f4b75d8ea5
FinnIckler <finnickler@gmail.com>
2024-01-24 22:12:06 +0100

    move documents to s3

--- WcaOnRails/app/helpers/documents_helper.rb
+++ WcaOnRails/app/helpers/documents_helper.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
-
+require 'aws-sdk-s3'
 module DocumentsHelper
   def documents_list(directory)
-    safe_join Dir.glob("#{Rails.root}/public/documents/#{directory}/*.pdf")
-                 .map { |doc| File.basename(doc, ".pdf") }
-                 .map { |name| content_tag(:li, link_to(name, "/documents/#{directory}/#{name}.pdf")) }
+    s3 = Aws::S3::Resource.new(
+      region: EnvConfig.STORAGE_AWS_REGION,
+      credentials: Aws::InstanceProfileCredentials.new
+    )
+
+    bucket_name = 'wca-documents'
+    prefix = "documents/#{directory}/"
+
+    s3.bucket(bucket_name).objects(prefix: prefix).map do |object|
+      name = File.basename(object.key, ".pdf")
+      content_tag(:li, link_to(name, "https://documents.worldcubeassociation.org/#{object.key}"))
+    end
   end
 end
