thewca/worldcubeassociation.org

commit e2b01d54ddcdb9257cad2e517fdd1655c65a4aca

Switch payload -> selectedActivity

WcaOnRails/app/webpacker/components/EditSchedule/store/reducer.js
@@ -67,7 +67,7 @@ const reducers = {
           rooms: venue.rooms.map((room) => ({
             ...room,
             activities: room.activities.map((activity) => (
-              (activity.id === payload.activityId || (
+              (activity.id === selectedActivity.activityId || (
                 payload.updateMatches && doActivitiesMatch(activity, selectedActivity)
               ))
                 ? { ...activity, [payload.key]: payload.value }
@@ -113,7 +113,7 @@ const reducers = {
           rooms: venue.rooms.map((room) => ({
             ...room,
             activities: room.activities.map((activity) => (
-              (activity.id === payload.activityId || (
+              (activity.id === selectedActivity.activityId || (
                 payload.updateMatches && doActivitiesMatch(activity, selectedActivity)
               ))
                 ? moveActivityByDuration(activity, payload.isoDuration)
@@ -137,7 +137,7 @@ const reducers = {
           rooms: venue.rooms.map((room) => ({
             ...room,
             activities: room.activities.map((activity) => (
-              (activity.id === payload.activityId || (
+              (activity.id === selectedActivity.activityId || (
                 payload.updateMatches && doActivitiesMatch(activity, selectedActivity)
               ))
                 ? scaleActivitiesByDuration(activity, payload.isoDeltaStart, payload.isoDeltaEnd)

commit 695059c49095d1a88e6390ab0151a40e74d5bcd5

Fix bug introduced in e2b01d54ddcdb9257cad2e517fdd1655c65a4aca

WcaOnRails/app/webpacker/components/EditSchedule/store/reducer.js
@@ -67,7 +67,7 @@ const reducers = {
           rooms: venue.rooms.map((room) => ({
             ...room,
             activities: room.activities.map((activity) => (
-              (activity.id === selectedActivity.activityId || (
+              (activity.id === selectedActivity.id || (
                 payload.updateMatches && doActivitiesMatch(activity, selectedActivity)
               ))
                 ? { ...activity, [payload.key]: payload.value }
@@ -113,7 +113,7 @@ const reducers = {
           rooms: venue.rooms.map((room) => ({
             ...room,
             activities: room.activities.map((activity) => (
-              (activity.id === selectedActivity.activityId || (
+              (activity.id === selectedActivity.id || (
                 payload.updateMatches && doActivitiesMatch(activity, selectedActivity)
               ))
                 ? moveActivityByDuration(activity, payload.isoDuration)
@@ -137,7 +137,7 @@ const reducers = {
           rooms: venue.rooms.map((room) => ({
             ...room,
             activities: room.activities.map((activity) => (
-              (activity.id === selectedActivity.activityId || (
+              (activity.id === selectedActivity.id || (
                 payload.updateMatches && doActivitiesMatch(activity, selectedActivity)
               ))
                 ? scaleActivitiesByDuration(activity, payload.isoDeltaStart, payload.isoDeltaEnd)