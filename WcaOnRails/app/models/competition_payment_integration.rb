1ded7723900d4624ab64d0c113cb2d477e3409da
Duncan <duncanonthejob@gmail.com>
2024-01-31 16:25:14 +0200

    frontend, start of enums implementation (not yet working)

--- WcaOnRails/app/models/competition_payment_integration.rb
+++ WcaOnRails/app/models/competition_payment_integration.rb
@@ -5,23 +5,19 @@ class CompetitionPaymentIntegration < ApplicationRecord
 
   belongs_to :competition
 
-  # Refactor this to an integration_mapping
-  INTEGRATION_NAMES = ['paypal'].freeze
+  # enum connected_account_type: {
+  #   'paypal' => 'ConnectedPaypalAccount',
+  # }
 
   def self.paypal_connected?(competition)
-    competition.competition_payment_integrations.exists?(connected_account_type: 'ConnectedPaypalAccount')
+    competition.competition_payment_integrations.paypal.exists?
   end
 
   # TODO: Add tests for case where integration isn't found
   def self.disconnect(competition, integration_name)
-    raise ArgumentError("Invalid status. Allowed values are: #{INTEGRATION_NAMES.join(', ')}") unless INTEGRATION_NAMES.include?(integration_name)
+    raise ArgumentError("Invalid status. Allowed values are: #{connected_account_types.keys.join(', ')}") unless
+      connected_account_types.keys.include?(integration_name)
 
-    if integration_name == 'paypal'
-      integration = competition.competition_payment_integrations.find_by(connected_account_type: 'ConnectedPaypalAccount')
-      connected_account = integration.connected_account
-      connected_account.destroy if connected_account
-    end
-
-    integration.destroy if integration
+    competition.competition_payment_integrations.destory_by(connected_account_type: integration_name)
   end
 end
