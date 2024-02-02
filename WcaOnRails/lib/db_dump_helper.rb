2a07047578534845308d1ecac81209faab421e9b
FinnIckler <finnickler@gmail.com>
2024-02-02 12:14:39 +0100

    run rubocop

--- WcaOnRails/lib/db_dump_helper.rb
+++ WcaOnRails/lib/db_dump_helper.rb
@@ -90,7 +90,7 @@ module DbDumpHelper
       bucket = Aws::S3::Resource.new(
         region: EnvConfig.STORAGE_AWS_REGION,
         credentials: Aws::InstanceProfileCredentials.new,
-        ).bucket(BUCKET_NAME)
+      ).bucket(BUCKET_NAME)
       bucket.object(s3_path).upload_file(zip_filename, { acl: "public-read" })
     end
   end
