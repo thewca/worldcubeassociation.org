f2add940d53efff611cbe70e2df327270fa26f6d
FinnIckler <finnickler@gmail.com>
2024-02-08 11:35:28 +0100

    add wkhtmltopdf-binary gem

--- WcaOnRails/config/initializers/wicked_pdf.rb
+++ WcaOnRails/config/initializers/wicked_pdf.rb
@@ -13,9 +13,8 @@
 WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
-  exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
