77abb23b9ac76d2dfcd7c0d51e68b1c630a2e220
Gregor Billing <gbilling@worldcubeassociation.org>
2024-02-09 17:26:59 +0100

    Switch to maintained fork of Wkhtmlpdf-binary gem (#8897)

--- WcaOnRails/config/initializers/wicked_pdf.rb
+++ WcaOnRails/config/initializers/wicked_pdf.rb
@@ -15,7 +15,7 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf'),
+  exe_path: Gem.bin_path('wkhtmltopdf-binary-ng', 'wkhtmltopdf'),
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
