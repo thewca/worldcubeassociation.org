f36c9a43b6030b216f28acdbd9a0bd7343371803
FinnIckler <finnickler@gmail.com>
2024-02-12 20:09:03 +0100

    run rubocop

--- WcaOnRails/spec/controllers/api/internal/v1/api_internal_users_controller_spec.rb
+++ WcaOnRails/spec/controllers/api/internal/v1/api_internal_users_controller_spec.rb
@@ -11,25 +11,25 @@ RSpec.describe Api::Internal::V1::UsersController do
       allow(controller).to receive(:validate_token).and_return(true)
     end
     context 'returns the correct pii' do
-      get :competitor_info, params: { ids: [user1.id, user2.id]}
+      get :competitor_info, params: { ids: [user1.id, user2.id] }
 
       expect(response.body).to eq([{
-                                     :id => user1.id,
-                                     :wca_id => user1.wca_id,
-                                     :name => user1.name,
-                                     :gender => user1.gender,
-                                     :country_iso2 => user1.country_iso2,
-                                     :email => "user1@example.com",
-                                     :dob => "1-1-2000",
-                                   },
+        id: user1.id,
+        wca_id: user1.wca_id,
+        name: user1.name,
+        gender: user1.gender,
+        country_iso2: user1.country_iso2,
+        email: "user1@example.com",
+        dob: "1-1-2000",
+      },
                                    {
-                                     :id => user2.id,
-                                     :wca_id => user2.wca_id,
-                                     :name => user2.name,
-                                     :gender => user2.gender,
-                                     :country_iso2 => user2.country_iso2,
-                                     :email => "user2@example.com",
-                                     :dob => "1-1-2001",
+                                     id: user2.id,
+                                     wca_id: user2.wca_id,
+                                     name: user2.name,
+                                     gender: user2.gender,
+                                     country_iso2: user2.country_iso2,
+                                     email: "user2@example.com",
+                                     dob: "1-1-2001",
                                    }].to_json)
     end
   end
