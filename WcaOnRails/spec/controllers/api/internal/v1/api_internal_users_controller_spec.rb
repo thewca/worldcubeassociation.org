4898ee7f88a84cb67146948647da30b027e8dd8e
FinnIckler <finnickler@gmail.com>
2024-02-13 16:04:16 +0100

    symbolize names in json

--- WcaOnRails/spec/controllers/api/internal/v1/api_internal_users_controller_spec.rb
+++ WcaOnRails/spec/controllers/api/internal/v1/api_internal_users_controller_spec.rb
@@ -13,28 +13,27 @@ RSpec.describe Api::Internal::V1::UsersController do
     it 'returns the correct pii' do
       get :competitor_info, params: { ids: [user1.id, user2.id] }
 
-      parsed_body = JSON.parse(response.body)
+      parsed_body = JSON.parse(response.body, symbolize_names: true)
 
       expect(parsed_body).to eq([{
-        id: user1.id,
-        wca_id: user1.wca_id,
-        name: user1.name,
-        gender: user1.gender,
-        country_iso2: user1.country_iso2,
-        email: "user1@example.com",
-        dob: "2000-01-01",
-        class: "user",
-      }.stringify_keys,
-                                 {
-                                   id: user2.id,
-                                   wca_id: user2.wca_id,
-                                   name: user2.name,
-                                   gender: user2.gender,
-                                   country_iso2: user2.country_iso2,
-                                   email: "user2@example.com",
-                                   dob: "2001-01-01",
-                                   class: "user",
-                                 }.stringify_keys])
+                                  id: user1.id,
+                                  wca_id: user1.wca_id,
+                                  name: user1.name,
+                                  gender: user1.gender,
+                                  country_iso2: user1.country_iso2,
+                                  email: "user1@example.com",
+                                  dob: "2000-01-01",
+                                  class: "user",
+                                }, {
+                                  id: user2.id,
+                                  wca_id: user2.wca_id,
+                                  name: user2.name,
+                                  gender: user2.gender,
+                                  country_iso2: user2.country_iso2,
+                                  email: "user2@example.com",
+                                  dob: "2001-01-01",
+                                  class: "user",
+                                }])
     end
   end
 end
