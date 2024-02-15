13e928e00af059c4ff1660127635abd3e8744362
Duncan <duncanonthejob@gmail.com>
2024-02-15 09:24:01 +0200

    define date range using ... in clear payment integration job

--- WcaOnRails/app/jobs/clear_connected_payment_integrations.rb
+++ WcaOnRails/app/jobs/clear_connected_payment_integrations.rb
@@ -4,7 +4,7 @@ class ClearConnectedPaymentIntegrations < WcaCronjob
   DELAY_IN_DAYS = 21
 
   def perform
-    comps_to_disconnect = Competition.where("end_date < ?", DELAY_IN_DAYS.days.ago).joins(:competition_payment_integrations).distinct
+    comps_to_disconnect = Competition.where(end_date: ...DELAY_IN_DAYS.days.ago).joins(:competition_payment_integrations).distinct
     comps_to_disconnect.find_each do |comp|
       comp.disconnect_all_payment_integrations
     end
