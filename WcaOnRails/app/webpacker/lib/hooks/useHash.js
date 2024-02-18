59666bd35ae7d55b6357834ecd27a61ec22349d2
Daniel James <djdany444@gmail.com>
2024-02-19 00:31:51 +0530

    Use hash fix for some rare cases

--- WcaOnRails/app/webpacker/lib/hooks/useHash.js
+++ WcaOnRails/app/webpacker/lib/hooks/useHash.js
@@ -31,6 +31,13 @@ export default function useHash() {
     (newHash) => {
       if (newHash !== hash) {
         window.location.hash = newHash;
+        // The next line may not be actually required. This will anyway get called inside
+        // onHashChange because onHashChange is the listener to browser hash change. But sometimes
+        // the above line gets called before the listener is added (especially when network speed
+        // is less), and in that case it will skip calling setHash and hence leading to unexpected
+        // behavior in UI. Calling this here will make sure to get that called irrespective of
+        // whether listener is added or not and this won't have any negative impact on UI as well.
+        setHash(getHashFromBrowserUrl());
       }
     },
     [hash],
