7aed5f4ec5d60bb566e3f5f38e7613f9b5ea67a6
Finn Ickler <finnickler@gmail.com>
2024-02-24 15:36:12 +0100

    Redirect to v2 import page

--- WcaOnRails/app/controllers/registrations_controller.rb
+++ WcaOnRails/app/controllers/registrations_controller.rb
@@ -116,6 +116,9 @@ class RegistrationsController < ApplicationController
 
   def import
     @competition = competition_from_params
+    if @competition.uses_new_registration_service?
+      redirect_to Microservices.Registrations.registration_import_path(@competition.id)
+    end
   end
 
   def do_import
