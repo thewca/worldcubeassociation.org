38863a040b81bc195e47260ac72d53671bf98f80
Gregor Billing <gbilling@worldcubeassociation.org>
2024-03-07 11:59:23 +0100

    Add explanation comments to Rails black magic

--- app/models/concerns/microservice_registration_holder.rb
+++ app/models/concerns/microservice_registration_holder.rb
@@ -18,10 +18,13 @@ module MicroserviceRegistrationHolder
       raise "Unsupported model #{self.model_name} as MicroserviceRegistrationHolder. Currently supported are: #{Competition.model_name}, #{User.model_name}"
     end
 
-    # Let Rails do its thing via the `has_many` association defined at the top of the file
+    # Let Rails do its thing via the `has_many` association defined at the top of the file, but with a little extra
     super.all.extending do
+      # Tap into the Rails ActiveRecord engine to make sure that when records are actually loaded
+      # (i.e. after querying is completely done and the records are _just about_ to be retrieved from the DB)
       define_method :records do
         super().tap do |ar_models|
+          # we hydrate each model with the microservice information directly from above
           ar_models.each do |ar_model|
             matching_ms_model = ms_models.find { |ms_model| ms_model['competition_id'] == ar_model.competition_id && ms_model['user_id'] == ar_model.user_id }
             raise "No matching Microservice registration found. This should not happen!" unless matching_ms_model.present?
