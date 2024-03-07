25b2cbb82186ea525bf0dde5525e177270320bd2
Gregor Billing <gbilling@worldcubeassociation.org>
2024-03-07 11:30:13 +0100

    Throw error if the data is not loaded correctly

--- app/models/microservice_registration.rb
+++ app/models/microservice_registration.rb
@@ -6,18 +6,42 @@ class MicroserviceRegistration < ApplicationRecord
 
   delegate :name, :email, to: :user
 
-  attr_accessor :status, :event_ids
+  attr_accessor :ms_registration
+  attr_writer :status, :event_ids
 
   def load_ms_model(ms_model)
+    self.ms_registration = ms_model
+
     self.status = ms_model['status']
-    self.event_ids = ms_model['event_ids']
+
+    self.event_ids = ms_model['lanes']&.find do |lane|
+      lane['lane_name'] == 'competing'
+    end&.dig('lane_details', 'event_details')&.pluck('event_id')
+  end
+
+  def ms_loaded?
+    self.ms_registration.present?
+  end
+
+  private def read_ms_data(name_without_at)
+    instance_variable_get(:"@#{name_without_at}").tap do
+      raise "Microservice data not loaded!" unless ms_loaded?
+    end
+  end
+
+  def status
+    self.read_ms_data :status
+  end
+
+  def event_ids
+    self.read_ms_data :event_ids
   end
 
   def accepted?
-    status == "accepted"
+    self.status == "accepted"
   end
 
   def deleted?
-    status == "deleted"
+    self.status == "deleted"
   end
 end
