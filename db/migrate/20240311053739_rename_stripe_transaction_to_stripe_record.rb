809fe7ba5a157f9cac699a2dc82ca96ae8c9f37e
Duncan <duncanonthejob@gmail.com>
2024-03-11 16:57:59 +0200

    changed migration to use ActiveRecord not SQL

--- db/migrate/20240311053739_rename_stripe_transaction_to_stripe_record.rb
+++ db/migrate/20240311053739_rename_stripe_transaction_to_stripe_record.rb
@@ -8,19 +8,11 @@ class RenameStripeTransactionToStripeRecord < ActiveRecord::Migration[7.1]
 
     reversible do |direction|
       direction.up do
-        execute <<-SQL
-          UPDATE registration_payments
-          SET receipt_type = 'StripeRecord'
-          WHERE receipt_type = 'StripeTransaction'
-        SQL
+        RegistrationPayment.where(receipt_type: 'StripeTransaction').update_all(receipt_type: 'StripeRecord')
       end
 
       direction.down do
-        execute <<-SQL
-          UPDATE registration_payments
-          SET receipt_type = 'StripeTransaction'
-          WHERE receipt_type = 'StripeRecord'
-        SQL
+        RegistrationPayment.where(receipt_type: 'StripeRecord').update_all(receipt_type: 'StripeTransaction')
       end
     end
   end
