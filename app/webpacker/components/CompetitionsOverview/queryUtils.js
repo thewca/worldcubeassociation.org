a62195d7b95f0e35ef27319fa28149565135db97
Gregor Billing <gbilling@worldcubeassociation.org>
2024-03-29 20:50:36 +0100

    Use toISODate instead of custom yMd format

--- app/webpacker/components/CompetitionsOverview/queryUtils.js
+++ app/webpacker/components/CompetitionsOverview/queryUtils.js
@@ -42,25 +42,25 @@ export function createSearchParams(filterState, pageParam) {
 
   if (timeOrder === 'present') {
     searchParams.append('sort', 'start_date,end_date,name');
-    searchParams.append('ongoing_and_future', dateNow.toFormat('yyyy-MM-dd'));
+    searchParams.append('ongoing_and_future', dateNow.toISODate());
     searchParams.append('page', pageParam);
   } else if (timeOrder === 'recent') {
     // noinspection JSAnnotator
     const recentDaysAgo = dateNow.minus({ days: competitionConstants.competitionRecentDays });
 
     searchParams.append('sort', '-end_date,-start_date,name');
-    searchParams.append('start', recentDaysAgo.toFormat('yyyy-MM-dd'));
-    searchParams.append('end', dateNow.toFormat('yyyy-MM-dd'));
+    searchParams.append('start', recentDaysAgo.toISODate());
+    searchParams.append('end', dateNow.toISODate());
     searchParams.append('page', pageParam);
   } else if (timeOrder === 'past') {
     if (selectedYear === 'all_years') {
       searchParams.append('sort', '-end_date,-start_date,name');
-      searchParams.append('end', dateNow.toFormat('yyyy-MM-dd'));
+      searchParams.append('end', dateNow.toISODate());
       searchParams.append('page', pageParam);
     } else {
       searchParams.append('sort', '-end_date,-start_date,name');
       searchParams.append('start', `${selectedYear}-1-1`);
-      searchParams.append('end', dateNow.year === selectedYear ? dateNow.toFormat('yyyy-MM-dd') : `${selectedYear}-12-31`);
+      searchParams.append('end', dateNow.year === selectedYear ? dateNow.toISODate() : `${selectedYear}-12-31`);
       searchParams.append('page', pageParam);
     }
   } else if (timeOrder === 'by_announcement') {
@@ -71,8 +71,8 @@ export function createSearchParams(filterState, pageParam) {
     const endLuxon = DateTime.fromISO(customEndDate, { zone: 'UTC' });
 
     searchParams.append('sort', 'start_date,end_date,name');
-    searchParams.append('start', startLuxon.isValid ? startLuxon.toFormat('yyyy-MM-dd') : '');
-    searchParams.append('end', endLuxon.isValid ? endLuxon.toFormat('yyyy-MM-dd') : '');
+    searchParams.append('start', startLuxon.isValid ? startLuxon.toISODate() : '');
+    searchParams.append('end', endLuxon.isValid ? endLuxon.toISODate() : '');
     searchParams.append('page', pageParam);
   }
 
