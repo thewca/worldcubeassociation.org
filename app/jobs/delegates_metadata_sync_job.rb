127e66860bddd10c23d0ea310de2c06049d8e9b5
Daniel James <djdany444@gmail.com>
2024-04-02 15:54:26 +0530

    Review changes

--- app/jobs/delegates_metadata_sync_job.rb
+++ app/jobs/delegates_metadata_sync_job.rb
@@ -8,15 +8,10 @@ class DelegatesMetadataSyncJob < WcaCronjob
         first_delegated = user.actually_delegated_competitions.to_a.minimum(:start_date)
         last_delegated = user.actually_delegated_competitions.to_a.maximum(:start_date)
         total_delegated = user.actually_delegated_competitions.to_a.length
-        if role.metadata.first_delegated != first_delegated
-          role.metadata.update!(first_delegated: first_delegated)
-        end
-        if role.metadata.last_delegated != last_delegated
-          role.metadata.update!(last_delegated: last_delegated)
-        end
-        if role.metadata.total_delegated != total_delegated
-          role.metadata.update!(total_delegated: total_delegated)
-        end
+        role.metadata.first_delegated = first_delegated
+        role.metadata.last_delegated = last_delegated
+        role.metadata.total_delegated = total_delegated
+        role.metadata.save!
       end
     end
   end
