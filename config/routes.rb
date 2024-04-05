58bd79f2cf8780b3b2111ff93074cd5a03219e18
FinnIckler <finnickler@gmail.com>
2024-04-05 09:47:16 +0200

    renamed controller

--- config/routes.rb
+++ config/routes.rb
@@ -334,7 +334,7 @@ Rails.application.routes.draw do
       namespace :v1 do
         get '/users/:id/permissions' => 'permissions#index'
         post '/users/competitor-info' => 'users#competitor_info'
-        post '/mailer/registration' => 'mailer#registration'
+        post '/mailers/registration' => 'mailers#registration'
         post '/payment/init_stripe' => 'payment#init'
       end
     end
