9e0b8c48adde5da023c56476303e76d59172ed55
Simon Kelly <simonkellly@gmail.com>
2024-04-07 17:21:55 +0100

    Switch to using a global react parameter

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -27,6 +27,8 @@ class PersonsController < ApplicationController
   end
 
   def show
+    @enable_react = params[:beta]&.to_s == '0xDbOverload'
+
     @person = Person.current.includes(:user, :ranksSingle, :ranksAverage, :competitions).find_by_wca_id!(params[:id])
     @previous_persons = Person.where(wca_id: params[:id]).where.not(subId: 1).order(:subId)
     @ranks_single = @person.ranksSingle.select { |r| r.event.official? }
