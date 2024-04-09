8fce27ab96519674867fb76d765c123b6f6072cb
Gregor Billing <gbilling@worldcubeassociation.org>
2024-04-09 22:27:10 +0200

    Clean up column names in PaypalRecords table

--- app/models/paypal_record.rb
+++ app/models/paypal_record.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
 class PaypalRecord < ApplicationRecord
-  belongs_to :parent_record, class_name: "PaypalRecord", optional: true
-  has_many :child_records, class_name: "PaypalRecord", inverse_of: :parent_record, foreign_key: :parent_record_id
-
-  RECORD_TYPES = [
-    :payment,
-    :refund,
-    :capture,
-  ].freeze
-
   # Defined in: https://developer.paypal.com/docs/reports/reference/paypal-supported-currencies/
   PAYPAL_CURRENCY_CATEGORIES = {
     decimal: [ # Currencies that should be passed to paypal as decimal amounts (ie, cents/100)
@@ -42,8 +33,34 @@ class PaypalRecord < ApplicationRecord
     ],
   }.freeze
 
+  enum paypal_record_type: {
+    # We cannot call this "order" because that's a reserved keyword in SQL and Rails AR
+    paypal_order: "paypal_order",
+    capture: "capture",
+    refund: "refund",
+  }
+
+  # We don't need the native JSON type on DB level, so we serialize in Ruby.
+  # Also saves us from some pains because JSON columns are highly inconsistent among MySQL and MariaDB.
+  serialize :parameters, coder: JSON
+
+  has_one :registration_payment, as: :receipt
+  has_one :payment_intent, as: :payment_record
+
+  belongs_to :parent_record, class_name: "PaypalRecord", inverse_of: :child_records, optional: true
+  has_many :child_records, class_name: "PaypalRecord", inverse_of: :parent_record, foreign_key: :parent_record_id
+
+  def money_amount
+    ruby_amount = PaypalRecord.amount_to_ruby(
+      self.amount_paypal_denomination,
+      self.currency_code,
+    )
+
+    Money.new(ruby_amount, self.currency_code)
+  end
+
   # Paypal expects a decimal value in the format of a string, so we return a string from this function
-  def self.paypal_amount(amount, currency_code)
+  def self.amount_to_paypal(amount, currency_code)
     if PAYPAL_CURRENCY_CATEGORIES[:decimal].include?(currency_code)
       format("%.2f", amount.to_i / 100.0).to_s
     else
@@ -51,17 +68,34 @@ class PaypalRecord < ApplicationRecord
     end
   end
 
-  def self.ruby_amount(amount, currency_code)
+  def self.amount_to_ruby(amount, currency_code)
     if PAYPAL_CURRENCY_CATEGORIES[:decimal].include?(currency_code)
-      (amount.to_i * 100)
+      (amount.to_f * 100).to_i
     else
       amount.to_i
     end
   end
 
-  def capture_id
-    # NOTE: This only ever returns the id of the first capture associated with a record
-    # TODO: Add error so that this method can only be called for an order or receipt, not for a capture
-    child_records.first.record_id
+  def self.parse_paypal_datetime(raw_datetime)
+    return nil if raw_datetime.nil?
+
+    DateTime.parse(raw_datetime)
+  end
+
+  def self.create_from_api(api_record, record_type, parameters, merchant_id, parent_record = nil)
+    default_unit = api_record.dig('purchase_units', 0) || api_record
+
+    PaypalRecord.create!(
+      paypal_record_type: record_type,
+      parameters: parameters,
+      paypal_id: api_record['id'],
+      amount_paypal_denomination: default_unit['amount']['value'],
+      currency_code: default_unit['amount']['currency_code'],
+      paypal_status: api_record['status'],
+      merchant_id: merchant_id,
+      parent_record: parent_record,
+      created_at_remote: PaypalRecord.parse_paypal_datetime(api_record["create_time"]),
+      updated_at_remote: PaypalRecord.parse_paypal_datetime(api_record["update_time"]),
+    )
   end
 end
