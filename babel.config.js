9709b320cd3404628055e43716815b3669937ca2
Gregor Billing <gbilling@worldcubeassociation.org>
2024-04-10 19:42:33 +0200

    Simplify Babel config by relying on Shakapacker standards

--- babel.config.js
+++ babel.config.js
@@ -1,46 +1,16 @@
 module.exports = function(api) {
-  var validEnv = ['development', 'test', 'production']
-  var currentEnv = api.env()
-  var isDevelopmentEnv = api.env('development')
-  var isProductionEnv = api.env('production')
-  var isTestEnv = api.env('test')
+  const defaultConfigFunc = require('shakapacker/package/babel/preset.js')
+  const resultConfig = defaultConfigFunc(api)
 
-  if (!validEnv.includes(currentEnv)) {
-    throw new Error(
-      'Please specify a valid `NODE_ENV` or ' +
-      '`BABEL_ENV` environment variables. Valid values are "development", ' +
-      '"test", and "production". Instead, received: ' +
-      JSON.stringify(currentEnv) +
-      '.'
-    )
-  }
+  const isProductionEnv = api.env('production')
+  const isDevelopmentEnv = api.env('development')
 
-  return {
+  const changesOnDefault = {
     presets: [
-      isTestEnv && [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current'
-          },
-          modules: 'commonjs'
-        },
-        '@babel/preset-react'
-      ],
-      (isProductionEnv || isDevelopmentEnv) && [
-        '@babel/preset-env',
-        {
-          forceAllTransforms: true,
-          useBuiltIns: 'entry',
-          corejs: 3,
-          modules: false,
-          exclude: ['transform-typeof-symbol']
-        }
-      ],
       [
         '@babel/preset-react',
         {
-          development: isDevelopmentEnv || isTestEnv,
+          development: !isProductionEnv,
           useBuiltIns: true
         }
       ]
@@ -53,51 +23,8 @@ module.exports = function(api) {
           'semantic-css': ([, name]) => `stylesheets/semantic/components${name}.min.css`,
         }
       }],
-      // This cherry-picks needed lodash function from a generic 'import _ from lodash'.
-      'lodash',
-      'babel-plugin-macros',
-      '@babel/plugin-syntax-dynamic-import',
-      isTestEnv && 'babel-plugin-dynamic-import-node',
-      '@babel/plugin-transform-destructuring',
-      [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-transform-object-rest-spread',
-        {
-          useBuiltIns: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          helpers: false,
-          regenerator: true,
-          corejs: false
-        }
-      ],
-      [
-        '@babel/plugin-transform-regenerator',
-        {
-          async: false
-        }
-      ],
-      isProductionEnv && [
+      process.env.WEBPACK_SERVE && 'react-refresh/babel',
+      !isDevelopmentEnv && [
         'babel-plugin-transform-react-remove-prop-types',
         {
           removeImport: true
@@ -105,4 +32,9 @@ module.exports = function(api) {
       ]
     ].filter(Boolean)
   }
+
+  resultConfig.presets = [...resultConfig.presets, ...changesOnDefault.presets]
+  resultConfig.plugins = [...resultConfig.plugins, ...changesOnDefault.plugins ]
+
+  return resultConfig
 }
