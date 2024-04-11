c2458b3823a42cdcf1b40bb1876d94e3125cecd4
Gregor Billing <gbilling@worldcubeassociation.org>
2024-04-11 12:27:48 +0200

    ESLint on server rendering file

--- app/webpacker/packs/server_rendering.js
+++ app/webpacker/packs/server_rendering.js
@@ -1,5 +1,8 @@
 // By default, this pack is loaded for server-side rendering.
 // It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
+const componentRequireContext = require.context('components', true);
+const ReactRailsUJS = require('react_ujs');
+
+// This method "accidentally" sounds like a React Hook, but it's not!
+// eslint-disable-next-line react-hooks/rules-of-hooks
 ReactRailsUJS.useContext(componentRequireContext);
