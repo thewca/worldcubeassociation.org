2593c4145e56523bba4ce022471bb747aaaa2cac
FinnIckler <finnickler@gmail.com>
2024-04-12 10:13:19 -0400

    remove not needed api request

--- app/webpacker/components/RegistrationsV2/api/user/get/get_me.js
+++
@@ -1,7 +0,0 @@
-import externalServiceFetch from '../../helper/external_service_fetch';
-import { apiV0Urls } from '../../../../../lib/requests/routes.js.erb';
-
-export default async function getMe() {
-  const userRequest = await externalServiceFetch(apiV0Urls.users.me.info);
-  return userRequest.user;
-}
--- app/webpacker/components/RegistrationsV2/api/user/get/get_preferred_events.js
+++
@@ -1,6 +0,0 @@
-import externalServiceFetch from '../../helper/external_service_fetch';
-import { apiV0Urls } from '../../../../../lib/requests/routes.js.erb';
-
-export default async function getPreferredEvents() {
-  return externalServiceFetch(apiV0Urls.users.me.preferred_events);
-}
