688a999916b1bdd1e8be92220695a601c06b30e4
Daniel James <djdany444@gmail.com>
2024-04-12 05:51:08 +0530

    Review changes

--- db/migrate/20240411113530_rename_candidate_to_trainee.rb
+++ db/migrate/20240411113530_rename_candidate_to_trainee.rb
@@ -2,8 +2,6 @@
 
 class RenameCandidateToTrainee < ActiveRecord::Migration[7.1]
   def change
-    RolesMetadataDelegateRegions.where(status: 'candidate_delegate').each do |metadata|
-      metadata.update!(status: RolesMetadataDelegateRegions.statuses[:junior_delegate])
-    end
+    RolesMetadataDelegateRegions.where(status: 'candidate_delegate').update_all(status: RolesMetadataDelegateRegions.statuses[:junior_delegate])
   end
 end
