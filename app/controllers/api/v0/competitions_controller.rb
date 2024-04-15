d710c89d3d38c92039c09e5455e7319b79acaaff
Gregor Billing <gbilling@worldcubeassociation.org>
2024-04-15 11:03:35 +0200

    Compute hash without each_with_object

--- app/controllers/api/v0/competitions_controller.rb
+++ app/controllers/api/v0/competitions_controller.rb
@@ -38,11 +38,14 @@ class Api::V0::CompetitionsController < Api::V0::ApiController
   end
 
   def qualifications
-    competition = competition_from_params
-    qualifications = competition.competition_events.each_with_object({}) do |event, hash|
-      next if event.qualification.nil?
-      hash[event.event_id] = event.qualification.to_wcif
-    end
+    competition = competition_from_params(associations: [:competition_events])
+
+    qualifications = competition.competition_events
+                                .where.not(qualification: nil)
+                                .index_by(&:event_id)
+                                .transform_values(&:qualification)
+                                .transform_values(&:to_wcif)
+
     render json: qualifications
   end
 
