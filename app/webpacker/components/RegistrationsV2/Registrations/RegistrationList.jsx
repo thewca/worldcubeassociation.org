cea521d926230e24568118b3db8e01a534714805
FinnIckler <finnickler@gmail.com>
2024-04-24 09:04:53 +0200

    don't use useStore hook for waiting and registrations

--- app/webpacker/components/RegistrationsV2/Registrations/RegistrationList.jsx
+++ app/webpacker/components/RegistrationsV2/Registrations/RegistrationList.jsx
@@ -6,15 +6,15 @@ import React, {
   useReducer,
   useState,
 } from 'react';
-import { Flag, Icon, Table } from 'semantic-ui-react';
+import {
+  Flag, Icon, Message, Table,
+} from 'semantic-ui-react';
 import {
   getConfirmedRegistrations,
   getPsychSheetForEvent,
 } from '../api/registration/get/get_registrations';
 import useWithUserData from '../hooks/useWithUserData';
 import createSortReducer from '../reducers/sortReducer';
-import { setMessage } from '../Register/RegistrationMessage';
-import { useDispatch } from '../../../lib/providers/StoreProvider';
 import Loading from '../../Requests/Loading';
 import EventIcon from '../../wca/EventIcon';
 import { personUrl } from '../../../lib/requests/routes.js.erb';
@@ -77,20 +77,10 @@ function FooterContent({
 }
 
 export default function RegistrationList({ competitionInfo }) {
-  const dispatchStore = useDispatch();
-  const { isLoading: registrationsLoading, data: registrations } = useQuery({
+  const { isLoading: registrationsLoading, data: registrations, isError } = useQuery({
     queryKey: ['registrations', competitionInfo.id],
     queryFn: () => getConfirmedRegistrations(competitionInfo.id),
     retry: false,
-    onError: (data) => {
-      const { error } = data.json;
-      dispatchStore(setMessage(
-        error
-          ? `competitions.registration_v2.errors.${error}`
-          : 'registrations.flash.failed',
-        'negative',
-      ));
-    },
   });
 
   const [state, dispatch] = useReducer(sortReducer, {
@@ -157,167 +147,169 @@ export default function RegistrationList({ competitionInfo }) {
     return [];
   }, [dataWithUser, sortColumn, sortDirection, psychSheetEvent]);
 
+  // eslint-disable-next-line no-nested-ternary
   return registrationsLoading || userInfoLoading ? (
     <Loading />
-  ) : (
-    <div>
-      <Table sortable textAlign="left">
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell
-              sorted={sortColumn === 'name' ? sortDirection : undefined}
-              onClick={() => changeSortColumn('name')}
-            >
-              Name
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={sortColumn === 'country' ? sortDirection : undefined}
-              onClick={() => changeSortColumn('country')}
-            >
-              Citizen Of
-            </Table.HeaderCell>
-            {psychSheetEvent === undefined ? (
-              <>
-                {competitionInfo.event_ids.map((id) => (
+  ) : isError ? <Message> Loading the Registrations failed </Message>
+    : (
+      <div>
+        <Table sortable textAlign="left">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell
+                sorted={sortColumn === 'name' ? sortDirection : undefined}
+                onClick={() => changeSortColumn('name')}
+              >
+                Name
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={sortColumn === 'country' ? sortDirection : undefined}
+                onClick={() => changeSortColumn('country')}
+              >
+                Citizen Of
+              </Table.HeaderCell>
+              {psychSheetEvent === undefined ? (
+                <>
+                  {competitionInfo.event_ids.map((id) => (
+                    <Table.HeaderCell
+                      key={`registration-table-header-${id}`}
+                      onClick={() => setPsychSheetEvent(id)}
+                    >
+                      <EventIcon event={id} className="selected" />
+                    </Table.HeaderCell>
+                  ))}
                   <Table.HeaderCell
-                    key={`registration-table-header-${id}`}
-                    onClick={() => setPsychSheetEvent(id)}
+                    sorted={sortColumn === 'total' ? sortDirection : undefined}
+                    onClick={() => changeSortColumn('total')}
                   >
-                    <EventIcon event={id} className="selected" />
+                    Total
                   </Table.HeaderCell>
-                ))}
-                <Table.HeaderCell
-                  sorted={sortColumn === 'total' ? sortDirection : undefined}
-                  onClick={() => changeSortColumn('total')}
-                >
-                  Total
-                </Table.HeaderCell>
-              </>
-            ) : (
-              <>
-                <Table.HeaderCell
-                  collapsing
-                  onClick={() => setPsychSheetEvent(undefined)}
-                >
-                  <Icon name="backward" />
-                  {' '}
-                  Go back
-                </Table.HeaderCell>
-                <Table.HeaderCell>
-                  <EventIcon event={psychSheetEvent} className="selected" />
-                </Table.HeaderCell>
-                <Table.HeaderCell>
-                  <Icon name="trophy" />
-                  {' '}
-                  WR
-                </Table.HeaderCell>
-                <Table.HeaderCell
-                  sorted={
+                </>
+              ) : (
+                <>
+                  <Table.HeaderCell
+                    collapsing
+                    onClick={() => setPsychSheetEvent(undefined)}
+                  >
+                    <Icon name="backward" />
+                    {' '}
+                    Go back
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <EventIcon event={psychSheetEvent} className="selected" />
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <Icon name="trophy" />
+                    {' '}
+                    WR
+                  </Table.HeaderCell>
+                  <Table.HeaderCell
+                    sorted={
                     psychSheetSortBy === 'single' ? 'ascending' : undefined
                   }
-                  onClick={() => setPsychSheetSortBy('single')}
-                >
-                  Single
-                </Table.HeaderCell>
-                <Table.HeaderCell
-                  sorted={
+                    onClick={() => setPsychSheetSortBy('single')}
+                  >
+                    Single
+                  </Table.HeaderCell>
+                  <Table.HeaderCell
+                    sorted={
                     psychSheetSortBy === 'average' ? 'ascending' : undefined
                   }
-                  onClick={() => setPsychSheetSortBy('average')}
-                >
-                  Average
-                </Table.HeaderCell>
-              </>
-            )}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {data.length > 0 ? (
-            data.map((registration) => (
-              <Table.Row key={`registration-table-row-${registration.user.id}`}>
-                <Table.Cell>
-                  {registration.user.wca_id ? (
-                    <a
-                      href={personUrl(registration.user.wca_id)}
-                    >
-                      {registration.user.name}
-                    </a>
-                  ) : (
-                    registration.user.name
-                  )}
-                </Table.Cell>
-                <Table.Cell>
-                  <Flag
-                    name={registration.user.country.iso2.toLowerCase()}
-                  />
-                  {registration.user.country.name}
-                </Table.Cell>
-                {psychSheetEvent === undefined ? (
-                  <>
-                    {competitionInfo.event_ids.map((id) => (
-                      <Table.Cell
-                        key={`registration-table-row-${registration.user.id}-${id}`}
+                    onClick={() => setPsychSheetSortBy('average')}
+                  >
+                    Average
+                  </Table.HeaderCell>
+                </>
+              )}
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {data.length > 0 ? (
+              data.map((registration) => (
+                <Table.Row key={`registration-table-row-${registration.user.id}`}>
+                  <Table.Cell>
+                    {registration.user.wca_id ? (
+                      <a
+                        href={personUrl(registration.user.wca_id)}
                       >
-                        {registration.competing.event_ids.includes(id) ? (
-                          <EventIcon event={id} className="selected" />
-                        ) : null}
+                        {registration.user.name}
+                      </a>
+                    ) : (
+                      registration.user.name
+                    )}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Flag
+                      name={registration.user.country.iso2.toLowerCase()}
+                    />
+                    {registration.user.country.name}
+                  </Table.Cell>
+                  {psychSheetEvent === undefined ? (
+                    <>
+                      {competitionInfo.event_ids.map((id) => (
+                        <Table.Cell
+                          key={`registration-table-row-${registration.user.id}-${id}`}
+                        >
+                          {registration.competing.event_ids.includes(id) ? (
+                            <EventIcon event={id} className="selected" />
+                          ) : null}
+                        </Table.Cell>
+                      ))}
+                      <Table.Cell>
+                        {registration.competing.event_ids.length}
                       </Table.Cell>
-                    ))}
-                    <Table.Cell>
-                      {registration.competing.event_ids.length}
-                    </Table.Cell>
-                  </>
-                ) : (
-                  <>
-                    <Table.Cell />
-                    <Table.Cell
-                      collapsing
-                      textAlign="right"
-                      disabled={registration.tied_previous}
-                    >
-                      {registration.pos}
-                    </Table.Cell>
-                    <Table.Cell>
-                      {psychSheetSortBy === 'single'
-                        ? registration.single_rank
-                        : registration.average_rank}
-                    </Table.Cell>
-                    <Table.Cell>
-                      {formatCentiseconds(registration.single_best)}
-                    </Table.Cell>
-                    <Table.Cell>
-                      {formatCentiseconds(registration.average_best)}
-                    </Table.Cell>
-                  </>
-                )}
-              </Table.Row>
-            ))
-          ) : (
-            <Table.Row>
-              <Table.Cell
-                textAlign="center"
-                colSpan={
+                    </>
+                  ) : (
+                    <>
+                      <Table.Cell />
+                      <Table.Cell
+                        collapsing
+                        textAlign="right"
+                        disabled={registration.tied_previous}
+                      >
+                        {registration.pos}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {psychSheetSortBy === 'single'
+                          ? registration.single_rank
+                          : registration.average_rank}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {formatCentiseconds(registration.single_best)}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {formatCentiseconds(registration.average_best)}
+                      </Table.Cell>
+                    </>
+                  )}
+                </Table.Row>
+              ))
+            ) : (
+              <Table.Row>
+                <Table.Cell
+                  textAlign="center"
+                  colSpan={
                   psychSheetEvent === undefined
                     ? competitionInfo.event_ids.length + 3
                     : 7
                 }
-              >
-                {psychSheetEvent && isLoadingPsychSheet
-                  ? 'Crunching the data, please wait'
-                  : 'No matching records found'}
-              </Table.Cell>
-            </Table.Row>
-          )}
-        </Table.Body>
-        <Table.Footer>
-          <FooterContent
-            registrations={registrations}
-            psychSheetEvent={psychSheetEvent}
-            dataWithUser={dataWithUser}
-            competitionInfo={competitionInfo}
-          />
-        </Table.Footer>
-      </Table>
-    </div>
-  );
+                >
+                  {psychSheetEvent && isLoadingPsychSheet
+                    ? 'Crunching the data, please wait'
+                    : 'No matching records found'}
+                </Table.Cell>
+              </Table.Row>
+            )}
+          </Table.Body>
+          <Table.Footer>
+            <FooterContent
+              registrations={registrations}
+              psychSheetEvent={psychSheetEvent}
+              dataWithUser={dataWithUser}
+              competitionInfo={competitionInfo}
+            />
+          </Table.Footer>
+        </Table>
+      </div>
+    );
 }
--- app/webpacker/components/RegistrationsV2/Waiting/WaitingList.jsx
+++ app/webpacker/components/RegistrationsV2/Waiting/WaitingList.jsx
@@ -1,59 +1,50 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { Table, TableFooter } from 'semantic-ui-react';
+import { Message, Table, TableFooter } from 'semantic-ui-react';
 import { getWaitingCompetitors } from '../api/registration/get/get_registrations';
 import useWithUserData from '../hooks/useWithUserData';
-import { useDispatch } from '../../../lib/providers/StoreProvider';
-import { setMessage } from '../Register/RegistrationMessage';
 import Loading from '../../Requests/Loading';
 
 export default function WaitingList({ competitionInfo }) {
-  const dispatch = useDispatch();
-  const { isLoading: waitingLoading, data: waiting } = useQuery({
+  const { isLoading: waitingLoading, data: waiting, isError } = useQuery({
     queryKey: ['waiting', competitionInfo.id],
     queryFn: () => getWaitingCompetitors(competitionInfo.id),
     retry: false,
-    onError: (data) => {
-      const { error } = data.json;
-      dispatch(setMessage(
-        data
-          ? `competitions.registration_v2.errors.${error}`
-          : 'registrations.flash.failed',
-        'negative',
-      ));
-    },
   });
 
   const { isLoading: infoLoading, data: registrationsWithUser } = useWithUserData(waiting ?? []);
 
+  // eslint-disable-next-line no-nested-ternary
   return waitingLoading || infoLoading ? (
     <Loading />
-  ) : (
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Position</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {registrationsWithUser?.length ? (
-          registrationsWithUser
-            .toSorted(
-              (w1, w2) => w1.waiting_list_position - w2.waiting_list_position,
-            ) // We just care about the order of the waitlisted competitors
-            .map((w, i) => (
-              <Table.Row key={w.user_id}>
-                <Table.Cell>{w.user.name}</Table.Cell>
-                <Table.Cell>
-                  {w.waiting_list_position === 0 ? 'Not yet assigned' : i + 1}
-                </Table.Cell>
-              </Table.Row>
-            ))
-        ) : (
-          <TableFooter>No one on the Waiting List.</TableFooter>
-        )}
-      </Table.Body>
-    </Table>
-  );
+  )
+    : isError ? <Message> Loading the Waiting List failed </Message>
+      : (
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Position</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {registrationsWithUser?.length ? (
+              registrationsWithUser
+                .toSorted(
+                  (w1, w2) => w1.waiting_list_position - w2.waiting_list_position,
+                ) // We just care about the order of the waitlisted competitors
+                .map((w, i) => (
+                  <Table.Row key={w.user_id}>
+                    <Table.Cell>{w.user.name}</Table.Cell>
+                    <Table.Cell>
+                      {w.waiting_list_position === 0 ? 'Not yet assigned' : i + 1}
+                    </Table.Cell>
+                  </Table.Row>
+                ))
+            ) : (
+              <TableFooter>No one on the Waiting List.</TableFooter>
+            )}
+          </Table.Body>
+        </Table>
+      );
 }
