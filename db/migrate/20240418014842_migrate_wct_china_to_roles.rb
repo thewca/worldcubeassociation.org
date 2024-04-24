d8ab93c893fa24a406f33f69c304d014271ca263
Daniel James <djdany444@gmail.com>
2024-04-24 06:45:30 +0530

    Review changes

--- db/migrate/20240418014842_migrate_wct_china_to_roles.rb
+++ db/migrate/20240418014842_migrate_wct_china_to_roles.rb
@@ -1,23 +1,7 @@
 # frozen_string_literal: true
 
 class MigrateWctChinaToRoles < ActiveRecord::Migration[7.1]
-  def create_user_role_for_team_member(team_member, group)
-    if team_member.leader?
-      status = RolesMetadataTeamsCommittees.statuses[:leader]
-    elsif team_member.senior_member?
-      status = RolesMetadataTeamsCommittees.statuses[:senior_member]
-    else
-      status = RolesMetadataTeamsCommittees.statuses[:member]
-    end
-    metadata = RolesMetadataTeamsCommittees.create!(status: status)
-    UserRole.create!(
-      user_id: team_member.user_id,
-      group_id: group.id,
-      start_date: team_member.start_date,
-      end_date: team_member.end_date,
-      metadata: metadata,
-    )
-  end
+  include RoleMigrationHelper
 
   def change
     team = Team.c_find_by_friendly_id!('wct_china')
