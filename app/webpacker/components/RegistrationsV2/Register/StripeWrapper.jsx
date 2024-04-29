27c4c7c8f53af3daa627141542046fc6812f8f6e
FinnIckler <finnickler@gmail.com>
2024-04-29 18:20:37 +0200

    remove getPaymentId call

--- app/webpacker/components/RegistrationsV2/Register/StripeWrapper.jsx
+++ app/webpacker/components/RegistrationsV2/Register/StripeWrapper.jsx
@@ -1,40 +1,15 @@
 // Following https://github.com/stripe-samples/accept-a-payment/blob/main/payment-element/client/react-cra/src/Payment.js
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import { useQuery } from '@tanstack/react-query';
 import React, { useCallback, useEffect, useState } from 'react';
-import I18n from '../../../lib/i18n';
-import getPaymentId from '../api/registration/get/get_payment_intent';
 import PaymentStep from './PaymentStep';
-import { useDispatch } from '../../../lib/providers/StoreProvider';
-import { setMessage } from './RegistrationMessage';
 
 export default function StripeWrapper({
-  competitionInfo, stripePublishableKey, connectedAccountId, initialAmount,
+  competitionInfo, stripePublishableKey, connectedAccountId,
 }) {
   const [stripePromise, setStripePromise] = useState(null);
-  const dispatch = useDispatch();
-  const {
-    data: paymentInfo,
-    isLoading: isPaymentIdLoading,
-    isError,
-  } = useQuery({
-    queryKey: ['payment-secret', competitionInfo.id],
-    queryFn: () => getPaymentId(competitionInfo.id),
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-    staleTime: Infinity,
-    refetchOnMount: 'always',
-    onError: (data) => {
-      const { error } = data.json;
-      dispatch(setMessage(
-        error
-          ? I18n.t(`competitions.registration_v2.errors.${error}`)
-          : I18n.t('registrations.flash.failed') + data.message,
-        'negative',
-      ));
-    },
-  });
+  const initialAmount = competitionInfo.base_entry_fee_lowest_denomination;
+  const [amount, setAmount] = useState(initialAmount);
 
   useEffect(() => {
     setStripePromise(
@@ -44,8 +19,6 @@ export default function StripeWrapper({
     );
   }, [connectedAccountId, stripePublishableKey]);
 
-  const [amount, setAmount] = useState(initialAmount);
-
   const handleDonation = useCallback(async (donationAmount) => {
     // TODO: make sure this is always correct stripe money
     setAmount(initialAmount + donationAmount);
@@ -54,13 +27,10 @@ export default function StripeWrapper({
   return (
     <>
       <h1>Payment</h1>
-      {paymentInfo?.status === 'succeeded' && (
-        <div>Your payment has been successfully processed.</div>
-      )}
-      {!isPaymentIdLoading && stripePromise && !isError && (
+      { stripePromise && (
         <Elements
           stripe={stripePromise}
-          options={{ amount }}
+          options={{ amount, currency: competitionInfo.currency_code }}
         >
           <PaymentStep handleDonation={handleDonation} />
         </Elements>
