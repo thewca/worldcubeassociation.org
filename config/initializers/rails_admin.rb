f0e6fd6497c7a437d0dcd4b674587be3ff0aa017
Gregor Billing <gbilling@worldcubeassociation.org>
2024-04-29 20:56:27 +0200

    Move RailsAdmin mounting behind permissions barrier

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 RailsAdmin.config do |config|
   config.asset_source = :webpacker
 
   ### Popular gems integration
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  # == Devise ==
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
