edbe08027ed9134922021f3f2255b87392179546
Duncan <duncanonthejob@gmail.com>
2024-04-29 09:07:53 +0200

    rubocop fixes

--- db/migrate/20240426045248_add_newcomer_allowed_to_competition.rb
+++ db/migrate/20240426045248_add_newcomer_allowed_to_competition.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddNewcomerAllowedToCompetition < ActiveRecord::Migration[7.1]
   def change
     add_column :Competitions, :forbid_newcomers, :boolean, default: false, null: false
