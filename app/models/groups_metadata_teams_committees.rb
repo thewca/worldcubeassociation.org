e0cbf85aaedeb94535b12a764b71376ef0662e40
Daniel James <djdany444@gmail.com>
2024-04-30 00:56:13 +0530

    Replace some usages of teams with Team.banned (#9265)
    * Replace some usages of teams with Team.banned
    
    * Review changes
    
    * Review changes

--- app/models/groups_metadata_teams_committees.rb
+++ app/models/groups_metadata_teams_committees.rb
@@ -2,4 +2,11 @@
 
 class GroupsMetadataTeamsCommittees < ApplicationRecord
   has_one :user_group, as: :metadata
+
+  def self.at_least_senior_member?(role)
+    [
+      RolesMetadataTeamsCommittees.statuses[:senior_member],
+      RolesMetadataTeamsCommittees.statuses[:leader],
+    ].include?(UserRole.status(role))
+  end
 end
