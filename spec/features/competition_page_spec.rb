bddc54158373136aeada7a906323a1310b9cf18c
Duncan <duncanonthejob@gmail.com>
2024-05-01 11:06:17 +0200

    added anchor to event test

--- spec/features/competition_page_spec.rb
+++ spec/features/competition_page_spec.rb
@@ -9,22 +9,14 @@ RSpec.describe "competitions/show", type: :feature, js: true do
   let!(:round333_1) { FactoryBot.create(:round, competition: competition, event_id: "333", number: 1, cutoff: sixty_second_2_attempt_cutoff, advancement_condition: top_16_advance, total_number_of_rounds: 2) }
   let!(:round333_2) { FactoryBot.create(:round, competition: competition, event_id: "333", number: 2, total_number_of_rounds: 2) }
   let!(:round444_1) { FactoryBot.create(:round, competition: competition, event_id: "444", number: 1) }
+
   before do
     # Load all the rounds we just created.
     competition.reload
   end
 
-  # before do
-  #   assign(:competition, competition)
-  # end
-
-  # it "renders advancment condition for 333 round 1" do
-  #   render
-  #   expect(rendered).to match 'Top 16 advance to next round'
-  # end
-
   it "renders advancement condition for 333 round 1" do
-    visit competition_path(competition) # Replace with the path to your page
+    visit competition_path(competition, anchor: )
     expect(page).to have_text('Top 16 advance to next round')
   end
 end
