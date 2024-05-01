d13c53dd66936c5bacb7eb9985f5fa42fb3f5caf
Duncan <duncanonthejob@gmail.com>
2024-05-01 04:17:07 +0200

    added to_wcif to pr endpoint

--- app/controllers/api/v0/results_controller.rb
+++ app/controllers/api/v0/results_controller.rb
@@ -4,6 +4,6 @@ class Api::V0::ResultsController < Api::V0::ApiController
   def personal_records
     user = User.find(params.require(:user_id))
 
-    render json: user.personal_records
+    render json: user.personal_records.map(&:to_wcif)
   end
 end
