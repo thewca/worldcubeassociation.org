281e77817a3acf7de47b666c61c75ce9f00b691d
Daniel James <djdany444@gmail.com>
2024-05-03 14:56:10 +0530

    Add preferred contact mode for teams/committees (#9333)
    * Add preferred contact mode for teams/committees
    
    * Review changes
    
    * Review changes

--- app/models/groups_metadata_teams_committees.rb
+++ app/models/groups_metadata_teams_committees.rb
@@ -3,6 +3,12 @@
 class GroupsMetadataTeamsCommittees < ApplicationRecord
   has_one :user_group, as: :metadata
 
+  enum :preferred_contact_mode, {
+    no_contact: "no_contact",
+    email: "email",
+    contact_form: "contact_form",
+  }
+
   def self.at_least_senior_member?(role)
     [
       RolesMetadataTeamsCommittees.statuses[:senior_member],
