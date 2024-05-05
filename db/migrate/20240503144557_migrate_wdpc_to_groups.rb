482580f9355d41f49b42c25739b405127a8bed86
Gregor Billing <gbilling@worldcubeassociation.org>
2024-05-05 07:11:08 +0200

    Hotfix: Unscope hidden teams during migration

--- db/migrate/20240503144557_migrate_wdpc_to_groups.rb
+++ db/migrate/20240503144557_migrate_wdpc_to_groups.rb
@@ -4,7 +4,7 @@ class MigrateWdpcToGroups < ActiveRecord::Migration[7.1]
   include RoleMigrationHelper
 
   def change
-    team = Team.find_by_friendly_id('wdpc')
+    team = Team.with_hidden.find_by_friendly_id('wdpc')
     # email was removed in https://github.com/thewca/worldcubeassociation.org/pull/6065, but I don't
     # think there was any reason to remove it. Hence adding it back.
     metadata = GroupsMetadataTeamsCommittees.create!(email: 'dataprotection@worldcubeassociation.org', friendly_id: team.friendly_id)
