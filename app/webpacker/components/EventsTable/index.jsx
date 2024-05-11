dd7605037921f8a2e1152d2b45c0276fa3db9cbd
Duncan <52967253+dunkOnIT@users.noreply.github.com>
2024-05-11 11:34:44 +0200

    Improve responsive design of Events and Schedule tables (#9372)
    * added unstackable property to schedule table
    
    * also made events table unstackable
    
    * added scrollability per gregor suggestion
    
    * fixed indentation
    
    * Use singleLine on ShowEvents table to avoid line breaks
    
    * Make schedule events table responsive (again)
    
    * Reduce waste of horizontal space
    
    * Reduce waste of vertical space
    
    * Fix copy-paste whoopsie
    
    ---------
    
    Co-authored-by: Gregor Billing <gbilling@worldcubeassociation.org>

--- app/webpacker/components/EventsTable/index.jsx
+++ app/webpacker/components/EventsTable/index.jsx
@@ -19,82 +19,84 @@ import {
 
 export default function EventsTable({ competitionInfo, wcifEvents }) {
   return (
-    <Table striped selectable compact>
-      <TableHeader>
-        <TableRow>
-          <TableHeaderCell>
-            {i18n.t('competitions.results_table.event')}
-          </TableHeaderCell>
-          <TableHeaderCell>
-            {i18n.t('competitions.results_table.round')}
-          </TableHeaderCell>
-          <TableHeaderCell>
-            <a href="#format">{i18n.t('competitions.events.format')}</a>
-          </TableHeaderCell>
-          <TableHeaderCell>
-            <a href="#time-limit">{i18n.t('competitions.events.time_limit')}</a>
-          </TableHeaderCell>
-          {competitionInfo['uses_cutoff?'] && (
+    <div style={{ overflowX: 'scroll' }}>
+      <Table striped selectable compact unstackable singleLine>
+        <TableHeader>
+          <TableRow>
             <TableHeaderCell>
-              <a href="#cutoff">{i18n.t('competitions.events.cutoff')}</a>
+              {i18n.t('competitions.results_table.event')}
             </TableHeaderCell>
-          )}
-          <TableHeaderCell>
-            {i18n.t('competitions.events.proceed')}
-          </TableHeaderCell>
-          {competitionInfo['uses_qualification?'] && (
             <TableHeaderCell>
-              {i18n.t('competitions.events.qualification')}
+              {i18n.t('competitions.results_table.round')}
             </TableHeaderCell>
-          )}
-        </TableRow>
-      </TableHeader>
-
-      <TableBody>
-        {wcifEvents.map((event) => event.rounds.map((round, i) => (
-          <TableRow key={round.id}>
-            {i === 0 && (
-              <TableCell rowSpan={event.rounds.length}>
-                {events.byId[event.id].name}
-              </TableCell>
-            )}
-            <TableCell>{i18n.t(`rounds.${getRoundTypeId(i + 1, event.rounds.length, Boolean(round.cutoff))}.cellName`)}</TableCell>
-            <TableCell>
-              {round.cutoff && `${formats.byId[round.cutoff.numberOfAttempts].shortName} / `}
-              {formats.byId[round.format].shortName}
-            </TableCell>
-            <TableCell>
-              {timeLimitToString(round, wcifEvents)}
-              {round.timeLimit !== null && (
-                <>
-                  {round.timeLimit.cumulativeRoundIds.length === 1 && (
-                    <a href="#cumulative-time-limit">*</a>
-                  )}
-                  {round.timeLimit.cumulativeRoundIds.length > 1 && (
-                    <a href="#cumulative-across-rounds-time-limit">**</a>
-                  )}
-                </>
-              )}
-            </TableCell>
+            <TableHeaderCell>
+              <a href="#format">{i18n.t('competitions.events.format')}</a>
+            </TableHeaderCell>
+            <TableHeaderCell>
+              <a href="#time-limit">{i18n.t('competitions.events.time_limit')}</a>
+            </TableHeaderCell>
             {competitionInfo['uses_cutoff?'] && (
-              <TableCell>
-                {round.cutoff
-                  && cutoffToString(round)}
-              </TableCell>
+              <TableHeaderCell>
+                <a href="#cutoff">{i18n.t('competitions.events.cutoff')}</a>
+              </TableHeaderCell>
             )}
-            <TableCell>
-              {round.advancementCondition
-                && advancementConditionToString(round)}
-            </TableCell>
+            <TableHeaderCell>
+              {i18n.t('competitions.events.proceed')}
+            </TableHeaderCell>
             {competitionInfo['uses_qualification?'] && (
-              <TableCell>
-                { i === 0
-                && eventQualificationToString(event, event.qualification)}
-              </TableCell>
+              <TableHeaderCell>
+                {i18n.t('competitions.events.qualification')}
+              </TableHeaderCell>
             )}
           </TableRow>
-        )))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+
+        <TableBody>
+          {wcifEvents.map((event) => event.rounds.map((round, i) => (
+            <TableRow key={round.id}>
+              {i === 0 && (
+                <TableCell rowSpan={event.rounds.length}>
+                  {events.byId[event.id].name}
+                </TableCell>
+              )}
+              <TableCell>{i18n.t(`rounds.${getRoundTypeId(i + 1, event.rounds.length, Boolean(round.cutoff))}.cellName`)}</TableCell>
+              <TableCell>
+                {round.cutoff && `${formats.byId[round.cutoff.numberOfAttempts].shortName} / `}
+                {formats.byId[round.format].shortName}
+              </TableCell>
+              <TableCell>
+                {timeLimitToString(round, wcifEvents)}
+                {round.timeLimit !== null && (
+                  <>
+                    {round.timeLimit.cumulativeRoundIds.length === 1 && (
+                      <a href="#cumulative-time-limit">*</a>
+                    )}
+                    {round.timeLimit.cumulativeRoundIds.length > 1 && (
+                      <a href="#cumulative-across-rounds-time-limit">**</a>
+                    )}
+                  </>
+                )}
+              </TableCell>
+              {competitionInfo['uses_cutoff?'] && (
+                <TableCell>
+                  {round.cutoff
+                    && cutoffToString(round)}
+                </TableCell>
+              )}
+              <TableCell>
+                {round.advancementCondition
+                  && advancementConditionToString(round)}
+              </TableCell>
+              {competitionInfo['uses_qualification?'] && (
+                <TableCell>
+                  { i === 0
+                  && eventQualificationToString(event, event.qualification)}
+                </TableCell>
+              )}
+            </TableRow>
+          )))}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
