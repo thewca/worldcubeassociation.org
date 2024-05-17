64556198cdf5af1ee5baade8940a6daaf6e9335e
Duncan <52967253+dunkOnIT@users.noreply.github.com>
2024-05-17 07:26:34 +0200

    Update app/controllers/api/v0/api_controller.rb
    Co-authored-by: Gregor Billing <gbilling@worldcubeassociation.org>

--- app/controllers/api/v0/api_controller.rb
+++ app/controllers/api/v0/api_controller.rb
@@ -30,7 +30,7 @@ class Api::V0::ApiController < ApplicationController
   end
 
   def user_qualification_data
-    user = User.find(params.require(:user_id))
+    user = User.includes(person: [:ranksSingle, :ranksAverage]).find(params.require(:user_id))
     render json: user.person.personal_records.map(&:to_wcif)
   end
 
