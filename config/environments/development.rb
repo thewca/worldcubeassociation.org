e48f3d49737d2a2a86e7132cb79e391170260f8b
Gregor Billing <gbilling@worldcubeassociation.org>
2024-05-21 11:44:31 +0200

    Disable Bullet browser alert, keep all other logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   config.after_initialize do
     Bullet.enable = !EnvConfig.DISABLE_BULLET?
-    Bullet.alert = true
+    Bullet.alert = false
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.rails_logger = true
