d2853ac5cf08ac6fb1247557db149a425ce01138
FinnIckler <finnickler@gmail.com>
2024-06-04 17:40:10 +0200

    Unify payment logic for register v1 and v2 (#9425)
    * unify payment_completion
    
    * remove old payment finish method
    
    * correct error handling
    
    * get the registration out of the stripe intent
    
    * change URL
    
    * remove unnecessary instance variable
    
    * unify refund logic
    
    * run rubocop
    
    * put payment_id at the end in the URL
    
    * put refund_amount in POST
    
    * fix missing _path
    
    * correctly set return_urls
    
    * use the right variable on updated
    
    * change context for payment_completion
    
    * fix refund_payment post test
    
    * fix registration_payment_completion_path in registrations_spec
    
    * use @payment.receipt.id to refund
    
    * correctly get the holder out of the charge
    
    * add amount_available_for_refund method to stripe_record
    
    * add legacy redirects
    
    * Run Rubocop
    
    * fix right route being used when refunding and renamed route
    
    * use amount_available_for_refund in record
    
    * use ids instead of getting the whole object from the database
    
    * give object to registration_payment_completion_url
    
    * fix redirect and added a new error if stored_record is not present
    
    * make amount_available_for_refund return ruby money per default
    
    * move user equivalence check to init_stripe
    
    * fix amount_to_ruby call
    
    ---------
    
    Co-authored-by: Gregor Billing <gbilling@worldcubeassociation.org>

--- app/webpacker/components/RegistrationsV2/api/payment/get/getAvailableRefunds.js
+++ app/webpacker/components/RegistrationsV2/api/payment/get/getAvailableRefunds.js
@@ -1,9 +1,9 @@
 import { fetchJsonOrError } from '../../../../../lib/requests/fetchWithAuthenticityToken';
-import { paymentRefundsUrl } from '../../../../../lib/requests/routes.js.erb';
+import { availableRefundsUrl } from '../../../../../lib/requests/routes.js.erb';
 
 export default async function getAvailableRefunds(
   competitionId,
   userId,
 ) {
-  return fetchJsonOrError(paymentRefundsUrl(competitionId, userId));
+  return fetchJsonOrError(availableRefundsUrl(competitionId, userId));
 }
