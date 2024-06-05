aa4ff2f1106f00d0d9fee15c1e85647d2b10a9ef
FinnIckler <finnickler@gmail.com>
2024-06-05 17:20:42 +0200

    Implement registration_full? for v2 Registrations (#9519)
    * implement registration full for v2
    
    * remove unnecessary comment
    
    * Update app/models/competition.rb
    
    Co-authored-by: Gregor Billing <gregor.billing.dev@gmail.com>
    
    ---------
    
    Co-authored-by: Gregor Billing <gregor.billing.dev@gmail.com>

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -415,7 +415,8 @@ class Competition < ApplicationRecord
   end
 
   def registration_full?
-    competitor_limit_enabled? && registrations.accepted_and_paid_pending_count >= competitor_limit
+    competitor_count = uses_new_registration_service? ? Microservices::Registrations.competitor_count_by_competition(id) : registrations.accepted_and_paid_pending_count
+    competitor_limit_enabled? && competitor_count >= competitor_limit
   end
 
   def number_of_bookmarks
