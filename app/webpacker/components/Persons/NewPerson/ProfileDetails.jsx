b19dea6249edbbad45f7b4c30bd7a23ec7e68c63
Simon Kelly <simonkellly@gmail.com>
2024-06-14 03:22:07 +0100

    Add newer components

---
+++ app/webpacker/components/Persons/NewPerson/ProfileDetails.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Card, Grid, Header } from 'semantic-ui-react';
+import i18n from "../../../lib/i18n";
+
+function Stat({ children, label, width }) {
+  return (
+    <Grid.Column
+      stretched
+      className="stat"
+      width={width}
+    >
+      <Header size="tiny" sub block>
+        {label}
+        <Header className="stat">{children}</Header>
+      </Header>
+    </Grid.Column>
+  );
+}
+
+function FlagIcon({ countryIso2 }) {
+  return (
+    <span
+      className={`fi fi-${countryIso2.toLowerCase()}`}
+    />
+  );
+}
+
+export default function ProfileDetails({ person }) {
+  const profile = {
+    region: person.country,
+    wcaId: person.wcaId,
+    gender: person.gender,
+    competitions: person.competitionCount,
+    solves: person.completedSolves,
+  };
+  const statCount = 2
+    + (profile.gender ? 1 : 0)
+    + (profile.competitions ? 1 : 0)
+    + (profile.solves ? 1 : 0);
+
+  const regionWidth = 16; // (statCount % 2) == 1 ? 11 : 8;
+  const idWidth = (statCount % 2) === 0 ? 16 : 8;
+  const otherWidth = 8;
+
+  return (
+    <Card fluid className="large-card" color="blue">
+      <Card.Content>
+        <Card.Header textAlign="center">
+          Profile
+        </Card.Header>
+        <Grid textAlign="center" className="stat-grid">
+          <Stat label="Region" width={regionWidth}>
+            <FlagIcon countryIso2={profile.region.iso2} />
+            {' '}
+            {profile.region.name}
+          </Stat>
+          <Stat label="WCA ID" width={idWidth}>{profile.wcaId}</Stat>
+          {profile.gender && (
+            <Stat label="Gender" width={otherWidth}>
+              {i18n.t(`enums.user.gender.${profile.gender}`)}
+            </Stat>
+          )}
+          {profile.solves && <Stat label="Solves" width={otherWidth}>{profile.solves}</Stat>}
+          {profile.competitions && <Stat label="Competitions" width={otherWidth}>{profile.competitions}</Stat>}
+        </Grid>
+      </Card.Content>
+    </Card>
+  );
+}
