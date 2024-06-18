3642bc8590f4beab0462def8cba8d3499f324c1f
Daniel James <djdany444@gmail.com>
2024-06-18 21:37:49 +0530

    Increased maximum size of roles list API from 100 to 1000 (#9525)
    * Increased maximum size of roles list API from 100 to 1000
    
    * Review changes
    
    * Review changes
    
    * Review changes
    
    * Review changes
    
    * Update app/webpacker/lib/requests/routes.js.erb
    
    Co-authored-by: Gregor Billing <gregor.billing.dev@gmail.com>
    
    ---------
    
    Co-authored-by: Gregor Billing <gregor.billing.dev@gmail.com>

--- app/controllers/api/v0/user_roles_controller.rb
+++ app/controllers/api/v0/user_roles_controller.rb
@@ -47,8 +47,8 @@ class Api::V0::UserRolesController < Api::V0::ApiController
     roles = UserRole.filter_roles(roles, current_user, params)
     roles = UserRole.sort_roles(roles, params[:sort])
 
-    # Limiting to first 100 elements of roles array to avoid serializing of large array.
-    render json: roles.first(100)
+    # Paginating the list by sending only first 100 elements unless mentioned in the API.
+    paginate json: roles
   end
 
   # Returns a list of roles primarily based on groupType.
