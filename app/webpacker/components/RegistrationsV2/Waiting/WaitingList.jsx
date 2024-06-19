b8d3e5bbc17dd288ed277bbe16c598dc8060483b
FinnIckler <finnickler@gmail.com>
2024-06-19 13:30:13 +0200

    Use Segment in Waiting List

--- app/webpacker/components/RegistrationsV2/Waiting/WaitingList.jsx
+++ app/webpacker/components/RegistrationsV2/Waiting/WaitingList.jsx
@@ -1,10 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { Table, TableFooter } from 'semantic-ui-react';
+import {
+  Header, Segment, Table,
+} from 'semantic-ui-react';
 import { getWaitingCompetitors } from '../api/registration/get/get_registrations';
 import useWithUserData from '../hooks/useWithUserData';
 import Loading from '../../Requests/Loading';
 import Errored from '../../Requests/Errored';
+import i18n from '../../../lib/i18n';
 
 export default function WaitingList({ competitionInfo }) {
   const { isLoading: waitingLoading, data: waiting, isError } = useQuery({
@@ -25,31 +28,33 @@ export default function WaitingList({ competitionInfo }) {
     <Loading />
   )
     : (
-      <Table collapsing>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Position</Table.HeaderCell>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {registrationsWithUser?.length ? (
-            registrationsWithUser
-              .toSorted(
-                (w1, w2) => w1.waiting_list_position - w2.waiting_list_position,
-              ) // We just care about the order of the waitlisted competitors
-              .map((w, i) => (
-                <Table.Row key={w.user_id}>
-                  <Table.Cell>
-                    {w.waiting_list_position === 0 ? 'Not yet assigned' : i + 1}
-                  </Table.Cell>
-                  <Table.Cell>{w.user.name}</Table.Cell>
+      <Segment>
+        <Header>{i18n.t('registrations.list.waiting_list')}</Header>
+        { registrationsWithUser.length > 0
+          ? (
+            <Table collapsing>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Position</Table.HeaderCell>
+                  <Table.HeaderCell>{i18n.t('delegates_page.table.name')}</Table.HeaderCell>
                 </Table.Row>
-              ))
-          ) : (
-            <TableFooter>No one on the Waiting List.</TableFooter>
-          )}
-        </Table.Body>
-      </Table>
+              </Table.Header>
+              <Table.Body>
+                {registrationsWithUser
+                  .toSorted(
+                    (w1, w2) => w1.waiting_list_position - w2.waiting_list_position,
+                  ) // We just care about the order of the waitlisted competitors
+                  .map((w, i) => (
+                    <Table.Row key={w.user_id}>
+                      <Table.Cell>
+                        {w.waiting_list_position === 0 ? 'Not yet assigned' : i + 1}
+                      </Table.Cell>
+                      <Table.Cell>{w.user.name}</Table.Cell>
+                    </Table.Row>
+                  ))}
+              </Table.Body>
+            </Table>
+          ) : 'No one on the Waiting List.'}
+      </Segment>
     );
 }
