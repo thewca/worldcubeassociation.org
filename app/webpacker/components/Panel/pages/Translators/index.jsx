fc3cb85d9deaebff1560745bd9a2bbf5bee37600
Daniel James <djdany444@gmail.com>
2024-06-19 15:46:33 +0530

    Moved WST panel to newer format (#9557)

--- app/webpacker/components/Panel/pages/Translators/index.jsx
+++ app/webpacker/components/Panel/pages/Translators/index.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import {
   Button, Form, Header, Modal, Table,
 } from 'semantic-ui-react';
-import useLoadedData from '../../../lib/hooks/useLoadedData';
-import { apiV0Urls } from '../../../lib/requests/routes.js.erb';
-import { groupTypes } from '../../../lib/wca-data.js.erb';
-import Errored from '../../Requests/Errored';
-import Loading from '../../Requests/Loading';
-import useSaveAction from '../../../lib/hooks/useSaveAction';
-import WcaSearch from '../../SearchWidget/WcaSearch';
-import SEARCH_MODELS from '../../SearchWidget/SearchModel';
+import useLoadedData from '../../../../lib/hooks/useLoadedData';
+import { apiV0Urls } from '../../../../lib/requests/routes.js.erb';
+import { groupTypes } from '../../../../lib/wca-data.js.erb';
+import Errored from '../../../Requests/Errored';
+import Loading from '../../../Requests/Loading';
+import useSaveAction from '../../../../lib/hooks/useSaveAction';
+import WcaSearch from '../../../SearchWidget/WcaSearch';
+import SEARCH_MODELS from '../../../SearchWidget/SearchModel';
 
 export default function Translators() {
   const {
