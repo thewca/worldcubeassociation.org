210b4b06b0ef35ba25c235c2e74210b21c78778c
Daniel James <djdany444@gmail.com>
2024-06-25 09:46:13 +0530

    Fixed logic of at_least_senior_member?

--- app/models/roles_metadata_teams_committees.rb
+++ app/models/roles_metadata_teams_committees.rb
@@ -11,6 +11,6 @@ class RolesMetadataTeamsCommittees < ApplicationRecord
   has_one :user, through: :user_role
 
   def at_least_senior_member?
-    user_role.status_rank <= UserRole.status_rank(UserGroup.group_types[:teams_committees], RolesMetadataTeamsCommittees.statuses[:senior_member])
+    user_role.status_rank >= UserRole.status_rank(UserGroup.group_types[:teams_committees], RolesMetadataTeamsCommittees.statuses[:senior_member])
   end
 end
