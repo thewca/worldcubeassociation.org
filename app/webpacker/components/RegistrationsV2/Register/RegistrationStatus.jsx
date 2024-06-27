8f82dfb26a338d7958adfe1a30586aaf7239e856
Gregor Billing <gbilling@worldcubeassociation.org>
2024-06-27 10:58:44 +0200

    Reinstate 3-color info message scheme

--- app/webpacker/components/RegistrationsV2/Register/RegistrationStatus.jsx
+++ app/webpacker/components/RegistrationsV2/Register/RegistrationStatus.jsx
@@ -16,12 +16,31 @@ function registrationIconByStatus(registrationStatus) {
   }
 }
 
+// We are not YET adding this to the locale i18n because this new feature is a test run.
+// If we add these strings to en.yml immediately, translators will get a notification asking them
+//   to translate these strings during our test mode deployment. But we aren't even sure whether
+//   we want to keep these strings. So we hard-code them "for now" (when did that ever go wrong?)
+function canIBookPlaneTickets(registrationStatus) {
+  switch (registrationStatus) {
+    case 'pending':
+      return 'No, the organizers still have to manually approve your registration. This can take time.';
+    case 'accepted':
+      return 'Yes, happy competing!';
+    case 'cancelled':
+      return 'No, your registration has been deleted and you cannot compete anymore.';
+    case 'waiting_list':
+      return 'No, you are on a waiting list because the competitor limit has been reached. You will be contacted when a spot opens up, until then you unfortunately cannot compete.';
+    default:
+      return '[Testers: This should not happen. If you reached this message, please contact WST!]';
+  }
+}
+
 function HumanFriendlyRegistrationStatus({ registration }) {
-  const registrationAccepted = registration.competing.registration_status === 'accepted';
   return (
     <Message
-      info={!registrationAccepted}
-      success={registrationAccepted}
+      info={registration.competing.registration_status === 'pending'}
+      success={registration.competing.registration_status === 'accepted'}
+      negative={registration.competing.registration_status === 'cancelled'}
       icon
     >
       <Icon name="plane" />
@@ -29,18 +48,18 @@ function HumanFriendlyRegistrationStatus({ registration }) {
         <Message.Header>
           Can I pack my bags and book a flight?
         </Message.Header>
-        { registrationAccepted ? 'Yes, happy competing' : 'No, the organizers still have to manually approve your registration. This can take time.'}
+        {canIBookPlaneTickets(registration.competing.registration_status)}
       </Message.Content>
     </Message>
   );
 }
 
 function SimpleRegistrationStatus({ registration }) {
-  const registrationAccepted = registration.competing.registration_status === 'accepted';
   return (
     <Message
-      info={!registrationAccepted}
-      success={registrationAccepted}
+      info={registration.competing.registration_status === 'pending'}
+      success={registration.competing.registration_status === 'accepted'}
+      negative={registration.competing.registration_status === 'cancelled'}
       icon
     >
       <Icon name={registrationIconByStatus(registration.competing.registration_status)} />
