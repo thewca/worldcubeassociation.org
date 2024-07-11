ae1fc613fce4ed4f79595a59549abc177b15227f
Gregor Billing <gbilling@worldcubeassociation.org>
2024-07-11 19:26:26 +0200

    Enable in-memory caching in tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,10 +24,11 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{1.hour.to_i}",
   }
 
-  # Show full error reports and disable caching.
+  # Show full error reports and disable controller caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.cache_store = :null_store
+  # We do want to use a memory cache for other tests though, for example to include Country, Continent, etc. models.
+  config.cache_store = :memory_store
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = :none
