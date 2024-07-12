5e56b91c68ade54c5a08bd751b970dd799cd1b62
darshanvss <darshanvss77@gmail.com>
2024-07-12 20:09:29 +0530

    fixed ban new button issue

--- app/webpacker/components/Panel/pages/BannedCompetitorsPage/BannedCompetitors.jsx
+++ app/webpacker/components/Panel/pages/BannedCompetitorsPage/BannedCompetitors.jsx
@@ -1,67 +1,78 @@
-import React, { useState } from 'react';
-import {
-  Button, Icon, Modal, Table,
-} from 'semantic-ui-react';
-import UserBadge from '../../../UserBadge';
-import BanendCompetitorForm from './BannedCompetitorForm';
+import React, { useState } from "react";
+import { Button, Icon, Modal, Table, Segment } from "semantic-ui-react";
+import UserBadge from "../../../UserBadge";
+import BannedCompetitorForm from "./BannedCompetitorForm";
 
 export default function BannedCompetitors({
   bannedCompetitorRoles,
   sync,
   canEditBannedCompetitors,
+  isPastBannedCompetitorList,
 }) {
   const [banModalParams, setBanModalParams] = useState(null);
 
   return (
     <>
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell width={5}>User</Table.HeaderCell>
-            <Table.HeaderCell width={2}>Start date</Table.HeaderCell>
-            <Table.HeaderCell width={2}>End date</Table.HeaderCell>
-            <Table.HeaderCell width={4}>Reason</Table.HeaderCell>
-            <Table.HeaderCell width={3}>Scope</Table.HeaderCell>
-            {canEditBannedCompetitors && <Table.HeaderCell width={2}>Edit</Table.HeaderCell>}
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {bannedCompetitorRoles.map((role) => (
-            <Table.Row key={role.id}>
-              <Table.Cell>
-                <UserBadge
-                  user={role.user}
-                  hideBorder
-                  leftAlign
-                  subtexts={role.user.wca_id ? [role.user.wca_id] : []}
-                />
-              </Table.Cell>
-              <Table.Cell>{role.start_date}</Table.Cell>
-              <Table.Cell>{role.end_date}</Table.Cell>
-              <Table.Cell>{role.metadata.ban_reason}</Table.Cell>
-              <Table.Cell>{role.metadata.scope}</Table.Cell>
+      {canEditBannedCompetitors && !isPastBannedCompetitorList && (
+        <Button onClick={() => setBanModalParams({ action: "new" })}>
+          Ban new competitor
+        </Button>
+      )}
+      {bannedCompetitorRoles.length > 0 ? (
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell width={5}>User</Table.HeaderCell>
+              <Table.HeaderCell width={2}>Start date</Table.HeaderCell>
+              <Table.HeaderCell width={2}>End date</Table.HeaderCell>
+              <Table.HeaderCell width={4}>Reason</Table.HeaderCell>
+              <Table.HeaderCell width={3}>Scope</Table.HeaderCell>
               {canEditBannedCompetitors && (
-                <Table.Cell>
-                  <Icon
-                    name="edit"
-                    link
-                    onClick={() => setBanModalParams({ action: 'edit', role })}
-                  />
-                </Table.Cell>
+                <Table.HeaderCell width={2}>Edit</Table.HeaderCell>
               )}
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-      {canEditBannedCompetitors && <Button onClick={() => setBanModalParams({ action: 'new' })}>Ban new competitor</Button>}
-      <Modal
-        open={!!banModalParams}
-        onClose={() => setBanModalParams(null)}
-      >
+          </Table.Header>
+
+          <Table.Body>
+            {bannedCompetitorRoles.map((role) => (
+              <Table.Row key={role.id}>
+                <Table.Cell>
+                  <UserBadge
+                    user={role.user}
+                    hideBorder
+                    leftAlign
+                    subtexts={role.user.wca_id ? [role.user.wca_id] : []}
+                  />
+                </Table.Cell>
+                <Table.Cell>{role.start_date}</Table.Cell>
+                <Table.Cell>{role.end_date}</Table.Cell>
+                <Table.Cell>{role.metadata.ban_reason}</Table.Cell>
+                <Table.Cell>{role.metadata.scope}</Table.Cell>
+                {canEditBannedCompetitors && (
+                  <Table.Cell>
+                    <Icon
+                      name="edit"
+                      link
+                      onClick={() =>
+                        setBanModalParams({ action: "edit", role })
+                      }
+                    />
+                  </Table.Cell>
+                )}
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      ) : (
+        <Segment placeholder textAlign="center">
+          <p>No Data to Show.</p>
+        </Segment>
+      )}
+
+      <Modal open={!!banModalParams} onClose={() => setBanModalParams(null)}>
         <Modal.Header>Add/Edit Banned Competitor</Modal.Header>
         <Modal.Content>
-          <BanendCompetitorForm
+          <BannedCompetitorForm
             sync={sync}
             banAction={banModalParams?.action}
             banActionRole={banModalParams?.role}
--- app/webpacker/components/Panel/pages/BannedCompetitorsPage/index.jsx
+++ app/webpacker/components/Panel/pages/BannedCompetitorsPage/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Header } from 'semantic-ui-react';
-import useLoadedData from '../../../../lib/hooks/useLoadedData';
-import { apiV0Urls } from '../../../../lib/requests/routes.js.erb';
-import { groupTypes } from '../../../../lib/wca-data.js.erb';
-import Loading from '../../../Requests/Loading';
-import Errored from '../../../Requests/Errored';
-import BannedCompetitors from './BannedCompetitors';
-import useLoggedInUserPermissions from '../../../../lib/hooks/useLoggedInUserPermissions';
+import React from "react";
+import { Header } from "semantic-ui-react";
+import useLoadedData from "../../../../lib/hooks/useLoadedData";
+import { apiV0Urls } from "../../../../lib/requests/routes.js.erb";
+import { groupTypes } from "../../../../lib/wca-data.js.erb";
+import Loading from "../../../Requests/Loading";
+import Errored from "../../../Requests/Errored";
+import BannedCompetitors from "./BannedCompetitors";
+import useLoggedInUserPermissions from "../../../../lib/hooks/useLoggedInUserPermissions";
 
 export default function BannedCompetitorsPage() {
   const {
@@ -14,64 +14,81 @@ export default function BannedCompetitorsPage() {
     loading: bannedCompetitorRolesLoading,
     error: bannedCompetitorRolesError,
     sync: syncBannedCompetitorRoles,
-  } = useLoadedData(apiV0Urls.userRoles.list({
-    isActive: true,
-    groupType: groupTypes.banned_competitors,
-  }, 'startDate'));
+  } = useLoadedData(
+    apiV0Urls.userRoles.list(
+      {
+        isActive: true,
+        groupType: groupTypes.banned_competitors,
+      },
+      "startDate"
+    )
+  );
   const {
     data: pastBannedCompetitorRoles,
     loading: pastBannedCompetitorRolesLoading,
     error: pastBannedCompetitorRolesError,
     sync: syncPastBannedCompetitorRoles,
-  } = useLoadedData(apiV0Urls.userRoles.list({
-    isActive: false,
-    groupType: groupTypes.banned_competitors,
-  }, 'startDate'));
+  } = useLoadedData(
+    apiV0Urls.userRoles.list(
+      {
+        isActive: false,
+        groupType: groupTypes.banned_competitors,
+      },
+      "startDate"
+    )
+  );
   const {
     data: bannedGroups,
     loading: bannedGroupLoading,
     error: bannedGroupError,
   } = useLoadedData(apiV0Urls.userGroups.list(groupTypes.banned_competitors));
-  const { loggedInUserPermissions, permissionsLoading } = useLoggedInUserPermissions();
+  const { loggedInUserPermissions, permissionsLoading } =
+    useLoggedInUserPermissions();
 
-  if (bannedCompetitorRolesLoading || pastBannedCompetitorRolesLoading
-    || bannedGroupLoading || permissionsLoading) {
+  if (
+    bannedCompetitorRolesLoading ||
+    pastBannedCompetitorRolesLoading ||
+    bannedGroupLoading ||
+    permissionsLoading
+  ) {
     return <Loading />;
   }
-  if (bannedCompetitorRolesError || pastBannedCompetitorRolesError || bannedGroupError) {
+  if (
+    bannedCompetitorRolesError ||
+    pastBannedCompetitorRolesError ||
+    bannedGroupError
+  ) {
     return <Errored />;
   }
 
-  const canEditBannedCompetitors = bannedGroups.some(
-    (bannedGroup) => loggedInUserPermissions.canEditGroup(bannedGroup.id),
+  const canEditBannedCompetitors = bannedGroups.some((bannedGroup) =>
+    loggedInUserPermissions.canEditGroup(bannedGroup.id)
   );
-
-  if (bannedCompetitorRoles.length === 0 && pastBannedCompetitorRoles.length === 0) {
-    return 'No data to show.';
-  }
 
   return (
     <>
-      {bannedCompetitorRoles.length > 0 && (
+      {
         <>
           <Header>Banned Competitors</Header>
           <BannedCompetitors
             bannedCompetitorRoles={bannedCompetitorRoles}
             sync={syncBannedCompetitorRoles}
             canEditBannedCompetitors={canEditBannedCompetitors}
+            isPastBannedCompetitorList={false}
           />
         </>
-      )}
-      {pastBannedCompetitorRoles.length > 0 && (
+      }
+      {
         <>
           <Header>Past Banned Competitors</Header>
           <BannedCompetitors
             bannedCompetitorRoles={pastBannedCompetitorRoles}
             sync={syncPastBannedCompetitorRoles}
             canEditBannedCompetitors={canEditBannedCompetitors}
+            isPastBannedCompetitorList={true}
           />
         </>
-      )}
+      }
     </>
   );
 }
