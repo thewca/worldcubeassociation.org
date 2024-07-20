bc687fcc6fa636aea2f7a260dc89bdd18f88fa6c
Gregor Billing <gbilling@worldcubeassociation.org>
2024-07-20 14:48:14 +0200

    Use compact export format for PreferredFormat

--- app/models/preferred_format.rb
+++ app/models/preferred_format.rb
@@ -12,8 +12,20 @@ class PreferredFormat < ApplicationRecord
 
   default_scope -> { order(:ranking) }
 
+  def self.all_raw
+    self.static_json_data.flat_map do |event_id, format_ids|
+      format_ids.map.with_index do |format_id, idx|
+        { event_id: event_id, format_id: format_id, ranking: idx + 1 }
+      end
+    end
+  end
+
   def self.dump_static
-    # In the ORDER BY call, `rank` comes from Event, `ranking` comes from PreferredFormat.
-    self.unscoped.joins(:event).order(:rank, :ranking).as_json
+    self.unscoped
+        .joins(:event)
+        .order(:rank)
+        .group_by(&:event_id)
+        .transform_values { |el| el.sort_by(&:ranking).pluck(:format_id) }
+        .as_json
   end
 end
