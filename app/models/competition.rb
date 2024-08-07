3328123bf0e4afee7388ce91d69e74f555dfa46c
Duncan <duncanonthejob@gmail.com>
2024-08-07 15:18:54 +0200

    switched to registration_currently_open?

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -476,7 +476,7 @@ class Competition < ApplicationRecord
         end
       end
 
-      if self.registration_full? && self.registration_open?
+      if self.registration_full? && self.registration_currently_open?
         warnings[:waiting_list] = registration_full_message
       end
 
@@ -725,7 +725,7 @@ class Competition < ApplicationRecord
   end
 
   def should_render_register_v2?(user)
-    uses_new_registration_service? && user.cannot_register_for_competition_reasons(self).empty? && (registration_open? || user_can_pre_register?(user))
+    uses_new_registration_service? && user.cannot_register_for_competition_reasons(self).empty? && (registration_currently_open? || user_can_pre_register?(user))
   end
 
   before_validation :unpack_delegate_organizer_ids
@@ -953,7 +953,7 @@ class Competition < ApplicationRecord
     true
   end
 
-  def registration_open?
+  def registration_currently_open?
     use_wca_registration? && !cancelled? && !registration_not_yet_opened? && !registration_past?
   end
 
@@ -1373,7 +1373,7 @@ class Competition < ApplicationRecord
   end
 
   def orga_can_close_reg_full_limit?
-    registration_full? && registration_open?
+    registration_full? && registration_currently_open?
   end
 
   def display_name(short: false)
@@ -1839,7 +1839,7 @@ class Competition < ApplicationRecord
                allow_registration_without_qualification refund_policy_percent use_wca_registration guests_per_registration_limit venue contact
                force_comment_in_registration use_wca_registration external_registration_page guests_entry_fee_lowest_denomination guest_entry_status
                information events_per_registration_limit],
-      methods: %w[url website short_name city venue_address venue_details latitude_degrees longitude_degrees country_iso2 event_ids registration_open?
+      methods: %w[url website short_name city venue_address venue_details latitude_degrees longitude_degrees country_iso2 event_ids registration_currently_open?
                   main_event_id number_of_bookmarks using_payment_integrations? uses_qualification? uses_cutoff? competition_series_ids registration_full?],
       include: %w[delegates organizers],
     }
