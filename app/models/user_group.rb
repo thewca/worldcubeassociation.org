4a9a4464909ea02dd030482f45ff282f6b0552c8
Gregor Billing <gbilling@worldcubeassociation.org>
2024-08-07 19:00:49 +0900

    Use cached entities in existing UserGroup methods

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -102,47 +102,47 @@ class UserGroup < ApplicationRecord
   end
 
   def self.teams_committees_group_wct
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wct').user_group
+    GroupsMetadataTeamsCommittees.wct.user_group
   end
 
   def self.teams_committees_group_wcat
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wcat').user_group
+    GroupsMetadataTeamsCommittees.wcat.user_group
   end
 
   def self.teams_committees_group_wdc
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wdc').user_group
+    GroupsMetadataTeamsCommittees.wdc.user_group
   end
 
   def self.teams_committees_group_wdpc
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wdpc').user_group
+    GroupsMetadataTeamsCommittees.wdpc.user_group
   end
 
   def self.teams_committees_group_wec
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wec').user_group
+    GroupsMetadataTeamsCommittees.wec.user_group
   end
 
   def self.teams_committees_group_weat
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'weat').user_group
+    GroupsMetadataTeamsCommittees.weat.user_group
   end
 
   def self.teams_committees_group_wfc
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wfc').user_group
+    GroupsMetadataTeamsCommittees.wfc.user_group
   end
 
   def self.teams_committees_group_wmt
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wmt').user_group
+    GroupsMetadataTeamsCommittees.wmt.user_group
   end
 
   def self.teams_committees_group_wqac
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wqac').user_group
+    GroupsMetadataTeamsCommittees.wqac.user_group
   end
 
   def self.teams_committees_group_wrc
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wrc').user_group
+    GroupsMetadataTeamsCommittees.wrc.user_group
   end
 
   def self.teams_committees_group_wrt
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wrt').user_group
+    GroupsMetadataTeamsCommittees.wrt.user_group
   end
 
   def self.council_group_wac
@@ -150,23 +150,23 @@ class UserGroup < ApplicationRecord
   end
 
   def self.teams_committees_group_wst
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wst').user_group
+    GroupsMetadataTeamsCommittees.wst.user_group
   end
 
   def self.teams_committees_group_wst_admin
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wst_admin').user_group
+    GroupsMetadataTeamsCommittees.wst_admin.user_group
   end
 
   def self.teams_committees_group_wct_china
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wct_china').user_group
+    GroupsMetadataTeamsCommittees.wct_china.user_group
   end
 
   def self.teams_committees_group_wat
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wat').user_group
+    GroupsMetadataTeamsCommittees.wat.user_group
   end
 
   def self.teams_committees_group_wsot
-    GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wsot').user_group
+    GroupsMetadataTeamsCommittees.wsot.user_group
   end
 
   def self.banned_competitors_group
