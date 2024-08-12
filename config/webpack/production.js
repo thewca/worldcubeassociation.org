c9f033027a7acffd6d04d9b7585a8c8a3f48c57e
FinnIckler <finnickler@gmail.com>
2024-08-12 13:38:18 -0400

    fix setting public path

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,7 +2,8 @@ const webpackConfig = require('./serverClientOrBoth');
 
 const productionEnvOnly = (_clientWebpackConfig, _serverWebpackConfig) => {
   // place any code here that is for production only
-  _clientWebpackConfig.config.set('output.publicPath', `${process.env.BUILD_TAG}/packs`);
+  // eslint-disable-next-line no-param-reassign
+  _clientWebpackConfig.output.publicPath = `${process.env.ASSET_HOST}/${process.env.BUILD_TAG}/packs`;
 };
 
 module.exports = webpackConfig(productionEnvOnly);
