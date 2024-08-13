f7c31f78a365573f2197427b30f7cf7ccc532a64
Gregor Billing <gbilling@worldcubeassociation.org>
2024-08-13 09:57:04 +0900

    Set archived team end roles to 2024-07-31 per Board feedback

--- db/migrate/20240811094358_august24_motion_changes_wec.rb
+++ db/migrate/20240811094358_august24_motion_changes_wec.rb
@@ -4,7 +4,7 @@ class August24MotionChangesWec < ActiveRecord::Migration[7.1]
   def change
     legacy_wec = GroupsMetadataTeamsCommittees.find_by(friendly_id: 'wec')
 
-    legacy_wec.user_group.active_roles.update_all(end_date: Date.today)
+    legacy_wec.user_group.active_roles.update_all(end_date: '2024-07-31')
     legacy_wec.user_group.update!(is_active: false)
   end
 end
--- db/migrate/20240811100821_august24_motion_changes_wac.rb
+++ db/migrate/20240811100821_august24_motion_changes_wac.rb
@@ -4,7 +4,7 @@ class August24MotionChangesWac < ActiveRecord::Migration[7.1]
   def change
     legacy_wac = GroupsMetadataCouncils.find_by(friendly_id: 'wac')
 
-    legacy_wac.user_group.active_roles.update_all(end_date: Date.today)
+    legacy_wac.user_group.active_roles.update_all(end_date: '2024-07-31')
     legacy_wac.user_group.update!(is_active: false)
   end
 end
