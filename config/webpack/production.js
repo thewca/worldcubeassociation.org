653e2c56e86b9fcac0ccdc7c8318bbf854fb205d
FinnIckler <finnickler@gmail.com>
2024-08-13 11:48:04 -0400

    set NODE_ENV to dev in WCA Webpacker

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,7 +3,7 @@ const webpackConfig = require('./serverClientOrBoth');
 const productionEnvOnly = (_clientWebpackConfig, _serverWebpackConfig) => {
   // place any code here that is for production only
   // eslint-disable-next-line no-param-reassign
-  _clientWebpackConfig.output.publicPath = `${process.env.ASSET_HOST}/${process.env.BUILD_TAG}/packs`;
+  _clientWebpackConfig.output.publicPath = `${process.env.SHAKAPACKER_ASSET_HOST}/${process.env.BUILD_TAG}/packs/`;
 };
 
 module.exports = webpackConfig(productionEnvOnly);
