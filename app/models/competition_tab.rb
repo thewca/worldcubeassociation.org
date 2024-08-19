e37d6e9b0ccb6fd8e5f67b80b50574404d630679
Daniel James <djdany444@gmail.com>
2024-08-19 06:47:56 +0530

    Add validation to competition tab content to verify if the URL actually exists

--- app/models/competition_tab.rb
+++ app/models/competition_tab.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'net/http'
+require 'uri'
+
 class CompetitionTab < ApplicationRecord
   belongs_to :competition
 
@@ -32,6 +35,34 @@ class CompetitionTab < ApplicationRecord
   after_destroy :fix_display_order
   private def fix_display_order
     competition.tabs.where("display_order > ?", display_order).update_all("display_order = display_order - 1")
+  end
+
+  private def url_exists?(url, max_redirects = 5)
+    uri = URI(url)
+    Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      request = Net::HTTP::Get.new(uri)
+      response = http.request(request)
+      if (300..399).cover?(response.code.to_i) && max_redirects > 0
+        location = response['location']
+        return url_exists?(location, max_redirects - 1)
+      else
+        return response.code == '200'
+      end
+    end
+  end
+
+  validate :verify_if_urls_are_valid, on: :update
+  private def verify_if_urls_are_valid
+    base_url = Rails.application.routes.url_helpers.competitions_url + '/'
+    content.scan(/\[(.*?)\]\((.*?)\)/) do |match|
+      hyperlink = match[1]
+      unless hyperlink.starts_with?('http') # Validation is to be done only for the relative hyperlinks.
+        url = base_url + hyperlink
+        unless url_exists?(url)
+          errors.add(:content, "URL #{url} does not exist.")
+        end
+      end
+    end
   end
 
   def reorder(direction)
