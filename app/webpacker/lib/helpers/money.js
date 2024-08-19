b4770dddbe3398062d4e2d22d4f22e0c9ddeb347
FinnIckler <finnickler@gmail.com>
2024-08-19 09:51:47 -0400

    suppress default export as long as the money lib has only one function

--- app/webpacker/lib/helpers/money.js
+++ app/webpacker/lib/helpers/money.js
@@ -1,5 +1,6 @@
 import { currenciesData } from '../wca-data.js.erb';
 
+// eslint-disable-next-line import/prefer-default-export
 export function isoMoneyToHumanReadable(amount, isoCode, name = false) {
   const currency = currenciesData.byIso[isoCode];
   return `${currency.symbol}${amount
