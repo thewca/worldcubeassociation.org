03c033173b86a46e0208c4f24fbac88d434c0406
FinnIckler <finnickler@gmail.com>
2024-08-19 09:47:26 -0400

    fix total still being shown when payment integrations are not activated

--- app/webpacker/components/RegistrationsV2/RegistrationAdministration/RegistrationsAdministrationTable.jsx
+++ app/webpacker/components/RegistrationsV2/RegistrationAdministration/RegistrationsAdministrationTable.jsx
@@ -7,64 +7,7 @@ import _ from 'lodash';
 import i18n from '../../../lib/i18n';
 import TableHeader from './AdministrationTableHeader';
 import TableRow from './AdministrationTableRow';
-import { isoMoneyToHumanReadable } from '../../../lib/helpers/money';
-
-function FooterContent({
-  registrations, competitionInfo,
-  eventsToggled,
-}) {
-  const newcomerCount = registrations.filter(
-    (reg) => reg.user.wca_id === undefined,
-  ).length;
-
-  const countryCount = new Set(
-    registrations.map((reg) => reg.user.country.iso2),
-  ).size;
-
-  const guestCount = _.sum(registrations.map((r) => r.guests));
-
-  const moneyCount = _.sum(registrations.filter(
-    (r) => r.payment.payment_amount_iso,
-  ).map((r) => r.payment.payment_amount_iso));
-
-  const moneyCountHumanReadable = isoMoneyToHumanReadable(
-    moneyCount,
-    competitionInfo.currency_code,
-  );
-
-  const eventCounts = Object.fromEntries(
-    competitionInfo.event_ids.map((evt) => {
-      const competingCount = registrations.filter(
-        (reg) => reg.competing.event_ids.includes(evt),
-      ).length;
-
-      return [evt, competingCount];
-    }),
-  );
-
-  return (
-    <Table.Row>
-      <Table.Cell colSpan={4}>
-        {`${newcomerCount} First-Timers + ${
-          registrations.length - newcomerCount
-        } Returners = ${registrations.length} People`}
-      </Table.Cell>
-      <Table.Cell>{`${countryCount}  Countries`}</Table.Cell>
-      <Table.Cell />
-      <Table.Cell>{moneyCountHumanReadable}</Table.Cell>
-      { eventsToggled ? competitionInfo.event_ids.map((evt) => (
-        <Table.Cell key={`footer-count-${evt}`}>
-          {eventCounts[evt]}
-        </Table.Cell>
-      )) : <Table.Cell />}
-      <Table.Cell>{guestCount}</Table.Cell>
-      <Table.Cell />
-      <Table.Cell />
-      <Table.Cell />
-      <Table.Cell />
-    </Table.Row>
-  );
-}
+import RegistrationAdministrationTableFooter from './RegistrationAdministrationTableFooter';
 
 export default function RegistrationAdministrationTable({
   columnsExpanded,
@@ -139,7 +82,7 @@ export default function RegistrationAdministrationTable({
         </Droppable>
       </DragDropContext>
       <TableFooter>
-        <FooterContent
+        <RegistrationAdministrationTableFooter
           registrations={registrations}
           competitionInfo={competitionInfo}
           eventsToggled={columnsExpanded.events}
