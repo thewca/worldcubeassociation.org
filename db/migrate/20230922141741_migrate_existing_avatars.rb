238ea8092b4750825e8200bc5d6dddb1b0aadb57
Gregor Billing <gbilling@worldcubeassociation.org>
2024-08-29 16:13:33 +0900

    Make avatar porting migration much more efficient

--- db/migrate/20230922141741_migrate_existing_avatars.rb
+++ db/migrate/20230922141741_migrate_existing_avatars.rb
@@ -8,34 +8,44 @@ class MigrateExistingAvatars < ActiveRecord::Migration[7.0]
     add_column :users, :current_avatar_id, :bigint, after: :legacy_avatar
     add_column :users, :pending_avatar_id, :bigint, after: :current_avatar_id
 
-    User.where.not(legacy_avatar: nil).find_each do |user|
-      avatar = UserAvatar.create!({
-                                    user_id: user.id,
-                                    filename: user.legacy_avatar,
-                                    status: UserAvatar.statuses[:approved],
-                                    thumbnail_crop_x: user.saved_avatar_crop_x,
-                                    thumbnail_crop_y: user.saved_avatar_crop_y,
-                                    thumbnail_crop_w: user.saved_avatar_crop_w,
-                                    thumbnail_crop_h: user.saved_avatar_crop_h,
-                                    backend: UserAvatar.backends[:s3_legacy_cdn],
-                                  })
+    reversible do |direction|
+      direction.up do
+        execute <<-SQL
+            INSERT INTO user_avatars
+            (user_id, filename, status, thumbnail_crop_x, thumbnail_crop_y, thumbnail_crop_w, thumbnail_crop_h, backend, created_at, updated_at)
+            SELECT id, legacy_avatar, 'approved', saved_avatar_crop_x, saved_avatar_crop_y, saved_avatar_crop_w, saved_avatar_crop_h, 's3-legacy-cdn', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+            FROM users
+            WHERE legacy_avatar IS NOT NULL
+        SQL
 
-      user.update_attribute :current_avatar, avatar
-    end
+        execute <<-SQL
+            UPDATE users
+            INNER JOIN user_avatars
+                ON users.id = user_avatars.user_id
+                    AND user_avatars.status = 'approved'
+            SET users.current_avatar_id = user_avatars.id
+        SQL
 
-    User.where.not(legacy_pending_avatar: nil).find_each do |user|
-      avatar = UserAvatar.create!({
-                                    user_id: user.id,
-                                    filename: user.legacy_pending_avatar,
-                                    status: UserAvatar.statuses[:pending],
-                                    thumbnail_crop_x: user.saved_pending_avatar_crop_x,
-                                    thumbnail_crop_y: user.saved_pending_avatar_crop_y,
-                                    thumbnail_crop_w: user.saved_pending_avatar_crop_w,
-                                    thumbnail_crop_h: user.saved_pending_avatar_crop_h,
-                                    backend: UserAvatar.backends[:s3_legacy_cdn],
-                                  })
+        execute <<-SQL
+            INSERT INTO user_avatars
+            (user_id, filename, status, thumbnail_crop_x, thumbnail_crop_y, thumbnail_crop_w, thumbnail_crop_h, backend, created_at, updated_at)
+            SELECT id, legacy_pending_avatar, 'pending', saved_pending_avatar_crop_x, saved_pending_avatar_crop_y, saved_pending_avatar_crop_w, saved_pending_avatar_crop_h, 's3-legacy-cdn', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+            FROM users
+            WHERE legacy_pending_avatar IS NOT NULL
+        SQL
 
-      user.update_attribute :pending_avatar, avatar
+        execute <<-SQL
+            UPDATE users
+            INNER JOIN user_avatars
+                ON users.id = user_avatars.user_id
+                    AND user_avatars.status = 'pending'
+            SET users.pending_avatar_id = user_avatars.id
+        SQL
+      end
+
+      direction.down do
+        UserAvatar.delete_all
+      end
     end
   end
 end
