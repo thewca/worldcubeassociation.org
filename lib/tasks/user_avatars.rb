62dceec09327141f8ea7dffac26c44561c2b9a54
Gregor Billing <gbilling@worldcubeassociation.org>
2024-08-29 23:17:08 +0900

    De-duplicate migration task code

--- lib/tasks/user_avatars.rb
+++ lib/tasks/user_avatars.rb
@@ -1,23 +1,30 @@
 # frozen_string_literal: true
 
+MASTER_PREFIX = 'uploads/user/avatar/'.freeze
+PREFIX_REGEX = /\/([1-9][[:digit:]]{3}[[:upper:]]{4}[[:digit:]]{2})\/.*\/([[:alnum:]]+\.[[:alnum:]]+)/.freeze
+
+def s3_active_storage
+  ActiveStorage::Blob.services.fetch(EnvConfig.AVATARS_PUBLIC_STORAGE)
+end
+
+def public_s3_bucket
+  # Break into the ActiveStorage S3 implementation which holds an internal, fully initialized SDK resource
+  s3_active_storage.send(:bucket)
+end
+
+def list_avatar_files
+  public_s3_bucket.objects({ prefix: MASTER_PREFIX })
+end
+
 namespace :user_avatars do
   desc "Migrate old, deleted S3 avatars"
   task :migrate_deleted => :environment do
-    service = ActiveStorage::Blob.services.fetch(EnvConfig.AVATARS_PUBLIC_STORAGE)
-
-    s3_bucket = service.send(:bucket)
-
-    master_prefix = 'uploads/user/avatar/'
-    prefix_regex = /\/([1-9][[:digit:]]{3}[[:upper:]]{4}[[:digit:]]{2})\/.*\/([[:alnum:]]+\.[[:alnum:]]+)/
-
-    files = s3_bucket.objects({ prefix: master_prefix, delimiter: '/' })
-
     user_cache = {}
 
-    files.each do |f|
-      return if f.key == master_prefix
+    list_avatar_files.each do |f|
+      return if f.key == MASTER_PREFIX
 
-      prefix_regex.match(f.key) do |match|
+      PREFIX_REGEX.match(f.key) do |match|
         wca_id = match[1]
         filename = match[2]
 
@@ -38,34 +45,27 @@ namespace :user_avatars do
             status: is_rejected ? UserAvatar.statuses[:rejected] : UserAvatar.statuses[:deprecated],
           )
 
+          downloaded_image = StringIO.new(s3_active_storage.download(f.key))
+
           historic_avatar.attach_image(
-            io: service.download(f.key),
+            io: downloaded_image,
             filename: filename,
           )
         end
 
-        service.delete(f.key)
+        s3_active_storage.delete(f.key)
       end
     end
   end
 
   desc "Migrate current avatars"
   task :migrate_current => :environment do
-    service = ActiveStorage::Blob.services.fetch(EnvConfig.AVATARS_PUBLIC_STORAGE)
-
-    s3_bucket = service.send(:bucket)
-
-    master_prefix = 'uploads/user/avatar/'
-    prefix_regex = /\/([1-9][[:digit:]]{3}[[:upper:]]{4}[[:digit:]]{2})\/.*\/([[:alnum:]]+\.[[:alnum:]]+)/
-
-    files = s3_bucket.objects({ prefix: master_prefix, delimiter: '/' })
-
     user_cache = {}
 
-    files.each do |f|
-      return if f.key == master_prefix
+    list_avatar_files.each do |f|
+      return if f.key == MASTER_PREFIX
 
-      prefix_regex.match(f.key) do |match|
+      PREFIX_REGEX.match(f.key) do |match|
         wca_id = match[1]
         filename = match[2]
 
@@ -83,12 +83,14 @@ namespace :user_avatars do
 
         return unless matching_avatar.present?
 
+        downloaded_image = StringIO.new(s3_active_storage.download(f.key))
+
         matching_avatar.attach_image(
-          io: service.download(f.key),
+          io: downloaded_image,
           filename: filename,
         )
 
-        service.delete(f.key)
+        s3_active_storage.delete(f.key)
 
         matching_avatar.update!(backend: 'active-storage')
       end
