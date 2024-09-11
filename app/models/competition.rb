740bb160713bebfc6386b5903e9a445098860363
FinnIckler <finnickler@gmail.com>
2024-09-11 18:55:46 +0200

    run rubocop

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -191,7 +191,7 @@ class Competition < ApplicationRecord
   validates_numericality_of :events_per_registration_limit, only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: :number_of_events, allow_blank: true, if: :event_restrictions?
   validates :id, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: MAX_ID_LENGTH },
                  format: { with: VALID_ID_RE }, if: :name_valid_or_updating?
-  validates :payment_information, presence: true, acceptance: { accept: true, message: I18n.t('competitions.errors.must_specify_payment_info_if_external')}, if: :needs_payment_information?
+  validates :payment_information, presence: true, acceptance: { accept: true, message: I18n.t('competitions.errors.must_specify_payment_info_if_external') }, if: :needs_payment_information?
   private def needs_payment_information?
     !using_payment_integrations? && base_entry_fee_lowest_denomination > 0
   end
