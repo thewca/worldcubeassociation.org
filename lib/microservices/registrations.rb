650240724eabf853044f8b773c3148b17d5a81f1
FinnIckler <finnickler@gmail.com>
2024-09-14 13:49:23 +0200

    allow V2 Competitions to add On the Spot Registrations

--- lib/microservices/registrations.rb
+++ lib/microservices/registrations.rb
@@ -31,6 +31,10 @@ module Microservices
       "/api/internal/v1/#{attendee_id}"
     end
 
+    def self.add_registration_path(competition_id)
+      "/api/internal/v1/#{competition_id}/add"
+    end
+
     def self.get_competitor_count_path(competition_id)
       "/api/v1/#{competition_id}/count"
     end
@@ -46,6 +50,19 @@ module Microservices
         headers: { Microservices::Auth::MICROSERVICE_AUTH_HEADER => Microservices::Auth.get_wca_token },
         &FaradayConfig
       )
+    end
+
+    def self.add_registration(competition_id, user_id, event_ids, comment, competing_status, current_user)
+      response = self.registration_connection.post(self.add_registration_path(competition_id)) do |req|
+        req.body = { competition_id: competition_id,
+                     user_id: user_id,
+                     event_ids: event_ids,
+                     comment: comment,
+                     competing_status: competing_status,
+                     current_user: current_user }.to_json
+      end
+      raise I18n.t("registrations.add.errors.already_registered") unless response.success?
+      response.body
     end
 
     def self.registrations_by_user(user_id, cache: true)
