7b7efaad26c9fd541e51413b538a55b9a54c2fd8
Gregor Billing <gbilling@worldcubeassociation.org>
2024-09-20 21:18:33 +0900

    Compute raw record references as part of CAD

--- lib/auxiliary_data_computation.rb
+++ lib/auxiliary_data_computation.rb
@@ -5,6 +5,7 @@ module AuxiliaryDataComputation
     self.compute_concise_results
     self.compute_rank_tables
     self.unfold_result_attempts
+    self.store_raw_records
   end
 
   ## Build 'concise results' tables.
@@ -123,6 +124,17 @@ module AuxiliaryDataComputation
       ActiveRecord::Base.connection.execute <<-SQL
         INSERT INTO #{temp_table_name} (result_id, idx, value)
         #{subquery}
+      SQL
+    end
+  end
+
+  def self.store_raw_records
+    DbHelper.with_temp_table('auxiliary_raw_records') do |temp_table_name|
+      ActiveRecord::Base.connection.execute <<-SQL
+        INSERT INTO #{temp_table_name} (result_id, type, value, record_name)
+        (SELECT Results.id AS result_id, 'single' AS type, best AS value, regionalSingleRecord AS record_name FROM Results WHERE regionalSingleRecord<>'')
+        UNION ALL
+        (SELECT Results.id AS result_id, 'average' AS type, average AS value, regionalAverageRecord AS record_name FROM Results WHERE regionalAverageRecord<>'')
       SQL
     end
   end
