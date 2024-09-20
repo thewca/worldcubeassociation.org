f883b6f1a663e6699cada0a30ee282d910d25a4c
Gregor Billing <gbilling@worldcubeassociation.org>
2024-09-20 21:25:33 +0900

    Move auxiliary_* rankings/records tables from CAD to CRR

--- lib/auxiliary_data_computation.rb
+++ lib/auxiliary_data_computation.rb
@@ -4,8 +4,6 @@ module AuxiliaryDataComputation
   def self.compute_everything
     self.compute_concise_results
     self.compute_rank_tables
-    self.unfold_result_attempts
-    self.store_raw_records
   end
 
   ## Build 'concise results' tables.
@@ -106,36 +104,6 @@ module AuxiliaryDataComputation
           end
         end
       end
-    end
-  end
-
-  def self.unfold_result_attempts
-    DbHelper.with_temp_table('auxiliary_result_attempts') do |temp_table_name|
-      subqueries = (1..5).map do |i|
-        <<-SQL
-          SELECT id, #{i} AS idx, value#{i} AS value
-          FROM Results
-          WHERE value#{i} > 0
-        SQL
-      end
-
-      subquery = "(" + subqueries.join(") UNION ALL (") + ")"
-
-      ActiveRecord::Base.connection.execute <<-SQL
-        INSERT INTO #{temp_table_name} (result_id, idx, value)
-        #{subquery}
-      SQL
-    end
-  end
-
-  def self.store_raw_records
-    DbHelper.with_temp_table('auxiliary_raw_records') do |temp_table_name|
-      ActiveRecord::Base.connection.execute <<-SQL
-        INSERT INTO #{temp_table_name} (result_id, type, value, record_name)
-        (SELECT Results.id AS result_id, 'single' AS type, best AS value, regionalSingleRecord AS record_name FROM Results WHERE regionalSingleRecord<>'')
-        UNION ALL
-        (SELECT Results.id AS result_id, 'average' AS type, average AS value, regionalAverageRecord AS record_name FROM Results WHERE regionalAverageRecord<>'')
-      SQL
     end
   end
 end
--- lib/records_rankings_computation.rb
+++ lib/records_rankings_computation.rb
@@ -2,13 +2,37 @@
 
 module RecordsRankingsComputation
   def self.compute_everything
-    self.compute_records_tables
-    self.compute_ranking_tables
+    self.unfold_result_attempts
+    self.store_raw_records
   end
 
-  def self.compute_records_tables
+  def self.unfold_result_attempts
+    DbHelper.with_temp_table('auxiliary_result_attempts') do |temp_table_name|
+      subqueries = (1..5).map do |i|
+        <<-SQL
+          SELECT id, #{i} AS idx, value#{i} AS value
+          FROM Results
+          WHERE value#{i} > 0
+        SQL
+      end
+
+      subquery = "(" + subqueries.join(") UNION ALL (") + ")"
+
+      ActiveRecord::Base.connection.execute <<-SQL
+        INSERT INTO #{temp_table_name} (result_id, idx, value)
+        #{subquery}
+      SQL
+    end
   end
 
-  def self.compute_ranking_tables
+  def self.store_raw_records
+    DbHelper.with_temp_table('auxiliary_raw_records') do |temp_table_name|
+      ActiveRecord::Base.connection.execute <<-SQL
+        INSERT INTO #{temp_table_name} (result_id, type, value, record_name)
+        (SELECT Results.id AS result_id, 'single' AS type, best AS value, regionalSingleRecord AS record_name FROM Results WHERE regionalSingleRecord<>'')
+        UNION ALL
+        (SELECT Results.id AS result_id, 'average' AS type, average AS value, regionalAverageRecord AS record_name FROM Results WHERE regionalAverageRecord<>'')
+      SQL
+    end
   end
 end
