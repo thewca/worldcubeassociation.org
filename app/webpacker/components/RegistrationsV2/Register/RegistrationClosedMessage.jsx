72786e7277e73737604cb2d7b1a04dee7281aee5
FinnIckler <finnickler@gmail.com>
2024-09-20 18:51:07 +0200

    Update app/webpacker/components/RegistrationsV2/Register/RegistrationClosedMessage.jsx
    Co-authored-by: Kevin Matthews <49137025+kr-matthews@users.noreply.github.com>

--- app/webpacker/components/RegistrationsV2/Register/RegistrationClosedMessage.jsx
+++ app/webpacker/components/RegistrationsV2/Register/RegistrationClosedMessage.jsx
@@ -40,7 +40,7 @@ export default function RegistrationClosedMessage({
 
   return (
     <Message color="blue">
-      { timeLeft.hours < 1
+      { timeLeft.days < 1 && timeLeft.hours < 1
         ? <I18nHTMLTranslate i18nKey="competitions.registration_v2.register.will_open_countdown" options={{ minutes: timeLeft.minutes, seconds: timeLeft.seconds }} />
         : <I18nHTMLTranslate i18nKey="registrations.will_open_html" options={{ days: start.toRelative(), time: DateTime.fromISO(registrationStart).toLocaleString(DateTime.DATETIME_FULL) }} />}
     </Message>
