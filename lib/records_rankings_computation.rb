1214285fb2e4ad915e6ef900278608861e24a3fd
Gregor Billing <gbilling@worldcubeassociation.org>
2024-09-20 12:52:22 +0900

    Add job to precompute records and rankings tables

---
+++ lib/records_rankings_computation.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module RecordsRankingsComputation
+  def self.compute_everything
+    self.compute_records_tables
+    self.compute_ranking_tables
+  end
+
+  def self.compute_records_tables
+  end
+
+  def self.compute_ranking_tables
+  end
+end
