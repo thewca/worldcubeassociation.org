52ba83ba2f2ddd1f3976bf03f87a69fff5454016
Kevin Matthews <kvnrmatthews@gmail.com>
2024-10-04 16:22:38 -0700

    Remove EditVenues component from EditSchedule

--- app/webpacker/components/EditSchedule/index.js
+++ app/webpacker/components/EditSchedule/index.js
@@ -2,11 +2,9 @@ import React, {
   useCallback,
   useEffect,
   useMemo,
-  useState,
 } from 'react';
 
 import {
-  Accordion,
   Button,
   Container,
   Message,
@@ -19,12 +17,10 @@ import { changesSaved } from './store/actions';
 import wcifScheduleReducer from './store/reducer';
 import Store, { useDispatch, useStore } from '../../lib/providers/StoreProvider';
 import ConfirmProvider from '../../lib/providers/ConfirmProvider';
-import EditVenues from './EditVenues';
 import EditActivities from './EditActivities';
 
 function EditSchedule({
   wcifEvents,
-  countryZones,
   referenceTime,
   calendarLocale,
 }) {
@@ -35,8 +31,6 @@ function EditSchedule({
   } = useStore();
 
   const dispatch = useDispatch();
-
-  const [openAccordion, setOpenAccordion] = useState(-1);
 
   const unsavedChanges = useMemo(() => (
     !_.isEqual(wcifSchedule, initialWcifSchedule)
@@ -89,69 +83,21 @@ function EditSchedule({
   const renderIntroductionMessage = () => (
     <Container text>
       <p>
-        Depending on the size and setup of the competition, it may take place in
-        several rooms of several venues.
-        Therefore a schedule is necessarily linked to a specific room.
-        Each room may have its own schedule (with all or a subset of events).
-        So you can start creating the competition&rsquo;s schedule below by adding at
-        least one venue with one room.
-        Then you will be able to select this room in the &quot;Edit schedules&quot;
-        panel, and drag and drop event rounds (or attempts for some events) on it.
-      </p>
-      <p>
-        For the typical simple competition, creating one &quot;Main venue&quot;
-        with one &quot;Main room&quot; is enough.
-        If your competition has a single venue but multiple &quot;stages&quot; with different
-        schedules, please input them as different rooms.
+        To create a schedule, first visit the &quot;Manage venues&quot; panel to add venues and rooms/stages.
       </p>
     </Container>
   );
-
-  const handleAccordionClick = (evt, titleProps) => {
-    const { index } = titleProps;
-    const newIndex = openAccordion === index ? -1 : index;
-
-    setOpenAccordion(newIndex);
-  };
 
   return (
     <>
       {renderIntroductionMessage()}
       <div>
         {unsavedChanges && renderUnsavedChangesAlert()}
-        <Accordion fluid styled>
-          <Accordion.Title
-            index={0}
-            active={openAccordion === 0}
-            onClick={handleAccordionClick}
-          >
-            Edit venues information
-          </Accordion.Title>
-          <Accordion.Content
-            active={openAccordion === 0}
-          >
-            <EditVenues
-              countryZones={countryZones}
-              referenceTime={referenceTime}
-            />
-          </Accordion.Content>
-          <Accordion.Title
-            index={1}
-            active={openAccordion === 1}
-            onClick={handleAccordionClick}
-          >
-            Edit schedules
-          </Accordion.Title>
-          <Accordion.Content
-            active={openAccordion === 1}
-          >
-            <EditActivities
-              wcifEvents={wcifEvents}
-              referenceTime={referenceTime}
-              calendarLocale={calendarLocale}
-            />
-          </Accordion.Content>
-        </Accordion>
+          <EditActivities
+            wcifEvents={wcifEvents}
+            referenceTime={referenceTime}
+            calendarLocale={calendarLocale}
+          />
         {unsavedChanges && renderUnsavedChangesAlert()}
       </div>
     </>
@@ -162,7 +108,6 @@ export default function Wrapper({
   competitionId,
   wcifEvents,
   wcifSchedule,
-  countryZones,
   referenceTime,
   calendarLocale,
 }) {
@@ -178,7 +123,6 @@ export default function Wrapper({
       <ConfirmProvider>
         <EditSchedule
           wcifEvents={wcifEvents}
-          countryZones={countryZones}
           referenceTime={referenceTime}
           calendarLocale={calendarLocale}
         />
