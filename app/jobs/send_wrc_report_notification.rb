948a0f1e6cfc559e55e599cd004fb1978de9b97e
Duncan <duncanonthejob@gmail.com>
2024-10-04 08:16:31 +0200

    rubocp

--- app/jobs/send_wrc_report_notification.rb
+++ app/jobs/send_wrc_report_notification.rb
@@ -4,7 +4,7 @@ class SendWrcReportNotification < WcaCronjob
   def perform(competition)
     delegate_report = competition.delegate_report
 
-    response = Faraday.post(EnvConfig.WRC_WEBHOOK_URL) do |req|
+    Faraday.post(EnvConfig.WRC_WEBHOOK_URL) do |req|
       req.headers['Content-Type'] = 'application/json'
       req.headers['Authorization'] = "Basic " + Base64.strict_encode64(
         "#{EnvConfig.WRC_WEBHOOK_USERNAME}:#{EnvConfig.WRC_WEBHOOK_PASSWORD}",
