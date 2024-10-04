f6a2bc9bd7193f6c5b0e8712c7ade3c8749976dc
Kevin Matthews <kvnrmatthews@gmail.com>
2024-10-04 16:28:40 -0700

    Remove unused schedule utils

--- app/webpacker/components/EditSchedule/utils.js
+++ app/webpacker/components/EditSchedule/utils.js
@@ -1,6 +1,5 @@
 import {
   addIsoDurations,
-  changeTimezoneKeepingLocalTime,
   millisecondsBetween,
   moveByIsoDuration,
   rescaleIsoDuration,
@@ -91,16 +90,3 @@ export const scaleActivitiesByDuration = (activity, isoDeltaStart, isoDeltaEnd) 
     }),
   });
 };
-
-export const changeActivityTimezone = (activity, oldTimezone, newTimezone) => ({
-  ...activity,
-  startTime: changeTimezoneKeepingLocalTime(activity.startTime, oldTimezone, newTimezone),
-  endTime: changeTimezoneKeepingLocalTime(activity.endTime, oldTimezone, newTimezone),
-  childActivities: activity.childActivities.map((childActivity) => (
-    changeActivityTimezone(
-      childActivity,
-      oldTimezone,
-      newTimezone,
-    )
-  )),
-});
