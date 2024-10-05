2850f2420e27cec0ac280f0c86aefbd92abf103e
Kevin Matthews <kvnrmatthews@gmail.com>
2024-10-05 11:50:07 -0700

    Remove venue tests from schedule spec

--- spec/features/competition_manage_schedule_spec.rb
+++ spec/features/competition_manage_schedule_spec.rb
@@ -9,37 +9,6 @@ RSpec.feature "Competition schedule management" do
     allow_any_instance_of(ActionController::Base).to receive(:protect_against_forgery?).and_return(true)
   end
 
-  context "unconfirmed competition without schedule" do
-    let!(:competition) { FactoryBot.create(:competition, :with_delegate, :registration_open, event_ids: ["333", "444"], with_rounds: true) }
-    background do
-      sign_in competition.delegates.first
-      visit "/competitions/#{competition.id}/schedule/edit"
-    end
-
-    scenario "can add a venue and a room", js: true do
-      find("div", class: 'title', text: 'Edit venues information').click
-
-      within(:css, "#venues-edit-panel-body") do
-        click_button "Add a venue"
-        fill_in("venue-name", with: "Venue")
-        click_button "Add room"
-        fill_in("room-name", with: "Youpitralala")
-        within(:css, "div[name='timezone'][role='listbox']>div.menu", visible: :all) do
-          # Using a timezone that does not follow Daylight Savings, so that we get consistent results all year round
-          find("div", class: "item", text: "Asia/Tokyo (Japan Standard Time, UTC+9)", visible: :all).trigger(:click)
-        end
-        within(:css, "div[name='countryIso2'][role='combobox']>div.menu[role='listbox']", visible: :all) do
-          find("div", class: "item", text: "United States", visible: :all).trigger(:click)
-        end
-      end
-
-      save_schedule_react
-
-      expect(competition.competition_venues.map(&:name)).to match_array %w(Venue)
-      expect(competition.competition_venues.flat_map(&:venue_rooms).map(&:name)).to match_array %w(Youpitralala)
-    end
-  end
-
   context "unconfirmed competition with schedule" do
     let!(:competition) { FactoryBot.create(:competition, :with_delegate, :registration_open, :with_valid_schedule, event_ids: ["333", "444"]) }
     background do
