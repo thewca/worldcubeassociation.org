bdb801cd434bf7743e47bb47a0f13fca7b5f98ac
Kevin Matthews <kvnrmatthews@gmail.com>
2024-10-05 11:51:06 -0700

    Remove schedule tests from venues spec

--- spec/features/competition_manage_venues_spec.rb
+++ spec/features/competition_manage_venues_spec.rb
@@ -33,35 +33,16 @@ RSpec.feature "Competition venues management" do
         end
       end
 
-      save_schedule_react
+      save_venues_react
 
       expect(competition.competition_venues.map(&:name)).to match_array %w(Venue)
       expect(competition.competition_venues.flat_map(&:venue_rooms).map(&:name)).to match_array %w(Youpitralala)
     end
   end
 
-  context "unconfirmed competition with schedule" do
-    let!(:competition) { FactoryBot.create(:competition, :with_delegate, :registration_open, :with_valid_schedule, event_ids: ["333", "444"]) }
-    background do
-      sign_in competition.delegates.first
-      visit "/competitions/#{competition.id}/schedule/edit"
-    end
-
-    scenario "room calendar is rendered", js: true do
-      find("div", class: 'title', text: 'Edit schedules').click
-
-      within(:css, "#schedules-edit-panel-body") do
-        # click_link doesn't work because Capybara expects links to always have an href
-        find("a", class: 'item', text: "Room 1 for venue 1").click
-        # 2 is the number of non-nested activities created by the factory
-        # Nested activity are not supported (yet) in the schedule manager
-        expect(all('.fc-event').size).to eq(2)
-      end
-    end
-  end
 end
 
-def save_schedule_react
+def save_venues_react
   first(:button, "save your changes!", visible: true).click
   # Wait for ajax to complete.
   expect(page).to have_no_content("You have unsaved changes")
