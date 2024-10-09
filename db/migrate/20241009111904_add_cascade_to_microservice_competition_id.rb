c8acd61d16b495c64ba03056a3b23ba482952afc
Duncan <duncanonthejob@gmail.com>
2024-10-09 13:41:27 +0200

    switched to on_delete: :cascade

--- db/migrate/20241009111904_add_cascade_to_microservice_competition_id.rb
+++ db/migrate/20241009111904_add_cascade_to_microservice_competition_id.rb
@@ -3,6 +3,6 @@
 class AddCascadeToMicroserviceCompetitionId < ActiveRecord::Migration[7.2]
   def change
     remove_foreign_key :microservice_registrations, :Competitions
-    add_foreign_key :microservice_registrations, :Competitions, on_update: :cascade
+    add_foreign_key :microservice_registrations, :Competitions, on_update: :cascade, on_delete: :cascade
   end
 end
