c89649d576778cd68ef46e0abe56bab6b313ecbc
FinnIckler <finnickler@gmail.com>
2024-10-26 18:24:07 +0200

    add missing comma

--- app/webpacker/components/RegistrationsV2/api/routes.js
+++ app/webpacker/components/RegistrationsV2/api/routes.js
@@ -2,14 +2,15 @@ import {
   allRegistrationsUrl, bulkUpdateRegistrationUrl,
   confirmedRegistrationsUrl, paymentTicketUrl,
   singleRegistrationUrl, submitRegistrationUrl, updateRegistrationUrl,
-  wcaRegistrationUrl
+  wcaRegistrationUrl,
 } from '../../../lib/requests/routes.js.erb';
 
+/* eslint-disable import/prefer-default-export */
 export const registrationRoutes = {
   v2: {
     confirmedRegistrationsUrl: (competitionId) => `${wcaRegistrationUrl}/api/v1/registrations/${competitionId}`,
     allRegistrationsUrl: (competitionId) => `${wcaRegistrationUrl}/api/v1/registrations/${competitionId}/admin`,
-    singleRegistrationUrl: (competitionId, userId) => `${wcaRegistrationUrl}/api/v1/register?user_id=${userId}&competition_id=${competitionId}`
+    singleRegistrationUrl: (competitionId, userId) => `${wcaRegistrationUrl}/api/v1/register?user_id=${userId}&competition_id=${competitionId}`,
     updateRegistrationUrl: `${wcaRegistrationUrl}/api/v1/register`,
     bulkUpdateRegistrationUrl: `${wcaRegistrationUrl}/api/v1/bulk_update`,
     submitRegistrationUrl: `${wcaRegistrationUrl}/api/v1/register`,
@@ -23,5 +24,5 @@ export const registrationRoutes = {
     updateRegistrationUrl,
     bulkUpdateRegistrationUrl,
     paymentTicketUrl,
-  }
-}
+  },
+};
