e752244db4c18216d2485b35ad27d17e87b06247
FinnIckler <finnickler@gmail.com>
2024-10-28 14:34:19 +0100

    Introduce registration version column instead of uses_v2_registrations (#10132)
    * introduce registration version column
    
    * rubocop
    
    * fix sanitizer
    
    * use integer in enum
    
    * set default value for enum and correct setting registration version
    
    * use new column in form
    
    * eslint
    
    * remove last references to uses_v2_registrations
    
    * set registration_version to v1 in factory
    
    * Change back occurences of registration_version to less intrusive checkbox
    
    * Further distinguish between V2 and V3 in convenience methods
    
    ---------
    
    Co-authored-by: Gregor Billing <gbilling@worldcubeassociation.org>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_09_111904) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_25_161404) do
   create_table "Competitions", id: { type: :string, limit: 32, default: "" }, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 50, default: "", null: false
     t.string "cityName", limit: 50, default: "", null: false
@@ -79,9 +79,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_09_111904) do
     t.integer "events_per_registration_limit"
     t.boolean "force_comment_in_registration"
     t.integer "posting_by"
-    t.boolean "uses_v2_registrations", default: false, null: false
     t.boolean "forbid_newcomers", default: false, null: false
     t.string "forbid_newcomers_reason"
+    t.integer "registration_version", default: 0, null: false
     t.index ["cancelled_at"], name: "index_Competitions_on_cancelled_at"
     t.index ["countryId"], name: "index_Competitions_on_countryId"
     t.index ["end_date"], name: "index_Competitions_on_end_date"
