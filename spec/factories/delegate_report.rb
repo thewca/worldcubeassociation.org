b206f9d3760a9959a8bfd35139724ca72829dae8
Gregor Billing <gbilling@worldcubeassociation.org>
2024-10-29 15:50:10 +0900

    Add dummy images in DR test

--- spec/factories/delegate_report.rb
+++ spec/factories/delegate_report.rb
@@ -8,10 +8,28 @@ FactoryBot.define do
       schedule_url { "http://example.com" }
       posted_at { Time.now }
       posted_by_user { FactoryBot.create(:user) }
+      upload_files { true }
+    end
+
+    transient do
+      upload_files { false }
     end
 
     initialize_with do
       competition.delegate_report
+    end
+
+    after(:create) do |dr, evaluator|
+      if evaluator.upload_files
+        default_io = File.open(Rails.root.join('app', 'assets', 'images', 'og-wca_logo.png'), 'rb')
+
+        dr.required_setup_images_count.times do |i|
+          dr.setup_images.attach(
+            io: default_io,
+            filename: "venue_setup_#{i}.png",
+          )
+        end
+      end
     end
   end
 end
