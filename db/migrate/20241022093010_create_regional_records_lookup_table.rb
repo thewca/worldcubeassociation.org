9257ce3d39c2b7882d68c5aa7e43c6760c0b9cc3
Gregor Billing <gbilling@worldcubeassociation.org>
2024-10-29 11:28:25 +0900

    Add result ID to regional_records_lookup

--- db/migrate/20241022093010_create_regional_records_lookup_table.rb
+++ db/migrate/20241022093010_create_regional_records_lookup_table.rb
@@ -3,6 +3,7 @@
 class CreateRegionalRecordsLookupTable < ActiveRecord::Migration[7.2]
   def change
     create_table :regional_records_lookup do |t|
+      t.references :Results, foreign_key: true, null: false, type: :int
       t.string "countryId", null: false
       t.string "eventId", null: false
       t.date "competitionEndDate", null: false
@@ -12,5 +13,9 @@ class CreateRegionalRecordsLookupTable < ActiveRecord::Migration[7.2]
       t.index [:eventId, :countryId, :best, :competitionEndDate]
       t.index [:eventId, :countryId, :average, :competitionEndDate]
     end
+
+    # Small hack because Rails doesn't support custom `t.references` names
+    #   but our Results tables have their own nomenclature
+    rename_column :regional_records_lookup, :Results_id, :resultId
   end
 end
