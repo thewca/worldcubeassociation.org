68207635ce40eab43a3331d000d16889bfa7cd70
FinnIckler <finnickler@gmail.com>
2024-10-29 10:57:16 +0100

    add prepare task

--- app/jobs/add_registration_job.rb
+++ app/jobs/add_registration_job.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class AddRegistrationJob < ApplicationJob
+
+  def self.prepare_task(user_id, competition_id)
+    message_deduplication_id = "competing-registration-#{competition_id}-#{user_id}"
+    message_group_id = competition_id
+    self.set(message_group_id: message_group_id, message_deduplication_id: message_deduplication_id)
+  end
+
   def perform(lane_name, competition_id, user_id, lane_params)
     lane_model_name = lane_name.upcase_first
 
