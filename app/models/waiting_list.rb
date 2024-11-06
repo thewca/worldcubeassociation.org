8435baee294469e6f271ab29f743e3aa5b204844
Duncan <duncanonthejob@gmail.com>
2024-11-06 12:29:31 +0200

    changed registration_id to entry_id

--- app/models/waiting_list.rb
+++ app/models/waiting_list.rb
@@ -3,28 +3,28 @@
 class WaitingList < ActiveRecord::Base
   belongs_to :holder, polymorphic: true
 
-  def remove(registration_id)
-    update_column :entries, entries - [registration_id]
+  def remove(entry_id)
+    update_column :entries, entries - [entry_id]
   end
 
-  def add(registration_id)
+  def add(entry_id)
     if entries.nil?
-      update_column :entries, [registration_id]
+      update_column :entries, [entry_id]
     else
-      update_column :entries, entries + [registration_id]
+      update_column :entries, entries + [entry_id]
     end
   end
 
-  def move_to_position(registration_id, new_position)
+  def move_to_position(entry_id, new_position)
     raise ArgumentError.new('Target position out of waiting list range') if new_position > entries.length || new_position < 1
 
-    old_index = entries.find_index(registration_id)
+    old_index = entries.find_index(entry_id)
     return if old_index == new_position-1
 
     update_column :entries, entries.insert(new_position-1, entries.delete_at(old_index))
   end
 
-  def position(registration_id)
-    entries.index(registration_id) + 1
+  def position(entry_id)
+    entries.index(entry_id) + 1
   end
 end
