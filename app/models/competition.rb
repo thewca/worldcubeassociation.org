456699871273708610696203ab541e3ec2970534
Duncan <duncanonthejob@gmail.com>
2024-11-12 15:34:21 +0200

    switch reg system default to v3

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -96,7 +96,7 @@ class Competition < ApplicationRecord
     restricted: 2,
   }, prefix: true
 
-  NEW_REG_SYSTEM_DEFAULT = :v2
+  NEW_REG_SYSTEM_DEFAULT = :v3
 
   enum :registration_version, [:v1, :v2, :v3], prefix: true, default: NEW_REG_SYSTEM_DEFAULT
 
