eb277f74d34a5e32a9e0deed08656576c60a4bd4
Duncan <duncanonthejob@gmail.com>
2024-11-14 06:46:05 +0200

    update accepted scope to include competing_status

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,7 +2,11 @@
 
 class Registration < ApplicationRecord
   scope :pending, -> { where(accepted_at: nil, deleted_at: nil, is_competing: true) }
-  scope :accepted, -> { where.not(accepted_at: nil).where(deleted_at: nil) }
+  scope :accepted, -> {
+    where.not(accepted_at: nil)
+      .where(deleted_at: nil)
+      .or(where(competing_status: 'accepted'))
+  }
   scope :deleted, -> { where.not(deleted_at: nil) }
   scope :cancelled, -> { where(competing_status: 'cancelled') }
   scope :rejected, -> { where(competing_status: 'rejected') }
