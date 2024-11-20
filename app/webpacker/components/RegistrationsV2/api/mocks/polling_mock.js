a4620cc16dd8a350573c1c9042fdbb28068bf6b2
FinnIckler <finnickler@gmail.com>
2024-11-20 12:00:00 +0100

    fix updating and polling mock

--- app/webpacker/components/RegistrationsV2/api/mocks/polling_mock.js
+++ app/webpacker/components/RegistrationsV2/api/mocks/polling_mock.js
@@ -2,9 +2,10 @@ import { getSingleRegistration } from '../registration/get/get_registrations';
 
 export default async function pollingMock(
   userId,
+  competition,
 ) {
   // Now that we are doing more things on Registration create we have to poll ourselves
-  const registration = await getSingleRegistration(userId);
+  const registration = await getSingleRegistration(userId, competition);
 
   return {
     processing: !registration,
