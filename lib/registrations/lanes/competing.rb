1572301475a97f2732b8c495fe7b3dd41513bbf3
Duncan <duncanonthejob@gmail.com>
2024-11-21 14:57:52 +0200

    added mailer job but untested

--- lib/registrations/lanes/competing.rb
+++ lib/registrations/lanes/competing.rb
@@ -15,7 +15,9 @@ module Registrations
         end)
         changes = registration.changes.transform_values { |change| change[1] }
         changes[:event_ids] = lane_params[:competing][:event_ids]
-        registration.save!
+        if registration.save!
+          RegistrationsMailer.notify_organizers_of_new_registration(@registration).deliver_later
+          RegistrationsMailer.notify_registrant_of_new_registration(@registration).deliver_later
         registration.add_history_entry(changes, "worker", user_id, "Worker processed")
       end
 
