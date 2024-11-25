8d8a65cf49b5456ce1bf164db1e4351efd34100e
Gregor Billing <gbilling@worldcubeassociation.org>
2024-11-25 18:37:43 +0900

    Fix pending registration payments getting erroneously displayed in V3 panel (#10298)
    * Define proper association on refunded registration payments
    
    * Add Rake task to clean out unsuccessful Stripe RegPayments
    
    * Determine refundable payments based on RegistrationPayment entities

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -7,32 +7,32 @@ class PaymentController < ApplicationController
       competition_id, user_id = attendee_id.split("-")
       competition = Competition.find(competition_id)
 
-      registration = Registration.includes(:payment_intents).find_by(competition: competition, user_id: user_id)
+      registration = Registration.includes(:registration_payments).find_by(competition: competition, user_id: user_id)
 
       return render status: :bad_request, json: { error: "Registration not found" } unless registration.present?
 
       return render status: :unauthorized, json: { error: 'unauthorized' } unless current_user.can_manage_competition?(competition)
 
-      intents = registration.payment_intents
+      # Use `filter` here on purpose because the whole `registration_payments` list has been included above.
+      #   Using `where` would create an SQL query, but it would also break (i.e. make redundant) the `includes` call above.
+      root_payments = registration.registration_payments.filter { |rp| rp.refunded_registration_payment_id.nil? }
 
-      charges = intents.flat_map { |intent|
-        payment_provider = CompetitionPaymentIntegration::INTEGRATION_RECORD_TYPES.invert[intent.payment_record_type]
+      charges = root_payments.map { |reg_payment|
+        payment_provider = CompetitionPaymentIntegration::INTEGRATION_RECORD_TYPES.invert[reg_payment.receipt_type]
 
-        intent.payment_record.child_records.charge.map { |record|
-          available_amount = record.ruby_amount_available_for_refund
-          full_amount_ruby = StripeRecord.amount_to_ruby(record.amount_stripe_denomination, record.currency_code)
+        available_amount = reg_payment.amount_available_for_refund
+        full_amount_ruby = reg_payment.amount_lowest_denomination
 
-          human_amount_refundable = helpers.ruby_money_to_human_readable(available_amount, record.currency_code)
-          human_amount_payment = helpers.ruby_money_to_human_readable(full_amount_ruby, record.currency_code)
+        human_amount_refundable = helpers.ruby_money_to_human_readable(available_amount, reg_payment.currency_code)
+        human_amount_payment = helpers.ruby_money_to_human_readable(full_amount_ruby, reg_payment.currency_code)
 
-          {
-            payment_id: record.id,
-            payment_provider: payment_provider,
-            ruby_amount_refundable: available_amount,
-            human_amount_refundable: human_amount_refundable,
-            human_amount_payment: human_amount_payment,
-            currency_code: record.currency_code,
-          }
+        {
+          payment_id: reg_payment.receipt_id,
+          payment_provider: payment_provider,
+          ruby_amount_refundable: available_amount,
+          human_amount_refundable: human_amount_refundable,
+          human_amount_payment: human_amount_payment,
+          currency_code: reg_payment.currency_code,
         }
       }
 
