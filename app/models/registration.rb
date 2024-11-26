6fac10d8189ebf4aa5a452af6f851e8ffde4b982
Duncan <duncanonthejob@gmail.com>
2024-11-26 10:06:20 +0200

    allow user to register for another series comp if they have a pending registration

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -113,7 +113,7 @@ class Registration < ApplicationRecord
   end
 
   def might_attend?
-    waitlisted? || pending? || accepted?
+    Registrations::Helper::MIGHT_ATTEND_STATES.include?(competing_status)
   end
 
   def self.status_from_timestamp(accepted_at, deleted_at)
