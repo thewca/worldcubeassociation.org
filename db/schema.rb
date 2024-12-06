5eab8976f424e05d375daa31e16c95ca7797e3d3
Duncan <52967253+dunkOnIT@users.noreply.github.com>
2024-12-06 13:53:19 +0200

    more schema.rb reversion

--- db/schema.rb
+++ db/schema.rb
@@ -82,7 +82,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_18_145843) do
     t.boolean "forbid_newcomers", default: false, null: false
     t.string "forbid_newcomers_reason"
     t.integer "registration_version", default: 0, null: false
-    t.integer "newcomer_reserved_spots", default: 0, null: false
     t.index ["cancelled_at"], name: "index_Competitions_on_cancelled_at"
     t.index ["countryId"], name: "index_Competitions_on_countryId"
     t.index ["end_date"], name: "index_Competitions_on_end_date"
