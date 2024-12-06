827f8460e8d503b98b10bc5da8cdd83157505568
FinnIckler <finnickler@gmail.com>
2024-12-06 15:17:13 +0100

    use the correct i18n keys

--- app/webpacker/components/MyCompetitions/UpcomingCompetitionTable.jsx
+++ app/webpacker/components/MyCompetitions/UpcomingCompetitionTable.jsx
@@ -59,13 +59,13 @@ export default function UpcomingCompetitionTable({
         <Table.Row>
           { shouldShowRegistrationStatus && <Table.HeaderCell /> }
           <Table.HeaderCell>
-            {I18n.t('competitions.adjacent_competitions.name')}
+            {I18n.t('competitions.competition_info.name')}
           </Table.HeaderCell>
           <Table.HeaderCell>
-            {I18n.t('competitions.adjacent_competitions.location')}
+            {I18n.t('competitions.competition_info.location')}
           </Table.HeaderCell>
           <Table.HeaderCell>
-            {I18n.t('competitions.adjacent_competitions.date')}
+            {I18n.t('competitions.competition_info.date')}
           </Table.HeaderCell>
           <Table.HeaderCell />
           {canAdminCompetitions
