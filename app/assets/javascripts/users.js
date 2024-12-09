5a264595b1b11e1bdca4d864c5c23a875a897de1
FinnIckler <finnickler@gmail.com>
2024-12-09 14:54:34 +0100

    ignore old assets in linting

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,44 +1,44 @@
-onPage('users#edit, users#update', () => {
+onPage('users#edit, users#update', function() {
   // Hide/show avatar picker based on if the user is trying to to remove
   // the current avatar.
-  $('input#user_remove_avatar').on('change', (e) => {
-    const toDelete = e.currentTarget.checked;
+  $('input#user_remove_avatar').on("change", function(e) {
+    var toDelete = e.currentTarget.checked;
     $('.form-group.user_avatar').toggle(!toDelete);
-  }).trigger('change');
+  }).trigger("change");
 
-  const $approve_wca_id = $('#approve-wca-id');
-  const $unconfirmed_wca_id = $('#user_unconfirmed_wca_id');
-  const $unconfirmed_wca_id_profile_link = $('a#unconfirmed-wca-id-profile');
-  $approve_wca_id.on('click', (e) => {
-    $('#user_wca_id').val($unconfirmed_wca_id.val());
+  var $approve_wca_id = $('#approve-wca-id');
+  var $unconfirmed_wca_id = $("#user_unconfirmed_wca_id");
+  var $unconfirmed_wca_id_profile_link = $("a#unconfirmed-wca-id-profile");
+  $approve_wca_id.on("click", function(e) {
+    $("#user_wca_id").val($unconfirmed_wca_id.val());
     $unconfirmed_wca_id.val('');
     $unconfirmed_wca_id.trigger('input');
   });
-  $unconfirmed_wca_id.on('input', (e) => {
-    const unconfirmed_wca_id = $unconfirmed_wca_id.val();
-    $approve_wca_id.prop('disabled', !unconfirmed_wca_id);
+  $unconfirmed_wca_id.on("input", function(e) {
+    var unconfirmed_wca_id = $unconfirmed_wca_id.val();
+    $approve_wca_id.prop("disabled", !unconfirmed_wca_id);
     $unconfirmed_wca_id_profile_link.parent().toggle(!!unconfirmed_wca_id);
-    $unconfirmed_wca_id_profile_link.attr('href', `/persons/${unconfirmed_wca_id}`);
+    $unconfirmed_wca_id_profile_link.attr('href', "/persons/" + unconfirmed_wca_id);
   });
   $unconfirmed_wca_id.trigger('input');
 
   // Change the 'section' parameter when a tab is switched.
-  $('a[data-toggle="tab"]').on('show.bs.tab', function () {
-    const section = $(this).attr('href').slice(1);
-    window.wca.setUrlParams({ section });
+  $('a[data-toggle="tab"]').on('show.bs.tab', function() {
+    var section = $(this).attr('href').slice(1);
+    window.wca.setUrlParams({ section: section });
   });
   // Require the user to confirm reading guidelines.
-  $('#upload-avatar-form input[type="submit"]').on('click', (event) => {
-    const $confirmation = $('#guidelines-confirmation');
-    if (!$confirmation[0].checked) {
+  $('#upload-avatar-form input[type="submit"]').on('click', function(event) {
+    var $confirmation = $('#guidelines-confirmation');
+    if(!$confirmation[0].checked) {
       event.preventDefault();
       alert($confirmation.data('alert'));
     }
   });
 
   // Show avatar removal confirmation form
-  $(document).ready(() => {
-    $('.remove-avatar').click(() => {
+  $(document).ready(function(){
+    $('.remove-avatar').click(function(){
       $('.remove-avatar-confirm').show();
     });
   });
