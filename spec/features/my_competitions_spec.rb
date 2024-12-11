86fdac5b00ed6dcb726ab8f28b1680605be66c96
FinnIckler <finnickler@gmail.com>
2024-12-11 10:19:35 +0100

    move pending call

--- spec/features/my_competitions_spec.rb
+++ spec/features/my_competitions_spec.rb
@@ -11,11 +11,11 @@ RSpec.feature "My competitions page", clean_db_with_truncation: true do
     end
 
     context "after registring for a competition" do
-      pending("Pending until we find a way to test React components signed icklerf 10-12-2024")
       let!(:competition) { FactoryBot.create :competition, :visible, :registration_open }
       let!(:registration) { FactoryBot.create :registration, :accepted, competition: competition, user: user }
 
       scenario "the user visits his competitions page" do
+        pending("Pending until we find a way to test React components signed icklerf 10-12-2024")
         visit "/competitions/mine"
         expect(page).to have_text(competition.name)
       end
