7b3db498f75a26dc37440a5c0a7207b8e1eb2bfe
FinnIckler <finnickler@gmail.com>
2024-12-12 17:09:29 +0100

    move to grid

--- app/webpacker/components/CompetitionTabs/GeneralInfoTab.jsx
+++ app/webpacker/components/CompetitionTabs/GeneralInfoTab.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
-import { Button, Icon } from 'semantic-ui-react';
+import {
+  Button, Grid, GridColumn, GridRow, Icon,
+} from 'semantic-ui-react';
 import { DateTime } from 'luxon';
 import I18n from '../../lib/i18n';
 import { countries } from '../../lib/wca-data.js.erb';
@@ -21,51 +23,52 @@ export default function GeneralInfoTab({
   const [showHighlights, setShowHighlights] = useState(false);
 
   return (
-    <div className="row competition-info">
-      <div className="col-md-6">
-        <dl className="dl-horizontal compact">
-          <dt>{I18n.t('competitions.competition_info.date')}</dt>
-          <dd>
-            {competition.date_range}
-            <a
-              href={competitionUrl(competition.id, 'ics')}
-              title={I18n.t('competitions.competition_info.add_to_calendar')}
-              data-toggle="tooltip"
-              data-placement="top"
-              data-container="body"
-            >
-              <Icon name="calendar plus" />
-            </a>
-          </dd>
+    <Grid>
+      <GridRow>
+        <GridColumn width={8}>
+          <dl className="dl-horizontal compact">
+            <dt>{I18n.t('competitions.competition_info.date')}</dt>
+            <dd>
+              {competition.date_range}
+              <a
+                href={competitionUrl(competition.id, 'ics')}
+                title={I18n.t('competitions.competition_info.add_to_calendar')}
+                data-toggle="tooltip"
+                data-placement="top"
+                data-container="body"
+              >
+                <Icon name="calendar plus" />
+              </a>
+            </dd>
 
-          <dt>{I18n.t('competitions.competition_info.city')}</dt>
-          <dd>
-            {competition.city}
-            {`, ${countries.byIso2[competition.country_iso2].name}`}
-          </dd>
+            <dt>{I18n.t('competitions.competition_info.city')}</dt>
+            <dd>
+              {competition.city}
+              {`, ${countries.byIso2[competition.country_iso2].name}`}
+            </dd>
 
-          <dt>{I18n.t('competitions.competition_info.venue')}</dt>
-          <dd><Markdown md={competition.venue} id="competition-info-venue" /></dd>
+            <dt>{I18n.t('competitions.competition_info.venue')}</dt>
+            <dd><Markdown md={competition.venue} id="competition-info-venue" /></dd>
 
-          <dt className="text-muted">{I18n.t('competitions.competition_info.address')}</dt>
-          <dd>
-            <a href={linkToGoogleMapsPlace(
-              competition.latitude_degrees,
-              competition.longitude_degrees,
-            )}
-            >
-              {competition.venue_address}
-            </a>
-          </dd>
+            <dt className="text-muted">{I18n.t('competitions.competition_info.address')}</dt>
+            <dd>
+              <a href={linkToGoogleMapsPlace(
+                competition.latitude_degrees,
+                competition.longitude_degrees,
+              )}
+              >
+                {competition.venue_address}
+              </a>
+            </dd>
 
-          {competition.venue_details && (
+            {competition.venue_details && (
             <>
               <dt className="text-muted">{I18n.t('competitions.competition_info.details')}</dt>
               <dd>{competition.venue_details}</dd>
             </>
-          )}
+            )}
 
-          {competition.external_website && (
+            {competition.external_website && (
             <>
               <dt>{I18n.t('competitions.competition_info.website')}</dt>
               <dd>
@@ -74,24 +77,24 @@ export default function GeneralInfoTab({
                 </a>
               </dd>
             </>
-          )}
+            )}
 
-          <dt>{I18n.t('competitions.competition_info.contact')}</dt>
-          <dd>
-            {competition.contact ? (
-              <Markdown md={competition.contact} id="competition-info-contact" />
-            ) : (
-              <a
-                href={
+            <dt>{I18n.t('competitions.competition_info.contact')}</dt>
+            <dd>
+              {competition.contact ? (
+                <Markdown md={competition.contact} id="competition-info-contact" />
+              ) : (
+                <a
+                  href={
                   `/contact?contactRecipient=competition&competitionId=${competition.id}`
                 }
-              >
-                {I18n.t('competitions.competition_info.organization_team')}
-              </a>
-            )}
-          </dd>
+                >
+                  {I18n.t('competitions.competition_info.organization_team')}
+                </a>
+              )}
+            </dd>
 
-          {competition.organizers.length > 0 && (
+            {competition.organizers.length > 0 && (
             <>
               <dt>{I18n.t('competitions.competition_info.organizer_plural', { count: competition.organizers.length })}</dt>
               <dd>
@@ -103,20 +106,20 @@ export default function GeneralInfoTab({
                 ) : `${user.name} `))}
               </dd>
             </>
-          )}
+            )}
 
-          <dt>{I18n.t('competitions.competition_info.delegate', { count: competition.delegates.length })}</dt>
-          <dd>
-            {competition.delegates.map((user, i) => (user.wca_id ? (
-              <a href={personUrl(user.wca_id)}>
-                {user.name}
-                {i !== competition.delegates.length - 1 && ', '}
-              </a>
-            ) : `${user.name} `))}
-          </dd>
-        </dl>
+            <dt>{I18n.t('competitions.competition_info.delegate', { count: competition.delegates.length })}</dt>
+            <dd>
+              {competition.delegates.map((user, i) => (user.wca_id ? (
+                <a href={personUrl(user.wca_id)}>
+                  {user.name}
+                  {i !== competition.delegates.length - 1 && ', '}
+                </a>
+              ) : `${user.name} `))}
+            </dd>
+          </dl>
 
-        {competition['has_schedule?'] && (
+          {competition['has_schedule?'] && (
           <dl className="dl-horizontal">
             <dt><Icon name="print" /></dt>
             <dd>
@@ -138,92 +141,92 @@ export default function GeneralInfoTab({
               />
             </dd>
           </dl>
-        )}
-      </div>
+          )}
+        </GridColumn>
 
-      <div className="col-md-6">
-        <dl className="dl-horizontal">
-          <dt>{I18n.t('competitions.competition_info.information')}</dt>
-          <dd>
-            <Markdown md={competition.information} id="competition-info-information" />
-          </dd>
-        </dl>
+        <GridColumn width={8}>
+          <dl className="dl-horizontal">
+            <dt>{I18n.t('competitions.competition_info.information')}</dt>
+            <dd>
+              <Markdown md={competition.information} id="competition-info-information" />
+            </dd>
+          </dl>
 
-        <dl className="dl-horizontal">
-          <dt>{I18n.t('competitions.competition_info.events')}</dt>
-          <dd className="competition-events-list">
-            {competition.events.map((event) => (
-              <React.Fragment key={event.id}>
-                <EventIcon id={event.id} />
-              </React.Fragment>
-            ))}
-          </dd>
+          <dl className="dl-horizontal">
+            <dt>{I18n.t('competitions.competition_info.events')}</dt>
+            <dd className="competition-events-list">
+              {competition.events.map((event) => (
+                <React.Fragment key={event.id}>
+                  <EventIcon id={event.id} />
+                </React.Fragment>
+              ))}
+            </dd>
 
-          {competition.main_event_id && (
+            {competition.main_event_id && (
             <>
               <dt>{I18n.t('competitions.competition_info.main_event')}</dt>
               <dd className="competition-events-list">
                 <EventIcon id={competition.main_event_id} />
               </dd>
             </>
-          )}
+            )}
 
-          {competition['results_posted?'] && (
+            {competition['results_posted?'] && (
             <>
               <dt>{I18n.t('competitions.nav.menu.competitors')}</dt>
               <dd>{competition.competitors.length}</dd>
             </>
-          )}
-        </dl>
+            )}
+          </dl>
 
-        {(competition.media.accepted ?? []).map((mediaType) => (
-          <div className="panel panel-default" key={mediaType.type}>
-            <div className="panel-heading">
-              <h4 className="panel-title">
-                <a
-                  data-toggle="collapse"
-                  href={`#collapse-${mediaType.type}`}
-                  className="collapsed"
-                >
-                  {`${mediaType.type}s (${mediaType.items.length})`}
-                </a>
-              </h4>
-            </div>
-            <div id={`collapse-${mediaType.type}`} className="panel-collapse collapse">
-              <ul className="list-group">
-                {mediaType.items.map((item) => (
+          {(competition.media.accepted ?? []).map((mediaType) => (
+            <div className="panel panel-default" key={mediaType.type}>
+              <div className="panel-heading">
+                <h4 className="panel-title">
                   <a
-                    href={item.uri}
-                    className="list-group-item"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    key={item.text}
+                    data-toggle="collapse"
+                    href={`#collapse-${mediaType.type}`}
+                    className="collapsed"
                   >
-                    {item.text}
+                    {`${mediaType.type}s (${mediaType.items.length})`}
                   </a>
-                ))}
-              </ul>
+                </h4>
+              </div>
+              <div id={`collapse-${mediaType.type}`} className="panel-collapse collapse">
+                <ul className="list-group">
+                  {mediaType.items.map((item) => (
+                    <a
+                      href={item.uri}
+                      className="list-group-item"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      key={item.text}
+                    >
+                      {item.text}
+                    </a>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
 
-        {!competition['results_posted?'] && competition.competitor_limit_enabled && (
+          {!competition['results_posted?'] && competition.competitor_limit_enabled && (
           <dl className="dl-horizontal">
             <dt>{I18n.t('competitions.competition_info.competitor_limit')}</dt>
             <dd>{competition.competitor_limit}</dd>
           </dl>
-        )}
+          )}
 
-        {!competition['results_posted?'] && (
+          {!competition['results_posted?'] && (
           <dl className="dl-horizontal">
             <dt>{I18n.t('competitions.competition_info.number_of_bookmarks')}</dt>
             <dd>{competition.number_of_bookmarks}</dd>
           </dl>
-        )}
-      </div>
+          )}
+        </GridColumn>
 
-      <div className="col-md-12">
-        {competition.registration_open && competition.registration_close && (
+        <GridColumn width={16}>
+          {competition.registration_open && competition.registration_close && (
           <dl className="dl-horizontal">
             <dt>{I18n.t('competitions.competition_info.registration_period.label')}</dt>
             <dd>
@@ -252,35 +255,35 @@ export default function GeneralInfoTab({
               </p>
             </dd>
           </dl>
-        )}
+          )}
 
-        <dl className="dl-horizontal">
-          <dt>{I18n.t('competitions.competition_info.registration_requirements')}</dt>
-          <dd>
-            <div>
-              {showRegistrationRequirements ? (
-                <>
-                  <div>
-                    <RegistrationRequirements
-                      competition={competition}
-                      userInfo={userInfo}
-                      showLinksToRegisterPage
-                    />
-                  </div>
-                  <Button onClick={() => setShowRegistrationRequirements(false)}>
-                    {I18n.t('competitions.competition_info.hide_requirements')}
+          <dl className="dl-horizontal">
+            <dt>{I18n.t('competitions.competition_info.registration_requirements')}</dt>
+            <dd>
+              <div>
+                {showRegistrationRequirements ? (
+                  <>
+                    <div>
+                      <RegistrationRequirements
+                        competition={competition}
+                        userInfo={userInfo}
+                        showLinksToRegisterPage
+                      />
+                    </div>
+                    <Button onClick={() => setShowRegistrationRequirements(false)}>
+                      {I18n.t('competitions.competition_info.hide_requirements')}
+                    </Button>
+                  </>
+                ) : (
+                  <Button onClick={() => setShowRegistrationRequirements(true)}>
+                    {I18n.t('competitions.competition_info.click_to_display_requirements_html')}
                   </Button>
-                </>
-              ) : (
-                <Button onClick={() => setShowRegistrationRequirements(true)}>
-                  {I18n.t('competitions.competition_info.click_to_display_requirements_html')}
-                </Button>
-              )}
-            </div>
-          </dd>
-        </dl>
+                )}
+              </div>
+            </dd>
+          </dl>
 
-        {competition.userCanViewResults && (competition.main_event_id || records) && (
+          {competition.userCanViewResults && (competition.main_event_id || records) && (
           <dl className="dl-horizontal">
             <dt>{I18n.t('competitions.competition_info.highlights')}</dt>
             <dd>
@@ -303,8 +306,9 @@ export default function GeneralInfoTab({
               </div>
             </dd>
           </dl>
-        )}
-      </div>
-    </div>
+          )}
+        </GridColumn>
+      </GridRow>
+    </Grid>
   );
 }
