2143b9b3f7b0e2c5366b8dd332a278dd376952aa
FinnIckler <finnickler@gmail.com>
2024-12-17 12:13:07 +0100

    fix listing of Sibling Competitions

--- app/webpacker/components/CompetitionTabs/Requirements/CompetitionSeriesRequirement.jsx
+++ app/webpacker/components/CompetitionTabs/Requirements/CompetitionSeriesRequirement.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { List } from 'semantic-ui-react';
 import I18n from '../../../lib/i18n';
+import { competitionUrl } from '../../../lib/requests/routes.js.erb';
+import I18nHTMLTranslate from '../../I18nHTMLTranslate';
 
 export default function CompetitionSeriesRequirement({ competition }) {
   return (
     <>
-      <p>
-        {I18n.t('competitions.competition_info.part_of_a_series_list', {
-          name: competition.competition_series.name,
-        })}
-      </p>
-      <List>
+      {I18n.t('competitions.competition_info.part_of_a_series_list', {
+        name: competition.competition_series.name,
+      })}
+      <List bulleted>
         {competition.series_sibling_competitions.map((comp) => (
-          <List.Item key={comp.id}>
-            <a href={`/competitions/${comp.id}`}>{comp.name}</a>
+          <List.Item key={comp.id} style={{ marginLeft: '2em' }}>
+            <a href={competitionUrl(comp.id)}>{comp.name}</a>
           </List.Item>
         ))}
       </List>
-      <p>{I18n.t('competitions.competition_info.series_registration_warning_html')}</p>
+      <br />
+      <I18nHTMLTranslate i18nKey="competitions.competition_info.series_registration_warning_html" />
     </>
   );
 }
