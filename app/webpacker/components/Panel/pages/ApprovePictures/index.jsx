e31e465041e39ed9d88c20c88568719712fef91b
FinnIckler <finnickler@gmail.com>
2024-12-18 15:57:49 +0100

    create new Avatar Approval Component

---
+++ app/webpacker/components/Panel/pages/ApprovePictures/index.jsx
@@ -0,0 +1,103 @@
+import React, { useCallback, useState } from 'react';
+import {
+  Button,
+  CardGroup,
+  Header,
+  List,
+  Segment,
+} from 'semantic-ui-react';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import { useMutation } from '@tanstack/react-query';
+import I18n from '../../../../lib/i18n';
+import AvatarCard from './AvatarCard';
+import updateAvatars from './api/updateAvatars';
+import WCAQueryClientProvider from '../../../../lib/providers/WCAQueryClientProvider';
+
+// {
+// "avatars[68402][action]": "approve",
+// "avatars[68402][rejection_reason]": "",
+// "commit": "Submit"
+// }
+
+const transformAvatarsForRequest = (approvedAvatars, rejectedAvatars) => {
+  const avatars = {};
+
+  approvedAvatars.forEach(({ id }) => {
+    avatars[id] = { action: 'approve' };
+  });
+
+  rejectedAvatars.forEach(({ id, rejectionReason }) => {
+    avatars[id] = { action: 'reject', rejection_reason: rejectionReason };
+  });
+
+  return { avatars };
+};
+
+export default function Wrapper({ pendingUsers }) {
+  return (
+    <WCAQueryClientProvider>
+      <ApprovePictures pendingUsers={pendingUsers} />
+    </WCAQueryClientProvider>
+  );
+}
+
+function ApprovePictures({ pendingUsers }) {
+  const [approvedAvatars, setApprovedAvatarIds] = useState([]);
+  const [rejectedAvatars, setRejectedAvatarIds] = useState([]);
+  const [deferredAvatars, setDeferredAvatarIds] = useState([]);
+
+  const onApprove = useCallback((avatar) => {
+    setApprovedAvatarIds((prev) => (prev.includes(avatar.id) ? prev.filter((a) => a.id !== avatar.id)
+      : [...prev, avatar]));
+  }, [setApprovedAvatarIds]);
+
+  const onReject = useCallback((avatar, rejectionReason) => {
+    setRejectedAvatarIds((prev) => (prev.includes(avatar.id) ? prev.filter((a) => a.id !== avatar.id)
+      : [...prev, { ...avatar, rejectionReason }]));
+  }, [setRejectedAvatarIds]);
+
+  const onDefer = useCallback((avatar) => {
+    setDeferredAvatarIds((prev) => (prev.includes(avatar.id) ? prev.filter((a) => a.id !== avatar.id)
+      : [...prev, avatar]));
+  }, [setDeferredAvatarIds]);
+
+  const { mutate: decideOnAvatars } = useMutation({
+    mutationFn: () => updateAvatars(transformAvatarsForRequest(approvedAvatars, rejectedAvatars)),
+    onSuccess: (data) => {
+      setApprovedAvatarIds([]);
+      setRejectedAvatarIds([]);
+    },
+  });
+
+  return (
+    <>
+      <Segment>
+        <Header as="h3">Guidelines</Header>
+        <List bulleted>
+          {I18n.tArray('users.edit.avatar_guidelines').map((g, idx) => <List.Item key={g}>{g}</List.Item>)}
+        </List>
+        <Header as="h4">Additional guidelines for Staff Members</Header>
+        <List bulleted>
+          {I18n.tArray('users.edit.staff_avatar_guidelines.paragraphs').map((g) => <List.Item key={g}>{g}</List.Item>)}
+        </List>
+      </Segment>
+      {pendingUsers.length > 0 ? (
+        <CardGroup>
+          {pendingUsers.map((user) => (
+            <AvatarCard
+              user={user}
+              onApprove={onApprove}
+              onReject={onReject}
+              onDefer={onDefer}
+            />
+          ))}
+          <Button primary onClick={decideOnAvatars}> Submit </Button>
+        </CardGroup>
+      ) : (
+        <p>
+          No new pictures have been submitted
+        </p>
+      )}
+    </>
+  );
+}
