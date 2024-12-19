0d19eb04e9cc8463f21a3968efa2d245954ff100
FinnIckler <finnickler@gmail.com>
2024-12-19 17:34:13 +0100

    use three ifs in GuestRequirements

--- app/webpacker/components/CompetitionTabs/Requirements/GuestRequirements.jsx
+++ app/webpacker/components/CompetitionTabs/Requirements/GuestRequirements.jsx
@@ -5,25 +5,21 @@ import { isoMoneyToHumanReadable } from '../../../lib/helpers/money';
 export default function GuestRequirements({ competition }) {
   if (competition.guests_entry_fee_lowest_denomination) {
     return (
-      <>
-        {I18n.t('competitions.competition_info.guests_pay', {
-          guests_base_fee:
+      I18n.t('competitions.competition_info.guests_pay', {
+        guests_base_fee:
             isoMoneyToHumanReadable(
               competition.guests_entry_fee_lowest_denomination,
               competition.currency_code,
             ),
-        })}
-      </>
+      })
     );
   }
 
-  return (
-    <>
-      {competition['all_guests_allowed?'] ? (
-        I18n.t('competitions.competition_info.guests_free.free')
-      ) : competition['some_guests_allowed?'] && (
-        I18n.t('competitions.competition_info.guests_free.restricted')
-      )}
-    </>
-  );
+  if (competition['all_guests_allowed?']) {
+    return I18n.t('competitions.competition_info.guests_free.free');
+  }
+
+  if (competition['some_guests_allowed?']) {
+    return I18n.t('competitions.competition_info.guests_free.restricted');
+  }
 }
