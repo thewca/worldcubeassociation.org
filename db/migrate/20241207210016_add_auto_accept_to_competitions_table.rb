a695518110515026d5e458586304f39dcd4c365d
Duncan <duncanonthejob@gmail.com>
2024-12-19 12:42:09 +0200

    removed unnecessary comments from migration

--- db/migrate/20241207210016_add_auto_accept_to_competitions_table.rb
+++ db/migrate/20241207210016_add_auto_accept_to_competitions_table.rb
@@ -2,8 +2,7 @@
 
 class AddAutoAcceptToCompetitionsTable < ActiveRecord::Migration[7.2]
   def change
-    # TODO: Ask ChatGPT if these look fine
     add_column :Competitions, :auto_accept_registrations, :boolean, default: false, null: false
-    add_column :Competitions, :auto_accept_disable_threshold, :integer, default: 0, null: false # TODO: Add validation that this can't be > competitor_limit
+    add_column :Competitions, :auto_accept_disable_threshold, :integer, default: 0, null: false
   end
 end
