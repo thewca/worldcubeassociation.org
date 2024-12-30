c65c2bb49ef0e04caa714f30839b25861b64825c
Duncan <duncanonthejob@gmail.com>
2024-12-30 14:35:27 +0200

    added reg history test and retrieve user data for worker changes

--- app/webpacker/components/RegistrationsV2/RegistrationEdit/RegistrationEditor.jsx
+++ app/webpacker/components/RegistrationsV2/RegistrationEdit/RegistrationEditor.jsx
@@ -50,7 +50,9 @@ export default function RegistrationEditor({ competitor, competitionInfo }) {
 
   const { isLoading, data: competitorsInfo } = useQuery({
     queryKey: ['history-user', serverRegistration?.history],
-    queryFn: () => getUsersInfo(_.uniq(serverRegistration.history.flatMap((e) => (e.actor_type === 'user' ? Number(e.actor_id) : [])))),
+    queryFn: () => getUsersInfo(_.uniq(serverRegistration.history.flatMap((e) => (
+      (e.actor_type === 'user' || e.actor_type === 'worker') ? Number(e.actor_id) : [])
+    ))),
     enabled: Boolean(serverRegistration),
   });
 
