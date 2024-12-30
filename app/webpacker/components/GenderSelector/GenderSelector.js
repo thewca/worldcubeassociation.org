b8b6d190269f4c625a16394596fa352cfa33bbcc
Gregor Billing <gbilling@worldcubeassociation.org>
2024-12-30 23:45:41 +0900

    Localize GenderSelector label

--- app/webpacker/components/GenderSelector/GenderSelector.js
+++ app/webpacker/components/GenderSelector/GenderSelector.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import { Form } from 'semantic-ui-react';
 
 import { genders } from '../../lib/wca-data.js.erb';
+import I18n from '../../lib/i18n';
 
 const genderOptions = _.map(genders.byId, (gender) => ({
   key: gender.id,
@@ -19,7 +20,7 @@ function GenderSelector({
   return (
     <Form.Select
       name={name}
-      label="Gender"
+      label={I18n.t('activerecord.attributes.user.gender')}
       value={gender}
       options={genderOptions}
       onChange={onChange}
