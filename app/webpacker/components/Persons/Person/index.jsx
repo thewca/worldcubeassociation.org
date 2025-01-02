e593381d5115034de1b91ef31c09402385af98b1
FinnIckler <finnickler@gmail.com>
2025-01-02 17:45:00 +0100

    make person box sticky on computer and tablet only

--- app/webpacker/components/Persons/Person/index.jsx
+++ app/webpacker/components/Persons/Person/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
-  Container, Grid, GridColumn, GridRow, Segment, Tab, TabPane,
+  Container, Grid, GridColumn, GridRow, Segment, Sticky, Tab, TabPane,
 } from 'semantic-ui-react';
 import Details from './Details';
 import PersonalRecords from './PersonalRecords';
@@ -75,48 +75,62 @@ export default function Person({
 }) {
   const medalsAndRecords = (person.medals.total > 0 ? 1 : 0)
     + (person.records.total > 0 ? 1 : 0);
+  const ref = useRef();
 
   return (
-    <Container fluid>
-      <Grid columns={2} stackable>
-        <GridRow>
-          <GridColumn width={4}>
-            <Segment>
-              <Details
-                person={person}
-                canEditUser={canEditUser}
-                editUrl={editUrl}
-              />
-            </Segment>
-          </GridColumn>
-          <GridColumn width={12}>
-            <Segment>
-              <PersonalRecords
-                person={person}
-                averageRanks={person.averageRanks}
-                singleRanks={person.singleRanks}
-              />
-            </Segment>
-            {medalsAndRecords > 0 && (
-            <Segment>
-              <Grid columns={medalsAndRecords} stackable>
-                {person.medals.total > 0 && (
-                <GridColumn>
-                  <MedalCollection person={person} />
-                </GridColumn>
-                )}
-                {person.records.total > 0 && (
-                <GridColumn>
-                  <RecordCollection person={person} />
-                </GridColumn>
-                )}
-              </Grid>
-            </Segment>
-            )}
-            <TabSection person={person} />
-          </GridColumn>
-        </GridRow>
-      </Grid>
-    </Container>
+    <div ref={ref}>
+      <Container fluid>
+        <Grid columns={2} stackable>
+          <GridRow>
+            <GridColumn width={4} only="computer tablet">
+              <Sticky context={ref}>
+                <Segment raised>
+                  <Details
+                    person={person}
+                    canEditUser={canEditUser}
+                    editUrl={editUrl}
+                  />
+                </Segment>
+              </Sticky>
+            </GridColumn>
+            <GridColumn width={4} only="mobile">
+              <Segment raised>
+                <Details
+                  person={person}
+                  canEditUser={canEditUser}
+                  editUrl={editUrl}
+                />
+              </Segment>
+            </GridColumn>
+            <GridColumn width={12}>
+              <Segment raised>
+                <PersonalRecords
+                  person={person}
+                  averageRanks={person.averageRanks}
+                  singleRanks={person.singleRanks}
+                />
+              </Segment>
+              {medalsAndRecords > 0 && (
+              <Segment raised>
+                <Grid columns={medalsAndRecords} stackable>
+                  {person.medals.total > 0 && (
+                  <GridColumn>
+                    <MedalCollection person={person} />
+                  </GridColumn>
+                  )}
+                  {person.records.total > 0 && (
+                  <GridColumn>
+                    <RecordCollection person={person} />
+                  </GridColumn>
+                  )}
+                </Grid>
+              </Segment>
+              )}
+              <TabSection person={person} />
+            </GridColumn>
+          </GridRow>
+        </Grid>
+      </Container>
+    </div>
   );
 }
