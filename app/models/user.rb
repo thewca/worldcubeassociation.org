1d4297c6913df85c8e3b1dbb26a5a19eef15381d
Daniel James <djdany444@gmail.com>
2025-01-03 12:00:08 +0530

    Few more panel cleanups (#9671)
    * Few more panel cleanups
    
    * Review changes
    
    ---------
    
    Co-authored-by: Daniel M James <danieljamesin@google.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1424,10 +1424,6 @@ class User < ApplicationRecord
     end
   end
 
-  def can_access_at_least_one_panel?
-    panels_with_access.any?
-  end
-
   def subordinate_delegates
     delegate_roles
       .filter { |role| role.is_lead? }
