5b4268c245c87c259647856a01b74ccfe04c8850
Daniel James <djdany444@gmail.com>
2025-01-03 18:25:14 +0530

    Review changes

--- app/webpacker/components/ContactEditProfilePage/index.jsx
+++ app/webpacker/components/ContactEditProfilePage/index.jsx
@@ -11,19 +11,51 @@ import EditProfileForm from './EditProfileForm';
 import useLoggedInUserPermissions from '../../lib/hooks/useLoggedInUserPermissions';
 import useQueryParams from '../../lib/hooks/useQueryParams';
 import useInputState from '../../lib/hooks/useInputState';
-import WcaSearch from '../SearchWidget/WcaSearch';
+import { IdWcaSearch } from '../SearchWidget/WcaSearch';
 import SEARCH_MODELS from '../SearchWidget/SearchModel';
 
 const CONTACT_EDIT_PROFILE_QUERY_CLIENT = new QueryClient();
 
 export default function ContactEditProfilePage({ loggedInUserId, recaptchaPublicKey }) {
   const [queryParams] = useQueryParams();
+  const editOthersProfileMode = Boolean(queryParams.editOthersProfile);
+  const { data: loggedInUserData, isLoading, isError } = useQuery({
+    queryKey: ['userData'],
+    queryFn: () => fetchJsonOrError(apiV0Urls.users.me.userDetails),
+    enabled: (
+      // If not logged in, fetching WCA ID of logged in user is not possible.
+      !!loggedInUserId
+      // If the user needs to edit other's profile, then fetching own WCA ID is not needed.
+       || !editOthersProfileMode
+    ),
+  }, CONTACT_EDIT_PROFILE_QUERY_CLIENT);
+  const { loggedInUserPermissions, loading } = useLoggedInUserPermissions();
+  const [inputWcaId, setInputWcaId] = useInputState();
   const [contactSuccess, setContactSuccess] = useState(false);
+
+  const wcaId = editOthersProfileMode ? inputWcaId : loggedInUserData?.data?.user?.wca_id;
+
+  if (isLoading || loading) return <Loading />;
+  if (isError) return <Errored />;
 
   if (!loggedInUserId) {
     return (
       <Message error>
         <I18nHTMLTranslate i18nKey="page.contact_edit_profile.not_logged_in_error" />
+      </Message>
+    );
+  }
+  if (loggedInUserData && !wcaId) {
+    return (
+      <Message error>
+        <I18nHTMLTranslate i18nKey="page.contact_edit_profile.no_profile_error" />
+      </Message>
+    );
+  }
+  if (editOthersProfileMode && !loggedInUserPermissions.canRequestToEditOthersProfile) {
+    return (
+      <Message error>
+        <I18nHTMLTranslate i18nKey="page.contact_edit_profile.no_permission_error" />
       </Message>
     );
   }
@@ -36,77 +68,19 @@ export default function ContactEditProfilePage({ loggedInUserId, recaptchaPublic
     );
   }
 
-  if (queryParams.editOthersProfile) {
-    return (
-      <ContactEditProfileOthers
-        setContactSuccess={setContactSuccess}
-        recaptchaPublicKey={recaptchaPublicKey}
-      />
-    );
-  }
-  return (
-    <ContactEditProfileSelf
-      setContactSuccess={setContactSuccess}
-      recaptchaPublicKey={recaptchaPublicKey}
-    />
-  );
-}
-
-function ContactEditProfileSelf({ setContactSuccess, recaptchaPublicKey }) {
-  const { data: loggedInUserData, isLoading, isError } = useQuery({
-    queryKey: ['userData'],
-    queryFn: () => fetchJsonOrError(apiV0Urls.users.me.userDetails),
-  }, CONTACT_EDIT_PROFILE_QUERY_CLIENT);
-  const wcaId = loggedInUserData?.data?.user?.wca_id;
-
-  if (isLoading) return <Loading />;
-  if (isError) return <Errored />;
-  if (!wcaId) {
-    return (
-      <Message error>
-        <I18nHTMLTranslate i18nKey="page.contact_edit_profile.no_profile_error" />
-      </Message>
-    );
-  }
-
   return (
     <Container text>
       <Header as="h2">{I18n.t('page.contact_edit_profile.title')}</Header>
-      <EditProfileForm
-        wcaId={wcaId}
-        onContactSuccess={() => setContactSuccess(true)}
-        recaptchaPublicKey={recaptchaPublicKey}
-      />
-    </Container>
-  );
-}
-
-function ContactEditProfileOthers({ setContactSuccess, recaptchaPublicKey }) {
-  const { loggedInUserPermissions, loading } = useLoggedInUserPermissions();
-  const [person, setPerson] = useInputState();
-  const wcaId = person?.id;
-
-  if (loading) return <Loading />;
-
-  if (!loggedInUserPermissions.canRequestToEditOthersProfile) {
-    return (
-      <Message error>
-        <I18nHTMLTranslate i18nKey="page.contact_edit_profile.no_permission_error" />
-      </Message>
-    );
-  }
-
-  return (
-    <Container text>
-      <Header as="h2">{I18n.t('page.contact_edit_profile.title')}</Header>
-      <WcaSearch
-        model={SEARCH_MODELS.person}
-        multiple={false}
-        value={person}
-        onChange={setPerson}
-        disabled={!!wcaId}
-        label={I18n.t('page.contact_edit_profile.form.wca_id_search.label')}
-      />
+      {editOthersProfileMode && (
+        <IdWcaSearch
+          model={SEARCH_MODELS.person}
+          multiple={false}
+          value={inputWcaId}
+          onChange={setInputWcaId}
+          disabled={!!inputWcaId}
+          label={I18n.t('page.contact_edit_profile.form.wca_id_search.label')}
+        />
+      )}
       {wcaId && (
         <EditProfileForm
           wcaId={wcaId}
