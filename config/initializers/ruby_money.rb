cca9d8eac0483149ef910a0d2e2ee7bd12810442
Daniel James <djdany444@gmail.com>
2025-01-03 19:12:12 +0530

    Change RubyMoney bank to currencylayer

--- config/initializers/ruby_money.rb
+++ config/initializers/ruby_money.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
+require 'money/bank/currencylayer_bank'
+
 Money.locale_backend = :i18n
 
-eu_bank = EuCentralBank.new
-Money.default_bank = eu_bank
+mclb = Money::Bank::CurrencylayerBank.new
+mclb.access_key = ENV.fetch("CURRENCY_LAYER_API_KEY", "")
+mclb.currencylayer = true
+mclb.ttl_in_seconds = 86_400
+mclb.cache = 'rails_money_cache.txt' # To be replaced with proper cache path.
+mclb.update_rates
+
+Money.default_bank = mclb
 Money.default_currency = Money::Currency.new("USD")
 Money.rounding_mode = BigDecimal::ROUND_HALF_UP
