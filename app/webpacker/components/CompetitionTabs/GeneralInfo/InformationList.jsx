bbeac57a8c0885a1ee51d2fe1af1535642072dfa
Gregor Billing <gbilling@worldcubeassociation.org>
2025-01-05 21:04:57 +0900

    Merge information grid and grid entry file

--- app/webpacker/components/CompetitionTabs/GeneralInfo/InformationList.jsx
+++ app/webpacker/components/CompetitionTabs/GeneralInfo/InformationList.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
-import DataListGridEntry from './DataListGridEntry';
+import { Grid, Header, Icon } from 'semantic-ui-react';
+
+function DataListGridEntry({
+  header,
+  children,
+  icon,
+  headerBias = 0,
+}) {
+  return (
+    <>
+      <Grid.Row verticalAlign="middle" only="computer">
+        <Grid.Column width={4 - headerBias} textAlign="right">
+          { icon ? <Icon name={icon} />
+            : <Header as="h5">{header}</Header>}
+        </Grid.Column>
+        <Grid.Column width={12 + headerBias}>
+          {children}
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row only="tablet mobile">
+        <Grid.Column width={16} textAlign="left">
+          <b>{header}</b>
+          <br />
+          {children}
+        </Grid.Column>
+      </Grid.Row>
+    </>
+  );
+}
 
 export default function InformationList({ items, headerBias = 0 }) {
   return (
