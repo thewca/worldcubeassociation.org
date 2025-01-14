36bd884db672c79083d966eae313d5f1de3b5034
Maksymilian Gala <maksymilian.gala1@gmail.com>
2025-01-14 20:40:23 +0100

    Review changes

--- app/controllers/api/v0/persons_controller.rb
+++ app/controllers/api/v0/persons_controller.rb
@@ -17,8 +17,12 @@ class Api::V0::PersonsController < Api::V0::ApiController
   def show
     person = Person.current.includes(:user, :ranksSingle, :ranksAverage).find_by_wca_id!(params[:wca_id])
     private_attributes = []
-    if current_user && (current_user.can_admin_results? || current_user.wca_id == params[:wca_id])
-      private_attributes = %w[incorrect_wca_id_claim_count dob]
+    if current_user
+      if current_user.wca_id == params[:wca_id]
+        private_attributes = %w[dob]
+      elsif current_user.can_admin_results? || current_user.any_kind_of_delegate?
+        private_attributes = %w[incorrect_wca_id_claim_count dob]
+      end
     end
     render json: person_to_json(person, private_attributes)
   end
