c82b09c66c39589f5d326bb0763959e54d2cac8e
Maksymilian Gala <maksymilian.gala1@gmail.com>
2025-01-15 11:12:59 +0100

    Review changes

--- app/controllers/api/v0/persons_controller.rb
+++ app/controllers/api/v0/persons_controller.rb
@@ -19,9 +19,9 @@ class Api::V0::PersonsController < Api::V0::ApiController
     person = Person.current.includes(:user, :ranksSingle, :ranksAverage).find_by_wca_id!(wca_id)
     private_attributes = []
     if current_user
-      if current_user&.wca_id == wca_id || current_user&.any_kind_of_delegate?
+      if current_user.wca_id == wca_id || current_user.any_kind_of_delegate?
         private_attributes = %w[dob]
-      elsif current_user&.can_admin_results?
+      elsif current_user.can_admin_results?
         private_attributes = %w[incorrect_wca_id_claim_count dob]
       end
     end
