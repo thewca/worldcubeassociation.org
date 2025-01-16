033fb26665f235e9c64c527430d1ca98460eacb6
Duncan <duncanonthejob@gmail.com>
2025-01-16 07:59:44 +0200

    fixed eslint

--- app/webpacker/components/RegistrationsV2/api/registration/patch/auto_accept.js
+++ app/webpacker/components/RegistrationsV2/api/registration/patch/auto_accept.js
@@ -1,8 +1,7 @@
-import fetchWithJWTToken from '../../../../../lib/requests/fetchWithJWTToken';
 import { fetchJsonOrError } from '../../../../../lib/requests/fetchWithAuthenticityToken';
 import { disableAutoAcceptUrl } from '../../../../../lib/requests/routes.js.erb';
 
-export async function disableAutoAccept(competitionId) {
+export default async function disableAutoAccept(competitionId) {
   const route = disableAutoAcceptUrl(competitionId);
   const { data } = await fetchJsonOrError(route, {
     method: 'PATCH',
