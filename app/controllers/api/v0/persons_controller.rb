9ace921b464156342cb839d21754aa37601c6b42
Maksymilian Gala <maksymilian.gala1@gmail.com>
2025-01-21 11:30:16 +0100

    Allow user to access their own DOB (#10618)

--- app/controllers/api/v0/persons_controller.rb
+++ app/controllers/api/v0/persons_controller.rb
@@ -15,10 +15,15 @@ class Api::V0::PersonsController < Api::V0::ApiController
   end
 
   def show
-    person = Person.current.includes(:user, :ranksSingle, :ranksAverage).find_by_wca_id!(params[:wca_id])
+    wca_id = params[:wca_id]
+    person = Person.current.includes(:user, :ranksSingle, :ranksAverage).find_by_wca_id!(wca_id)
     private_attributes = []
-    if current_user && current_user.can_admin_results?
-      private_attributes = %w[incorrect_wca_id_claim_count dob]
+    if current_user
+      if current_user.wca_id == wca_id || current_user.any_kind_of_delegate?
+        private_attributes = %w[dob]
+      elsif current_user.can_admin_results?
+        private_attributes = %w[incorrect_wca_id_claim_count dob]
+      end
     end
     render json: person_to_json(person, private_attributes)
   end
