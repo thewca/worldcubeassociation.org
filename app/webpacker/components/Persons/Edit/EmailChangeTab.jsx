dd87ab950c2053fe38d3efc7acd09610c4c6de62
FinnIckler <finnickler@gmail.com>
2025-01-22 17:10:48 +0100

    email change tab

--- app/webpacker/components/Persons/Edit/EmailChangeTab.jsx
+++ app/webpacker/components/Persons/Edit/EmailChangeTab.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Form, Modal } from 'semantic-ui-react';
 import useInputState from '../../../lib/hooks/useInputState';
 import I18nHTMLTranslate from '../../I18nHTMLTranslate';
+import { updateUserUrl } from '../../../lib/requests/routes.js.erb';
 
 export default function EmailChangeTab({ user, recentlyAuthenticated }) {
   const [email, setEmail] = useInputState(user.email);
@@ -18,14 +19,14 @@ export default function EmailChangeTab({ user, recentlyAuthenticated }) {
   }
 
   return (
-    <Form>
+    <Form method="POST" action={updateUserUrl(user.id)}>
       <input type="hidden" name="_method" value="patch" />
       <input type="hidden" name="authenticity_token" value={document.querySelector('meta[name=csrf-token]').content} />
       <Form.Field>
-        <Form.Input value={email} onChange={setEmail} label="Email" />
+        <Form.Input name="user[email]" value={email} onChange={setEmail} label="Email" />
         <I18nHTMLTranslate i18nKey="users.edit.confirm_new_email" />
       </Form.Field>
-      <Form.Button>Save</Form.Button>
+      <Form.Button type="submit">Save</Form.Button>
     </Form>
   );
 }
