9450c9f2d177839c496e781fe42a16b58ab9a663
Kevin Matthews <kvnrmatthews@gmail.com>
2025-01-22 17:50:36 -0800

    Directly pass user id to react

--- app/webpacker/components/RegistrationsV2/Registrations/index.jsx
+++ app/webpacker/components/RegistrationsV2/Registrations/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import RegistrationList from './RegistrationList';
 
-export default function Index({ competitionInfo, userInfo }) {
+export default function Index({ competitionInfo, userId }) {
   return (
     <QueryClientProvider client={new QueryClient()}>
       <RegistrationList
         competitionInfo={competitionInfo}
-        userId={userInfo?.id}
+        userId={userId}
       />
     </QueryClientProvider>
   );
