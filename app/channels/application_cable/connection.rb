099cf877189dc019fb07f29841339501547f58e3
FinnIckler <finnickler@gmail.com>
2025-01-24 13:39:44 +0100

    add action cable channels for wca live

---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,20 @@
+# app/channels/application_cable/connection.rb
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if (verified_user = User.find_by(id: cookies.encrypted[:user_id]))
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+  end
+end
