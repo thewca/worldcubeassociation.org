66496043f8776338be7c9eb9d5acb42cfced5014
Daniel James <djdany444@gmail.com>
2025-02-04 06:31:46 +0530

    Dues estimator to reduce currencylayer usage

---
+++ app/webpacker/components/CompetitionForm/FormSections/DuesEstimate.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Input, Modal } from 'semantic-ui-react';
+import { QueryClient, useQuery } from '@tanstack/react-query';
+import useInputState from '../../../lib/hooks/useInputState';
+import { calculateDuesUrl } from '../../../lib/requests/routes.js.erb';
+import { currenciesData } from '../../../lib/wca-data.js.erb';
+import Loading from '../../Requests/Loading';
+import Errored from '../../Requests/Errored';
+import { fetchJsonOrError } from '../../../lib/requests/fetchWithAuthenticityToken';
+
+const CALCULATE_DUES_QUERY_CLIENT = new QueryClient();
+
+export default function DuesEstimate({
+  close, countryId, currencyCode, baseEntryFee, competitorLimit,
+}) {
+  const [competitorCount, setCompetitorCount] = useInputState(competitorLimit || 0);
+  const currencyInfo = currenciesData.byIso[currencyCode] || currenciesData.byIso.USD;
+
+  const {
+    data, isLoading, isError,
+  } = useQuery({
+    queryKey: [countryId, currencyCode, baseEntryFee],
+    queryFn: () => fetchJsonOrError(calculateDuesUrl(countryId, currencyCode, baseEntryFee)),
+  }, CALCULATE_DUES_QUERY_CLIENT);
+
+  const { dues_value: duesValue } = data?.data || {};
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Errored error="Dues fetching failed..." />;
+
+  return (
+    <Modal
+      open
+      onClose={close}
+      closeIcon
+    >
+      <Modal.Header>Dues Estimate</Modal.Header>
+      <Modal.Content>
+        <Input
+          label="Competitor count"
+          type="number"
+          value={competitorCount}
+          onChange={setCompetitorCount}
+        />
+        <p>
+          {`Dues for ${competitorCount} competitors (approximately): ${currencyInfo?.symbol || ''}${(duesValue * competitorCount).toFixed(2)}`}
+        </p>
+      </Modal.Content>
+    </Modal>
+  );
+}
