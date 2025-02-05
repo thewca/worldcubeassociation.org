4005c64a76d115b63a8b2443fcc23afc1b15ae20
FinnIckler <finnickler@gmail.com>
2025-02-05 18:59:59 +0100

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -150,6 +150,9 @@ Rails.application.routes.draw do
   get 'competitions/edit/registration-collisions-json' => 'competitions#registration_collisions_json', as: :registration_collisions_json
   get 'competitions/edit/series-eligible-competitions-json' => 'competitions#series_eligible_competitions_json', as: :series_eligible_competitions_json
 
+  get 'api/competitions/:competition_id/rounds/:round_id/open' => 'live#open_round', as: :live_open_round
+  get 'competitions/:competition_id/live/admin' => 'live#schedule_admin', as: :live_schedule_admin
+
   get 'results/rankings', to: redirect('results/rankings/333/single', status: 302)
   get 'results/rankings/333mbf/average',
       to: redirect(status: 302) { |params, request| URI.parse(request.original_url).query ? "results/rankings/333mbf/single?#{URI.parse(request.original_url).query}" : "results/rankings/333mbf/single" }
