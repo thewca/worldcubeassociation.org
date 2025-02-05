6f496ddf99eaeb14e44618704fa6a6e591573bf9
FinnIckler <finnickler@gmail.com>
2025-02-05 14:56:33 +0100

    make AddLiveResultJob actually create the live_result

--- app/jobs/add_live_result_job.rb
+++ app/jobs/add_live_result_job.rb
@@ -4,8 +4,22 @@ class AddLiveResultJob < ApplicationJob
   self.queue_adapter = :shoryuken if WcaLive.sqs_queued?
   queue_as EnvConfig.LIVE_QUEUE if WcaLive.sqs_queued?
 
+  # params: { results, round_id, user_id, entered_by }
   def perform(params)
-    ActionCable.server.broadcast(WcaLive.broadcast_key(params[:round_id]),
-                                 { attempts: params[:results], user_id: params[:user_id] })
+    results = params[:results]
+    attempts = results.map.with_index(1) { |r, i| LiveAttempt.build(result: r, attempt_number: i) }
+    round = Round.find(params[:round_id])
+    event = round.event
+    format = round.format
+
+    r = Result.build({ value1: results[0], value2: results[1] || 0, value3: results[2] || 0, value4: results[3] || 0, value5: results[4] || 0, event_id: event.id, round_type_id: round.round_type_id, format_id: format.id })
+
+    LiveResult.create!(registration_id: params[:registration_id],
+                       round: round,
+                       live_attempts: attempts,
+                       entered_by: params[:entered_by],
+                       entered_at: Time.now.utc,
+                       best:  r.compute_correct_best,
+                       average: r.compute_correct_average)
   end
 end
