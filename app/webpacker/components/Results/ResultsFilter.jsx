aa0d2407d8a8ec34fcf7d3b4fddf3876c305e4a7
Gregor Billing <gbilling@worldcubeassociation.org>
2025-02-06 16:10:29 +0900

    Review: Use 'clear' button instead of 'all' button in events selector

--- app/webpacker/components/Results/ResultsFilter.jsx
+++ app/webpacker/components/Results/ResultsFilter.jsx
@@ -48,8 +48,8 @@ export default function ResultsFilter({
             title={I18n.t('results.selector_elements.events_selector.event')}
             selectedEvents={[event]}
             onEventSelection={({ eventId }) => setEvent(eventId)}
-            hideAllButton={!isRecords}
-            hideClearButton
+            hideAllButton
+            hideClearButton={!isRecords}
             showBreakBeforeButtons={false}
           />
         </Form.Field>
