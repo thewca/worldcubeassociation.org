16f09744303c5051282ad1c50083290ec8c3d5e3
Kevin Matthews <kvnrmatthews@gmail.com>
2025-02-05 21:10:55 -0800

    Use tanstack table for Competitors

--- app/webpacker/components/RegistrationsV2/Registrations/Competitors.jsx
+++ app/webpacker/components/RegistrationsV2/Registrations/Competitors.jsx
@@ -1,26 +1,95 @@
 import { useQuery } from '@tanstack/react-query';
-import React, {
-  useMemo,
-  useReducer,
-} from 'react';
+import React from 'react';
 import {
   Flag, Segment, Table,
 } from 'semantic-ui-react';
 import _ from 'lodash';
 import {
+  flexRender, getCoreRowModel, getSortedRowModel, useReactTable
+} from '@tanstack/react-table';
+import {
   getConfirmedRegistrations,
 } from '../api/registration/get/get_registrations';
-import createSortReducer from '../reducers/sortReducer';
 import EventIcon from '../../wca/EventIcon';
 import { personUrl } from '../../../lib/requests/routes.js.erb';
 import I18n from '../../../lib/i18n';
 import { countries } from '../../../lib/wca-data.js.erb';
-import { getPeopleCounts, getTotals, getUserPositionInfo } from './utils';
+import { expandSortTerm, getPeopleCounts, getTotals, getUserPositionInfo } from './utils';
 import PreTableInfo from './PreTableInfo';
 import Errored from '../../Requests/Errored';
 import Loading from '../../Requests/Loading';
 
-const sortReducer = createSortReducer(['name', 'country', 'total']);
+const getColumns = (eventIds) => [
+  {
+    accessorKey: 'user.name',
+    header: I18n.t('activerecord.attributes.registration.name'),
+    cell: (info) => info.getValue() ? (
+      <a href={personUrl(info.row.original.user.wca_id)}>
+        {info.getValue()}
+      </a>
+    ) : (
+      info.getValue()
+    ),
+    footer: (info) => {
+      const registrationCount = info.table.getCoreRowModel().rows.length;
+      return (
+        `${
+          registrationCount
+        } ${
+          I18n.t(
+            'registrations.registration_info_people.person',
+            { count: info.table.getCoreRowModel().rows.length },
+          )
+        }`
+      )
+    },
+  },
+  {
+    id: 'country',
+    header: I18n.t('activerecord.attributes.user.country_iso2'),
+    accessorFn: (row) => countries.byIso2[row.user.country.iso2].name,
+    cell: (info) => (
+      <>
+        <Flag
+          className={info.row.original.user.country.iso2.toLowerCase()}
+        />
+        {info.getValue()}
+      </>
+    ),
+    footer: (info) => {
+      const { countryCount } = getTotals(
+        info.table.getCoreRowModel().rows.map((row) => row.original),
+        eventIds,
+      );
+      return `${I18n.t('registrations.list.country_plural', { count: countryCount })}`
+    }
+  },
+  ...eventIds.map((id) => ({
+    id,
+    header: <EventIcon id={id} size="1em" className="selected" />,
+    enableSorting: false,
+    accessorFn: (row) => row.competing.event_ids.includes(id),
+    cell: (info) => (
+      info.getValue() && <EventIcon id={id} size="1em" hoverable={false} />
+    ),
+    footer: (info) => (
+      info.table.getCoreRowModel().rows.filter((row) => row.getValue(id)).length
+    ),
+    meta: { style: { textAlign: 'center' } },
+  })),
+  {
+    id: 'event-count',
+    header: I18n.t('registrations.list.total'),
+    accessorFn: (row) => row.competing.event_ids.length,
+    footer: (info) => (
+      info.table.getCoreRowModel().rows.reduce(
+        (acc, row) => acc + row.getValue('event-count'),
+        0,
+      )
+    ),
+    meta: { style: { textAlign: 'center' } },
+  },
+];
 
 export default function Competitors({
   competitionInfo,
@@ -30,45 +99,25 @@ export default function Competitors({
   userRowRef,
   onScrollToMeClick,
 }) {
-  const { isLoading, data: registrations, isError } = useQuery({
+  const { isLoading, data, isError } = useQuery({
     queryKey: ['registrations', competitionInfo.id],
     queryFn: () => getConfirmedRegistrations(competitionInfo),
     retry: false,
+    // react table sorting is stable, and we want the default tie-breaker
+    //  to be name (not registration id, which the backend sorts by)
+    select: (queryData) => queryData.toSorted((a, b) => a.user.name.localeCompare(b.user.name))
   });
 
-  const [sortState, sortDispatch] = useReducer(sortReducer, {
-    sortColumn: 'name',
-    sortDirection: 'ascending',
+  const table = useReactTable({
+    data: data || [],
+    columns: getColumns(eventIds),
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    enableSortingRemoval: false,
+    initialState: {
+      sorting: [{ id: 'user_name', desc: false }],
+    },
   });
-  const { sortColumn: sortedColumn, sortDirection: sortedDirection } = sortState;
-  const changeSortColumn = (name) => sortDispatch({ type: 'CHANGE_SORT', sortColumn: name });
-
-  // TODO: use react table (future PR)
-  const data = useMemo(() => {
-    if (registrations) {
-      let orderBy = [];
-      switch (sortedColumn) {
-        case 'country':
-          orderBy = [
-            (item) => countries.byIso2[item.user.country.iso2].name,
-          ];
-          break;
-        case 'total':
-          orderBy = [
-            (item) => item.competing.event_ids.length,
-          ];
-          break;
-        default:
-          break;
-      }
-      // always sort by user name as a fallback
-      orderBy.push('user.name');
-      const direction = sortedDirection === 'descending' ? 'desc' : 'asc';
-
-      return _.orderBy(registrations, orderBy, [direction]);
-    }
-    return [];
-  }, [registrations, sortedColumn, sortedDirection]);
 
   if (isError) {
     return (
@@ -84,11 +133,11 @@ export default function Competitors({
     );
   }
 
-  const { userIsInTable } = getUserPositionInfo(registrations, userId);
+  const { userIsInTable } = getUserPositionInfo(data, userId);
 
-  const { registrationCount, newcomerCount, returnerCount } = getPeopleCounts(
-    registrations,
-  );
+  const {
+    registrationCount, newcomerCount, returnerCount
+  } = getPeopleCounts(data);
 
   return (
     <>
@@ -101,164 +150,68 @@ export default function Competitors({
         onScrollToMeClick={onScrollToMeClick}
       />
       <Table striped sortable unstackable compact singleLine textAlign="left">
-        <CompetitorsHeader
-          eventIds={eventIds}
-          sortedColumn={sortedColumn}
-          sortedDirection={sortedDirection}
-          onSortableColumnClick={changeSortColumn}
-          onEventColumnClick={onEventClick}
-        />
-        <CompetitorsBody
-          registrations={data}
-          eventIds={eventIds}
-          userId={userId}
-          userRowRef={userRowRef}
-        />
-        <CompetitorsFooter
-          registrations={registrations}
-          eventIds={eventIds}
-        />
-      </Table>
-    </>
-  );
-}
-
-function CompetitorsHeader({
-  eventIds,
-  sortedColumn,
-  sortedDirection,
-  onSortableColumnClick,
-  onEventColumnClick,
-}) {
-  return (
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell
-          sorted={sortedColumn === 'name' ? sortedDirection : undefined}
-          onClick={() => onSortableColumnClick('name')}
-        >
-          {I18n.t('activerecord.attributes.registration.name')}
-        </Table.HeaderCell>
-        <Table.HeaderCell
-          sorted={sortedColumn === 'country' ? sortedDirection : undefined}
-          onClick={() => onSortableColumnClick('country')}
-        >
-          {I18n.t('activerecord.attributes.user.country_iso2')}
-        </Table.HeaderCell>
-        {eventIds.map((id) => (
-          <Table.HeaderCell
-            textAlign="center"
-            key={`registration-table-header-${id}`}
-            onClick={() => onEventColumnClick(id)}
-          >
-            <EventIcon id={id} size="1em" className="selected" />
-          </Table.HeaderCell>
-        ))}
-        <Table.HeaderCell
-          textAlign="center"
-          sorted={sortedColumn === 'total' ? sortedDirection : undefined}
-          onClick={() => onSortableColumnClick('total')}
-        >
-          {I18n.t('registrations.list.total')}
-        </Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-  );
-}
-
-function CompetitorsBody({
-  registrations,
-  eventIds,
-  userId,
-  userRowRef,
-}) {
-  return (
-    <Table.Body>
-      {registrations.length > 0 ? (
-        registrations.map((registration) => {
-          const isUser = registration.user_id === userId;
-          return (
-            <Table.Row
-              key={`registration-table-row-${registration.user.id}`}
-              active={isUser}
-            >
-              <Table.Cell>
-                <div ref={isUser ? userRowRef : undefined}>
-                  {registration.user.wca_id ? (
-                    <a
-                      href={personUrl(registration.user.wca_id)}
-                    >
-                      {registration.user.name}
-                    </a>
-                  ) : (
-                    registration.user.name
-                  )}
-                </div>
-              </Table.Cell>
-              <Table.Cell>
-                <Flag
-                  className={registration.user.country.iso2.toLowerCase()}
-                />
-                {countries.byIso2[registration.user.country.iso2].name}
-              </Table.Cell>
-              {eventIds.map((id) => (
-                <Table.Cell
-                  textAlign="center"
-                  key={`registration-table-row-${registration.user.id}-${id}`}
+        <Table.Header>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <Table.Row key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <Table.HeaderCell
+                  key={header.id}
+                  sorted={expandSortTerm(header.column.getIsSorted())}
+                  onClick={
+                    header.column.getCanSort()
+                      ? header.column.getToggleSortingHandler()
+                      : () => onEventClick(header.column.id)
+                  }
+                  textAlign={header.column.columnDef.meta?.style?.textAlign}
                 >
-                  {registration.competing.event_ids.includes(id) && (
-                    <EventIcon id={id} size="1em" hoverable={false} />
-                  )}
-                </Table.Cell>
+                  {flexRender(header.column.columnDef.header, header.getContext())}
+                </Table.HeaderCell>
               ))}
-              <Table.Cell textAlign="center">
-                {registration.competing.event_ids.length}
+            </Table.Row>
+          ))}
+        </Table.Header>
+        <Table.Body>
+          {table.getSortedRowModel().rows.length > 0 ? (
+            table.getSortedRowModel().rows.map((row) => {
+              const isUser = row.original.user_id === userId; 
+              return (
+                <Table.Row active={isUser}>
+                  {row.getVisibleCells().map((cell, index) => (
+                    <Table.Cell
+                      key={cell.id}
+                      textAlign={cell.column.columnDef.meta?.style?.textAlign}
+                    >
+                      <div ref={(isUser && (index === 0)) ? userRowRef : undefined}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </div>
+                    </Table.Cell>
+                  ))}
+                </Table.Row>
+              );
+            })
+          ) : (
+            <Table.Row>
+              <Table.Cell colSpan={table.getAllColumns().length} textAlign='center'>
+                {I18n.t('competitions.registration_v2.list.empty')}
               </Table.Cell>
             </Table.Row>
-          );
-        })
-      ) : (
-        <Table.Row>
-          <Table.Cell
-            textAlign="center"
-            colSpan={eventIds.length + 3}
-          />
-        </Table.Row>
-      )}
-    </Table.Body>
-  );
-}
-
-function CompetitorsFooter({
-  registrations,
-  eventIds,
-}) {
-  const {
-    registrationCount, countryCount, eventCounts, eventCountsSum,
-  } = getTotals(registrations, eventIds);
-
-  return (
-    <Table.Footer>
-      <Table.Row>
-        <Table.Cell>
-          {
-            `${
-              registrationCount
-            } ${
-              I18n.t('registrations.registration_info_people.person', { count: registrationCount })
-            }`
-          }
-        </Table.Cell>
-        <Table.Cell>
-          {`${I18n.t('registrations.list.country_plural', { count: countryCount })}`}
-        </Table.Cell>
-        {eventIds.map((evt) => (
-          <Table.Cell textAlign="center" key={`footer-count-${evt}`}>
-            {eventCounts[evt]}
-          </Table.Cell>
-        ))}
-        <Table.Cell textAlign="center">{eventCountsSum}</Table.Cell>
-      </Table.Row>
-    </Table.Footer>
+          )}
+        </Table.Body>
+        <Table.Footer>
+          {table.getFooterGroups().map(footerGroup => (
+            <Table.Row key={footerGroup.id}>
+              {footerGroup.headers.map(header => (
+                <Table.HeaderCell
+                  key={header.id}
+                  textAlign={header.column.columnDef.meta?.style?.textAlign}
+                >
+                  {flexRender(header.column.columnDef.footer, header.getContext())}
+                </Table.HeaderCell>
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Footer>
+      </Table>
+    </>
   );
 }
