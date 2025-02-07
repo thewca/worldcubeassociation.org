979f5da907dedb45c42b360023dab021e981d0cd
FinnIckler <finnickler@gmail.com>
2025-02-07 12:25:37 +0100

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -789,9 +789,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_02_154917) do
     t.integer "result", null: false
     t.integer "attempt_number", null: false
     t.bigint "replaced_by_id"
+    t.datetime "entered_at", null: false
+    t.integer "entered_by_id", null: false
     t.bigint "live_result_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["entered_by_id"], name: "index_live_attempts_on_entered_by_id"
     t.index ["live_result_id"], name: "index_live_attempts_on_live_result_id"
     t.index ["replaced_by_id"], name: "index_live_attempts_on_replaced_by_id"
   end
@@ -799,8 +802,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_02_154917) do
   create_table "live_results", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "registration_id", null: false
     t.bigint "round_id", null: false
-    t.integer "entered_by_id", null: false
-    t.datetime "entered_at", null: false
+    t.datetime "last_attempt_entered_at", null: false
     t.integer "ranking"
     t.integer "best", null: false
     t.integer "average", null: false
@@ -810,7 +812,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_02_154917) do
     t.boolean "advancing_questionable", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["entered_by_id"], name: "index_live_results_on_entered_by_id"
     t.index ["registration_id", "round_id"], name: "index_live_results_on_registration_id_and_round_id", unique: true
     t.index ["registration_id"], name: "index_live_results_on_registration_id"
     t.index ["round_id"], name: "index_live_results_on_round_id"
@@ -1115,6 +1116,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_02_154917) do
     t.text "round_results", size: :medium
     t.integer "total_number_of_rounds", null: false
     t.string "old_type", limit: 1
+    t.boolean "is_open", default: false, null: false
     t.index ["competition_event_id", "number"], name: "index_rounds_on_competition_event_id_and_number", unique: true
   end
 
@@ -1426,7 +1428,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_02_154917) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "live_attempts", "live_attempts", column: "replaced_by_id"
-  add_foreign_key "live_results", "users", column: "entered_by_id"
+  add_foreign_key "live_attempts", "users", column: "entered_by_id"
   add_foreign_key "microservice_registrations", "Competitions", column: "competition_id", on_update: :cascade, on_delete: :cascade
   add_foreign_key "microservice_registrations", "users"
   add_foreign_key "oauth_openid_requests", "oauth_access_grants", column: "access_grant_id", on_delete: :cascade
