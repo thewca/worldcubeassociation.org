b5c9c2aeb8f65a1e9b9c114d833f850e3911fe5e
FinnIckler <finnickler@gmail.com>
2025-02-07 12:29:29 +0100

    slim down serialization options

--- app/models/live_result.rb
+++ app/models/live_result.rb
@@ -16,17 +16,13 @@ class LiveResult < ApplicationRecord
   has_one :event, through: :round
 
   DEFAULT_SERIALIZE_OPTIONS = {
-    only: %w[ranking registration_id round live_attempts round best average single_record_tag average_record_tag advancing advancing_questionable entered_at entered_by_id],
+    only: %w[ranking registration_id round_id best average single_record_tag average_record_tag advancing advancing_questionable entered_at entered_by_id],
     methods: %w[event_id attempts result_id],
-    include: %w[event live_attempts round],
+    include: %w[],
   }.freeze
 
   def serializable_hash(options = nil)
     super(DEFAULT_SERIALIZE_OPTIONS.merge(options || {}))
-  end
-
-  def result_id
-    id
   end
 
   def event_id
