3300137dfc11d9e1698a5f7b692fe9b259bede40
Duncan <duncanonthejob@gmail.com>
2025-02-11 08:44:30 +0200

    eslint

--- app/webpacker/components/RegistrationsV2/Register/RegistrationOverview.jsx
+++ app/webpacker/components/RegistrationsV2/Register/RegistrationOverview.jsx
@@ -27,7 +27,7 @@ export default function RegistrationOverview({
   const isRejected = registration.competing.registration_status === 'rejected';
 
   const deleteAllowed = (registration.competing.registration_status !== 'accepted'
-    || competitionInfo.cancellation_restrictions === 'no_restrictions' );
+    || competitionInfo.cancellation_restrictions === 'no_restrictions');
 
   const queryClient = useQueryClient();
 
