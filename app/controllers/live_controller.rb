f17abc087b4457ada5a328499f0e3ee330652f19
FinnIckler <finnickler@gmail.com>
2025-02-12 12:11:45 +0100

    add new controller methods

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -7,4 +7,26 @@ class LiveController < ApplicationController
 
     @rounds = Round.joins(:competition_event).where(competition_event: { competition_id: @competition_id })
   end
+
+  def podiums
+    @competition = Competition.find(params[:competition_id])
+    @competitors = @competition.registrations.accepted
+    @results = @competition.rounds.select(&:final_round?)
+  end
+
+  def competitors
+    @competition = Competition.find(params[:competition_id])
+    @competitors = @competition.registrations.accepted
+  end
+
+  def by_persons
+    registration_id = params.require(:registration_id)
+    registration = Registration.find(registration_id)
+
+    @competition_id = params[:competition_id]
+    @competition = Competition.find(@competition_id)
+
+    @user = registration.user
+    @results = registration.live_results.includes([:live_attempts])
+  end
 end
