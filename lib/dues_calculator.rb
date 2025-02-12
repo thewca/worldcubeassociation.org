6a8bae5ecac7372291b20a2c0a4987e4c2c106d2
Daniel James <djdany444@gmail.com>
2025-02-12 11:08:41 +0530

    Review changes

--- lib/dues_calculator.rb
+++ lib/dues_calculator.rb
@@ -11,17 +11,15 @@ module DuesCalculator
   def self.dues_per_competitor_in_usd(country_iso2, base_entry_fee_lowest_denomination, currency_code)
     country_band = CountryBand.find_by(iso2: country_iso2)
     country_band_detail = country_band&.active_country_band_detail
-    registration_fees_in_usd = Money.new(base_entry_fee_lowest_denomination, currency_code).exchange_to("USD")
 
     return nil if country_band_detail.nil?
 
     # Calculation of 'registration fee dues'
-    due_percent_registration_fee = country_band_detail.due_percent_registration_fee
-    registration_fee_dues = registration_fees_in_usd * due_percent_registration_fee / 100.0
+    registration_fees_in_usd = Money.new(base_entry_fee_lowest_denomination, currency_code).exchange_to("USD")
+    registration_fee_dues = registration_fees_in_usd * country_band_detail.due_percent_registration_fee / 100.0
 
     # Calculation of 'country band dues'
-    due_amount_per_competitor_us_cents = country_band_detail.due_amount_per_competitor_us_cents
-    country_band_dues = Money.new(due_amount_per_competitor_us_cents, "USD")
+    country_band_dues = Money.new(country_band_detail.due_amount_per_competitor_us_cents, "USD")
 
     # The maximum of the two is the total dues per competitor
     [registration_fee_dues, country_band_dues].max
