327985c27da9a6666fa7d5fb62da9ac9d86a2a25
FinnIckler <finnickler@gmail.com>
2025-02-13 05:41:20 +0100

    WCA Live: Recompute Advancing on update (#10815)
    * advancing but failing tests
    
    * fix factory issues
    
    * Optimize registration loading in Round model
    
    * first -> take
    
    * switch to after_save
    
    * fix merge whoopsie
    
    * moved BEST_POSSIBLE_SCORE to a constant
    
    * refactor method
    
    * Move method after merge
    
    * Use 'count' instead of 'length' just to be sure
    
    * Fix early loading quirk in live_result factory
    
    * Add test cases for other advancement_conditions
    
    * Implement method stubs and associations needed for attempt_result_condition
    
    * Make sure we have rankings to rely on for advancing
    
    * Mock competition round and registration to make sure they don't mock their own independent associations
    
    * Factor out common mocked models in test
    
    * Crazy weird classloader fix
    
    * Generate registrations on-the-fly
    
    * Move 'registrations' back out of in-test scope again
    
    * Move advancement_condition computations into recompute logic
    
    * Finals obviously don't have advancement conditions
    
    ---------
    
    Co-authored-by: Gregor Billing <gbilling@worldcubeassociation.org>

--- app/models/live_result.rb
+++ app/models/live_result.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class LiveResult < ApplicationRecord
+  BEST_POSSIBLE_SCORE = 1
+
   has_many :live_attempts
   alias_method :attempts, :live_attempts
 
   after_create :recompute_ranks
   after_update :recompute_ranks, if: :should_recompute?
+
+  # This hook has to be executed _after_ computing the rankings
+  after_save :recompute_advancing, if: :should_recompute?
 
   after_save :notify_users
 
@@ -16,6 +21,7 @@ class LiveResult < ApplicationRecord
   alias_attribute :result_id, :id
 
   has_one :event, through: :round
+  has_one :format, through: :round
 
   DEFAULT_SERIALIZE_OPTIONS = {
     only: %w[ranking registration_id round_id best average single_record_tag average_record_tag advancing advancing_questionable entered_at entered_by_id],
@@ -31,9 +37,13 @@ class LiveResult < ApplicationRecord
     event.id
   end
 
+  def to_solve_time(field)
+    SolveTime.new(event_id, field, send(field))
+  end
+
   def potential_score
     rank_by = round.format.sort_by == 'single' ? 'best' : 'average'
-    complete? ? self[rank_by.to_sym] : best_possible_score
+    complete? ? self[rank_by.to_sym] : BEST_POSSIBLE_SCORE
   end
 
   def should_recompute?
@@ -77,5 +87,30 @@ class LiveResult < ApplicationRecord
 
     def notify_users
       ActionCable.server.broadcast("results_#{round_id}", serializable_hash)
+    end
+
+    def recompute_advancing
+      # Only ranked results can be considered for advancing.
+      round_results = round.live_results.where.not(ranking: nil)
+      round_results.update_all(advancing: false, advancing_questionable: false)
+
+      missing_attempts = round.total_accepted_registrations - round_results.count
+      potential_results = Array.new(missing_attempts) { LiveResult.build(round: round) }
+      results_with_potential = (round_results.to_a + potential_results).sort_by(&:potential_score)
+
+      qualifying_index = if round.final_round?
+                           3
+                         else
+                           # Our Regulations allow at most 75% of competitors to proceed
+                           max_qualifying = (round_results.count * 0.75).floor
+                           [round.advancement_condition.max_advancing(round_results), max_qualifying].min
+                         end
+
+      round_results.update_all("advancing_questionable = ranking BETWEEN 1 AND #{qualifying_index}")
+
+      # Determine which results would advance if everyone achieved their best possible attempt.
+      advancing_ids = results_with_potential.take(qualifying_index).select(&:complete?).pluck(:id)
+
+      LiveResult.where(id: advancing_ids).update_all(advancing: true)
     end
 end
