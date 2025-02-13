54ec6575b219fc5780fc70c1d5bda80064b87730
Gregor Billing <gbilling@worldcubeassociation.org>
2025-02-13 11:47:26 +0900

    Generate registrations on-the-fly

--- spec/requests/live_recompute_advancing_spec.rb
+++ spec/requests/live_recompute_advancing_spec.rb
@@ -17,17 +17,18 @@ end
 RSpec.describe "WCA Live API" do
   describe "Advancing Recomputation" do
     let(:competition) { FactoryBot.create(:competition, event_ids: ["333"]) }
-    let(:registrations) { FactoryBot.create_list(:registration, 5, :accepted, event_ids: ["333"], competition: competition) }
 
     context 'with a ranking advancement condition' do
       it 'returns results with ranking better or equal to the given level' do
         round = FactoryBot.create(:round, number: 1, total_number_of_rounds: 2, event_id: "333", competition: competition, advancement_condition: ranking_condition)
 
-        expect(round.total_accepted_registrations).to eq 5
-
         5.times do |i|
-          FactoryBot.create(:live_result, registration: registrations[i], round: round, ranking: i + 1, average: (i + 1) * 100)
+          registration = FactoryBot.create(:registration, :accepted, competition: competition, event_ids: ["333"])
+          FactoryBot.create(:live_result, registration: registration, round: round, ranking: i + 1, average: (i + 1) * 100)
         end
+
+        expect(round.total_accepted_registrations).to eq 5
+        expect(round.competitors_live_results_entered).to eq 5
 
         expect(round.live_results.pluck(:advancing)).to eq([true, true, true, false, false])
       end
@@ -37,11 +38,13 @@ RSpec.describe "WCA Live API" do
       it 'returns results with ranking better or equal to the given level' do
         round = FactoryBot.create(:round, number: 1, total_number_of_rounds: 2, event_id: "333", competition: competition, advancement_condition: percent_condition)
 
-        expect(round.total_accepted_registrations).to eq 5
-
         5.times do |i|
-          FactoryBot.create(:live_result, registration: registrations[i], round: round, ranking: i + 1, average: (i + 1) * 100)
+          registration = FactoryBot.create(:registration, :accepted, competition: competition, event_ids: ["333"])
+          FactoryBot.create(:live_result, registration: registration, round: round, ranking: i + 1, average: (i + 1) * 100)
         end
+
+        expect(round.total_accepted_registrations).to eq 5
+        expect(round.competitors_live_results_entered).to eq 5
 
         # 40% of 5 is exactly 2.
         expect(round.live_results.pluck(:advancing)).to eq([true, true, false, false, false])
@@ -52,11 +55,13 @@ RSpec.describe "WCA Live API" do
       it 'returns results with ranking better or equal to the given level' do
         round = FactoryBot.create(:round, number: 1, total_number_of_rounds: 2, event_id: "333", competition: competition, advancement_condition: attempt_result_condition)
 
-        expect(round.total_accepted_registrations).to eq 5
-
         5.times do |i|
-          FactoryBot.create(:live_result, registration: registrations[i], round: round, ranking: i + 1, average: (i + 1) * 100)
+          registration = FactoryBot.create(:registration, :accepted, competition: competition, event_ids: ["333"])
+          FactoryBot.create(:live_result, registration: registration, round: round, ranking: i + 1, average: (i + 1) * 100)
         end
+
+        expect(round.total_accepted_registrations).to eq 5
+        expect(round.competitors_live_results_entered).to eq 5
 
         # Only strictly _better_ than 3 seconds will proceed, so that's two entries.
         expect(round.live_results.pluck(:advancing)).to eq([true, true, false, false, false])
