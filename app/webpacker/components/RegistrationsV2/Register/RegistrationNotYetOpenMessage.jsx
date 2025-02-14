c9af76f70963fdfd057a27062e7b89fd4450cdfd
Gregor Billing <gbilling@worldcubeassociation.org>
2025-02-14 17:24:55 +0900

    Move timing function for countdown into hook

--- app/webpacker/components/RegistrationsV2/Register/RegistrationNotYetOpenMessage.jsx
+++ app/webpacker/components/RegistrationsV2/Register/RegistrationNotYetOpenMessage.jsx
@@ -1,15 +1,18 @@
 import { DateTime } from 'luxon';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Message } from 'semantic-ui-react';
 import I18nHTMLTranslate from '../../I18nHTMLTranslate';
 import { fullTimeDiff } from '../../../lib/utils/dates';
+import usePerpetualState from '../hooks/usePerpetualState';
 
 export default function RegistrationNotYetOpenMessage({
   registrationStart,
   onTimerEnd,
 }) {
   const start = DateTime.fromISO(registrationStart);
-  const [timeLeft, setTimeLeft] = useState(fullTimeDiff(start));
+  const recomputeDiff = useCallback(() => fullTimeDiff(start), [start]);
+
+  const timeLeft = usePerpetualState(recomputeDiff);
 
   useEffect(() => {
     if (timeLeft.days === 0
@@ -20,20 +23,12 @@ export default function RegistrationNotYetOpenMessage({
     }
   }, [onTimerEnd, timeLeft]);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTimeLeft(fullTimeDiff(start));
-    }, 1000); // Update every second
-
-    return () => clearInterval(intervalId); // Cleanup interval on unmount
-  }, [registrationStart, onTimerEnd, start]);
-
   if (timeLeft.seconds < 0) {
     return null;
   }
 
   return (
-    <Message color="blue">
+    <Message info>
       { timeLeft.days < 1 && timeLeft.hours < 1
         ? <I18nHTMLTranslate i18nKey="competitions.registration_v2.register.will_open_countdown" options={{ minutes: timeLeft.minutes, seconds: timeLeft.seconds }} />
         // i18n-tasks-use t('registrations.will_open_html')
