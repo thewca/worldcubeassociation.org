f4808c1bfba1ae419be324ae80ac47940b6e2d03
Kevin Matthews <kvnrmatthews@gmail.com>
2025-02-14 19:27:14 -0800

    Add withTooltip prop to CountryFlag

--- app/webpacker/components/wca/CountryFlag.js
+++ app/webpacker/components/wca/CountryFlag.js
@@ -3,17 +3,28 @@ import { Popup } from 'semantic-ui-react';
 import classnames from 'classnames';
 import { countries } from '../../lib/wca-data.js.erb';
 
-function CountryFlag({ iso2 }) {
+export default function CountryFlag({ iso2, withTooltip = true }) {
+  if (withTooltip) {
+    return <CountryFlagWithTooltip iso2={iso2} />;
+  }
+
+  return <CountryFlagWithoutTooltip iso2={iso2} />;
+}
+
+function CountryFlagWithTooltip({ iso2 }) {
   return (
     <Popup
       id="resultCountryFlagTooltip"
       position="top center"
       content={countries.byIso2[iso2].name}
-      trigger={(
-        <span className={classnames('fi', `fi-${iso2.toLowerCase()}`)} />
-      )}
+      // trigger <CountryFlagWithoutTooltip iso2={iso2} /> doesn't work (??)
+      trigger={<span className={classnames('fi', `fi-${iso2.toLowerCase()}`)} />}
     />
   );
 }
 
-export default CountryFlag;
+function CountryFlagWithoutTooltip({ iso2 }) {
+  return (
+    <span className={classnames('fi', `fi-${iso2.toLowerCase()}`)} />
+  );
+}
