54b0b2592984e84256cd5f41b9e6a14047785a5c
Kevin Matthews <kvnrmatthews@gmail.com>
2025-02-14 19:14:55 -0800

    Remove unused props from CountryFlag

--- app/webpacker/components/wca/CountryFlag.js
+++ app/webpacker/components/wca/CountryFlag.js
@@ -3,19 +3,14 @@ import { Popup } from 'semantic-ui-react';
 import classnames from 'classnames';
 import { countries } from '../../lib/wca-data.js.erb';
 
-/* eslint react/jsx-props-no-spreading: "off" */
-function CountryFlag({
-  iso2,
-  className,
-  ...other
-}) {
+function CountryFlag({ iso2 }) {
   return (
     <Popup
       id="resultCountryFlagTooltip"
       position="top center"
       content={countries.byIso2[iso2].name}
       trigger={(
-        <span {...other} className={classnames('fi', `fi-${iso2.toLowerCase()}`, className)} />
+        <span className={classnames('fi', `fi-${iso2.toLowerCase()}`)} />
       )}
     />
   );
