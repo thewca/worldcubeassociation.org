13c407b1269af733539f252437892e14906832de
Duncan <duncanonthejob@gmail.com>
2025-02-26 08:16:27 +0200

    default newcomer_month_reserved_spots to null

--- db/schema.rb
+++ db/schema.rb
@@ -82,8 +82,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_18_070141) do
     t.boolean "forbid_newcomers", default: false, null: false
     t.string "forbid_newcomers_reason"
     t.integer "registration_version", default: 0, null: false
-    t.integer "newcomer_month_reserved_spots", default: 0, null: false
     t.integer "competitor_can_cancel", default: 0, null: false
+    t.integer "newcomer_month_reserved_spots"
     t.index ["cancelled_at"], name: "index_Competitions_on_cancelled_at"
     t.index ["countryId"], name: "index_Competitions_on_countryId"
     t.index ["end_date"], name: "index_Competitions_on_end_date"
