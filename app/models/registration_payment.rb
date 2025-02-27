ad20999d944658c2175b1d10b38851398090049b
Gregor Billing <gbilling@worldcubeassociation.org>
2025-02-27 14:20:21 +0900

    Move refund condition to :unless directly in hook

--- app/models/registration_payment.rb
+++ app/models/registration_payment.rb
@@ -9,7 +9,7 @@ class RegistrationPayment < ApplicationRecord
   belongs_to :refunded_registration_payment, class_name: 'RegistrationPayment', optional: true
   has_many :refunding_registration_payments, class_name: 'RegistrationPayment', inverse_of: :refunded_registration_payment, foreign_key: :refunded_registration_payment_id, dependent: :destroy
 
-  after_create :attempt_auto_close
+  after_create :attempt_auto_close, unless: :refunded_registration_payment_id?
 
   monetize :amount_lowest_denomination,
            as: "amount",
@@ -30,6 +30,6 @@ class RegistrationPayment < ApplicationRecord
   end
 
   private def attempt_auto_close
-    registration.competition.attempt_auto_close! if amount_lowest_denomination > 0
+    registration.competition.attempt_auto_close!
   end
 end
