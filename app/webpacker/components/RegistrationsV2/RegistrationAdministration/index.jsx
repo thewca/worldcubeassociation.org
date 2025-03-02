d1bbacb5f5f2a26b812af51d3488805a2dcf82ce
Kevin Matthews <kvnrmatthews@gmail.com>
2025-03-02 11:08:45 -0800

    Wrap Reg Admin List in Confirm Provider

--- app/webpacker/components/RegistrationsV2/RegistrationAdministration/index.jsx
+++ app/webpacker/components/RegistrationsV2/RegistrationAdministration/index.jsx
@@ -5,6 +5,7 @@ import RegistrationMessage from '../Register/RegistrationMessage';
 import messageReducer from '../reducers/messageReducer';
 import StoreProvider from '../../../lib/providers/StoreProvider';
 import WCAQueryClientProvider from '../../../lib/providers/WCAQueryClientProvider';
+import ConfirmProvider from '../../../lib/providers/ConfirmProvider';
 
 export default function RegistrationEdit({ competitionInfo }) {
   const ref = useRef();
@@ -12,10 +13,12 @@ export default function RegistrationEdit({ competitionInfo }) {
     <div ref={ref}>
       <WCAQueryClientProvider>
         <StoreProvider reducer={messageReducer} initialState={{ messages: [] }}>
-          <Sticky context={ref} offset={60}>
-            <RegistrationMessage />
-          </Sticky>
-          <RegistrationAdministrationList competitionInfo={competitionInfo} />
+          <ConfirmProvider>
+            <Sticky context={ref} offset={60}>
+              <RegistrationMessage />
+            </Sticky>
+            <RegistrationAdministrationList competitionInfo={competitionInfo} />
+          </ConfirmProvider>
         </StoreProvider>
       </WCAQueryClientProvider>
     </div>
