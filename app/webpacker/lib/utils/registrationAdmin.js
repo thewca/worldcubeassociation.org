e36c5406218c46b4dd13a8a68f7eb93318ef462d
Kevin Matthews <49137025+kr-matthews@users.noreply.github.com>
2025-03-02 18:44:02 -0800

    Reg Admin warning when skipping waitlisted registrations (#10938)
    * Wrap Reg Admin List in Confirm Provider
    
    * Show default confirm when skipping a waitlist reg
    
    * Custom warning message with user's name
    
    * I18n for new warning string
    
    * Show skip count instead of first skipped name
    
    & simplify logic
    
    * waitlist -> Waitling List

--- app/webpacker/lib/utils/registrationAdmin.js
+++ app/webpacker/lib/utils/registrationAdmin.js
@@ -9,3 +9,49 @@ export const WAITLIST_ICON = 'hourglass';
 export const APPROVED_ICON = 'check';
 export const CANCELLED_ICON = 'trash';
 export const REJECTED_ICON = 'x';
+
+function getSortedWaitlistRegistrations(registrations) {
+  return registrations.filter(
+    (reg) => reg.competing.registration_status === 'waiting_list',
+  ).toSorted(
+    (a, b) => a.competing.waiting_list_position - b.competing.waiting_list_position,
+  );
+}
+
+function getLastSelectedOnWaitlist(
+  waitlistRegistrations,
+  selectedWaitlistIds,
+) {
+  return waitlistRegistrations.findLast(
+    (reg) => selectedWaitlistIds.includes(reg.user_id),
+  );
+}
+
+export function getSkippedWaitlistCount(
+  registrations,
+  partitionedSelectedIds,
+) {
+  const {
+    pending, waiting, cancelled, rejected,
+  } = partitionedSelectedIds;
+  const aNonAcceptedNonWaitlistIsSelected = pending.length > 0
+    || cancelled.length > 0
+    || rejected.length > 0;
+
+  const waitlistRegistrations = getSortedWaitlistRegistrations(registrations);
+  const lastSelectedOnWaitlist = getLastSelectedOnWaitlist(
+    waitlistRegistrations,
+    waiting,
+  );
+
+  const shouldBeSelectedRegistrations = aNonAcceptedNonWaitlistIsSelected
+    ? waitlistRegistrations
+    : waitlistRegistrations.slice(
+      0,
+      lastSelectedOnWaitlist?.competing?.waiting_list_position ?? 0,
+    );
+
+  return shouldBeSelectedRegistrations.filter(
+    (reg) => !waiting.includes(reg.user_id),
+  ).length;
+}
