ebec4bd817a84636902c8f9985be1b7d47143bbd
FinnIckler <finnickler@gmail.com>
2025-03-03 17:09:07 +0100

    bring back accidental deletion

--- lib/registrations/registration_checker.rb
+++ lib/registrations/registration_checker.rb
@@ -182,6 +182,9 @@ module Registrations
       def validate_update_status!(new_status, competition, current_user, target_user, registration, events)
         raise WcaExceptions::RegistrationError.new(:unprocessable_entity, Registrations::ErrorCodes::INVALID_REQUEST_DATA) unless
           Registration.competing_statuses.include?(new_status)
+        raise WcaExceptions::RegistrationError.new(:forbidden, Registrations::ErrorCodes::COMPETITOR_LIMIT_REACHED) if
+          new_status == Registrations::Helper::STATUS_ACCEPTED && competition.competitor_limit_enabled? &&
+          competition.registrations.accepted.count >= competition.competitor_limit
         raise WcaExceptions::RegistrationError.new(:forbidden, Registrations::ErrorCodes::ALREADY_REGISTERED_IN_SERIES) if
           new_status == Registrations::Helper::STATUS_ACCEPTED && existing_registration_in_series?(competition, target_user)
 
