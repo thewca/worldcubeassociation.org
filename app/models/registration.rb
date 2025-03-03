bc468efaa4a9bddafc7d5f8921907b6865c59c03
Duncan <duncanonthejob@gmail.com>
2025-03-03 07:07:35 +0200

    simplified boolean per review comment

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -398,8 +398,7 @@ class Registration < ApplicationRecord
   end
 
   def consider_auto_close
-    return competition.attempt_auto_close! if outstanding_entry_fees == 0
-    false
+    outstanding_entry_fees.zero? && competition.attempt_auto_close!
   end
 
   validate :only_one_accepted_per_series
