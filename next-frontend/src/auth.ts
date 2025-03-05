0873a36926c43cd070a762cd7e3cdbd335a57eeb
Gregor Billing <gbilling@worldcubeassociation.org>
2025-03-05 23:52:57 +0900

    Override 'profile' scope

--- next-frontend/src/auth.ts
+++ next-frontend/src/auth.ts
@@ -6,10 +6,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     {
       id: "WCA",
       name: "WCA-OIDC-Provider",
-      type: "oauth",
+      type: "oidc",
       issuer: "http://wca_on_rails:3000",
       clientId: "k3cCulPkUe6BCYAB8xP1v4eYMxvKZdzUwH0K3-0R_H0",
       clientSecret: "euFyUD_CTFiSv7mRX0z77LBUQ9teHQo6checdcXSWzc",
+      authorization: {
+        params: {
+          scope: "public openid email"
+        }
+      },
     },
   ],
 })
