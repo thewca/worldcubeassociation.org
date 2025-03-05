36796bbb2fa5a6767b0e57feb04210c5657f733b
Gregor Billing <gbilling@worldcubeassociation.org>
2025-03-05 23:52:37 +0900

    Make OIDC authorization endpoint viewable from the outside world

--- config/initializers/doorkeeper_openid_connect.rb
+++ config/initializers/doorkeeper_openid_connect.rb
@@ -38,4 +38,22 @@ Doorkeeper::OpenidConnect.configure do
       resource_owner.teams
     end
   end
+
+  # DO NOT MERGE IN THIS STATE!
+  # Future WST members: If you ever find this in our code in its current state, punch Gregor.
+  discovery_url_options do
+    {
+      # Override the host only for the `authorization` endpoint, because
+      #   it's the only one that users need to manually access through a browser.
+      # All other endpoints follow ROOT_URL, and you (the developer) are responsible
+      #   for providing correct values there. In case of local development, we use Docker
+      #   which has independent but network-linked containers, so the ROOT_URL will probably
+      #   be something like `wca_on_rails:3000` (which is the way other containers talk to Rails).
+      #   However, that URL won't work when opening it in the browser, which is why we do this override.
+      # Note that the doorkeeper-openid_connect gem exposes this setting (probably) for exactly this
+      #   use-case. It's even a listed sample snippet in their documentation: Just ABOVE
+      #   https://github.com/doorkeeper-gem/doorkeeper-openid_connect?tab=readme-ov-file#scopes
+      authorization: { host: 'http://localhost:3000' }
+    }
+  end
 end
