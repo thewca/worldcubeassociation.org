42fee9afe6458143f38074a2e251ce71f1f05569
Kevin Matthews <kvnrmatthews@gmail.com>
2025-03-06 18:33:20 -0800

    Add refs to table sections

--- app/webpacker/components/RegistrationsV2/RegistrationAdministration/RegistrationAdministrationList.jsx
+++ app/webpacker/components/RegistrationsV2/RegistrationAdministration/RegistrationAdministrationList.jsx
@@ -63,6 +63,15 @@ export default function RegistrationAdministrationList({ competitionInfo }) {
 
   const actionsRef = useRef();
 
+  const pendingRef = useRef();
+  const waitlistRef = useRef();
+  const approvedRef = useRef();
+  const cancelledRef = useRef();
+  const rejectedRef = useRef();
+  const tableRefs = useMemo(() => ({
+    pendingRef, waitlistRef, approvedRef, cancelledRef, rejectedRef,
+  }), []);
+
   const {
     isLoading: isRegistrationsLoading,
     data: registrations,
@@ -172,6 +181,7 @@ export default function RegistrationAdministrationList({ competitionInfo }) {
             title={I18n.t('competitions.registration_v2.list.pending.title')}
             inParens={pending.length}
             color={PENDING_COLOR}
+            sectionRef={pendingRef}
           />
         ),
       },
@@ -206,6 +216,7 @@ export default function RegistrationAdministrationList({ competitionInfo }) {
             title={I18n.t('competitions.registration_v2.list.waitlist.title')}
             inParens={waiting.length}
             color={WAITLIST_COLOR}
+            sectionRef={waitlistRef}
           />
         ),
       },
@@ -257,6 +268,7 @@ export default function RegistrationAdministrationList({ competitionInfo }) {
               }`
             }
             color={APPROVED_COLOR}
+            sectionRef={approvedRef}
           />
         ),
       },
@@ -285,6 +297,7 @@ export default function RegistrationAdministrationList({ competitionInfo }) {
             title={I18n.t('competitions.registration_v2.list.cancelled.title')}
             inParens={cancelled.length}
             color={CANCELLED_COLOR}
+            sectionRef={cancelledRef}
           />
         ),
       },
@@ -318,6 +331,7 @@ export default function RegistrationAdministrationList({ competitionInfo }) {
             title={I18n.t('competitions.registration_v2.list.rejected.title')}
             inParens={rejected.length}
             color={REJECTED_COLOR}
+            sectionRef={rejectedRef}
           />
         ),
       },
@@ -400,12 +414,14 @@ export default function RegistrationAdministrationList({ competitionInfo }) {
 }
 
 function SectionToggle({
-  icon, title, inParens, color,
+  icon, title, inParens, color, sectionRef,
 }) {
   return (
-    <Header as="span" size="large">
-      <Icon name={icon} color={color} />
-      {`${title} (${inParens})`}
-    </Header>
+    <span ref={sectionRef} style={{ scrollMarginTop: '7em' }}>
+      <Header as="span" size="large">
+        <Icon name={icon} color={color} />
+        {`${title} (${inParens})`}
+      </Header>
+    </span>
   );
 }
