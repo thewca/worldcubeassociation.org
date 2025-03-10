39083da8a942782aba425dca352338e6d107b9cc
Gregor Billing <gbilling@worldcubeassociation.org>
2025-03-10 16:06:19 +0900

    Regenerate Payload importmap

--- next-frontend/src/app/(payload)/admin/importMap.js
+++ next-frontend/src/app/(payload)/admin/importMap.js
@@ -1,5 +1,5 @@
-import { SignInWithAuthjsButton } from 'payload-authjs/components'
+import { SignInWithAuthjsButton as SignInWithAuthjsButton_06d0cb594d8f6ba2ac35015f930c882e } from 'payload-authjs/components'
 
 export const importMap = {
-  "payload-authjs/components#SignInWithAuthjsButton": SignInWithAuthjsButton
+  "payload-authjs/components#SignInWithAuthjsButton": SignInWithAuthjsButton_06d0cb594d8f6ba2ac35015f930c882e
 }
