8ef66d109aea63079c96d5e24f5cfaa73f4777d7
FinnIckler <finnickler@gmail.com>
2025-03-11 14:42:40 +0100

    add token refresh

--- next-frontend/src/auth.ts
+++ next-frontend/src/auth.ts
@@ -13,14 +13,63 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   ],
   callbacks: {
-    jwt({ token, trigger, session, account }) {
-      if(account?.access_token){
-        return { ...token, accessToken: account.access_token }
+    async jwt({ token, account }) {
+      if (account) {
+        // First-time login, save the `access_token`, its expiry and the `refresh_token`
+        return {
+          ...token,
+          userId: account.userId,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          refresh_token: account.refresh_token,
+        }
+      } else if (Date.now() < (token.expires_at as number) * 1000) {
+        // Subsequent logins, but the `access_token` is still valid
+        return token
+      } else {
+        // Subsequent logins, but the `access_token` has expired, try to refresh it
+        if (!token.refresh_token) throw new TypeError("Missing refresh_token")
+
+        try {
+          const response = await fetch(`${process.env.OIDC_ISSUER}/oauth/token`, {
+            method: "POST",
+            body: new URLSearchParams({
+              client_id: process.env.OIDC_CLIENT_ID!,
+              client_secret: process.env.OIDC_CLIENT_SECRET!,
+              grant_type: "refresh_token",
+              refresh_token: token.refresh_token! as string,
+            }),
+          })
+
+          const tokensOrError = await response.json()
+
+          if (!response.ok) throw tokensOrError
+
+          const newTokens = tokensOrError as {
+            access_token: string
+            expires_in: number
+            refresh_token?: string
+          }
+
+          return {
+            ...token,
+            access_token: newTokens.access_token,
+            expires_at: Math.floor(Date.now() / 1000 + newTokens.expires_in),
+            // Some providers only issue refresh tokens once, so preserve if we did not get a new one
+            refresh_token: newTokens.refresh_token
+              ? newTokens.refresh_token
+              : token.refresh_token,
+          }
+        } catch (error) {
+          console.error("Error refreshing access_token", error)
+          // If we fail to refresh the token, return an error so we can handle it on the page
+          token.error = "RefreshTokenError"
+          return token
+        }
       }
-      return { ...token };
     },
     async session({ session, token }) {
-      session.accessToken = token.accessToken
+      session.accessToken = token.access_token
       session.user.id = token.userId
       return session
     }
