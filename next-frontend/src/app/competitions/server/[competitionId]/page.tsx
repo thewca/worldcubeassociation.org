916860c2baa7a7da9bd8a528e07f1dca82ed442a
FinnIckler <finnickler@gmail.com>
2025-03-11 16:39:25 +0100

    also use openApi fetch in the server

--- next-frontend/src/app/competitions/server/[competitionId]/page.tsx
+++ next-frontend/src/app/competitions/server/[competitionId]/page.tsx
@@ -1,12 +1,17 @@
-import { components } from "@/lib/wca/wcaSchema";
 import { Container, Heading } from "@chakra-ui/react";
 import PermissionProvider from "@/providers/PermissionProvider";
 import PermissionsTestMessage from "@/components/competitions/permissionsTestMessage";
+import { serverClient } from "@/lib/wca/wcaAPI";
 
 export default async function CompetitionOverView({ params }: { params: Promise<{ competitionId: string }> }){
   const { competitionId } = await params;
-  const data = await fetch(`${process.env.OIDC_ISSUER}/api/v0/competitions/${competitionId}/`);
-  const competitionInfo: components["schemas"]["CompetitionInfo"] = await data.json();
+  const { data: competitionInfo, error } = await serverClient.GET("/competitions/{competitionId}/", { params: { path: { competitionId }}})
+
+  if(error){
+    return <p>
+      Error fetching competition
+    </p>
+  }
 
   if(!competitionInfo){
     return <p>
