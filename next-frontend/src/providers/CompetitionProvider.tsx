220c4865ebcace5966136950d97bc93c3525d9c3
FinnIckler <finnickler@gmail.com>
2025-03-11 16:52:01 +0100

    allow access to the raw query in competition provider

--- next-frontend/src/providers/CompetitionProvider.tsx
+++ next-frontend/src/providers/CompetitionProvider.tsx
@@ -2,19 +2,21 @@
 
 import React, { createContext, useContext } from "react";
 import { components} from "@/lib/wca/wcaSchema";
-import { useQuery } from "@tanstack/react-query";
+import {useQuery, UseQueryResult} from "@tanstack/react-query";
 import useAPI from "@/lib/wca/useAPI";
 
-const CompetitionContext = createContext<components["schemas"]["CompetitionInfo"] | null>(null);
+const CompetitionContext = createContext<[components["schemas"]["CompetitionInfo"] | null, UseQueryResult | null]>([null, null]);
 
-export const useCompetitionInfo = () => useContext(CompetitionContext);
+export const useCompetitionInfo = () => useContext(CompetitionContext)[0];
+export const useCompetitionInfoQuery = () => useContext(CompetitionContext)[1];
 
 export default function CompetitionProvider({ competitionId, children }: { competitionId: string, children: React.ReactNode }){
   const api = useAPI();
-  const { data: request, isLoading } = useQuery({
-    queryKey: ["permissions", competitionId],
+  const query = useQuery({
+    queryKey: ["competition", competitionId],
     queryFn: () => api.GET("/competitions/{competitionId}/", { params: { path: { competitionId } } }),
   })
+  const { data: request, isLoading } = query;
 
   if(isLoading) {
     return (
@@ -22,8 +24,14 @@ export default function CompetitionProvider({ competitionId, children }: { compe
     )
   }
 
+  if(!request?.data) {
+    return (
+      <p>Error fetching competition</p>
+    )
+  }
+
   return (
-    <CompetitionContext.Provider value={request!.data!}>
+    <CompetitionContext.Provider value={[request.data, query]}>
       {children}
     </CompetitionContext.Provider>
   )
