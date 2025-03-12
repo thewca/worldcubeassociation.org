716bd8271492851ca65d1e51d2fd105330588583
FinnIckler <finnickler@gmail.com>
2025-03-12 11:50:41 +0100

    remove wrong controller merge whoops

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -269,11 +269,7 @@ class RegistrationsController < ApplicationController
 
   def register
     @competition = competition_from_params
-    @registration = nil
-    @selected_events = []
     if current_user
-      @registration = @competition.registrations.find_or_initialize_by(user_id: current_user.id, competition_id: @competition.id)
-      @selected_events = @registration.saved_and_unsaved_events.empty? ? @registration.user.preferred_events : @registration.saved_and_unsaved_events
       @is_processing = Rails.cache.read(CacheAccess.registration_processing_cache_key(@competition.id, current_user.id)) || false
     end
   end
