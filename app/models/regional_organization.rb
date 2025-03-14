63e17036c0f513e9226b2671eb8cb4e87eb23c3b
FinnIckler <finnickler@gmail.com>
2025-03-14 12:19:45 +0100

    Activate Rails/Validation (#11045)

--- app/models/regional_organization.rb
+++ app/models/regional_organization.rb
@@ -9,7 +9,7 @@ class RegionalOrganization < ApplicationRecord
   scope :pending_approval, -> { where(start_date: nil) }
   scope :previously_acknowledged, -> { where("start_date IS NOT NULL AND end_date IS NOT NULL AND end_date < ?", Date.today) }
 
-  validates_presence_of :name, :country, :email, :address, :directors_and_officers, :area_description, :past_and_current_activities, :future_plans
+  validates :name, :country, :email, :address, :directors_and_officers, :area_description, :past_and_current_activities, :future_plans, presence: true
   validates :website, presence: true, format: { with: %r{\Ahttps?://.*\z} }
   validates :logo, presence: true, blob: { content_type: 'image/png', size_range: 0..(200.kilobytes) }
   validates :bylaws, presence: true, blob: { content_type: 'application/pdf', size_range: 0..(250.kilobytes) }
