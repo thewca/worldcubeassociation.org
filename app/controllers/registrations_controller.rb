159941b56aa99c56ee478e449e82357ae609dae1
FinnIckler <finnickler@gmail.com>
2025-03-19 13:47:03 +0100

    Get Processing Status from Redis into the frontend (#10297)
    * Get Processing Status from Redis into the frontend
    
    * Update en.yml
    
    * only set isProcessing in useEffect
    
    * revert changes that are now part of the other PR
    
    * set isProcessing in Registration Provider
    
    * lint
    
    * remove wrong controller merge whoops
    
    * make line breaks
    
    * rename variable from context
    
    * Update app/controllers/registrations_controller.rb
    
    Co-authored-by: Gregor Billing <gregor.billing.dev@gmail.com>
    
    * false as fallback for cache access
    
    * inline cache check because current_user needs to be present
    
    * use present? instead
    
    ---------
    
    Co-authored-by: Duncan <52967253+dunkOnIT@users.noreply.github.com>
    Co-authored-by: Gregor Billing <gregor.billing.dev@gmail.com>

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -256,6 +256,7 @@ class RegistrationsController < ApplicationController
 
   def register
     @competition = competition_from_params
+    @is_processing = current_user.present? && Rails.cache.read(CacheAccess.registration_processing_cache_key(@competition.id, current_user.id)).present?
   end
 
   def payment_denomination
