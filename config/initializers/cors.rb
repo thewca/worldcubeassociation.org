3572968003257e28db811bdb29cb41711a7bdcbd
FinnIckler <finnickler@gmail.com>
2025-03-24 14:13:47 +0100

    move the cors file to its own initializer

---
+++ config/initializers/cors.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+if Rails.env.local?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+
+      resource '*',
+               headers: :any,
+               expose: %i[Authorization Total Per-Page Link],
+               methods: :any
+    end
+  end
+else
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+
+      resource(
+        '/api/v0/*',
+        headers: %w[Origin X-Requested-With Content-Type Accept Authorization],
+        methods: %i[get post delete put patch options head],
+        expose: %w[Total Per-Page Link],
+        max_age: 0,
+        credentials: false,
+        )
+
+      resource(
+        '/api/v1/*',
+        headers: %w[Origin X-Requested-With Content-Type Accept Authorization],
+        methods: %i[get post delete put patch options head],
+        expose: %w[Total Per-Page Link],
+        max_age: 0,
+        credentials: false,
+        )
+    end
+
+    allow do
+      origins 'https://worldcubeassociation.org', 'https://staging.worldcubeassociation.org'
+
+      resource(
+        '/api/internal/*',
+        headers: %w[Origin X-Requested-With Content-Type Accept Authorization],
+        methods: %i[get post delete put patch options head],
+        max_age: 0,
+        credentials: false,
+        )
+    end
+  end
+end
