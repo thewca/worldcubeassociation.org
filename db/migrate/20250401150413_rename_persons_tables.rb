ae47ad0311936f315b9c37e05cb51965587dbb91
FinnIckler <finnickler@gmail.com>
2025-04-01 16:06:18 +0200

    rubocop

--- db/migrate/20250401150413_rename_persons_tables.rb
+++ db/migrate/20250401150413_rename_persons_tables.rb
@@ -2,14 +2,14 @@
 
 class RenamePersonsTables < ActiveRecord::Migration[7.2]
   def change
-    change_table :Persons do |t|
+    change_table :Persons, bulk: true do |t|
       t.rename :subId, :sub_id
       t.rename :countryId, :country_id
     end
 
     rename_table :Persons, :persons
 
-    change_table :InboxPersons do |t|
+    change_table :InboxPersons, bulk: true do |t|
       t.rename :wcaId, :wca_id
       t.rename :countryId, :country_iso2
       t.rename :competitionId, :competition_id
