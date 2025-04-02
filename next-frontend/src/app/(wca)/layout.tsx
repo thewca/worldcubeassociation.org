a2db3f28445a62005ecd9df221ac42121e7c4cde
Gregor Billing <gbilling@worldcubeassociation.org>
2025-04-02 19:12:10 +0900

    Banish TTNorms into a separate export until we figure out font loading

--- next-frontend/src/app/(wca)/layout.tsx
+++ next-frontend/src/app/(wca)/layout.tsx
@@ -6,68 +6,24 @@ import PermissionProvider from "@/providers/PermissionProvider";
 import { Provider as UiProvider } from "@/components/ui/provider";
 import Navbar from "./navbar";
 import Footer from "@/components/Footer";
-import localFont from "next/font/local";
-import { Rubik } from "next/font/google";
 import RandomBackground from "@/components/RandomBackground";
-
-const loadWcaFont = () => {
-  const envFontPath = process.env.WCA_FONT_RELATIVE_PATH;
-
-  if (envFontPath) {
-    return localFont({
-      src: [
-        {
-          path: `${envFontPath}/TT_Norms_Pro_Light.woff2`,
-          weight: "300",
-          style: "normal",
-        },
-        {
-          path: `${envFontPath}/TT_Norms_Pro_Light_Italic.woff2`,
-          weight: "300",
-          style: "italic",
-        },
-        {
-          path: `${envFontPath}/TT_Norms_Pro_Regular.woff2`,
-          weight: "400",
-          style: "normal",
-        },
-        {
-          path: `${envFontPath}/TT_Norms_Pro_Medium.woff2`,
-          weight: "500",
-          style: "normal",
-        },
-        {
-          path: `${envFontPath}/TT_Norms_Pro_Bold.woff2`,
-          weight: "700",
-          style: "normal",
-        },
-        {
-          path: `${envFontPath}/TT_Norms_Pro_Condensed_ExtraBold.woff2`,
-          weight: "800",
-          style: "normal",
-        },
-      ],
-    });
-  } else {
-    return Rubik();
-  }
-};
+import { Rubik } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "WCA Website",
   description: "WST x SLATE",
 };
 
-const configuredFont = loadWcaFont();
+const devFont = Rubik({ subsets: ["latin"] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html suppressHydrationWarning>
-      <body className={configuredFont.className}>
+      <body className={devFont.className}>
         <WCAQueryClientProvider>
           <AuthProvider>
             <UiProvider>
