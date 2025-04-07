0429b8a5df93f31fc6d553780b9ba8232f7c9594
ando527 <ando5@MitchellsLaptop.>
2025-04-07 12:59:21 +1000

    Initial commit for Competitions page

---
+++ next-frontend/src/components/competitions/TabRegister.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card, Text } from "@chakra-ui/react";
+
+export default function TabRegister() {
+  return (
+    <Card.Root>
+      <Card.Body>
+        <Text>Register</Text>
+      </Card.Body>
+    </Card.Root>
+  );
+}
---
+++ next-frontend/src/components/competitions/TabSchedule.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card, Text } from "@chakra-ui/react";
+
+export default function TabSchedule() {
+  return (
+    <Card.Root>
+      <Card.Body>
+        <Text>Schedule</Text>
+      </Card.Body>
+    </Card.Root>
+  );
+}
