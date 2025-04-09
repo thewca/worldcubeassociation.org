b5e7d0df9bf0904d7742d4bc35e297e3bb22ba37
Nicholas McKee <105333582+mckeenicholas@users.noreply.github.com>
2025-04-09 11:40:03 -0400

    Update us_city_validator.rb
    Co-authored-by: Maksymilian Gala <maksymilian.gala1@gmail.com>

--- lib/country_city_validators/us_city_validator.rb
+++ lib/country_city_validators/us_city_validator.rb
@@ -63,7 +63,7 @@ module CountryCityValidators
   ).to_set
 
   US_DISTRICTS = %w(
-    District\ of\ Columbia.
+    District\ of\ Columbia
   ).to_set
 
   class UsCityValidator < CityCommaRegionValidator
