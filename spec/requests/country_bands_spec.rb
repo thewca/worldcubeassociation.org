2c933ee7b5935b06fd04379e02508d9157016436
Duncan <duncanonthejob@gmail.com>
2025-04-11 12:33:29 +0200

    more tests

--- spec/requests/country_bands_spec.rb
+++ spec/requests/country_bands_spec.rb
@@ -23,7 +23,8 @@ RSpec.describe "Country bands controller" do
     end
 
     context "when signed in as a regular user" do
-      sign_in { create :user }
+      let(:user) { create :user }
+      before { sign_in user}
       it "redirect to root" do
         get edit_country_band_path(0)
         expect(response).to redirect_to root_url
@@ -31,7 +32,9 @@ RSpec.describe "Country bands controller" do
     end
 
     context "when signed in as a WFC member" do
-      sign_in { create :user, :wfc_member }
+      let(:wfc_member) { create :user, :wfc_member }
+      before { sign_in wfc_member}
+
       it "shows the page" do
         get edit_country_band_path(0)
         expect(response).to be_successful
@@ -43,7 +46,9 @@ RSpec.describe "Country bands controller" do
     let(:some_countries) { ["US", "AL"] }
 
     context "when signed in as a regular user" do
-      sign_in { create :user }
+      let(:user) { create :user }
+      before { sign_in user}
+
       it "redirect to root" do
         put country_band_path(0, params: { countries: { iso2s: some_countries.join(",") } })
         expect(response).to redirect_to root_url
