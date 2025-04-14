ea0993af4617c2244af1e0b55b3997d091e9180c
Gregor Billing <gbilling@worldcubeassociation.org>
2025-04-14 11:13:49 +0900

    Silence Dart SCSS deprecations

--- config/webpack/commonWebpackConfig.js
+++ config/webpack/commonWebpackConfig.js
@@ -52,6 +52,16 @@ const customConfig = {
   ignoreWarnings: [/Module not found: Error: Can't resolve 'react-dom\/client'/],
 };
 
+// Find the SASS/SCSS compiler pre-packaged into Shakapacker.
+// This is the recomended way of augmenting/configuring pre-packaged loader rules
+//   per https://github.com/shakacode/shakapacker?tab=readme-ov-file#webpack-configuration
+const sassRule = baseClientWebpackConfig.module.rules.find((rule) => rule.test.test('.scss'));
+const sassLoader = sassRule.use.find((useDirective) => useDirective.loader?.includes('sass-loader'));
+
+// Mute SCSS deprecation warnings for stuff in transitive dependencies that is out of our control
+//   Reference: https://sass-lang.com/documentation/js-api/interfaces/options/#quietDeps
+sassLoader.options.sassOptions.quietDeps = true;
+
 // Copy the object using merge b/c the baseClientWebpackConfig and commonOptions are mutable globals
 const commonWebpackConfig = () => merge({}, baseClientWebpackConfig, customConfig);
 
