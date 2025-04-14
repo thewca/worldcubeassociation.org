362baa2f8d693d329766a585f896900af3df0912
FinnIckler <finnickler@gmail.com>
2025-04-14 13:21:26 +0200

    add necessary methods to ManualPaymentRecord

--- app/models/manual_payment_record.rb
+++ app/models/manual_payment_record.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
 class ManualPaymentRecord < ApplicationRecord
-  belongs_to :registration
+  has_one :registration_payment, as: :receipt
+  has_one :payment_intent, as: :payment_record
 
   validates :payment_reference, presence: true
+
+  def determine_wca_status
+    "succeeded"
+  end
+
+  def retrieve_remote
+    self
+  end
+
+  def money_amount
+    Money.new(self.amount_iso_denomination, self.currency_code)
+  end
+
+  def ruby_amount_available_for_refund
+    self.amount_iso_denomination
+  end
 end
