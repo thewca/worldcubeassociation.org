b127c83088f280e6a02fd7f15e24407f7c6cdcc7
Gregor Billing <gbilling@worldcubeassociation.org>
2025-04-18 22:42:46 +0900

    Use more shorthands and less providers in UI

--- app/webpacker/components/ManualPaymentSetup/index.jsx
+++ app/webpacker/components/ManualPaymentSetup/index.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import {
-  Form, FormField, Header,
-} from 'semantic-ui-react';
+import { Form, Header } from 'semantic-ui-react';
 import I18n from '../../lib/i18n';
 import MarkdownEditor from '../wca/FormBuilder/input/MarkdownEditor';
 import useInputState from '../../lib/hooks/useInputState';
 import { paymentSetupUrl } from '../../lib/requests/routes.js.erb';
-import WCAQueryClientProvider from '../../lib/providers/WCAQueryClientProvider';
 
-export default function Wrapper({ competitionId }) {
-  return (
-    <WCAQueryClientProvider>
-      <ManualPaymentSetup competitionId={competitionId} />
-    </WCAQueryClientProvider>
-  );
-}
-
-function ManualPaymentSetup({ competitionId }) {
+export default function ManualPaymentSetup({ competitionId }) {
   const [paymentInfo, setPaymentInfo] = useInputState('');
   const [paymentReference, setPaymentReference] = useInputState('');
 
@@ -26,15 +15,20 @@ function ManualPaymentSetup({ competitionId }) {
         {I18n.t('payments.payment_setup.manual_payments_header')}
       </Header>
       <Form method="GET" action={paymentSetupUrl(competitionId, 'manual')}>
-        <FormField>
-          <label htmlFor="paymentInfo">{I18n.t('payments.payment_setup.account_details.manual.payment_info')}</label>
-          <MarkdownEditor id="paymentInfo" value={paymentInfo} onChange={setPaymentInfo} imageUploadEnabled />
-          <input value={paymentInfo} name="payment_information" hidden />
-        </FormField>
-        <FormField>
-          <label htmlFor="paymentReference">{I18n.t('payments.payment_setup.account_details.manual.payment_reference')}</label>
-          <Form.Input id="paymentReference" name="payment_reference" value={paymentReference} onChange={setPaymentReference} />
-        </FormField>
+        <Form.Field
+          label={I18n.t('payments.payment_setup.account_details.manual.payment_info')}
+          control={MarkdownEditor}
+          value={paymentInfo}
+          onChange={setPaymentInfo}
+          imageUploadEnabled
+        />
+        <input value={paymentInfo} name="payment_information" hidden />
+        <Form.Input
+          label={I18n.t('payments.payment_setup.account_details.manual.payment_reference')}
+          name="payment_reference"
+          value={paymentReference}
+          onChange={setPaymentReference}
+        />
         <Form.Button type="submit">
           Submit
         </Form.Button>
