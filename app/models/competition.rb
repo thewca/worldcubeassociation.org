0cad02b636ca952863e1acfc19558f1489bb064f
Gregor Billing <gbilling@worldcubeassociation.org>
2025-04-22 15:42:42 +0900

    Switch main schema to use competitorCanCancel

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2775,7 +2775,7 @@ class Competition < ApplicationRecord
             "waitingListDeadlineDate" => date_json_schema("date-time"),
             "eventChangeDeadlineDate" => date_json_schema("date-time"),
             "allowOnTheSpot" => { "type" => ["boolean", "null"] },
-            "allowSelfDeleteAfterAcceptance" => { "type" => "boolean" },
+            "competitorCanCancel" => { "type" => "string" },
             "allowSelfEdits" => { "type" => "boolean" },
             "guestsEnabled" => { "type" => "boolean" },
             "guestEntryStatus" => { "type" => "string" },
