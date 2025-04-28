5e2c3447378fbc05d4850d22d33d4ae532679827
Duncan <duncanonthejob@gmail.com>
2025-04-28 15:40:18 +0200

    added auto_accept_enabled? method

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -522,8 +522,12 @@ class Registration < ApplicationRecord
     super(DEFAULT_SERIALIZE_OPTIONS.merge(options || {}))
   end
 
+  def auto_accept_enabled?
+    Rails.env.production? && EnvConfig.WCA_LIVE_SITE?
+  end
+
   def attempt_auto_accept
-    return false if Rails.env.production? && EnvConfig.WCA_LIVE_SITE?
+    return false unless auto_accept_enabled
 
     failure_reason = auto_accept_failure_reason
     if failure_reason.present?
