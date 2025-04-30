e914efbbc7e18383146ef57c1d50802367f790f8
Duncan <52967253+dunkOnIT@users.noreply.github.com>
2025-04-30 15:02:25 +0200

    switch .blank? to &.zero?: (#11447)

--- app/helpers/persons_helper.rb
+++ app/helpers/persons_helper.rb
@@ -3,7 +3,7 @@
 module PersonsHelper
   def rank_td(rank_object, type)
     rank = rank_object&.public_send("#{type}_rank")
-    rank = "-" if rank.blank?
+    rank = "-" if rank&.zero?
     content_tag :td, rank, class: "#{type}-rank #{'record' if rank == 1}"
   end
 
