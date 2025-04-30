037163d1d22472c12b7eabc22eb3bf5bd52f8517
FinnIckler <finnickler@gmail.com>
2025-04-30 14:37:43 +0200

    rubocop

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -116,7 +116,7 @@ RSpec.describe RegistrationsController, :clean_db_with_truncation do
           refund_amount = -1
           post :refund_payment, params: { competition_id: competition.id, payment_integration: :stripe, payment_id: @payment.receipt.id, payment: { refund_amount: refund_amount } }
           expect(response).to have_http_status(:bad_request)
-          expect(response.parsed_body).to eq { "error" => "refund_amount_too_low" }
+          expect(response.parsed_body).to(eq { "error" => "refund_amount_too_low" })
           expect(competition.base_entry_fee).to be > 0
           expect(registration.outstanding_entry_fees).to eq 0
           expect(@payment.reload.amount_available_for_refund).to eq competition.base_entry_fee.cents
@@ -126,7 +126,7 @@ RSpec.describe RegistrationsController, :clean_db_with_truncation do
           refund_amount = competition.base_entry_fee.cents * 2
           post :refund_payment, params: { competition_id: competition.id, payment_integration: :stripe, payment_id: @payment.receipt.id, payment: { refund_amount: refund_amount } }
           expect(response).to have_http_status(:bad_request)
-          expect(response.parsed_body).to eq { "error" => "refund_amount_too_high" }
+          expect(response.parsed_body).to(eq { "error" => "refund_amount_too_high" })
           expect(competition.base_entry_fee).to be > 0
           expect(registration.outstanding_entry_fees).to eq 0
           expect(@payment.reload.amount_available_for_refund).to eq competition.base_entry_fee.cents
@@ -136,7 +136,7 @@ RSpec.describe RegistrationsController, :clean_db_with_truncation do
           ClearConnectedPaymentIntegrations.perform_now
           post :refund_payment, params: { competition_id: competition.id, payment_integration: :stripe, payment_id: @payment.receipt.id, payment: { refund_amount: competition.base_entry_fee.cents } }
           expect(response).to have_http_status(:not_found)
-          expect(response.parsed_body).to eq { "error" => "provider_disconnected" }
+          expect(response.parsed_body).to(eq { "error" => "provider_disconnected" })
           expect(@payment.reload.amount_available_for_refund).to eq competition.base_entry_fee.cents
         end
       end
