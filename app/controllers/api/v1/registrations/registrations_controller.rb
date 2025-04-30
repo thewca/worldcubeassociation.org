42d55d8e5f44536c29805078454685fc659ebb5f
Duncan <duncanonthejob@gmail.com>
2025-04-30 12:04:25 +0200

    more rubocop

--- app/controllers/api/v1/registrations/registrations_controller.rb
+++ app/controllers/api/v1/registrations/registrations_controller.rb
@@ -312,7 +312,7 @@ class Api::V1::Registrations::RegistrationsController < Api::V1::ApiController
 
     def contains_admin_fields?(request)
       admin_fields = %w[admin_comment waiting_list_position]
-      
+
       request['competing']&.keys&.any? { |key| admin_fields.include?(key) }
     end
 end
