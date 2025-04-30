4968f0dc6cbc15b7c343270e26dc917e4ff99c58
FinnIckler <finnickler@gmail.com>
2025-04-30 16:26:03 +0200

    switch to new API

--- app/webpacker/components/RegistrationsV2/Register/PaymentOverview.jsx
+++ app/webpacker/components/RegistrationsV2/Register/PaymentOverview.jsx
@@ -5,6 +5,7 @@ import getRegistrationPayments from '../api/payment/get/getRegistrationPayments'
 import Loading from '../../Requests/Loading';
 import I18n from '../../../lib/i18n';
 import { useRegistration } from '../lib/RegistrationProvider';
+import { isoMoneyToHumanReadable } from '../../../lib/helpers/money';
 
 export default function PaymentOverview() {
   const { registration } = useRegistration();
@@ -26,7 +27,7 @@ export default function PaymentOverview() {
     <Message success icon>
       <Icon name="checkmark" />
       <Message.Content>
-        {I18n.t('registrations.entry_fees_fully_paid', { paid: registration?.payment.payment_amount_human_readable })}
+        {I18n.t('registrations.entry_fees_fully_paid', { paid: isoMoneyToHumanReadable(_.sumBy(payments, 'iso_amount_refundable'), payments[0].currency_code) })}
       </Message.Content>
     </Message>
   );
