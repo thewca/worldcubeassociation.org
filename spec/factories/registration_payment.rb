a10f1cf87a792aff97e9f790b7eff124b2d34f4c
Duncan <52967253+dunkOnIT@users.noreply.github.com>
2025-05-02 12:40:22 +0200

    Reject payment ticket when `outstanding_entry_fees` == 0 (#11398)
    * reject payment ticket is registration is already paid for
    
    * changed wording to outstanding payment
    
    * rubocop
    
    * test fixes
    
    * corrected test names
    
    * rubocop
    
    * fix transient registration reference
    
    * changed tests back from any_instance_of
    
    * add in method call count assertion
    
    * remove tag

--- spec/factories/registration_payment.rb
+++ spec/factories/registration_payment.rb
@@ -2,6 +2,21 @@
 
 FactoryBot.define do
   factory :registration_payment do
-    amount_lowest_denomination { 0 }
+    transient do
+      competition { registration.competition }
+    end
+
+    registration { nil }
+    user_id { registration&.user_id }
+    amount_lowest_denomination { competition&.base_entry_fee_lowest_denomination }
+    currency_code { competition&.currency_code }
+
+    trait :refund do
+      amount_lowest_denomination { -competition.base_entry_fee_lowest_denomination }
+    end
+
+    trait :with_donation do
+      amount_lowest_denomination { competition.base_entry_fee_lowest_denomination * 2 }
+    end
   end
 end
