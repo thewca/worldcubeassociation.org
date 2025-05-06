9b163e8484af28285b6c8129393b08d6c6268dc4
Duncan <duncanonthejob@gmail.com>
2025-05-06 06:22:12 +0200

    rubocop

--- app/models/registration_payment.rb
+++ app/models/registration_payment.rb
@@ -10,7 +10,7 @@ class RegistrationPayment < ApplicationRecord
   has_many :refunding_registration_payments, class_name: 'RegistrationPayment', inverse_of: :refunded_registration_payment, foreign_key: :refunded_registration_payment_id, dependent: :destroy
 
   delegate :auto_accept_in_current_env?, :auto_accept_registrations, to: :registration
-  after_create :auto_accept_hook, if: [:auto_accept_in_current_env?, :auto_accept_registrations]
+  after_create :auto_accept_hook, if: %i[auto_accept_in_current_env? auto_accept_registrations]
   after_create :auto_close_hook, unless: :refunded_registration_payment_id?
 
   monetize :amount_lowest_denomination,
@@ -64,5 +64,4 @@ class RegistrationPayment < ApplicationRecord
 
     v2_json
   end
-
 end
--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -507,11 +507,11 @@ RSpec.describe Registration do
     end
 
     it 'updates registration history' do
-      registration.update_lanes!({ user_id: registration.user.id, competing: { event_ids: ['333', '444', '555'] } }.with_indifferent_access, registration.user.id)
+      registration.update_lanes!({ user_id: registration.user.id, competing: { event_ids: %w[333 444 555] } }.with_indifferent_access, registration.user.id)
       last_entry = registration.reload.registration_history.entries.last
       expect(last_entry[:actor_type]).to eq('user')
       expect(last_entry[:actor_id].to_i).to eq(registration.user.id)
-      expect(last_entry[:changed_attributes]).to eq({ event_ids: ["444", "555"] })
+      expect(last_entry[:changed_attributes]).to eq({ event_ids: %w[444 555] })
     end
 
     describe 'update waiting list position' do
