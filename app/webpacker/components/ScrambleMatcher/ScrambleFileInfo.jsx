fabaa7694f6c6b0dd8ef97792d47090c3fab8a65
FinnIckler <finnickler@gmail.com>
2025-05-12 20:43:41 +0200

    fix accordion

--- app/webpacker/components/ScrambleMatcher/ScrambleFileInfo.jsx
+++ app/webpacker/components/ScrambleMatcher/ScrambleFileInfo.jsx
@@ -1,35 +1,42 @@
 import React, { useState } from 'react';
 import {
-  Accordion, Card, CardContent, CardHeader, Header, List, ListItem,
+  Accordion, Card, CardContent, CardDescription, CardHeader, Header, Icon, List, ListItem,
 } from 'semantic-ui-react';
 
 export default function ScrambleFileInfo({ scramble }) {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <Card>
-      <CardHeader
-        content={(
-          <Header>
+    <Card fluid>
+      <Accordion open={expanded} styled fluid>
+        <Accordion.Title onClick={() => setExpanded(!expanded)}>
+          <CardHeader>
+            <Header>
+              <Icon name="dropdown" />
+              {scramble.competitionName}
+            </Header>
+          </CardHeader>
+          <CardDescription>
             Generated with
             {' '}
             {scramble.version}
+            <br />
             On
             {' '}
             {scramble.generationDate}
-          </Header>
-          )}
-      />
-      <Accordion open={expanded} onTitleClick={() => setExpanded(!expanded)}>
-        <CardContent>
-          <List>
-            {scramble.sheets.map((sheet) => (
-              <ListItem key={sheet.id}>
-                {sheet.title}
-              </ListItem>
-            ))}
-          </List>
-        </CardContent>
+          </CardDescription>
+        </Accordion.Title>
+        <Accordion.Content active={expanded}>
+          <CardContent>
+            <List>
+              {scramble.sheets.map((sheet) => (
+                <ListItem key={sheet.id}>
+                  {sheet.title}
+                </ListItem>
+              ))}
+            </List>
+          </CardContent>
+        </Accordion.Content>
       </Accordion>
     </Card>
   );
