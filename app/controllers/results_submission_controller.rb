89f8fc77ae0677970da08a347451bd2ff501bee0
Gregor Billing <gbilling@worldcubeassociation.org>
2025-05-14 21:19:22 +0900

    Provide upload API for inbox scrambles (#11576)
    * Add scramble set migrations and schema
    
    * Add AR models
    
    * Add rudimentary JSON upload endpoint
    
    * Fix competition ID backfilling
    
    * Return already existing scramble set if found
    
    * Fix nested serialization
    
    * Wrap scramble linking in transaction
    
    * Add sanitizers for new tables

--- app/controllers/results_submission_controller.rb
+++ app/controllers/results_submission_controller.rb
@@ -12,6 +12,69 @@ class ResultsSubmissionController < ApplicationController
     @results_validator.validate(@competition.id)
   end
 
+  def upload_scramble_json
+    competition = competition_from_params
+
+    raw_file_contents = params.require(:tnoodle).require(:json).read
+    tnoodle_json = JSON.parse(raw_file_contents, symbolize_names: true)
+
+    # The original Java `LocalDateTime` format is defined as `"MMM dd, yyyy h:m:s a"`.
+    generation_date = DateTime.strptime(tnoodle_json[:generationDate], "%b %d, %Y %l:%M:%S %p")
+
+    tnoodle_wcif = tnoodle_json[:wcif]
+
+    existing_upload = ScrambleFileUpload.find_by(
+      competition: competition,
+      scramble_program: tnoodle_json[:version],
+      generated_at: generation_date,
+    )
+
+    return render json: { success: :ok, scramble_file: existing_upload } if existing_upload.present?
+
+    scr_file_upload = ScrambleFileUpload.create!(
+      uploaded_by_user: current_user,
+      uploaded_at: DateTime.now,
+      competition: competition,
+      scramble_program: tnoodle_json[:version],
+      generated_at: generation_date,
+      raw_wcif: tnoodle_json[:wcif],
+    )
+
+    scr_file_upload.transaction do
+      tnoodle_wcif[:events].each do |wcif_event|
+        competition_event = competition.competition_events.find_by(event_id: wcif_event[:id])
+
+        wcif_event[:rounds].each do |wcif_round|
+          competition_round = competition_event.rounds.find { it.wcif_id == wcif_round[:id] }
+
+          wcif_round[:scrambleSets].each_with_index do |wcif_scramble_set, idx|
+            scramble_set = scr_file_upload.inbox_scramble_sets.create!(
+              ordered_index: idx,
+              matched_round: competition_round,
+            )
+
+            wcif_scramble_set[:scrambles].each_with_index do |wcif_scramble, n|
+              scramble_set.inbox_scrambles.create!(
+                scramble_string: wcif_scramble,
+                scramble_number: n + 1,
+              )
+            end
+
+            wcif_scramble_set[:extraScrambles].each_with_index do |wcif_extra_scramble, n|
+              scramble_set.inbox_scrambles.create!(
+                scramble_string: wcif_extra_scramble,
+                scramble_number: n + 1,
+                is_extra: true,
+              )
+            end
+          end
+        end
+      end
+    end
+
+    render json: { success: :ok, scramble_file: scr_file_upload }
+  end
+
   def upload_json
     @competition = competition_from_params
     return redirect_to competition_submit_results_edit_path if @competition.results_submitted?
