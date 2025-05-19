6cdc8c9fad09525d102fb9220863fb8bda1f94cc
Gregor Billing <gbilling@worldcubeassociation.org>
2025-05-19 16:05:06 +0900

    Switch back to dragging table routes while recomputing labels

--- app/webpacker/components/ScrambleMatcher/ScrambleMatch.jsx
+++ app/webpacker/components/ScrambleMatcher/ScrambleMatch.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon, Ref, Table } from 'semantic-ui-react';
 import { activityCodeToName } from '@wca/helpers';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
@@ -12,7 +12,18 @@ export default function ScrambleMatch({
   const { scrambleSetCount } = activeRound;
   const scrambleSets = matchState[activeRound.id] ?? [];
 
+  const [currentDragStart, setCurrentDragStart] = useState(null);
+  const [currentDragIndex, setCurrentDragIndex] = useState(null);
+
+  const handleOnBeforeDragStart = (init) => {
+    setCurrentDragStart(init.source?.index);
+    setCurrentDragIndex(init.source?.index);
+  };
+
   const handleOnDragEnd = (result) => {
+    setCurrentDragStart(null);
+    setCurrentDragIndex(null);
+
     const { destination, source } = result;
 
     if (destination) {
@@ -20,16 +31,41 @@ export default function ScrambleMatch({
     }
   };
 
+  const handleOnDragUpdate = (update) => {
+    setCurrentDragIndex(update.destination?.index);
+  };
+
+  const dragDistance = currentDragIndex === null ? 0 : currentDragIndex - currentDragStart;
+  const dragDirection = Math.sign(dragDistance);
+
+  const computeOnDragIndex = (elIndex, isDragging = false) => {
+    if (isDragging) {
+      return currentDragIndex;
+    }
+
+    const dragRangeStart = Math.min(currentDragStart, currentDragIndex);
+    const dragRangeEnd = Math.max(currentDragStart, currentDragIndex);
+
+    if (elIndex >= dragRangeStart && elIndex <= dragRangeEnd) {
+      return elIndex - dragDirection;
+    }
+    return elIndex;
+  };
+
   /* eslint-disable react/jsx-props-no-spreading */
   return (
-    <Table definition fixed>
+    <Table definition>
       <Table.Header>
         <Table.Row>
-          <Table.HeaderCell width={4} />
+          <Table.HeaderCell />
           <Table.HeaderCell>Assigned Scrambles</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
+      <DragDropContext
+        onBeforeDragStart={handleOnBeforeDragStart}
+        onDragUpdate={handleOnDragUpdate}
+        onDragEnd={handleOnDragEnd}
+      >
         <Droppable droppableId="scrambles">
           {(providedDroppable) => (
             <Ref innerRef={providedDroppable.innerRef}>
@@ -40,44 +76,46 @@ export default function ScrambleMatch({
                   const isExtra = index >= scrambleSetCount;
 
                   return (
-                    <Table.Row
+                    <Draggable
                       key={scramble.id}
-                      negative={hasError || isExtra}
+                      draggableId={scramble.id.toString()}
+                      index={index}
                     >
-                      <Table.Cell textAlign="right">
-                        {isExpected
-                          ? `${activityCodeToName(activeRound.id)}, Group ${index + 1}`
-                          : 'Extra Scramble set (unassigned)'}
-                        {(hasError || isExtra) && (
-                          <>
-                            {' '}
-                            <Icon name="exclamation triangle" />
-                            {hasError ? 'Missing scramble' : 'Unexpected Scramble Set'}
-                          </>
-                        )}
-                      </Table.Cell>
-                      <Draggable
-                        key={scramble.id}
-                        draggableId={scramble.id.toString()}
-                        index={index}
-                      >
-                        {(providedDraggable) => (
+                      {(providedDraggable, snapshot) => {
+                        const definitionIndex = computeOnDragIndex(index, snapshot.isDragging);
+
+                        return (
                           <Ref innerRef={providedDraggable.innerRef}>
-                            <Table.Cell
+                            <Table.Row
+                              key={scramble.id}
                               {...providedDraggable.draggableProps}
-                              {...providedDraggable.dragHandleProps}
+                              negative={hasError || isExtra}
                             >
-                              <Icon name="bars" />
-                              {events.byId[scramble.event_id].name}
-                              {' '}
-                              {roundTypes.byId[scramble.round_type_id].name}
-                              {' - '}
-                              {String.fromCharCode(64 + scramble.scramble_set_number)}
-                            </Table.Cell>
+                              <Table.Cell textAlign="right" collapsing>
+                                {isExpected
+                                  ? `${activityCodeToName(activeRound.id)}, Group ${definitionIndex + 1}`
+                                  : 'Extra Scramble set (unassigned)'}
+                                {(hasError || isExtra) && (
+                                  <>
+                                    {' '}
+                                    <Icon name="exclamation triangle" />
+                                    {hasError ? 'Missing scramble' : 'Unexpected Scramble Set'}
+                                  </>
+                                )}
+                              </Table.Cell>
+                              <Table.Cell {...providedDraggable.dragHandleProps}>
+                                <Icon name="bars" />
+                                {events.byId[scramble.event_id].name}
+                                {' '}
+                                {roundTypes.byId[scramble.round_type_id].name}
+                                {' - '}
+                                {String.fromCharCode(64 + scramble.scramble_set_number)}
+                              </Table.Cell>
+                            </Table.Row>
                           </Ref>
-                        )}
-                      </Draggable>
-                    </Table.Row>
+                        );
+                      }}
+                    </Draggable>
                   );
                 })}
                 {providedDroppable.placeholder}
