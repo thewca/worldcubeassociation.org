3aff07f3eae574f205eb35aff5085bebf7d368af
pranav-027 <pd2772001@gmail.com>
2025-05-26 12:27:31 +0530

    review changes in tests

--- spec/lib/finish_unfinished_persons_spec.rb
+++ spec/lib/finish_unfinished_persons_spec.rb
@@ -7,23 +7,16 @@ RSpec.describe FinishUnfinishedPersons do
     let(:competition_year) { 2023 }
     let(:available_per_semi) { {} }
 
-    before do
-      allow(Person).to receive_messages(
-        where: Person,
-        order: Person,
-        pick: nil,
-      )
-    end
-
     context 'when a unique semi-ID can be generated' do
       let(:person_name) { 'John Doe' }
 
       it 'returns the correct semi-ID and updates available_per_semi' do
+        FactoryBot.create(:person, wca_id: nil)
         semi_id, updated_available = described_class.compute_semi_id(competition_year, person_name, available_per_semi)
 
         expect(semi_id).to start_with('2023')
         expect(updated_available).to have_key(semi_id)
-        expect(updated_available[semi_id]).to eq(98) # 99 - 1
+        expect(updated_available[semi_id]).to eq(98)
       end
     end
 
@@ -31,13 +24,14 @@ RSpec.describe FinishUnfinishedPersons do
       let(:person_name) { 'John Doe' }
 
       before do
-        allow(Person).to receive(:pick).and_return('2023DOEJ01')
+        FactoryBot.create(:person, wca_id: '2023DOEJ01')
       end
 
       it 'adjusts the semi-ID to avoid conflicts' do
         semi_id, updated_available = described_class.compute_semi_id(competition_year, person_name, available_per_semi)
 
         expect(semi_id).to start_with('2023')
+        expect(semi_id).to eq('2023DOEJ02')
         expect(updated_available[semi_id]).to be < 99
       end
     end
@@ -45,9 +39,11 @@ RSpec.describe FinishUnfinishedPersons do
     context 'when no semi-ID can be generated' do
       let(:person_name) { 'John Doe' }
 
-      it 'raises an error' do
-        allow(Person).to receive(:pick).and_return('2023DOEJ99')
+      before do
+        FactoryBot.create(:person, wca_id: '2023DOEJ99')
+      end
 
+      it 'raises an error' do
         expect do
           described_class.compute_semi_id(competition_year, person_name, available_per_semi)
         end.to raise_error(RuntimeError, /Could not compute a semi-id for John Doe/)
@@ -58,12 +54,13 @@ RSpec.describe FinishUnfinishedPersons do
       let(:person_name) { 'John Doe Jr.' }
 
       it 'ignores the generational suffix and generates the correct semi-ID' do
+        FactoryBot.create(:person, wca_id: nil)
         semi_id, updated_available = described_class.compute_semi_id(competition_year, person_name, available_per_semi)
 
         expect(semi_id).to start_with('2023')
-        expect(semi_id).to include('DOEJ') # Ensure the suffix is ignored
+        expect(semi_id).to include('DOEJ')
         expect(updated_available).to have_key(semi_id)
-        expect(updated_available[semi_id]).to eq(98) # 99 - 1
+        expect(updated_available[semi_id]).to eq(98)
       end
     end
   end
