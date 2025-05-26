c48762485b89bc3c3b1c7ac540a5a3cbf3f77667
FinnIckler <finnickler@gmail.com>
2025-05-26 12:06:27 +0200

    switch to enum

--- app/models/regional_record.rb
+++ app/models/regional_record.rb
@@ -1,31 +1,30 @@
 # frozen_string_literal: true
 
 class RegionalRecord < ApplicationRecord
+  enum :record_scope, {
+    NR: 0,
+    CR: 1,
+    WR: 2,
+  }
+
   RECORD_SCOPES = %w[WR CR NR].freeze
   RECORD_TYPES = %w[single average].freeze
 
-  SCOPES_FOR = {
-    NR: %w[WR CR NR].freeze,
-    CR: %w[WR CR].freeze,
-    WR: %w[WR].freeze,
-  }.freeze
-
   validates :record_type, presence: true, inclusion: { in: RECORD_TYPES }
-  validates :record_scope, presence: true, inclusion: { in: RECORD_SCOPES }
   validates :event_id, presence: true
   validates :record_timestamp, presence: true
   validates :value, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :country_id, presence: true, if: -> { record_scope == 'NR' }
   validates :continent_id, presence: true, if: -> { record_scope == 'CR' }
 
-  scope :world_records, -> { where(record_scope: 'WR') }
+  scope :world_records, -> { where(record_scope: :WR) }
   scope :national_records, lambda { |country_id = nil|
-    scope = where(record_scope: 'NR')
+    scope = where(record_scope >= :NR)
     country_id.present? ? scope.where(country_id: country_id) : scope
   }
 
   scope :continental_records, lambda { |continent_id = nil|
-    scope = where(record_scope: 'CR')
+    scope = where(record_scope >= :CR)
     continent_id.present? ? scope.where(continent_id: continent_id) : scope
   }
 
@@ -42,7 +41,7 @@ class RegionalRecord < ApplicationRecord
   belongs_to :result
 
   def self.record_for(event_id, record_type, scope, country_id: nil, continent_id: nil, date: nil)
-    query = where(event_id: event_id, record_type: record_type, record_scope: SCOPES_FOR[scope])
+    query = where(event_id: event_id, record_type: record_type, record_scope: scope...)
     query = query.where(country_id: country_id) if country_id
     query = query.where(continent_id: continent_id) if continent_id
     query = query.where(record_timestamp: ...date) if date
