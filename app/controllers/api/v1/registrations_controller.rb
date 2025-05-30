b1431dd710fd260ef4544d25dc6773073c903541
Duncan <duncanonthejob@gmail.com>
2025-05-30 14:43:10 +0200

    allow_self_edit only applies to accepted registrations

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -139,7 +139,10 @@ class Api::V1::RegistrationsController < Api::V1::ApiController
       can_administer_or_current_user?(@competition, @current_user, target_user)
 
     raise WcaExceptions::RegistrationError.new(:forbidden, Registrations::ErrorCodes::USER_EDITS_NOT_ALLOWED) unless
-      @competition.registration_edits_currently_permitted? || @current_user.can_manage_competition?(@competition) || user_uncancelling_registration?(@registration, new_status)
+      !(@registration.competing_status_accepted? && @competition.cannot_edit_accepted_registrations?) &&
+      @competition.registration_edits_currently_permitted? ||
+      @current_user.can_manage_competition?(@competition) ||
+      user_uncancelling_registration?(@registration, new_status)
 
     raise WcaExceptions::RegistrationError.new(:unauthorized, Registrations::ErrorCodes::REGISTRATION_IS_REJECTED) if
       user_is_rejected?(@current_user, target_user, @registration) && !organizer_modifying_own_registration?(@competition, @current_user, target_user)
