d9c41609d24db241774058aac4e13cd89c26d8b6
pranav-027 <pd2772001@gmail.com>
2025-06-02 09:53:35 +0530

    remove mocks

--- spec/lib/finish_unfinished_persons_spec.rb
+++ spec/lib/finish_unfinished_persons_spec.rb
@@ -11,9 +11,6 @@ RSpec.describe FinishUnfinishedPersons, type: :module do
       let(:person) { create(:person, name: 'John Smith') }
 
       it 'generates a semi_id with the correct format' do
-        allow(FinishUnfinishedPersons).to receive(:extract_roman_name).with('John Smith').and_return('John Smith')
-        allow(FinishUnfinishedPersons).to receive(:remove_accents).with('John Smith').and_return('John Smith')
-
         semi_id, = FinishUnfinishedPersons.compute_semi_id(competition_year, person.name, available_per_semi)
 
         expect(semi_id).to match(/\A2023[A-Z]{4}\z/)
@@ -23,12 +20,9 @@ RSpec.describe FinishUnfinishedPersons, type: :module do
     end
 
     context 'with a generational suffix' do
-      let(:person) { create(:person, name: 'John Smith Jr') }
+      let(:person) { create(:person, name: 'John Smith Jr.') }
 
       it 'correctly identifies the last name and generates a semi_id' do
-        allow(FinishUnfinishedPersons).to receive(:extract_roman_name).with('John Smith Jr').and_return('John Smith Jr')
-        allow(FinishUnfinishedPersons).to receive(:remove_accents).with('John Smith Jr').and_return('John Smith Jr')
-
         semi_id, = FinishUnfinishedPersons.compute_semi_id(competition_year, person.name, available_per_semi)
 
         expect(semi_id).to start_with('2023SMIT')
@@ -36,12 +30,9 @@ RSpec.describe FinishUnfinishedPersons, type: :module do
     end
 
     context 'with an extended generational suffix' do
-      let(:person) { create(:person, name: 'John Smith Jnr') }
+      let(:person) { create(:person, name: 'John Smith III') }
 
       it 'correctly identifies the last name for JNR' do
-        allow(FinishUnfinishedPersons).to receive(:extract_roman_name).with('John Smith Jnr').and_return('John Smith Jnr')
-        allow(FinishUnfinishedPersons).to receive(:remove_accents).with('John Smith Jnr').and_return('John Smith Jnr')
-
         semi_id, = FinishUnfinishedPersons.compute_semi_id(competition_year, person.name, available_per_semi)
 
         expect(semi_id).to start_with('2023SMIT')
@@ -52,9 +43,6 @@ RSpec.describe FinishUnfinishedPersons, type: :module do
       let(:person) { create(:person, name: 'José García') }
 
       it 'handles accented characters correctly' do
-        allow(FinishUnfinishedPersons).to receive(:extract_roman_name).with('José García').and_return('Jose Garcia')
-        allow(FinishUnfinishedPersons).to receive(:remove_accents).with('Jose Garcia').and_return('Jose Garcia')
-
         semi_id, = FinishUnfinishedPersons.compute_semi_id(competition_year, person.name, available_per_semi)
 
         expect(semi_id).to start_with('2023GARC')
@@ -65,9 +53,6 @@ RSpec.describe FinishUnfinishedPersons, type: :module do
       let(:person) { create(:person, name: 'Madonna') }
 
       it 'uses the single name as the last name' do
-        allow(FinishUnfinishedPersons).to receive(:extract_roman_name).with('Madonna').and_return('Madonna')
-        allow(FinishUnfinishedPersons).to receive(:remove_accents).with('Madonna').and_return('Madonna')
-
         semi_id, = FinishUnfinishedPersons.compute_semi_id(competition_year, person.name, available_per_semi)
 
         expect(semi_id).to start_with('2023MADO')
@@ -78,9 +63,6 @@ RSpec.describe FinishUnfinishedPersons, type: :module do
       let(:person) { create(:person, name: 'John O\'Connor') }
 
       it 'sanitizes special characters' do
-        allow(FinishUnfinishedPersons).to receive(:extract_roman_name).with('John O\'Connor').and_return('John OConnor')
-        allow(FinishUnfinishedPersons).to receive(:remove_accents).with('John OConnor').and_return('John OConnor')
-
         semi_id, = FinishUnfinishedPersons.compute_semi_id(competition_year, person.name, available_per_semi)
 
         expect(semi_id).to start_with('2023OCON')
