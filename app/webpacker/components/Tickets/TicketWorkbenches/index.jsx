d76b70337d264c6eaa5aaaae19b3ccd7455ba583
Daniel James <djdany444@gmail.com>
2025-06-04 20:38:40 +0530

    Review changes

--- app/webpacker/components/Tickets/TicketWorkbenches/index.jsx
+++
@@ -1,8 +0,0 @@
-import { ticketTypes, ticketStakeholderRoles } from '../../../lib/wca-data.js.erb';
-import EditPersonTicketWorkbench from './EditPersonTicketWorkbench';
-
-export default {
-  [ticketTypes.edit_person]: {
-    [ticketStakeholderRoles.actioner]: EditPersonTicketWorkbench,
-  },
-};
--- db/migrate/20250603141309_add_stakeholder_role_to_ticket_stakeholders.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddStakeholderRoleToTicketStakeholders < ActiveRecord::Migration[7.2]
-  def change
-    add_column :ticket_stakeholders, :stakeholder_role, :string
-  end
-end
