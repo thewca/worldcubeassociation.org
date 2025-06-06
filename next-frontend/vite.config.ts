86acc6dc970843eaf5ebf54b7d63af82e061f017
Duncan <duncanonthejob@gmail.com>
2025-06-06 07:33:37 +0200

    minor config changes

--- next-frontend/vite.config.ts
+++ next-frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts', // optional if you have global setups
+    setupFiles: './vitest.setup.ts',
   },
   resolve: {
     alias: {
