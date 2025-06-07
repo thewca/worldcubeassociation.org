5b746ef095c748e982e400997e33965a573d8668
Gregor Billing <gbilling@worldcubeassociation.org>
2025-06-07 20:43:44 +0900

    Run Prettier over new config file

--- next-frontend/next.config.ts
+++ next-frontend/next.config.ts
@@ -2,7 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 import nextRoutes from "nextjs-routes/config";
 
-const withRoutes = nextRoutes({ outDir: 'src' });
+const withRoutes = nextRoutes({ outDir: "src" });
 
 const nextConfig: NextConfig = {
   experimental: {
