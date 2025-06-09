f5ecb65eb0d1d17544ec6e34e54a5088a0191646
Daniel James <djdany444@gmail.com>
2025-06-09 17:20:25 +0530

    Review changes

--- app/controllers/wfc_controller.rb
+++ app/controllers/wfc_controller.rb
@@ -8,17 +8,17 @@ class WfcController < ApplicationController
   end
 
   def competition_export
-    select_attributes = %i[
-      id name start_date end_date
-      country_id announced_at results_posted_at
-      currency_code base_entry_fee_lowest_denomination
+    select_attributes = [
+      :id, :name, :start_date, :end_date,
+      :country_id, :announced_at, :results_posted_at,
+      :currency_code, :base_entry_fee_lowest_denomination,
+      "count(distinct persons.id) as num_competitors"
     ]
     from = params.require(:from_date)
     to = params.require(:to_date)
     response.headers["Content-Disposition"] = "attachment; filename=\"wfc-competitions-export-#{from}-#{to}.tsv\""
     @competitions = Competition
                     .select(select_attributes)
-                    .select('COUNT(DISTINCT persons.id) AS num_competitors')
                     .includes(:delegates, :championships, :organizers, :events, organizers: [:wfc_dues_redirect])
                     .left_joins(:competitors)
                     .group(:id)
