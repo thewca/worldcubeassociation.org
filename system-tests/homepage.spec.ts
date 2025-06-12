d1255fbe04dd52810101f888720b03fa163cda2b
Duncan <duncanonthejob@gmail.com>
2025-06-12 17:11:52 +0200

    start of CI playwright action

--- system-tests/homepage.spec.ts
+++ system-tests/homepage.spec.ts
@@ -3,6 +3,5 @@ import { test, expect } from '@playwright/test';
 test('displays placeholder homepage text', async ({ page }) => {
   await page.goto('http://localhost:3001/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page.getByText('No homepage content yet, go ahead and add some!')).toBeVisible();
+  await expect(page.getByText('im so goofy teehee')).toBeVisible();
 });
