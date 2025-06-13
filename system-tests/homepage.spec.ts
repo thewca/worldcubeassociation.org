33aff04536021cc3c89a2ae5d92fb17b09dd0074
Duncan <duncanonthejob@gmail.com>
2025-06-13 09:04:18 +0200

    corrected the test text lol

--- system-tests/homepage.spec.ts
+++ system-tests/homepage.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('displays placeholder homepage text', async ({ page }) => {
   await page.goto('http://localhost:3001/');
 
-  await expect(page.getByText('im so goofy teehee')).toBeVisible();
+  await expect(page.getByText('No homepage content yet, go ahead and add some!')).toBeVisible();
 });
