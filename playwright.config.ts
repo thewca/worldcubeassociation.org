501ea9493fcbefa513ad5add3a134afacc0d3be1
Duncan <duncanonthejob@gmail.com>
2025-06-13 11:35:13 +0200

    run server via playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn dev',
+    url: 'http://localhost:3001',
+    reuseExistingServer: !process.env.CI,
+  },
 });
