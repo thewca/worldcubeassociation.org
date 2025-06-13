8b026ee17062b8d39a1e1cec31e48cc18853b54e
FinnIckler <finnickler@gmail.com>
2025-06-13 14:57:02 +0200

    Only load new relic instrumentation in prod (#11784)

--- next-frontend/src/instrumentation.ts
+++ next-frontend/src/instrumentation.ts
@@ -1,5 +1,8 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
+  if (
+    process.env.NEXT_RUNTIME === "nodejs" &&
+    process.env.NODE_ENV === "production"
+  ) {
     await import("newrelic");
   }
 }
