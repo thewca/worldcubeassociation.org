2727063a027f2bb80fcd9496afef2e70b83821b2
Duncan <duncanonthejob@gmail.com>
2025-06-19 08:29:41 +0200

    experimenting with twitch embed

--- lib/twitch_interface.rb
+++ lib/twitch_interface.rb
@@ -1,12 +1,21 @@
 module TwitchInterface
+  # def self.get_latest_vod
+  #   # url = 'videos?user_id=233980668&type=archive&first=1' # wca user_id
+  #   url = 'videos?user_id=65806828&type=archive&first=1' # dev_spajus user_id
+  #   # url = 'users?login=dev_spajus'
+  #   response = twitch_connection.get(url)
+  #   puts '======================================'
+  #   puts response.body
+  #   response.body['data'].first&.dig('id')
+  # end
 
-  def self.wca_is_live?
-    url = 'streams?user_login=worldcubeassociation'
-    response = twitch_connection.get(url)
-    response.body
-  end
+  # def self.wca_is_live?
+  #   url = 'streams?user_login=worldcubeassociation'
+  #   response = twitch_connection.get(url)
+  #   response.body['data'].first&.dig('user_login') == 'worldcubeassociation' # Stream data will not be returned if the channel is not live
+  # end
 
-  def self.twitch_connection
+  private_class_method def self.twitch_connection
     Faraday.new(
       url: 'https://api.twitch.tv/helix/',
       headers: {
@@ -18,7 +27,7 @@ module TwitchInterface
     )
   end
 
-  def self.generate_access_token
+  private_class_method def self.generate_access_token
     options = {
       site: 'https://id.twitch.tv',
       token_url: '/oauth2/token',
