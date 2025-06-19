e83e2ddd974778a16b2638e07b45c70fbf18e3b0
FinnIckler <finnickler@gmail.com>
2025-06-19 15:04:33 +0200

    make sure we inited i18n for client only use

--- next-frontend/src/lib/i18n/useI18n.ts
+++ next-frontend/src/lib/i18n/useI18n.ts
@@ -3,6 +3,7 @@
 import { useTranslation } from "react-i18next";
 import { storageKey } from "@/lib/i18n/settings";
 import Cookies from "js-cookie";
+import "./i18n";
 
 export function useT(options?: object) {
   const lng = Cookies.get(storageKey);
