a8a4f52546fb4c5934e959d7c6911d8bc29dc179
FinnIckler <finnickler@gmail.com>
2025-06-19 14:28:18 +0200

    get rid of one useEffect

--- next-frontend/src/lib/i18n/useI18n.ts
+++ next-frontend/src/lib/i18n/useI18n.ts
@@ -1,18 +1,13 @@
 "use client";
 
 import i18next from "./i18n";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { storageKey } from "@/lib/i18n/settings";
 import Cookies from "js-cookie";
 
 export function useT(options?: object) {
   const lng = Cookies.get(storageKey);
-  const [activeLng, setActiveLng] = useState(i18next.resolvedLanguage);
-  useEffect(() => {
-    if (activeLng === i18next.resolvedLanguage) return;
-    setActiveLng(i18next.resolvedLanguage);
-  }, [activeLng]);
   useEffect(() => {
     if (!lng || i18next.resolvedLanguage === lng) return;
     i18next.changeLanguage(lng);
