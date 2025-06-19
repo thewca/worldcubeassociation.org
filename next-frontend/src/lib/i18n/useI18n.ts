3e3d1afe86a34e4218ea44169877e6e517f2f43a
Gregor Billing <gbilling@worldcubeassociation.org>
2025-06-20 07:32:09 +0900

    Directly pass down i18n instance to client hook

--- next-frontend/src/lib/i18n/useI18n.ts
+++ next-frontend/src/lib/i18n/useI18n.ts
@@ -3,12 +3,12 @@
 import { useTranslation } from "react-i18next";
 import { storageKey, defaultNamespace } from "@/lib/i18n/settings";
 import Cookies from "js-cookie";
-import "./i18n";
+import i18n from "./i18n";
 
 import type { UseTranslationOptions } from "react-i18next";
 
 export function useT(options?: UseTranslationOptions<typeof defaultNamespace>) {
   const lng = Cookies.get(storageKey);
 
-  return useTranslation(defaultNamespace, { ...options, lng });
+  return useTranslation(defaultNamespace, { ...options, lng, i18n });
 }
