0fac57bb2f7c381af0b5352e3388550b3b0ac177
FinnIckler <finnickler@gmail.com>
2025-06-19 14:14:16 +0200

    parse acceptLanguage

--- next-frontend/src/lib/i18n/get18n.ts
+++ next-frontend/src/lib/i18n/get18n.ts
@@ -1,17 +1,21 @@
 "use server";
 
 import i18next from "./i18n";
-import { storageKey, fallbackLng } from "./settings";
+import { storageKey, fallbackLng, languages } from "./settings";
 import { cookies, headers } from "next/headers";
+import parser from "accept-language-parser";
 
 export async function getT(options?: { keyPrefix?: string }) {
   const cookieList = await cookies();
   const headerList = await headers();
 
+  const acceptLanguage = parser.pick(
+    languages,
+    headerList.get("Accept-Language") ?? [],
+  );
+
   const lng =
-    cookieList.get(storageKey)?.value ||
-    headerList.get("Accept-Language") ||
-    fallbackLng;
+    cookieList.get(storageKey)?.value || acceptLanguage || fallbackLng;
 
   return {
     t: i18next.getFixedT(lng, "translation", options?.keyPrefix),
