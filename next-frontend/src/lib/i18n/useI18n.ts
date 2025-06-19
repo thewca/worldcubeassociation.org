dc05e3773ac6c3eed7a6709e1ed0a7a1d7eb220f
FinnIckler <finnickler@gmail.com>
2025-06-19 14:36:14 +0200

    get rid of the last useEffect

--- next-frontend/src/lib/i18n/useI18n.ts
+++ next-frontend/src/lib/i18n/useI18n.ts
@@ -1,7 +1,5 @@
 "use client";
 
-import i18next from "./i18n";
-import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { storageKey } from "@/lib/i18n/settings";
 import Cookies from "js-cookie";
@@ -9,10 +7,5 @@ import Cookies from "js-cookie";
 export function useT(options?: object) {
   const lng = Cookies.get(storageKey);
 
-  useEffect(() => {
-    if (!lng || i18next.resolvedLanguage === lng) return;
-    i18next.changeLanguage(lng);
-  }, [lng]);
-
-  return useTranslation("translation", options);
+  return useTranslation("translation", { ...options, lng });
 }
