ade085758bf47b6a818c9eb72d008cd4568c759a
FinnIckler <finnickler@gmail.com>
2025-06-20 17:54:04 +0200

    add new api

--- next-frontend/src/types/openapi.ts
+++ next-frontend/src/types/openapi.ts
@@ -211,10 +211,34 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/regional-organizations": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** A list of currently acknowledged regional organizations */
+        get: operations["getOrganizations"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
+        RegionalOrganization: {
+            name: string;
+            country: string;
+            website: string;
+            /** Format: uri */
+            logo_url?: string;
+        };
         Person: {
             /** @example 267 */
             id: number;
@@ -588,6 +612,26 @@ export interface operations {
                     [name: string]: unknown;
                 };
                 content?: never;
+            };
+        };
+    };
+    getOrganizations: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Retrieved organizations */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["RegionalOrganization"][];
+                };
             };
         };
     };
