1c33c46834f98a5285b9dc582e0ab4c934795b41
Duncan <duncanonthejob@gmail.com>
2025-06-20 14:50:27 +0200

    working setup to run playwright in docker

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -71,10 +71,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'cd next-frontend && corepack install && yarn install && yarn dev --port 3001',
-    url: 'http://localhost:3001',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
+  // webServer: {
+  //   command: 'cd next-frontend && corepack install && yarn install && yarn dev --port 3001',
+  //   url: 'http://localhost:3001',
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 120 * 1000,
+  // },
 });
--- system-tests/homepage.spec.ts
+++ system-tests/homepage.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('displays placeholder homepage text', async ({ page }) => {
-  await page.goto('http://localhost:3001/');
+  await page.goto('/');
 
   await expect(page.getByText('No homepage content yet, go ahead and add some!')).toBeVisible();
 });
