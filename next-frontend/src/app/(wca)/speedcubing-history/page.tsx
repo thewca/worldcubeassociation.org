a4e474cf75337199dc4fd9258e50bbd73ec01ce4
FinnIckler <finnickler@gmail.com>
2025-06-20 12:24:16 +0200

    add page

---
+++ next-frontend/src/app/(wca)/speedcubing-history/page.tsx
@@ -0,0 +1,70 @@
+"use server";
+
+import {
+  Container,
+  Heading,
+  VStack,
+  Text,
+  Box,
+  Image,
+  Center,
+} from "@chakra-ui/react";
+import Quote from "@/components/Quote";
+import { getPayload } from "payload";
+import config from "@payload-config";
+import { Media } from "@/types/payload";
+import { MarkdownText } from "@/components/Markdown";
+import { getT } from "@/lib/i18n/get18n";
+
+export default async function SpeedcubingHistory() {
+  const payload = await getPayload({ config });
+
+  const historyPage = await payload.findGlobal({
+    slug: "speedcubing-history-page",
+  });
+
+  const historyItems = historyPage.blocks;
+
+  if (historyItems.length === 0) {
+    return <Heading>No History Items, add some!</Heading>;
+  }
+
+  const { t } = await getT();
+
+  return (
+    <Container>
+      <VStack gap="8" width="full" pt="8" alignItems="left">
+        <Heading size="5xl">{t("speedcubing_history.title")}</Heading>
+        {historyItems.map((item) => {
+          switch (item.blockType) {
+            case "quote": {
+              return (
+                <Quote
+                  key={item.id}
+                  content={item.contentMarkdown!}
+                  author={item.quotedPerson}
+                />
+              );
+            }
+            case "paragraph": {
+              return (
+                <MarkdownText key={item.id} content={item.contentMarkdown!} />
+              );
+            }
+            case "captionedImage": {
+              const image = item.image as Media;
+              return (
+                <Center key={item.id}>
+                  <Box>
+                    <Image src={image.url!} alt={item.caption} />
+                    <Text>{item.caption}</Text>
+                  </Box>
+                </Center>
+              );
+            }
+          }
+        })}
+      </VStack>
+    </Container>
+  );
+}
