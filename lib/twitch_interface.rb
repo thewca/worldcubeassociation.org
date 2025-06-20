15a431ee34e687258827e93d3d0585d9672a6407
Duncan <duncanonthejob@gmail.com>
2025-06-20 06:23:56 +0200

    added live check and vod rendering

--- lib/twitch_interface.rb
+++ lib/twitch_interface.rb
@@ -1,19 +1,29 @@
 module TwitchInterface
-  # def self.get_latest_vod
-  #   # url = 'videos?user_id=233980668&type=archive&first=1' # wca user_id
-  #   url = 'videos?user_id=65806828&type=archive&first=1' # dev_spajus user_id
-  #   # url = 'users?login=dev_spajus'
-  #   response = twitch_connection.get(url)
-  #   puts '======================================'
-  #   puts response.body
-  #   response.body['data'].first&.dig('id')
-  # end
+  TWITCH_CHANNEL_LOGIN_NAME = 'lofigirl'
+  # TWITCH_CHANNEL_ID = '233980668' # wca
+  TWITCH_CHANNEL_ID = '65806828' # dev_spajus
 
-  # def self.wca_is_live?
-  #   url = 'streams?user_login=worldcubeassociation'
-  #   response = twitch_connection.get(url)
-  #   response.body['data'].first&.dig('user_login') == 'worldcubeassociation' # Stream data will not be returned if the channel is not live
-  # end
+  def self.get_latest_vod
+    url = "videos?user_id=#{TWITCH_CHANNEL_ID}&type=archive&first=1"
+    response = twitch_connection.get(url)
+    puts '======================================'
+    puts response.body
+    response.body['data'].first&.dig('id')
+  end
+
+  def self.get_user_id(login_name)
+    url = "users?login=#{login_name}"
+    response = twitch_connection.get(url)
+    puts '======================================'
+    puts response.body
+    response.body['data'].first&.dig('id')
+  end
+
+  def self.wca_is_live?
+    url = "streams?user_login=#{TWITCH_CHANNEL_LOGIN_NAME}"
+    response = twitch_connection.get(url)
+    response.body['data'].first&.dig('user_login') == TWITCH_CHANNEL_LOGIN_NAME # Stream data will not be returned if the channel is not live
+  end
 
   private_class_method def self.twitch_connection
     Faraday.new(
