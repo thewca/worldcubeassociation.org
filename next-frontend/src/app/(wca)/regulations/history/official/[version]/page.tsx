ed49f26b61f5329233d4fce1094e8f0921106a14
FinnIckler <finnickler@gmail.com>
2025-06-24 12:22:24 +0200

    add AspectRatio

--- next-frontend/src/app/(wca)/regulations/history/official/[version]/page.tsx
+++ next-frontend/src/app/(wca)/regulations/history/official/[version]/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { AspectRatio, Container } from "@chakra-ui/react";
 
 export default function HistoricalRegulation() {
   const params = useParams<{ version: string }>();
 
   return (
-    <iframe
-      src={`http://regulations.worldcubeassociation.org/history/official/${params.version}/index.html.erb`}
-    ></iframe>
+    <Container>
+      <AspectRatio>
+        <iframe
+          width={"100%"}
+          src={`http://regulations.worldcubeassociation.org/history/official/${params.version}/index.html`}
+        ></iframe>
+      </AspectRatio>
+    </Container>
   );
 }
