abaabbab999496c6e51586a5f6cb968876ead0a8
FinnIckler <finnickler@gmail.com>
2025-06-27 12:03:14 +0200

    rubocop

--- app/controllers/api/v0/incidents_controller.rb
+++ app/controllers/api/v0/incidents_controller.rb
@@ -4,16 +4,16 @@ class Api::V0::IncidentsController < Api::V0::ApiController
   def index
     base_model = Incident.includes(:competitions, :incident_tags)
     incidents = if current_user&.can_manage_incidents?
-                   base_model.all
-                 else
-                   base_model.resolved
-                 end
+                  base_model.all
+                else
+                  base_model.resolved
+                end
 
     incidents = incidents.search(params[:q], params: params)
     render json: paginate(
       incidents.as_json(
         can_view_delegate_matters: current_user&.can_view_delegate_matters?,
-        ),
-      )
+      ),
+    )
   end
 end
