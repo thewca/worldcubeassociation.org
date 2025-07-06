d8f122e2cf0d5f9756ca5063c40df54bae981d17
Duncan <duncanonthejob@gmail.com>
2025-07-06 10:44:39 +0200

    removed competitiosn spec test

--- spec/requests/competitions_spec.rb
+++ spec/requests/competitions_spec.rb
@@ -227,16 +227,6 @@ RSpec.describe "competitions" do
         competition.update!(start_date: 5.weeks.from_now, end_date: 5.weeks.from_now)
       end
 
-      it 'cannot set auto_accept_preference to live' do
-        competitor_limit_params = competition.to_form_data['competitorLimit']
-        competitor_limit_params['autoAcceptPreference'] = 'live'
-        update_params = build_competition_update(competition, competitorLimit: competitor_limit_params)
-        patch competition_path(competition), params: update_params, as: :json
-
-        expect(response).not_to be_successful
-        expect(competition.reload.auto_accept_preference_live?).to be(false)
-      end
-
       context 'when handling unconfirmed competitions' do
         it 'can set championship types' do
           expect(competition.confirmed?).to be false
