f7942789d59c14e77537953ac93480ea653be286
pranav-027 <pd2772001@gmail.com>
2025-07-09 11:15:43 +0530

    review changes in test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -51,8 +51,8 @@ RSpec.describe Post do
 
   context "unstick_at behavior" do
     it "defaults unstick_at to 2 weeks from now when sticky is true and unstick_at is nil" do
-      post = build(:sticky_post, unstick_at: nil)
-      post.valid?
+      post = create(:sticky_post, unstick_at: nil)
+      expect(post).to be_valid
       expect(post.unstick_at).to eq(2.weeks.from_now.to_date)
     end
   end
