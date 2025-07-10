f4992168c4293fcb156aa89d95f7893ee2092399
Daniel James <djdany444@gmail.com>
2025-07-10 15:20:20 +0530

    Fixes on results submission form (#11976)

--- app/webpacker/components/CompetitionResultSubmission/api/submitToWrt.js
+++ app/webpacker/components/CompetitionResultSubmission/api/submitToWrt.js
@@ -1,8 +1,8 @@
-import { fetchWithAuthenticityToken } from '../../../lib/requests/fetchWithAuthenticityToken';
+import { fetchJsonOrError } from '../../../lib/requests/fetchWithAuthenticityToken';
 import { actionUrls } from '../../../lib/requests/routes.js.erb';
 
 export default async function submitToWrt({ competitionId, message }) {
-  const { data } = await fetchWithAuthenticityToken(
+  const { data } = await fetchJsonOrError(
     actionUrls.competitionResultSubmission.submitToWrt(competitionId),
     {
       method: 'POST',
