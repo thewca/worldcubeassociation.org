7d9aea86478a34b7a7227304bc5793283d1fcf0d
Gregor Billing <gbilling@worldcubeassociation.org>
2025-07-11 15:22:58 +0900

    Improve overlay behavior of UserBadge

--- next-frontend/src/components/UserBadge.tsx
+++ next-frontend/src/components/UserBadge.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Box, Card, HStack, Text } from "@chakra-ui/react";
-import NextImage from "next/image";
+import {
+  Box,
+  Card,
+  Center,
+  HStack,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
+import Image from "next/image";
 import RoleBadge, { StaffColor } from "@/components/RoleBadge";
 import Link from "next/link";
 
 interface UserBadgeData {
   name: string;
-  profilePicture: string;
+  profilePicture?: string;
   roles?: { teamRole: string; teamText?: string; staffColor: StaffColor }[];
   wcaId: string;
 }
@@ -18,61 +25,51 @@ const UserBadge: React.FC<UserBadgeData> = ({
   wcaId,
 }) => {
   return (
-    <Card.Root
-      bg="grey.solid"
-      color="wcawhite.contrast"
-      maxWidth="400px"
-      height="120px"
-      rounded="xl"
-      size="sm"
-      shadow="wca"
-      position="sticky"
-      top="20px"
-    >
-      <Card.Header>
-        <Link href={`/persons/${wcaId}`}>
-          <HStack direction="row" align="center" gap="16px">
-            <Box
-              width="80px"
-              height="80px"
-              position="relative"
-              overflow="hidden"
-              borderRadius="md"
-            >
-              <NextImage
-                src={profilePicture}
-                alt="Profile Photo"
-                fill
-                style={{ objectFit: "cover" }}
-              />
-            </Box>
-            <Box>
-              <Text textStyle="3xl" fontWeight="bold">
-                {name}
-              </Text>
-              {roles && (
-                <HStack
-                  direction="row"
-                  wrap="wrap"
-                  gap="4px 8px"
-                  marginTop="4px"
-                >
-                  {roles.map((role, index) => (
-                    <RoleBadge
-                      key={index}
-                      teamRole={role.teamRole}
-                      teamText={role.teamText ?? ""}
-                      staffColor={role.staffColor}
-                      fontSize={"0.7em"}
-                    />
-                  ))}
-                </HStack>
-              )}
-            </Box>
-          </HStack>
-        </Link>
-      </Card.Header>
-    </Card.Root>
+    <LinkBox asChild>
+      <Card.Root
+        bg="grey.solid"
+        color="wcawhite.contrast"
+        rounded="xl"
+        size="sm"
+        shadow="wca"
+        flexDirection="row"
+        overflow="hidden"
+        maxW="xl"
+      >
+        {profilePicture && (
+          <Box objectFit="cover" width="75px" height="75px" position="relative">
+            <Image
+              src={profilePicture}
+              alt="Profile Picture"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </Box>
+        )}
+        <Center>
+          <Card.Body>
+            <Card.Title>
+              <LinkOverlay asChild>
+                <Link href={`/persons/${wcaId}`}>{name}</Link>
+              </LinkOverlay>
+            </Card.Title>
+            {roles && (
+              <HStack direction="row" wrap="wrap" gap="4px 8px" marginTop="4px">
+                {roles.map((role, index) => (
+                  <RoleBadge
+                    key={index}
+                    teamRole={role.teamRole}
+                    teamText={role.teamText ?? ""}
+                    staffColor={role.staffColor}
+                    fontSize={"0.7em"}
+                  />
+                ))}
+              </HStack>
+            )}
+          </Card.Body>
+        </Center>
+      </Card.Root>
+    </LinkBox>
   );
 };
 
