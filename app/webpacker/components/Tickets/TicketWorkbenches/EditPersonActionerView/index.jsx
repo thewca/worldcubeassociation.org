693a7c72296ef4a58c087a0354ae432caa967310
Daniel James <djdany444@gmail.com>
2025-07-11 11:17:23 +0530

    Few cleanups in tickets (#11978)
    * Few cleanups in tickets
    
    * Review changes
    
    * Review changes

--- app/webpacker/components/Tickets/TicketWorkbenches/EditPersonActionerView/index.jsx
+++ app/webpacker/components/Tickets/TicketWorkbenches/EditPersonActionerView/index.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import EditPersonForm from '../../../Panel/pages/EditPersonPage/EditPersonForm';
-import useSaveAction from '../../../../lib/hooks/useSaveAction';
-import { actionUrls } from '../../../../lib/requests/routes.js.erb';
 import { ticketStatuses } from '../../../../lib/wca-data.js.erb';
-import Loading from '../../../Requests/Loading';
 import EditPersonValidations from './EditPersonValidations';
 import EditPersonRequestedChangesList from './EditPersonRequestedChangesList';
 
-export default function EditPersonActionerView({ ticketDetails, sync, currentStakeholder }) {
+export default function EditPersonActionerView({ ticketDetails, updateStatus }) {
   const { ticket } = ticketDetails;
-  const { save, saving } = useSaveAction();
 
-  const closeTicket = () => {
-    save(
-      actionUrls.tickets.updateStatus(ticket.id),
-      {
-        ticket_status: ticketStatuses.edit_person.closed,
-        acting_stakeholder_id: currentStakeholder.id,
-      },
-      sync,
-      { method: 'POST' },
-    );
-  };
+  const closeTicket = () => updateStatus(ticketStatuses.edit_person.closed);
 
   if (ticketDetails.ticket.metadata.status === ticketStatuses.edit_person.closed) {
     return null;
   }
-  if (saving) return <Loading />;
 
   return (
     <>
