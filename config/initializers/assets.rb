2c8bec070cc696a346c5d723bdf53843edfff55a
FinnIckler <finnickler@gmail.com>
2025-07-14 14:07:07 -0400

    fix for including graphiql playground

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# as per https://github.com/rmosolgo/graphiql-rails/issues/75
+Rails.application.config.assets.precompile += %w[graphiql/rails/application.js graphiql/rails/application.css] if Rails.env.development?
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w[oms.js]
