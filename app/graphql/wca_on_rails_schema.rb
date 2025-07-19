63cde6942440121b8cbd058de6c404c456c718c0
FinnIckler <finnickler@gmail.com>
2025-07-19 11:56:11 -0400

    cleanup schema

--- app/graphql/wca_on_rails_schema.rb
+++ app/graphql/wca_on_rails_schema.rb
@@ -8,22 +8,11 @@ class WcaOnRailsSchema < GraphQL::Schema
 
   use GraphQL::Schema::Visibility
 
-  # GraphQL-Ruby calls this when something goes wrong while running a query:
-
-  # Union and Interface Resolution
-  def self.resolve_type(_abstract_type, _obj, _ctx)
-    # TODO: Implement this method
-    # to return the correct GraphQL object type for `obj`
-    raise(GraphQL::RequiredImplementationMissingError)
-  end
-
   # Limit the size of incoming queries:
   max_query_string_tokens(5000)
 
   # Stop validating when it encounters this many errors:
   validate_max_errors(100)
-
-  # Relay-style Object Identification:
 
   # Return a string UUID for `object`
   def self.id_from_object(object, _type_definition, _query_ctx)
