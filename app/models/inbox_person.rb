024b877ae030c2286c42110a87e0b71048fa0f5f
Gregor Billing <gbilling@worldcubeassociation.org>
2025-07-22 17:40:52 +0900

    Remove obscure numeric_id hack

--- app/models/inbox_person.rb
+++ app/models/inbox_person.rb
@@ -8,22 +8,10 @@ class InboxPerson < ApplicationRecord
   #   These IDs are only unique per competition however, so we use a composite foreign key.
   self.primary_key = %i[id competition_id]
 
-  def numeric_id
-    # This is the "raw" ID directly from the database column.
-    #   When calling `my_inbox_person.id` on the Rails model, you will receive an array
-    #   as the return value, because of the `self.primary_key` override at the top of the file.
-    # In some contexts, we do want to access the raw ID however
-    #   (most notably, in validators when comparing for equality)
-    # Note also, that doing `self[:id]` is not the same as `self.id`!
-    #   - The latter gives the Rails "smart-cast" composite ID (as an array in this case)
-    #   - The former gives the _attribute_ called `id` directly from the raw database record
-    self[:id]
-  end
-
   belongs_to :person, -> { current }, foreign_key: "wca_id", primary_key: "wca_id", optional: true
   belongs_to :country, foreign_key: "country_iso2", primary_key: "iso2"
 
-  alias_method :ref_id, :numeric_id
+  alias_attribute :ref_id, :id
   alias_method :wca_person, :person
 
   # Compatibility layer for results posting code that doesn't care whether it's a real person or an inbox person
