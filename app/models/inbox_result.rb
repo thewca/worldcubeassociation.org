997f2d01f395a9a1c265afdbf43051d4d0e71c10
Gregor Billing <gbilling@worldcubeassociation.org>
2025-07-22 14:17:39 +0900

    Use properly defined composite primary key

--- app/models/inbox_result.rb
+++ app/models/inbox_result.rb
@@ -4,7 +4,7 @@ class InboxResult < ApplicationRecord
   include Resultable
 
   # see result.rb for explanation of the scope
-  belongs_to :inbox_person, primary_key: %i[id competition_id], foreign_key: %i[person_id competition_id], optional: true
+  belongs_to :inbox_person, foreign_key: %i[person_id competition_id], optional: true
 
   alias_method :person, :inbox_person
 
