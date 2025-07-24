46e5e1c5a99e791ec80cbd976f98e97e5658ec80
Gregor Billing <gbilling@worldcubeassociation.org>
2025-07-24 11:06:17 +0900

    Almost server components

--- next-frontend/src/app/(wca)/teams-committees/page.tsx
+++ next-frontend/src/app/(wca)/teams-committees/page.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { useEffect, useMemo, useState } from "react";
 import {
   Container,
   Heading,
@@ -9,69 +6,34 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-import { useT } from "@/lib/i18n/useI18n";
+import { getT } from "@/lib/i18n/get18n";
 import { useQuery } from "@tanstack/react-query";
-import useAPI from "@/lib/wca/useAPI";
 import Loading from "@/components/ui/loading";
 import { Prose } from "@/components/ui/prose";
 import { components } from "@/types/openapi";
 import Link from "next/link";
 import { usePermissions } from "@/providers/PermissionProvider";
 import UserBadge from "@/components/UserBadge";
+import {
+  getTeamCommitteeMembers,
+  getTeamsCommittees,
+} from "@/lib/wca/roles/teamsCommittees";
+import Errored from "@/components/ui/errored";
 
-export default function TeamsCommitteesPage() {
-  const I18n = useT();
+export default async function TeamsCommitteesPage() {
+  const { t } = await getT();
 
-  const api = useAPI();
+  const { data: teamsCommittees, error } = await getTeamsCommittees();
 
-  const { data: teamsCommittees, isLoading: teamsCommitteesLoading } = useQuery(
-    {
-      queryKey: ["teams_committees"],
-      queryFn: () =>
-        api.GET("/user_groups", {
-          params: {
-            query: {
-              isActive: true,
-              groupType: "teams_committees",
-              isHidden: false,
-            },
-          },
-        }),
-    },
-  );
-
-  const isLoading = teamsCommitteesLoading;
-
-  const groups = useMemo(() => teamsCommittees?.data ?? [], [teamsCommittees]);
-
-  const [hash, setHash] = useState<string | null>("");
-
-  const activeGroup = useMemo(
-    () => groups.find((group) => group.metadata!.friendly_id === hash),
-    [groups, hash],
-  );
-
-  const hashIsValid = Boolean(activeGroup);
-
-  useEffect(() => {
-    if (!isLoading && !hashIsValid) {
-      if (groups.length > 0) {
-        setHash(groups[0].metadata!.friendly_id!);
-      } else {
-        setHash(null);
-      }
-    }
-  }, [isLoading, hashIsValid, groups, setHash]);
-
-  if (isLoading || !activeGroup) return <Loading />;
+  if (error) return <Errored error={error} />;
 
   return (
     <Container>
       <VStack align={"left"} gap="8" width="full" pt="8" alignItems="left">
         <Heading size="5xl">
-          {I18n.t("page.teams_committees_councils.title")}
+          {t("page.teams_committees_councils.title")}
         </Heading>
-        <Prose>{I18n.t("page.teams_committees_councils.description")}</Prose>
+        <Prose>{t("page.teams_committees_councils.description")}</Prose>
         <Tabs.Root
           variant="enclosed"
           orientation="vertical"
@@ -80,13 +42,13 @@ export default function TeamsCommitteesPage() {
           unmountOnExit
         >
           <Tabs.List height="fit-content" position="sticky" top="3">
-            {groups.map((group) => (
+            {teamsCommittees.map((group) => (
               <Tabs.Trigger value={group.name} key={group.id}>
                 {group.name}
               </Tabs.Trigger>
             ))}
           </Tabs.List>
-          {groups.map((group) => (
+          {teamsCommittees.map((group) => (
             <Tabs.Content value={group.name} key={group.id} w={"full"}>
               <TeamTab group={group} />
             </Tabs.Content>
@@ -97,8 +59,12 @@ export default function TeamsCommitteesPage() {
   );
 }
 
-function TeamTab({ group }: { group: components["schemas"]["UserGroup"] }) {
-  const I18n = useT();
+async function TeamTab({
+  group,
+}: {
+  group: components["schemas"]["UserGroup"];
+}) {
+  const I18n = await getT();
 
   const { metadata, name, id } = group;
   const { friendly_id, email } = metadata!;
@@ -124,32 +90,21 @@ function TeamTab({ group }: { group: components["schemas"]["UserGroup"] }) {
   );
 }
 
-function MemberTable({
+async function MemberTable({
   id,
   isActive = true,
 }: {
   id: number;
   isActive?: boolean;
 }) {
-  const I18n = useT();
-  const api = useAPI();
+  const { t } = await getT();
 
-  const { data: roleRequest, isLoading } = useQuery({
-    queryKey: ["roles", id, isActive],
-    queryFn: () =>
-      api.GET("/user_roles", {
-        params: {
-          query: {
-            isActive: isActive,
-            groupId: id,
-          },
-        },
-      }),
-  });
+  const { data: roles, error } = await getTeamCommitteeMembers(
+    id,
+    isActive,
+  );
 
-  const roles = useMemo(() => roleRequest?.data ?? [], [roleRequest]);
-
-  if (isLoading) return <Loading />;
+  if (error) return <Errored error={error} />;
 
   return (
     <SimpleGrid columns={{ md: 1, sm: 1, lg: 2 }} gap={"20px"}>
@@ -161,7 +116,7 @@ function MemberTable({
           wcaId={role.user.wca_id}
           roles={[
             {
-              teamRole: I18n.t(
+              teamRole: t(
                 `enums.user_roles.status.${role.group.group_type}.${role.metadata.status}`,
               ),
               staffColor: "red",
