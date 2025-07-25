f2a75edf97edecfe76c52416ee545a2184f561ee
Duncan <duncanonthejob@gmail.com>
2025-07-25 07:36:21 +0200

    added tests for refund available

--- app/controllers/api/v1/registration_payments_controller.rb
+++ app/controllers/api/v1/registration_payments_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::RegistrationPaymentsController < Api::V1::ApiController
 
     # Use `filter` here on purpose because the whole `registration_payments` list has been included above.
     #   Using `where` would create an SQL query, but it would also break (i.e. make redundant) the `includes` call above.
-    root_payments = registration.registration_payments#.filter { it.refunded_registration_payment_id.nil? }
+    root_payments = registration.registration_payments.filter { it.refunded_registration_payment_id.nil? }
     serialized_payments = root_payments.map { it.to_v2_json(refunds: true) }
 
     render json: { charges: serialized_payments }
--- spec/models/registration_payments_spec.rb
+++ spec/models/registration_payments_spec.rb
@@ -4,13 +4,23 @@ require 'rails_helper'
 
 RSpec.describe RegistrationPayment do
   describe '#amount_available_for_refund' do
-    it 'returns 10 when no refund' do
+    let!(:competition) { create(:competition, :stripe_connected) }
+    let(:registration) { create(:registration, competition: competition) }
+    let!(:registration_payment) { create(:registration_payment, registration: registration) }
+
+    it 'returns 1000 when no refund' do
+      expect(registration_payment.amount_available_for_refund).to eq(1000)
+
     end
 
-    it 'returns 5 when partially refunded' do
+    it 'returns 500 when partially refunded' do
+      refund = create(:registration_payment, :refund, registration: registration, amount_lowest_denomination: -500)
+      expect(registration_payment.amount_available_for_refund).to eq(500)
     end
 
     it 'returns 0 when fully refunded' do
+      refund = create(:registration_payment, :refund, registration: registration)
+      expect(registration_payment.amount_available_for_refund).to eq(0)
     end
   end
 end
