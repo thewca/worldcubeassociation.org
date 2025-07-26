81d28cafe78c10d74e5c6f973482f915cca1e721
Gregor Billing <gbilling@worldcubeassociation.org>
2025-07-26 14:29:46 +0900

    Move 'unsaved changes' window alert to shared hook code

---
+++ app/webpacker/lib/hooks/useUnsavedChangesAlert.js
@@ -0,0 +1,24 @@
+import { useCallback, useEffect } from 'react';
+
+const useUnsavedChangesAlert = (hasUnsavedChanges) => {
+  const onUnload = useCallback((e) => {
+    // Prompt the user before letting them navigate away from this page with unsaved changes.
+    if (hasUnsavedChanges) {
+      const confirmationMessage = 'You have unsaved changes, are you sure you want to leave?';
+      e.returnValue = confirmationMessage;
+      return confirmationMessage;
+    }
+
+    return null;
+  }, [hasUnsavedChanges]);
+
+  return useEffect(() => {
+    window.addEventListener('beforeunload', onUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', onUnload);
+    };
+  }, [onUnload]);
+};
+
+export default useUnsavedChangesAlert;
