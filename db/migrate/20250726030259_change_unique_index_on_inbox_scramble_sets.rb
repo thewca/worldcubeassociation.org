312c36381a3248e5b1c1ee8f393a5f7aa74ec1b3
Gregor Billing <gbilling@worldcubeassociation.org>
2025-07-26 17:05:58 +0900

    Change migration strategy

--- db/migrate/20250726030259_change_unique_index_on_inbox_scramble_sets.rb
+++ db/migrate/20250726030259_change_unique_index_on_inbox_scramble_sets.rb
@@ -2,9 +2,6 @@
 
 class ChangeUniqueIndexOnInboxScrambleSets < ActiveRecord::Migration[7.2]
   def change
-    change_table :inbox_scramble_sets, bulk: true do |t|
-      t.remove_index %i[competition_id event_id round_number scramble_set_number], unique: true
-      t.index %i[competition_id event_id round_number ordered_index], unique: true
-    end
+    remove_index :inbox_scramble_sets, %i[competition_id event_id round_number scramble_set_number], unique: true
   end
 end
--- db/migrate/20250726044928_add_unique_index_on_inbox_scrambles.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddUniqueIndexOnInboxScrambles < ActiveRecord::Migration[7.2]
-  def change
-    add_index :inbox_scrambles, %i[inbox_scramble_set_id ordered_index], unique: true
-  end
-end
