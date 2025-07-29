881a799e4c2ade6aec3edd495f7e587d2a8f2abb
Gregor Billing <gbilling@worldcubeassociation.org>
2025-07-29 21:13:26 +0900

    Correctly compile entry point lookup

--- app/webpacker/components/ScrambleMatcher/MatchingPickerChain.jsx
+++ app/webpacker/components/ScrambleMatcher/MatchingPickerChain.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import _ from 'lodash';
 import PickerWithMatching from './PickerWithMatching';
-import { parseActivityCode } from '../../lib/utils/wcif';
-
-function extractEventId(roundId) {
-  return parseActivityCode(roundId).eventId;
-}
+import { compileLookup } from './util';
 
 export default function MatchingPickerChain({
   wcifEvents,
   matchState,
   dispatchMatchState,
 }) {
-  const wrappedMatchState = _.groupBy(
-    _.map(matchState, (values, id) => ({ id, values })),
-    (item) => extractEventId(item.id),
-  );
+  const baseLookup = compileLookup(wcifEvents, {
+    mapping: (event) => event.rounds.map(
+      (round) => ({
+        ...round,
+        scrambleSets: matchState[round.id],
+      }),
+    ),
+  });
 
   return (
     <PickerWithMatching
       pickerKey="events"
       selectableEntities={wcifEvents}
-      entityLookup={wrappedMatchState}
+      entityLookup={baseLookup}
       dispatchMatchState={dispatchMatchState}
       nestedPickers={[
-        { key: 'rounds', mapping: 'values' },
+        { key: 'rounds', mapping: 'scrambleSets' },
         { key: 'groups', mapping: 'inbox_scrambles' },
       ]}
     />
