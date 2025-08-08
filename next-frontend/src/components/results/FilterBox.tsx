7479a83f40d0a85420a83ec25afae9deb6d0414a
FinnIckler <finnickler@gmail.com>
2025-08-08 18:21:29 +0200

    add Gender and Show button groups

--- next-frontend/src/components/results/FilterBox.tsx
+++ next-frontend/src/components/results/FilterBox.tsx
@@ -1,4 +1,11 @@
-import { Box, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  VStack,
+  HStack,
+  Field,
+} from "@chakra-ui/react";
 import EventSelector from "@/components/EventSelector";
 import { useT } from "@/lib/i18n/useI18n";
 import RegionSelector from "@/components/RegionSelector";
@@ -52,6 +59,73 @@ export default function FilterBox({
           t={t}
           name={t("delegates_page.all_regions")}
         />
+        <HStack>
+          <Field.Root>
+            <Field.Label>Gender</Field.Label>
+            <ButtonGroup attached size="md" width="100%">
+              <Button
+                flex={1}
+                onClick={() => filterActions.setGender("All")}
+                variant={filterState.gender == "All" ? "outline" : "solid"}
+              >
+                All
+              </Button>
+              <Button
+                flex={1}
+                onClick={() => filterActions.setGender("Male")}
+                variant={filterState.gender == "Male" ? "outline" : "solid"}
+              >
+                Male
+              </Button>
+              <Button
+                flex={1}
+                onClick={() => filterActions.setGender("Female")}
+                variant={filterState.gender == "Female" ? "outline" : "solid"}
+              >
+                Female
+              </Button>
+            </ButtonGroup>
+          </Field.Root>
+          <Field.Root>
+            <Field.Label>Show</Field.Label>
+            <ButtonGroup attached size="md" width="100%">
+              <Button
+                flex={1}
+                onClick={() => filterActions.setShow("mixed")}
+                variant={filterState.show == "mixed" ? "outline" : "solid"}
+              >
+                Mixed
+              </Button>
+              <Button
+                flex={1}
+                onClick={() => filterActions.setShow("slim")}
+                variant={filterState.show == "slim" ? "outline" : "solid"}
+              >
+                Slim
+              </Button>
+              <Button
+                flex={1}
+                onClick={() => filterActions.setShow("separate")}
+                variant={filterState.show == "separate" ? "outline" : "solid"}
+              >
+                Separate
+              </Button>
+              <Button
+                flex={1}
+                onClick={() => filterActions.setShow("history")}
+                variant={filterState.show == "history" ? "outline" : "solid"}
+              >
+                History
+              </Button>
+              <Button
+                flex={1}
+                onClick={() => filterActions.setShow("Mixed History")}
+              >
+                Mixed History
+              </Button>
+            </ButtonGroup>
+          </Field.Root>
+        </HStack>
       </VStack>
     </Box>
   );
