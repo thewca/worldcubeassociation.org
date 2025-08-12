183ef6553a210c2092ffae80804ecd4a24c4954d
FinnIckler <finnickler@gmail.com>
2025-08-12 17:41:36 +0200

    remove border of iframe

--- next-frontend/src/app/(wca)/regulations/page.tsx
+++ next-frontend/src/app/(wca)/regulations/page.tsx
@@ -1,14 +1,13 @@
-"use client";
-
 import { AspectRatio, Container } from "@chakra-ui/react";
 
-export default function Regulations() {
+export default async function Regulations() {
   return (
-    <Container>
+    <Container bg="bg">
       <AspectRatio>
         <iframe
-          width={"100%"}
-          src={`https://regulations.worldcubeassociation.org`}
+          width="100%"
+          src="https://regulations.worldcubeassociation.org"
+          style={{ border: "none", background: "transparent" }}
         ></iframe>
       </AspectRatio>
     </Container>
