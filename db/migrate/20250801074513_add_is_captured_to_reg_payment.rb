bdec5bd74121f3ccb154ebe3860d5dca9eaf6730
Duncan <duncanonthejob@gmail.com>
2025-08-18 07:56:00 +0200

    rename migration

--- db/migrate/20250801074513_add_is_captured_to_reg_payment.rb
+++ db/migrate/20250801074513_add_is_captured_to_reg_payment.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class LinkPaymentIntentAndRegPayment < ActiveRecord::Migration[7.2]
+class AddIsCapturedToRegPayment < ActiveRecord::Migration[7.2]
   def change
     add_column :registration_payments, :is_captured, :boolean, default: true, null: false
   end
