3933f95909e48c8db566c2bdbdbb04afc72bcea5
FinnIckler <finnickler@gmail.com>
2025-08-25 16:37:37 +0200

    Only set foreign_key_checks to false in dev dump (#12312)

--- lib/database_dumper.rb
+++ lib/database_dumper.rb
@@ -1227,9 +1227,7 @@ module DatabaseDumper
 
     # Turn of foreign key checking to avoid errors when dumping data caused by foreign keys referencing not yet
     # existing rows.
-    ActiveRecord::Base.connection.execute("SET unique_checks=0")
     ActiveRecord::Base.connection.execute("SET foreign_key_checks=0")
-    ActiveRecord::Base.connection.execute("SET autocommit=0")
 
     LogTask.log_task "Populating sanitized tables in '#{dump_db_name}'" do
       ordered_table_names.each do |table_name|
@@ -1261,9 +1259,7 @@ module DatabaseDumper
       ActiveRecord::Base.connection.execute("INSERT INTO #{dump_db_name}.server_settings (name, value, created_at, updated_at) VALUES ('#{dump_ts_name}', UNIX_TIMESTAMP(), NOW(), NOW())") if dump_ts_name.present?
 
       # Turn these back on. We do establish a new connection again in the ensure block, but just in case this carries over
-      ActiveRecord::Base.connection.execute("SET unique_checks=1")
       ActiveRecord::Base.connection.execute("SET foreign_key_checks=1")
-      ActiveRecord::Base.connection.execute("SET autocommit=1")
     end
 
     yield dump_db_name
