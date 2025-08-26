9b2c644f654a438008a1c63957a631c4cc78c2a9
FinnIckler <finnickler@gmail.com>
2025-08-26 13:52:26 +0200

    move TabMenu to client component

--- next-frontend/src/app/(wca)/competitions/[competitionId]/layout.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/layout.tsx
@@ -1,6 +1,5 @@
-import { Container, Separator, Tabs } from "@chakra-ui/react";
-import Link from "next/link";
-import { route } from "nextjs-routes";
+import { Container } from "@chakra-ui/react";
+import TabMenu from "@/components/competitions/TabMenu";
 
 export default async function CompetitionLayout({
   children,
@@ -13,62 +12,7 @@ export default async function CompetitionLayout({
 
   return (
     <Container minW="80vw" p="8">
-      <Tabs.Root
-        variant="enclosed"
-        w="100%"
-        defaultValue="general"
-        orientation="vertical"
-        lazyMount
-        unmountOnExit
-      >
-        <Tabs.List height="fit-content" position="sticky" top="3">
-          <Link
-            href={route({
-              pathname: "/competitions/[competitionId]",
-              query: { competitionId },
-            })}
-          >
-            <Tabs.Trigger value="general">General</Tabs.Trigger>
-          </Link>
-          <Link
-            href={route({
-              pathname: "/competitions/[competitionId]/register",
-              query: { competitionId },
-            })}
-          >
-            <Tabs.Trigger value="register">Register</Tabs.Trigger>
-          </Link>
-          <Link
-            href={route({
-              pathname: "/competitions/[competitionId]/competitors",
-              query: { competitionId },
-            })}
-          >
-            <Tabs.Trigger value="competitors">Competitors</Tabs.Trigger>
-          </Link>
-          <Link
-            href={route({
-              pathname: "/competitions/[competitionId]/events",
-              query: { competitionId },
-            })}
-          >
-            <Tabs.Trigger value="events">Events </Tabs.Trigger>
-          </Link>
-          <Link
-            href={route({
-              pathname: "/competitions/[competitionId]/schedule",
-              query: { competitionId },
-            })}
-          >
-            <Tabs.Trigger value="schedule">Schedule</Tabs.Trigger>
-          </Link>
-          <Separator />
-          <Tabs.Trigger value="custom-1">Custom 1</Tabs.Trigger>
-          <Tabs.Trigger value="custom-2">Custom 2</Tabs.Trigger>
-          <Tabs.Trigger value="custom-3">Custom 3</Tabs.Trigger>
-        </Tabs.List>
-        {children}
-      </Tabs.Root>
+      <TabMenu competitionId={competitionId}>{children}</TabMenu>
     </Container>
   );
 }
