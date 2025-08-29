1621def13018acc7bcadd5115df605b0af4ebd22
FinnIckler <finnickler@gmail.com>
2025-08-29 17:14:26 +0200

    refactor cards and add some to podiums page

--- next-frontend/src/app/(wca)/competitions/[competitionId]/podiums/page.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/podiums/page.tsx
@@ -1,9 +1,19 @@
-import { Container, Heading, VStack } from "@chakra-ui/react";
+import {
+  Card,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import ResultsTable from "@/components/results/ResultsTable";
 import events, { WCA_EVENT_IDS } from "@/lib/wca/data/events";
 import { getPodiums } from "@/lib/wca/competitions/getPodiums";
 import { Fragment } from "react";
 import _ from "lodash";
+import { getT } from "@/lib/i18n/get18n";
+import { getCompetitionInfo } from "@/lib/wca/competitions/getCompetitionInfo";
+import { EventCard, InfoCard } from "@/components/competitions/Cards";
 
 export default async function PodiumsPage({
   params,
@@ -11,6 +21,19 @@ export default async function PodiumsPage({
   params: Promise<{ competitionId: string }>;
 }) {
   const { competitionId } = await params;
+
+  const { data: competitionInfo, error } =
+    await getCompetitionInfo(competitionId);
+
+  if (error) {
+    return <Text>Error fetching competition</Text>;
+  }
+
+  if (!competitionInfo) {
+    return <Text>Competition does not exist</Text>;
+  }
+
+  const { t } = await getT();
 
   const podiumsRequest = await getPodiums(competitionId);
 
@@ -20,27 +43,43 @@ export default async function PodiumsPage({
 
   return (
     <Container bg="bg">
-      <VStack align="left" gap={4}>
-        <Heading size="5xl">Podiums</Heading>
-        {WCA_EVENT_IDS.map((eventId) => {
-          const results = resultsByEvent[eventId];
-          if (!results) {
-            return null;
-          }
-          return (
-            <Fragment key={eventId}>
-              <Heading size="2xl">{events.byId[eventId].name}</Heading>
-              <ResultsTable
-                results={results.toSorted((a, b) => a.pos - b.pos)}
-                competitionId={competitionId}
-                eventId={eventId}
-                isAdmin={false}
-              />
-            </Fragment>
-          );
-        })}
-      </VStack>
-      <Heading size="5xl">Podiums</Heading>
+      <HStack gap="8" alignItems="stretch">
+        <InfoCard competitionInfo={competitionInfo} t={t} />
+        <EventCard competitionInfo={competitionInfo} />s
+      </HStack>
+      <Card.Root variant="plain" mt="8">
+        <Card.Body>
+          <Card.Title>
+            <Text
+              fontSize="md"
+              textTransform="uppercase"
+              fontWeight="medium"
+              letterSpacing="wider"
+            >
+              Podiums
+            </Text>
+          </Card.Title>
+          <VStack align="left" gap={4}>
+            {WCA_EVENT_IDS.map((eventId) => {
+              const results = resultsByEvent[eventId];
+              if (!results) {
+                return null;
+              }
+              return (
+                <Fragment key={eventId}>
+                  <Heading size="2xl">{events.byId[eventId].name}</Heading>
+                  <ResultsTable
+                    results={results.toSorted((a, b) => a.pos - b.pos)}
+                    competitionId={competitionId}
+                    eventId={eventId}
+                    isAdmin={false}
+                  />
+                </Fragment>
+              );
+            })}
+          </VStack>
+        </Card.Body>
+      </Card.Root>
     </Container>
   );
 }
