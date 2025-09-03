6ebebe6691a441570d9b19b86d13abff73be40a3
FinnIckler <finnickler@gmail.com>
2025-09-03 16:42:38 +0200

    make very simple list menu for now

--- next-frontend/src/components/competitions/MobileMenu.tsx
+++ next-frontend/src/components/competitions/MobileMenu.tsx
@@ -2,16 +2,7 @@
 
 import Link from "next/link";
 import { route } from "nextjs-routes";
-import {
-  Button,
-  CloseButton,
-  Drawer,
-  List,
-  Portal,
-  VStack,
-} from "@chakra-ui/react";
-import { usePathname } from "next/navigation";
-import _ from "lodash";
+import { List, VStack } from "@chakra-ui/react";
 
 export default function MobileMenu({
   competitionId,
@@ -20,67 +11,40 @@ export default function MobileMenu({
   children: React.ReactNode;
   competitionId: string;
 }) {
-  const pathName = usePathname();
-
-  const path = _.last(pathName.split("/"));
-  const currentPath = path === competitionId ? "general" : path;
-
   return (
     <VStack hideFrom="md">
-      <Drawer.Root placement="top">
-        <Drawer.Trigger asChild>
-          <Button variant="outline" size="sm" w="100%">
+      <List.Root>
+        <List.Item>
+          <Link
+            href={route({
+              pathname: "/competitions/[competitionId]",
+              query: { competitionId },
+            })}
+          >
             General
-          </Button>
-        </Drawer.Trigger>
-        <Portal>
-          <Drawer.Backdrop />
-          <Drawer.Positioner>
-            <Drawer.Content>
-              <Drawer.Header>
-                <Drawer.Title>Menu</Drawer.Title>
-              </Drawer.Header>
-              <Drawer.Body>
-                <List.Root>
-                  <List.Item>
-                    <Link
-                      href={route({
-                        pathname: "/competitions/[competitionId]",
-                        query: { competitionId },
-                      })}
-                    >
-                      General
-                    </Link>
-                  </List.Item>
-                  <List.Item>
-                    <Link
-                      href={route({
-                        pathname: "/competitions/[competitionId]/register",
-                        query: { competitionId },
-                      })}
-                    >
-                      Register
-                    </Link>
-                  </List.Item>
-                  <List.Item>
-                    <Link
-                      href={route({
-                        pathname: "/competitions/[competitionId]/competitors",
-                        query: { competitionId },
-                      })}
-                    >
-                      Competitors
-                    </Link>
-                  </List.Item>
-                </List.Root>
-              </Drawer.Body>
-              <Drawer.CloseTrigger asChild>
-                <CloseButton size="sm" />
-              </Drawer.CloseTrigger>
-            </Drawer.Content>
-          </Drawer.Positioner>
-        </Portal>
-      </Drawer.Root>
+          </Link>
+        </List.Item>
+        <List.Item>
+          <Link
+            href={route({
+              pathname: "/competitions/[competitionId]/register",
+              query: { competitionId },
+            })}
+          >
+            Register
+          </Link>
+        </List.Item>
+        <List.Item>
+          <Link
+            href={route({
+              pathname: "/competitions/[competitionId]/competitors",
+              query: { competitionId },
+            })}
+          >
+            Competitors
+          </Link>
+        </List.Item>
+      </List.Root>
       {children}
     </VStack>
   );
