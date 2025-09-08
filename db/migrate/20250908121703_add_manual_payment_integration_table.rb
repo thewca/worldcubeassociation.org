b6b12ebde2e2978fbf87da9a11a9182ab19ef332
Duncan <duncanonthejob@gmail.com>
2025-09-08 14:59:36 +0200

    add stripe integration test and fix method name

--- db/migrate/20250908121703_add_manual_payment_integration_table.rb
+++ db/migrate/20250908121703_add_manual_payment_integration_table.rb
@@ -3,7 +3,7 @@
 class AddManualPaymentIntegrationTable < ActiveRecord::Migration[7.2]
   def change
      create_table :manual_payment_integrations do |t|
-      t.string :payment_reference, null: false # TODO: What is this property doing?
+      t.string :payment_reference, null: false
       t.text :payment_information, null: false
       t.timestamps
     end
