bfbe4c30d656bf845f92b18cd8536b1ca16c5edb
Duncan <duncanonthejob@gmail.com>
2025-09-08 14:22:44 +0200

    add competition model changes

---
+++ db/migrate/20250908121703_add_manual_payment_integration_table.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddManualPaymentIntegrationTable < ActiveRecord::Migration[7.2]
+  def change
+     create_table :manual_payment_integrations do |t|
+      t.string :payment_reference, null: false # TODO: What is this property doing?
+      t.text :payment_information, null: false
+      t.timestamps
+    end
+  end
+end
