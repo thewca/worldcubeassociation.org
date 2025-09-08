4c373f356cf06ba511036fa6fda2a97bc1b137a4
FinnIckler <finnickler@gmail.com>
2025-09-08 17:17:54 +0200

    move the rest of the APIs

--- next-frontend/src/types/openapi.ts
+++ next-frontend/src/types/openapi.ts
@@ -451,6 +451,23 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/users/me/permissions": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** Get user's permissions */
+        get: operations["getUserPermissions"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/regulations/translations": {
         parameters: {
             query?: never;
@@ -496,23 +513,6 @@ export interface paths {
         };
         /** Get Export Information */
         get: operations["getExports"];
-        put?: never;
-        post?: never;
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
-    };
-    "/users/me/permissions": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        /** Get user's permissions */
-        get: operations["getUserPermissions"];
         put?: never;
         post?: never;
         delete?: never;
@@ -578,72 +578,6 @@ export interface paths {
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
-        RegionalOrganization: {
-            name: string;
-            country_iso2: string;
-            website: string;
-            /** Format: uri */
-            logo_url?: string;
-        };
-        CompetitionPermissions: string[] | string;
-        UserPermissions: {
-            can_attend_competitions: {
-                scope: components["schemas"]["CompetitionPermissions"];
-                /** Format: date-time */
-                until?: string | null;
-            };
-            can_organize_competitions: {
-                scope: components["schemas"]["CompetitionPermissions"];
-            };
-            can_administer_competitions: {
-                scope: components["schemas"]["CompetitionPermissions"];
-            };
-            can_view_delegate_admin_page: {
-                scope: components["schemas"]["CompetitionPermissions"];
-            };
-            can_view_delegate_report: {
-                scope: components["schemas"]["CompetitionPermissions"];
-            };
-            can_edit_delegate_report: {
-                scope: components["schemas"]["CompetitionPermissions"];
-            };
-            can_create_groups: {
-                scope: string[];
-            };
-            can_read_groups_current: {
-                scope: string[];
-            };
-            can_read_groups_past: {
-                scope: string[];
-            };
-            can_edit_groups: {
-                scope: string[];
-            };
-            can_access_panels: {
-                scope: string[];
-            };
-            can_request_to_edit_others_profile: {
-                scope: string[] | string;
-            };
-        };
-        User: {
-            /** Format: uuid */
-            id?: string;
-            name?: string;
-            /** Format: email */
-            email?: string;
-        };
-        PersonalRecord: {
-            eventId?: string;
-            /** @description Best performance in milliseconds */
-            bestTime?: number;
-            /** @description Average performance in milliseconds */
-            averageTime?: number;
-        };
-        Event: {
-            id?: string;
-            name?: string;
-        };
         UserAvatar: {
             /**
              * Format: uri
@@ -1156,6 +1090,47 @@ export interface components {
             };
             class?: string;
         };
+        CompetitionPermissions: string[] | string;
+        UserPermissions: {
+            can_attend_competitions: {
+                scope: components["schemas"]["CompetitionPermissions"];
+                /** Format: date-time */
+                until?: string | null;
+            };
+            can_organize_competitions: {
+                scope: components["schemas"]["CompetitionPermissions"];
+            };
+            can_administer_competitions: {
+                scope: components["schemas"]["CompetitionPermissions"];
+            };
+            can_view_delegate_admin_page: {
+                scope: components["schemas"]["CompetitionPermissions"];
+            };
+            can_view_delegate_report: {
+                scope: components["schemas"]["CompetitionPermissions"];
+            };
+            can_edit_delegate_report: {
+                scope: components["schemas"]["CompetitionPermissions"];
+            };
+            can_create_groups: {
+                scope: string[];
+            };
+            can_read_groups_current: {
+                scope: string[];
+            };
+            can_read_groups_past: {
+                scope: string[];
+            };
+            can_edit_groups: {
+                scope: string[];
+            };
+            can_access_panels: {
+                scope: string[];
+            };
+            can_request_to_edit_others_profile: {
+                scope: string[] | string;
+            };
+        };
         Translation: {
             version: string;
             language: string;
@@ -1179,6 +1154,13 @@ export interface components {
             /** Format: uri */
             developer_url?: string;
         };
+        RegionalOrganization: {
+            name: string;
+            country_iso2: string;
+            website: string;
+            /** Format: uri */
+            logo_url?: string;
+        };
     };
     responses: never;
     parameters: never;
@@ -1188,26 +1170,6 @@ export interface components {
 }
 export type $defs = Record<string, never>;
 export interface operations {
-    getExports: {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        requestBody?: never;
-        responses: {
-            /** @description Successfully retrieved exports */
-            200: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["ExportInfo"];
-                };
-            };
-        };
-    };
     getUserPermissions: {
         parameters: {
             query?: never;
@@ -1226,12 +1188,25 @@ export interface operations {
                     "application/json": components["schemas"]["UserPermissions"];
                 };
             };
-            /** @description Unauthorized */
-            401: {
+        };
+    };
+    getExports: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Successfully retrieved exports */
+            200: {
                 headers: {
                     [name: string]: unknown;
                 };
-                content?: never;
+                content: {
+                    "application/json": components["schemas"]["ExportInfo"];
+                };
             };
         };
     };
