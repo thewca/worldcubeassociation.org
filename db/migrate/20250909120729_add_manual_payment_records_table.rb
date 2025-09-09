227ea5a15cb9472b9bf893f0f1644979c729f818
Duncan <duncanonthejob@gmail.com>
2025-09-09 14:08:29 +0200

    remove old cpi migration and add MPR migration

--- db/migrate/20250909120729_add_manual_payment_records_table.rb
+++ db/migrate/20250909120729_add_manual_payment_records_table.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
-class AddManualCpiTables < ActiveRecord::Migration[7.2]
+class AddManualPaymentRecordsTable < ActiveRecord::Migration[7.2]
   def change
-    create_table :manual_payment_integrations do |t|
-      t.string :payment_reference, null: false
-      t.text :payment_information, null: false
-      t.timestamps
-    end
-
     create_table :manual_payment_records do |t|
       t.string :payment_reference
       t.string :manual_status, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_08_121703) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_09_120729) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
