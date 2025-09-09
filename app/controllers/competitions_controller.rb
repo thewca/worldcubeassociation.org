c21d3b5368cf0ccf16963794988082d3cc096ee0
Duncan <duncanonthejob@gmail.com>
2025-09-09 11:13:50 +0200

    remove stray '?'

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -248,7 +248,7 @@ class CompetitionsController < ApplicationController
       return redirect_to competition_payment_integration_setup_path(competition)
     end
 
-    if payment_integration == :manual && ManualPaymentIntegration.manual_payments_disabled??
+    if payment_integration == :manual && ManualPaymentIntegration.manual_payments_disabled?
       flash[:error] = 'Manual payments are not yet available in production environments'
       return redirect_to competition_payment_integration_setup_path(competition)
     end
