dc8ada3a89fdd710226421c3a29f832369218d7f
Duncan <duncanonthejob@gmail.com>
2025-09-09 19:14:46 +0200

    update pmtTicketUrl

--- app/webpacker/components/RegistrationsV2/api/payment/get/getPaymentTicket.js
+++ app/webpacker/components/RegistrationsV2/api/payment/get/getPaymentTicket.js
@@ -4,8 +4,9 @@ import { paymentTicketUrl } from '../../../../../lib/requests/routes.js.erb';
 export default async function getPaymentTicket(
   registrationId,
   donationAmount,
+  paymentIntegration,
 ) {
-  const route = paymentTicketUrl(registrationId, donationAmount);
+  const route = paymentTicketUrl(registrationId, donationAmount, paymentIntegration);
   const { data } = await fetchWithJWTToken(route, {
     method: 'GET',
   });
