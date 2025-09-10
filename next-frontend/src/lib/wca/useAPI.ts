ca7bb38346a7a60711ec503c47ae720837b8d36e
FinnIckler <finnickler@gmail.com>
2025-09-10 16:56:56 +0200

    undo temporary fixes

--- next-frontend/src/lib/wca/useAPI.ts
+++ next-frontend/src/lib/wca/useAPI.ts
@@ -2,15 +2,15 @@ import { useSession } from "next-auth/react";
 import { useMemo } from "react";
 import { authenticatedClient, unauthenticatedClient } from "@/lib/wca/wcaAPI";
 
-export default function useAPI(v1: boolean = false) {
+export default function useAPI() {
   const { data: session } = useSession();
 
   return useMemo(() => {
-    if (false) {
+    if (session) {
       // @ts-expect-error TODO: Fix this
       return authenticatedClient(session.accessToken);
     } else {
-      return unauthenticatedClient(v1);
+      return unauthenticatedClient;
     }
-  }, [session, v1]);
+  }, [session]);
 }
