dbfcc287f7c6504d10bdc2159d58a12553d4d23d
Duncan <duncanonthejob@gmail.com>
2025-09-11 16:01:42 +0200

    add type to removed column

--- db/migrate/20250911125658_remove_auto_accept_registrations_column.rb
+++ db/migrate/20250911125658_remove_auto_accept_registrations_column.rb
@@ -2,6 +2,6 @@
 
 class RemoveAutoAcceptRegistrationsColumn < ActiveRecord::Migration[7.2]
   def change
-    remove_column :competitions, :auto_accept_registrations
+    remove_column :competitions, :auto_accept_registrations, :boolean
   end
 end
