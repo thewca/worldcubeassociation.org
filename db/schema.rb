55aba2b05086f44a3f62daf03bc0e4d3a4c2f1a4
Duncan <52967253+dunkOnIT@users.noreply.github.com>
2025-09-11 13:42:58 +0200

    [MPI 1] Connect Manual Payment Integration (#12408)
    * added payment integration tests
    
    * copying in backend code
    
    * add competition model changes
    
    * add manual integration setup route
    
    * add stripe integration test and fix method name
    
    * add frontend elements for connecting MPI
    
    * rubocop
    
    * manual payments have their own setup page now
    
    * add back edit and add edit account key
    
    * removed logger
    
    * enable find_payment, and raise controller error if using manual payments
    
    * remove stray '?'
    
    * remove prepare intent and create intent - moved to MPI 2
    
    * update competition_info with manual payment info
    
    * undo competition info changes
    
    * minor changes - line and connect_integration
    
    * standarize manual payment methods with other payment methods, add tests, rename MPI fields
    
    * get request tests passing
    
    * change field names in react page
    
    * better access for manual payment account - tests not working yet
    
    * get tests working
    
    * appease the linter gods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_28_075341) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_09_120729) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -791,6 +791,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_28_075341) do
     t.integer "notification_radius_km"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "manual_payment_integrations", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "payment_reference_label", null: false
+    t.text "payment_instructions", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "oauth_access_grants", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
