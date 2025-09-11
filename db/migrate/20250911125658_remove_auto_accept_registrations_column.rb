d16ecb405ce200f4429c147e2515e6cf28fc3342
Duncan <duncanonthejob@gmail.com>
2025-09-11 15:01:58 +0200

    remove auto accept registrations column

---
+++ db/migrate/20250911125658_remove_auto_accept_registrations_column.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class RemoveAutoAcceptRegistrationsColumn < ActiveRecord::Migration[7.2]
+  def change
+    remove_column :competitions, :auto_accept_registrations
+  end
+end
