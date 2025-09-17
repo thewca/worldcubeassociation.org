26eb39b269c41148df7d178a9f409f235a74fcf8
FinnIckler <finnickler@gmail.com>
2025-09-17 11:14:42 +0200

    fix params

--- next-frontend/src/app/(wca)/regulations/translations/[language]/page.tsx
+++ next-frontend/src/app/(wca)/regulations/translations/[language]/page.tsx
@@ -4,13 +4,13 @@ import { useParams } from "next/navigation";
 import { AspectRatio, Container } from "@chakra-ui/react";
 
 export default function TranslatedRegulations() {
-  const params = useParams<{ language: string }>();
+  const params = useParams<"/regulations/translations/[language]">();
 
   return (
     <Container>
       <AspectRatio>
         <iframe
-          width={"100%"}
+          width="100%"
           src={`https://regulations.worldcubeassociation.org/translations/${params.language}`}
         ></iframe>
       </AspectRatio>
