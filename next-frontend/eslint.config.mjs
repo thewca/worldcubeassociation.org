bfdcc510aec38c07ee90c4ccf265ee4b9cddf131
Gregor Billing <gbilling@worldcubeassociation.org>
2025-09-23 17:19:18 +0200

    Migrate to EsLint CLI via codemod

--- next-frontend/eslint.config.mjs
+++ next-frontend/eslint.config.mjs
@@ -15,6 +15,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslintPluginPrettierRecommended,
   ...pluginQuery.configs["flat/recommended"],
