41ce0daa680e0453299ec4ef34559cf717c58459
FinnIckler <finnickler@gmail.com>
2025-09-23 17:05:23 +0200

    Add the ability to link through payload images (#12461)
    * Add the ability to link through payload images
    
    * Inline polymorphic props in MediaImage
    
    * Move polymorphic wrapper to helper component
    
    * Linting
    
    * Yay importMap
    
    * Payload autogen new-line
    
    ---------
    
    Co-authored-by: Gregor Billing <gbilling@worldcubeassociation.org>

--- next-frontend/src/app/(wca)/page.tsx
+++ next-frontend/src/app/(wca)/page.tsx
@@ -8,7 +8,6 @@ import {
   Card,
   Separator,
   Box,
-  Image as ChakraImage,
   Heading,
   Text,
   Tabs,
@@ -49,6 +48,7 @@ import type {
 import Link from "next/link";
 import { route } from "nextjs-routes";
 import { getT } from "@/lib/i18n/get18n";
+import { MediaImage } from "@/components/MediaImage";
 
 const TextCard = ({ block }: { block: TextCardBlock }) => {
   return (
@@ -59,11 +59,7 @@ const TextCard = ({ block }: { block: TextCardBlock }) => {
       width="full"
     >
       {block.headerImage && (
-        <ChakraImage
-          src={(block.headerImage as Media).url ?? undefined}
-          alt={(block.headerImage as Media).alt ?? undefined}
-          aspectRatio="3/1"
-        />
+        <MediaImage media={block.headerImage as Media} aspectRatio="3/1" />
       )}
       <Card.Body>
         <Card.Title>{block.heading}</Card.Title>
@@ -125,9 +121,8 @@ const ImageBanner = ({ block }: { block: ImageBannerBlock }) => {
       size="lg"
     >
       <Box position="relative" width="50%" overflow="hidden">
-        <ChakraImage
-          src={(block.mainImage as Media).url ?? undefined}
-          alt={(block.mainImage as Media).alt ?? undefined}
+        <MediaImage
+          media={block.mainImage as Media}
           objectFit="cover"
           width="100%"
           height="40vh"
@@ -188,9 +183,9 @@ const ImageOnlyCard = ({ block }: { block: ImageOnlyCardBlock }) => {
       colorPalette={block.colorPalette}
       width="full"
     >
-      <ChakraImage
-        src={(block.mainImage as Media).url ?? undefined}
-        alt={(block.mainImage as Media).alt ?? block.heading ?? undefined}
+      <MediaImage
+        media={block.mainImage as Media}
+        altFallback={block.heading}
         aspectRatio="2/1"
       />
       {block.heading && (
@@ -319,17 +314,10 @@ const TestimonialsSpinner = ({ block }: { block: TestimonialsBlock }) => {
                 overflow="hidden"
                 colorPalette={slide.colorPalette}
               >
-                <ChakraImage
-                  src={
-                    testimonial.image != null
-                      ? ((testimonial.image as Media).url ?? undefined)
-                      : "/placeholder.png"
-                  }
-                  alt={
-                    testimonial.image != null
-                      ? (testimonial.image as Media).alt
-                      : testimonial.punchline
-                  }
+                <MediaImage
+                  media={testimonial.image as Media}
+                  srcFallback="/placeholder.png"
+                  altFallback={testimonial.punchline}
                   maxW="1/3"
                   objectFit="cover"
                 />
