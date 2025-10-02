869b08b1f3ca0b48138b1846f94fa91a5baabfb3
FinnIckler <finnickler@gmail.com>
2025-10-02 15:33:28 -0400

    create attempts in results

--- spec/factories/results.rb
+++ spec/factories/results.rb
@@ -15,6 +15,7 @@ FactoryBot.define do
       # In order to be valid, a result must have a round.
       # Make sure it exists before going through validations.
       FactoryBot.create(:round, competition: result.competition, event_id: result.event_id, format_id: result.format_id) if !result.round && !options.skip_round_creation
+      result.create_or_update_attempts
     end
 
     competition_id { competition.id }
