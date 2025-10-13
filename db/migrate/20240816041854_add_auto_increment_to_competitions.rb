6aae6c00759c884d3f36b7226f41c5363d9d032b
FinnIckler <finnickler@gmail.com>
2025-10-13 10:58:49 -0400

    remove microservice registrations from migration

--- db/migrate/20240816041854_add_auto_increment_to_competitions.rb
+++ db/migrate/20240816041854_add_auto_increment_to_competitions.rb
@@ -2,8 +2,6 @@
 
 class AddAutoIncrementToCompetitions < ActiveRecord::Migration[7.1]
   def up
-    remove_foreign_key :microservice_registrations, :Competitions
-
     execute "ALTER TABLE Competitions DROP PRIMARY KEY"
 
     rename_column :Competitions, :id, :competition_id
@@ -13,13 +11,9 @@ class AddAutoIncrementToCompetitions < ActiveRecord::Migration[7.1]
 
     execute "UPDATE Competitions c, (SELECT competition_id, ROW_NUMBER() OVER (ORDER BY created_at, announced_at, start_date, competition_id) AS rn FROM Competitions) h SET c.id = h.rn WHERE c.competition_id = h.competition_id"
     change_column :Competitions, :id, :primary_key, auto_increment: true
-
-    add_foreign_key :microservice_registrations, :Competitions, primary_key: :competition_id
   end
 
   def down
-    remove_foreign_key :microservice_registrations, :Competitions
-
     change_column :Competitions, :id, :integer, auto_increment: false
     execute "ALTER TABLE Competitions DROP PRIMARY KEY"
 
@@ -29,7 +23,5 @@ class AddAutoIncrementToCompetitions < ActiveRecord::Migration[7.1]
     rename_column :Competitions, :competition_id, :id
 
     execute "ALTER TABLE Competitions ADD PRIMARY KEY(id)"
-
-    add_foreign_key :microservice_registrations, :Competitions
   end
 end
