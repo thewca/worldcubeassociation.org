f164a6f65b0f777b8a0262971496903b59561cc9
FinnIckler <finnickler@gmail.com>
2025-10-29 10:49:06 +0100

    add validation to not change from 2025 to 2026 and vice versa

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -57,6 +57,12 @@ class Competition < ApplicationRecord
            allow_nil: true,
            with_model_currency: :currency_code
 
+  validates :start_date, :cant_change_across_regulation_boundaries
+
+  private def cant_change_across_regulation_boundaries
+    errors.add(:start_date, "You can't change the start date across Regulation boundaries.") if start_date_was.year == 2025 && start_date.year == 2026 || start_date_was.year == 2026 && start_date.year == 2025
+  end
+
   scope :not_cancelled, -> { where(cancelled_at: nil) }
   scope :visible, -> { where(show_at_all: true) }
   scope :not_visible, -> { where(show_at_all: false) }
