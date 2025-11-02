73574d18ae1d07539214cbc13d11132b3e547e41
Gregor Billing <gbilling@worldcubeassociation.org>
2025-11-02 15:15:10 +0100

    WIP page improvements

--- next-frontend/src/app/(wca)/competitions/[competitionId]/register/page.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/register/page.tsx
@@ -1,3 +1,40 @@
-export default function RegisterPage() {
-  return "Register";
+import { auth } from "@/auth";
+import {Alert, Code} from "@chakra-ui/react";
+import { cache } from "react";
+import { serverClientWithToken } from "@/lib/wca/wcaAPI";
+
+const fetchConfig = cache(async (authToken: string, competitionId: string) => {
+  const client = serverClientWithToken(authToken);
+
+  return await client.GET("/v1/competitions/{competitionId}/registration_config", {
+    params: { path: { competitionId } }
+  })
+})
+
+export default async function RegisterPage({
+  params
+}: {
+  params: Promise<{ competitionId: string }>
+}) {
+  const session = await auth();
+
+  if (session === null) {
+    return (
+      <Alert.Root>
+        <Alert.Indicator />
+        <Alert.Content>You need to log in first</Alert.Content>
+      </Alert.Root>
+    );
+  }
+
+  const competitionId = (await params).competitionId;
+  const stepConfig = await fetchConfig(session.accessToken, competitionId);
+
+  if (stepConfig.error) {
+    return "Something went wrong while fetching"
+  }
+
+  return (
+    <Code>{JSON.stringify(stepConfig.data)}</Code>
+  );
 }
