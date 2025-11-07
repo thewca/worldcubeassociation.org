b9b09c14fe0576025e792584db0ef777907f098a
Gregor Billing <gbilling@worldcubeassociation.org>
2025-11-07 20:13:01 +0100

    Add draft registration info to register page

--- next-frontend/src/app/(wca)/competitions/[competitionId]/register/page.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/register/page.tsx
@@ -1,11 +1,14 @@
 import { auth } from "@/auth";
-import { Alert, Box, Button, ButtonGroup, Steps } from "@chakra-ui/react";
+import {Alert, Box, Button, ButtonGroup, Card, HStack, Steps, VStack} from "@chakra-ui/react";
 import { cache } from "react";
 import { serverClientWithToken } from "@/lib/wca/wcaAPI";
 import { getT } from "@/lib/i18n/get18n";
 import type { components } from "@/types/openapi";
 import StepPanelContents from "@/app/(wca)/competitions/[competitionId]/register/StepPanelContents";
 import { getCompetitionInfo } from "@/lib/wca/competitions/getCompetitionInfo";
+import {RegistrationCard} from "@/components/competitions/Cards";
+import RegistrationRequirementsCard
+  from "@/app/(wca)/competitions/[competitionId]/register/RegistrationRequirementsCard";
 
 const fetchConfig = cache(async (authToken: string, competitionId: string) => {
   const client = serverClientWithToken(authToken);
@@ -46,6 +49,7 @@ export default async function RegisterPage({
 
   const competitionInfo = competitionInfoResponse.data;
 
+  // @ts-expect-error TODO: Fix this
   const stepConfig = await fetchConfig(session.accessToken, competitionId);
 
   if (stepConfig.error) {
@@ -58,37 +62,46 @@ export default async function RegisterPage({
   ] satisfies Step[];
 
   return (
-    <Steps.Root count={steps.length}>
-      <Steps.List>
-        {steps.map((step, idx) => {
-          const stepTranslationLookup = `competitions.registration_v2.register.panel.${step.key}`;
-          const stepTitle = t(`${stepTranslationLookup}.title`)
+    <VStack>
+      <Box width="full">
+        <RegistrationRequirementsCard competitionInfo={competitionInfo} />
+      </Box>
+      <Card.Root coloredBg width="full">
+        <Card.Body>
+          <Steps.Root count={steps.length}>
+            <Steps.List>
+              {steps.map((step, idx) => {
+                const stepTranslationLookup = `competitions.registration_v2.register.panel.${step.key}`;
+                const stepTitle = t(`${stepTranslationLookup}.title`)
 
-          return (
-            <Steps.Item key={step.key} index={idx} title={stepTitle}>
-              <Steps.Trigger disabled={!step.isEditable}>
-                <Steps.Indicator/>
-                <Box>
-                  <Steps.Title>{stepTitle}</Steps.Title>
-                  <Steps.Description>{t(`${stepTranslationLookup}.description`)}</Steps.Description>
-                </Box>
-              </Steps.Trigger>
-              <Steps.Separator/>
-            </Steps.Item>
-          );
-        })}
-      </Steps.List>
+                return (
+                  <Steps.Item key={step.key} index={idx} title={stepTitle}>
+                    <Steps.Trigger disabled={!step.isEditable}>
+                      <Steps.Indicator/>
+                      <Box>
+                        <Steps.Title>{stepTitle}</Steps.Title>
+                        <Steps.Description>{t(`${stepTranslationLookup}.description`)}</Steps.Description>
+                      </Box>
+                    </Steps.Trigger>
+                    <Steps.Separator/>
+                  </Steps.Item>
+                );
+              })}
+            </Steps.List>
 
-      <StepPanelContents steps={steps} competitionInfo={competitionInfo} />
+            <StepPanelContents steps={steps} competitionInfo={competitionInfo} />
 
-      <ButtonGroup size="sm" variant="outline">
-        <Steps.PrevTrigger asChild>
-          <Button>Prev</Button>
-        </Steps.PrevTrigger>
-        <Steps.NextTrigger asChild>
-          <Button>Next</Button>
-        </Steps.NextTrigger>
-      </ButtonGroup>
-    </Steps.Root>
+            <ButtonGroup size="sm" variant="outline">
+              <Steps.PrevTrigger asChild>
+                <Button>Prev</Button>
+              </Steps.PrevTrigger>
+              <Steps.NextTrigger asChild>
+                <Button>Next</Button>
+              </Steps.NextTrigger>
+            </ButtonGroup>
+          </Steps.Root>
+        </Card.Body>
+      </Card.Root>
+    </VStack>
   );
 }
