e60111e7d5c8314c36a473bb0e129a7723d6b3dc
FinnIckler <finnickler@gmail.com>
2025-11-07 13:49:53 -0500

    add consistency check

--- app/models/linked_rounds.rb
+++ app/models/linked_rounds.rb
@@ -3,6 +3,28 @@
 class LinkedRounds < ApplicationRecord
   has_many :rounds
 
+  validate :all_round_consistency
+
+  private def all_round_consistency
+    return if rounds.empty?
+
+    competition_ids = rounds.map(&:competition_id).uniq
+    event_ids       = rounds.map(&:event_id).uniq
+    round_numbers   = rounds.map(&:number).uniq
+
+    if competition_ids.size > 1
+      errors.add(:rounds, "must all belong to the same competition")
+    end
+
+    if event_ids.size > 1
+      errors.add(:rounds, "must all belong to the same event")
+    end
+
+    if round_numbers.size > 1
+      errors.add(:rounds, "must all belong to the same round number")
+    end
+  end
+
   def results
     rounds.flat_map { it.results }
   end
