28052075489b450e4a80110c687b4552818a9a72
FinnIckler <finnickler@gmail.com>
2025-11-07 15:29:54 -0500

    fix validation

--- app/models/linked_round.rb
+++ app/models/linked_round.rb
@@ -4,10 +4,7 @@ class LinkedRound < ApplicationRecord
   has_many :rounds
   has_many :results, through: :rounds
   has_many :competitions, through: :rounds
-  has_many :events, through: :rounds
+  has_many :competition_events, -> { distinct }, through: :rounds
 
-  validates :competition_ids, length: { is: 1, message: "must all belong to the same competition" }
-  validates :event_ids, length: { is: 1, message: "must all belong to the same event" }
-
-  validate :all_round_consistency
+  validates :competition_event_ids, length: { is: 1, message: "must all belong to the same competition" }, if: -> { competition_event_ids.present? }
 end
