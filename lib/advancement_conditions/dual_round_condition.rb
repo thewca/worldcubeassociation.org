4bc9dd0382fc4b4dea55912dbbffff76baf29d20
FinnIckler <finnickler@gmail.com>
2025-11-12 08:46:12 -0500

    change max_advancing to nil and add comment

--- lib/advancement_conditions/dual_round_condition.rb
+++ lib/advancement_conditions/dual_round_condition.rb
@@ -10,8 +10,12 @@ module AdvancementConditions
       I18n.t("advancement_condition#{'.short' if short}.dual")
     end
 
-    def max_advancing(results)
-      results.count
+    # No one "advances" to the linked round of a dual round
+    # so these are ignored in an advancing check,
+    # instead the results of all linked rounds are merged
+    # and the advancement condition of the last round is applied
+    def max_advancing(_results)
+      nil
     end
   end
 end
