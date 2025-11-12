d731f9d84d64dc08a547a41743647f4a04e55994
FinnIckler <finnickler@gmail.com>
2025-11-12 09:32:41 -0500

    use to solve_time to correctly merge results

--- app/models/linked_round.rb
+++ app/models/linked_round.rb
@@ -2,17 +2,17 @@
 
 class LinkedRound < ApplicationRecord
   has_many :rounds
-  has_many :round_results, through: :rounds, as: :results
+  has_many :round_results, through: :rounds, source: :results
   has_many :competition_events, -> { distinct }, through: :rounds
 
   validates :competition_event_ids, length: { maximum: 1, message: "must all belong to the same competition" }
 
   def results
-    results_by_person_id = round_results.group_by(:person_id)
+    results_by_person_id = round_results.group_by(&:person_id)
     persons = results_by_person_id.keys
     best_result_per_person = persons.map do |person|
-      results_by_person_id[person.id].min_by { |result| result.should_compute_average? ? result.average : result.best }
+      results_by_person_id[person].min_by { |result| result.should_compute_average? ? result.average : result.best }
     end
-    best_result_per_person.sort_by { |result| result.should_compute_average? ? result.average : result.best }
+    best_result_per_person.sort_by { |result| result.should_compute_average? ? result.to_solve_time(:average) : result.to_solve_time(:best) }
   end
 end
