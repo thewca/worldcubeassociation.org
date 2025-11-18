bc5808b84db1eb1627b06839ad218d1cda07dcd1
FinnIckler <finnickler@gmail.com>
2025-11-18 16:58:48 -0500

    add controller and error codes

--- app/controllers/api/v1/live/live_controller.rb
+++ app/controllers/api/v1/live/live_controller.rb
@@ -1,4 +1,62 @@
 # frozen_string_literal: true
 
 class Api::V1::Live::LiveController < Api::V1::ApiController
+  def add_result
+    competition_id = params.require(:competition_id)
+    render_error(:unauthorized, LiveResults::ErrorCodes::USER_INSUFFICIENT_PERMISSIONS) unless @current_user.can_manage_competition?(Competition.find(competition_id))
+
+    results = params.permit(attempts: %i[result attempt_number])[:attempts]
+    round_id = params.require(:round_id)
+    registration_id = params.require(:registration_id)
+
+    render_error(:unprocessable_content, LiveResults::ErrorCodes::LIVE_RESULT_ALREADY_EXISTS) if LiveResult.exists?(round_id: round_id, registration_id: registration_id)
+
+    AddLiveResultJob.perform_later(results, round_id, registration_id, current_user)
+
+    render json: { status: "ok" }
+  end
+
+  def update_result
+    results = params.require(:attempts)
+    round = Round.find(params.require(:round_id))
+    render_error(:unauthorized, LiveResults::ErrorCodes::USER_INSUFFICIENT_PERMISSIONS) unless @current_user.can_manage_competition?(round.competition)
+
+    registration_id = params.require(:registration_id)
+
+    result = LiveResult.includes(:live_attempts).find_by(round: round, registration_id: registration_id)
+
+    render_error(:unprocessable_content, LiveResults::ErrorCodes::LIVE_RESULT_NOT_FOUND) if result.blank?
+
+    previous_attempts = result.live_attempts.index_by(&:attempt_number)
+
+    new_attempts = results.map.with_index(1) do |r, i|
+      previous_attempt = previous_attempts[i]
+
+      if previous_attempt.present?
+        if previous_attempt.result == r
+          previous_attempt
+        else
+          previous_attempt.update_with_history_entry(r, current_user)
+        end
+      else
+        LiveAttempt.build_with_history_entry(r, i, current_user)
+      end
+    end
+
+    r = Result.new(
+      value1: results[0],
+      value2: results[1],
+      value3: results[2],
+      value4: results[3] || 0,
+      value5: results[4] || 0,
+      event_id: round.event.id,
+      round_type_id: round.round_type_id,
+      round_id: round.id,
+      format_id: round.format_id,
+    )
+
+    result.update(average: r.compute_correct_average, best: r.compute_correct_best, live_attempts: new_attempts, last_attempt_entered_at: Time.now.utc)
+
+    render json: { status: "ok" }
+  end
 end
