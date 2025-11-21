c2e3e1237317b4d71a7ef6ae1b1bd57ed366f338
Gregor Billing <gbilling@worldcubeassociation.org>
2025-11-21 09:04:49 +0100

    Fix test factories

--- spec/lib/results_validators/scrambles_validator_spec.rb
+++ spec/lib/results_validators/scrambles_validator_spec.rb
@@ -51,9 +51,9 @@ RSpec.describe SV do
         ]
 
         # Scrambles are shared between InboxResult and Result
-        create_scramble_set(5, competition_id: competition1.id, event_id: "333oh")
-        create_scramble_set(5, competition_id: competition1.id, event_id: "333")
-        create_scramble_set(5, competition_id: competition2.id, event_id: "222")
+        create_scramble_set(5, competition: competition1, round: round_333oh, event_id: "333oh")
+        create_scramble_set(5, competition: competition1, event_id: "333")
+        create_scramble_set(5, competition: competition2, round: round_222, event_id: "222")
 
         validator_args.each do |arg|
           sv = SV.new.validate(**arg)
@@ -69,7 +69,7 @@ RSpec.describe SV do
           create(result_kind, :blind_mo3, competition: competition2, round: round_333bf)
         end
 
-        create_scramble_set(2, competition_id: competition2.id, event_id: "333bf")
+        create_scramble_set(2, competition: competition2, round: round_333bf, event_id: "333bf")
 
         expected_errors = [
           RV::ValidationError.new(SV::MISSING_SCRAMBLES_FOR_COMPETITION_ERROR,
@@ -97,9 +97,9 @@ RSpec.describe SV do
         end
 
         # Create three groups of scrambles:
-        create_scramble_set(5, competition_id: competition1.id, event_id: "333oh", group_id: "A")
-        create_scramble_set(5, competition_id: competition1.id, event_id: "333oh", group_id: "B")
-        create_scramble_set(5, competition_id: competition1.id, event_id: "333oh", group_id: "C")
+        create_scramble_set(5, competition: competition1, round: round_333oh, event_id: "333oh", group_id: "A")
+        create_scramble_set(5, competition: competition1, round: round_333oh, event_id: "333oh", group_id: "B")
+        create_scramble_set(5, competition: competition1, round: round_333oh, event_id: "333oh", group_id: "C")
 
         expected_errors = [
           RV::ValidationError.new(SV::WRONG_NUMBER_OF_SCRAMBLE_SETS_ERROR,
@@ -123,8 +123,8 @@ RSpec.describe SV do
         end
 
         # Create two groups in fmc:
-        create_scramble_set(3, competition_id: competition3.id, event_id: "333fm", group_id: "A")
-        create_scramble_set(3, competition_id: competition3.id, event_id: "333fm", group_id: "B")
+        create_scramble_set(3, competition: competition3, round: round, event_id: "333fm", group_id: "A")
+        create_scramble_set(3, competition: competition3, round: round, event_id: "333fm", group_id: "B")
 
         expected_warnings = [
           RV::ValidationWarning.new(SV::MULTIPLE_FMC_GROUPS_WARNING,
@@ -151,13 +151,13 @@ RSpec.describe SV do
 
         # Create two groups in multi: for attempt 1 they did 2 groups,
         # for the others they just did one.
-        create_scramble_set(3, competition_id: competition1.id, event_id: "333mbf", group_id: "A")
-        create_scramble_set(1, competition_id: competition1.id, event_id: "333mbf", group_id: "B")
+        create_scramble_set(3, competition: competition1, round: round_333mbf_1, event_id: "333mbf", group_id: "A")
+        create_scramble_set(1, competition: competition1, round: round_333mbf_1, event_id: "333mbf", group_id: "B")
 
         # Now for competition2, both groups have only two scrambles but the format
         # is bo3, so the round is missing scrambles.
-        create_scramble_set(2, competition_id: competition2.id, event_id: "333mbf", group_id: "A")
-        create_scramble_set(2, competition_id: competition2.id, event_id: "333mbf", group_id: "B")
+        create_scramble_set(2, competition: competition2, round: round_333mbf_2, event_id: "333mbf", group_id: "A")
+        create_scramble_set(2, competition: competition2, round: round_333mbf_2, event_id: "333mbf", group_id: "B")
 
         expected_errors = [
           RV::ValidationError.new(SV::MISSING_SCRAMBLES_FOR_MULTI_ERROR,
