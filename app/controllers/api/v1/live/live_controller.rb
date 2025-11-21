15ee6d4568a546d881b273e5070feb6b6267e003
FinnIckler <finnickler@gmail.com>
2025-11-21 10:09:22 -0500

    fix after merge

--- app/controllers/api/v1/live/live_controller.rb
+++ app/controllers/api/v1/live/live_controller.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class Api::V1::Live::LiveController < Api::V1::ApiController
-  skip_before_action :require_user, only: %i[round_results, by_person]
+  skip_before_action :require_user, only: %i[round_results by_person]
   def round_results
     round_id = params.require(:round_id)
 
     round = Round.includes(live_results: %i[live_attempts round event]).find(round_id)
 
     render json: round.to_live_json
+  end
 
   def by_person
     registration_id = params.require(:registration_id)
