5945fad7ab494a9d40465ee649be7bd8bf892fdf
FinnIckler <finnickler@gmail.com>
2025-11-25 13:41:27 +0100

    use Status indicator and chakra animation

--- next-frontend/src/components/live/ConnectionPulse.tsx
+++ next-frontend/src/components/live/ConnectionPulse.tsx
@@ -1,39 +1,31 @@
+"use client";
+
 import {
   CONNECTION_COLORS,
-  CONNECTION_STATE_CONNECTED,
   ConnectionState,
 } from "@/lib/hooks/useResultsSubscription";
-import usePerpetualState from "@/lib/hooks/usePerpetualState";
-import { Box } from "@chakra-ui/react";
-const PULSE_DURATION = 2000;
+import { Status } from "@chakra-ui/react";
+import { useT } from "@/lib/i18n/useI18n";
 
 export default function ConnectionPulse({
   connectionState,
-  animationDuration = PULSE_DURATION,
 }: {
   connectionState: ConnectionState;
   animationDuration?: number;
 }) {
-  const animationPulse = usePerpetualState(
-    (prev) => !prev,
-    animationDuration * 1.5,
-  );
+  const { t } = useT();
 
   const connectionColor = CONNECTION_COLORS[connectionState];
 
-  const isConnected = connectionState === CONNECTION_STATE_CONNECTED;
-
-  const shouldShow = !isConnected || animationPulse;
-
   return (
-    <Box
-      w="10px"
-      h="10px"
-      borderRadius="full"
-      bg={connectionColor}
-      opacity={shouldShow ? 1 : 0}
-      transitionDuration={`${animationDuration}ms`}
-      transition="opacity"
-    />
+    <Status.Root colorPalette={connectionColor}>
+      <Status.Indicator
+        animationName="ping"
+        animationDuration="1s"
+        animationTimingFunction="cubic-bezier(0, 0, 0.2, 1)"
+        animationIterationCount="infinite"
+      />
+      {t(`competitions.live.connection.${connectionState}`)}
+    </Status.Root>
   );
 }
