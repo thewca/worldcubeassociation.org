7bb1ecc3003c304500b56b73a4ec542b960de5bf
FinnIckler <finnickler@gmail.com>
2025-11-25 17:44:13 +0100

    basic code cleanup

--- next-frontend/src/app/(wca)/persons/[wcaId]/page.tsx
+++ next-frontend/src/app/(wca)/persons/[wcaId]/page.tsx
@@ -12,6 +12,9 @@ import MapTab from "@/components/persons/MapTab";
 import ChampionshipPodiumsTab from "@/components/persons/ChampionshipPodiums";
 import type { components } from "@/types/openapi";
 import { StaffColor } from "@/components/RoleBadge";
+import { formatAttemptResult } from "@/lib/wca/wcif/attempts";
+import events from "@/lib/wca/data/events";
+import { getT } from "@/lib/i18n/get18n";
 
 export default async function PersonOverview({
   params,
@@ -20,6 +23,7 @@ export default async function PersonOverview({
 }) {
   const { wcaId } = await params;
   const { data: personDetails, error } = await getResultsByPerson(wcaId);
+  const { t } = await getT();
 
   if (error) {
     return <Text>Error fetching person</Text>;
@@ -29,20 +33,11 @@ export default async function PersonOverview({
     return <Text>Person does not exist</Text>;
   }
 
-  let genderText = "Male";
-  if (personDetails.person.gender == "f") {
-    genderText = "Female";
-  } else if (personDetails.person.gender == "o") {
-    genderText = "o";
-  }
-
   const roles: {
     teamRole: string;
     teamText: string;
     staffColor: StaffColor;
-  }[] = [];
-
-  personDetails.person.teams.forEach(
+  }[] = personDetails.person.teams.map(
     (team: {
       friendly_id: string;
       leader: boolean;
@@ -54,11 +49,11 @@ export default async function PersonOverview({
 
       if (teamText == "BOARD") {
         staffColour = "black";
-      } else if (team.leader == true) {
+      } else if (team.leader) {
         teamRole = "LEADER";
         staffColour = "blue";
       } else {
-        if (team.senior_member == true) {
+        if (team.senior_member) {
           teamRole = "SENIOR MEMBER";
           staffColour = "yellow";
         } else {
@@ -67,11 +62,11 @@ export default async function PersonOverview({
         }
       }
 
-      roles.push({
+      return {
         teamRole: teamRole,
         teamText: teamText,
         staffColor: staffColour,
-      });
+      };
     },
   );
 
@@ -105,99 +100,17 @@ export default async function PersonOverview({
       components["schemas"]["SingleAndAverageRank"]
     >,
   ): RecordItem[] => {
-    const eventOrder = [
-      "333",
-      "222",
-      "444",
-      "555",
-      "666",
-      "777",
-      "333bf",
-      "333fm",
-      "333oh",
-      "clock",
-      "minx",
-      "pyram",
-      "skewb",
-      "sq1",
-      "444bf",
-      "555bf",
-      "333mbf",
-      "magic",
-      "mmagic",
-      "333mbo",
-    ];
-
-    // Helper function to decode 333mbf results
-    const decode333mbf = (result: number): string => {
-      const resultStr = result.toString();
-      const isOldFormat = resultStr.startsWith("1");
-
-      if (isOldFormat) {
-        const SS = parseInt(resultStr.slice(1, 3));
-        const AA = parseInt(resultStr.slice(3, 5));
-        const TTTTT = parseInt(resultStr.slice(5));
-
-        const solved = 99 - SS;
-        const attempted = AA;
-        const timeInSeconds = TTTTT === 99999 ? "Unknown" : secToMin(TTTTT);
-
-        return `${solved}/${attempted} ${timeInSeconds}`;
-      } else {
-        const DD = parseInt(resultStr.slice(0, 2));
-        const TTTTT = parseInt(resultStr.slice(2, 7));
-        const MM = parseInt(resultStr.slice(7, 9));
-
-        const difference = 99 - DD;
-        const missed = MM;
-        const solved = difference + missed;
-        const attempted = solved + missed;
-        const timeInSeconds = TTTTT === 99999 ? "Unknown" : secToMin(TTTTT);
-
-        return `${solved}/${attempted} ${timeInSeconds}`;
-      }
-    };
-
-    // Helper function to format results (including 333fm averages)
-    const formatResult = (event: string, result: number): string => {
-      if (event === "333fm") {
-        if (result <= 99) {
-          // For single results (number of moves)
-          return result.toString();
-        }
-      }
-
-      if (result > 5999) {
-        const minutes = Math.floor(result / 6000);
-        const seconds = ((result % 6000) / 100).toFixed(2);
-        return `${minutes}:${seconds.padStart(5, "0")}`; // Ensures two decimal places
-      }
-      return (result / 100).toFixed(2); // Converts centiseconds to seconds
-    };
-
-    // Helper function to convert seconds to mm:ss format
-    const secToMin = (seconds: number): string => {
-      const minutes = Math.floor(seconds / 60);
-      const remainingSeconds = seconds % 60;
-      const paddedSeconds = remainingSeconds.toString().padStart(2, "0");
-
-      return `${minutes}:${paddedSeconds}`;
-    };
-
     // Transform the personalRecords object into an array
     const recordsArray = Object.entries(personalRecords).map(
       ([event, record]) => ({
         event,
-        single:
-          event === "333mbf"
-            ? decode333mbf(record.single.best)
-            : formatResult(event, record.single.best),
+        single: formatAttemptResult(record.single.best, event),
         snr: record.single.country_rank,
         scr: record.single.continent_rank,
         swr: record.single.world_rank,
         average:
           record.average && record.average.best > 0
-            ? formatResult(event, record.average.best)
+            ? formatAttemptResult(record.average.best, event)
             : "",
         anr: record.average?.country_rank || 0,
         acr: record.average?.continent_rank || 0,
@@ -206,33 +119,23 @@ export default async function PersonOverview({
     );
 
     // Reorder the array based on eventOrder
-    return eventOrder
-      .map((event) => recordsArray.find((record) => record.event === event))
+    return events.official
+      .map((event) => recordsArray.find((record) => record.event === event.id))
       .filter((record) => !!record); // Remove undefined items
   };
 
-  let hasRecords = false;
-  let hasMedals = false;
-  if (
+  const hasRecords =
     personDetails.records.national > 0 ||
     personDetails.records.continental > 0 ||
-    personDetails.records.world > 0
-  ) {
-    hasRecords = true;
-  }
-
-  if (
+    personDetails.records.world > 0;
+  const hasMedals =
     personDetails.medals.gold > 0 ||
     personDetails.medals.silver > 0 ||
-    personDetails.medals.bronze > 0
-  ) {
-    hasMedals = true;
-  }
+    personDetails.medals.bronze > 0;
 
   return (
     <Container centerContent maxW="1800px">
       {/* Profile Section */}
-      {/* TODO SLATE - stick the bottom of this Profile card to the bottom of the page */}
       <SimpleGrid gap={8} columns={24} padding={5}>
         <GridItem colSpan={7} h="80lvh" position="sticky" top="0px" pt="20px">
           <ProfileCard
@@ -240,7 +143,7 @@ export default async function PersonOverview({
             profilePicture={personDetails.person.avatar.url}
             roles={roles}
             wcaId={wcaId}
-            gender={genderText}
+            gender={t("enums.user.gender")}
             regionIso2={personDetails.person.country_iso2}
             competitions={personDetails.competition_count}
             completedSolves={1659}
