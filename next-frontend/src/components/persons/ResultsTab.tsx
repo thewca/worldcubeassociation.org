97ce6aa25e43c29c936ff38e8055a70d04960907
FinnIckler <finnickler@gmail.com>
2025-12-01 10:11:07 +0100

    add eventselector

--- next-frontend/src/components/persons/ResultsTab.tsx
+++ next-frontend/src/components/persons/ResultsTab.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React, { useState } from "react";
-import { Text } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
 import useAPI from "@/lib/wca/useAPI";
 import { ByCompetitionTable } from "@/components/results/ResultsTable";
 import { useT } from "@/lib/i18n/useI18n";
+import EventSelector from "@/components/EventSelector";
 
 interface ResultsTabProps {
   wcaId: string;
@@ -13,7 +14,7 @@ const ResultsTab: React.FC<ResultsTabProps> = ({ wcaId }) => {
   const api = useAPI();
   const { t } = useT();
 
-  const [eventId] = useState("333");
+  const [eventId, setEventId] = useState("333");
 
   const { data: resultsQuery, isLoading } = api.useQuery(
     "get",
@@ -31,7 +32,18 @@ const ResultsTab: React.FC<ResultsTabProps> = ({ wcaId }) => {
     return <Text>Failed fetching results</Text>;
   }
 
-  return <ByCompetitionTable results={resultsQuery} t={t} />;
+  return (
+    <VStack>
+      <EventSelector
+        title=""
+        selectedEvents={[eventId]}
+        onEventClick={(e) => setEventId(e)}
+        hideAllButton={true}
+        hideClearButton={true}
+      />
+      <ByCompetitionTable results={resultsQuery} t={t} />
+    </VStack>
+  );
 };
 
 export default ResultsTab;
