3003c39fda4e9d41e777d5a72c054dc4e2437b8e
FinnIckler <finnickler@gmail.com>
2025-12-05 17:03:04 +0100

    put navigation arrows next to avatars

--- next-frontend/src/components/competitions/OrganizerCard.tsx
+++ next-frontend/src/components/competitions/OrganizerCard.tsx
@@ -15,13 +15,11 @@ import { MarkdownProse } from "@/components/Markdown";
 import WcaDocsIcon from "@/components/icons/WcaDocsIcon";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 
-function OrganizerCarousel({
-  organizers,
-}: {
-  organizers:
-    | components["schemas"]["Person"][]
-    | components["schemas"]["Organizer"][];
-}) {
+type OrganizerPerson =
+  | components["schemas"]["Person"]
+  | components["schemas"]["Organizer"];
+
+function OrganizerCarousel({ organizers }: { organizers: OrganizerPerson[] }) {
   return (
     <Carousel.Root
       orientation="horizontal"
@@ -29,32 +27,32 @@ function OrganizerCarousel({
       autoSize
       spacing="30px"
       maxW="full"
+      allowMouseDrag
     >
-      <Carousel.ItemGroup>
-        {organizers.map((delegate, index) => (
-          <Carousel.Item
-            key={index}
-            index={index}
-            snapAlign="center"
-            width="auto"
-          >
-            <HStack>
-              <Avatar.Root>
-                <Avatar.Fallback name={delegate.name} />
-                <Avatar.Image src={delegate.avatar.thumb_url} />
-              </Avatar.Root>
-              <ChakraLink href={delegate.url}>{delegate.name}</ChakraLink>
-            </HStack>
-          </Carousel.Item>
-        ))}
-      </Carousel.ItemGroup>
       <Carousel.Control justifyContent="start" gap="4" width="full">
         <Carousel.PrevTrigger asChild>
           <IconButton size="xs" variant="ghost">
             <LuChevronLeft />
           </IconButton>
         </Carousel.PrevTrigger>
-
+        <Carousel.ItemGroup>
+          {organizers.map((delegate, index) => (
+            <Carousel.Item
+              key={index}
+              index={index}
+              snapAlign="center"
+              width="auto"
+            >
+              <HStack>
+                <Avatar.Root>
+                  <Avatar.Fallback name={delegate.name} />
+                  <Avatar.Image src={delegate.avatar.thumb_url} />
+                </Avatar.Root>
+                <ChakraLink href={delegate.url}>{delegate.name}</ChakraLink>
+              </HStack>
+            </Carousel.Item>
+          ))}
+        </Carousel.ItemGroup>
         <Carousel.NextTrigger asChild>
           <IconButton size="xs" variant="ghost">
             <LuChevronRight />
