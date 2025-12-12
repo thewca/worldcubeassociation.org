7ca90731bf87647294537c12526560561ee85555
Duncan <duncanonthejob@gmail.com>
2025-12-12 10:07:08 +0200

    base url change, upgrade playwright, clean up system-tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.PLAYWRIGHT_BASE_URL,
+    baseURL: process.env.SYSTEM_TEST_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
