92d5c4617822adf60d44a9495f12117c1421086b
Duncan <52967253+dunkOnIT@users.noreply.github.com>
2025-12-15 13:57:25 +0200

    v2.0.0 Results Export (#12829)
    * edit dumpers and add v2 results readme
    
    * add a v2 results export
    
    * rubocop
    
    * generalize export and limit to 100 rows
    
    * updated results export page and ported existing tests to v2
    
    * added functino and tests for results_export_live_versions, use in rake task
    
    * remove accidentally added files
    
    * rubocop
    
    * tests for conditional rendering and updated result_export API endpoint
    
    * rubocop, use current_version_key instead of :v2
    
    * basic diff review
    
    * correct argument name
    
    * correct indentation
    
    * remove blank line in readme
    
    * change table naming, readme and remove value1-5
    
    * removed routes helpers
    
    * removed lingering camelCase instances'
    
    * minor text fixes
    
    * date constant and move fake_values to copy
    
    * adding test for result export
    
    * trying to troubleshoot export
    
    * Roll back copy that isn't actually a copy
    
    * Read foreign keys in the moment when they're actually well-defined
    
    * Do not treat columns as copy that aren't a copy
    
    * update changelog with renamed columns
    
    ---------
    
    Co-authored-by: Gregor Billing <gbilling@worldcubeassociation.org>

--- app/controllers/database_controller.rb
+++ app/controllers/database_controller.rb
@@ -1,23 +1,53 @@
 # frozen_string_literal: true
 
 class DatabaseController < ApplicationController
-  RESULTS_README_TEMPLATE = 'database/public_results_readme'
-
+  REGULATIONS_ENDDATE_2025 = "2025-12-31"
   def results_export
-    @sql_path, @sql_filesize = DbDumpHelper.cached_results_export_info("sql")
-    @tsv_path, @tsv_filesize = DbDumpHelper.cached_results_export_info("tsv")
+    flash[:warning] = I18n.t('database.results_export.deprecation_warning')
+    @export_version = DatabaseDumper.current_results_export_version
+    @sql_path, @sql_filesize = DbDumpHelper.cached_results_export_info("sql", :v2)
+    @tsv_path, @tsv_filesize = DbDumpHelper.cached_results_export_info("tsv", :v2)
 
     @sql_filename = File.basename(@sql_path)
     @tsv_filename = File.basename(@tsv_path)
   end
 
   def sql_permalink
-    url, = DbDumpHelper.cached_results_export_info("sql")
+    if Date.today > Date.parse(REGULATIONS_ENDDATE_2025)
+      return render json: {
+        error: "gone",
+        message: "v1 of the Results Export has been deprecated. Please update to v2 by referring to the README and links at: https://www.worldcubeassociation.org/export/results",
+      }, status: :gone
+    end
+
+    url, = DbDumpHelper.cached_results_export_info("sql", :v1)
     redirect_to url, status: :moved_permanently, allow_other_host: true
   end
 
   def tsv_permalink
-    url, = DbDumpHelper.cached_results_export_info("tsv")
+    if Date.today > Date.parse(REGULATIONS_ENDDATE_2025)
+      return render json: {
+        error: "gone",
+        message: "v1 of the Results Export has been deprecated. Please update to v2 by referring to the README and links at: https://www.worldcubeassociation.org/export/results",
+      }, status: :gone
+    end
+
+    url, = DbDumpHelper.cached_results_export_info("tsv", :v1)
+    redirect_to url, status: :moved_permanently, allow_other_host: true
+  end
+
+  def results_permalink
+    version = params.require(:version).to_sym
+    file_type = params.require(:file_type)
+
+    if Date.today > Date.parse(REGULATIONS_ENDDATE_2025) && version == :v1
+      return render json: {
+        error: "gone",
+        message: "v1 of the Results Export has been deprecated. Please update to v2 by referring to the README and links at: https://www.worldcubeassociation.org/export/results",
+      }, status: :gone
+    end
+
+    url, = DbDumpHelper.cached_results_export_info(file_type, version)
     redirect_to url, status: :moved_permanently, allow_other_host: true
   end
 
@@ -25,13 +55,14 @@ class DatabaseController < ApplicationController
     @rel_download_path = DbDumpHelper.public_s3_path(DbDumpHelper::DEVELOPER_EXPORT_SQL_PERMALINK)
   end
 
-  def self.render_readme(rendering_engine, export_timestamp)
-    locals = { long_date: export_timestamp, export_version: DatabaseDumper::PUBLIC_RESULTS_VERSION }
+  def self.render_readme(rendering_engine, export_timestamp, version)
+    locals = { long_date: export_timestamp, export_version: DatabaseDumper::RESULTS_EXPORT_VERSIONS[version][:version_number] }
 
+    partial_filename = "database/#{version}_public_results_readme"
     if rendering_engine.respond_to?(:render_to_string)
-      rendering_engine.render_to_string(partial: RESULTS_README_TEMPLATE, formats: :md, locals: locals)
+      rendering_engine.render_to_string(partial: partial_filename, formats: :md, locals: locals)
     else
-      rendering_engine.render(partial: RESULTS_README_TEMPLATE, formats: :md, locals: locals)
+      rendering_engine.render(partial: partial_filename, formats: :md, locals: locals)
     end
   end
 end
