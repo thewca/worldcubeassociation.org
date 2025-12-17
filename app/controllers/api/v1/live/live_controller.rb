b5e90a6a9175b37618b7c1ac9cd5317a1206d3d3
FinnIckler <finnickler@gmail.com>
2025-12-17 11:44:37 +0100

    Add podium route

--- app/controllers/api/v1/live/live_controller.rb
+++ app/controllers/api/v1/live/live_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Live::LiveController < Api::V1::ApiController
-  skip_before_action :require_user, only: %i[round_results by_person]
+  skip_before_action :require_user, only: %i[round_results by_person podiums]
   def round_results
     round_id = params.require(:round_id)
 
@@ -21,5 +21,12 @@ class Api::V1::Live::LiveController < Api::V1::ApiController
     user_wcif["results"] = results
 
     render json: user_wcif
+  end
+
+  def podiums
+    competition = Competition.find(params.require(:competition_id))
+    final_rounds = competition.rounds.includes(live_results: %i[live_attempts round event]).select(&:final_round?)
+
+    render json: final_rounds.map { |r| r.to_live_json(only_podiums: true)}
   end
 end
