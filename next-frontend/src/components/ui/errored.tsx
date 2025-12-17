db73ce8fe4d04122e603131b2ade9fd14895da73
FinnIckler <finnickler@gmail.com>
2025-12-17 19:07:36 +0100

    new error page

--- next-frontend/src/components/ui/errored.tsx
+++ next-frontend/src/components/ui/errored.tsx
@@ -1,12 +1,40 @@
-import { Alert } from "@chakra-ui/react";
+import { Accordion, Alert, CodeBlock } from "@chakra-ui/react";
+import { TFunction } from "i18next";
 
-export default function Errored({ error }: { error: string }) {
+export default function Errored({
+  error,
+  t,
+  request,
+}: {
+  error: string;
+  request?: Request;
+  t: TFunction;
+}) {
   return (
     <Alert.Root>
       <Alert.Indicator />
       <Alert.Content>
-        <Alert.Title>Error fetching Data</Alert.Title>
-        <Alert.Description>{error}</Alert.Description>
+        <Alert.Title>{t("errors.next_frontend.title")}</Alert.Title>
+        <Alert.Description>
+          {t("errors.next_frontend.description", { error })}
+          <Accordion.Root collapsible>
+            <Accordion.ItemTrigger>
+              <Accordion.Item value="details">
+                {t("errors.next_frontend.details")}
+              </Accordion.Item>
+              <Accordion.ItemIndicator />
+            </Accordion.ItemTrigger>
+            <Accordion.ItemContent>
+              <Accordion.ItemBody>
+                <CodeBlock.Root code={JSON.stringify(request)} language="json">
+                  <CodeBlock.Code>
+                    <CodeBlock.CodeText />
+                  </CodeBlock.Code>
+                </CodeBlock.Root>
+              </Accordion.ItemBody>
+            </Accordion.ItemContent>
+          </Accordion.Root>
+        </Alert.Description>
       </Alert.Content>
     </Alert.Root>
   );
