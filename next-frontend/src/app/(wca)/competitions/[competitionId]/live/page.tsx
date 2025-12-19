6c85488526fe8f92f64f8430071bfe54d1c5465e
FinnIckler <finnickler@gmail.com>
2025-12-19 16:40:17 +0100

    Add Live Home Page

--- next-frontend/src/app/(wca)/competitions/[competitionId]/live/page.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/live/page.tsx
@@ -1,5 +1,9 @@
 import { getSchedule } from "@/lib/wca/competitions/getSchedule";
 import Errored from "@/components/ui/errored";
+import { earliestWithLongestTieBreaker } from "@/lib/wca/wcif/activities";
+import LiveView from "@/components/competitions/Schedule/LiveView";
+import { getEvents } from "@/lib/wca/competitions/wcif/getEvents";
+import { getT } from "@/lib/i18n/get18n";
 
 export default async function LiveOverview({
   params,
@@ -7,12 +11,37 @@ export default async function LiveOverview({
   params: Promise<{ competitionId: string }>;
 }) {
   const { competitionId } = await params;
+  const { t } = await getT();
 
-  const { error, data: schedule } = await getSchedule(competitionId);
+  const { error, data: wcifSchedule } = await getSchedule(competitionId);
 
   if (error) {
-    return <Errored error="Error fetching Schedule" />;
+    return <Errored error={`${error.data.id} not found`} />;
   }
 
-  return schedule.;
+  const { error: wcifEventsError, data: wcifEvents } =
+    await getEvents(competitionId);
+
+  if (wcifEventsError) {
+    return <Errored error={`${wcifEventsError.data.id} not found`} />;
+  }
+
+  const allActivitiesSorted = wcifSchedule.venues
+    .flatMap((venue) => venue.rooms)
+    .flatMap((room) => room.activities)
+    .toSorted(earliestWithLongestTieBreaker);
+
+  const uniqueTimeZones = [
+    ...new Set(wcifSchedule.venues.map((venue) => venue.timezone)),
+  ];
+
+  return (
+    <LiveView
+      competitionId={competitionId}
+      activities={allActivitiesSorted}
+      timeZone={uniqueTimeZones[0]}
+      wcifEvents={wcifEvents}
+      t={t}
+    />
+  );
 }
