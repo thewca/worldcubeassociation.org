53aa5b28e7560ccc723e035db61fdfb6d544ec24
FinnIckler <finnickler@gmail.com>
2026-01-05 12:29:51 +0100

    remove unnecessary code

--- app/models/sanity_check.rb
+++ app/models/sanity_check.rb
@@ -5,10 +5,6 @@ class SanityCheck < ApplicationRecord
 
   belongs_to :sanity_check_category
 
-  def self.data_file_handle
-    self.name.pluralize.underscore.to_s
-  end
-
   # Overwrite method to handle .sql files
   def self.all_raw_sanitized
     column_symbols = column_names.map(&:to_sym)
@@ -16,7 +12,6 @@ class SanityCheck < ApplicationRecord
     all_raw.map do |attributes|
       attrs = attributes.symbolize_keys
       attrs[:query] = Rails.root.join("lib", "sanity_check_sql", attrs[:query_file]).read
-      delete attrs[:query_file]
 
       attrs.slice(*column_symbols)
     end
--- app/models/sanity_check_category.rb
+++ app/models/sanity_check_category.rb
@@ -4,8 +4,4 @@ class SanityCheckCategory < ApplicationRecord
   include StaticData
 
   has_many :sanity_checks
-
-  def self.data_file_handle
-    self.name.pluralize.underscore.to_s
-  end
 end
