2c1c83060afa3a9e25688d1294e95912b31d3e67
FinnIckler <finnickler@gmail.com>
2026-01-05 16:27:06 +0100

    add extra test

--- spec/models/sanity_check_spec.rb
+++ spec/models/sanity_check_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe SanityCheck do
     it "Can read all files" do
       SanityCheck.find_each do |sanity_check|
         expect { sanity_check.query }.not_to raise_error
+        expect(sanity_check.query.presence).not_to be_nil
       end
     end
   end
