7409125ba4617562c6a11e1bcb31a19b05b49a51
Gregor Billing <gbilling@worldcubeassociation.org>
2026-01-05 19:18:04 +0100

    Add EXPLAIN tests to make sure every query is syntactically correct

--- spec/models/sanity_check_spec.rb
+++ spec/models/sanity_check_spec.rb
@@ -4,10 +4,23 @@ require 'rails_helper'
 
 RSpec.describe SanityCheck do
   context "SQL Files" do
-    it "Can read all files" do
+    it "can read all files" do
       SanityCheck.find_each do |sanity_check|
         expect { sanity_check.query }.not_to raise_error
         expect(sanity_check.query.presence).not_to be_nil
+      end
+    end
+
+    it "can (potentially) execute all queries" do
+      SanityCheck.find_each do |sanity_check|
+        original_query = sanity_check.query
+        # We prefix an `EXPLAIN` to skip actually executing the query,
+        #   but still force the SQL server to _fully parse_ it and see whether it contains syntactic mistakes.
+        # This is a "smoke test" to prevent deprecated `SELECT * FROM Results` (note the uppercase R)
+        #   from slipping through. The actual data / semantics of the queries are tested elsewhere.
+        explain_query = "EXPLAIN #{original_query}"
+
+        expect { ActiveRecord::Base.connection.execute explain_query }.not_to raise_error
       end
     end
   end
