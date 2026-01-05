2bdc550c8c571ac87d4b8ae4c008446298091a2c
FinnIckler <finnickler@gmail.com>
2026-01-05 15:27:06 +0100

    rubocop

--- app/mailers/sanity_check_mailer.rb
+++ app/mailers/sanity_check_mailer.rb
@@ -3,7 +3,7 @@
 class SanityCheckMailer < ApplicationMailer
   def notify_of_sanity_check_results(email_to)
     results = SanityCheckCategory.where(email_to: email_to).flat_map(:latest_results)
-    @non_empty_results = results.filter { |r| !r.results_without_exclusions.empty?}
+    @non_empty_results = results.filter { |r| !r.results_without_exclusions.empty? }
 
     start_of_month = Time.now.change(day: 1)
 
@@ -13,6 +13,6 @@ class SanityCheckMailer < ApplicationMailer
       to: email_to,
       cc: "software@worldcubeassociation.org",
       subject: "Sanity Check #{month_name} #{year_name}",
-      )
+    )
   end
 end
