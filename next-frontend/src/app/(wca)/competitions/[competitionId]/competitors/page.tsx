cef9c3dde718f6543a0a3b5bdd1929029bd6b694
FinnIckler <finnickler@gmail.com>
2026-01-05 20:23:53 +0100

    review changes

--- next-frontend/src/app/(wca)/competitions/[competitionId]/competitors/page.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/competitors/page.tsx
@@ -1,6 +1,8 @@
 import TabCompetitors from "@/components/competitions/TabCompetitors";
 import { getCompetitionInfo } from "@/lib/wca/competitions/getCompetitionInfo";
 import { hasPassed } from "@/lib/wca/dates";
+import OpenapiError from "@/components/ui/openapiError";
+import { getT } from "@/lib/i18n/get18n";
 
 export default async function Competitors({
   params,
@@ -8,11 +10,17 @@ export default async function Competitors({
   params: Promise<{ competitionId: string }>;
 }) {
   const { competitionId } = await params;
+  const { t } = await getT();
 
-  const { data: competitionInfo, error } =
-    await getCompetitionInfo(competitionId);
+  const {
+    data: competitionInfo,
+    error,
+    response,
+  } = await getCompetitionInfo(competitionId);
 
-  if (error || !competitionInfo) return { title: "Competition Not Found" };
+  if (error) {
+    return <OpenapiError response={response} t={t} />;
+  }
 
   return (
     <TabCompetitors
