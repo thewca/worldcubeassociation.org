2b8c7d73ddc1cd40d28e672d9a7daa3a675746b3
FinnIckler <finnickler@gmail.com>
2026-01-06 18:50:32 +0100

    rubocop

--- app/jobs/all_sanity_checks_job.rb
+++ app/jobs/all_sanity_checks_job.rb
@@ -4,8 +4,8 @@ class AllSanityChecksJob < WcaCronjob
   def perform
     grouped_by_email = SanityCheckCategory.all.group_by(&:email_to)
 
-    grouped_by_email.each do | email_to, sanity_checks |
-      sanity_checks.each do | sanity_check_category |
+    grouped_by_email.each do |email_to, sanity_checks|
+      sanity_checks.each do |sanity_check_category|
         SanityCheckCategoryJob.perform_now(sanity_check_category)
       end
       SanityCheckResultsJob.perform_now(email_to)
