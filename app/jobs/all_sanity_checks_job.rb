b58b5f2ce403dfd8eaac2904f8bda1aac0d934e4
FinnIckler <finnickler@gmail.com>
2026-01-06 15:46:10 +0100

    use distinct in sending emails

--- app/jobs/all_sanity_checks_job.rb
+++ app/jobs/all_sanity_checks_job.rb
@@ -6,7 +6,7 @@ class AllSanityChecksJob < WcaCronjob
       SanityCheckCategoryJob.perform_later(sanity_check_category)
     end
     # Enqueue Results job after, these are run on a single thread one after another
-    SanityCheckCategory.select(:email_to).uniq.each do |email_to|
+    SanityCheckCategory.distinct.pluck(:email_to).each do |email_to|
       SanityCheckResultsJob.perform_later(email_to)
     end
   end
