1ea9f337236ce107f562d81eb8754045909e8c95
FinnIckler <finnickler@gmail.com>
2026-01-07 11:01:48 +0100

    Test Names with numbers or non-desired special characters

--- spec/models/sanity_check_spec.rb
+++ spec/models/sanity_check_spec.rb
@@ -36,4 +36,58 @@ RSpec.describe SanityCheck do
       expect(stored_queries).to match_array(used_queries)
     end
   end
+
+  context "Person Data Irregularities" do
+    context "Names with numbers or non-desired special characters" do
+      let(:query) { SanityCheck.find(1).query }
+
+      def run_query
+        ActiveRecord::Base.connection.exec_query(query)
+      end
+
+      it "correctly finds all irregular names" do
+        irregular_people = [
+          create(:person, name: "0"),
+          create(:person, name: "John1"),
+          create(:person, name: "Jane_Doe"),
+          create(:person, name: "Alice@Wonderland"),
+          create(:person, name: "Bob#Builder"),
+          create(:person, name: "Back`Tick"),
+          create(:person, name: "Cash$Money"),
+          create(:person, name: "Caret^Top"),
+          create(:person, name: "Amp&Sand"),
+          create(:person, name: "Pipe|Name"),
+          create(:person, name: "Brace{Name}"),
+          create(:person, name: "Bracket[Name]"),
+          create(:person, name: "Plus+Minus"),
+          create(:person, name: "Equals=Name"),
+          create(:person, name: "Question?Mark"),
+          create(:person, name: "Greater>Less<"),
+          create(:person, name: "Comma,Name"),
+          create(:person, name: "Tilde~Name"),
+          create(:person, name: %q(Quote"Name)),
+          create(:person, name: "Back\\Slash")
+        ]
+
+        result_ids = run_query.map { |r| r["id"] }
+
+        expect(result_ids).to match_array(irregular_people.map(&:id))
+      end
+
+      it "does not flag valid names" do
+        valid_people = [
+          create(:person, name: "John"),
+          create(:person, name: "Jane Doe"),
+          create(:person, name: "Mary-Jane"),
+          create(:person, name: "Jean Luc"),
+          create(:person, name: "OConnor"),
+          create(:person, name: "Anne-Marie")
+        ]
+
+        result_ids = run_query.to_a
+
+        expect(result_ids & valid_people.map(&:id)).to be_empty
+      end
+    end
+  end
 end
