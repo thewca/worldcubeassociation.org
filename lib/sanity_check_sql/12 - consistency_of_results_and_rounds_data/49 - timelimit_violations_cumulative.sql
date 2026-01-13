8cb0da93990758b591436a488d8f9cd52ae48017
FinnIckler <finnickler@gmail.com>
2026-01-13 17:03:51 +0100

    Cumulative WIP

--- lib/sanity_check_sql/12 - consistency_of_results_and_rounds_data/49 - timelimit_violations_cumulative.sql
+++ lib/sanity_check_sql/12 - consistency_of_results_and_rounds_data/49 - timelimit_violations_cumulative.sql
@@ -1,15 +1,35 @@
-SELECT GROUP_CONCAT(ro.id), COUNT(*) AS noOfRounds, SUM(IF(value1<0,0,value1)+IF(value2<0,0,value2)+IF(value3<0,0,value3)+IF(value4<0,0,value4)+IF(value5<0,0,value5)) AS sumOfSolves,
-       REVERSE(SUBSTRING_INDEX(REVERSE(SUBSTRING_INDEX(time_limit, ',', 1)), ':', 1)) AS timeLimit,
-       ce.competition_id, ro.time_limit, r.person_id, r.person_name, r.country_id
-FROM rounds ro
-       INNER JOIN competition_events ce ON ce.id = ro.competition_event_id
-       INNER JOIN (SELECT DISTINCT competition_id, event_id, round_type_id FROM results
-                   WHERE RIGHT(competition_id, 4) >= 2013) re
-                  ON re.competition_id = ce.competition_id AND re.event_id = ce.event_id AND
-                     (CASE ro.number WHEN ro.total_number_of_rounds THEN re.round_type_id IN ('c', 'f')
-                                     WHEN 0 THEN re.round_type_id IN ('0', 'b', 'h') WHEN 1 THEN re.round_type_id IN ('1', 'd')
-                                     WHEN 2 THEN re.round_type_id IN ('2', 'e') WHEN 3 THEN re.round_type_id IN ('3', 'g') END)
-       JOIN results r ON ce.competition_id=r.competition_id AND ce.event_id=r.event_id AND re.round_type_id=r.round_type_id
-WHERE time_limit IS NOT NULL AND time_limit LIKE '%[%","%]%'
-GROUP BY person_id, person_name, country_id, competition_id, time_limit
-HAVING sumOfSolves>=timeLimit
+WITH round_numbers AS (
+  SELECT
+    t0.*,
+    ROW_NUMBER() OVER (
+      PARTITION BY t0.competition_id, t0.event_id
+      ORDER BY rt.`rank`
+      ) AS round
+  FROM (
+         SELECT DISTINCT
+           r.competition_id,
+           r.event_id,
+           r.round_type_id
+         FROM results r
+       ) t0
+         JOIN round_types rt ON t0.round_type_id = rt.id
+)
+
+SELECT r.person_id, r.competition_id, JSON_EXTRACT(time_limit, '$.cumulativeRoundIds') AS rounds, r.round_type_id,
+       SUM(CASE WHEN value > 0 THEN value ELSE 0 END) AS total_time,
+       JSON_EXTRACT(time_limit, '$.centiseconds') AS time_limit
+FROM results r
+       JOIN result_attempts ra ON ra.result_id = r.id
+       JOIN competition_events ce
+            ON r.competition_id = ce.competition_id
+              AND r.event_id = ce.event_id
+       JOIN round_numbers rn
+            ON rn.competition_id = r.competition_id
+              AND rn.event_id = r.event_id
+              AND rn.round_type_id = r.round_type_id
+       JOIN rounds ro
+            ON ce.id = ro.competition_event_id
+              AND ro.number = rn.round
+WHERE JSON_LENGTH(JSON_EXTRACT(time_limit, '$.cumulativeRoundIds')) > 0 # only apply to rounds with a cumulative time limit
+GROUP BY r.competition_id, r.person_id, rounds
+HAVING total_time >= time_limit
