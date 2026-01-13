89eaed238a8a32a6f0b7011fd333ac014abbb7d7
FinnIckler <finnickler@gmail.com>
2026-01-13 11:57:29 +0100

    Replace value1..5 in sanity checks: Different Result Types

--- lib/sanity_check_sql/2 - irregular_results/15 - different_result_types.sql
+++ lib/sanity_check_sql/2 - irregular_results/15 - different_result_types.sql
@@ -1,12 +1,16 @@
-SELECT competition_id, event_id, round_type_id, COUNT(solves) as num_results
-FROM (SELECT DISTINCT competition_id,
-                      event_id,
-                      round_type_id,
-                      IF(value1 <> 0, 1, 0)
-                        + IF(value2 <> 0, 1, 0) + IF(value3 <> 0, 1, 0) + IF(value4 <> 0, 1, 0) +
-                      IF(value5 <> 0, 1, 0) as solves
-      FROM results
-      WHERE RIGHT(competition_id, 4) >= 2013) re
+WITH relevant_results AS (SELECT *
+                          FROM results
+                          WHERE RIGHT(competition_id, 4) >= 2013),
+
+     num_attempts_per_result AS (SELECT person_id, event_id, competition_id, round_type_id, COUNT(*) AS num_attempts
+                                 FROM relevant_results AS r
+                                        JOIN result_attempts ra ON ra.result_id = r.id
+                                 GROUP BY result_id)
+
+SELECT competition_id,
+       event_id,
+       round_type_id,
+       COUNT(DISTINCT num_attempts) AS different_result_types
+FROM num_attempts_per_result
 GROUP BY competition_id, event_id, round_type_id
-HAVING IF(round_type_id in ('c', 'd', 'e', 'g', 'h'), num_results > 2, num_results > 1)
-ORDER BY competition_id
+HAVING different_result_types > IF(round_type_id IN ('c', 'd', 'e', 'g', 'h'), 2, 1);
