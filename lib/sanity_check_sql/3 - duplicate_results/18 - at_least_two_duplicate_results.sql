1e78c2a3ba480ae728d10e28dccf5094e0d60ef5
FinnIckler <finnickler@gmail.com>
2026-01-14 21:45:56 +0100

    Replace value 1 5 sanity checks duplicate results (#13197)
    * Replace value1..5 in sanity checks: At least two duplicate results
    
    * add back the result_ids column for exclusions
    
    * use GROUP_CONCAT instead
    
    * manually change to 333mbo

--- lib/sanity_check_sql/3 - duplicate_results/18 - at_least_two_duplicate_results.sql
+++ lib/sanity_check_sql/3 - duplicate_results/18 - at_least_two_duplicate_results.sql
@@ -1,17 +1,19 @@
-SELECT value1,
-       value2,
-       value3,
-       value4,
-       value5,
+WITH attempt_sequences AS (SELECT result_id,
+                                  GROUP_CONCAT(value ORDER BY attempt_number) AS attempt_sequence,
+                                  MAX(value)                                  AS worst
+                           FROM result_attempts
+                           GROUP BY result_id
+                           HAVING worst > 0)
+
+SELECT attempt_sequence,
        count(*)                              as num_rows,
-       GROUP_CONCAT(id ORDER BY id)          as result_ids,
-       GROUP_CONCAT(person_id)               as people,
+       GROUP_CONCAT(t1.id ORDER BY t1.id)    as result_ids,
+       GROUP_CONCAT(distinct person_id)      as people,
        GROUP_CONCAT(distinct event_id)       as events,
        GROUP_CONCAT(distinct competition_id) as competitions
-FROM results
-WHERE event_id not in ('333mbo', '333fm')
-  AND
-  IF(value1 > 0, 1, 0) + IF(value2 > 0, 1, 0) + IF(value3 > 0, 1, 0) + IF(value4 > 0, 1, 0) + IF(value5 > 0, 1, 0) > 1
-GROUP BY value1, value2, value3, value4, value5
+FROM results t1
+       JOIN attempt_sequences t2 ON t2.result_id = t1.id
+WHERE t1.event_id NOT IN ('333mbo', '333fm')
+GROUP BY attempt_sequence
 HAVING num_rows > 1
-   AND count(distinct competition_id) = 1
+   AND COUNT(DISTINCT competition_id) = 1
