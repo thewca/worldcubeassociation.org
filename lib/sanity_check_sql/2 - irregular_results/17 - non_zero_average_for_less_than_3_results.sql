83d424902b7d8657a13d40e5cb310e5ca5a92306
FinnIckler <finnickler@gmail.com>
2026-01-14 16:58:01 +0100

    add extra test and fix query

--- lib/sanity_check_sql/2 - irregular_results/17 - non_zero_average_for_less_than_3_results.sql
+++ lib/sanity_check_sql/2 - irregular_results/17 - non_zero_average_for_less_than_3_results.sql
@@ -6,3 +6,4 @@ WITH target_result_ids AS (SELECT result_id, COUNT(*) AS solves
 SELECT r.id, r.person_id, r.competition_id, r.round_type_id, r.average, t2.solves
 FROM results r
        JOIN target_result_ids t2 ON t2.result_id = r.id
+WHERE average <> 0
