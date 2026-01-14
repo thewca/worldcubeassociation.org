9c89bd14bbdea3d95b71647ba879f5f5cd5f8f87
FinnIckler <finnickler@gmail.com>
2026-01-14 15:00:20 +0100

    review changes

--- lib/sanity_check_sql/2 - irregular_results/15 - different_result_types.sql
+++ lib/sanity_check_sql/2 - irregular_results/15 - different_result_types.sql
@@ -1,16 +1,23 @@
-WITH relevant_results AS (SELECT *
-                          FROM results
-                          WHERE RIGHT(competition_id, 4) >= 2013),
-
-     num_attempts_per_result AS (SELECT person_id, event_id, competition_id, round_type_id, COUNT(*) AS num_attempts
-                                 FROM relevant_results AS r
-                                        JOIN result_attempts ra ON ra.result_id = r.id
-                                 GROUP BY result_id)
-
-SELECT competition_id,
-       event_id,
-       round_type_id,
-       COUNT(DISTINCT num_attempts) AS different_result_types
-FROM num_attempts_per_result
-GROUP BY competition_id, event_id, round_type_id
+WITH relevant_results AS (
+  SELECT id, competition_id, event_id, round_type_id
+  FROM results
+  WHERE RIGHT(competition_id, 4) >= '2013'
+),
+     attempt_counts AS (
+       SELECT ra.result_id, COUNT(*) AS num_attempts
+       FROM result_attempts ra
+              INNER JOIN relevant_results rr ON ra.result_id = rr.id
+       GROUP BY ra.result_id
+     )
+SELECT
+  r.competition_id,
+  r.event_id,
+  r.round_type_id,
+  COUNT(DISTINCT ac.num_attempts) AS different_result_types
+FROM relevant_results r
+       JOIN attempt_counts ac ON ac.result_id = r.id
+GROUP BY
+  r.competition_id,
+  r.event_id,
+  r.round_type_id
 HAVING different_result_types > IF(round_type_id IN ('c', 'd', 'e', 'g', 'h'), 2, 1);
