2d601eba2aa4791f61843e1b4571b7939f8b2b52
FinnIckler <finnickler@gmail.com>
2026-01-14 12:04:54 +0100

    rubocop

--- db/migrate/20260114113003_add_result_attempts_index.rb
+++ db/migrate/20260114113003_add_result_attempts_index.rb
@@ -3,7 +3,7 @@
 class AddResultAttemptsIndex < ActiveRecord::Migration[8.1]
   def change
     change_table :result_attempts, bulk: true do |t|
-      t.index ["result_id", "attempt_number", "value"]
+      t.index %w[result_id attempt_number value]
     end
   end
 end
