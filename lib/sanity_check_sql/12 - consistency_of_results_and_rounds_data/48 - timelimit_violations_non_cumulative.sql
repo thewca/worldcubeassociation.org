5a7925654e6c4184e27e080831ffece718b69224
FinnIckler <finnickler@gmail.com>
2026-01-16 13:57:21 +0100

    directly join rounds and bring back 2013 filter

--- lib/sanity_check_sql/12 - consistency_of_results_and_rounds_data/48 - timelimit_violations_non_cumulative.sql
+++ lib/sanity_check_sql/12 - consistency_of_results_and_rounds_data/48 - timelimit_violations_non_cumulative.sql
@@ -1,42 +1,16 @@
-WITH round_numbers AS (
-  SELECT t0.*,
-         ROW_NUMBER() OVER (
-           PARTITION BY t0.competition_id, t0.event_id
-           ORDER BY rt.`rank`
-           ) AS round
-  FROM (SELECT DISTINCT r.competition_id,
-                        r.event_id,
-                        r.round_type_id
-        FROM results r) t0
-         JOIN round_types rt ON t0.round_type_id = rt.id
-),
-     eligible_rounds AS (
-       SELECT ro.id,
-              ro.competition_event_id,
-              ro.number,
-              JSON_EXTRACT(ro.time_limit, '$.centiseconds') AS time_limit_cs
-       FROM rounds ro
-       WHERE ro.time_limit IS NOT NULL
-         AND JSON_LENGTH(JSON_EXTRACT(ro.time_limit, '$.cumulativeRoundIds')) = 0
-     )
 SELECT r.person_id,
        r.competition_id,
        r.event_id,
-       rn.round,
+       ro.number AS round,
        r.round_type_id,
        ra.value,
-       er.time_limit_cs AS time_limit
-FROM eligible_rounds er
-       JOIN competition_events ce ON ce.id = er.competition_event_id
-       JOIN round_numbers rn
-            ON rn.competition_id = ce.competition_id
-              AND rn.event_id = ce.event_id
-              AND rn.round = er.number
-       JOIN results r
-            ON r.competition_id = rn.competition_id
-              AND r.event_id = rn.event_id
-              AND r.round_type_id = rn.round_type_id
+       JSON_EXTRACT(ro.time_limit, '$.centiseconds') AS time_limit
+FROM results r
+       JOIN rounds ro ON r.round_id = ro.id
        JOIN result_attempts ra
             ON ra.result_id = r.id
-              AND ra.value > er.time_limit_cs
-WHERE er.time_limit_cs IS NOT NULL
+WHERE ro.time_limit IS NOT NULL
+  AND JSON_LENGTH(JSON_EXTRACT(ro.time_limit, '$.cumulativeRoundIds')) = 0
+  AND JSON_EXTRACT(ro.time_limit, '$.centiseconds') IS NOT NULL
+  AND ra.value > JSON_EXTRACT(ro.time_limit, '$.centiseconds')
+  AND RIGHT(r.competition_id, 4) > 2013
