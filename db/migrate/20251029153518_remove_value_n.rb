7072157d4f18bc18232008de4f034205ce64963d
Gregor Billing <gbilling@worldcubeassociation.org>
2026-01-20 18:06:39 +0100

    Fix migrations after merge

--- db/migrate/20251029153518_remove_value_n.rb
+++ db/migrate/20251029153518_remove_value_n.rb
@@ -3,6 +3,12 @@
 class RemoveValueN < ActiveRecord::Migration[7.2]
   def change
     change_table :results, bulk: true do |t|
+      t.remove_index %i[event_id value1]
+      t.remove_index %i[event_id value2]
+      t.remove_index %i[event_id value3]
+      t.remove_index %i[event_id value4]
+      t.remove_index %i[event_id value5]
+
       t.remove :value1, type: :integer
       t.remove :value2, type: :integer
       t.remove :value3, type: :integer
