31b7ce2c0db8364b76084778c1735303167a614d
Daniel James <djdany444@gmail.com>
2026-01-21 22:26:16 +0530

    Fix edit person approve feature in tickets

--- spec/factories/tickets_edit_person.rb
+++ spec/factories/tickets_edit_person.rb
@@ -5,16 +5,12 @@ FactoryBot.define do
     status { :open }
     wca_id { FactoryBot.create(:user_with_wca_id).wca_id }
 
-    trait :edit_name do
-      after(:create) do |edit_name_ticket|
-        edit_name_ticket.ticket = FactoryBot.create(:ticket, metadata: edit_name_ticket)
-        FactoryBot.create(
-          :tickets_edit_name_field,
-          tickets_edit_person_id: edit_name_ticket.id,
-        )
+    trait :default_stakeholders do
+      after(:create) do |edit_person_ticket|
+        edit_person_ticket.ticket = FactoryBot.create(:ticket, metadata: edit_person_ticket)
         FactoryBot.create(
           :ticket_stakeholder,
-          ticket: edit_name_ticket.ticket,
+          ticket: edit_person_ticket.ticket,
           stakeholder: UserGroup.teams_committees_group_wrt,
           connection: :assigned,
           stakeholder_role: TicketStakeholder.stakeholder_roles[:actioner],
@@ -22,7 +18,7 @@ FactoryBot.define do
         )
         FactoryBot.create(
           :ticket_stakeholder,
-          ticket: edit_name_ticket.ticket,
+          ticket: edit_person_ticket.ticket,
           stakeholder: FactoryBot.create(:user),
           connection: :cc,
           stakeholder_role: TicketStakeholder.stakeholder_roles[:requester],
@@ -31,6 +27,49 @@ FactoryBot.define do
       end
     end
 
-    factory :edit_name_ticket, traits: [:edit_name]
+    trait :edit_name do
+      after(:create) do |edit_name_ticket|
+        FactoryBot.create(
+          :tickets_edit_name_field,
+          tickets_edit_person_id: edit_name_ticket.id,
+          old_value: edit_name_ticket.person.name,
+        )
+      end
+    end
+
+    trait :edit_dob do
+      after(:create) do |edit_dob_ticket|
+        FactoryBot.create(
+          :tickets_edit_dob_field,
+          tickets_edit_person_id: edit_dob_ticket.id,
+          old_value: edit_dob_ticket.person.dob,
+        )
+      end
+    end
+
+    trait :edit_country do
+      after(:create) do |edit_country_ticket|
+        FactoryBot.create(
+          :tickets_edit_country_field,
+          tickets_edit_person_id: edit_country_ticket.id,
+          old_value: edit_country_ticket.person.country_iso2,
+        )
+      end
+    end
+
+    trait :edit_gender do
+      after(:create) do |edit_gender_ticket|
+        FactoryBot.create(
+          :tickets_edit_gender_field,
+          tickets_edit_person_id: edit_gender_ticket.id,
+          old_value: edit_gender_ticket.person.gender,
+        )
+      end
+    end
+
+    factory :edit_name_ticket, traits: %i[edit_name default_stakeholders]
+    factory :edit_dob_ticket, traits: %i[edit_dob default_stakeholders]
+    factory :edit_country_ticket, traits: %i[edit_country default_stakeholders]
+    factory :edit_gender_ticket, traits: %i[edit_gender default_stakeholders]
   end
 end
