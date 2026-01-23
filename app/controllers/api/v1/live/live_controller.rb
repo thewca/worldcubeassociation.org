5bf7a91b93ef67de8d568844e1fabe61a10c5bee
FinnIckler <finnickler@gmail.com>
2026-01-23 17:35:49 +0100

    make round_results route work by wcif_id

--- app/controllers/api/v1/live/live_controller.rb
+++ app/controllers/api/v1/live/live_controller.rb
@@ -3,9 +3,17 @@
 class Api::V1::Live::LiveController < Api::V1::ApiController
   skip_before_action :require_user, only: %i[round_results by_person podiums]
   def round_results
-    round_id = params.require(:round_id)
+    competition = Competition.find(params.require(:competition_id))
 
-    round = Round.includes(live_results: %i[live_attempts round event]).find(round_id)
+    event_id, number = params.require(:round_id).split("-")
+
+    if event_id.nil? || number.nil?
+      return render json: { status: "round not found" }, status: :not_found
+    end
+
+    competition_event = competition.competition_events.find_by(event_id: event_id)
+
+    round = competition.includes(rounds: %i[live_results: %i[live_attempts round event]]).rounds.find_by(competition_event_id: competition_event.id, number: number)
 
     render json: round.to_live_json
   end
