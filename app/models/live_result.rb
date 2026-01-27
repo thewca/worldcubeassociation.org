8f5940c7badf8ed76a8d25d499a10ae270ce3bde
FinnIckler <finnickler@gmail.com>
2026-01-27 16:41:50 +0100

    add a to_inbox_result method and use it

--- app/models/live_result.rb
+++ app/models/live_result.rb
@@ -99,6 +99,26 @@ class LiveResult < ApplicationRecord
     end
   end
 
+  def to_inbox_result
+    attempt_values = result.attempts.map(&:value)
+    InboxResult.new({
+                      competition: competition,
+                      person_id: result.person_id,
+                      pos: result.ranking,
+                      event_id: round.event_id,
+                      round_type_id: round.round_type_id,
+                      round_id: round.id,
+                      format_id: round.format_id,
+                      best: result.best,
+                      average: result.average,
+                      value1: attempt_values[0],
+                      value2: attempt_values[1] || 0,
+                      value3: attempt_values[2] || 0,
+                      value4: attempt_values[3] || 0,
+                      value5: attempt_values[4] || 0,
+                    })
+  end
+
   def to_wcif
     {
       "personId" => self.registration.registrant_id,
