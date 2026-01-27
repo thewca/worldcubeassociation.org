bbc737436a1cc1c1579cc97e081dc48ffdd7ca5a
FinnIckler <finnickler@gmail.com>
2026-01-27 12:03:22 +0100

    use ScheduleActivity approach

--- app/controllers/api/v1/live/live_controller.rb
+++ app/controllers/api/v1/live/live_controller.rb
@@ -5,7 +5,9 @@ class Api::V1::Live::LiveController < Api::V1::ApiController
   def round_results
     competition = Competition.find(params.require(:competition_id))
 
-    event_id, number = params.require(:round_id).split("-")
+    activity_code =  ScheduleActivity.parse_activity_code(params.require(:round_id))
+
+    event_id, number = activity_code.values_at(:event_id, :round_number)
 
     return render json: { status: "round not found" }, status: :not_found if event_id.nil? || number.nil?
 
