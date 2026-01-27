34396d66504abacec3ef2216373b99a2ab6d4db2
Gregor Billing <gbilling@worldcubeassociation.org>
2026-01-27 17:19:22 +0100

    WIP SQL query

--- lib/auxiliary_data_computation.rb
+++ lib/auxiliary_data_computation.rb
@@ -50,56 +50,52 @@ module AuxiliaryDataComputation
       %w[average ranks_average concise_average_results],
     ].each do |field, table_name, concise_table_name|
       DbHelper.with_temp_table(table_name) do |temp_table_name|
-        current_country_by_wca_id = Person.current.pluck(:wca_id, :country_id).to_h
-        # Get all personal records (note: people that changed their country appear once for each country).
-        personal_records_with_event = ActiveRecord::Base.connection.execute <<-SQL.squish
-          SELECT event_id, person_id, country_id, continent_id, MIN(#{field}) value
-          FROM #{concise_table_name}
-          GROUP BY person_id, country_id, continent_id, event_id
+        ActiveRecord::Base.connection.execute <<-SQL.squish
+          INSERT INTO #{temp_table_name} (person_id, event_id, best, world_rank, continent_rank, country_rank)
+          WITH current_person_data AS (
+            SELECT wca_id, country_id, continent_id
+            FROM persons
+              INNER JOIN countries ON persons.country_id = countries.id
+            WHERE sub_id = 1
+          ),
+          person_stats AS (
+            SELECT r.person_id,
+                   r.event_id,
+                   r.#{field} AS value,
+                   r.id AS result_id,
+                   ROW_NUMBER() OVER (
+                     PARTITION BY r.person_id, r.event_id
+                     ORDER BY r.#{field}, r.id
+                   ) AS rn,
+                   MIN(CASE WHEN r.continent_id = p.continent_id THEN r.#{field} END)
+                     OVER (PARTITION BY r.person_id, r.event_id) AS continent_valid_best,
+                   MIN(CASE WHEN r.country_id = p.country_id THEN r.#{field} END)
+                     OVER (PARTITION BY r.person_id, r.event_id) AS country_valid_best,
+                   p.continent_id AS current_continent_id,
+                   p.country_id AS current_country_id
+            FROM #{concise_table_name} r
+              INNER JOIN current_person_data p ON r.person_id = p.wca_id
+          )
+          SELECT person_id,
+                 event_id,
+                 value,
+                 RANK() OVER (PARTITION BY event_id ORDER BY value) AS world_rank,
+                 IF(continent_valid_best IS NULL, 0,
+                   RANK() OVER (
+                     PARTITION BY event_id, current_continent_id
+                     ORDER BY (continent_valid_best IS NULL), continent_valid_best
+                   )
+                 ) AS continent_rank,
+                 IF(country_valid_best IS NULL, 0,
+                   RANK() OVER (
+                     PARTITION BY event_id, current_country_id
+                     ORDER BY (country_valid_best IS NULL), country_valid_best
+                   )
+                 ) AS country_rank
+          FROM person_stats
+          WHERE rn = 1
           ORDER BY event_id, value
         SQL
-        personal_records_with_event.group_by(&:first).each do |event_id, personal_records|
-          personal_rank = Hash.new { |h, k| h[k] = {} }
-          ranked = Hash.new { |h, k| h[k] = {} }
-          counter = Hash.new(0)
-          current_rank = Hash.new(0)
-          previous_value = {}
-          personal_records.each do |_, person_id, country_id, continent_id, value|
-            # Update the region states (unless we have ranked this person already,
-            # e.g. 2008SEAR01 twice in North America and World because of his two countries).
-            ["World", continent_id, country_id].each do |region|
-              next if ranked[region][person_id]
-
-              counter[region] += 1
-              # As we ordered by value it can either be greater or tie the previous one.
-              current_rank[region] = counter[region] if previous_value[region].nil? || value > previous_value[region]
-              previous_value[region] = value
-              ranked[region][person_id] = true
-            end
-            cached_country = Country.c_find(current_country_by_wca_id[person_id])
-            # The only known case where this happens if current_country_by_wca_id[person_id] is nil,
-            # in other words, the person_id from the concise*results table is not found in the Persons table.
-            # In the past, this has occurred when temporary results have been inserted for newcomers.
-            next if cached_country.nil?
-
-            # Set the person's data (first time the current location is matched).
-            personal_rank[person_id][:best] ||= value
-            personal_rank[person_id][:world_rank] ||= current_rank["World"]
-            personal_rank[person_id][:continent_rank] ||= current_rank[continent_id] if continent_id == cached_country.continent_id
-            personal_rank[person_id][:country_rank] ||= current_rank[country_id] if country_id == cached_country.id
-          end
-          values = personal_rank.map do |person_id, rank_data|
-            # NOTE: continent_rank and country_rank may be not present because of a country change, in such case we default to 0.
-            "('#{person_id}', '#{event_id}', #{rank_data[:best]}, #{rank_data[:world_rank]}, #{rank_data[:continent_rank] || 0}, #{rank_data[:country_rank] || 0})"
-          end
-          # Insert 500 rows at once to avoid running into too long query.
-          values.each_slice(500) do |values_subset|
-            ActiveRecord::Base.connection.execute <<-SQL.squish
-              INSERT INTO #{temp_table_name} (person_id, event_id, best, world_rank, continent_rank, country_rank) VALUES
-              #{values_subset.join(",\n")}
-            SQL
-          end
-        end
       end
     end
   end
