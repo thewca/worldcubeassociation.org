08a3298739f73dc861676f63796e37221594df2b
Gregor Billing <gbilling@worldcubeassociation.org>
2026-01-30 13:46:27 +0100

    Import attempts within the main merge transaction

--- lib/competition_results_import.rb
+++ lib/competition_results_import.rb
@@ -56,16 +56,23 @@ module CompetitionResultsImport
           round_type_id: inbox_res.round_type_id,
           round_id: inbox_res.round_id,
           format_id: inbox_res.format_id,
+          value1: inbox_res.value1,
+          value2: inbox_res.value2,
+          value3: inbox_res.value3,
+          value4: inbox_res.value4,
+          value5: inbox_res.value5,
           best: inbox_res.best,
           average: inbox_res.average,
         }
       end
 
       Result.insert_all!(result_rows)
+
+      attempts = competition.reload.results.flat_map { it.result_attempts_attributes(result_id: it.id) }
+      ResultAttempt.insert_all!(attempts)
+
       competition.inbox_results.destroy_all
     end
-    attempts = competition.reload.results.flat_map { it.result_attempts_attributes(result_id: it.id) }
-    ResultAttempt.insert_all!(attempts)
   end
 
   def self.post_results_error(comp)
