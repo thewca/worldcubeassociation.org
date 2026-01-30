741cdfd8da3344b7fdb13898bab211922513267a
Gregor Billing <gbilling@worldcubeassociation.org>
2026-01-30 18:42:06 +0100

    Fix sanity check

--- lib/sanity_check_sql/9 - suspicious_or_irregular_rounds_data/70 - invalid_cumulative_limit.sql
+++ lib/sanity_check_sql/9 - suspicious_or_irregular_rounds_data/70 - invalid_cumulative_limit.sql
@@ -3,5 +3,5 @@ FROM (SELECT competition_id, CONCAT(event_id, "-r", number) AS round_id, time_li
       FROM rounds ro
              JOIN competition_events ce ON ro.competition_event_id = ce.id
       WHERE JSON_LENGTH(JSON_EXTRACT(time_limit, '$.cumulativeRoundIds')) > 0 # only apply to rounds with a cumulative time limit
-        AND competition_id IN (SELECT id FROM competitions WHERE announced_at IS NOT NULL AND cancelled_at IS NULL)) t1
+        AND competition_id IN (SELECT competition_id FROM competitions WHERE announced_at IS NOT NULL AND cancelled_at IS NULL)) t1
 WHERE LENGTH(REPLACE(time_limit, round_id, "")) = LENGTH(time_limit);
