cafff1e1137e2dc44ac49b09a0e8d2768bf5bcb1
FinnIckler <finnickler@gmail.com>
2026-02-06 12:04:58 +0100

    fix add test

--- spec/requests/live_add_spec.rb
+++ spec/requests/live_add_spec.rb
@@ -15,11 +15,11 @@ RSpec.describe "WCA Live API" do
       round.open_round!
 
       live_request = {
-        attempts: [111, 222, 333, 444, 555],
+        attempts: [{ value: 111, attempt_number: 1 }, { value: 222, attempt_number: 2 }, { value: 333, attempt_number: 3 }, { value: 444, attempt_number: 4 }, { value: 555, attempt_number: 5 }],
         registration_id: registration.id,
       }
 
-      post add_live_result_path(competition.id, round.id), params: live_request
+      post api_v1_competition_live_add_results_path(competition.id, round.wcif_id), params: live_request
       expect(response).to be_successful
       perform_enqueued_jobs
 
@@ -27,10 +27,10 @@ RSpec.describe "WCA Live API" do
       expect(result).to be_present
 
       expect(result.live_attempts.map { |l| { attempt_number: l.attempt_number, value: l.value } }).to contain_exactly({ attempt_number: 1, value: 111 },
-                                                                                                                       { attempt_number: 2, value: 222 },
-                                                                                                                       { attempt_number: 3, value: 333 },
-                                                                                                                       { attempt_number: 4, value: 444 },
-                                                                                                                       { attempt_number: 5, value: 555 })
+                                                                                                                         { attempt_number: 2, value: 222 },
+                                                                                                                         { attempt_number: 3, value: 333 },
+                                                                                                                         { attempt_number: 4, value: 444 },
+                                                                                                                         { attempt_number: 5, value: 555 })
       expect(result.best).to eq 111
       expect(result.average).to eq 333
     end
@@ -43,11 +43,11 @@ RSpec.describe "WCA Live API" do
       registration = create(:registration, :accepted, competition: competition)
 
       live_request = {
-        attempts: [111, 222, 333, 444, 555],
+        attempts: [{ value: 111, attempt_number: 1 }, { value: 222, attempt_number: 2 }, { value: 333, attempt_number: 3 }, { value: 444, attempt_number: 4 }, { value: 555, attempt_number: 5 }],
         registration_id: registration.id,
       }
 
-      post add_live_result_path(competition.id, round.id), params: live_request
+      post api_v1_competition_live_add_results_path(competition.id, round.wcif_id), params: live_request
       expect(response).not_to be_successful
     end
 
@@ -61,11 +61,11 @@ RSpec.describe "WCA Live API" do
       round.open_round!
 
       live_request = {
-        attempts: [111, 222, 333, 444, 555],
+        attempts: [{ value: 111, attempt_number: 1 }, { value: 222, attempt_number: 2 }, { value: 333, attempt_number: 3 }, { value: 444, attempt_number: 4 }, { value: 555, attempt_number: 5 }],
         registration_id: registration.id,
       }
 
-      post add_live_result_path(competition.id, round.id), params: live_request
+      post api_v1_competition_live_add_results_path(competition.id, round.wcif_id), params: live_request
       expect(response).not_to be_successful
       expect(response.parsed_body["status"]).to eq "user is not part of this round"
     end
