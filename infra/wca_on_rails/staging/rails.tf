16fee052e1b43e889243cfec2c6e2210dafddcb6
FinnIckler <finnickler@gmail.com>
2026-02-09 12:24:05 +0100

    Allow PMA to talk to new staging server and keep it up to date with 5.2 (#13388)
    * allow staging to connect to new instance via IAM auth
    
    * make suer PMA is up to date with 5.2 releases

--- infra/wca_on_rails/staging/rails.tf
+++ infra/wca_on_rails/staging/rails.tf
@@ -149,10 +149,10 @@ locals {
     { # The PHPMyAdmin Docker file allows us to pass the user config as a base 64 encoded environment variable
       name = "PMA_USER_CONFIG_BASE64"
       value = base64encode(templatefile("../templates/config.user.inc.php.tftpl",
-        { rds_host: "staging-worldcubeassociation-dot-org.comp2du1hpno.us-west-2.rds.amazonaws.com",
-          rds_replica_host: "readonly-staging-worldcubeassociation-dot-org.comp2du1hpno.us-west-2.rds.amazonaws.com",
-          # There is no dump replica on staging
-          dump_replica_host: "readonly-staging-worldcubeassociation-dot-org.comp2du1hpno.us-west-2.rds.amazonaws.com"}))
+        { rds_host: "staging-v2-worldcubeassociation-dot-org.comp2du1hpno.us-west-2.rds.amazonaws.com",
+          # There is no read only or dump replica on staging
+          rds_replica_host: "staging-v2-worldcubeassociation-dot-org.comp2du1hpno.us-west-2.rds.amazonaws.com",
+          dump_replica_host: "staging-v2-worldcubeassociation-dot-org.comp2du1hpno.us-west-2.rds.amazonaws.com"}))
     }
   ]
 }
