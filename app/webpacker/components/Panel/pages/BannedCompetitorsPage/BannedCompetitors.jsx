e6aa29301dc15f33109e8b1002769a85f75bb9e2
Brandon-Reichman <reichman.brandon@gmail.com>
2026-02-10 00:39:44 -0600

    add pagination to banned competitors component

--- app/webpacker/components/Panel/pages/BannedCompetitorsPage/BannedCompetitors.jsx
+++ app/webpacker/components/Panel/pages/BannedCompetitorsPage/BannedCompetitors.jsx
@@ -1,55 +1,71 @@
 import React from 'react';
 import { Icon, Table, Segment } from 'semantic-ui-react';
 import UserBadge from '../../../UserBadge';
+import PaginationFooter from '../../../PaginationFooter';
+import usePagination from '../../../../lib/hooks/usePagination';
 
 export default function BannedCompetitors({
   bannedCompetitorRoles,
   canEditBannedCompetitors,
   editBannedCompetitor,
 }) {
+  const pagination = usePagination(25);
+  const { entriesPerPage, activePage } = pagination;
+  const paginatedRoles = bannedCompetitorRoles.slice(
+    (activePage - 1) * entriesPerPage,
+    activePage * entriesPerPage,
+  );
   return bannedCompetitorRoles.length > 0 ? (
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell width={5}>User</Table.HeaderCell>
-          <Table.HeaderCell width={2}>Start date</Table.HeaderCell>
-          <Table.HeaderCell width={2}>End date</Table.HeaderCell>
-          <Table.HeaderCell width={4}>Reason</Table.HeaderCell>
-          <Table.HeaderCell width={3}>Scope</Table.HeaderCell>
-          {canEditBannedCompetitors && (
-          <Table.HeaderCell width={2}>Edit</Table.HeaderCell>
-          )}
-        </Table.Row>
-      </Table.Header>
-
-      <Table.Body>
-        {bannedCompetitorRoles.map((role) => (
-          <Table.Row key={role.id}>
-            <Table.Cell>
-              <UserBadge
-                user={role.user}
-                hideBorder
-                leftAlign
-                subtexts={role.user.wca_id ? [role.user.wca_id] : []}
-              />
-            </Table.Cell>
-            <Table.Cell>{role.start_date}</Table.Cell>
-            <Table.Cell>{role.end_date}</Table.Cell>
-            <Table.Cell>{role.metadata.ban_reason}</Table.Cell>
-            <Table.Cell>{role.metadata.scope}</Table.Cell>
+    <>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell width={5}>User</Table.HeaderCell>
+            <Table.HeaderCell width={2}>Start date</Table.HeaderCell>
+            <Table.HeaderCell width={2}>End date</Table.HeaderCell>
+            <Table.HeaderCell width={4}>Reason</Table.HeaderCell>
+            <Table.HeaderCell width={3}>Scope</Table.HeaderCell>
             {canEditBannedCompetitors && (
-            <Table.Cell>
-              <Icon
-                name="edit"
-                link
-                onClick={() => editBannedCompetitor({ action: 'edit', role })}
-              />
-            </Table.Cell>
+            <Table.HeaderCell width={2}>Edit</Table.HeaderCell>
             )}
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+        </Table.Header>
+
+        <Table.Body>
+          {paginatedRoles.map((role) => (
+            <Table.Row key={role.id}>
+              <Table.Cell>
+                <UserBadge
+                  user={role.user}
+                  hideBorder
+                  leftAlign
+                  subtexts={role.user.wca_id ? [role.user.wca_id] : []}
+                />
+              </Table.Cell>
+              <Table.Cell>{role.start_date}</Table.Cell>
+              <Table.Cell>{role.end_date}</Table.Cell>
+              <Table.Cell>{role.metadata.ban_reason}</Table.Cell>
+              <Table.Cell>{role.metadata.scope}</Table.Cell>
+              {canEditBannedCompetitors && (
+              <Table.Cell>
+                <Icon
+                  name="edit"
+                  link
+                  onClick={() => editBannedCompetitor({ action: 'edit', role })}
+                />
+              </Table.Cell>
+              )}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+
+      <PaginationFooter
+          pagination={pagination}
+          totalPages={Math.ceil(bannedCompetitorRoles.length / entriesPerPage)}
+          totalEntries={bannedCompetitorRoles.length}
+        />
+    </>
   ) : (
     <Segment placeholder textAlign="center">
       <p>No Data to Show.</p>
