8b8a208a063cfabef88df18c3a56ebc60367f380
FinnIckler <finnickler@gmail.com>
2026-02-17 12:32:07 +0100

    add type alias

--- next-frontend/src/lib/live/decompressDiff.ts
+++ next-frontend/src/lib/live/decompressDiff.ts
@@ -2,10 +2,14 @@ import { DiffedLiveResult } from "@/lib/hooks/useResultsSubscription";
 import { components } from "@/types/openapi";
 import _ from "lodash";
 
+type PartialLiveResultWithRegistrationId = Partial<
+  components["schemas"]["LiveResult"]
+> &
+  Pick<components["schemas"]["LiveResult"], "registration_id">;
+
 export function decompressDiff(
   diff: DiffedLiveResult,
-): Partial<components["schemas"]["LiveResult"]> &
-  Pick<components["schemas"]["LiveResult"], "registration_id"> {
+): PartialLiveResultWithRegistrationId {
   // This looks a little silly right now, but this is part of https://github.com/thewca/worldcubeassociation.org/pull/13352
   // where the actual compressed values will be put in.
   return _.omitBy(
@@ -14,6 +18,5 @@ export function decompressDiff(
       attempts: diff.live_attempts,
     },
     _.isUndefined,
-  ) as Partial<components["schemas"]["LiveResult"]> &
-    Pick<components["schemas"]["LiveResult"], "registration_id">;
+  ) as PartialLiveResultWithRegistrationId;
 }
