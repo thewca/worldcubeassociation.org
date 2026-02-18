f8a2797e55f0079f6efb7338352a9bf49a9fe883
Daniel James <djdany444@gmail.com>
2026-02-18 19:36:13 +0530

    Extend newcomer page access till results submission (#13462)
    * Extend newcomer page access till results submission, also improve success message
    
    * Review changes

--- spec/requests/results_submission_spec.rb
+++ spec/requests/results_submission_spec.rb
@@ -115,16 +115,37 @@ RSpec.describe ResultsSubmissionController do
       end
     end
 
-    context "when competition is not upcoming (past competition)" do
-      let(:past_comp) { create(:competition, :announced, :past) }
+    context "when competition is ongoing and results not yet submitted" do
+      let(:comp_delegate) { create(:delegate) }
+      let(:ongoing_comp) { create(:competition, :announced, :ongoing, delegates: [comp_delegate]) }
+
+      it "allows access for WRT user" do
+        sign_in wrt_user
+
+        get competition_newcomer_name_format_check_path(ongoing_comp.id)
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "does not allows access for a Delegate of this current competition" do
+        sign_in comp_delegate
+
+        get competition_newcomer_name_format_check_path(ongoing_comp.id)
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context "when competition has results submitted" do
+      let(:results_submitted_comp) { create(:competition, :announced, :with_valid_submitted_results) }
 
       it "returns bad_request for WRT user" do
         sign_in wrt_user
 
-        get competition_newcomer_name_format_check_path(past_comp.id)
+        get competition_newcomer_name_format_check_path(results_submitted_comp.id)
 
         expect(response).to have_http_status(:bad_request)
-        expect(response.parsed_body["error"]).to eq("The newcomer check dashboard can only be used for upcoming competitions.")
+        expect(response.parsed_body["error"]).to eq("The newcomer check dashboard can only be used before the results are submitted.")
       end
     end
   end
