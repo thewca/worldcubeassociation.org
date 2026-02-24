b8eefc8bbe2224291c24450e1e9db71605588ed1
Daniel James <djdany444@gmail.com>
2026-02-24 18:20:04 +0530

    Review changes

--- app/webpacker/lib/providers/ConfirmProvider.js
+++ app/webpacker/lib/providers/ConfirmProvider.js
@@ -85,12 +85,20 @@ export default function ConfirmProvider({ children }) {
           <Modal.Header>Confirm Action</Modal.Header>
           <Modal.Content>
             <p>{options.content}</p>
-            <p>{`Type "${options.requireInput}" to confirm`}</p>
+            <p>
+              Type
+              {' '}
+              <code>{options.requireInput}</code>
+              {' '}
+              to confirm
+            </p>
             <Input
               fluid
               placeholder={options.requireInput}
               value={inputValue}
               onChange={setInputValue}
+              // Disable pasting to force user to type the exact string
+              onPaste={(e) => e.preventDefault()}
               error={inputError}
               autoFocus
             />
