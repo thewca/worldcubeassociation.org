29d7da65b64ca855313c66c380a80bb1230dde4b
FinnIckler <finnickler@gmail.com>
2026-02-24 13:56:46 +0100

    fix lint

--- app/jobs/update_live_result_job.rb
+++ app/jobs/update_live_result_job.rb
@@ -33,7 +33,6 @@ class UpdateLiveResultJob < ApplicationJob
     after_state = round.to_live_state
     diff = Live::DiffHelper.round_state_diff(before_state, after_state)
 
-    puts(diff)
     diff = Live::DiffHelper.add_forecast_stats(diff, round)
 
     ActionCable.server.broadcast(Live::Config.broadcast_key(round.wcif_id), diff)
--- next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/AddResults.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/AddResults.tsx
@@ -3,13 +3,10 @@ import { components } from "@/types/openapi";
 import { SimpleGrid, GridItem } from "@chakra-ui/react";
 import AttemptsForm from "@/components/live/AttemptsForm";
 import { Format } from "@/lib/wca/data/formats";
-import LiveResultsTable from "@/components/live/LiveResultsTable";
 import LiveUpdatingResultsTable from "@/components/live/LiveUpdatingResultsTable";
-import { useLiveResults } from "@/providers/LiveResultProvider";
 import events from "@/lib/wca/data/events";
 import { parseActivityCode } from "@/lib/wca/wcif/rounds";
 import { LiveResultAdminProvider } from "@/providers/LiveResultAdminProvider";
-import _ from "lodash";
 
 export default function AddResults({
   format,
