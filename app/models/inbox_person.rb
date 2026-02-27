c992619ee6ee1bab4577d11b675b9dd1dafb6d17
Daniel James <djdany444@gmail.com>
2026-02-27 20:30:53 +0530

    Verify if inbox_person has a registration mismatch

--- app/models/inbox_person.rb
+++ app/models/inbox_person.rb
@@ -10,6 +10,7 @@ class InboxPerson < ApplicationRecord
 
   belongs_to :person, -> { current }, foreign_key: "wca_id", primary_key: "wca_id", optional: true
   belongs_to :country, foreign_key: "country_iso2", primary_key: "iso2"
+  has_one :registration, foreign_key: %i[registrant_id competition_id], primary_key: %i[id competition_id]
 
   alias_attribute :ref_id, :id
   alias_method :wca_person, :person
@@ -24,5 +25,23 @@ class InboxPerson < ApplicationRecord
 
   def country
     Country.c_find_by_iso2(self.country_iso2)
+  end
+
+  def registration_mismatches
+    return [] unless registration
+
+    mismatches = []
+    mismatches << "name ('#{name}' vs '#{registration.name}')" if name != registration.name
+    mismatches << "country ('#{country_iso2}' vs '#{registration.country_iso2}')" if country_iso2 != registration.country_iso2
+    mismatches << "gender ('#{gender}' vs '#{registration.gender}')" if gender != registration.gender
+    mismatches << "dob ('#{dob}' vs '#{registration.dob}')" if dob.to_s != registration.dob&.to_s
+
+    # inbox_persons.wca_id has a DB default of "" (never nil), while users.wca_id is nullable.
+    # .presence normalizes both to nil.
+    person_wca_id = wca_id.presence
+    registration_wca_id = registration.wca_id.presence
+    mismatches << "WCA ID ('#{person_wca_id}' vs '#{registration_wca_id}')" if person_wca_id != registration_wca_id
+
+    mismatches
   end
 end
