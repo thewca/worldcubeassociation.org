c80a1eec186e87caa4307092dcd3f15bd270a574
pranav-027 <pd2772001@gmail.com>
2026-03-04 11:16:50 +0530

    Enforce ASCII apostrophes in names

---
+++ db/migrate/20260304054202_normalize_person_name_quotes_to_ascii_apostrophe.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class NormalizePersonNameQuotesToAsciiApostrophe < ActiveRecord::Migration[8.1]
+  def up
+    Person.where("name REGEXP ?", "[’‘]").find_each do |person|
+      new_name = person.name
+                       .gsub("’", "'")
+                       .gsub("‘", "'")
+
+      next if new_name == person.name
+
+      person.execute_edit_person_request("fix", { name: new_name })
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
