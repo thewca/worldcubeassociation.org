5c0c915ece6f50804173fe0eded5539860aacda9
pranav-027 <pd2772001@gmail.com>
2026-03-04 21:44:40 +0530

    remove db migration for Apostrophe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_04_054202) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_25_164242) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", precision: nil, null: false
