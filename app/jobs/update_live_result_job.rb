7a9c7bdb2c67e17b9b1712713be21d0c06d97c16
Gregor Billing <gbilling@worldcubeassociation.org>
2026-03-10 15:10:55 +0100

    Simplify update_live_results_job attempt insertion

--- app/jobs/update_live_result_job.rb
+++ app/jobs/update_live_result_job.rb
@@ -5,28 +5,23 @@ class UpdateLiveResultJob < ApplicationJob
   queue_as EnvConfig.LIVE_QUEUE if Live::Config.sqs_queued?
 
   def perform(live_result, results, entered_by_id)
-    previous_attempts = live_result.live_attempts.index_by(&:attempt_number)
+    LiveAttempt.upsert_all(results)
 
-    new_attempts = results.map do |r|
-      previous_attempt = previous_attempts[r[:attempt_number]]
-
-      if previous_attempt.present?
-        previous_attempt.update(value: r[:value])
-        previous_attempt
-      else
-        LiveAttempt.build(**r)
-      end
-    end
+    attempt_numbers = results.pluck(:attempt_number)
+    live_result.result_attempts.where.not(attempt_number: attempt_numbers).delete_all
 
     round = live_result.round
 
     # We need the state before the result is updated
     before_state = round.to_live_state
 
+    new_attempts = live_result.result_attempts.reload # We did some `upsert_all` and `delete_all` shenanigans above, which bypass Rails memory. Hence reloading...
     average, best = LiveResult.compute_average_and_best(new_attempts, round)
 
-    live_result.update!(live_attempts: new_attempts, best: best, average: average, last_attempt_entered_at: Time.now.utc)
-    live_result.live_result_history_entries.create!(entered_by_id: entered_by_id, action_type: :scoretaking, attempt_details: new_attempts.pluck(:value))
+    live_result.update!(best: best, average: average, last_attempt_entered_at: live_result.current_time_from_proper_timezone)
+
+    history_ordered_results = results.sort_by { it[:attempt_number] }.pluck(:value)
+    live_result.live_result_history_entries.create!(entered_by_id: entered_by_id, action_type: :scoretaking, attempt_details: history_ordered_results)
 
     after_state = round.to_live_state
     diff = Live::DiffHelper.round_state_diff(before_state, after_state)
