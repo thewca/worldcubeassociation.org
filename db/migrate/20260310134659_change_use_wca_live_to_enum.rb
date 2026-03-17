05c6488034b6e81d7ba67b6fc39b2367764a31ed
FinnIckler <finnickler@gmail.com>
2026-03-17 14:15:26 +0100

    refactor migration

--- db/migrate/20260310134659_change_use_wca_live_to_enum.rb
+++ db/migrate/20260310134659_change_use_wca_live_to_enum.rb
@@ -1,39 +1,20 @@
 # frozen_string_literal: true
 
 class ChangeUseWcaLiveToEnum < ActiveRecord::Migration[8.1]
-  def up
-    add_column :competitions, :use_wca_live_for_scoretaking_tmp, :integer, default: 0, null: false
+  def change
+    add_column :competitions, :software_for_scoretaking, :integer, default: 0, null: false
 
-    execute <<~SQL.squish
-      UPDATE competitions
-      SET use_wca_live_for_scoretaking_tmp =
-        CASE
-          WHEN use_wca_live_for_scoretaking = TRUE THEN 1
-          ELSE 0
-        END
-    SQL
-
-    change_table :competitions, bulk: true do |t|
-      t.remove_column :use_wca_live_for_scoretaking
-      t.rename_column :use_wca_live_for_scoretaking_tmp, :software_for_scoretaking
+    up_only do
+      execute <<~SQL.squish
+        UPDATE competitions
+        SET software_for_scoretaking =
+          CASE
+            WHEN use_wca_live_for_scoretaking = TRUE THEN 1
+            ELSE 0
+          END
+      SQL
     end
-  end
 
-  def down
-    add_column :competitions, :use_wca_live_for_scoretaking_tmp, :boolean, default: false, null: false
-
-    execute <<~SQL.squish
-      UPDATE competitions
-      SET use_wca_live_for_scoretaking_tmp =
-        CASE
-          WHEN software_for_scoretaking = 1 THEN TRUE
-          ELSE FALSE
-        END
-    SQL
-
-    change_table :competitions, bulk: true do |t|
-      t.remove_column :software_for_scoretaking
-      t.rename_column :use_wca_live_for_scoretaking_tmp, :use_wca_live_for_scoretaking
-    end
+    remove_column :competitions, :use_wca_live_for_scoretaking, :boolean
   end
 end
