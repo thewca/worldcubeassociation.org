2ad7ed09f281506266d37f0e71919cb6dc761c8d
FinnIckler <finnickler@gmail.com>
2026-03-23 19:52:33 +0100

    correctly use service connect on the api server

--- infra/wca_on_rails/staging/rails.tf
+++ infra/wca_on_rails/staging/rails.tf
@@ -268,11 +268,13 @@ resource "aws_ecs_task_definition" "api" {
       memory = 2048
       portMappings = [
         {
-          # The hostPort is automatically set for awsvpc network mode,
-          # see https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_PortMapping.html#ECS-Type-PortMapping-hostPort
+          name = "rails-staging-port"
+          # Set hostport for service discovery
+          hostPort = 3000
           containerPort = 3000
           protocol      = "tcp"
-        },
+          appProtocol = "http"
+        }
       ]
       logConfiguration = {
         logDriver = "awslogs"
@@ -419,12 +421,6 @@ resource "aws_ecs_service" "rails" {
   tags = {
     Name = var.name_prefix
   }
-
-  service_connect_configuration {
-    enabled = true
-    namespace = aws_service_discovery_private_dns_namespace.this.name
-  }
-
 }
 
 resource "aws_ecs_service" "api" {
@@ -479,6 +475,22 @@ resource "aws_ecs_service" "api" {
 
   tags = {
     Name = var.name_prefix
+  }
+
+  service_connect_configuration {
+    enabled = true
+    namespace = aws_service_discovery_private_dns_namespace.this.name
+    service {
+      port_name = "rails-staging-port"
+      discovery_name = "rails-cluster"
+      timeout {
+        per_request_timeout_seconds = 60
+      }
+      client_alias {
+        port = 3000
+        dns_name = local.rails_internal_dns
+      }
+    }
   }
 
 }
