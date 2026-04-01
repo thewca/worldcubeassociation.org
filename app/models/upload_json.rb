77c966c8fc587647db25dad59c1717d36798ecae
Gregor Billing <gbilling@worldcubeassociation.org>
2026-04-01 14:28:20 +0200

    Speed up JSON file import

--- app/models/upload_json.rb
+++ app/models/upload_json.rb
@@ -71,7 +71,6 @@ class UploadJson
             pos: result["position"],
             event_id: event["eventId"],
             round_type_id: round_type_id,
-            round_id: competition_round&.id,
             format_id: round["formatId"],
             best: result["best"],
             average: result["average"],
@@ -82,10 +81,11 @@ class UploadJson
             value5: individual_results[4],
           }
           new_res = InboxResult.new(new_result_attributes)
-          # Using this way of setting the attribute saves one SELECT per result
-          # to validate the competition presence.
+          # Using this way of setting the attribute saves two SELECTs per result
+          # to validate the competition and round presence.
           # (a lot of time considering all the results to import!)
           new_res.competition = competition
+          new_res.round = competition_round
           results_to_import << new_res
         end
 
@@ -94,16 +94,18 @@ class UploadJson
           %w[scrambles extraScrambles].each do |scramble_type|
             group[scramble_type]&.each_with_index do |scramble, index|
               new_scramble_attributes = {
-                competition_id: competition_id,
                 event_id: event["eventId"],
                 round_type_id: round_type_id,
-                round_id: competition_round&.id,
                 group_id: group["group"],
                 is_extra: scramble_type == "extraScrambles",
                 scramble_num: index + 1,
                 scramble: scramble,
               }
-              scrambles_to_import << Scramble.new(new_scramble_attributes)
+              new_scr = Scramble.new(new_scramble_attributes)
+              # See inbox_results above for an explanation on setting these associations
+              new_scr.competition = competition
+              new_scr.round = competition_round
+              scrambles_to_import << new_scr
             end
           end
         end
