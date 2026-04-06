79c7b772d11dc1037be66405220322cd049d1a53
Gregor Billing <gbilling@worldcubeassociation.org>
2026-04-06 10:56:06 +0200

    Include extras in MatchedScrambles key (#13870)
    * Re-span matching index to cover extras
    
    * Consider extras in matching UI

--- app/models/matched_scramble.rb
+++ app/models/matched_scramble.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MatchedScramble < ApplicationRecord
-  default_scope { order(:ordered_index) }
+  default_scope { order(:is_extra, :ordered_index) }
 
   belongs_to :matched_scramble_set
   belongs_to :external_scramble, optional: true
@@ -9,7 +9,7 @@ class MatchedScramble < ApplicationRecord
   scope :not_extra, -> { where(is_extra: false) }
 
   validates :ordered_index, numericality: { only_integer: true, greater_than_or_equal_to: 0 },
-                            uniqueness: { scope: :matched_scramble_set_id }
+                            uniqueness: { scope: %i[is_extra matched_scramble_set_id] }
 
   delegate :scramble_string, to: :external_scramble, prefix: :external, allow_nil: true
 
