7308494959e29c1c178b82f86702ce7cfa2b3eb6
FinnIckler <finnickler@gmail.com>
2026-04-07 14:01:00 +0200

    lint

--- db/migrate/20260407105148_add_live_attempts_count_to_live_results.rb
+++ db/migrate/20260407105148_add_live_attempts_count_to_live_results.rb
@@ -3,11 +3,13 @@
 class AddLiveAttemptsCountToLiveResults < ActiveRecord::Migration[8.1]
   def change
     add_column :live_results, :live_attempts_count, :integer, default: 0, null: false
-    execute <<~SQL
-      UPDATE live_results lr
-      SET live_attempts_count = (
-        SELECT COUNT(*) FROM live_attempts la WHERE la.live_result_id = lr.id
-      )
-    SQL
+    up_only do
+      execute <<~SQL.squish
+        UPDATE live_results lr
+        SET live_attempts_count = (
+          SELECT COUNT(*) FROM live_attempts la WHERE la.live_result_id = lr.id
+        )
+      SQL
+    end
   end
 end
