4ace008e75903145fd1a05fd079628e4625b1de4
FinnIckler <finnickler@gmail.com>
2026-04-07 18:16:58 +0200

    reset_counters in UpdateLiveResultJob

--- app/jobs/update_live_result_job.rb
+++ app/jobs/update_live_result_job.rb
@@ -14,7 +14,10 @@ class UpdateLiveResultJob < ApplicationJob
       attempt_numbers = results.pluck(:attempt_number)
       live_result.live_attempts.where.not(attempt_number: attempt_numbers).delete_all
 
-      new_attempts = live_result.live_attempts.reload # We did some `upsert_all` and `delete_all` shenanigans above, which bypass Rails memory. Hence reloading...
+      # We did some `upsert_all` and `delete_all` shenanigans above, which bypass Rails memory. Hence reloading and resetting...
+      new_attempts = live_result.live_attempts.reload
+      LiveResult.reset_counters(live_result.id, :live_attempts)
+
       average, best = LiveResult.compute_average_and_best(new_attempts, round)
 
       live_result.update!(best: best, average: average, last_attempt_entered_at: Time.now.utc)
