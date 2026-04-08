c121c9f01cfa355c031551dca5efefe61c1f249f
FinnIckler <finnickler@gmail.com>
2026-04-08 13:29:45 +0200

    Show Toaster warning when result is locked.

--- next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/AddResults.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/AddResults.tsx
@@ -1,14 +1,14 @@
-"use client";
 import { SimpleGrid, GridItem } from "@chakra-ui/react";
 import AttemptsForm from "@/components/live/AttemptsForm";
 import formats from "@/lib/wca/data/formats";
 import LiveUpdatingResultsTable from "@/components/live/LiveUpdatingResultsTable";
 import { parseActivityCode } from "@/lib/wca/wcif/rounds";
-import { LiveResultAdminProvider } from "@/providers/LiveResultAdminProvider";
 import { LiveRoundAdminBase } from "@/types/live";
 import ConfirmProvider from "@/providers/ConfirmProvider";
+import { toaster } from "@/components/ui/toaster";
+import { getT } from "@/lib/i18n/get18n";
 
-export default function AddResults({
+export default async function AddResults({
   competitionId,
   roundName,
   round,
@@ -20,30 +20,39 @@ export default function AddResults({
   const { eventId } = parseActivityCode(round.id);
   const format = formats.byId[round.format];
 
-  return (
-    <LiveResultAdminProvider round={round} competitionId={competitionId}>
-      <SimpleGrid columns={16} gap={6}>
-        <GridItem colSpan={4}>
-          <ConfirmProvider>
-            <AttemptsForm
-              header="Add Result"
-              eventId={eventId}
-              solveCount={format.expected_solve_count}
-            />
-          </ConfirmProvider>
-        </GridItem>
+  const { t } = await getT();
 
-        <GridItem colSpan={12}>
-          <LiveUpdatingResultsTable
-            roundWcifId={round.id}
-            formatId={round.format}
-            competitionId={competitionId}
-            isAdminView
-            canManage
-            title={roundName}
+  const isLocked = round.state === "locked";
+
+  if (isLocked) {
+    toaster.create({
+      description: t("competitions.live.admin.warnings.round_locked"),
+      type: "info",
+    });
+  }
+
+  return (
+    <SimpleGrid columns={16} gap={6}>
+      <GridItem colSpan={4}>
+        <ConfirmProvider>
+          <AttemptsForm
+            header="Add Result"
+            eventId={eventId}
+            solveCount={format.expected_solve_count}
           />
-        </GridItem>
-      </SimpleGrid>
-    </LiveResultAdminProvider>
+        </ConfirmProvider>
+      </GridItem>
+
+      <GridItem colSpan={12}>
+        <LiveUpdatingResultsTable
+          roundWcifId={round.id}
+          formatId={round.format}
+          competitionId={competitionId}
+          isAdminView
+          canManage
+          title={roundName}
+        />
+      </GridItem>
+    </SimpleGrid>
   );
 }
