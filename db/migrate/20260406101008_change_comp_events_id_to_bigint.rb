450fd97b9b7fdb9e2ef2942e3fd308966ce53fe6
Gregor Billing <gbilling@worldcubeassociation.org>
2026-04-08 14:39:39 +0200

    Add sanitizer queries

--- db/migrate/20260406101008_change_comp_events_id_to_bigint.rb
+++ db/migrate/20260406101008_change_comp_events_id_to_bigint.rb
@@ -4,6 +4,11 @@ class ChangeCompEventsIdToBigint < ActiveRecord::Migration[8.1]
   def change
     reversible do |dir|
       dir.up do
+        execute "DELETE FROM rounds WHERE competition_event_id NOT IN (SELECT id FROM competition_events)"
+
+        execute "DELETE FROM registration_competition_events WHERE registration_id IS NULL"
+        execute "DELETE FROM registration_competition_events WHERE competition_event_id NOT IN (SELECT id FROM competition_events)"
+
         change_column :competition_events, :id, :bigint
 
         change_column :registration_competition_events, :competition_event_id, :bigint
