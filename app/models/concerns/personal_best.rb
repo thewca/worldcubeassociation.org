c6492f1e81e01b235df12d5c64f91e47fd74321e
Gregor Billing <gbilling@worldcubeassociation.org>
2026-04-09 07:24:49 +0200

    Simplify serialization in PersonalBest

--- app/models/concerns/personal_best.rb
+++ app/models/concerns/personal_best.rb
@@ -5,47 +5,36 @@ require 'active_support/concern'
 module PersonalBest
   extend ActiveSupport::Concern
 
+  def self.rank_value_key(version: Competition::WCIF_STABLE_VERSION)
+    if Gem::Version.new(version) >= Gem::Version.new("2.0.0")
+      "value"
+    else
+      "best"
+    end
+  end
+
   def rank_to_wcif(type, version: Competition::WCIF_STABLE_VERSION)
-    base_wcif = {
+    {
       "eventId" => event_id,
+      PersonalBest.rank_value_key(version: version) => best,
       "worldRanking" => world_rank,
       "continentalRanking" => continent_rank,
       "nationalRanking" => country_rank,
       "type" => type,
     }
-
-    if Gem::Version.new(version) >= Gem::Version.new("2.0.0")
-      base_wcif.merge("value" => best)
-    else
-      base_wcif.merge("best" => best)
-    end
   end
 
   def self.wcif_json_schema(version: Competition::WCIF_STABLE_VERSION)
-    if Gem::Version.new(version) >= Gem::Version.new("2.0.0")
-      {
-        "type" => "object",
-        "properties" => {
-          "eventId" => { "type" => "string", "enum" => Event.pluck(:id) },
-          "value" => { "type" => "integer" },
-          "worldRanking" => { "type" => "integer" },
-          "continentalRanking" => { "type" => "integer" },
-          "nationalRanking" => { "type" => "integer" },
-          "type" => { "type" => "string", "enum" => %w[single average] },
-        },
-      }
-    else
-      {
-        "type" => "object",
-        "properties" => {
-          "eventId" => { "type" => "string", "enum" => Event.pluck(:id) },
-          "best" => { "type" => "integer" },
-          "worldRanking" => { "type" => "integer" },
-          "continentalRanking" => { "type" => "integer" },
-          "nationalRanking" => { "type" => "integer" },
-          "type" => { "type" => "string", "enum" => %w[single average] },
-        },
-      }
-    end
+    {
+      "type" => "object",
+      "properties" => {
+        "eventId" => { "type" => "string", "enum" => Event.pluck(:id) },
+        PersonalBest.rank_value_key(version: version) => { "type" => "integer" },
+        "worldRanking" => { "type" => "integer" },
+        "continentalRanking" => { "type" => "integer" },
+        "nationalRanking" => { "type" => "integer" },
+        "type" => { "type" => "string", "enum" => %w[single average] },
+      },
+    }
   end
 end
