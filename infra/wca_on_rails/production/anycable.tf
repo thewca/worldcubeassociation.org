dc2f50964a3087476dad46b0842e5d19d631aa34
FinnIckler <finnickler@gmail.com>
2026-04-10 14:23:01 +0200

    use separate anycable redis

--- infra/wca_on_rails/production/anycable.tf
+++ infra/wca_on_rails/production/anycable.tf
@@ -22,7 +22,7 @@ locals {
     },
     {
       name = "ANYCABLE_REDIS_URL",
-      value = "redis://wca-main-sidekiq.iebvzt.ng.0001.usw2.cache.amazonaws.com:6379"
+      value = "redis://anycable-production-cache-0001-001.anycable-production-cache.iebvzt.usw2.cache.amazonaws.com:6379"
     },
     {
       name = "ANYCABLE_PATH",
