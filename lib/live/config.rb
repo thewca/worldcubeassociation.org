e93d3a3f64cbf1fc63e25ff32a538add3eaabeda
FinnIckler <finnickler@gmail.com>
2026-04-10 17:20:05 +0200

    enable WCA Live in Config

--- lib/live/config.rb
+++ lib/live/config.rb
@@ -3,13 +3,11 @@
 module Live
   module Config
     def self.enabled?
-      # We have to return `true` during assets compilation so that all routes
-      #   are defined and can be exported to the JS routes ERB template.
-      !EnvConfig.WCA_LIVE_SITE? || EnvConfig.ASSETS_COMPILATION?
+      true
     end
 
     def self.sqs_queued?
-      Rails.env.production? && self.enabled?
+      Rails.env.production?
     end
 
     def self.broadcast_key(competition_id, round_id)
