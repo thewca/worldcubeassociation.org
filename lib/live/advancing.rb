d4c5c2284888d9c115bb268b8e2245958cbbfa59
FinnIckler <finnickler@gmail.com>
2026-04-13 19:19:05 +0200

    fix live advancing module

--- lib/live/advancing.rb
+++ lib/live/advancing.rb
@@ -31,22 +31,22 @@ module Live
 
       live_results.where(id: hypothetically_advancing_ids & candidate_ids)
     end
-  end
 
-  def self.recompute_advancing(results_per_person, results_to_update, advancement_determining_condition, can_update_advancing: true)
-    results_with_potential = results_per_person.select { it.locked_by_id.nil? }.sort_by(&:potential_solve_time)
+    def self.recompute_advancing(results_per_person, results_to_update, advancement_determining_condition, can_update_advancing: true)
+      results_with_potential = results_per_person.select { it.locked_by_id.nil? }.sort_by(&:potential_solve_time)
 
-    advancing_ids = advancement_determining_condition.apply(results_with_potential).pluck(:registration_id)
-    max_advancing = advancement_determining_condition.max_qualifying(results_with_potential)
+      advancing_ids = advancement_determining_condition.apply(results_with_potential).pluck(:registration_id)
+      max_advancing = advancement_determining_condition.max_qualifying(results_with_potential)
 
-    if can_update_advancing && advancing_ids.any?
-      results_to_update.update_all(
-        ["advancing = (registration_id IN (?)), advancing_questionable = (global_pos <= ?)", advancing_ids, max_advancing],
-      )
-    else
-      results_to_update.update_all(
-        ["advancing = FALSE, advancing_questionable = (global_pos <= ?)", max_advancing],
-      )
+      if can_update_advancing && advancing_ids.any?
+        results_to_update.update_all(
+          ["advancing = (registration_id IN (?)), advancing_questionable = (global_pos <= ?)", advancing_ids, max_advancing],
+        )
+      else
+        results_to_update.update_all(
+          ["advancing = FALSE, advancing_questionable = (global_pos <= ?)", max_advancing],
+        )
+      end
     end
   end
 end
