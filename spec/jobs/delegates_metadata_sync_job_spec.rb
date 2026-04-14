75d3fba760a5ef174f385f4a505b1786302d64c7
Maksymilian Gala <maksymilian.gala1@gmail.com>
2026-04-14 21:59:39 +0200

    Fix tests

--- spec/jobs/delegates_metadata_sync_job_spec.rb
+++ spec/jobs/delegates_metadata_sync_job_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe DelegatesMetadataSyncJob do
   describe "delegates metadata sync job" do
     let(:delegate) { create(:delegate_role) }
     let(:delegate2) { create(:delegate_role) }
-    let(:competition1) { create(:competition, :visible, delegates: [delegate.user, delegate2.user], start_date: 4.weeks.ago, end_date: 4.weeks.ago + 2.days, lead_delegate_id: delegate2.id) }
-    let(:competition2) { create(:competition, :past, :visible, delegates: [delegate.user, delegate2.user], start_date: 2.weeks.ago, end_date: 2.weeks.ago + 3.days, lead_delegate_id: delegate.id) }
+    let(:competition1) { create(:competition, :visible, delegates: [delegate.user, delegate2.user], start_date: 4.weeks.ago, end_date: 4.weeks.ago + 2.days, lead_delegate_id: delegate2.user.id) }
+    let(:competition2) { create(:competition, :past, :visible, delegates: [delegate.user, delegate2.user], start_date: 2.weeks.ago, end_date: 2.weeks.ago + 3.days, lead_delegate_id: delegate.user.id) }
 
     it "syncs successfully" do
       competition1.delegates.reload
