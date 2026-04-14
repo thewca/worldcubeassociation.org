6bc25fce660375052f447138a058f8dcc3694ef4
FinnIckler <finnickler@gmail.com>
2026-04-14 16:50:00 +0200

    use Alert instead

--- next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/AddResults.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/AddResults.tsx
@@ -1,17 +1,14 @@
-"use client";
 import { SimpleGrid, GridItem } from "@chakra-ui/react";
+import ClosableAlert from "@/components/ui/ClosableAlert";
 import AttemptsForm from "@/components/live/AttemptsForm";
 import formats from "@/lib/wca/data/formats";
 import LiveUpdatingResultsTable from "@/components/live/LiveUpdatingResultsTable";
 import { parseActivityCode } from "@/lib/wca/wcif/rounds";
-import { LiveResultAdminProvider } from "@/providers/LiveResultAdminProvider";
 import { LiveRoundAdminBase } from "@/types/live";
 import ConfirmProvider from "@/providers/ConfirmProvider";
-import { toaster } from "@/components/ui/toaster";
-import { useT } from "@/lib/i18n/useI18n";
-import { useEffect } from "react";
+import { getT } from "@/lib/i18n/get18n";
 
-export default function AddResults({
+export default async function AddResults({
   competitionId,
   roundName,
   round,
@@ -23,43 +20,38 @@ export default function AddResults({
   const { eventId } = parseActivityCode(round.id);
   const format = formats.byId[round.format];
 
-  const { t } = useT();
+  const { t } = await getT();
 
-  const isLocked = round.state === "locked";
-
-  useEffect(() => {
-    if (isLocked) {
-      toaster.create({
-        description: t("competitions.live.admin.warnings.round_locked"),
-        type: "info",
-      });
-    }
-  }, [isLocked, t]);
+  const isLocked = true;
 
   return (
-    <LiveResultAdminProvider round={round} competitionId={competitionId}>
-      <ConfirmProvider>
-        <SimpleGrid columns={16} gap={6}>
-          <GridItem colSpan={4}>
-            <AttemptsForm
-              header="Add Result"
-              eventId={eventId}
-              solveCount={format.expected_solve_count}
-            />
-          </GridItem>
+    <ConfirmProvider>
+      {isLocked && (
+        <ClosableAlert
+          status="warning"
+          title={t("competitions.live.admin.warnings.round_locked")}
+        />
+      )}
+      <SimpleGrid columns={16} gap={6}>
+        <GridItem colSpan={4}>
+          <AttemptsForm
+            header="Add Result"
+            eventId={eventId}
+            solveCount={format.expected_solve_count}
+          />
+        </GridItem>
 
-          <GridItem colSpan={12}>
-            <LiveUpdatingResultsTable
-              roundWcifId={round.id}
-              formatId={round.format}
-              competitionId={competitionId}
-              isAdminView
-              canManage
-              title={roundName}
-            />
-          </GridItem>
-        </SimpleGrid>
-      </ConfirmProvider>
-    </LiveResultAdminProvider>
+        <GridItem colSpan={12}>
+          <LiveUpdatingResultsTable
+            roundWcifId={round.id}
+            formatId={round.format}
+            competitionId={competitionId}
+            isAdminView
+            canManage
+            title={roundName}
+          />
+        </GridItem>
+      </SimpleGrid>
+    </ConfirmProvider>
   );
 }
