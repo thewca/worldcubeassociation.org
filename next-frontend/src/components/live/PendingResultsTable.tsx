344fadb3e3f11e60b1e6554ff9871ed3d80a4a7b
FinnIckler <finnickler@gmail.com>
2026-04-15 13:56:21 +0200

    add heading and use collapsible for a nicer UI experience

--- next-frontend/src/components/live/PendingResultsTable.tsx
+++ next-frontend/src/components/live/PendingResultsTable.tsx
@@ -1,4 +1,4 @@
-import { Table } from "@chakra-ui/react";
+import { Collapsible, Heading, Table, VStack } from "@chakra-ui/react";
 import { formatAttemptResult } from "@/lib/wca/wcif/attempts";
 import formats from "@/lib/wca/data/formats";
 import { padSkipped } from "@/lib/live/padSkipped";
@@ -20,45 +20,54 @@ export default function PendingResultsTable({
 
   const attemptIndexes = [...Array(solveCount).keys()];
 
-  if (pendingLiveResults.length > 0) {
-    return (
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>Competitor</Table.ColumnHeader>
-            <Table.ColumnHeader>Country</Table.ColumnHeader>
-            {attemptIndexes.map((num) => (
-              <Table.ColumnHeader key={num} textAlign="right">
-                {num + 1}
-              </Table.ColumnHeader>
-            ))}
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {pendingLiveResults.map((pendingResult) => {
-            const competitor = competitors.get(pendingResult.registration_id)!;
-
-            return (
-              <Table.Row key={`${pendingResult.registration_id}`}>
-                <Table.Cell>{competitor.registrant_id}</Table.Cell>
-                <Table.Cell>{competitor.name}</Table.Cell>
-                {padSkipped(
-                  pendingResult.attempts,
-                  format.expected_solve_count,
-                ).map((attempt) => (
-                  <Table.Cell
-                    textAlign="right"
-                    key={`${competitor.id}-${attempt.attempt_number}`}
-                  >
-                    {formatAttemptResult(attempt.value, eventId)}
-                  </Table.Cell>
+  return (
+    <Collapsible.Root open={pendingLiveResults.length > 0}>
+      <Collapsible.Content>
+        <VStack align="start">
+          <Heading textStyle="h3" textAlign="left">
+            Processing
+          </Heading>
+          <Table.Root>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeader>Competitor</Table.ColumnHeader>
+                <Table.ColumnHeader>Country</Table.ColumnHeader>
+                {attemptIndexes.map((num) => (
+                  <Table.ColumnHeader key={num} textAlign="right">
+                    {num + 1}
+                  </Table.ColumnHeader>
                 ))}
               </Table.Row>
-            );
-          })}
-        </Table.Body>
-      </Table.Root>
-    );
-  }
+            </Table.Header>
+
+            <Table.Body>
+              {pendingLiveResults.map((pendingResult) => {
+                const competitor = competitors.get(
+                  pendingResult.registration_id,
+                )!;
+
+                return (
+                  <Table.Row key={`${pendingResult.registration_id}`}>
+                    <Table.Cell>{competitor.registrant_id}</Table.Cell>
+                    <Table.Cell>{competitor.name}</Table.Cell>
+                    {padSkipped(
+                      pendingResult.attempts,
+                      format.expected_solve_count,
+                    ).map((attempt) => (
+                      <Table.Cell
+                        textAlign="right"
+                        key={`${competitor.id}-${attempt.attempt_number}`}
+                      >
+                        {formatAttemptResult(attempt.value, eventId)}
+                      </Table.Cell>
+                    ))}
+                  </Table.Row>
+                );
+              })}
+            </Table.Body>
+          </Table.Root>
+        </VStack>
+      </Collapsible.Content>
+    </Collapsible.Root>
+  );
 }
