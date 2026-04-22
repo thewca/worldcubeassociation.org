2f5b83d4e6480a11879aa29fe9ccc736c474318b
Gregor Billing <gbilling@worldcubeassociation.org>
2026-04-22 23:04:29 +0200

    Hot-again-fix: A third pass it is

--- app/models/competition_event.rb
+++ app/models/competition_event.rb
@@ -128,10 +128,14 @@ class CompetitionEvent < ApplicationRecord
     end
     # Have to do this in a second pass because nested associations (mostly `linked_round` and `participation_source`)
     #   need the record to already exist in the database in order to reference their IDs
-    new_rounds = model_rounds.zip(wcif["rounds"]).map do |round, round_wcif|
+    new_rounds = model_rounds.map do |round|
       round.update!(**Round.wcif_backlinking(round, model_rounds))
-      round.load_live_results!(round_wcif["results"], current_user) if round_wcif["results"]
       round
+    end
+    # This is not techincally a third pass, because we're not updating the round itself.
+    #   But for the advancement through rounds, the whole CE already needs to be fully linked up
+    new_rounds.zip(wcif["rounds"]).each do |round, round_wcif|
+      round.load_live_results!(round_wcif["results"], current_user) if round_wcif["results"].present?
     end
     wcif_qualification = CompetitionEvent.load_wcif_qualification(wcif, version: version)
     self.update!(
