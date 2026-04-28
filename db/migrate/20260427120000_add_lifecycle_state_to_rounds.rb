769700c29ad79d33bf3b20b277d6184819233aa9
FinnIckler <finnickler@gmail.com>
2026-04-27 20:56:50 -0400

    make migration more efficient

--- db/migrate/20260427120000_add_lifecycle_state_to_rounds.rb
+++ db/migrate/20260427120000_add_lifecycle_state_to_rounds.rb
@@ -1,9 +1,10 @@
-class AddLifecycleStateToRounds < ActiveRecord::Migration[7.2]
+class AddLifecycleStateToRounds < ActiveRecord::Migration[8.1]
   def up
     add_column :rounds, :lifecycle_state, :string, limit: 10, null: false, default: "pending"
 
-    Round.reset_column_information
-    Round.find_each do |round|
+    Round.joins(:competition).where.not(competitions: { results_submitted_at: nil }).update_all(lifecycle_state: "done")
+
+    Round.joins(:competition).where(competitions: { results_submitted_at: nil }).find_each do |round|
       round.update_columns(lifecycle_state: round.inferred_lifecycle_state)
     end
   end
