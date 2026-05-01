d567e4f6a999f919a0e1a6b309c1f1384b99c3b8
FinnIckler <finnickler@gmail.com>
2026-05-01 13:26:03 +0200

    AddPerson fixes

--- next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/AddPerson.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/AddPerson.tsx
@@ -114,7 +114,9 @@ function AddPersonCombobox({
   registrations: RegistrationData[];
 }) {
   const { collection, filter } = useListCollection({
-    initialItems: registrations,
+    initialItems: registrations.toSorted(
+      (a, b) => a.registrant_id - b.registrant_id,
+    ),
     itemToValue: (competitor) => competitor.id.toString(),
     itemToString: (competitor) => competitor.user.name,
     filter: (itemText, filterText, item) =>
@@ -126,6 +128,7 @@ function AddPersonCombobox({
     <VStack>
       <Combobox.Root
         collection={collection}
+        inputBehavior="autohighlight"
         onInputValueChange={(e) => filter(e.inputValue)}
         onValueChange={(e) => setSelectedCompetitor(parseInt(e.value[0], 10))}
       >
