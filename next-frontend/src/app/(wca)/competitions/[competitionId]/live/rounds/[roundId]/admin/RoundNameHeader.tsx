23fb1489ae42a8eb6a3391d00ab184ac33f01782
FinnIckler <finnickler@gmail.com>
2026-05-06 10:34:43 +0200

    refactor DoubleCheck

--- next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/RoundNameHeader.tsx
+++ next-frontend/src/app/(wca)/competitions/[competitionId]/live/rounds/[roundId]/admin/RoundNameHeader.tsx
@@ -1,15 +1,10 @@
 "use client";
 
 import { Heading } from "@chakra-ui/react";
-import { getRoundName } from "@/lib/wca/live/getRoundName";
-import { TFunction } from "i18next";
-import { useAllRoundsInfo, useRoundInfo } from "@/providers/RoundInfoProvider";
+import { useRoundName } from "@/lib/wca/live/getRoundName";
 
-export default function RoundNameHeader({ t }: { t: TFunction }) {
-  const { rounds } = useAllRoundsInfo();
-  const { id } = useRoundInfo();
-
-  const roundName = getRoundName(id, t, rounds);
+export default function RoundNameHeader() {
+  const roundName = useRoundName();
 
   return <Heading textStyle="h1">{roundName}</Heading>;
 }
