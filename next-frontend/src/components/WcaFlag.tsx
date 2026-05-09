6db93706f62cfa3ff40086c63eb26f1daac6e38c
Simon Kelly <26287997+simonkellly@users.noreply.github.com>
2026-05-09 08:26:30 +0100

    Switch TW Flag to react-world-flags format

--- next-frontend/src/components/WcaFlag.tsx
+++ next-frontend/src/components/WcaFlag.tsx
@@ -1,26 +1,16 @@
-import _TwFlag from "@/components/icons/flags/_TwFlag";
-
 import type { ComponentPropsWithoutRef } from "react";
 import Flag from "react-world-flags";
 
-type FlagProps = ComponentPropsWithoutRef<typeof Flag>;
-type TwFlagProps = ComponentPropsWithoutRef<typeof _TwFlag>;
-
-type WcaFlagProps = {
-  code?: string,
-  fallback?: React.ReactNode | null;
-} & Pick<
-  FlagProps & TwFlagProps,
-  Extract<keyof FlagProps, keyof TwFlagProps>
->;
-
-const WcaFlag = (props: WcaFlagProps) => {
-  if (props.code?.toUpperCase() === "TW") {
-    const { code: _code, fallback: _fallback, ...twProps } = props;
-    return <_TwFlag {...twProps} />;
+const WcaFlag = ({
+  code,
+  fallback,
+  ...rest
+}: ComponentPropsWithoutRef<typeof Flag>) => {
+  if (code?.toUpperCase() === "TW") {
+    return <img {...rest} src="/flags/FLAG_TW.svg" />;
   }
 
-  return <Flag {...props} />;
+  return <Flag code={code} fallback={fallback} {...rest} />;
 };
 
 export default WcaFlag;
