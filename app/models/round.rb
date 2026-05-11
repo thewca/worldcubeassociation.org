e43a94047646f827e75991966a20cc7c7e094e2b
FinnIckler <finnickler@gmail.com>
2026-05-11 14:43:38 +0200

    use or

--- app/models/round.rb
+++ app/models/round.rb
@@ -318,15 +318,17 @@ class Round < ApplicationRecord
     if live_results.loaded?
       live_results.count(&:complete?)
     else
-      cutoff_done = if cutoff.blank?
-                      0
-                    else
-                      live_results
-                        .where(live_attempts_count: cutoff.number_of_attempts)
-                        .where("best >= ? OR best < 0", cutoff.attempt_result)
-                        .count
-                    end
-      live_results.where(live_attempts_count: format.expected_solve_count).count + cutoff_done
+      fully_done = live_results.where(live_attempts_count: format.expected_solve_count)
+
+      if cutoff.present?
+        only_done_cutoff_attempts = live_results
+                                    .where(live_attempts_count: cutoff.number_of_attempts)
+        didnt_meet_cutoff = only_done_cutoff_attempts.where(best: cutoff.attempt_result..)
+                                                     .or(only_done_cutoff_attempts.where(best: ...0))
+        return fully_done.or(didnt_meet_cutoff).count
+      end
+
+      fully_done.count
     end
   end
 
