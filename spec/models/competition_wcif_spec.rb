8966707a6a12ef2c706b293568d36c5429ffe4e3
FinnIckler <finnickler@gmail.com>
2026-05-11 14:23:16 +0200

    revert test changes

--- spec/models/competition_wcif_spec.rb
+++ spec/models/competition_wcif_spec.rb
@@ -1259,14 +1259,14 @@ RSpec.describe "Competition WCIF" do
         wcif_333_event["rounds"][0]["results"] = [
           {
             "personId" => competitors[0].registrant_id,
-            "ranking" => 1,
+            "ranking" => 10,
             "attempts" => [{ "result" => 456, "reconstruction" => nil }] * 5,
             "best" => 456,
             "average" => 456,
           },
           {
             "personId" => competitors[1].registrant_id,
-            "ranking" => 2,
+            "ranking" => 5,
             "attempts" => [{ "result" => 784, "reconstruction" => nil }] * 5,
             "best" => 784,
             "average" => 784,
