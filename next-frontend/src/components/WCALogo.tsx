279c001004db9afbe34717762dbff2555a47a2ac
FinnIckler <finnickler@gmail.com>
2026-05-13 18:45:57 +0200

    use ColoModeValue

--- next-frontend/src/components/WCALogo.tsx
+++ next-frontend/src/components/WCALogo.tsx
@@ -1,33 +1,24 @@
+"use client";
+
 import Link from "next/link";
-import { Box, Image as ChakraImage } from "@chakra-ui/react";
+import { Image as ChakraImage } from "@chakra-ui/react";
 import Image from "next/image";
 import { IconButton } from "@chakra-ui/react";
 import React from "react";
+import { useColorModeValue } from "@/components/ui/color-mode";
 
 export default function WCALogo() {
+  const { src, alt } = useColorModeValue(
+    { src: "/logo.png", alt: "Wca Logo Light" },
+    { src: "/logo_dark.png", alt: "Wca Logo Dark" },
+  );
+
   return (
     <IconButton asChild variant="ghost">
       <Link href="/">
-        <Box _dark={{ display: "none" }}>
-          <ChakraImage asChild maxW={10}>
-            <Image
-              src="/logo.png"
-              alt="WCA Logo Light"
-              height={50}
-              width={50}
-            />
-          </ChakraImage>
-        </Box>
-        <Box display="none" _dark={{ display: "block" }}>
-          <ChakraImage asChild maxW={10}>
-            <Image
-              src="/logo_dark.png"
-              alt="WCA Logo Dark"
-              height={50}
-              width={50}
-            />
-          </ChakraImage>
-        </Box>
+        <ChakraImage asChild maxW={10}>
+          <Image src={src} alt={alt} height={50} width={50} />
+        </ChakraImage>
       </Link>
     </IconButton>
   );
