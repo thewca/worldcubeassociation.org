7bc7394dbc0b6a00c39d2b979465c2fbd1756db1
FinnIckler <finnickler@gmail.com>
2026-05-18 15:43:33 +0200

    Add PR tag when creating Live Results

--- app/jobs/update_live_result_job.rb
+++ app/jobs/update_live_result_job.rb
@@ -22,14 +22,52 @@ class UpdateLiveResultJob < ApplicationJob
       # so we can't set it directly in update!.
       LiveResult.reset_counters(live_result.id, :live_attempts)
 
+      person = live_result.registration.person
+
       live_result.update!(
         best: best,
         average: average,
         last_attempt_entered_at: Time.now.utc,
+        single_record_tag: compute_pr(live_result, best, person, :single),
+        average_record_tag: compute_pr(live_result, average, person, :average),
       )
 
       history_ordered_results = new_attempts.order(:attempt_number).pluck(:value)
       live_result.live_result_history_entries.create!(entered_by_id: entered_by_id, action_type: :scoretaking, attempt_details: history_ordered_results)
     end
   end
+
+  private
+
+  VALUE_COLUMN = {
+    single: :best,
+    average: :average,
+  }
+
+  def compute_pr(live_result, value, person, type)
+    col = VALUE_COLUMN[type]
+    return nil if value <= 0
+    return nil if better_pr_in_previous_round?(live_result, "#{type}_record_tag", col, value)
+
+    if person.nil?
+      "PR"
+    else
+      pr = person.public_send(:"ranks_#{type}").find { |r| r.event_id == live_result.event_id }
+      "PR" if pr.nil? || value < pr.public_send(col)
+    end
+  end
+
+    def better_pr_in_previous_round?(live_result, tag_column, value_column, current_value)
+      round = live_result.round
+      previous_ids = round.competition_event.rounds
+                          .where(number: ...round.number)
+                          .ids
+      return false if previous_ids.empty?
+
+      best_previous_pr = LiveResult.where(registration_id: live_result.registration_id, round_id: previous_ids)
+                                   .where(tag_column => "PR")
+                                   .minimum(value_column)
+
+      best_previous_pr.present? && best_previous_pr <= current_value
+    end
 end
