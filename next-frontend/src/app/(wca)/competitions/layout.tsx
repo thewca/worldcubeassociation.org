fd4f9cf3cb893ec63b3406d0cc08c6721a215d72
FinnIckler <finnickler@gmail.com>
2026-05-18 17:38:11 +0200

    add comment

--- next-frontend/src/app/(wca)/competitions/layout.tsx
+++ next-frontend/src/app/(wca)/competitions/layout.tsx
@@ -8,5 +8,6 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
+  // Required by Next.js; this layout exists only to attach metadata to this route segment
   return children;
 }
