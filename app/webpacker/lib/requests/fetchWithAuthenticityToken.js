83ddce61d10275550ee4a6f9b07a07ee519a339d
FinnIckler <finnickler@gmail.com>
2026-05-27 10:44:43 +0200

    check content type before trying to parse

--- app/webpacker/lib/requests/fetchWithAuthenticityToken.js
+++ app/webpacker/lib/requests/fetchWithAuthenticityToken.js
@@ -14,14 +14,16 @@ export function fetchWithAuthenticityToken(url, fetchOptions) {
 
 export function fetchJsonOrError(url, fetchOptions = {}) {
   return fetchWithAuthenticityToken(url, fetchOptions)
-    .then((response) => response.json()
-      .then((json) => {
+    .then((response) => {
+      const contentType = response.headers.get('content-type');
+      if (!contentType || !contentType.includes('application/json')) {
+        throw new FetchJsonError(`${response.status}: ${response.statusText}\nPlease Report this to WST\nRequestId: ${response.headers.get('x-request-id')}`, response, { status: 'An unexpected error occurred.' });
+      }
+      return response.json().then((json) => {
         if (!response.ok) {
           throw new FetchJsonError(`${response.status}: ${response.statusText}\n${json.error}`, response, json);
         }
         return { data: json, headers: response.headers };
-      }).catch((err) => {
-        if (err instanceof FetchJsonError) throw err;
-        throw new FetchJsonError(`${response.status}: ${response.statusText}\nPlease Report this to WST\nRequestId: ${response.headers.get("x-request-id")}`, response, { status: 'An unexpected error occurred.' });
-      }));
+      });
+    });
 }
