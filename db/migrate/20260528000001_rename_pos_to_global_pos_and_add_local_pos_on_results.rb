180ee5a823717b9a20bd5cb4235107c272106101
FinnIckler <finnickler@gmail.com>
2026-05-28 14:09:43 +0200

    only add global_pos

--- db/migrate/20260528000001_rename_pos_to_global_pos_and_add_local_pos_on_results.rb
+++ db/migrate/20260528000001_rename_pos_to_global_pos_and_add_local_pos_on_results.rb
@@ -2,13 +2,11 @@
 
 class RenamePosToGlobalPosAndAddLocalPosOnResults < ActiveRecord::Migration[8.1]
   def up
-    rename_column :results, :pos, :local_pos
     add_column :results, :global_pos, :integer, limit: 2, default: 0, null: false
-    execute "UPDATE results SET global_pos = local_pos"
+    execute "UPDATE results SET global_pos = pos"
   end
 
   def down
     remove_column :results, :global_pos
-    rename_column :results, :local_pos, :pos
   end
 end
