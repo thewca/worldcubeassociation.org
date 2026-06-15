fcfc4c039a3925a31791614433cfe3d4b5c0ce57
FinnIckler <finnickler@gmail.com>
2026-06-15 16:56:10 +0200

    Change payload media upload config (no crop) (#14702)

--- next-frontend/src/types/payload.ts
+++ next-frontend/src/types/payload.ts
@@ -434,6 +434,24 @@ export interface Media {
   height?: number | null;
   focalX?: number | null;
   focalY?: number | null;
+  sizes?: {
+    thumbnail?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+    card?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+  };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -726,6 +744,30 @@ export interface MediaSelect<T extends boolean = true> {
   height?: T;
   focalX?: T;
   focalY?: T;
+  sizes?:
+    | T
+    | {
+        thumbnail?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+        card?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+      };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -1250,6 +1292,9 @@ export interface ImageBannerBlock {
 export interface ImageOnlyCardBlock {
   mainImage: string | Media;
   heading?: string | null;
+  /**
+   * Optional. If set, the whole card becomes a link to this URL.
+   */
   url?: string | null;
   textPosition?: ('top' | 'bottom') | null;
   colorPalette: ColorPaletteSelect;
