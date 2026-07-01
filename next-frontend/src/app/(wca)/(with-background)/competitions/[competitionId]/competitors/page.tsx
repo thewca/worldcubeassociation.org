b2a736c541249747c597a1807b5ce30ec119c1a6
FinnIckler <finnickler@gmail.com>
2026-07-01 17:26:59 +0200

    Add Link to the on the spot page on the competitor page

--- next-frontend/src/app/(wca)/(with-background)/competitions/[competitionId]/competitors/page.tsx
+++ next-frontend/src/app/(wca)/(with-background)/competitions/[competitionId]/competitors/page.tsx
@@ -3,6 +3,7 @@ import { getCompetitionInfo } from "@/lib/wca/competitions/getCompetitionInfo";
 import { hasPassed } from "@/lib/wca/dates";
 import OpenapiError from "@/components/ui/openapiError";
 import { getT } from "@/lib/i18n/get18n";
+import getPermissions from "@/lib/wca/permissions";
 
 export default async function Competitors({
   params,
@@ -12,22 +13,28 @@ export default async function Competitors({
   const { competitionId } = await params;
   const { t } = await getT();
 
-  const {
-    data: competitionInfo,
-    error,
-    response,
-  } = await getCompetitionInfo(competitionId);
+  const [{ data: competitionInfo, error, response }, permissions] =
+    await Promise.all([
+      getCompetitionInfo(competitionId),
+      getPermissions(),
+    ]);
 
   if (error) {
     return <OpenapiError response={response} t={t} />;
   }
 
+  const isLive =
+    hasPassed(competitionInfo.start_date) &&
+    !hasPassed(competitionInfo.end_date);
+
   return (
     <TabCompetitors
       id={competitionId}
-      isLive={
-        hasPassed(competitionInfo.start_date) &&
-        !hasPassed(competitionInfo.end_date)
+      isLive={isLive}
+      canAddOnTheSpot={
+        isLive &&
+        !!permissions &&
+        permissions.canAdministerCompetition(competitionId)
       }
     />
   );
