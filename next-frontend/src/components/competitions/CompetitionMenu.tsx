eae57359ce6db4fc9c0f20a51f463d0f9de6bd70
FinnIckler <finnickler@gmail.com>
2026-07-03 14:53:38 +0200

    move check into live menu

--- next-frontend/src/components/competitions/CompetitionMenu.tsx
+++ next-frontend/src/components/competitions/CompetitionMenu.tsx
@@ -6,8 +6,6 @@ import {
 } from "@/lib/wca/competitions/tabs";
 import TabMenu from "@/components/competitions/TabMenu";
 import LiveMenu from "@/components/competitions/LiveMenu";
-import { Alert, ClientOnly, Link } from "@chakra-ui/react";
-import I18nHTMLTranslate from "@/components/I18nHTMLTranslate";
 
 const LIVE_RESULT_BETA = !!process.env.LIVE_RESULT_BETA;
 
@@ -19,32 +17,6 @@ export default function CompetitionMenu({
   competitionInfo: components["schemas"]["CompetitionInfo"];
 }) {
   const { scoretaking_software } = competitionInfo;
-  if (scoretaking_software !== "internal" && LIVE_RESULT_BETA) {
-    return (
-      <Alert.Root status="error">
-        <Alert.Indicator />
-        <Alert.Content>
-          <ClientOnly>
-            <I18nHTMLTranslate
-              i18nKey={`competitions.live.incompatible.${scoretaking_software}`}
-            />
-          </ClientOnly>
-          {scoretaking_software === "wca_live" && (
-            <>
-              {" "}
-              <Link
-                href={`https://live.worldcubeassociation.org/link/competitions/${competitionInfo.id}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {competitionInfo.id}
-              </Link>
-            </>
-          )}
-        </Alert.Content>
-      </Alert.Root>
-    );
-  }
 
   if (!hasPassed(competitionInfo.start_date)) {
     const tabs = beforeCompetitionTabs(competitionInfo);
@@ -56,7 +28,14 @@ export default function CompetitionMenu({
   }
 
   if (!hasPassed(competitionInfo.end_date) || LIVE_RESULT_BETA) {
-    return <LiveMenu competitionInfo={competitionInfo}>{children}</LiveMenu>;
+    return (
+      <LiveMenu
+        competitionInfo={competitionInfo}
+        scoretakingSoftware={scoretaking_software}
+      >
+        {children}
+      </LiveMenu>
+    );
   }
   // TODO: Differentiate if the results have been posted
   const tabs = afterCompetitionTabs(competitionInfo);
