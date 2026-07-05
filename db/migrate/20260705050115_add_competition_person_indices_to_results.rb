788802f9f0ca67018fe0d39a44571ddbc0dfc42d
Daniel James <djdany444@gmail.com>
2026-07-05 12:02:13 +0530

    Fix rubocop

--- db/migrate/20260705050115_add_competition_person_indices_to_results.rb
+++ db/migrate/20260705050115_add_competition_person_indices_to_results.rb
@@ -2,7 +2,7 @@
 
 class AddCompetitionPersonIndicesToResults < ActiveRecord::Migration[7.0]
   def change
-    add_index :results, [:competition_id, :person_id]
-    add_index :inbox_results, [:competition_id, :person_id]
+    add_index :results, %i[competition_id person_id]
+    add_index :inbox_results, %i[competition_id person_id]
   end
 end
