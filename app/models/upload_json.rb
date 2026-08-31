683badcaa3379d063c41c48408372655a236e612
Gregor Billing <gbilling@worldcubeassociation.org>
2026-08-31 17:32:54 +0200

    Simplify parser logic branching

--- app/models/upload_json.rb
+++ app/models/upload_json.rb
@@ -14,11 +14,13 @@ class UploadJson
         if is_wcif
           import_version = parsed_json["formatVersion"] || Competition::WCIF_STABLE_VERSION
           Competition.validate_wcif_schema!(parsed_json, version: import_version)
-          errors.add(:results_file, "is not for this competition but for #{parsed_json['id']}!") if parsed_json["id"] != competition_id
         else
           JSON::Validator.validate!(ResultsValidators::JSONSchemas::RESULT_JSON_SCHEMA, parsed_json)
-          errors.add(:results_file, "is not for this competition but for #{parsed_json['competitionId']}!") if parsed_json["competitionId"] != competition_id
         end
+
+        # check the ID registered in the JSON next
+        json_competition_id = is_wcif ? parsed_json['id'] : parsed_json['competitionId']
+        errors.add(:results_file, "is not for this competition but for #{json_competition_id}!") if json_competition_id != competition_id
       rescue JSON::ParserError
         errors.add(:results_file, is_wcif ? "must be a valid JSON file" : "must be a JSON file from the Workbook Assistant")
       rescue JSON::Schema::ValidationError => e
@@ -38,96 +40,39 @@ class UploadJson
     results_file.rewind
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+  # rubocop:disable-next Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def temporary_results_data
     competition = Competition.includes(competition_events: [:rounds]).find(competition_id)
     persons_to_import = []
 
-    if is_wcif
-      import_version = parsed_json["formatVersion"] || Competition::WCIF_STABLE_VERSION
-
-      parsed_json["persons"].each do |p|
-        new_person_attributes = {
-          id: [competition_id, p["registrantId"]],
-          wca_id: p["wcaId"],
-          name: p["name"],
-          country_iso2: p["countryIso2"],
-          gender: p["gender"],
-          dob: p["birthdate"],
-        }
-        # mask uploaded DOB on staging to avoid accidentally importing PII
-        new_person_attributes["dob"] = "1954-12-04" if Rails.env.production? && !EnvConfig.WCA_LIVE_SITE?
-        persons_to_import << InboxPerson.new(new_person_attributes)
-      end
-    else
-      parsed_json["persons"].each do |p|
-        new_person_attributes = {
-          id: [competition_id, p["id"]],
-          wca_id: p["wcaId"],
-          name: p["name"],
-          country_iso2: p["countryId"],
-          gender: p["gender"],
-          dob: p["dob"],
-        }
-        # mask uploaded DOB on staging to avoid accidentally importing PII
-        new_person_attributes["dob"] = "1954-12-04" if Rails.env.production? && !EnvConfig.WCA_LIVE_SITE?
-        persons_to_import << InboxPerson.new(new_person_attributes)
-      end
+    registrations_data.each do |p|
+      new_person_attributes = {
+        id: [competition_id, p[is_wcif ? "registrantId" : "id"]],
+        wca_id: p["wcaId"],
+        name: p["name"],
+        country_iso2: p[is_wcif ? "countryIso2" : "countryId"],
+        gender: p["gender"],
+        dob: p[is_wcif ? "birthdate" : "dob"],
+      }
+      # mask uploaded DOB on staging to avoid accidentally importing PII
+      new_person_attributes["dob"] = "1954-12-04" if Rails.env.production? && !EnvConfig.WCA_LIVE_SITE?
+      persons_to_import << InboxPerson.new(new_person_attributes)
     end
+
     results_to_import = []
     scramble_sets_to_import = []
 
-    if is_wcif
-      parsed_json["events"].each do |event|
-        competition_event = competition.competition_events.find { |ce| ce.event_id == event["id"] }
-        event["rounds"].each do |round|
-          # H2H results are skipped
-          next if round['format'] == "h"
+    parsed_json["events"].each do |event|
+      competition_event = competition.competition_events.find { |ce| ce.event_id == event[is_wcif ? "id" : "eventId"] }
+      event["rounds"].each do |round|
+        # H2H results are skipped, as they get imported via a manual import process. See #13200 for more information
+        next if round['formatId'] == "h"
 
-          incoming_round_type_id = round["id"].split("-r").last
-
-          # In WCIF, round id is something like `333-r1`
-          # But we need `1`, `2`, `3` etc for `round_type_id`.
-          competition_round = competition_event.rounds.find do |cr|
-            [incoming_round_type_id, RoundType.toggle_cutoff(incoming_round_type_id)].include?(cr.round_type_id)
-          end
-          round_type_id = competition_round&.round_type_id || incoming_round_type_id
-
-          # Import results for round
-          round["results"].each do |result|
-            individual_results = result["attempts"].map { |a| a[LiveAttempt.wcif_result_field(import_version)] }
-            # Pad the results with 0 up to 5 results
-            individual_results.fill(0, individual_results.length...5)
-
-            new_result_attributes = {
-              person_id: result["personId"],
-              pos: result["ranking"],
-              global_pos: result["ranking"],
-              event_id: event["id"],
-              round_type_id: round_type_id,
-              format_id: round["format"],
-              best: result["best"],
-              average: result["average"],
-              value1: individual_results[0],
-              value2: individual_results[1],
-              value3: individual_results[2],
-              value4: individual_results[3],
-              value5: individual_results[4],
-            }
-            new_res = InboxResult.new(new_result_attributes)
-            new_res.competition = competition
-            new_res.round = competition_round
-            results_to_import << new_res
-          end
-        end
-      end
-    else
-      parsed_json["events"].each do |event|
-        competition_event = competition.competition_events.find { |ce| ce.event_id == event["eventId"] }
-        event["rounds"].each do |round|
-          # H2H results are skipped, as they get imported via a manual import process. See #13200 for more information
-          next if round['formatId'] == "h"
-
+        if is_wcif
+          round_number = ScheduleActivity.parse_activity_code(round["id"])[:round_number]
+          competition_round = competition_event.rounds.find_by!(number: round_number)
+          round_type_id = competition_round.round_type_id
+        else
           # Find the corresponding competition round and get the actual round_type_id
           # (in case the incoming one doesn't correspond to cutoff presence).
           incoming_round_type_id = round["roundId"]
@@ -135,37 +80,46 @@ class UploadJson
             [incoming_round_type_id, RoundType.toggle_cutoff(incoming_round_type_id)].include?(cr.round_type_id)
           end
           round_type_id = competition_round&.round_type_id || incoming_round_type_id
+        end
 
-          # Import results for round
-          round["results"].each do |result|
+        # Import results for round
+        round["results"].each do |result|
+          if is_wcif
+            import_version = parsed_json["formatVersion"] || Competition::WCIF_STABLE_VERSION
+            individual_results = result["attempts"].map { it[LiveAttempt.wcif_result_field(import_version)] }
+          else
             individual_results = result["results"]
-            # Pad the results with 0 up to 5 results
-            individual_results.fill(0, individual_results.length...5)
-            new_result_attributes = {
-              person_id: result["personId"],
-              pos: result["position"],
-              # For non-linked rounds global_pos equals pos; linked rounds get recomputed during import.
-              global_pos: result["position"],
-              event_id: event["eventId"],
-              round_type_id: round_type_id,
-              format_id: round["formatId"],
-              best: result["best"],
-              average: result["average"],
-              value1: individual_results[0],
-              value2: individual_results[1],
-              value3: individual_results[2],
-              value4: individual_results[3],
-              value5: individual_results[4],
-            }
-            new_res = InboxResult.new(new_result_attributes)
-            # Using this way of setting the attribute saves two SELECTs per result
-            # to validate the competition and round presence.
-            # (a lot of time considering all the results to import!)
-            new_res.competition = competition
-            new_res.round = competition_round
-            results_to_import << new_res
           end
 
+          # Pad the results with 0 up to 5 results
+          individual_results.fill(0, individual_results.length...5)
+
+          new_result_attributes = {
+            person_id: result["personId"],
+            pos: result[is_wcif ? "ranking" : "position"],
+            # For non-linked rounds global_pos equals pos; linked rounds get recomputed during import.
+            global_pos: result[is_wcif ? "ranking" : "position"],
+            event_id: event[is_wcif ? "id" : "eventId"],
+            round_type_id: round_type_id,
+            format_id: round[is_wcif ? "format" : "formatId"],
+            best: result["best"],
+            average: result["average"],
+            value1: individual_results[0],
+            value2: individual_results[1],
+            value3: individual_results[2],
+            value4: individual_results[3],
+            value5: individual_results[4],
+          }
+          new_res = InboxResult.new(new_result_attributes)
+          # Using this way of setting the attribute saves two SELECTs per result
+          # to validate the competition and round presence.
+          # (a lot of time considering all the results to import!)
+          new_res.competition = competition
+          new_res.round = competition_round
+          results_to_import << new_res
+        end
+
+        unless is_wcif
           # Import scrambles for round
           # I am too lazy to write actual parsing logic for A->1, B->2, ..., AA->27 etc.
           #   so this snippet is just "faking" the parse by using length-adjusted lexicographic sorting
@@ -191,17 +145,19 @@ class UploadJson
         end
       end
     end
+
     {
       results_to_import: results_to_import,
       scramble_sets_to_import: scramble_sets_to_import,
       persons_to_import: persons_to_import,
     }
   end
-  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
   def registrations_data
+    return parsed_json["persons"] unless is_wcif
+
     parsed_json["persons"].select do |person|
       person.dig("registration", "status") == "accepted"
-    end.map(&:with_indifferent_access)
+    end
   end
 end
