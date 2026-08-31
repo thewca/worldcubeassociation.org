e611c97ac33235f04da20f2a6eb37617c143fcb2
FinnIckler <finnickler@gmail.com>
2026-08-31 14:37:25 +0200

    add comment about why there is only one slug

--- next-frontend/src/app/(authjs)/api/auth/[...all]/route.ts
+++ next-frontend/src/app/(authjs)/api/auth/[...all]/route.ts
@@ -1,4 +1,6 @@
 import { toNextJsHandler } from "better-auth/next-js";
 import { auth } from "@/auth";
 
+// This is just the auth route for next, for payload, it is mounted as part of the payload catch all
+// see payload.auth.ts createCmsAuth->basePath
 export const { GET, POST } = toNextJsHandler(auth);
