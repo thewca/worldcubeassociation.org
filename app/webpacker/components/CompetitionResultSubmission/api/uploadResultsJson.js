a3090f556fc3a4c250f84b99af6cdabe58694b4f
Daniel James <djdany444@gmail.com>
2026-09-04 18:16:12 +0530

    Add support for importing competition results via WCIF files (#15394)
    * Add support for importing competition results via WCIF files
    
    * Review changes
    
    * Some more cleanups
    
    * Review change
    
    * Remove method after merge
    
    * Only allow WCIF upload for 'external' scoretaking
    
    * Merge upload views
    
    * Listen only to the checkbox
    
    * Wrap import into transaction
    
    * Pass along whether there are accepted registrations at all
    
    * Simplify parser logic branching
    
    * Consider existing registrations for import default flag
    
    * Make RuboCop happy
    
    * Fix callsite of registration data import
    
    * Swap out default values
    
    * Refine checkbox defaults and toggles in upload pane
    
    * Fix controller typo
    
    * Change checkbox default again
    
    * Only allow JSON files in file input
    
    ---------
    
    Co-authored-by: Gregor Billing <gbilling@worldcubeassociation.org>

--- app/webpacker/components/CompetitionResultSubmission/api/uploadResultsJson.js
+++ app/webpacker/components/CompetitionResultSubmission/api/uploadResultsJson.js
@@ -6,12 +6,16 @@ export default async function uploadResultsJson({
   resultFile,
   markResultSubmitted,
   storeUploadedJson,
+  importRegistrations,
+  isWcifFormat,
 }) {
   const formData = new FormData();
   formData.append('results_file', resultFile);
   formData.append('competition_id', competitionId);
   formData.append('mark_result_submitted', markResultSubmitted);
   formData.append('store_uploaded_json', storeUploadedJson);
+  formData.append('is_wcif', isWcifFormat);
+  formData.append('import_registrations', !!importRegistrations);
 
   const { data } = await fetchJsonOrError(
     actionUrls.competitionResultSubmission.uploadResultsJson(competitionId),
