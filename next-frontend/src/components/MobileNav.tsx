08b3dc8422bb7876a10511a6ce7cd3d5453a3b61
FinnIckler <finnickler@gmail.com>
2026-09-10 14:03:03 +0200

    add comment

--- next-frontend/src/components/MobileNav.tsx
+++ next-frontend/src/components/MobileNav.tsx
@@ -7,6 +7,8 @@ import { Button, Collapsible, useCollapsible } from "@chakra-ui/react";
 // through useCollapsibleContext.
 const CloseMobileNavContext = createContext<() => void>(() => {});
 
+// We currently need to have a separate Context because ark doesn't provide a CloseTrigger
+// https://github.com/chakra-ui/ark/discussions/4025
 export function MobileNavRoot({ children }: { children: React.ReactNode }) {
   const collapsible = useCollapsible();
 
