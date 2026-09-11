80eb3284cb2e95be734c6c35f337e72dff32b79a
FinnIckler <finnickler@gmail.com>
2026-09-11 17:12:08 +0200

    remove now unnecessary ClientOnly

--- next-frontend/src/app/(wca)/(with-background)/competitions/page.tsx
+++ next-frontend/src/app/(wca)/(with-background)/competitions/page.tsx
@@ -203,23 +203,21 @@ export default function CompetitionsPage() {
 
   return (
     <VStack gap="8" width="full">
-      <ClientOnly>
-        {!session.isPending && !session.data && (
-          <RemovableCard
-            imageUrl="newcomer.png"
-            heading="Why Compete?"
-            descriptionAs="div"
-            description={
-              <Trans
-                t={t}
-                i18nKey="competitions.index.why_compete_description_html"
-              />
-            }
-            buttonText="Learn More"
-            buttonUrl="/faq"
-          />
-        )}
-      </ClientOnly>
+      {!session.isPending && !session.data && (
+        <RemovableCard
+          imageUrl="newcomer.png"
+          heading="Why Compete?"
+          descriptionAs="div"
+          description={
+            <Trans
+              t={t}
+              i18nKey="competitions.index.why_compete_description_html"
+            />
+          }
+          buttonText="Learn More"
+          buttonUrl="/faq"
+        />
+      )}
       <Card.Root size={{ base: "sm", md: "md" }} width="full">
         <Tabs.Root
           variant="subtle"
