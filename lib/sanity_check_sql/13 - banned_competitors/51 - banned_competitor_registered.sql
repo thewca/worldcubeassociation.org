c7d54e733df1c742bc8d352f692982c72273386c
Sébastien Auroux <sebastien.auroux.sa@gmail.com>
2026-09-12 22:23:16 +0200

    Rework sanity check 51 to use competing_status instead of deleted_at

--- lib/sanity_check_sql/13 - banned_competitors/51 - banned_competitor_registered.sql
+++ lib/sanity_check_sql/13 - banned_competitors/51 - banned_competitor_registered.sql
@@ -10,8 +10,8 @@ WITH upcoming_competitions AS (
 banned_persons AS (
   SELECT
     role.user_id,
-    role.start_date AS start_date,
-    role.end_date AS end_date,
+    role.start_date,
+    role.end_date,
     users.wca_id AS person_id,
     users.name AS person_name
   FROM user_roles AS role
@@ -22,14 +22,14 @@ banned_persons AS (
   WHERE user_groups.group_type = 'banned_competitors'
 )
 SELECT DISTINCT
-  banned.person_name AS person_name,
-  banned.person_id AS person_id,
+  banned.person_name,
+  banned.person_id,
   comps.id AS competition_id,
   comps.start_date AS competition_start_date,
   comps.end_date AS competition_end_date,
   banned.start_date AS ban_start_date,
   banned.end_date AS ban_end_date,
-  (reg.deleted_at IS NOT NULL) AS deleted
+  reg.competing_status
 FROM registrations AS reg
 INNER JOIN upcoming_competitions AS comps
 ON reg.competition_id = comps.id
@@ -37,5 +37,5 @@ INNER JOIN banned_persons AS banned
 ON reg.user_id = banned.user_id
 WHERE banned.start_date <= comps.start_date
   AND (banned.end_date IS NULL OR banned.end_date > comps.end_date)
-  AND reg.accepted_at IS NOT NULL
-  AND reg.deleted_at IS NULL;
+  -- Statuses covered by Registration#might_attend? in app/models/registration.rb
+  AND reg.competing_status IN ('accepted', 'waiting_list');
