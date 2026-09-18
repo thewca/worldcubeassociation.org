ac4e37e513021cf545e2e4869dae00aebd172c5b
FinnIckler <finnickler@gmail.com>
2026-09-18 14:50:31 +0200

    use route helper

--- spec/support/cookie_banner_helper.rb
+++ spec/support/cookie_banner_helper.rb
@@ -8,7 +8,7 @@ module CookieBannerHelper
     # a subsequent `clear_cookies` can drop the session cookie before the request goes out, and the
     # acknowledgement is never recorded in the database.
     page.driver.with_playwright_page do |pw_page|
-      pw_page.expect_response(%r{/profile/acknowledge-cookies}) do
+      pw_page.expect_response("**#{profile_acknowledge_cookies_path}") do
         pw_page.click(ACKNOWLEDGE_SELECTOR)
       end
     end
