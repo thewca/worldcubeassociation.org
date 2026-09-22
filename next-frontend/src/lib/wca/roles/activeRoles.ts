a5d3899516e1739d204fcc82df3090b3bc5fc22f
FinnIckler <finnickler@gmail.com>
2026-09-22 15:18:24 +0200

    Fix /translators not showing all of them

--- next-frontend/src/lib/wca/roles/activeRoles.ts
+++ next-frontend/src/lib/wca/roles/activeRoles.ts
@@ -1,12 +1,15 @@
 import { serverClient } from "@/lib/wca/wcaAPI";
 import { cache } from "react";
 
-const getActiveRolesOfType = cache(async (groupType: string) => {
-  return await serverClient.GET("/v0/user_roles", {
-    params: { query: { isActive: true, groupType } },
-  });
-});
+const getActiveRolesOfType = cache(
+  async (groupType: string, perPage: number = 25) => {
+    return await serverClient.GET("/v0/user_roles", {
+      params: { query: { isActive: true, groupType, per_page: perPage } },
+    });
+  },
+);
 
 export const getOfficersRoles = () => getActiveRolesOfType("officers");
 export const getBoardRoles = () => getActiveRolesOfType("board");
-export const getTranslatorRoles = () => getActiveRolesOfType("translators");
+export const getTranslatorRoles = () =>
+  getActiveRolesOfType("translators", 100);
